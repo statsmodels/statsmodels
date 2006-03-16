48833e27f47f0e2e2c1ff3be54a9ccd44047e690
jonathan.taylor <>
2006-03-16 08:58:12 -0700

    cleaned "from numpy import *" in lib/fmri/utils
    got rid of unnecessary "reduceall"

--- lib/statistics/regression.py
+++ lib/statistics/regression.py
@@ -2,7 +2,7 @@ import numpy as N
 import numpy.linalg as L
 import enthought.traits as traits
 from model import Model
-from neuroimaging.fmri import utils
+import utils
 
 class RegressionOutput(traits.HasTraits):
 
