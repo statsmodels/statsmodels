ad1747597131f8eaa72d71751cd5732fe7df3e3c
jarrod.millman <>
2006-06-23 07:34:21 -0700

    fixed tests in test_regression

--- lib/neuroimaging/statistics/tests/test_regression.py
+++ lib/neuroimaging/statistics/tests/test_regression.py
@@ -1,8 +1,7 @@
-import neuroimaging.statistics as S
 import unittest
 import numpy.random as R
 import numpy as N
-
+from scipy.sandbox.models.regression import OLSModel, ARModel
 
 W = R.standard_normal
 
@@ -11,32 +10,32 @@ class RegressionTest(unittest.TestCase):
     def testOLS(self):
         X = W((40,10))
         Y = W((40,))
-        model = S.regression.OLSModel(design=X)
+        model = OLSModel(design=X)
         results = model.fit(Y)
-        self.assertEquals(results.df_resid(), 30)
+        self.assertEquals(results.df_resid, 30)
 
     def testAR(self):
         X = W((40,10))
         Y = W((40,))
-        model = S.regression.ARModel(design=X, rho=0.4)
+        model = ARModel(design=X, rho=0.4)
         results = model.fit(Y)
-        self.assertEquals(results.df_resid(), 30)
+        self.assertEquals(results.df_resid, 30)
 
     def testOLSdegenerate(self):
         X = W((40,10))
         X[:,0] = X[:,1] + X[:,2]
         Y = W((40,))
-        model = S.regression.OLSModel(design=X)
+        model = OLSModel(design=X)
         results = model.fit(Y)
-        self.assertEquals(results.df_resid(), 31)
+        self.assertEquals(results.df_resid, 31)
 
     def testARdegenerate(self):
         X = W((40,10))
         X[:,0] = X[:,1] + X[:,2]
         Y = W((40,))
-        model = S.regression.ARModel(design=X, rho=0.9)
+        model = ARModel(design=X, rho=0.9)
         results = model.fit(Y)
-        self.assertEquals(results.df_resid(), 31)
+        self.assertEquals(results.df_resid, 31)
 
 def suite():
     suite = unittest.makeSuite(RegressionTest)
