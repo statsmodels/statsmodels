7318002cabe789cc67a2480741b26c656da8dd52
jonathan.taylor <>
2006-06-26 07:56:23 -0700

    added numpy-aware version of traits to repository -- a la matplotlib
    idea is to switch "enthought.traits" to "neuroimaging.traits" with a check
    for existing enthought.traits in neuroimaging/__init__.py

--- setup.py
+++ setup.py
@@ -1,21 +1,32 @@
 import os, glob, string, shutil
 import sys
 sys.path.insert(0,"lib")
-from distutils.core import setup
+from distutils.core import setup, Extension
 from neuroimaging import packages, __version__, __doc__
 
-def main():
+
+def main(packages):
+
+    packages = ['']+list(packages)
+    ext_modules = []
+    package_dir = {'': 'lib'}
+
+    if 'neuroimaging.extra.enthought' in packages:
+        ext_modules += [Extension('extra.enthought.traits.ctraits',
+                                  ['lib/neuroimaging/extra/enthought/lib/traits/ctraits.c'])]
+        package_dir['neuroimaging.extra.enthought'] = 'lib/neuroimaging/extra/enthought/lib'
 
     setup( name = 'neuroimaging',
            version = __version__,
            description = 'This is a neuroimaging python package',
-           author = 'Various, one of whom is Jonathan Taylor',
-           author_email = 'jonathan.taylor@stanford.edu',
+           author = 'Various',
+           author_email = 'nipy-devel@neuroimaging.scipy.org',
            ext_package = 'neuroimaging',
-           packages=['']+list(packages),
-           package_dir = {'': 'lib'},
+           packages=packages,
+           ext_modules=ext_modules,
+           package_dir = package_dir,
            url = 'http://neuroimaging.scipy.org',
            long_description = __doc__)
 
 
-if __name__ == "__main__": main()
+if __name__ == "__main__": main(packages)
