a278fa15a54f085376990b156c4153a939aa27b3
jonathan.taylor <>
2006-07-05 05:28:11 -0700

    fixed a potential problem in util/numerix.py (setting an unnecessary os.environ var)
    moved the traits "check" to defines.py
    
    changed the default cache directory name for windows

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os, glob, string, shutil
 import sys
 sys.path.insert(0,"lib")
 from distutils.core import setup, Extension
-from neuroimaging import packages, __version__, __doc__
+from neuroimaging import packages, __version__, __doc__, ENTHOUGHT_TRAITS_DEF
 
 
 def main(packages):
@@ -11,7 +11,7 @@ def main(packages):
     ext_modules = []
     package_dir = {'': 'lib'}
 
-    if 'neuroimaging.extra.enthought' in packages:
+    if not ENTHOUGHT_TRAITS_DEF:
         ext_modules += [Extension('extra.enthought.traits.ctraits',
                                   [apply(os.path.join, 'lib/neuroimaging/extra/enthought/traits/ctraits.c'.split('/'))])]
         package_dir['neuroimaging.extra.enthought'] = apply(os.path.join, 'lib/neuroimaging/extra/enthought/'.split('/'))
