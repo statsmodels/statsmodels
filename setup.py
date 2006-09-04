2e46da8b416b9f8ac45f0e3ba8d5f960c572a28c
jarrod.millman <>
2006-09-03 22:07:01 -0700

    clean up

--- setup.py
+++ setup.py
@@ -1,25 +1,26 @@
-import os, glob, string, shutil
+from os.path import join
 import sys
 sys.path.insert(0,"lib")
 from distutils.core import setup, Extension
-from neuroimaging import packages, __version__, __doc__, ENTHOUGHT_TRAITS_DEF
 from numpy.distutils.misc_util import get_numpy_include_dirs
 
+from neuroimaging import packages, __version__, __doc__, ENTHOUGHT_TRAITS_DEF
 
 def main(packages):
 
     packages = ['']+list(packages)
     ext_modules = [Extension('data_io.formats.minc._mincutils',
-                                  [apply(os.path.join, 'lib/neuroimaging/data_io/formats/minc/_mincutils.c'.split('/'))],
-                             extra_link_args=["-lminc"],
-                            include_dirs=get_numpy_include_dirs())]
+      [join(*('lib/neuroimaging/data_io/formats/minc/_mincutils.c'.split('/')))],
+      extra_link_args=["-lminc"],
+      include_dirs=get_numpy_include_dirs())]
 
     package_dir = {'': 'lib'}
 
     if not ENTHOUGHT_TRAITS_DEF:
         ext_modules += [Extension('utils.enthought.traits.ctraits',
-                                  [apply(os.path.join, 'lib/neuroimaging/utils/enthought/traits/ctraits.c'.split('/'))])]
-        package_dir['neuroimaging.utils.enthought'] = apply(os.path.join, 'lib/neuroimaging/utils/enthought/'.split('/'))
+          [join(*('lib/neuroimaging/utils/enthought/traits/ctraits.c'.split('/')))])]
+        package_dir['neuroimaging.utils.enthought'] = \
+          join(*('lib/neuroimaging/utils/enthought/'.split('/')))
 
     setup( name = 'neuroimaging',
            version = __version__,
@@ -34,4 +35,5 @@ def main(packages):
            long_description = __doc__)
 
 
-if __name__ == "__main__": main(packages)
+if __name__ == "__main__":
+    main(packages)
