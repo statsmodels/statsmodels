271b0f6c34183e28e9610eada4d36f880ce2a306
tim.leslie <>
2006-11-25 02:25:44 -0700

    cleanups to assist with the iterator refactoring

--- lib/neuroimaging/algorithms/statistics/regression.py
+++ lib/neuroimaging/algorithms/statistics/regression.py
@@ -14,24 +14,23 @@ class LinearModelIterator(object):
         self.iterator = iter(iterator)
         self.outputs = [iter(output) for output in outputs]
 
-    def model(self, **keywords):
+
+    def model(self):
         """
         This method should take the iterator at its current state and
         return a LinearModel object.
         """
         return None
 
-    def fit(self, **keywords):
+    def fit(self):
         """
         Go through an iterator, instantiating model and passing data,
         going through outputs.
         """
-        tmp = [data for data in self.iterator]
-        for data in tmp:
+        for data in self.iterator:
             shape = data.shape[1:]
             data = data.reshape(data.shape[0], N.product(shape))
-            model = self.model()
-            results = model.fit(data)
+            results = self.model().fit(data)
             for output in self.outputs:
                 out = output.extract(results)
                 if output.nout > 1:
@@ -39,7 +38,6 @@ class LinearModelIterator(object):
                 else:
                     out.shape = shape
 
-                iter(output)
                 output.set_next(data=out)
 
 
