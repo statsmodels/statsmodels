bcc8160d38c1a1f88950c438e0545d83c34b5569
tim.leslie <>
2007-03-07 05:08:39 -0700

    add a flags parameter to test() to allow passing in during interactive sessions

--- lib/neuroimaging/algorithms/statistics/__init__.py
+++ lib/neuroimaging/algorithms/statistics/__init__.py
@@ -3,6 +3,9 @@ TODO
 """
 __docformat__ = 'restructuredtext'
 
-def test(level=1, verbosity=1):
+
+def test(level=1, verbosity=1, flags=[]):
+    from neuroimaging.utils.test_decorators import set_flags
+    set_flags(flags)
     from numpy.testing import NumpyTest
     return NumpyTest().test(level, verbosity)
