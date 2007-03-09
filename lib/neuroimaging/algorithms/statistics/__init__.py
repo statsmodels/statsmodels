0e7e3dbb207815c1fd6c5fc1b0e0011caa3b281d
tim.leslie <>
2007-03-09 11:27:01 -0700

    move set_flags from test_decorators to testutils

--- lib/neuroimaging/algorithms/statistics/__init__.py
+++ lib/neuroimaging/algorithms/statistics/__init__.py
@@ -5,7 +5,7 @@ __docformat__ = 'restructuredtext'
 
 
 def test(level=1, verbosity=1, flags=[]):
-    from neuroimaging.utils.test_decorators import set_flags
+    from neuroimaging.utils.testutils import set_flags
     set_flags(flags)
     from numpy.testing import NumpyTest
     return NumpyTest().test(level, verbosity)
