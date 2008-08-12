e5fbf0a51d686a32b411a93e0667e4d9c1e505c5
Christopher Burns <cburns@berkeley.edu>
2008-08-11 22:47:02 -0700

    Fix import statement.

--- neuroimaging/fixes/scipy/stats/models/bspline.py
+++ neuroimaging/fixes/scipy/stats/models/bspline.py
@@ -20,8 +20,7 @@ import numpy.linalg as L
 
 from scipy.linalg import solveh_banded
 from scipy.optimize import golden
-from scipy.stats.models import _hbspline
-
+from neuroimaging.fixes.scipy.stats.models import _hbspline
 
 # Issue warning regarding heavy development status of this module
 import warnings
