Added loglikelihood functions for all of the families.  All are correct (except the strange results with Gaussian). Continuing to work on test_glm_r and other tests.

nipy/fixes/scipy/stats/models/glm.py
@@ -300,6 +300,8 @@ class GLMResults(LikelihoodModelResults):   # could inherit from RegressionResul
     Class to contain GLM results
     '''
 
+    _llf = None
+
     def __init__(self, model, params, scale, tmp_results):
 #TODO: need to streamline init sig...
         super(GLMResults, self).__init__(model, params,
@@ -308,6 +310,7 @@ class GLMResults(LikelihoodModelResults):   # could inherit from RegressionResul
 
     def _get_results(self, model, tmp_results):
         self.tmp_results = tmp_results # temporarily here
+        self.nobs = model.y.shape[0]
         self.df_resid = model.df_resid
         self.df_model = model.df_model
         self.bse = np.sqrt(np.diag(tmp_results.cov_params(scale=model.scale)))
@@ -317,13 +320,23 @@ class GLMResults(LikelihoodModelResults):   # could inherit from RegressionResul
         self.resid_anscombe = None  # must be defined per family
         self.resid_dev = model.family.devresid(model.y, model.mu)
         self.pearson_X2 = np.sum(np.power((model.y,model.mu),2)/model.mu)
+        self.predict = np.dot(model._exog,self.params)
         null = WLS(model.y,np.ones((len(model.y),1)),weights=model.data_weights).\
                 fit().predict # predicted values of constant only fit
         self.null_deviance = model.family.deviance(model.y,null)
         self.deviance = model.family.deviance(model.y,model.mu)
 
+    @property
+    def llf(self):
+        if self._llf is None:
+            self._llf = self.model.family.logL(self.model.y,self.model.mu, scale=self.scale)
+        return self._llf
+
     def information_criteria(self):
-        self.aic = None
-        self.bic = None
+        llf = self.llf
+        aic = -2 * llf + 2*(self.df_model+1)
+        bic = self.model.family.deviance(self.model.y,self.model.mu) - \
+                (self.df_model+1)*np.log(self.nobs)
+        return dict(aic=aic, bic=bic)
 
 

nipy/fixes/scipy/stats/models/tests/rmodelwrap.py
@@ -2,14 +2,16 @@
 
 import numpy as np
 
+import rpy
 from rpy import r
 
 from exampledata import x, y
 
 class RModel(object):
     ''' Class gives R models scipy.models -like interface '''
-    def __init__(self, y, design, model_type=r.lm):
+    def __init__(self, y, design, model_type=r.lm, **kwds):
         ''' Set up and estimate R model with data and design '''
+        rpy.set_default_mode(rpy.NO_CONVERSION)
         self.y = y
         self.design = design
         self.model_type = model_type
@@ -18,13 +20,52 @@ class RModel(object):
         # Note the '-1' for no intercept - this is included in the design
         self.formula = r('y ~ %s-1' % '+'.join(self._design_cols))
         self.frame = r.data_frame(y=y, x=self.design)
-        self.results = self.model_type(self.formula,
-                                    data = self.frame)
+        results = self.model_type(self.formula,
+                                    data = self.frame, **kwds)
+
+        rpy.set_default_mode(rpy.BASIC_CONVERSION)
+        rsum = r.summary(results)
+        self.rsum = rsum
         # Provide compatible interface with scipy models
+        self.results = results.as_py()
         coeffs = self.results['coefficients']
-        self.beta = np.array([coeffs[c] for c in self._design_cols])
+        self.beta0 = np.array([coeffs[c] for c in self._design_cols])
+        self.nobs = len(self.results['residuals'])
         self.resid = self.results['residuals']
         self.predict = self.results['fitted.values']
+        #hasattr(rglmtestpoiss_res.resid, 'keys')
+        #self.resid = [self.results['residuals'][str(k)] for k in range(1, 1+self.nobs)]
+        #self.predict = [self.results['fitted.values'][str(k)] for k in range(1, 1+self.nobs)]
         self.df_resid = self.results['df.residual']
+        self.beta = rsum['coefficients'][:,0]
+        self.bse = rsum['coefficients'][:,1]
+        self.bt = rsum['coefficients'][:,2]
+        self.bpval = rsum['coefficients'][:,3]
+        #self.R2 = rsum['r.squared']
+        #self.adjR2 = rsum['adj.r.squared']
+        self.R2 = rsum.setdefault('r.squared', None)
+        self.adjR2 = rsum.setdefault('adj.r.squared', None)
+        self.aic = rsum.setdefault('aic', None)
 
+        #self.fstatistic = rsum['fstatistic']
+        self.fstatistic = rsum.setdefault('fstatistic', None)
+        self.df = rsum['df']
+        self.bcov_unscaled = rsum['cov.unscaled']
+        self.bcov = rsum.setdefault('cov.scaled', None)
+        if rsum.has_key('sigma'):
+            self.scale = rsum['sigma']
+        elif rsum.has_key('dispersion'):
+            self.scale = rsum['dispersion']
+        else:
+            self.scale = None
+        #TODO: add more glm results
 
+        if model_type == r.glm:
+            self.getglm()
+
+    def getglm(self):
+        self.deviance = self.rsum['deviance']
+        self.resid = [self.results['residuals'][str(k)] for k in range(1, 1+self.nobs)]
+        self.predict = [self.results['linear.predictors'][str(k)] for k in range(1, 1+self.nobs)]
+        self.predictedy = [self.results['fitted.values'][str(k)] for k in range(1, 1+self.nobs)]
+        self.weights = [self.results['weights'][str(k)] for k in range(1, 1+self.nobs)]