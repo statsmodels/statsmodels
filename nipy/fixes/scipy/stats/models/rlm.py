Made both standardized MAD and MAD. Fixed/hacked hampel test in test_rlm. Added psi_derivs to robust.norms for robust covariance matrices according to Huber and SAS. Not looking right yet.  Need to add tests from SAS.

nipy/fixes/scipy/stats/models/rlm.py
@@ -98,13 +98,12 @@ class RLM(LikelihoodModel):
     def initialize(self):
         self.history = {'deviance' : [np.inf], 'params' : [], 'scale' : []}
         self.iteration = 0
-#        self.y = self._endog
         self.calc_params = np.linalg.pinv(self._exog)
         self.normalized_cov_params = np.dot(self.calc_params,
                                         np.transpose(self.calc_params))
 #        self.df_resid = self._exog.shape[0] - utils.rank(self._exog)
-        self.df_resid = np.nan  # to avoid estimating residuals in WLS?
-        self.df_model = utils.rank(self._exog)-1
+        self.df_resid = np.nan  # to avoid estimating residuals in WLS? needed?
+        self.df_model = np.float(utils.rank(self._exog)-1)
 
     def score(self, params):
         pass
@@ -146,9 +145,13 @@ class RLM(LikelihoodModel):
 #            return scale.scale_est(self, resid)**2
 #        return np.median(np.fabs(resid))/Gaussian.ppf(3/4.)
 
-    def fit(self, maxiter=100, tol=1e-5, scale_est='MAD', init=None):
+    def fit(self, maxiter=100, tol=1e-5, scale_est='MAD', init=None, cov='H1'):
         self.scale_est = scale_est  # is this the best place to put this
                                     # are the other scales implemented?
+        if not cov.upper() in ["H1","H2","H3"]:
+            raise AttributeError, "Covariance matrix %s not understood" % cov
+        else:
+            self.cov = cov.upper()
         if not init:
             wls_results = WLS(self._endog, self._exog).fit()
             # initial guess is just OLS by default
@@ -182,17 +185,41 @@ class RLMResults(LikelihoodModelResults):
         self._get_results(model)
 
     def _get_results(self, model):
+        self.df_model = model.df_model
+        self.df_resid = np.float(model._exog.shape[0] - utils.rank(model._exog))
         self.fitted_values = np.dot(model._exog, self.params)
         self.resid = model._endog - self.fitted_values   # before bcov
         self.calc_params = model.calc_params    # for bvoc,
                                                 # this is getting sloppy
         self.bcov_unscaled = self.cov_params(scale=1)
-        self.bse = np.sqrt(np.diag(self.bcov_unscaled))
-        self.nobs = model._exog.shape[0]
-        self.stddev = 0 # TODO: Discussion in Huber (1981) Ch. 7
+
+        self.nobs = np.float(model._exog.shape[0])
+        m = 1./self.nobs * np.sum(model.M.psi_deriv(self.resid))
+        self.m = m
+        var_psiprime = 1./self.nobs * np.sum((model.M.psi_deriv(self.resid) - m)**2)
+# could be np.var(model.M.psi_deriv(self.resid))
+        self.var_psiprime = var_psiprime
+        k = 1 + (self.df_model+1)/self.nobs * var_psiprime/m**2
+        self.k = k
         self.weights = model.weights
-        self.df_model = model.df_model
-        self.df_resid = model._exog.shape[0] - utils.rank(model._exog)
+        if model.cov == "H1":
+            self.bcov_scaled = k**2 * (1/self.df_resid)*np.sum(model.M.psi(self.resid)**2)\
+                    /((1/self.nobs)*np.sum(model.M.psi_deriv(self.resid)))**2\
+                    *model.normalized_cov_params    # last term is dot(X.T,X)^-1
+        else:
+            W_inv = np.dot(np.dot(model.calc_params,model.M.psi_deriv(self.resid)),\
+                    model.calc_params.T)
+# should be
+# [W_jk]^-1 = [SUM(psi_deriv(r_i)*x_ij*x_jk)]^-1, but we'll see...
+            if model.cov == "H2":
+                self.bcov_scaled = k*(1/self.df_resid)*np.sum(\
+                        model.M.psi(self.resid)**2)/((1/self.nobs)*np.sum(\
+                        model.M.psi_deriv(self.resid)))*W_inv
+            elif model.cov == "H3":
+                self.bcov_scaled = k**-1*1/self.df_resid*np.sum(\
+                    model.M.psi(self.resid)**2)*np.dot(np.dot(W_inv,
+                    np.dot(model._exog.T,model._exog)),W_inv)
+        self.bse = np.sqrt(np.diag(self.bcov_scaled))
 
 if __name__=="__main__":
 #NOTE: This is to be removed
@@ -226,8 +253,8 @@ if __name__=="__main__":
     model_andrewWave = RLM(endog, exog, M=norms.AndrewWave())
     results_andrewWave = model_andrewWave.fit()
 
-#    model_hampel = RLM(endog, exog, M=norms.Hampel(a=1.7,b=3.4,c=8.5)) # convergence problems with scale changed, not with 2,4,8 though?
-#    results_hampel = model_hampel.fit()
+    model_hampel = RLM(endog, exog, M=norms.Hampel(a=1.7,b=3.4,c=8.5)) # convergence problems with scale changed, not with 2,4,8 though?
+    results_hampel = model_hampel.fit()
 
 ### Stack Loss Data ###
     from models.datasets.stackloss.data import load
@@ -238,7 +265,7 @@ if __name__=="__main__":
     results1 = m1.fit()
     m2 = RLM(data.endog, data.exog, M=norms.Hampel())  # has convergence problems with scale changed
     results2 = m2.fit()
-    m3 = RLM(data.endog, data.exog, M=norms.TukeyBiweight())
+    m3 = RLM(data.endog, data.exog, M=norms.TukeyBiweight())   #R=2.5 by default is SAS ?
     results3 = m3.fit()
 # Huber scale estimate do not currently work
 #    m4 = RLM(data.endog, data.exog, M=norms.HuberT())
@@ -251,25 +278,16 @@ if __name__=="__main__":
 
 
 
-#    print '''Least squares fit
-#%s
-#Huber Params, t = 2.
-#%s
-#Ramsay's E Params
-#%s
-#Andrew's Wave Params
-#%s
-#Hampel's 17A Function
-#%s
-#'''
-
-#% (results_ols.params, results_huber.params, results_ramsaysE.params,
-#            results_andrewWave.params, results_hampel.params)
-
-
-
-
-
-
-
+    print '''Least squares fit
+%s
+Huber Params, t = 2.
+%s
+Ramsay's E Params
+%s
+Andrew's Wave Params
+%s
+Hampel's 17A Function
+%s
+''' % (results_ols.params, results_huber.params, results_ramsaysE.params,
+            results_andrewWave.params, results_hampel.params)
 

nipy/fixes/scipy/stats/models/robust/norms.py
@@ -23,8 +23,15 @@ class RobustNorm(object):
         Abstract method:
         psi(z) / z
 
-        TODO: is the above correct... need to look at V&R
         """
+        raise NotImplementedError
+
+    def psi_deriv(self, z):
+        '''
+        Abstract method:
+
+        psi_derive = psi'
+        '''
         raise NotImplementedError
 
     def __call__(self, z):
@@ -48,6 +55,9 @@ class LeastSquares(RobustNorm):
 
     def weights(self, z):
         return np.ones(z.shape, np.float64)
+
+    def psi_deriv(self, z):
+        return np.ones(z,shape, np.float64)
 
 class HuberT(RobustNorm):
 
@@ -85,6 +95,10 @@ class HuberT(RobustNorm):
         test = self.subset(z)
         return test + (1 - test) * self.t / np.fabs(z)
 
+    def psi_deriv(self, z):
+        return np.less_equal(np.fabs(z), self.t)
+
+
 class RamsayE(RobustNorm):
 
     """
@@ -108,6 +122,11 @@ class RamsayE(RobustNorm):
     def weights(self, z):
         z = np.asarray(z)
         return np.exp(-self.a * np.fabs(z))
+
+    def psi_deriv(self, z):
+        return np.exp(-self.a * np.fabs(z)) + z**2*\
+                np.exp(-self.a*np.fabs(z))*-self.a/np.fabs(z)
+# I think this is correct.
 
 class AndrewWave(RobustNorm):
 
@@ -143,6 +162,10 @@ class AndrewWave(RobustNorm):
         test = self.subset(z)
         return test * np.sin(z / a) / (z / a)
 
+    def psi_deriv(self, z):
+        test = self.subset(z)
+        return test*np.cos(z / self.a)/self.a
+
 class TrimmedMean(RobustNorm):
     """
 
@@ -171,6 +194,11 @@ class TrimmedMean(RobustNorm):
     def weights(self, z):
         z = np.asarray(z)
         test = self.subset(z)
+        return test
+
+    def psi_derive(self, z):
+#NOTE I'm not sure if the robust covariance matrix is defined the same for Least Trimmed Mean
+        test = self.subzet(z)
         return test
 
 class Hampel(RobustNorm):
@@ -231,6 +259,9 @@ class Hampel(RobustNorm):
             t3 * a*(c-np.fabs(z))/(np.fabs(z)*(c-b)))
         return v
 
+    def psi_deriv(self, z):
+        t1, t2, t3 = self.subset(z)
+        return t1 + t3 * (self.a*np.sign(z)*z)/(np.fabs(z)*(self.c-self.b))
 
 
 class TukeyBiweight(RobustNorm):
@@ -243,7 +274,8 @@ class TukeyBiweight(RobustNorm):
     """
 
 
-    R = 4.685
+    def __init__(self, R = 4.685):
+        self.R = R
 
     def subset(self, z):
         z = np.fabs(np.asarray(z))
@@ -261,6 +293,10 @@ class TukeyBiweight(RobustNorm):
     def weights(self, z):
         subset = self.subset(z)
         return (1 - (z / self.R)**2)**2 * subset
+
+    def psi_deriv(self, z):
+        return (self.R - z)*(self.R + z)*(self.R**2 - 5*z**2)/self.R**4
+
 
 def estimate_location(a, scale, norm=HuberT(), axis=0, initial=None,
                       niter=30, tol=1.0e-06):

nipy/fixes/scipy/stats/models/robust/scale.py
@@ -26,13 +26,9 @@ def unsqueeze(data, axis, oldshape):
 
 def MAD(a, c=Gaussian.ppf(3/4.), axis=0):  # c \approx .6745
     """
-    Median Absolute Deviation along given axis of an array:
+    The Median Absolute Deviation along given axis of an array:
 
-    Not at the normal
-    median(abs(a - median(a))) / c
-
-    At the normal       # used by R,SAS
-    median(abs(a))/c
+    median(abs(a)) / c
 
     Reference
     ---------
@@ -43,6 +39,16 @@ def MAD(a, c=Gaussian.ppf(3/4.), axis=0):  # c \approx .6745
 #    d = unsqueeze(d, axis, a.shape)
 #    return np.median(np.fabs(a - d) / c, axis=axis)
     return np.median(np.fabs(a))/c
+
+def stanardized_MAD(a, c=Gaussian.ppf(3/4.), axis=0):
+    '''
+
+    The standardized Median Absolute Deviation along given axis of an array:
+
+    MAD = median(abs(a - median(a))) / c
+    '''
+    a = np.assarray(a)
+    return np.median(np.fabs(a - np.median(a)))/c
 
 class Huber(object):
     """

nipy/fixes/scipy/stats/models/tests/rmodelwrap.py
@@ -14,16 +14,16 @@ class RModel(object):
     ''' Class gives R models scipy.models -like interface '''
     def __init__(self, y, design, model_type=r.lm, **kwds):
         ''' Set up and estimate R model with data and design '''
-        rpy.set_default_mode(rpy.NO_CONVERSION)
-        self.y = y
-        self.design = design
+        self.y = np.array(y)
+        self.design = np.array(design)
         self.model_type = model_type
         self._design_cols = ['x.%d' % (i+1) for i in range(
             self.design.shape[1])]
         # Note the '-1' for no intercept - this is included in the design
         self.formula = r('y ~ %s-1' % '+'.join(self._design_cols))
         self.frame = r.data_frame(y=y, x=self.design)
-#          r.library('MASS') # has to be loaded in the tests
+        rpy.set_default_mode(rpy.NO_CONVERSION)
+
         results = self.model_type(self.formula,
                                     data = self.frame, **kwds)
         rpy.set_default_mode(rpy.BASIC_CONVERSION)

nipy/fixes/scipy/stats/models/tests/test_rlm.py
@@ -7,6 +7,8 @@ from numpy.testing import *
 import models
 from rmodelwrap import RModel
 from rpy import r
+import rpy # for hampel test...ugh
+import numpy as np # ditto
 from models.rlm import RLM
 
 DECIMAL = 4
@@ -68,14 +70,20 @@ class test_rlm(check_rlm_results):
         self.res2 = RModel(self.data.endog, self.data.exog,
                         r.rlm, psi="psi.huber")
 
-# This doesn't work because of what appears to be a bug in Rpy
-# though the results agree with R by eyeballing
-#    def test_hampel(self):
-#        results = RLM(self.data.endog, self.data.exog,
-#                    M=models.robust.norms.Hampel()).fit()
-#        self.res1 = results
-#        self.res2 = RModel(self.data.endog, self.data.exog,
-#                        r.rlm, psi="psi.hampel")
+    def test_hampel(self):
+        d = rpy.as_list(r('stackloss'))
+        y = d[0]['stack.loss']
+        x = np.column_stack(np.array(d[0][name]) for name in d[0].keys()[0:-1])
+        x = np.column_stack((x,np.ones((len(x),1))))
+        x = np.column_stack((x[:,2],x[:,1],x[:,0],x[:,3]))
+# why in the world the above works and just passing data.endog and data.exog does not is
+# completely beyond me
+        results = RLM(self.data.endog, self.data.exog,
+                    M=models.robust.norms.Hampel()).fit()
+
+        self.res1 = results
+        self.res2 = RModel(y, x,
+                        r.rlm, psi="psi.hampel")
 
 class test_rlm_bisquare(test_rlm):
     def __init__(self):