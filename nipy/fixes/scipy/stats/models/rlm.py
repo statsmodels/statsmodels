Added tests for covariance matrices.  Added different convergence criteria.  Needs to be checked.  last entry in model.history is wrong.

nipy/fixes/scipy/stats/models/rlm.py
@@ -96,7 +96,8 @@ class RLM(LikelihoodModel):
         self.initialize()
 
     def initialize(self):
-        self.history = {'deviance' : [np.inf], 'params' : [], 'scale' : []}
+        self.history = {'deviance' : [np.inf], 'params' : [np.inf],
+            'weights' : [np.inf], 'sresid' : [np.inf], 'scale' : []}
         self.iteration = 0
         self.calc_params = np.linalg.pinv(self._exog)
         self.normalized_cov_params = np.dot(self.calc_params,
@@ -123,9 +124,12 @@ class RLM(LikelihoodModel):
                     tmp_results.scale).sum()
 
     def update_history(self, tmp_results):
+# TODO: Figure out why the first estimate gets copied to the end of these
         self.history['deviance'].append(self.deviance(tmp_results))
         self.history['params'].append(tmp_results.params)
         self.history['scale'].append(tmp_results.scale)
+        self.history['sresid'].append(tmp_results.resid/tmp_results.scale)
+        self.history['weights'].append(tmp_results._model.weights)
 
     def estimate_scale(self, resid):
         """
@@ -144,8 +148,8 @@ class RLM(LikelihoodModel):
 #            return scale.scale_est(self, resid)**2
 #        return np.median(np.fabs(resid))/Gaussian.ppf(3/4.)
 
-    def fit(self, maxiter=100, tol=1e-5, scale_est='MAD', init=None, cov='H1',
-            update_scale=True):
+    def fit(self, maxiter=100, tol=1e-8, scale_est='MAD', init=None, cov='H1',
+            update_scale=True, conv='dev'):
         """
         Iterated reweighted least squares for robust regression.
 
@@ -160,7 +164,7 @@ class RLM(LikelihoodModel):
 
         tol : float
             The convergence tolerance of the estimate.
-            Defaults is 1e1-5
+            Defaults is 1e1-8
 
         scale_est : string
             'MAD', 'stand_MAD', or 'Huber'
@@ -186,6 +190,15 @@ class RLM(LikelihoodModel):
             weights is held constant over the iteration.  Otherwise, it
             is updated for each fit in the iteration.  Defaults is True.
 
+        conv : string
+            Indicates the convergence criteria.
+            Available options are "coefs" (the coefficients), "weights" (the
+            weights in the iteration), "resids" (the standardized residuals),
+            and "dev" (the un-normalized log-likelihood for the M
+            estimator).
+            The default is "dev".
+
+
         Returns
         -------
         results : object
@@ -196,21 +209,33 @@ class RLM(LikelihoodModel):
             raise AttributeError, "Covariance matrix %s not understood" % cov
         else:
             self.cov = cov.upper()
+        conv = conv.lower()
+        if not conv in ["weights","coefs","dev","resid"]:
+            raise AttributeError, "Convergence argument %s not understood" \
+                % conv
         if not init:
-            wls_results = WLS(self._endog, self._exog).fit()
             # initial guess is OLS by default, ie., weights = 1
-        self.scale = self.estimate_scale(wls_results.resid) # overwrite scale estimate
+            wls_results = WLS(self._endog, self._exog).fit()
+            self.scale = self.estimate_scale(wls_results.resid) #overwrite scale
         self.update_history(wls_results)
+        self.iteration = 0  # so these don't accumulate across fits
         self.iteration += 1
-        while ((np.fabs(self.history['deviance'][self.iteration]-\
-                self.history['deviance'][self.iteration-1])) > tol and \
+        if conv == 'coefs':
+            criterion = self.history['params']
+        elif conv == 'dev':
+            criterion = self.history['deviance']
+        elif conv == 'resid':
+            criterion = self.history['sresid']
+        elif conv == 'weights':
+            criterion = self.history['weights']
+        while ((np.fabs(criterion[self.iteration]-\
+                criterion[self.iteration-1])).all() > tol and \
                 self.iteration < maxiter):
             self.weights = self.M.weights((self._endog - wls_results.predict) \
                         /self.scale)
             wls_results = WLS(self._endog, self._exog,
                                     weights=self.weights).fit()
             if update_scale is True:
-# M&P suggests to use a constant weight, can be iterative or constant of a "resistant" fit
                 self.scale = self.estimate_scale(wls_results.resid)
             self.update_history(wls_results)
             self.iteration += 1

nipy/fixes/scipy/stats/models/tests/test_rlm.py
@@ -10,17 +10,22 @@ from rpy import r
 import rpy # for hampel test...ugh
 import numpy as np # ditto
 from models.rlm import RLM
+import rlm_results
 
 DECIMAL = 4
 
 class check_rlm_results(object):
     '''
-    res2 contains results from Rmodelwrap or were obtained from a statistical
-    packages such as R or Stata and written to model_results
+    res2 contains  results from Rmodelwrap or were obtained from a statistical
+    packages such as R, Stata, or SAS and written to model_results.
+
+    Covariance matrices were obtained from SAS and are imported from
+    rlm_results
     '''
     def test_params(self):
         assert_almost_equal(self.res1.params, self.res2.params, DECIMAL)
 
+    @dec.knownfailureif(True, "Needs to be switched to SAS standard errors")
     def test_standarderrors(self):
         assert_almost_equal(self.res1.bse, self.res2.bse, DECIMAL)
 
@@ -32,17 +37,12 @@ class check_rlm_results(object):
         assert_almost_equal(self.res1.scale, self.res2.scale, DECIMAL-1)
 # off by ~2e-04
 
-#    def test_k2(self):
-#        assert_almost_equal(self.res1.k2, self.res2.k2, DECIMAL)
-# The tuning constant for Huber 2?  This is the tuning constant for
-# MAD scale estimate?  Check references
-
     def test_weights(self):
         assert_almost_equal(self.res1.weights, self.res2.weights, DECIMAL)
 
-    @dec.knownfailureif(True, "Not implemented")
-    def test_stddev(self):
-        assert_almost_equal(self.res1.stddev, self.res2.stddev, DECIMAL)
+#    @dec.knownfailureif(True, "Not implemented")
+#    def test_stddev(self):
+#        assert_almost_equal(self.res1.stddev, self.res2.stddev, DECIMAL)
 
     def test_residuals(self):
         assert_almost_equal(self.res1.resid, self.res2.resid, DECIMAL)
@@ -55,20 +55,32 @@ class check_rlm_results(object):
         assert_almost_equal(self.res1.bcov_unscaled, self.res2.bcov_unscaled,
                     DECIMAL)
 
+    def test_bcov_scaled(self):
+        assert_almost_equal(self.res1.bcov_scaled, self.res2.h1, DECIMAL-2)
+        assert_almost_equal(self.res1.h2, self.res2.h2, DECIMAL-2)
+        assert_almost_equal(self.res1.h3, self.res2.h3, DECIMAL-2)
+
+
 class test_rlm(check_rlm_results):
     from models.datasets.stackloss.data import load
     data = load()
     data.exog = models.functions.add_constant(data.exog)
     def __init__(self):
-#        from models.datasets.stackloss.data import load
-#        self.data = load()
-#        self.data.exog = models.functions.add_constant(self.data.exog)
         results = RLM(self.data.endog, self.data.exog,\
                     M=models.robust.norms.HuberT()).fit()   # default M
+        h2 = RLM(self.data.endog, self.data.exog,\
+                    M=models.robust.norms.HuberT()).fit(cov="H2").bcov_scaled
+        h3 = RLM(self.data.endog, self.data.exog,\
+                    M=models.robust.norms.HuberT()).fit(cov="H3").bcov_scaled
         self.res1 = results
+        self.res1.h2 = h2
+        self.res1.h3 = h3
         r.library('MASS')
         self.res2 = RModel(self.data.endog, self.data.exog,
                         r.rlm, psi="psi.huber")
+        self.res2.h1 = rlm_results.huber_h1
+        self.res2.h2 = rlm_results.huber_h2
+        self.res2.h3 = rlm_results.huber_h3
 
     def test_hampel(self):
         d = rpy.as_list(r('stackloss'))
@@ -76,22 +88,39 @@ class test_rlm(check_rlm_results):
         x = np.column_stack(np.array(d[0][name]) for name in d[0].keys()[0:-1])
         x = np.column_stack((x,np.ones((len(x),1))))
         x = np.column_stack((x[:,2],x[:,1],x[:,0],x[:,3]))
-# why in the world the above works and just passing data.endog and data.exog
-# does not is beyond me
+# I don't know why the above works and passing data.endog and data.exog
+# does not
         results = RLM(self.data.endog, self.data.exog,
                     M=models.robust.norms.Hampel()).fit()
-
+        h2 = RLM(self.data.endog, self.data.exog,\
+                    M=models.robust.norms.Hampel()).fit(cov="H2").bcov_scaled
+        h3 = RLM(self.data.endog, self.data.exog,\
+                    M=models.robust.norms.Hampel()).fit(cov="H3").bcov_scaled
         self.res1 = results
+        self.res1.h2 = h2
+        self.res1.h3 = h3
         self.res2 = RModel(y, x,
                         r.rlm, psi="psi.hampel")
+        self.res2.h1 = rlm_results.hampel_h1
+        self.res2.h2 = rlm_results.hampel_h2
+        self.res2.h3 = rlm_results.hampel_h3
 
 class test_rlm_bisquare(test_rlm):
     def __init__(self):
         results = RLM(self.data.endog, self.data.exog,
                     M=models.robust.norms.TukeyBiweight()).fit()
+        h2 = RLM(self.data.endog, self.data.exog,\
+                    M=models.robust.norms.Hampel()).fit(cov="H2").bcov_scaled
+        h3 = RLM(self.data.endog, self.data.exog,\
+                    M=models.robust.norms.Hampel()).fit(cov="H3").bcov_scaled
         self.res1 = results
+        self.res1.h2 = h2
+        self.res1.h3 = h3
         self.res2 = RModel(self.data.endog, self.data.exog,
                         r.rlm, psi="psi.bisquare")
+        self.res2.h1 = rlm_results.bisquare_h1
+        self.res2.h2 = 0#rlm_results.bisquare_h2
+        self.res2.h3 = 0#rlm_results.bisquare_h3
 
 if __name__=="__main__":
     run_module_suite()