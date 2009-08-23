8a8d8c2ff3e978ab8dedafac22137d9f380e6c6d
Skipper Seabold <jsseabold@gmail.com>
2009-08-23 14:51:50 -0400

    Added a wrapper to __init__.py so that nosetests can be called the same across platforms.

--- scikits/statsmodels/__init__.py
+++ scikits/statsmodels/__init__.py
@@ -20,11 +20,14 @@ from datasets import *
 # tried add_subpackage in both setup.py
 
 __all__ = filter(lambda s:not s.startswith('_'),dir())
-from numpy.testing import Tester
+#from numpy.testing import Tester
 #test = Tester().test(extra_argv=["--exe"])
-test = Tester().test
+#test = Tester().test
+#test.extra_argv = ["--exe"]
 #TODO:
 #this can't work until bug in NoseTester is fixed?
+#fixed bugs in nosetester and it runs an infinite recursion loop
+#need to change the default variable without
 #def test():
 #    '''
 #    on posix systems, this need to be the test call
@@ -33,3 +36,46 @@ test = Tester().test
 #    import os
 #    package_name = os.path.dirname(os.path.abspath(__file__))
 #    Tester(package=package_name).test(extra_argv=["--exe"])
+from numpy.testing import Tester
+class NoseWrapper(Tester):
+    '''
+    This is simply a wrapper for numpt.testing.Tester, so that extra_argv can
+    be changed from its default None to ['--exe'] so that the tests can be run
+    the same across platforms.
+    '''
+    def test(self, label='fast', verbose=1, extra_argv=['--exe'], doctests=False,
+             coverage=False):
+        ''' Run tests for module using nose
+
+        %(test_header)s
+        doctests : boolean
+            If True, run doctests in module, default False
+        coverage : boolean
+            If True, report coverage of NumPy code, default False
+            (Requires the coverage module:
+             http://nedbatchelder.com/code/modules/coverage.html)
+        '''
+
+        # cap verbosity at 3 because nose becomes *very* verbose beyond that
+        verbose = min(verbose, 3)
+
+        from numpy.testing import utils
+        utils.verbose = verbose
+
+        if doctests:
+            print "Running unit tests and doctests for %s" % self.package_name
+        else:
+            print "Running unit tests for %s" % self.package_name
+
+        self._show_system_info()
+
+        # reset doctest state on every run
+        import doctest
+        doctest.master = None
+
+        argv, plugins = self.prepare_test_args(label, verbose, extra_argv,
+                                               doctests, coverage)
+        from numpy.testing.noseclasses import NumpyTestProgram
+        t = NumpyTestProgram(argv=argv, exit=False, plugins=plugins)
+        return t.result
+test = NoseWrapper().test
