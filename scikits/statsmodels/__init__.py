ad222fca9488ebee64b287256eeb320f0d85698f
Skipper Seabold <jsseabold@gmail.com>
2009-08-23 20:31:04 -0400

    Created stattests.py.  Deleted read_design (functionality in np.io.  Not used in nipy code.).  Deleted PostRegression class moved summary method to RegressionResults and statistical tests to stattests.  Messed with summary string formatting in summary method. Looks better but still needs work.  Regression docs are covered. omni_norm_test gets run by nose, and the nottest decorators don't seem to exclude it.

--- scikits/statsmodels/__init__.py
+++ scikits/statsmodels/__init__.py
@@ -7,35 +7,18 @@ __docformat__ = 'restructuredtext'
 from version import __version__
 from info import __doc__
 
-#import regression
 from regression import *
-#import glm
 from glm import *
-#import rlm
 from rlm import *
 import model
 import tools
 import datasets # why did I have to add this but robust and family are imported?
-from datasets import *
-# tried add_subpackage in both setup.py
+#from datasets import *
 
 __all__ = filter(lambda s:not s.startswith('_'),dir())
 #from numpy.testing import Tester
 #test = Tester().test(extra_argv=["--exe"])
 #test = Tester().test
-#test.extra_argv = ["--exe"]
-#TODO:
-#this can't work until bug in NoseTester is fixed?
-#fixed bugs in nosetester and it runs an infinite recursion loop
-#need to change the default variable without
-#def test():
-#    '''
-#    on posix systems, this need to be the test call
-#    '''
-#    from numpy.testing import Tester
-#    import os
-#    package_name = os.path.dirname(os.path.abspath(__file__))
-#    Tester(package=package_name).test(extra_argv=["--exe"])
 from numpy.testing import Tester
 class NoseWrapper(Tester):
     '''
