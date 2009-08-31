3d7bdf8e8ba4bad8b14769dfe0c6ac77e3c1ffde
Josef Perktold <josef.pktd@gmail.com>
2009-08-31 10:51:19 -0400

    run examples, correct import error in regression, incomplete test coverage for summary and stattools

--- scikits/statsmodels/examples/example_ols_minimal.py
+++ scikits/statsmodels/examples/example_ols_minimal.py
@@ -7,7 +7,7 @@ import scikits.statsmodels as sm
 
 nsample = 100
 x = np.linspace(0,10, 100)
-X = sm.tools.add_constant(np.column_stack((x, x**2)))
+X = sm.add_constant(np.column_stack((x, x**2)))
 beta = np.array([1, 0.1, 10])
 y = np.dot(X, beta) + np.random.normal(size=nsample)
 
--- scikits/statsmodels/examples/run_all.py
+++ scikits/statsmodels/examples/run_all.py
@@ -1,9 +1,9 @@
 
 
 
-filelist = ['example_glsar.py', 'example_wls.py',
+filelist = ['example_glsar.py', 'example_wls.py', 'example_gls.py',
             'example_glm.py', 'example_ols_tftest.py', 'example_rpy.py',
-            'example_gls.py', 'example_ols.py', 'example_rlm.py']
+            'example_ols.py', 'example_ols_minimal.py', 'example_rlm.py']
 
 for f in filelist:
     try:
