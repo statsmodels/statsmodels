073b731ea423476e43820331dea4b6afb2dc66d0
Josef Perktold <josef.pktd@gmail.com>
2009-09-08 00:57:12 -0400

    initial import of times series analysis draft versions

---
+++ scikits/statsmodels/sandbox/tsa/arima.py
@@ -0,0 +1,379 @@
+'''ARMA process and estimation with scipy.signal.lfilter
+
+2009-09-06: copied from try_signal.py
+    reparameterized same as signal.lfilter (positive coefficients)
+
+
+Notes
+-----
+* pretty fast
+* checked with Monte Carlo and cross comparison with statsmodels yule_walker
+  for AR numbers are close but not identical to yule_walker
+  not compared to other statistics packages, no degrees of freedom correction
+* good for one time calculations for entire time series, not for recursive
+  prediction
+* class structure not very clean yet
+* many one-liners with scipy.signal, but takes time to figure out usage
+* missing result statistics, e.g. t-values
+* no criteria for choice of number of lags
+* no constant term in ARMA process
+* no integration, differencing for ARIMA
+* written without textbook, works but not sure about everything
+  brief check
+
+* two names for lag polynomials ar = rhoy, ma = rhoe ?
+
+
+Properties:
+Judge, ... (1985): The Theory and Practise of Econometrics
+
+BigJudge p. 237ff:
+If the time series process is a stationary ARMA(p,q), then
+minimizing the sum of squares is asymptoticaly (as T-> inf)
+equivalent to the exact Maximum Likelihood Estimator
+
+Because Least Squares conditional on the initial information
+does not use all information, in small samples exact MLE can
+be better.
+
+Without the normality assumption, the least squares estimator
+is still consistent under suitable conditions, however not
+efficient
+
+Author: josefpktd
+License: BSD
+'''
+
+import numpy as np
+from scipy import signal, optimize
+
+class ARIMA(object):
+    '''currently ARMA only, no differencing used - no I
+
+    reparameterized
+         rhoy(L) y_t = rhoe(L) eta_t
+    '''
+    def __init__(self):
+        pass
+    def fit(self,x,p,q, rhoy0=None, rhoe0=None):
+        '''estimate lag coefficients of ARMA orocess by least squares
+
+        Parameters
+        ----------
+            x : array, 1d
+                time series data
+            p : int
+                number of AR lags to estimate
+            q : int
+                number of MA lags to estimate
+            rhoy0, rhoe0 : array_like (optional)
+                starting values for estimation
+
+        Returns
+        -------
+            rh, cov_x, infodict, mesg, ier : output of scipy.optimize.leastsq
+            rh :
+                estimate of lag parameters, concatenated [rhoy, rhoe]
+            cov_x :
+                unscaled (!) covariance matrix of coefficient estimates
+
+
+        '''
+        def errfn( rho):
+            #rhoy, rhoe = rho
+            rhoy = np.concatenate(([1], rho[:p]))
+            rhoe = np.concatenate(([1], rho[p:]))
+            etahatr = signal.lfilter(rhoy, rhoe, x)
+            #print rho,np.sum(etahatr*etahatr)
+            return etahatr
+
+        if rhoy0 is None:
+            rhoy0 = 0.5 * np.ones(p)
+        if rhoe0 is None:
+            rhoe0 = 0.5 * np.ones(q)
+        usels = True
+        if usels:
+            rh, cov_x, infodict, mesg, ier = \
+               optimize.leastsq(errfn, np.r_[rhoy0, rhoe0],ftol=1e-10,full_output=True)
+        else:
+            # fmin_bfgs is slow or doesn't work yet
+            errfnsum = lambda rho : np.sum(errfn(rho)**2)
+            #xopt, {fopt, gopt, Hopt, func_calls, grad_calls
+            rh,fopt, gopt, cov_x, _,_, ier = \
+                optimize.fmin_bfgs(errfnsum, np.r_[rhoy0, rhoe0], maxiter=2, full_output=True)
+            infodict, mesg = None, None
+        self.rh = rh
+        self.rhoy = np.concatenate(([1], rh[:p]))
+        self.rhoe = np.concatenate(([1], rh[p:])) #rh[-q:])) doesnt work for q=0
+        self.error_estimate = errfn(rh)
+        return rh, cov_x, infodict, mesg, ier
+
+    def errfn(self, rho=None, p=None, x=None):
+        ''' duplicate -> remove one
+        '''
+        #rhoy, rhoe = rho
+        if not rho is None:
+            rhoy = np.concatenate(([1],  rho[:p]))
+            rhoe = np.concatenate(([1],  rho[p:]))
+        else:
+            rhoy = self.rhoy
+            rhoe = self.rhoe
+        etahatr = signal.lfilter(rhoy, rhoe, x)
+        #print rho,np.sum(etahatr*etahatr)
+        return etahatr
+
+    def predicted(self, rhoy=None, rhoe=None):
+        '''past predicted values of time series
+        just added, not checked yet
+        '''
+        if rhoy is None:
+            rhoy = self.rhoy
+        if rhoe is None:
+            rhoe = self.rhoe
+        return self.x + self.error_estimate
+
+    def forecast(self, ar=None, ma=None, nperiod=10):
+        eta = np.r_[self.error_estimate, np.zeros(nperiod)]
+        if ar is None:
+            ar = self.rhoy
+        if ma is None:
+            ma = self.rhoe
+        return signal.lfilter(ma, ar, eta)
+
+    def generate_sample(self,ar,ma,nsample,std=1):
+        eta = std * np.random.randn(nsample)
+        return signal.lfilter(ma, ar, eta)
+
+def generate_sample(self, ar, ma, nsample, std=1, distrvs=np.random.randn):
+    eta = std * distrvs(nsample)
+    return signal.lfilter(ma, ar, eta)
+
+
+def impulse_response(ar, ma, nobs=100):
+    '''get the impulse response function for ARMA process
+
+    Parameters
+    ----------
+        ma : array_like
+            moving average lag polynomial
+        ar : array_like
+            auto regressive lag polynomial
+        nobs : int
+            number of observations to calculate
+
+
+    Examples
+    --------
+    AR(1)
+    >>> impulse_response([1.0, -0.8], [1.], nobs=10)
+    array([ 1.        ,  0.8       ,  0.64      ,  0.512     ,  0.4096    ,
+            0.32768   ,  0.262144  ,  0.2097152 ,  0.16777216,  0.13421773])
+
+    this is the same as
+    >>> 0.8**np.arange(10)
+    array([ 1.        ,  0.8       ,  0.64      ,  0.512     ,  0.4096    ,
+            0.32768   ,  0.262144  ,  0.2097152 ,  0.16777216,  0.13421773])
+
+    MA(2)
+    >>> impulse_response([1.0], [1., 0.5, 0.2], nobs=10)
+    array([ 1. ,  0.5,  0.2,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ])
+
+    ARMA(1,2)
+    >>> impulse_response([1.0, -0.8], [1., 0.5, 0.2], nobs=10)
+    array([ 1.        ,  1.3       ,  1.24      ,  0.992     ,  0.7936    ,
+            0.63488   ,  0.507904  ,  0.4063232 ,  0.32505856,  0.26004685])
+
+
+    '''
+    impulse = np.zeros(nobs)
+    impulse[0] = 1.
+    return signal.lfilter(ma, ar, impulse)
+
+
+
+
+def mcarma22(niter=10):
+    nsample = 1000
+    #ar = [1.0, 0, 0]
+    ar = [1.0, -0.75, -0.1]
+    #ma = [1.0, 0, 0]
+    ma = [1.0,  0.3,  0.2]
+    results = []
+    results_bse = []
+    arma = ARIMA()
+    for _ in range(niter):
+        y2 = arest.generate_sample(ar,ma,nsample,0.1)
+        rhohat2a, cov_x2a, infodict, mesg, ier = arest2.fit(y2,2,2)
+        results.append(rhohat2a)
+        err2a = arest.errfn(x=y2)
+        sige2a = np.sqrt(np.dot(err2a,err2a)/nsample)
+        results_bse.append(sige2a * np.sqrt(np.diag(cov_x2a)))
+    return np.r_[ar[1:], ma[1:]], np.array(results), np.array(results_bse)
+
+
+
+if __name__ == '__main__':
+
+    # Simulate AR(1)
+    #--------------
+    # ar * y = ma * eta
+    ar = [1, -0.8]
+    ma = [1.0]
+
+    # generate AR data
+    eta = 0.1 * np.random.randn(1000)
+    yar1 = signal.lfilter(ar, ma, eta)
+
+    print "\nExample 0"
+    arest = ARIMA()
+    rhohat, cov_x, infodict, mesg, ier = arest.fit(yar1,1,1)
+    print rhohat
+    print cov_x
+
+    print "\nExample 1"
+    ar = [1.0,  -0.8]
+    ma = [1.0,  0.5]
+    y1 = arest.generate_sample(ar,ma,1000,0.1)
+    rhohat1, cov_x1, infodict, mesg, ier = arest.fit(y1,1,1)
+    print rhohat1
+    print cov_x1
+    err1 = arest.errfn(x=y1)
+    print np.var(err1)
+    import scikits.statsmodels as sm
+    print sm.regression.yule_walker(y1, order=2, inv=True)
+
+    print "\nExample 2"
+    arest2 = ARIMA()
+    nsample = 1000
+    ar = [1.0, -0.6, -0.1]
+    ma = [1.0,  0.3,  0.2]
+    y2 = arest2.generate_sample(ar,ma,nsample,0.1)
+    rhohat2, cov_x2, infodict, mesg, ier = arest2.fit(y2,1,2)
+    print rhohat2
+    print cov_x2
+    err2 = arest.errfn(x=y2)
+    print np.var(err2)
+    print arest2.rhoy
+    print arest2.rhoe
+    print "true"
+    print ar
+    print ma
+    rhohat2a, cov_x2a, infodict, mesg, ier = arest2.fit(y2,2,2)
+    print rhohat2a
+    print cov_x2a
+    err2a = arest.errfn(x=y2)
+    print np.var(err2a)
+    print arest2.rhoy
+    print arest2.rhoe
+    print "true"
+    print ar
+    print ma
+
+    print sm.regression.yule_walker(y2, order=2, inv=True)
+
+    print "\nExample 20"
+    arest20 = ARIMA()
+    nsample = 1000
+    ar = [1.0]#, -0.8, -0.4]
+    ma = [1.0,  0.5,  0.2]
+    y3 = arest20.generate_sample(ar,ma,nsample,0.01)
+    rhohat3, cov_x3, infodict, mesg, ier = arest20.fit(y3,2,0)
+    print rhohat3
+    print cov_x3
+    err3 = arest20.errfn(x=y3)
+    print np.var(err3)
+    print np.sqrt(np.dot(err3,err3)/nsample)
+    print arest20.rhoy
+    print arest20.rhoe
+    print "true"
+    print ar
+    print ma
+
+    rhohat3a, cov_x3a, infodict, mesg, ier = arest20.fit(y3,0,2)
+    print rhohat3a
+    print cov_x3a
+    err3a = arest20.errfn(x=y3)
+    print np.var(err3a)
+    print np.sqrt(np.dot(err3a,err3a)/nsample)
+    print arest20.rhoy
+    print arest20.rhoe
+    print "true"
+    print ar
+    print ma
+
+    print sm.regression.yule_walker(y3, order=2, inv=True)
+
+    print "\nExample 02"
+    arest02 = ARIMA()
+    nsample = 1000
+    ar = [1.0, -0.8, 0.4] #-0.8, -0.4]
+    ma = [1.0]#,  0.8,  0.4]
+    y4 = arest02.generate_sample(ar,ma,nsample)
+    rhohat4, cov_x4, infodict, mesg, ier = arest02.fit(y4,2,0)
+    print rhohat4
+    print cov_x4
+    err4 = arest02.errfn(x=y4)
+    print np.var(err4)
+    sige = np.sqrt(np.dot(err4,err4)/nsample)
+    print sige
+    print sige * np.sqrt(np.diag(cov_x4))
+    print np.sqrt(np.diag(cov_x4))
+    print arest02.rhoy
+    print arest02.rhoe
+    print "true"
+    print ar
+    print ma
+
+    rhohat4a, cov_x4a, infodict, mesg, ier = arest02.fit(y4,0,2)
+    print rhohat4a
+    print cov_x4a
+    err4a = arest02.errfn(x=y4)
+    print np.var(err4a)
+    sige = np.sqrt(np.dot(err4a,err4a)/nsample)
+    print sige
+    print sige * np.sqrt(np.diag(cov_x4a))
+    print np.sqrt(np.diag(cov_x4a))
+    print arest02.rhoy
+    print arest02.rhoe
+    print "true"
+    print ar
+    print ma
+    import scikits.statsmodels as sm
+    print sm.regression.yule_walker(y4, order=2, method='mle', inv=True)
+
+    def mc_summary(res, rt=None):
+        if rt is None:
+            rt = np.zeros(res.shape[1])
+        print 'RMSE'
+        print np.sqrt(((res-rt)**2).mean(0))
+        print 'mean bias'
+        print (res-rt).mean(0)
+        print 'median bias'
+        print np.median((res-rt),0)
+        print 'median bias percent'
+        print np.median((res-rt)/rt*100,0)
+        print 'median absolute error'
+        print np.median(np.abs(res-rt),0)
+        print 'positive error fraction'
+        print (res > rt).mean(0)
+
+    run_mc = False
+    if run_mc:
+        import time
+        t0 = time.time()
+        rt, res_rho, res_bse = mcarma22(niter=1000)
+        print 'elapsed time for Monte Carlo', time.time()-t0
+        # 20 seconds for ARMA(2,2), 1000 iterations with 1000 observations
+        sige2a = np.sqrt(np.dot(err2a,err2a)/nsample)
+        print '\nbse of one sample'
+        print sige2a * np.sqrt(np.diag(cov_x2a))
+        print '\nMC of rho versus true'
+        mc_summary(res_rho, rt)
+        print '\nMC of bse versus zero'
+        mc_summary(res_bse)
+        print '\nMC of bse versus std'
+        mc_summary(res_bse, res_rho.std(0))
+
+    import matplotlib.pyplot as plt
+    plt.plot(arest2.forecast()[-100:])
+    plt.show()
---
+++ scikits/statsmodels/sandbox/tsa/movstat.py
@@ -0,0 +1,333 @@
+
+'''using scipy signal and numpy correlate to calculate some time series
+statistics
+
+see also scikits.timeseries  (movstat is partially inspired by it)
+(added 2009-08-29:
+timeseries moving stats are in c, autocorrelation similar to here
+I thought I saw moving stats somewhere in python, maybe not)
+
+
+TODO:
+
+moving statistics
+* filters don't handle boundary conditions nicely (correctly ?)
+  e.g. minimum order filter uses 0 for out of bounds value
+  -> append and prepend with last resp. first value
+* enhance for nd arrays, with axis = 0
+
+
+
+Note: Equivalence for 1D signals
+>>> np.all(signal.correlate(x,[1,1,1],'valid')==np.correlate(x,[1,1,1]))
+True
+>>> np.all(ndimage.filters.correlate(x,[1,1,1], origin = -1)[:-3+1]==np.correlate(x,[1,1,1]))
+True
+
+# multidimensional, but, it looks like it uses common filter across time series, no VAR
+ndimage.filters.correlate(np.vstack([x,x]),np.array([[1,1,1],[0,0,0]]), origin = 1)
+ndimage.filters.correlate(x,[1,1,1],origin = 1))
+ndimage.filters.correlate(np.vstack([x,x]),np.array([[0.5,0.5,0.5],[0.5,0.5,0.5]]), \
+origin = 1)
+
+>>> np.all(ndimage.filters.correlate(np.vstack([x,x]),np.array([[1,1,1],[0,0,0]]), origin = 1)[0]==\
+ndimage.filters.correlate(x,[1,1,1],origin = 1))
+True
+>>> np.all(ndimage.filters.correlate(np.vstack([x,x]),np.array([[0.5,0.5,0.5],[0.5,0.5,0.5]]), \
+origin = 1)[0]==ndimage.filters.correlate(x,[1,1,1],origin = 1))
+
+
+update:
+2009-09-06: cosmetic changes, rearrangements
+'''
+
+
+import numpy as np
+from scipy import signal
+
+from numpy.testing import assert_array_equal, assert_array_almost_equal
+
+
+
+
+def expandarr(x,k):
+    #make it work for 2D or nD with axis
+    kadd = k
+    if np.ndim(x) == 2:
+        kadd = (kadd, np.shape(x)[1])
+    return np.r_[np.ones(kadd)*x[0],x,np.ones(kadd)*x[-1]]
+
+def mov_order(x, order = 'med', windsize=3, lag='lagged'):
+    '''moving order statistics
+
+    Parameters
+    ----------
+    x : array
+       time series data
+    order : float or 'med', 'min', 'max'
+       which order statistic to calculate
+    windsize : int
+       window size
+    lag : 'lagged', 'centered', or 'leading'
+       location of window relative to current position
+
+    Returns
+    -------
+    filtered array
+
+
+    '''
+
+    #if windsize is even should it raise ValueError
+    if lag == 'lagged':
+        lead = windsize//2
+    elif lag == 'centered':
+        lead = 0
+    elif lag == 'leading':
+        lead = -windsize//2 +1
+    else:
+        raise ValueError
+    if np.isfinite(order) == True: #if np.isnumber(order):
+        ord = order   # note: ord is a builtin function
+    elif order == 'med':
+        ord = (windsize - 1)/2
+    elif order == 'min':
+        ord = 0
+    elif order == 'max':
+        ord = windsize - 1
+    else:
+        raise ValueError
+
+    #return signal.order_filter(x,np.ones(windsize),ord)[:-lead]
+    xext = expandarr(x, windsize)
+    #np.r_[np.ones(windsize)*x[0],x,np.ones(windsize)*x[-1]]
+    return signal.order_filter(xext,np.ones(windsize),ord)[windsize-lead:-(windsize+lead)]
+
+def check_movorder():
+    '''graphical test for movorder'''
+    import matplotlib.pylab as plt
+    x = np.arange(1,10)
+    xo = mov_order(x, order='max')
+    assert_array_equal(xo, x)
+    x = np.arange(10,1,-1)
+    xo = mov_order(x, order='min')
+    assert_array_equal(xo, x)
+    assert_array_equal(mov_order(x, order='min', lag='centered')[:-1], x[1:])
+
+    tt = np.linspace(0,2*np.pi,15)
+    x = np.sin(tt) + 1
+    xo = mov_order(x, order='max')
+    plt.figure()
+    plt.plot(tt,x,'.-',tt,xo,'.-')
+    plt.title('moving max lagged')
+    xo = mov_order(x, order='max', lag='centered')
+    plt.figure()
+    plt.plot(tt,x,'.-',tt,xo,'.-')
+    plt.title('moving max centered')
+    xo = mov_order(x, order='max', lag='leading')
+    plt.figure()
+    plt.plot(tt,x,'.-',tt,xo,'.-')
+    plt.title('moving max leading')
+
+# identity filter
+##>>> signal.order_filter(x,np.ones(1),0)
+##array([ 1.,  2.,  3.,  4.,  5.,  6.,  7.,  8.,  9.])
+# median filter
+##signal.medfilt(np.sin(x), kernel_size=3)
+##>>> plt.figure()
+##<matplotlib.figure.Figure object at 0x069BBB50>
+##>>> x=np.linspace(0,3,100);plt.plot(x,np.sin(x),x,signal.medfilt(np.sin(x), kernel_size=3))
+
+# remove old version
+##def movmeanvar(x, windowsize=3, valid='same'):
+##    '''
+##    this should also work along axis or at least for columns
+##    '''
+##    n = x.shape[0]
+##    x = expandarr(x, windowsize - 1)
+##    takeslice = slice(windowsize-1, n + windowsize-1)
+##    avgkern = (np.ones(windowsize)/float(windowsize))
+##    m = np.correlate(x, avgkern, 'same')#[takeslice]
+##    print m.shape
+##    print x.shape
+##    xm = x - m
+##    v = np.correlate(x*x, avgkern, 'same') - m**2
+##    v1 = np.correlate(xm*xm, avgkern, valid) #not correct for var of window
+###>>> np.correlate(xm*xm,np.array([1,1,1])/3.0,'valid')-np.correlate(xm*xm,np.array([1,1,1])/3.0,'valid')**2
+##    return m[takeslice], v[takeslice], v1
+
+def movmean(x, windowsize=3, lag='lagged'):
+    return movmoment(x, 1, windowsize=windowsize, lag=lag)
+
+def movvar(x, windowsize=3, lag='lagged'):
+    m1 = movmoment(x, 1, windowsize=windowsize, lag=lag)
+    m2 = movmoment(x, 2, windowsize=windowsize, lag=lag)
+    return m2 - m1*m1
+
+def movmoment(x, k, windowsize=3, lag='lagged'):
+    '''non-central moment
+
+
+    Parameters
+    ----------
+    x : array
+       time series data
+    windsize : int
+       window size
+    lag : 'lagged', 'centered', or 'leading'
+       location of window relative to current position
+
+    Returns
+    -------
+    mk : array
+        k-th moving non-central moment, with same shape as x
+
+
+    '''
+
+    windsize = windowsize
+    #if windsize is even should it raise ValueError
+    if lag == 'lagged':
+        lead = windsize//2
+    elif lag == 'centered':
+        lead = 0
+    elif lag == 'leading':
+        lead = -windsize//2 +1
+    else:
+        raise ValueError
+
+    avgkern = (np.ones(windowsize)/float(windowsize))
+    xext = expandarr(x, windsize-1)
+
+    if xext.ndim == 1:
+        return np.correlate(xext**k, avgkern, 'full')[windsize-lead:-(windsize+lead)]
+    else:
+        print xext.shape
+        print avgkern[:,None].shape
+
+        # try first with 2d along columns, possibly ndim with axis
+        return signal.correlate(xext**k, avgkern[:,None], 'full')[windsize-lead:-(windsize+lead),:]
+
+
+#None of the acovf, ... are tested; starting index? orientation?
+def acovf(x):
+    ''' autocovariance for 1D
+    '''
+    n = len(x)
+    xo = x - x.mean();
+    xi = np.ones(n);
+    d = np.correlate(xi, xi, 'full')
+    return ( np.correlate(xo, xo, 'full')/d )[n-1:]
+
+def ccovf(x,y):
+    ''' crosscovariance for 1D
+    '''
+    n = len(x)
+    xo = x - x.mean();
+    yo = y - y.mean();
+    xi = np.ones(10);
+    d = np.correlate(xi,xi,'full')
+    return ( np.correlate(xo,yo,'full')/d )[n-1:]
+
+def acf(x):
+    '''autocorrelation function for 1d'''
+    avf = acovf(x)
+    return avf/avf[0]
+
+def ccf(x,y):
+    '''cross-correlation function for 1d'''
+    cvf = ccovf(x,y)
+    return cvf/np.std(x)/np.std(y)
+
+
+#x=0.5**np.arange(10);xm=x-x.mean();a=np.correlate(xm,[1],'full')
+#x=0.5**np.arange(3);np.correlate(x,x,'same')
+##>>> x=0.5**np.arange(10);xm=x-x.mean();a=np.correlate(xm,xo,'full')
+##
+##>>> xo=np.ones(10);d=np.correlate(xo,xo,'full')
+##>>> xo
+##xo=np.ones(10);d=np.correlate(xo,xo,'full')
+##>>> x=np.ones(10);xo=x-x.mean();a=np.correlate(xo,xo,'full')
+##>>> xo=np.ones(10);d=np.correlate(xo,xo,'full')
+##>>> d
+##array([  1.,   2.,   3.,   4.,   5.,   6.,   7.,   8.,   9.,  10.,   9.,
+##         8.,   7.,   6.,   5.,   4.,   3.,   2.,   1.])
+
+
+##def ccovf():
+##    pass
+##    #x=0.5**np.arange(10);xm=x-x.mean();a=np.correlate(xm,xo,'full')
+
+__all__ = [mov_order, movmean, movvar, movmoment, acovf, ccovf, acf, ccf]
+
+if __name__ == '__main__':
+
+
+    T = 20
+    K = 2
+    x = np.column_stack([np.arange(T)]*K)
+    aav = acovf(x[:,0])
+    print aav[0] == np.var(x[:,0])
+    aac = acf(x[:,0])
+
+
+
+    print '\ncheckin moving mean and variance'
+    nobs = 10
+    x = np.arange(nobs)
+    ws = 3
+    ave = np.array([ 0., 1/3., 1., 2., 3., 4., 5., 6., 7., 8.,
+                  26/3., 9])
+    va = np.array([[ 0.        ,  0.        ],
+                   [ 0.22222222,  0.88888889],
+                   [ 0.66666667,  2.66666667],
+                   [ 0.66666667,  2.66666667],
+                   [ 0.66666667,  2.66666667],
+                   [ 0.66666667,  2.66666667],
+                   [ 0.66666667,  2.66666667],
+                   [ 0.66666667,  2.66666667],
+                   [ 0.66666667,  2.66666667],
+                   [ 0.66666667,  2.66666667],
+                   [ 0.22222222,  0.88888889],
+                   [ 0.        ,  0.        ]])
+    ave2d = np.c_[ave, 2*ave]
+    print movmean(x, windowsize=ws, lag='lagged')
+    print movvar(x, windowsize=ws, lag='lagged')
+    print [np.var(x[i-ws:i]) for i in range(ws, nobs)]
+    m1 = movmoment(x, 1, windowsize=3, lag='lagged')
+    m2 = movmoment(x, 2, windowsize=3, lag='lagged')
+    print m1
+    print m2
+    print m2 - m1*m1
+
+    # this implicitly also tests moment
+    assert_array_almost_equal(va[ws-1:,0],
+                    movvar(x, windowsize=3, lag='leading'))
+    assert_array_almost_equal(va[ws//2:-ws//2+1,0],
+                    movvar(x, windowsize=3, lag='centered'))
+    assert_array_almost_equal(va[:-ws+1,0],
+                    movvar(x, windowsize=ws, lag='lagged'))
+
+
+
+    print '\nchecking moving moment for 2d (columns only)'
+    x2d = np.c_[x, 2*x]
+    print movmoment(x2d, 1, windowsize=3, lag='centered')
+    print movmean(x2d, windowsize=ws, lag='lagged')
+    print movvar(x2d, windowsize=ws, lag='lagged')
+    assert_array_almost_equal(va[ws-1:,:],
+                    movvar(x2d, windowsize=3, lag='leading'))
+    assert_array_almost_equal(va[ws//2:-ws//2+1,:],
+                    movvar(x2d, windowsize=3, lag='centered'))
+    assert_array_almost_equal(va[:-ws+1,:],
+                    movvar(x2d, windowsize=ws, lag='lagged'))
+
+    assert_array_almost_equal(ave2d[ws-1:],
+                    movmoment(x2d, 1, windowsize=3, lag='leading'))
+    assert_array_almost_equal(ave2d[ws//2:-ws//2+1],
+                    movmoment(x2d, 1, windowsize=3, lag='centered'))
+    assert_array_almost_equal(ave2d[:-ws+1],
+                    movmean(x2d, windowsize=ws, lag='lagged'))
+
+    from scipy import ndimage
+    print ndimage.filters.correlate1d(x2d, np.array([1,1,1])/3., axis=0)
