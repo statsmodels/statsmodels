1f203e4f4c6a473e32919319afdd8f76d626d097
Josef Perktold <josef.pktd@gmail.com>
2009-09-15 09:46:05 -0400

    ols-rlm tutorial example: reduce sig, add linear

--- scikits/statsmodels/sandbox/regression/tut_ols_rlm.py
+++ scikits/statsmodels/sandbox/regression/tut_ols_rlm.py
@@ -15,11 +15,13 @@ nsample = 50
 x1 = np.linspace(0, 20, nsample)
 X = np.c_[x1, (x1-5)**2, np.ones(nsample)]
 
-sig = 0.5
+sig = 0.3   # smaller error variance makes OLS<->RLM contrast bigger
 beta = [0.5, -0.0, 5.]
 y_true2 = np.dot(X, beta)
 y2 = y_true2 + sig*1. * np.random.normal(size=nsample)
-y2[[39,41,43,45,48]] -= 5   # add some outliers
+y2[[39,41,43,45,48]] -= 5   # add some outliers (10% of nsample)
+
+#Example: estimate quadratic function (true is linear)
 
 plt.figure()
 plt.plot(x1, y2, 'o', x1, y_true2, 'b-')
@@ -44,5 +46,34 @@ print resrlm.bse
 # Note different spelling fitted_values with underline
 plt.plot(x1, resrlm.fitted_values, 'g.-')
 plt.title('blue: true,   red: OLS,   green: RLM')
+
+
+# Example: estimate linear function (true is linear)
+
+X2 = X[:,[0,2]]  # use only linear term and constant
+plt.figure()
+plt.plot(x1, y2, 'o', x1, y_true2, 'b-')
+
+
+res2 = sm.OLS(y2, X2).fit()
+print res2.params
+# Note: quadratic term captures outlier effect
+print res2.bse
+#print res2.predict
+prstd, iv_l, iv_u = wls_prediction_std(res2)
+plt.plot(x1, res2.fittedvalues, 'r-')
+plt.plot(x1, iv_u, 'r--')
+plt.plot(x1, iv_l, 'r--')
+
+
+#compare with robust estimator
+
+resrlm2 = sm.RLM(y2, X2).fit()
+print resrlm2.params
+print resrlm2.bse
+# Note different spelling fitted_values with underline
+plt.plot(x1, resrlm2.fitted_values, 'g.-')
+plt.title('blue: true,   red: OLS,   green: RLM')
+
 
 plt.show()
