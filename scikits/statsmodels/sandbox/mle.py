158b660e19e2c9e15af021eebf296005d28cb3f9
Josef Perktold <josef.pktd@gmail.com>
2009-09-20 08:08:10 -0400

    make foreign sandbox.mle example run

--- scikits/statsmodels/sandbox/mle.py
+++ scikits/statsmodels/sandbox/mle.py
@@ -1,10 +1,14 @@
 '''What's the origin of this file? It is not ours.
-Does not run because of missing mtx files
+Does not run because of missing mtx files, now included
+
+changes: JP corrections to imports so it runs, comment out print
 '''
 
+import numpy as np
 from numpy import dot,  outer, random, argsort
 from scipy import io, linalg, optimize
 from scipy.sparse import speye
+import matplotlib.pyplot as plt
 
 def R(v):
     rq = dot(v.T,A*v)/dot(v.T,B*v)
@@ -15,13 +19,13 @@ def R(v):
 def Rp(v):
     """ Gradient """
     result = 2*(A*v-R(v)*B*v)/dot(v.T,B*v)
-    print "Rp: ", result
+    #print "Rp: ", result
     return result
 
 def Rpp(v):
     """ Hessian """
     result = 2*(A-R(v)*B-outer(B*v,Rp(v))-outer(Rp(v),B*v))/dot(v.T,B*v)
-    print "Rpp: ", result
+    #print "Rpp: ", result
     return result
 
 
@@ -35,11 +39,12 @@ random.seed(1)
 v_0=random.rand(n)
 
 print "try fmin_bfgs"
-
+full_output = 1
 data=[]
-v,fopt, gopt, Hopt, func_calls, grad_calls, warnflag,allvecs = optimize.fmin_bfgs(R,v_0,fprime=Rp,full_output=1,retall=1)
+v,fopt, gopt, Hopt, func_calls, grad_calls, warnflag, allvecs = \
+        optimize.fmin_bfgs(R,v_0,fprime=Rp,full_output=full_output,retall=1)
 if warnflag == 0:
-   semilogy(arange(0,len(data)),data)
+   plt.semilogy(np.arange(0,len(data)),data)
    print 'Rayleigh quotient BFGS',R(v)
 
 
@@ -51,8 +56,10 @@ print "try fmin_ncg"
 # WARNING: the program may hangs if fmin_ncg is used
 #
 data=[]
-v,fopt, fcalls, gcalls, hcalls, warnflag,allvecs = optimize.fmin_ncg(R,v_0,fprime=Rp,fhess=Rpp,full_output=1,retall=1)
+v,fopt, fcalls, gcalls, hcalls, warnflag, allvecs = \
+        optimize.fmin_ncg(R,v_0,fprime=Rp,fhess=Rpp,full_output=full_output,retall=1)
 if warnflag==0:
-   semilogy(arange(0,len(data)),data)
+   plt.figure()
+   plt.semilogy(np.arange(0,len(data)),data)
    print 'Rayleigh quotient NCG',R(v)
 
