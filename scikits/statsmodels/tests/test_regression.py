c0ec5e64dff7f0fa54ba0e470d8dd273599af716
Josef Perktold <josef.pktd@gmail.com>
2009-11-14 19:47:48 -0500

    fix df_model in test_regression.py without constant, now all test pass with numpy 1.3.0

--- scikits/statsmodels/tests/test_regression.py
+++ scikits/statsmodels/tests/test_regression.py
@@ -472,10 +472,15 @@ class TestDataDimensions(CheckRegressionResults):
         self.exog_n_ = np.random.uniform(0,20,size=30)
         self.exog_n_one = self.exog_n_[:,None]
         self.degen_exog = self.exog_n_one[:-1]
-        self.res1 = OLS(self.endog_n_one, self.exog_n_one).fit()
+        self.mod1 = OLS(self.endog_n_one, self.exog_n_one)
+        self.mod1.df_model += 1
+        #self.mod1.df_resid -= 1
+        self.res1 = self.mod1.fit()
         # Note that these are created for every subclass..
         # A little extra overhead probably
-        self.res2 = OLS(self.endog_n_one, self.exog_n_one).fit()
+        self.mod2 = OLS(self.endog_n_one, self.exog_n_one)
+        self.mod2.df_model += 1
+        self.res2 = self.mod2.fit()
 
     def check_confidenceintervals(self, conf1, conf2):
         assert_almost_equal(conf1, conf2(), DECIMAL)
@@ -483,17 +488,23 @@ class TestDataDimensions(CheckRegressionResults):
 class TestNxNx(TestDataDimensions):
     def __init__(self):
         super(TestNxNx, self).__init__()
-        self.res2 = OLS(self.endog_n_,self.exog_n_).fit()
+        self.mod2 = OLS(self.endog_n_,self.exog_n_)
+        self.mod2.df_model += 1
+        self.res2 = self.mod2.fit()
 
 class TestNxOneNx(TestDataDimensions):
     def __init__(self):
         super(TestNxOneNx, self).__init__()
-        self.res2 = OLS(self.endog_n_one, self.exog_n_).fit()
+        self.mod2 = OLS(self.endog_n_one, self.exog_n_)
+        self.mod2.df_model += 1
+        self.res2 = self.mod2.fit()
 
 class TestNxNxOne(TestDataDimensions):
     def __init__(self):
         super(TestNxNxOne, self).__init__()
-        self.res2 = OLS(self.endog_n_, self.exog_n_one).fit()
+        self.mod2 = OLS(self.endog_n_, self.exog_n_one)
+        self.mod2.df_model += 1
+        self.res2 = self.mod2.fit()
 
 def test_bad_size():
     np.random.seed(54321)
