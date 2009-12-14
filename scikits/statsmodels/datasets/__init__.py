ea49ea0bd74d0767d1a13d46f01a264ab1625829
Skipper Seabold <jsseabold@gmail.com>
2009-12-14 16:50:16 -0500

    Added some developer notes for adding datasets to the sandbox.  Added spector data for discrete model examples.

--- scikits/statsmodels/datasets/__init__.py
+++ scikits/statsmodels/datasets/__init__.py
@@ -3,4 +3,4 @@ Datasets module
 """
 #__all__ = filter(lambda s:not s.startswith('_'),dir())
 import committee, ccard, copper, cpunish, data_utils, longley, scotland, \
-    stackloss, star98, sunspots
+    stackloss, star98, sunspots, spector
