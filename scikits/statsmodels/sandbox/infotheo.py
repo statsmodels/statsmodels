b5a92ec77a65c319ff26887e59372ed92e731753
Skipper Seabold <jsseabold@gmail.com>
2010-02-12 11:01:02 -0500

    Some more work on infotheo.

--- scikits/statsmodels/sandbox/infotheo.py
+++ scikits/statsmodels/sandbox/infotheo.py
@@ -39,16 +39,35 @@ from matplotlib import pyplot as plt
 #TODO: change these to use maxentutils so that over/underflow is handled
 #with the logsumexp.
 
-from scipy.maxentropy import logsumexp
+from scipy.maxentropy import logsumexp as lse
 
-#The below was taken from Warren's post
-#http://mail.scipy.org/pipermail/scipy-user/2009-October/022931.html
-def my_logsumexp(a, axis=None):
+def logsumexp(a, axis=None):
     """
+    Compute the log of the sum of exponentials log(e^{a_1}+...e^{a_n}) of a
+
+    Avoids numerical overflow.
+
+    Parameters
+    ----------
+    a : array-like
+        The vector to exponentiate and sum
+    axis : int, optional
+        The axis along which to apply the operation.  Defaults is None.
+
+    Returns
+    -------
+    sum(log(exp(a)))
+
+    Notes
+    -----
+    This function was taken from the mailing list
+    http://mail.scipy.org/pipermail/scipy-user/2009-October/022931.html
+
+    This should be superceded by the ufunc when it is finished.
     """
     if axis is None:
         # Use the scipy.maxentropy version.
-        return logsumexp(a)
+        return lse(a)
     a = asarray(a)
     shp = list(a.shape)
     shp[axis] = 1
@@ -133,7 +152,8 @@ def bitstonats(X):
     """
     return logbasechange(2, np.e) * X
 
-# Shannon's entropy
+#TODO: make this entropy, and then have different measures as
+#a method
 def shannonentropy(px, logbase=2):
     """
     This is Shannon's entropy
@@ -158,11 +178,10 @@ def shannonentropy(px, logbase=2):
     -----
     shannonentropy(0) is defined as 0
     """
+#TODO: haven't defined the px,py case?
     px = np.asarray(px)
     if not np.all(px <= 1) or not np.all(px >= 0):
         raise ValueError, "px does not define proper distribution"
-
-
     entropy = -np.sum(np.nan_to_num(px*np.log2(px)))
     if logbase != 2:
         return logbasechange(2,logbase) * entropy
@@ -218,7 +237,6 @@ def condentropy(px, py, pxpy=None, logbase=2):
         raise ValueError, "pxpy is not a proper joint distribtion"
     if pxpy == None:
         pxpy = np.outer(py,px)
-
     condent = np.sum(pxpy * np.nan_to_num(np.log2(py/pxpy)))
     if logbase == 2:
         return condent
@@ -285,6 +303,13 @@ def corrent(px,py,pxpy,logbase=2):
 
     corrent(px,py,pxpy) = 1 - condent(px,py,pxpy)/shannonentropy(py)
     """
+    if not _isproperdist(px) or not _isproperdist(py):
+        raise ValueError, "px or py is not a proper probability distribution"
+    if pxpy != None and not _isproperdist(pxpy):
+        raise ValueError, "pxpy is not a proper joint distribtion"
+    if pxpy == None:
+        pxpy = np.outer(py,px)
+
     return mutualinfo(px,py,pxpy,logbase=logbase)/shannonentropy(py,
             logbase=logbase)
 
@@ -310,17 +335,100 @@ def covent(px,py,pxpy,logbase=2):
 
     Returns
     -------
-    mutualinfo(px,py,pxpy,logbase=logbase)/shannonentropy(py,logbase=logbase)
+    condent(px,py,pxpy,logbase=logbase) + condent(py,px,pxpy,
+            logbase=logbase)
 
     Notes
     -----
     This is also equivalent to
 
-    corrent(px,py,pxpy) = 1 - condent(px,py,pxpy)/shannonentropy(py)
+    covent(px,py,pxpy) = condent(px,py,pxpy) + condent(py,px,pxpy)
+    """
+    if not _isproperdist(px) or not _isproperdist(py):
+        raise ValueError, "px or py is not a proper probability distribution"
+    if pxpy != None and not _isproperdist(pxpy):
+        raise ValueError, "pxpy is not a proper joint distribtion"
+    if pxpy == None:
+        pxpy = np.outer(py,px)
+
+    return condent(px,py,pxpy,logbase=logbase) + condent(py,px,pxpy,
+            logbase=logbase)
+
+
+#### Generalized Entropies ####
+
+def renyientropy(px,alpha=1,logbase=2,measure='R'):
+    """
+    Renyi's generalized entropy
+
+    Parameters
+    ----------
+    px : array-like
+        Discrete probability distribution of random variable X.  Note that
+        px is assumed to be a proper probability distribution.
+    logbase : int or np.e, optional
+        Default is 2 (bits)
+    alpha : float or inf
+        The order of the entropy.  The default is 1, which in the limit
+        is just Shannon's entropy.  2 is Renyi (Collision) entropy.  If
+        the string "inf" or numpy.inf is specified the min-entropy is returned.
+    measure : str, optional
+        The type of entropy measure desired.  'R' returns Renyi entropy
+        measure.  'T' returns the Tsallis entropy measure.
+
+    Returns
+    -------
+    1/(1-alpha)*log(sum(px**alpha))
+
+    In the limit as alpha -> 1, Shannon's entropy is returned.
+
+    In the limit as alpha -> inf, min-entropy is returned.
+    """
+#TODO:finish returns
+#TODO:add checks for measure
+    if not _isproperdist(px):
+        raise ValueError, "px is not a proper probability distribution"
+    alpha = float(alpha)
+    if alpha == 1:
+        genent = shannonentropy(px)
+        if logbase != 2:
+            return logbasechange(2, logbase) * genent
+        return genent
+    elif 'inf' in string(alpha).lower() or alpha == np.inf:
+        return -np.log(np.max(px))
+
+    # gets here if alpha != (1 or inf)
+    px = px**alpha
+    genent = np.log(px.sum())
+    if logbase == 2:
+        return 1/(1-alpha) * genent
+    else:
+        return 1/(1-alpha) * logbasechange(2, logbase) * genent
+
+#TODO: before completing this, need to rethink the organization of
+# (relative) entropy measures, ie., all put into one function
+# and have kwdargs, etc.?
+def gencrossentropy(px,py,pxpy,alpha=1,logbase=2, measure='T'):
+    """
+    Generalized cross-entropy measures.
+
+    Parameters
+    ----------
+    px : array-like
+        Discrete probability distribution of random variable X
+    py : array-like
+        Discrete probability distribution of random variable Y
+    pxpy : 2d array-like, optional
+        Joint probability distribution of X and Y.  If pxpy is None, X and Y
+        are assumed to be independent.
+    logbase : int or np.e, optional
+        Default is 2 (bits)
+    measure : str, optional
+        The measure is the type of generalized cross-entropy desired. 'T' is
+        the cross-entropy version of the Tsallis measure.  'CR' is Cressie-Read
+        measure.
 
     """
-
-
 
 
 if __name__ == "__main__":
@@ -374,12 +482,37 @@ and Synthesis"
     print H_X,H_Y, H_XY, H_XgivenY, H_YgivenX, D_YX, D_XY, I_XY
 
     print "discretize functions"
-    X=np.array([21.2,44.5,31.0,19.5,40.6,38.7,11.1,15.8,31.9,25.8,20.2,14.2,24.0,21.0,
-        11.3,18.0,16.3,22.2,7.8,27.8,16.3,35.1,14.9,17.1,28.2,16.4,16.5,46.0,9.5,18.8,
-        32.1,26.1,16.1,7.3,21.4,20.0,29.3,14.9,8.3,22.5,12.8,26.9,25.5,22.9,11.2,20.7,
-        26.2,9.3,10.8,15.6])
+    X=np.array([21.2,44.5,31.0,19.5,40.6,38.7,11.1,15.8,31.9,25.8,20.2,14.2,
+        24.0,21.0,11.3,18.0,16.3,22.2,7.8,27.8,16.3,35.1,14.9,17.1,28.2,16.4,
+        16.5,46.0,9.5,18.8,32.1,26.1,16.1,7.3,21.4,20.0,29.3,14.9,8.3,22.5,
+        12.8,26.9,25.5,22.9,11.2,20.7,26.2,9.3,10.8,15.6])
+    discX = discretize(X)
+    #CF: R's infotheo
+#TODO: compare to pyentropy quantize?
+    print
+    print "Example in section 3.6 of Golan, using table 3.3"
+    print "Bounding errors using Fano's inequality"
+    print "H(P_{e}) + P_{e}log(K-1) >= H(X|Y)"
+    print "or, a weaker inequality"
+    print "P_{e} >= [H(X|Y) - 1]/log(K)"
+    print "P(x) = %s" % px
+    print "X = 3 has the highest probability, so this is the estimate Xhat"
+    pe = 1 - px[2]
+    print "The probability of error Pe is 1 - p(X=3) = %0.4g" % pe
+    H_pe = shannonentropy([pe,1-pe])
+    print "H(Pe) = %0.4g and K=3" % H_pe
+    print "H(Pe) + Pe*log(K-1) = %0.4g >= H(X|Y) = %0.4g" % \
+            (H_pe+pe*np.log2(2), H_XgivenY)
+    print "or using the weaker inequality"
+    print "Pe = %0.4g >= [H(X) - 1]/log(K) = %0.4g" % (pe, (H_X - 1)/np.log2(3))
+    print "Consider now, table 3.5, where there is additional information"
+    print "The conditional probabilities of P(X|Y=y) are "
+    w2 = np.array([[0.,0.,1.],[1/3.,1/3.,1/3.],[1/6.,1/3.,1/2.]])
+    print w2
+# not a proper distribution?
+    print "The probability of error given this information is"
+    print "Pe = [H(X|Y) -1]/log(K) = %0.4g" % ((np.mean([0,shannonentropy(w2[1]),shannonentropy(w2[2])])-1)/np.log2(3))
+    print "such that more information lowers the error"
 
-
-
-
-
+### Stochastic processes
+    markovchain = np.array([[.553,.284,.163],[.465,.312,.223],[.420,.322,.258]])
