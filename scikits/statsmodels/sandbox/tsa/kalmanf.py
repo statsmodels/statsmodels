a9460386cdf63277acc288d1101637f256b738d3
Skipper Seabold <jsseabold@gmail.com>
2010-04-16 15:00:35 -0400

    A few docstring clean ups.  StateSpaceModel and Kalman filter is correct versus Matlab.

--- scikits/statsmodels/sandbox/tsa/kalmanf.py
+++ scikits/statsmodels/sandbox/tsa/kalmanf.py
@@ -7,36 +7,61 @@ Durbin., J and Koopman, S.J.  `Time Series Analysis by State Space Methods`.
     Oxford, 2001.
 
 Hamilton, J.D.  `Time Series Analysis`.  Princeton, 1994.
+
+Notes
+-----
+This file follows Hamilton's notation pretty closely.
 """
 
 from scipy import optimize
+import numpy as np
 from var import chain_dot #TODO: move this to tools
 
-#TODO: split out the loglikelihood part into its own function
+#TODO: split out the parts into their own function
+def kalman_pred():
+    return
+
+def kalman_loglike():
+    return
 
 def kalmanfilter(F, A, H, Q, R, y, X, xi10, ntrain):
     """
+    Returns the negative log-likelihood of y conditional on the information set
+
+    Assumes that the initial state and all innovations are multivariate
+    Gaussian.
 
     Parameters
     -----------
-    f : array-like
-        f is the transition matrix for the hidden state
-    h : array-like
-        Relates the observable state to the hidden state.
+    F : array-like
+        The (r x r) array holding the transition matrix for the hidden state.
+    A : array-like
+        The (nobs x k) array relating the predetermined variables to the
+        observed data.
+    H : array-like
+        The (nobs x r) array relating the hidden state vector to the
+        observed data.
+    Q : array-like
+        (r x r) variance/covariance matrix on the error term in the hidden
+        state transition.
+    R : array-like
+        (nobs x nobs) variance/covariance of the noise in the observation
+        equation.
     y : array-like
-        Observed data
-    x10 : array-like
-        Is the initial prior on the initial state vector
-    q : array-like
-        Variance/Covariance matrix on the error term in the hidden state
+        The (nobs x 1) array holding the observed data.
+    X : array-like
+        The (nobs x k) array holding the predetermined variables data.
+    xi10 : array-like
+        Is the (r x 1) initial prior on the initial state vector.
     ntrain : int
-        The number of training periods for the filter.
+        The number of training periods for the filter.  This is the number of
+        observations that do not affect the likelihood.
 
 
     Returns
     -------
     likelihood
-        The negatiev of the log likelihood
+        The negative of the log likelihood
     history or priors, history of posterior
 
     TODO: change API, update names
@@ -46,6 +71,7 @@ def kalmanfilter(F, A, H, Q, R, y, X, xi10, ntrain):
 # uses log of Hamilton 13.4.1
     F = np.asarray(F)
     H = np.asarray(H)
+    n = H.shape[1]  # remember that H gets transposed
     y = np.asarray(y)
     A = np.asarray(A)
     if y.ndim == 1: # note that Y is in rows for now
@@ -63,82 +89,38 @@ def kalmanfilter(F, A, H, Q, R, y, X, xi10, ntrain):
 # Eq. 12.2.21, other version says P0 = Q
 #    p10 = np.dot(np.linalg.inv(np.eye(r**2)-np.kron(F,F)),Q.ravel('F'))
 #    p10 = np.reshape(P0, (r,r), order='F')
-
-# Assume a fixed, known intial point and set P0 = Q?
-# this one doesn't take as long and gets "closer" to the answer.
+# Assume a fixed, known intial point and set P0 = Q
+# this one doesn't take as long and gets "closer" to the answer?
     p10 = Q
 
     loglikelihood = 0
     for i in range(nobs):
-        if i > ntrain:
-            HTPHR = chain_dot(H.T,p10,H)+R
-            if HTPHR.ndim == 1:
-                HTPHRinv = 1./HTPHR
-            else:
-                HTPHRinv = np.linalg.inv(HTPHR)
-    #        FPH = chain_dot(F,P,H)
-            part1 = y[i] - np.dot(A.T,X) - np.dot(H.T,xi10)
-            HTPHRdet = np.linalg.det(HTPHR)
-            part2 = -.5*chain_dot(part1.T,HTPHRinv,part1)
-# I don't think these values can ever be returned.  There will be another
-# error.  Need to test with ill-conditioned problem.
-#            if HTPHRdet > 10e-300: # not singular
-            loglike_interm = (-nobs/2.) * np.log(2*np.pi) - .5*\
+        HTPHR = chain_dot(H.T,p10,H)+R
+        if HTPHR.ndim == 1:
+            HTPHRinv = 1./HTPHR
+        else:
+            HTPHRinv = np.linalg.inv(HTPHR) # correct
+        part1 = y[i] - np.dot(A.T,X) - np.dot(H.T,xi10) # correct
+        if i >= ntrain: # zero-index, but ntrain isn't
+            HTPHRdet = np.linalg.det(HTPHR) # correct
+            part2 = -.5*chain_dot(part1.T,HTPHRinv,part1) # correct
+#TODO: Need to test with ill-conditioned problem.
+            loglike_interm = (-n/2.) * np.log(2*np.pi) - .5*\
                         np.log(HTPHRdet) + part2
-#                if loglike_interm > 10e300:
-#                    raise ValueError("""There was an error in forming likelihood.
-#Derivative term is greater than 10e300.""")
             loglikelihood += loglike_interm
 
-            # 13.2.15 Update linear project, guess xi now based on y
-            xi11 = xi10 + chain_dot(p10, H, HTPHRinv, part1)
-            # 13.2.16 MSE of that update projection
-            p11 = p10 - chain_dot(p10, H, HTPHRinv, H.T, p10)
-            # 13.2.17 Update forecast about xi based on our guess of F
-            xi10 = np.dot(F,xi11)
-            # 13.2.21 Update the MSE of the forecast
-            p10 = chain_dot(F,p11,F.T) + Q
+        # 13.2.15 Update current state xi_t based on y
+        xi11 = xi10 + chain_dot(p10, H, HTPHRinv, part1)
+        # 13.2.16 MSE of that state
+        p11 = p10 - chain_dot(p10, H, HTPHRinv, H.T, p10)
+        # 13.2.17 Update forecast about xi_{t+1} based on our F
+        xi10 = np.dot(F,xi11)
+        # 13.2.21 Update the MSE of the forecast
+        p10 = chain_dot(F,p11,F.T) + Q
     return -loglikelihood
 
-# The below is Luca's version
-#    n = H.shape[1]
-#    nobs = y.shape[1]
-#    if history == False:
-#        xi10History = xi10
-#        xi11History = xi10History
-
-#    p10 = q # eq 13.2.21
-#    loglikelihood = 0
-
-#    for i in range(nobs):
-#        hP_p10_h = np.linalg.inv(chain_dot(h.T,p10,h))
-#        part1 = y[:,i] - np.dot(h.T,xi10History)
-
-        # after training, don't throw this away
-#        if i > ntrain:
-#            part2 = -0.5 * chain_dot(part1.T,hP_p10_h,part1)
-#            det_hpp10h = np.linalg.det(chain_dot(h.T,p10,h))
-#            if det_hpp10h > 10e-300: # not singular
-#                loglike_int = (-n/2.)*np.log(2*np.pi)-.5*np.log(det_hpp10h)+part2
-#                if loglike_int > 10e300:
-#                    raise ValueError("There was an error in forming likelihood")
-#                loglikelihood += loglike_int
-
-        # 13.2.15
-#        xi11History = xi10History + chain_dot(p10,h,hP_p10_h,
-#                part1)
-        # 13.2.16
-
-#        p11 = p10 - chain_dot(p10,h,hP_p10_h,h.T,p10)
-        # 13.2.17
-#        xi10History = np.dot(f,xi11History)
-        # 13.2.21
-#        p10 = chain_dot(f,p11,f.T) + q
-#    return -loglikelihood
-
-#TODO: make a state space model
 class StateSpaceModel(object):
-    def __init__(self, endog, exog=None):
+    def __init__(self, endog, exog=None, ARMA=(0,0)):
         """
         Parameters
         ----------
@@ -150,7 +132,7 @@ class StateSpaceModel(object):
         Notes
         -----
         exog are not handled right now.
-        Created with a (V)ARMA in mind.
+        Created with a (V)ARMA in mind, but not really general yet.
         """
         endog = np.asarray(endog)
         if endog.ndim == 1:
@@ -175,7 +157,6 @@ class StateSpaceModel(object):
 #        self.Hmat =
 #        if n == 1:
 #            F =
-#        self._updatematrices(start_params)
 
     def _updateloglike(self, params, ntrain, penalty, upperbounds, lowerbounds,
             F,A,H,Q,R):
@@ -264,25 +245,21 @@ class StateSpaceModel(object):
             same order as `start_params`
         """
         y = self.endog
-#        xi10 = self.xi10
-#        Q = self.Q
         ntrain = ntrain
         _updateloglike = self._updateloglike
         params = start_params
         if method.lower() == 'bfgs':
             results = optimize.fmin_bfgs(_updateloglike, params,
                     args = (ntrain, penalty, upperbounds, lowerbounds,
-                        F,A,H,Q,R), gtol= 1e-8, epsilon=1e-10)
+                        F,A,H,Q,R), gtol= 1e-8, epsilon=1e-5)
             #TODO: provide more options to user for optimize
         self.params = results
-
-
 
 def updatematrices(params, y, xi10, ntrain, penalty, upperbound, lowerbound):
     """
     TODO: change API, update names
 
-    This isn't general
+    This isn't general.  Copy of Luca's matlab example.
     """
     paramsorig = params
     # are the bounds binding?
@@ -439,9 +416,19 @@ if __name__ == "__main__":
         cholQ = np.array([[x[1],0],[0,x[2]]])
         return np.dot(cholQ,cholQ.T)
     H = np.ones((2,1))
-    ssm_model = StateSpaceModel(y)
+    ssm_model = StateSpaceModel(y)  # need to pass in Xi10!
     ssm_model.fit_kalman(start_params=params, F=F, Q=Q, H=H,
             upperbounds=upperbounds, lowerbounds=lowerbounds)
-# why does the above take 3 times as many iterations?
+# why does the above take 3 times as many iterations than direct max?
 
+    # compare directly to matlab output
+    from scipy import io
+    y_matlab = io.loadmat('./kalman_y.mat')['y'].reshape(-1,1)
+    ssm_model2 = StateSpaceModel(y_matlab)
+    ssm_model2.fit_kalman(start_params=params, F=F, Q=Q, H=H,
+            upperbounds=upperbounds, lowerbounds=lowerbounds)
 
+# matlab output
+    thetaunc = np.array([0.7833, 1.1688, 0.5584])
+    np.testing.assert_almost_equal(ssm_model2.params, thetaunc, 4)
+# WooHoo!
