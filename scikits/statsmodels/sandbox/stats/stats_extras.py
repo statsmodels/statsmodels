e81e5485c17a339d5fece732e7783bdd6b314821
Josef Perktold <josef.pktd@gmail.com>
2010-04-18 11:44:00 -0400

    copy useful function (binned gof chisquaretest) and script from scipy.stats distributions test suite (author Josef)

--- scikits/statsmodels/sandbox/stats/stats_extras.py
+++ scikits/statsmodels/sandbox/stats/stats_extras.py
@@ -284,3 +284,78 @@ def powerdiscrepancy(o, e, lambd=0.0, axis=0, ddof=0):
 
     return D_obs, stats.chi2.sf(D_obs,k-1-ddof)
 
+
+
+#todo: need also binning for continuous distribution
+#      and separated binning function to be used for powerdiscrepancy
+
+
+def gof_chisquare_discrete(distfn, arg, rvs, alpha, msg):
+    '''perform chisquare test for random sample of a discrete distribution
+
+    Parameters
+    ----------
+    distname : string
+        name of distribution function
+    arg : sequence
+        parameters of distribution
+    alpha : float
+        significance level, threshold for p-value
+
+    Returns
+    -------
+    result : bool
+        0 if test passes, 1 if test fails
+
+    Notes
+    -----
+    originally written for scipy.stats test suite,
+    still needs to be checked for standalone usage, insufficient input checking
+    may not run yet (after copy/paste)
+
+    refactor: maybe a class, check returns, or separate binning from
+        test results
+    '''
+
+    # define parameters for test
+##    n=2000
+    n = len(rvs)
+    nsupp = 20
+    wsupp = 1.0/nsupp
+
+##    distfn = getattr(stats, distname)
+##    np.random.seed(9765456)
+##    rvs = distfn.rvs(size=n,*arg)
+
+    # construct intervals with minimum mass 1/nsupp
+    # intervalls are left-half-open as in a cdf difference
+    distsupport = xrange(max(distfn.a, -1000), min(distfn.b, 1000) + 1)
+    last = 0
+    distsupp = [max(distfn.a, -1000)]
+    distmass = []
+    for ii in distsupport:
+        current = distfn.cdf(ii,*arg)
+        if  current - last >= wsupp-1e-14:
+            distsupp.append(ii)
+            distmass.append(current - last)
+            last = current
+            if current > (1-wsupp):
+                break
+    if distsupp[-1]  < distfn.b:
+        distsupp.append(distfn.b)
+        distmass.append(1-last)
+    distsupp = np.array(distsupp)
+    distmass = np.array(distmass)
+
+    # convert intervals to right-half-open as required by histogram
+    histsupp = distsupp+1e-8
+    histsupp[0] = distfn.a
+
+    # find sample frequencies and perform chisquare test
+    freq,hsupp = np.histogram(rvs,histsupp,new=True)
+    cdfs = distfn.cdf(distsupp,*arg)
+    (chis,pval) = stats.chisquare(np.array(freq),n*distmass)
+
+    return chis, pval, (pval > alpha), 'chisquare - test for %s' \
+           'at arg = %s with pval = %s' % (msg,str(arg),str(pval))
+
