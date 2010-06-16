91d552e5602f8c5dd7c0f8eac8372471aecf3014
Skipper Seabold <jsseabold@gmail.com>
2010-06-16 10:57:22 -0400

    Moved grangercausalitytests to tsa/stattools.  Fixed imports

--- scikits/statsmodels/sandbox/tsa/tsatools.py
+++ scikits/statsmodels/sandbox/tsa/tsatools.py
@@ -1,8 +1,4 @@
-
-
 import numpy as np
-
-
 
 def lagmat(x, maxlag, trim='forward'):
     '''create 2d array of lags
@@ -89,85 +85,11 @@ def lagmat2ds(x, maxlag0, maxlagex=None, dropex=0, trim='forward'):
     return np.column_stack(lagsli)
 
 
-def grangercausalitytests(x, maxlag):
-    '''four tests for granger causality of 2 timeseries
-
-    this is a proof-of concept implementation
-    not cleaned up, has some duplicate calculations,
-    memory intensive - builds full lag array for variables
-    prints results
-    not verified with other packages,
-    all four tests give similar results (1 and 4 identical)
-
-    Parameters
-    ----------
-    x : array, 2d, (nobs,2)
-        data for test whether the time series in the second column Granger
-        causes the time series in the first column
-    maxlag : integer
-        the Granger causality test results are calculated for all lags up to
-        maxlag
-
-    Returns
-    -------
-    None : no returns
-        all test results are currently printed
-
-    Notes
-    -----
-    TODO: convert to function that returns and compare with other packages
-
-    '''
-    from scipy import stats # lazy import
-    import scikits.statsmodels as sm  # absolute import for now
-
-    for mlg in range(1, maxlag+1):
-        print '\nGranger Causality'
-        print 'number of lags (no zero)', mlg
-        mxlg = mlg + 1 # Note number of lags starting at zero in lagmat
-
-        # create lagmat of both time series
-        dta = lagmat2ds(x, mxlg, trim='both', dropex=1)
-
-        #add constant
-        dtaown = sm.add_constant(dta[:,1:mxlg])
-        dtajoint = sm.add_constant(dta[:,1:])
-
-        #run ols on both models without and with lags of second variable
-        res2down = sm.OLS(dta[:,0], dtaown).fit()
-        res2djoint = sm.OLS(dta[:,0], dtajoint).fit()
-
-        #print results
-        #for ssr based tests see: http://support.sas.com/rnd/app/examples/ets/granger/index.htm
-        #the other tests are made-up
-
-        # Granger Causality test using ssr (F statistic)
-        fgc1 = (res2down.ssr-res2djoint.ssr)/res2djoint.ssr/(mxlg-1)*res2djoint.df_resid
-        print 'ssr based F test:         F=%-8.4f, p=%-8.4f, df_denom=%d, df_num=%d' % \
-              (fgc1, stats.f.sf(fgc1, mxlg-1, res2djoint.df_resid), res2djoint.df_resid, mxlg-1)
-
-        # Granger Causality test using ssr (ch2 statistic)
-        fgc2 = res2down.nobs*(res2down.ssr-res2djoint.ssr)/res2djoint.ssr
-        print 'ssr based chi2 test:   chi2=%-8.4f, p=%-8.4f, df=%d' %  \
-              (fgc2, stats.chi2.sf(fgc2, mxlg-1), mxlg-1)
-
-        #likelihood ratio test pvalue:
-        lr = -2*(res2down.llf-res2djoint.llf)
-        print 'likelihood ratio test: chi2=%-8.4f, p=%-8.4f, df=%d' %  \
-              (lr, stats.chi2.sf(lr, mxlg-1), mxlg-1)
-
-        # F test that all lag coefficients of exog are zero
-        rconstr = np.column_stack((np.zeros((mxlg-1,mxlg-1)), np.eye(mxlg-1, mxlg-1),\
-                                   np.zeros((mxlg-1, 1))))
-        ftres = res2djoint.f_test(rconstr)
-        print 'parameter F test:         F=%-8.4f, p=%-8.4f, df_denom=%d, df_num=%d' % \
-              (ftres.fvalue, ftres.pvalue, ftres.df_denom, ftres.df_num)
-
-__all__ = ['lagmat', 'lagmat2ds', 'grangercausalitytests']
+__all__ = ['lagmat', 'lagmat2ds']
 
 if __name__ == '__main__':
     # sanity check, mainly for imports
     x = np.random.normal(size=(100,2))
     tmp = lagmat(x,2)
     tmp = lagmat2ds(x,2)
-    grangercausalitytests(x, 2)
+#    grangercausalitytests(x, 2)
