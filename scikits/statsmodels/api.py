6d1a64f40f5eb3285415dc299fde22a1ec2b1440
Skipper Seabold <jsseabold@gmail.com>
2011-01-11 12:30:49 -0500

    Silence test in test_arma.  Add api.py to vcs

---
+++ scikits/statsmodels/api.py
@@ -0,0 +1,13 @@
+import iolib, datasets, tools
+from tools.tools import add_constant, categorical
+import regression
+from regression.regression import OLS, GLS, WLS
+from .glm.glm import GLM
+from .glm import families
+import robust
+from .robust.rlm import RLM
+from .discrete.discretemod import Poisson, Logit, Probit, MNLogit
+import tsa
+from __init__ import test
+from version import __version__
+from info import __doc__
