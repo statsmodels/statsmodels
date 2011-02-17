b5f8574b38177468e86275a641f67034d4e5eccb
Skipper Seabold <jsseabold@gmail.com>
2011-02-17 16:23:33 -0500

    Include tsa/var/data in install

--- setup.py
+++ setup.py
@@ -122,7 +122,12 @@ def configuration(parent_package='', top_path=None, package_name=DISTNAME):
     config.add_data_dir('scikits/statsmodels/regression/tests')
     config.add_data_dir('scikits/statsmodels/robust/tests')
     config.add_data_dir('scikits/statsmodels/tsa/var/tests')
-    config.add_data_dir('scikits/statsmodels/tsa/var/data')
+    vardatafiles = [os.path.join(r,d) for r,ds,f in \
+                    os.walk('scikits/statsmodels/tsa/var/data')
+                    for d in f if not os.path.splitext(d)[1] in ['.py',
+                    '.pyc']]
+    for f in vardatafiles:
+        config.add_data_files(f)
     extradatafiles = [os.path.join(r,d) for r,ds,f in \
                       os.walk('scikits/statsmodels/datasets')
                       for d in f if not os.path.splitext(d)[1] in
