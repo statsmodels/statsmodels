24f6effcb4837066d3762838080df3eb17db9a87
Skipper Seabold <jsseabold@gmail.com>
2011-02-19 22:00:04 -0500

    Fix backslash in sphinx conf.py so docs will build

--- scikits/statsmodels/docs/source/conf.py
+++ scikits/statsmodels/docs/source/conf.py
@@ -16,7 +16,7 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('..\sphinxext'))
+sys.path.insert(0, os.path.abspath('../sphinxext'))
 
 
 # -- General configuration -----------------------------------------------------
