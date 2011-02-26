536dc8416e9d075a73e3a758c437001e0b50cdc2
Skipper Seabold <jsseabold@gmail.com>
2011-02-25 19:32:54 -0500

    Rename robust.rlm -> robust.robust_linear_model. Make sure all tests can be run from their sub-package.

--- scikits/statsmodels/api.py
+++ scikits/statsmodels/api.py
@@ -5,7 +5,7 @@ from .regression.linear_model import OLS, GLS, WLS, GLSAR
 from .genmod.generalized_linear_model import GLM
 from .genmod import families
 import robust
-from .robust.rlm import RLM
+from .robust.robust_linear_model import RLM
 from .discrete.discrete_model import Poisson, Logit, Probit, MNLogit
 import tsa
 from __init__ import test
