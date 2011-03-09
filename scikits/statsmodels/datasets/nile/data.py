ce6212495c40e099d5332a09bbb5185fab287db0
Skipper Seabold <jsseabold@gmail.com>
2011-03-09 14:45:43 -0500

    Add Nile River dataset

---
+++ scikits/statsmodels/datasets/nile/data.py
@@ -0,0 +1,93 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# Last Change: Tue Jul 17 05:00 PM 2007 J
+
+# The code and descriptive text is copyrighted and offered under the terms of
+# the BSD License from the authors; see below. However, the actual dataset may
+# have a different origin and intellectual property status. See the SOURCE and
+# COPYRIGHT variables for this information.
+
+# Copyright (c) 2007 David Cournapeau <cournape@gmail.com>
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in
+#       the documentation and/or other materials provided with the
+#       distribution.
+#     * Neither the author nor the names of any contributors may be used
+#       to endorse or promote products derived from this software without
+#       specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+__all__ = ['COPYRIGHT','TITLE','SOURCE','DESCRSHORT','DESCRLONG','NOTE', 'load']
+
+"""Name of dataset."""
+
+__docformat__ = 'restructuredtext'
+
+COPYRIGHT   = """E.g., This is public domain."""
+TITLE       = """Title of the dataset"""
+SOURCE      = """
+This section should provide a link to the original dataset if possible and
+attribution and correspondance information for the dataset's original author
+if so desired.
+"""
+
+DESCRSHORT  = """A short description."""
+
+DESCRLONG   = """A longer description of the dataset."""
+
+#suggested notes
+NOTE        = """
+Number of observations:
+Number of variables:
+Variable name definitions:
+
+Any other useful information that does not fit into the above categories.
+"""
+
+from numpy import recfromtxt, column_stack, array
+from scikits.statsmodels.datasets import Dataset
+from os.path import dirname, abspath
+
+def load():
+    """
+    Load the Nile data and return a Dataset class instance.
+
+    Returns
+    -------
+    Dataset instance:
+        See DATASET_PROPOSAL.txt for more information.
+    """
+    filepath = dirname(abspath(__file__))
+##### EDIT THE FOLLOWING TO POINT TO DatasetName.csv #####
+    data = recfromtxt(filepath + '/nile.csv', delimiter=",",
+            names=True, dtype=float, usecols=(1))
+    names = list(data.dtype.names)
+##### SET THE INDEX #####
+    endog = array(data[names[0]], dtype=float)
+    endog_name = names[0]
+##### SET THE INDEX #####
+#    exog = column_stack(data[i] for i in names[1:]).astype(float)
+#    exog_name = names[1:]
+    dataset = Dataset(data=data, names=names, endog=endog,
+            endog_name = endog_name)
+    return dataset
