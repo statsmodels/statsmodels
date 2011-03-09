1b22ae01ee361ce99200ba67ddad5808a0d89836
Skipper Seabold <jsseabold@gmail.com>
2011-03-08 21:51:54 -0500

    Added tsa.add_lag with tests

--- scikits/statsmodels/tsa/api.py
+++ scikits/statsmodels/tsa/api.py
@@ -5,7 +5,7 @@ from .var.varmod import VAR
 from .var.dynamic import DynamicVAR
 
 import tsatools
-from .tsatools import (add_trend, detrend, lagmat, lagmat2ds)
+from .tsatools import (add_trend, detrend, lagmat, lagmat2ds, add_lag)
 import interp
 import stattools
 from .stattools import (adfuller, acovf, q_stat, acf, pacf_yw, pacf_ols, pacf,
