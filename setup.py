b0ae103314696ebf68362f92cbafdc3f44b10ab7
Skipper Seabold <jsseabold@gmail.com>
2011-03-26 12:11:37 -0400

    Add vector_ar/tests to setup.py

--- setup.py
+++ setup.py
@@ -159,7 +159,7 @@ def configuration(parent_package='', top_path=None, package_name=DISTNAME):
     config.add_data_dir('scikits/statsmodels/genmod/tests')
     config.add_data_dir('scikits/statsmodels/regression/tests')
     config.add_data_dir('scikits/statsmodels/robust/tests')
-    config.add_data_dir('scikits/statsmodels/tsa/var/tests')
+    config.add_data_dir('scikits/statsmodels/tsa/vector_ar/tests')
     vardatafiles = [os.path.join(r,d) for r,ds,f in \
                     os.walk('scikits/statsmodels/tsa/vector_ar/data')
                     for d in f if not os.path.splitext(d)[1] in ['.py',
