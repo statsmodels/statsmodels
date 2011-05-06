16bf4da3019eb7ac20ec3d81cb8898e833ceb8d4
Skipper Seabold <jsseabold@gmail.com>
2011-05-05 21:19:39 -0400

    Played around with KDE fft implementation.

--- scikits/statsmodels/sandbox/nonparametric/kde.py
+++ scikits/statsmodels/sandbox/nonparametric/kde.py
@@ -23,28 +23,59 @@ import numpy as np
     #TODO: finish docstring.
 #    return np.
 
+def linbin(X,a,b,M, trunc=1):
+    """
+    Linear Binning as described in Fan and Marron (1994)
+    """
+    gcnts = np.zeros(M)
+    delta = (b-a)/(M-1)
+
+    for x in X:
+        lxi = ((x - a)/delta) # +1
+        li = int(lxi)
+        rem = lxi - li
+        if li > 1 and li < M:
+            gcnts[li] = gcnts[li] + 1-rem
+            gcnts[li+1] = gcnts[li+1] + rem
+        if li > M and trunc == 0:
+            gcnts[M] = gncts[M] + 1
+
+    return gcnts
+
+#This is much faster, might be able to speed it up still?
 def counts(x,v):
     """
     Counts the number of elements of x that fall within v
 
     Notes
     -----
-    Flattens both x and v.
+    Using np.digitize and np.bincount
     """
-    x = x.flatten()
-    v = v.flatten()
-    c = []
-    nv = np.r_[-np.inf,v]
-    for i in range(1,len(nv)):
-        t = np.sum(np.logical_and(x<=nv[i],x>nv[i-1]))
-        c.append(t)
-    return np.asarray(c)
+    idx = np.digitize(x,v)
+    return np.bincount(idx, minlength=len(v))
+
+#def counts(x,v):
+#    """
+#    Counts the number of elements of x that fall within v
+#
+#    Notes
+#    -----
+#    Flattens both x and v.
+#    """
+#    x = x.flatten()
+#    v = v.flatten()
+#    c = []
+#    nv = np.r_[-np.inf,v]
+#    for i in range(1,len(nv)):
+#        t = np.sum(np.logical_and(x<=nv[i],x>nv[i-1]))
+#        c.append(t)
+#    return np.asarray(c)
 
 
 def kdesum(x,axis=0):
     return np.asarray([np.sum(x[i] - x, axis) for i in range(len(x))])
 
-def kdensity(X, kernel="epa", bw=None, weights=None, gridsize=None, axis=0, clip=(-np.inf,np.inf)):
+def kdensity(X, kernel="epa", bw=None, weights=None, gridsize=None, axis=0, clip=(-np.inf,np.inf), cut=3):
     """
     Rosenblatz-Parzen univariate kernel desnity estimator
 
@@ -79,15 +110,7 @@ def kdensity(X, kernel="epa", bw=None, weights=None, gridsize=None, axis=0, clip
     X = X[np.logical_and(X>clip[0], X<clip[1])] # won't work for two columns.
                                                 # will affect underlying data?
     nobs = float(len(X)) # after trim
-    if gridsize == None:
-        gridsize = np.max((nobs,512.))
-    # round gridsize up to the next power of 2
-    gridsize = 2**np.ceil(np.log2(gridsize))
-    # define mesh
-    grid = np.linspace(np.min(X,axis),np.max(X,axis),gridsize)
-    # this will fail for not 1 column
-    if grid.ndim == 1:
-        grid = grid[:,None]
+
     # if bw is None, select optimal bandwidth for kernel
     if bw == None:
         if kernel.lower() == "gauss":
@@ -98,6 +121,17 @@ def kdensity(X, kernel="epa", bw=None, weights=None, gridsize=None, axis=0, clip
         h = c * nobs**(-1/5.)
     else:
         h = bw
+
+    if gridsize == None:
+        gridsize = np.max((nobs,512.))
+    # round gridsize up to the next power of 2
+    gridsize = 2**np.ceil(np.log2(gridsize))
+    # define mesh
+    grid = np.linspace(np.min(X,axis) - cut*bw,np.max(X,axis)+cut*bw,gridsize)
+    # this will fail for not 1 column
+    if grid.ndim == 1:
+        grid = grid[:,None]
+
     k = (X.T - grid)/h  # uses broadcasting
 # res = np.repeat(x,n).reshape(m,n).T - np.repeat(xi,m).reshape(n,m))/h
     if kernel.lower() == "epa":
@@ -117,7 +151,7 @@ def kdensity(X, kernel="epa", bw=None, weights=None, gridsize=None, axis=0, clip
 #TODO: need to check this
 #    return k.mean(1),k
 
-def kdensityf(X, kernel="epa", bw=None, weights=None, gridsize=None, clip=(-np.inf,np.inf)):
+def kdensityf(X, kernel="epa", bw=None, weights=None, gridsize=None, clip=(-np.inf,np.inf), cut=3):
     """
     Rosenblatz-Parzen univariate kernel desnity estimator
 
@@ -151,6 +185,15 @@ def kdensityf(X, kernel="epa", bw=None, weights=None, gridsize=None, clip=(-np.i
     X = X[np.logical_and(X>clip[0], X<clip[1])] # won't work for two columns.
                                                 # will affect underlying data?
     nobs = float(len(X)) # after trim
+    if bw == None:
+        if kernel.lower() == "gauss":
+            c = 1.0592 * np.std(X, ddof=1)
+        if kernel.lower() == "epa":
+            c = 1.0487 * np.std(X, ddof=1) # is this correct?
+#TODO: can use windows from scipy.signal?
+        bw = c * nobs**(-1/5.)
+
+
     if gridsize == None:
         gridsize = np.max((nobs,512.))
     # round gridsize up to the next power of 2
@@ -158,39 +201,41 @@ def kdensityf(X, kernel="epa", bw=None, weights=None, gridsize=None, clip=(-np.i
 
     # Discretize the data on an M-element grid over [a,b] to find the weight seq.
     # define grid
-    grid,delta = np.linspace(np.min(X),np.max(X),gridsize,retstep=True)
-    RANGE = np.max(X) - np.min(X)
+    grid,delta = np.linspace(np.min(X)-cut*bw,np.max(X)+cut*bw,gridsize,retstep=True)
+    RANGE = np.max(X)+cut*bw - np.min(X)-cut*bw
     # sort the data
     X.sort(0)
     # how fine is the data vis-a-vis the grid?
     count = counts(X,grid)
+
     # make a weights array
 # weighting according to Silverman
-#    wt = np.zeros_like(grid)    #xi_{k} in Silverman
-#    j = 0
-#    for k in range(int(gridsize-1)):
-#        if count[k]>0: # there are points of X in the grid here
-#            Xingrid = X[j:j+count[k]] # get all these points
-#            # get weights at grid[k],grid[k+1]
-#            wt[k] += np.sum(grid[k+1]-Xingrid)
-#            wt[k+1] += np.sum(Xingrid-grid[k])
-#            j += count[k]
-#    wt /= (nobs-1)*delta**2 # normalize wt to sum to 1/delta # why nobs-1
-                            # missing weight for the last data point
+    wt = np.zeros_like(grid)    #xi_{k} in Silverman
+    j = 0
+    for k in range(int(gridsize-1)):
+        if count[k]>0: # there are points of X in the grid here
+            Xingrid = X[j:j+count[k]] # get all these points
+            # get weights at grid[k],grid[k+1]
+            wt[k] += np.sum(grid[k+1]-Xingrid)
+            wt[k+1] += np.sum(Xingrid-grid[k])
+            j += count[k]
+    wt /= (nobs)*delta**2 # normalize wt to sum to 1/delta
 
 # Weights according to Hall and Jones
 # still use count
-    wt = np.zeros_like(grid[:gridsize/2])
+#    wt = np.zeros_like(grid[:gridsize/2])
 
 
 #    print nobs
 #    print len(wt)
     assert np.allclose(np.sum(wt), 1/delta)
+
     # step 2 compute FFT of the weights
 #    y = np.fft.rfft(wt) # RFFT uses opposite sign vs. Silverman and doesn't
                         # normalize by len(wt)
 # so Silverman's definition corresponds to
-#    y = np.fft.irfft(wt, n =gridsize)
+    y = np.fft.irfft(wt, n = gridsize)
+#    y = np.fft.ifft(wt, n = gridsize)
     # put in order expected
     # see Monro (1976) AS 97 (FORRT) real parts first then the imaginary parts.
     # brute force
@@ -206,30 +251,54 @@ def kdensityf(X, kernel="epa", bw=None, weights=None, gridsize=None, clip=(-np.i
     s = 2*ell*np.pi/RANGE
 
     # step 3 and 4 for optimal bandwidt compute zstar and the density estimate f
-    if bw == None:
-        if kernel.lower() == "gauss":
-            c = 1.0592 * np.std(X, ddof=1)
-        if kernel.lower() == "epa":
-            c = 1.0487 * np.std(X, ddof=1) # is this correct?
-#TODO: can use windows from scipy.signal?
-        bw = c * nobs**(-1/5.)
 #   3.59
 
 # if use irfft then you don't have to reorder
-    print y.shape
-    print s.shape
     zstar = np.exp(-.5*bw**2*s**2)*y
 #    zstar = np.exp(-.5*bw**2*s**2)*y_neworder
-#    f = np.fft.irfft(zstar)
+    f = np.fft.rfft(zstar)
 # and given the defintion in Silverman and numpy then f should be
-    f = np.real(np.fft.fft(zstar))
+#    f = np.real(np.fft.fft(zstar))
     return f
 
 if __name__ == "__main__":
+    import numpy as np
+    np.random.seed(12345)
     xi = np.random.randn(100)
-    f,k = kdensity(xi)
+    f,k = kdensity(xi, kernel="gauss", bw=.372735)
     f2 = kdensityf(xi, kernel="gauss")
 
+# do Fourier method by hand
+    X = xi.copy()
+    nobs = len(X)
+#    c = 1.0592 * np.std(X, axis=0, ddof=1)
+    c = .9 * np.std(X, axis=0, ddof=1)
+    bw = c * nobs ** -.2
+    gridsize = 512
+    cut = 3
+    a,b = X.min() - cut*bw, X.max()+cut*bw
+    grid,delta = np.linspace(a,b,gridsize,retstep=True)
+    wt = np.zeros_like(grid)    #xi_{k} in Silverman
+    j = 0
+    count = counts(X,grid)
+    RANGE = np.max(X)+cut*bw - np.min(X)-cut*bw
+    for k in range(int(gridsize-1)):
+        if count[k]>0: # there are points of X in the grid here
+            Xingrid = X[j:j+count[k]] # get all these points
+            # get weights at grid[k],grid[k+1]
+            wt[k] += np.sum(grid[k+1]-Xingrid)
+            wt[k+1] += np.sum(Xingrid-grid[k])
+            j += count[k]
+    wt /= (nobs)*delta**2 # normalize wt to sum to 1/delta
+    y = np.fft.ifft(wt, n = gridsize)
+    ell = np.arange(-gridsize/2.,gridsize/2.)
+    s = 2*ell*np.pi/RANGE
+    zstar = np.exp(-.5*bw**2*s**2)*y
+    f2 = np.fft.fft(zstar)
+
+
+
+"""
     # Hall and Jones example
     c = [4,2,1,2,0,0,0,0]
     kappa = [7,5,2,0,0,0,2,5]
@@ -244,3 +313,4 @@ if __name__ == "__main__":
     # can this be done with fftconvolve?
     #from scipy import signal
 #TODO: note that scipy.fftpack looks to be more "standard" notation
+"""
