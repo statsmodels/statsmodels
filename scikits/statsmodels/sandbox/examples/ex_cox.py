d1c7fd9cf6c528a6ff905cfc60dceab04278aac9
Scott Piraino <scottpiraino@gmail.com>
2011-07-19 20:24:20 -0400

    Added stratification example to ex_cox.py

--- scikits/statsmodels/sandbox/examples/ex_cox.py
+++ scikits/statsmodels/sandbox/examples/ex_cox.py
@@ -44,7 +44,7 @@ print "wald test"
 print "test stat:", wald
 print "df:", df
 print "p-value:", pval
-##respective functions are also available for score
+##respective function are also available for score
 ##and likelihood-ratio tests
 con_int = results.conf_int()
 print "confidence intervals for parameters"
@@ -58,10 +58,48 @@ print test_coeffs
 ##index two column is the z-score
 ##index three column is the p-value
 
+##Maybe we are interested in some classification of "expressed"
+##and "not expressed" rather than just the floating point expression
+##values. The dataset does not included this, so, for demonstration
+##purposes, say that a gene is "expressed" (1) if its expression value
+##is greater than the mean for the gene, and not expressed (0)
+##otherwise. We can create an indicator variable "expressed", that
+##represents whether the first gene in the dataset is "expressed"
+
+surv = Survival(0, censoring=1, data=darray)
+gene1 = exog[:,0]
+expressed = gene1 > gene1.mean()
+##replacing the column for th first gene with the indicatore variable
+exog[:,0] = expressed
+##create a model with the new exogenous variable
+cox2 = CoxPH(surv, exog)
+
+##We can now create a stratified cox model using the "stratify" method
+##stratify using the index 0 variable (the indicator variable)
+##copy=False alters the object cox. Default is copy=True, to create
+##a new CoxPH object
+cox2.stratify(0,copy=False)
+##This can also be achieved by specifying an argument to the
+##"strata" parameter when the model is instantiated
+
+##The stratified model now has the stratified variable stored seperately
+print "\nstratified cox model"
+print "sratified variable"
+print cox2.strata[range(5),:]
+print "unstratified variables"
+print cox2.exog[range(5),:]
+
+##Just like the previous model, we can fit the model
+results2 = cox2.fit()
+
+##This new CoxResults object has access to all the methods of a standard
+##cox model
+print "estimated parameters with stratification"
+print results2.params
+
 ##Planned developments for the CoxPH class:
 ##Currently only works for right-censored data. Handling for more
 ##complex censoring is forthcoming
-##stratified Cox models
 ##fitting with time-dependent covariates and time-dependent
 ##coefficients
 ##quick summary method
