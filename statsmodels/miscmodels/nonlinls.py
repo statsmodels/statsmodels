b6a597842a28193c777454bc86c56aca2c937004
Josef Perktold <josef.pktd@gmail.com>
2011-08-31 12:46:44 -0400

    BUG, ENH: correct weights in NonlinearLS, no unittests yet

--- statsmodels/miscmodels/nonlinls.py
+++ statsmodels/miscmodels/nonlinls.py
@@ -9,13 +9,28 @@ import numpy as np
 from scipy import optimize
 
 from statsmodels.base.model import Model
-
+from statsmodels.tools.decorators import cache_readonly
+from statsmodels.regression.linear_model import RegressionResults
 
 class Results(object):
     '''just a dummy placeholder for now
     most results from RegressionResults can be used here
+
+    still used while testing
     '''
     pass
+
+class NonLinearLSResults(RegressionResults):
+    '''just a dummy placeholder for now
+    most results from RegressionResults can be used here
+    '''
+
+    @cache_readonly
+    def wresid(self):
+        return self.resid * np.sqrt(self.model.weights)
+        return self.model.geterrors(self, self.params)#, weights=None)
+#        return self.model.wendog - self.model.predict(self.model.wexog,
+#                self.params)
 
 
 ##def getjaccov(retval, n):
@@ -122,6 +137,35 @@ class NonlinearLS(Model):  #or subclass a model
         else:
             self.weights = None
 
+    #copied from WLS
+    def whiten(self, X):
+        """
+        Whitener for WLS model, multiplies each column by sqrt(self.weights)
+
+        Parameters
+        ----------
+        X : array-like
+            Data to be whitened
+
+        Returns
+        -------
+        sqrt(weights)*X
+        """
+
+        X = np.asarray(X)
+        if self.weights is None:
+            return X
+        else:
+            weights = np.sqrt(self.weights)
+            if X.ndim == 1:
+                return X * np.sqrt(weights)
+            elif X.ndim == 2:
+                if np.shape(self.weights) == ():
+                    whitened = np.sqrt(weights)*X
+                else:
+                    whitened = np.sqrt(weights)[:,None]*X
+                return whitened
+
     def predict(self, exog, params=None):
         #copied from GLS, Model has different signature
         return self._predict(params)
@@ -138,7 +182,7 @@ class NonlinearLS(Model):  #or subclass a model
             if self.weights is None:
                 return self.endog - self._predict(params)
             else:
-                weights = self.weights
+                weights = np.sqrt(self.weights)
         return weights * (self.endog - self._predict(params))
 
     def errorsumsquares(self, params):
@@ -188,8 +232,10 @@ class NonlinearLS(Model):  #or subclass a model
         fitres.params = popt
         fitres.pcov = pcov
         fitres.rawres = res
-        self.wendog = self.endog  #add weights
-        self.wexog = self.jac_predict(popt)
+##        self.wendog = self.endog  #add weights
+##        self.wexog = self.jac_predict(popt)
+        self.wendog = self.whiten(self.endog)  #add weights
+        self.wexog = self.whiten(self.jac_predict(popt))
         pinv_wexog = np.linalg.pinv(self.wexog)
         self.normalized_cov_params = np.dot(pinv_wexog,
                                          np.transpose(pinv_wexog))
@@ -201,11 +247,15 @@ class NonlinearLS(Model):  #or subclass a model
 
 ##        if not weights is None
 ##            fitres.wexogw = self.weights * self.jacpredict(popt)
+
         from statsmodels.regression import RegressionResults
-        results = RegressionResults
+        #results = RegressionResults
 
         beta = popt
-        lfit = RegressionResults(self, beta,
+#        lfit = RegressionResults(self, beta,
+#                       normalized_cov_params=self.normalized_cov_params)
+
+        lfit = NonLinearLSResults(self, beta,
                        normalized_cov_params=self.normalized_cov_params)
 
         lfit.fitres = fitres   #mainly for testing
@@ -286,7 +336,7 @@ if __name__ == '__main__':
         return (y - func(params, x))**2
 
 
-
+    from scipy import optimize
 
     x = np.linspace(0,4,50)
     params = np.array([2.5, 1.3, 0.5])
