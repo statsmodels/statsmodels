3d0ef1ebe60e8582f32ad9df9505b099fa6a8684
Skipper Seabold <jsseabold@gmail.com>
2011-09-06 00:08:33 -0400

    ENH: Allow pandas import unconditionally

--- scikits/statsmodels/tsa/api.py
+++ scikits/statsmodels/tsa/api.py
@@ -2,11 +2,7 @@ from .ar_model import AR
 from .arima_model import ARMA
 import vector_ar as var
 from .vector_ar.var_model import VAR
-try:
-    import pandas
-    from .vector_ar.dynamic import DynamicVAR
-except:
-    pass
+from .vector_ar.dynamic import DynamicVAR
 import filters
 import tsatools
 from .tsatools import (add_trend, detrend, lagmat, lagmat2ds, add_lag)
