d272353cb199e4831d81ca231143fc8ce021d4b6
Skipper Seabold <jsseabold@gmail.com>
2011-09-06 01:53:22 -0400

    ENH: Add some more datetools for working with strings

--- scikits/statsmodels/tsa/base/datetools.py
+++ scikits/statsmodels/tsa/base/datetools.py
@@ -2,6 +2,7 @@ import re
 import datetime
 from pandas import datetools as pandas_datetools
 import scikits.timeseries as ts
+import numpy as np
 
 #TODO: unify all the frequency information
 def _date_from_idx(d1, idx, freq):
@@ -39,8 +40,8 @@ _quarter_to_day = {
         "IV" : (12, 31)
         }
 
-mdays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-_months_with_days = zip(range(1,13),mdays)
+_mdays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+_months_with_days = zip(range(1,13), _mdays)
 _month_to_day = dict(zip(map(str,range(1,13)), _months_with_days))
 _month_to_day.update(dict(zip(["I", "II", "III", "IV", "V", "VI",
                                "VII", "VIII", "IX", "X", "XI", "XII"],
@@ -66,7 +67,7 @@ _m_pattern = '''
 
 (:?m)           # use m or a : as a separator
 
-(([1-9][1-2]?)|(I?XI{0,2}|I?VI{0,3}|I{1,3}))  # match 1-12 or
+(([1-9][0-2]?)|(I?XI{0,2}|I?VI{0,3}|I{1,3}))  # match 1-12 or
                                               # I-XII roman numerals
 
 $               # end of string
@@ -88,9 +89,11 @@ def date_parser(timestr, parserinfo=None, **kwargs):
     if re.search(_q_pattern, timestr, flags):
         y,q = timestr.replace(":","").lower().split('q')
         month, day = _quarter_to_day[q.upper()]
+        year = int(y)
     elif re.search(_m_pattern, timestr, flags):
         y,m = timestr.replace(":","").lower().split('m')
         month, day = _month_to_day[m.upper()]
+        year = int(y)
         if _is_leap(y) and month == 2:
             day += 1
     elif re.search(_y_pattern, timestr, flags):
@@ -100,3 +103,85 @@ def date_parser(timestr, parserinfo=None, **kwargs):
         return pandas_datetools.parser.parse(timestr, parserinfo, kwargs)
 
     return datetime.datetime(year, month, day)
+
+def date_range_str(start, end=None, length=None):
+    """
+    Returns a list of abbreviated date strings.
+
+    Parameters
+    ----------
+    start : str
+        The first abbreviated date, for instance, '1965q1' or '1965m1'
+    end : str, optional
+        The last abbreviated date if length is None.
+    length : int, optional
+        The length of the returned array of end is None.
+
+    Returns
+    -------
+    date_range : list
+        List of strings
+    """
+    flags = re.IGNORECASE | re.VERBOSE
+    #_check_range_inputs(end, length, freq)
+    start = start.lower()
+    if re.search(_m_pattern, start, flags):
+        annual_freq = 12
+        split = 'm'
+    elif re.search(_q_pattern, start, flags):
+        annual_freq = 4
+        split = 'q'
+    yr1, offset1 = map(int, start.replace(":","").split(split))
+    if end is not None:
+        end = end.lower()
+        yr2, offset2 = map(int, end.replace(":","").split(split))
+        length = (yr2 - yr1) * annual_freq + offset2
+    elif length:
+        yr2 = yr1 + length // annual_freq
+        offset2 = length % annual_freq
+    years = np.repeat(range(yr1, yr2), annual_freq).tolist()
+    years = np.r_[years, [str(yr2)]*offset2]
+    offset = np.tile(np.arange(1, annual_freq+1), yr2-yr1)
+    offset = np.r_[offset, np.arange(1,offset2+1).astype('a2')]
+    date_arr_range = [''.join([i,split,j]) for i,j in zip(years, offset)]
+    return date_arr_range
+
+def dates_from_str(dates):
+    """
+    Turns a sequence of date strings and returns a list of datetime.
+
+    Parameters
+    ----------
+    dates : array-like
+        A sequence of abbreviated dates as string. For instance,
+        '1996m1' or '1996Q1'. The datetime dates are at the end of the
+        period.
+
+    Returns
+    -------
+    date_list : array
+        A list of datetime types.
+    """
+    return map(date_parser, dates)
+
+def dates_from_range(start, end=None, length=None):
+    """
+    Turns a sequence of date strings and returns a list of datetime.
+
+    Parameters
+    ----------
+    start : strdates = sm.tsa.datetools.date_range('1960m1', length=nobs)
+arma_mod = sm.tsa.ARMA(y, dates=dates, freq='m')
+        The first abbreviated date, for instance, '1965q1' or '1965m1'
+    end : str, optional
+        The last abbreviated date if length is None.
+    length : int, optional
+        The length of the returned array of end is None.
+
+    Returns
+    -------
+    date_list : array
+        A list of datetime types.
+    """
+    dates = date_range_str(start, end, length)
+    return dates_from_str(dates)
