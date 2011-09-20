6abf2cb73e8ed9cd73ae6848a4a5555c8051a82d
Josef Perktold <josef.pktd@gmail.com>
2011-09-20 12:56:14 -0400

    REF: add summary2 to linear_model Results, add test file (prints currently)

---
+++ scikits/statsmodels/iolib/tests/test_summary.py
@@ -0,0 +1,53 @@
+'''examples to check summary, not converted to tests yet
+
+
+'''
+
+if __name__ == '__main__':
+    
+    from scikits.statsmodels.regression.tests.test_regression  import TestOLS
+
+    #def mytest():
+    aregression = TestOLS()
+    TestOLS.setupClass()
+    results = aregression.res1
+    r_summary = str(results.summary())
+    print r_summary
+    olsres = results
+
+    print '\n\n'
+
+    r_summary = str(results.summary2())
+    print r_summary
+    print '\n\n'
+
+
+    from scikits.statsmodels.discrete.tests.test_discrete  import TestProbitNewton
+
+    aregression = TestProbitNewton()
+    TestProbitNewton.setupClass()
+    results = aregression.res1
+    r_summary = str(results.summary())
+    print r_summary
+
+    from scikits.statsmodels.robust.tests.test_rlm  import TestHampel
+
+    aregression = TestHampel()
+    #TestHampel.setupClass()
+    results = aregression.res1
+    r_summary = str(results.summary())
+    print r_summary
+
+    print '\n\n'
+
+    from scikits.statsmodels.genmod.tests.test_glm  import TestGlmBinomial
+
+    aregression = TestGlmBinomial()
+    #TestGlmBinomial.setupClass()
+    results = aregression.res1
+    r_summary = str(results.summary2())
+    print r_summary
+
+    #print results.summary2(return_fmt='latex')
+    #print results.summary2(return_fmt='csv')
+
