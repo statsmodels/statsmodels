a3abff437d0ba3e9a76205ddb913f0ceb6e61a35
Josef Perktold <josef.pktd@gmail.com>
2011-09-27 13:10:14 -0400

    ENH: add tests for stats/stattools, add Anderson Darling for normal distribution

---
+++ scikits/statsmodels/stats/adnorm.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 25 21:23:38 2011
+
+Author: Josef Perktold and Scipy developers
+License : BSD-3
+"""
+
+import numpy as np
+from scipy import stats
+
+from numpy import exp
+
+def anderson_statistic(x, dist='norm', fit=True, params=(), axis=0):
+    '''calculate anderson-darling A2 statistic
+    
+    Parameters
+    ----------
+    x : array_like
+        data
+    dist : 'norm' or callable
+        null distribution for the test statistic
+    fit : bool
+        If True, then the distribution parameters are estimated.
+        Currently only for 1d data x, except in case dist='norm'
+    params : tuple
+        optional distribution parameters if fit is False
+    axis : integer
+        If dist is 'norm' or fit is False, then data can be an n-dimensional
+        and axis specifies the axis of a variable
+        
+    Returns
+    -------
+    ad2 : float or ndarray
+        Anderson-Darling statistic
+
+
+    '''
+    x = np.asarray(x)
+    y = np.sort(x, axis=axis)
+    N = y.shape[axis]
+    if fit:
+        if dist == 'norm':
+            xbar = np.expand_dims(np.mean(x, axis=axis), axis)
+            s = np.expand_dims(np.std(x, ddof=1, axis=axis), axis)
+            w = (y-xbar)/s
+            z = stats.norm.cdf(w)
+            #print z
+        elif hasattr(dist, '__call__'):
+            params = dist.fit(x)
+            #print params
+            z = dist.cdf(y, *params)
+            print z
+    else:
+        if hasattr(dist, '__call__'):
+            z = dist.cdf(y, *params)
+        else:
+            raise ValueError('if fit is false, then dist needs to be callable')
+
+    i = np.arange(1,N+1)
+    sl1 = [None]*x.ndim
+    sl1[axis] = slice(None)
+    sl2 = [slice(None)]*x.ndim
+    sl2[axis] = slice(None,None,-1)
+    S = np.sum((2*i[sl1]-1.0)/N*(np.log(z)+np.log(1-z[sl2])), axis=axis)
+    A2 = -N-S
+    return A2
+
+
+def ad_normal(x, axis=0):
+    '''Anderson-Darling test for normal distribution unknown mean and variance
+    
+    Parameter
+    ---------
+    x : array_like
+        data array, currently only 1d
+        
+    Returns
+    -------
+    ad2 : float
+        Anderson Darling test statistic
+    pval : float
+        pvalue for hypothesis that the data comes from a normal distribution
+        with unknown mean and variance
+    
+    '''
+    #ad2 = stats.anderson(x)[0]
+    ad2 = anderson_statistic(x, dist='norm', fit=True, axis=axis)
+    n = x.shape[axis]
+    
+    ad2a = ad2 * (1 + 0.75/n + 2.25/n**2)
+    
+    if np.size(ad2a) == 1:
+        if (ad2a >= 0.00 and ad2a < 0.200):
+            pval = 1 - np.exp(-13.436 + 101.14 * ad2a - 223.73 * ad2a**2)
+        elif ad2a < 0.340:
+            pval = 1 - np.exp(-8.318 + 42.796 * ad2a - 59.938 * ad2a**2)
+        elif ad2a < 0.600:
+            pval = np.exp(0.9177 - 4.279 * ad2a - 1.38 * ad2a**2)
+        elif ad2a <= 13:
+            pval = np.exp(1.2937 - 5.709 * ad2a + 0.0186 * ad2a**2)
+        else:
+            pval = 0.0  # is < 4.9542108058458799e-31
+    
+    else:
+        bounds = np.array([0.0, 0.200, 0.340, 0.600])
+        
+        pval0 = lambda ad2a: np.nan*np.ones_like(ad2a)
+        pval1 = lambda ad2a: 1 - np.exp(-13.436 + 101.14 * ad2a - 223.73 * ad2a**2)
+        pval2 = lambda ad2a: 1 - np.exp(-8.318 + 42.796 * ad2a - 59.938 * ad2a**2)
+        pval3 = lambda ad2a: np.exp(0.9177 - 4.279 * ad2a - 1.38 * ad2a**2)
+        pval4 = lambda ad2a: np.exp(1.2937 - 5.709 * ad2a + 0.0186 * ad2a**2)
+        
+        pvalli = [pval0, pval1, pval2, pval3, pval4]
+        
+        idx = np.searchsorted(bounds, ad2a, side='right')
+        pval = np.nan*np.ones_like(ad2a)
+        for i in range(5):
+            mask = (idx == i)
+            pval[mask] = pvalli[i](ad2a[mask])
+
+    return ad2, pval
+
+
+if __name__ == '__main__':
+    x = np.array([-0.1184, -1.3403,  0.0063, -0.612 , -0.3869, -0.2313, -2.8485,
+           -0.2167,  0.4153,  1.8492, -0.3706,  0.9726, -0.1501, -0.0337,
+           -1.4423,  1.2489,  0.9182, -0.2331, -0.6182,  0.183 ])
+    r_res = np.array([0.58672353588821502, 0.1115380760041617])
+    ad2, pval = ad_normal(x)
+    print ad2, pval
+    print r_res - [ad2, pval]
+    
+    print anderson_statistic((x-x.mean())/x.std(), dist=stats.norm, fit=0)
+    print anderson_statistic(x, dist=stats.norm, fit=True)
---
+++ scikits/statsmodels/stats/tests/test_statstools.py
@@ -0,0 +1,116 @@
+
+
+import numpy as np
+from numpy.testing import assert_almost_equal
+from scikits.statsmodels.stats.stattools import (omni_normtest, jarque_bera,
+                            durbin_watson)
+from scikits.statsmodels.stats.adnorm import ad_normal                         
+
+
+#a random array, rounded to 4 decimals
+x = np.array([-0.1184, -1.3403,  0.0063, -0.612 , -0.3869, -0.2313, -2.8485,
+       -0.2167,  0.4153,  1.8492, -0.3706,  0.9726, -0.1501, -0.0337,
+       -1.4423,  1.2489,  0.9182, -0.2331, -0.6182,  0.183 ])
+
+
+def test_omni_normtest():
+    from scipy import stats
+    st_pv_R = np.array(
+              [[3.994138321207883, -1.129304302161460,  1.648881473704978],
+               [0.1357325110375005, 0.2587694866795507, 0.0991719192710234]])
+
+    nt = omni_normtest(x)
+    assert_almost_equal(nt, st_pv_R[:,0], 14)
+
+    st = stats.skewtest(x)
+    assert_almost_equal(st, st_pv_R[:,1], 15)
+
+    kt = stats.kurtosistest(x)
+    assert_almost_equal(kt, st_pv_R[:,2], 15)
+
+    st_pv_R = np.array(
+              [[34.523210399523926,  4.429509162503833,  3.860396220444025],
+               [3.186985686465249e-08, 9.444780064482572e-06, 1.132033129378485e-04]])
+
+    x2 = x**2
+    nt = omni_normtest(x2)
+    assert_almost_equal(nt, st_pv_R[:,0], 14)
+
+    st = stats.skewtest(x2)
+    assert_almost_equal(st, st_pv_R[:,1], 15)
+
+    kt = stats.kurtosistest(x2)
+    assert_almost_equal(kt, st_pv_R[:,2], 15)
+    
+def test_jarque_bera():
+    st_pv_R = np.array([1.9662677226861689, 0.3741367669648314])
+    jb = jarque_bera(x)[:2]
+    assert_almost_equal(jb, st_pv_R, 14)
+
+    st_pv_R = np.array([78.329987305556, 0.000000000000])
+    jb = jarque_bera(x**2)[:2]
+    assert_almost_equal(jb, st_pv_R, 13)
+
+    st_pv_R = np.array([5.7135750796706670, 0.0574530296971343])
+    jb = jarque_bera(np.log(x**2))[:2]
+    assert_almost_equal(jb, st_pv_R, 14)
+
+    st_pv_R = np.array([2.6489315748495761, 0.2659449923067881])
+    jb = jarque_bera(np.exp(-x**2))[:2]
+    assert_almost_equal(jb, st_pv_R, 15)
+
+def test_shapiro():
+    #testing scipy.stats
+    from scipy.stats import shapiro
+    
+    st_pv_R = np.array([0.939984787255526, 0.239621898000460])
+    sh = shapiro(x)
+    assert_almost_equal(sh, st_pv_R, 6)
+
+    #st is ok -7.15e-06, pval agrees at -3.05e-10
+    st_pv_R = np.array([5.799574255943298e-01, 1.838456834681376e-06 * 1e4])
+    sh = shapiro(x**2)*np.array([1,1e4])
+    assert_almost_equal(sh, st_pv_R, 6)
+
+    st_pv_R = np.array([0.91730442643165588, 0.08793704167882448 ])
+    sh = shapiro(np.log(x**2))
+    assert_almost_equal(sh, st_pv_R, 6)
+
+    #diff is [  9.38773155e-07,   5.48221246e-08]
+    st_pv_R = np.array([0.818361863493919373, 0.001644620895206969 ])
+    sh = shapiro(np.exp(-x**2))
+    assert_almost_equal(sh, st_pv_R, 6)
+    
+def test_adnorm():
+    #not run yet
+    st_pv = []
+    st_pv_R = np.array([0.5867235358882148, 0.1115380760041617])
+    ad = ad_normal(x)
+    assert_almost_equal(ad, st_pv_R, 14)
+    st_pv.append(st_pv_R)
+
+    st_pv_R = np.array([2.976266267594575e+00, 8.753003709960645e-08])
+    ad = ad_normal(x**2)
+    assert_almost_equal(ad, st_pv_R, 13)
+    st_pv.append(st_pv_R)
+
+    st_pv_R = np.array([0.4892557856308528, 0.1968040759316307])
+    ad = ad_normal(np.log(x**2))
+    assert_almost_equal(ad, st_pv_R, 14)
+    st_pv.append(st_pv_R)
+
+    st_pv_R = np.array([1.4599014654282669312, 0.0006380009232897535])
+    ad = ad_normal(np.exp(-x**2))
+    assert_almost_equal(ad, st_pv_R, 15)
+    st_pv.append(st_pv_R)
+
+    ad = ad_normal(np.column_stack((x,x**2, np.log(x**2),np.exp(-x**2))).T,
+                   axis=1)
+    assert_almost_equal(ad, np.column_stack(st_pv), 14)
+
+
+if __name__ == '__main__':
+    test_omni_normtest()
+    test_jarque_bera()
+    test_shapiro()
+    test_adnorm()
