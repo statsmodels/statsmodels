cb25ea605b5ab495da4466a1e92f72bcdf1dbf39
Josef Perktold <josef.pktd@gmail.com>
2011-10-01 08:52:58 -0400

    ENH: mctools: add docstrings and TODOs for StatTestMC

--- scikits/statsmodels/sandbox/tools/mctools.py
+++ scikits/statsmodels/sandbox/tools/mctools.py
@@ -8,6 +8,20 @@ as functions in the argument.
 Author: Josef Perktold (josef-pktd)
 License: BSD-3
 
+
+TODOs, Design
+-------------
+If we only care about univariate analysis, i.e. marginal if statistics returns
+more than one value, the we only need to store the sorted mcres not the
+original res. Do we want to extend to multivariate analysis?
+
+Use distribution function to keep track of MC results, ECDF, non-paramatric?
+Large parts are similar to a 2d array of independent multivariate random
+variables. Joint distribution is not used (yet).
+
+I guess this is currently only for one sided test statistics, e.g. for
+two-sided tests basend on t or normal distribution use the absolute value.
+
 '''
 
 
@@ -22,6 +36,67 @@ class StatTestMC(object):
     TODO
     print summary, for quantiles and for histogram
     draft in trying out script log
+
+    Parameters
+    ----------
+    dgp : callable
+        Function that generates the data to be used in Monte Carlo that should
+        return a new sample with each call
+    statistic : callable
+        Function that calculates the test statistic, which can return either
+        a single statistic or a 1d array_like (tuple, list, ndarray).
+        see also statindices in description of run
+
+    Attributes
+    ----------
+    many methods store intermediate results
+
+    self.mcres : ndarray (nrepl, nreturns) or (nrepl, len(statindices))
+        Monte Carlo results stored by run
+
+
+    Notes
+    -----
+
+    .. Warning::
+       This is (currently) designed for a single call to run. If run is
+       called a second time with different arguments, then some attributes might
+       not be updated, and, therefore, not correspond to the same run.
+
+    .. Warning::
+       Under Construction, don't expect stability in Api or implementation
+
+
+    Examples
+    --------
+
+    Define a function that defines our test statistic:
+
+    def lb(x):
+        s,p = acorr_ljungbox(x, lags=4)
+        return np.r_[s, p]
+
+    Note lb returns eight values.
+
+    Define a random sample generator, for example 500 independently, normal
+    distributed observations in a sample:
+
+
+    def normalnoisesim(nobs=500, loc=0.0):
+        return (loc+np.random.randn(nobs))
+
+    Create instance and run Monte Carlo. Using statindices=range(4) means that
+    only the first for values of the return of the statistic (lb) are stored
+    in the Monte Carlo results.
+
+    mc1 = StatTestMC(normalnoisesim, lb)
+    mc1.run(5000, statindices=range(4))
+
+    Most of the other methods take an idx which indicates for which columns
+    the results should be presented, e.g.
+
+    print mc1.cdf(crit, [1,2,3])[1]
+
 
     """
 
@@ -90,6 +165,11 @@ class StatTestMC(object):
         '''calculate histogram values
 
         does not do any plotting
+
+        I don't remember what I wanted here, looks similar to the new cdf
+        method, but this also does a binned pdf (self.histo)
+
+
         '''
         if self.mcres.ndim == 2:
             if  not idx is None:
@@ -114,7 +194,7 @@ class StatTestMC(object):
         self.cumhistoreversed = np.cumsum(histo[0][::-1])[::-1]*1./self.nrepl
         return histo, self.cumhisto, self.cumhistoreversed
 
-    #use cache decoratot
+    #use cache decorator instead
     def get_mc_sorted(self):
         if not hasattr(self, 'mcressort'):
             self.mcressort = np.sort(self.mcres, axis=0)
@@ -124,9 +204,30 @@ class StatTestMC(object):
     def quantiles(self, idx=None, frac=[0.01, 0.025, 0.05, 0.1, 0.975]):
         '''calculate quantiles of Monte Carlo results
 
+        similar to ppf
 
-        changes:
-        does all sort at once, but reports only one at a time
+        Parameters
+        ----------
+        idx : None or list of integers
+            List of indices into the Monte Carlo results (columns) that should
+            be used in the calculation
+        frac : array_like, float
+            Defines which quantiles should be calculated. For example a frac
+            of 0.1 finds the 10% quantile, x such that cdf(x)=0.1
+
+        Returns
+        -------
+        frac : ndarray
+            same values as input, TODO: I should drop this again ?
+        quantiles : ndarray, (len(frac), len(idx))
+            the quantiles with frac in rows and idx variables in columns
+
+        Notes
+        -----
+
+        rename to ppf ? make frac required
+        change sequence idx, frac
+
 
         '''
 
@@ -144,11 +245,25 @@ class StatTestMC(object):
         return frac, mc_sorted[(self.nrepl*frac).astype(int)]
 
     def cdf(self, x, idx=None):
-        '''calculate quantiles of Monte Carlo results
+        '''calculate cumulative probabilities of Monte Carlo results
+
+        Parameters
+        ----------
+        idx : None or list of integers
+            List of indices into the Monte Carlo results (columns) that should
+            be used in the calculation
+        frac : array_like, float
+            Defines which quantiles should be calculated. For example a frac
+            of 0.1 finds the 10% quantile, x such that cdf(x)=0.1
+
+        Returns
+        -------
+        x : ndarray
+            same as input, TODO: I should drop this again ?
+        probs : ndarray, (len(x), len(idx))
+            the quantiles with frac in rows and idx variables in columns
 
 
-        changes:
-        does all sort at once, but reports only one at a time
 
         '''
         idx = np.atleast_1d(idx).tolist()  #assure iterable, use list ?
@@ -179,7 +294,32 @@ class StatTestMC(object):
         probs = np.asarray(probs).T
         return x, probs
 
-    def plot_hist(self, idx, distpdf, bins=50, ax=None):
+    def plot_hist(self, idx, distpdf=None, bins=50, ax=None, kwds=None):
+        '''plot the histogram against a reference distribution
+
+        Parameters
+        ----------
+        idx : None or list of integers
+            List of indices into the Monte Carlo results (columns) that should
+            be used in the calculation
+        distpdf : callable
+            probability density function of reference distribution
+        bins : integer or array_like
+            used unchanged for matplotlibs hist call
+        ax : TODO: not implemented yet
+        kwds : None or tuple of dicts
+            extra keyword options to the calls to the matplotlib functions,
+            first dictionary is for his, second dictionary for plot of the
+            reference distribution
+
+        Returns
+        -------
+        None
+
+
+        '''
+        if kwds is None:
+            kwds = ({},{})
         if self.mcres.ndim == 2:
             if not idx is None:
                 mcres = self.mcres[:,idx]
@@ -197,12 +337,31 @@ class StatTestMC(object):
 #            fig = plt.figure()
 #            ax = fig.addaxis()
         fig = plt.figure()
-        plt.hist(mcres, bins=bins, normed=True)
-        plt.plot(lsp, distpdf(lsp), 'r')
+        plt.hist(mcres, bins=bins, normed=True, **kwds[0])
+        plt.plot(lsp, distpdf(lsp), 'r', **kwds[1])
+
 
     def summary_quantiles(self, idx, distppf, frac=[0.01, 0.025, 0.05, 0.1, 0.975],
                           varnames=None, title=None):
-        '''summary table for quantiles
+        '''summary table for quantiles (critical values)
+
+        Parameters
+        ----------
+        idx : None or list of integers
+            List of indices into the Monte Carlo results (columns) that should
+            be used in the calculation
+        distppf : callable
+            probability density function of reference distribution
+            TODO: use `crit` values instead or additional, see summary_cdf
+        frac : array_like, float
+            probabilities for which
+        varnames : None, or list of strings
+            optional list of variable names, same length as idx
+
+        Returns
+        -------
+        table : instance of SimpleTable
+            use `print table` to see results
 
         '''
         idx = np.atleast_1d(idx)  #assure iterable, use list ?
@@ -231,15 +390,27 @@ class StatTestMC(object):
                           headers=headers)
 
     def summary_cdf(self, idx, frac, crit, varnames=None, title=None):
-        '''summary table for quantiles
+        '''summary table for cumulative density function
 
-        currently just a partial copy from python session, for ljung-box example
 
-        add also
-        >>> lb_dist.ppf([0.01, 0.025, 0.05, 0.1, 0.975])
-        array([  0.29710948,   0.48441856,   0.71072302,   1.06362322,  11.14328678])
-        >>> stats.kstest(mc1.mcres[:,3], stats.chi2(4).cdf)
-        (0.052009265258216836, 0.0086211970272969118)
+        Parameters
+        ----------
+        idx : None or list of integers
+            List of indices into the Monte Carlo results (columns) that should
+            be used in the calculation
+        frac : array_like, float
+            probabilities for which
+        crit : array_like
+            values for which cdf is calculated
+        varnames : None, or list of strings
+            optional list of variable names, same length as idx
+
+        Returns
+        -------
+        table : instance of SimpleTable
+            use `print table` to see results
+
+
         '''
         idx = np.atleast_1d(idx)  #assure iterable, use list ?
 
