a14f80bfc86cfd7317f2bdc48d0ca5c544fe87b1
Skipper Seabold <jsseabold@gmail.com>
2011-10-03 13:29:34 -0400

    ENH: Make sure structured arrays pass through models

--- scikits/statsmodels/base/data.py
+++ scikits/statsmodels/base/data.py
@@ -8,6 +8,7 @@ from pandas import DataFrame, Series, TimeSeries
 from scikits.timeseries import time_series
 from scikits.statsmodels.tools.decorators import (resettable_cache,
                 cache_readonly, cache_writable)
+import scikits.statsmodels.tools.data as data_util
 
 class ModelData(object):
     """
@@ -78,9 +79,13 @@ class ModelData(object):
         return None
 
     def _get_yarr(self, endog):
+        if data_util.is_structured_ndarray(endog):
+            endog = data_util.struct_to_ndarray(endog)
         return np.asarray(endog).squeeze()
 
     def _get_xarr(self, exog):
+        if data_util.is_structured_ndarray(exog):
+            endog = data_util.struct_to_ndarray(exog)
         return np.asarray(exog)
 
     def _check_integrity(self):
