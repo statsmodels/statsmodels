a07cb7c4d5b7856c674936b9351ccb2a7d974a86
Josef Perktold <josef.pktd@gmail.com>
2011-10-31 20:14:58 -0400

    BUG: don't shadow module, remove lowess from __init__.py, fix paths

--- scikits/statsmodels/nonparametric/__init__.py
+++ scikits/statsmodels/nonparametric/__init__.py
@@ -1,5 +1,5 @@
 from kde import KDE
-from lowess import lowess
+#from lowess import lowess #don't do that, shadowing the module
 import bandwidths
 
 from scikits.statsmodels import NoseWrapper as Tester
