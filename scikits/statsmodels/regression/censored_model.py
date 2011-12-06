8828f44de80f682d090bd56e34c8760cf2741502
Skipper Seabold <jsseabold@gmail.com>
2011-12-06 18:43:06 -0500

    ENH: Add first pass at Tobit model. Estimation works.

---
+++ scikits/statsmodels/regression/censored_model.py
@@ -0,0 +1,175 @@
+import numpy as np
+import scikits.statsmodels.base.model as base
+import scikits.statsmodels.base.wrapper as wrap
+from scipy.stats import norm
+from scikits.statsmodels.sandbox.regression.numdiff import (approx_hess_cs,
+    approx_fprime_cs, approx_hess, approx_fprime)
+
+#TODO: clean-up estimation, delete webuse, write specific results class,
+#      figure out good separation for params and scale, can't concentrate
+#      likelihood, generalize margins from discrete for use here as well
+
+class Tobit(base.LikelihoodModel):
+    def __init__(self, endog, exog, left=True, right=True):
+        super(Tobit, self).__init__(endog, exog)
+        # set up censoring
+        self._init_censored(left, right)
+
+    def _init_censored(self, left, right):
+        if left is False and right is False:
+            raise ValueError("Must give a censoring level on the right or "
+                             "left.")
+        endog = self.endog
+        exog = self.exog
+        if left is True:
+            left = self.endog.min()
+        elif left is False:
+            left = -np.inf
+        if right is True:
+            right = self.endog.max()
+        elif right is False:
+            right = np.inf
+        self.left, self.right = left, right
+
+        # do this all here, so that we only have to do the indexing once
+        left_idx = endog <= left # do we need to attach these?
+        right_idx = endog >= right
+        self._left_endog = endog[left_idx]
+        self._right_endog = endog[right_idx]
+        self._left_exog = exog[left_idx]
+        self._right_exog = exog[right_idx]
+        center_idx = ~np.logical_or(left_idx, right_idx)
+        self._center_endog = endog[center_idx]
+        self._center_exog = exog[center_idx]
+
+    def score(self, params):
+        #NOTE: it might be easier to just use the numerical differentiation
+        #they're almost exactly the same here
+        sigma = np.exp(params[-1])
+        params = params[:-1]
+
+        left_exog = self._left_exog
+        if left_exog is not None:
+            scaled_left_exog = (self.left - np.dot(left_exog, params)) / sigma
+            part1 = norm.pdf(scaled_left_exog)/norm.cdf(scaled_left_exog)
+            dLdB_left = -np.sum(left_exog/sigma * part1[:,None], axis=0)
+            dLdSigma_left = -np.sum(part1 * scaled_left_exog, axis=0)
+        else:
+            dLdB_left = dLdSigma_left = 0
+
+        right_exog = self._right_exog
+        if right_exog is not None:
+            scaled_right_exog = (np.dot(right_exog,
+                                            params) - self.right) / sigma
+            part1 = norm.pdf(scaled_right_exog)/norm.cdf(scaled_right_exog)
+            dLdB_right = np.sum(right_exog/sigma * part1[:,None], axis=0)
+            dLdSigma_right = -np.sum(scaled_right_exog*part1, axis=0)
+        else:
+            dLdB_right = dLdSigma_right = 0
+
+        endog = self._center_endog
+        exog = self._center_exog
+        scaled_center_exog = (endog - np.dot(exog, params))/sigma
+        dLdB_center = np.sum(exog/sigma * scaled_center_exog[:,None], axis=0)
+        dLdSigma_center = np.sum(scaled_center_exog**2 - 1, axis=0)
+        dLdparams = dLdB_left + dLdB_right + dLdB_center
+        dLdsigma = dLdSigma_left + dLdSigma_right + dLdSigma_center
+        return np.r_[dLdparams, dLdsigma]
+        #loglike = self.loglike
+        #return approx_fprime(params, loglike)
+
+    def hessian(self, params):
+        loglike = self.loglike
+        return approx_hess(params, loglike)[0]
+
+    def loglike(self, params): #NOTE: these needs sigma as last parameter
+        sigma = np.exp(params[-1])
+        params = params[:-1]
+
+        left_exog = self._left_exog
+        if left_exog is not None:
+            left_exog = (self.left - np.dot(left_exog, params)) / sigma
+            left_like = np.sum(norm.logcdf(left_exog))
+        else:
+            left_like = 0
+        right_exog = self._right_exog
+        if right_exog is not None:
+            right_exog = (np.dot(right_exog, params) - self.right) / sigma
+            right_like = np.sum(norm.logcdf(right_exog))
+        else:
+            right_like = 0
+        center_endog = self._center_endog
+        center_exog = self._center_exog
+        center_like = np.sum(norm.logpdf((center_endog - np.dot(center_exog,
+                                   params)) / sigma) - np.log(sigma))
+        llf = left_like + right_like + center_like
+        return llf
+
+    def _get_start_params(self):
+        """
+        Get starting parameters from an OLS estimation
+        """
+        ols_res = sm.OLS(self.endog, self.exog).fit()
+        params = ols_res.params
+        sigma = np.log(ols_res.scale ** .5)
+        start_params = np.r_[params, sigma]
+        return np.r_[1,1,1] # seems to consistently work better like this
+        #return start_params
+
+    def fit(self, **kwargs):
+        start_params = self._get_start_params()
+        mlefit = super(Tobit, self).fit(start_params=start_params, **kwargs)
+        sigma = np.exp(mlefit.params[-1])
+        mlefit.scale = sigma
+        mlefit.params = mlefit.params[:-1]
+        return mlefit
+
+def webuse(data, baseurl='http://www.stata-press.com/data/r11/'):
+    """
+    Parameters
+    ----------
+    data : str
+    Name of dataset to fetch.
+
+    Examples
+    --------
+    >>> dta = webuse('auto')
+
+    Notes
+    -----
+    Make sure baseurl has trailing forward slash. Doesn't do any
+    error checking in response URLs.
+    """
+    # lazy imports
+    import pandas
+    from scikits.statsmodels.iolib import genfromdta
+    from urllib2 import urlopen
+    from urlparse import urljoin
+    from StringIO import StringIO
+
+    url = urljoin(baseurl, data+'.dta')
+    dta = urlopen(url)
+    dta = StringIO(dta.read()) # make it truly file-like
+    return genfromdta(dta)
+
+if __name__ == "__main__":
+    import pandas
+    import scikits.statsmodels.api as sm
+    dta = webuse('auto')
+    df = pandas.DataFrame.from_records(dta)
+    df.ix[df['rep78'] == -999, 'rep78'] = np.nan
+    df['weight'] = df['weight'].astype(float) #TODO: fix StataReader for ints
+    exog = sm.add_constant(df['weight'] / 1000, prepend=True)
+    endog = df['mpg']
+    mod = Tobit(endog, exog, left=17, right=False).fit(method='bfgs')
+    bse = np.sqrt(np.diag(np.linalg.inv(-approx_hess(np.r_[mod.params,
+                    np.log(mod.scale)], mod.model.loglike)[0])))
+    # now since we take the exponent in the likelihood of tobit, we have
+    # f(x) = exp(log(x)) == exp(x)*1/x, so
+    bse[-1] = bse[-1] * mod.scale
+
+    mod2 = Tobit(endog, exog, left=False, right=24).fit(method='bfgs')
+
+    mod3 = Tobit(endog, exog, left=17, right=24).fit(method='bfgs')
+
+    # compare to Stata results in the manual, parameters and bse look good
