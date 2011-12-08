ENH: Add Results class to Tobit. Fix Hessian precision. Use Fair data in example

scikits/statsmodels/regression/censored_model.py
@@ -2,6 +2,7 @@ import numpy as np
 import scikits.statsmodels.base.model as base
 import scikits.statsmodels.base.wrapper as wrap
 from scipy.stats import norm
+from scikits.statsmodels.tools.decorators import cache_readonly
 from scikits.statsmodels.sandbox.regression.numdiff import (approx_hess_cs,
     approx_fprime_cs, approx_hess, approx_fprime)
 
@@ -13,6 +14,7 @@ class Tobit(base.LikelihoodModel):
     def __init__(self, endog, exog, left=True, right=True):
         super(Tobit, self).__init__(endog, exog)
         # set up censoring
+        self._transparams = False # need to exp(sigma) to keep positive in fit
         self._init_censored(left, right)
 
     def _init_censored(self, left, right):
@@ -45,11 +47,13 @@ class Tobit(base.LikelihoodModel):
     def score(self, params):
         #NOTE: it might be easier to just use the numerical differentiation
         #they're almost exactly the same here
-        sigma = np.exp(params[-1])
+        sigma = params[-1]
+        if self._transparams:
+            sigma = np.exp(sigma)
         params = params[:-1]
 
         left_exog = self._left_exog
-        if left_exog is not None:
+        if left_exog.size > 0:
             scaled_left_exog = (self.left - np.dot(left_exog, params)) / sigma
             part1 = norm.pdf(scaled_left_exog)/norm.cdf(scaled_left_exog)
             dLdB_left = -np.sum(left_exog/sigma * part1[:,None], axis=0)
@@ -58,7 +62,6 @@ class Tobit(base.LikelihoodModel):
             dLdB_left = dLdSigma_left = 0
 
         right_exog = self._right_exog
-        if right_exog is not None:
             scaled_right_exog = (np.dot(right_exog,
                                             params) - self.right) / sigma
             part1 = norm.pdf(scaled_right_exog)/norm.cdf(scaled_right_exog)
@@ -80,22 +83,30 @@ class Tobit(base.LikelihoodModel):
 
     def hessian(self, params):
         loglike = self.loglike
-        return approx_hess(params, loglike)[0]
+        return approx_hess(params, loglike, epsilon=1e-5)[0]
 
     def loglike(self, params): #NOTE: these needs sigma as last parameter
-        sigma = np.exp(params[-1])
+        sigma = params[-1]
+        if self._transparams:
+            sigma = np.exp(sigma)
         params = params[:-1]
 
         left_exog = self._left_exog
-        if left_exog is not None:
+        if left_exog.size > 0:
             left_exog = (self.left - np.dot(left_exog, params)) / sigma
             left_like = np.sum(norm.logcdf(left_exog))
+            # can get overflow from the above if cdf is very small
+            # seems to mean bad starting values, but might need a check
+            #if np.isinf(left_like) and left_like < 0:
+            #    left_like = -1e4
         else:
             left_like = 0
         right_exog = self._right_exog
-        if right_exog is not None:
+        if right_exog.size > 0:
             right_exog = (np.dot(right_exog, params) - self.right) / sigma
             right_like = np.sum(norm.logcdf(right_exog))
+            #if np.isinf(right_like) and left_like < 0:
+            #    left_like = -1e4
         else:
             right_like = 0
         center_endog = self._center_endog
@@ -109,20 +120,44 @@ class Tobit(base.LikelihoodModel):
         """
         Get starting parameters from an OLS estimation
         """
-        ols_res = sm.OLS(self.endog, self.exog).fit()
+        ols_res = sm.OLS(self._center_endog, self._center_exog).fit()
         params = ols_res.params
         sigma = np.log(ols_res.scale ** .5)
         start_params = np.r_[params, sigma]
-        return np.r_[1,1,1] # seems to consistently work better like this
-        #return start_params
+        return start_params
 
-    def fit(self, **kwargs):
-        start_params = self._get_start_params()
+    def fit(self, start_params = None, **kwargs):
+        self._transparams = True
+        if start_params is None:
+            start_params = self._get_start_params()
+        else:
+            start_params[-1] = np.log(start_params[-1])
         mlefit = super(Tobit, self).fit(start_params=start_params, **kwargs)
+        self._transparams = False
         sigma = np.exp(mlefit.params[-1])
-        mlefit.scale = sigma
-        mlefit.params = mlefit.params[:-1]
-        return mlefit
+        mlefit.scale = mlefit.params[-1] = sigma
+        mlefit.model_params = mlefit.params[:-1]
+        return TobitResults(self, mlefit)
+
+class TobitResults(base.LikelihoodModelResults):
+    def __init__(self, model, mlefit):
+        self.model = model
+        #self._full_params = np.r_[mlefit.params, mlefit.scale]
+        self.__dict__.update(mlefit.__dict__)
+
+    def cov_params(self):
+        hess = self.model.hessian(self.params)
+        return np.linalg.inv(-hess)
+
+    #@cache_readonly
+    #def tvalues(self):
+        #    return self._full_params / self.bse
+
+    #@cache_readonly
+    #def bse(self):
+    #    covar = self.cov_params()
+    #    bse = np.sqrt(np.diag(covar))
+    #    return bse
 
 def webuse(data, baseurl='http://www.stata-press.com/data/r11/'):
     """
@@ -161,15 +196,30 @@ if __name__ == "__main__":
     df['weight'] = df['weight'].astype(float) #TODO: fix StataReader for ints
     exog = sm.add_constant(df['weight'] / 1000, prepend=True)
     endog = df['mpg']
-    mod = Tobit(endog, exog, left=17, right=False).fit(method='bfgs')
-    bse = np.sqrt(np.diag(np.linalg.inv(-approx_hess(np.r_[mod.params,
-                    np.log(mod.scale)], mod.model.loglike)[0])))
+    #mod = Tobit(endog, exog, left=17, right=False).fit(method='bfgs')
+
+    #bse = np.sqrt(np.diag(np.linalg.inv(-approx_hess(np.r_[mod.params,
+    #                np.log(mod.scale)], mod.model.loglike)[0])))
     # now since we take the exponent in the likelihood of tobit, we have
     # f(x) = exp(log(x)) == exp(x)*1/x, so
-    bse[-1] = bse[-1] * mod.scale
+    #bse[-1] = bse[-1] * mod.scale
 
-    mod2 = Tobit(endog, exog, left=False, right=24).fit(method='bfgs')
+    #mod2 = Tobit(endog, exog, left=False, right=24).fit(method='bfgs')
 
-    mod3 = Tobit(endog, exog, left=17, right=24).fit(method='bfgs')
+    #mod3 = Tobit(endog, exog, left=17, right=24).fit(method='bfgs')
 
     # compare to Stata results in the manual, parameters and bse look good
+
+    # replicate Fair's results
+    data = sm.datasets.fair.load()
+    endog = data.endog
+    exog = sm.add_constant(data.exog, prepend=True)
+    mod = Tobit(endog, exog, left=0, right=False).fit(method='bfgs')
+    #NOTE: newton is really sensitive to start_params
+    #fair_mod = Tobit(endog, exog, left=0, right=False).fit(method='newton')
+    #cov = np.linalg.inv(-approx_hess(mod.params, mod.model.loglike)[0])
+    #bse = np.sqrt(np.diag(np.linalg.inv(-approx_hess(mod.params,
+    #                    mod.model.loglike)[0])))
+    # now since we take the exponent in the likelihood of tobit, we have
+    # f(x) = exp(log(x)) == exp(x)*1/x, so
+    #bse[-1] = bse[-1] * mod.scale