612a5c9b08931b3f30ee80c7043458fa6d6f7efd
Ralf Gommers <ralf.gommers@googlemail.com>
2011-12-12 23:54:32 +0100

    DOC: add example for fboxplot.

--- scikits/statsmodels/graphics/fboxplot.py
+++ scikits/statsmodels/graphics/fboxplot.py
@@ -151,6 +151,20 @@ def fboxplot(data, xdata=None, labels=None, wfactor=1.5, ax=None):
 
     Examples
     --------
+    >>> data = sm.datasets.elnino.load()
+
+    >>> fig = plt.figure()
+    >>> ax = fig.add_subplot(111)
+    >>> res = fboxplot(data.raw_data[:, 1:], wfactor=2.58,
+                   labels=data.raw_data[:, 0].astype(int), ax=ax)
+
+    >>> ax.set_xlabel("Month of the year")
+    >>> ax.set_ylabel("Sea surface temperature (C)")
+    >>> ax.set_xticks(np.arange(13, step=3) - 1)
+    >>> ax.set_xticklabels(["", "Mar", "Jun", "Sep", "Dec"])
+    >>> ax.set_xlim([-0.2, 11.2])
+
+    >>> plt.show()
 
     """
     # TODO:
