13fcdc22f613e53bbe525335504426273af526b2
Skipper Seabold <jsseabold@gmail.com>
2011-12-13 19:12:29 -0500

    ENH: Finish Olsen reparam for left censored Tobit. Added analytic Hessian. Erratic solver behavior.

--- scikits/statsmodels/regression/censored_model.py
+++ scikits/statsmodels/regression/censored_model.py
@@ -19,12 +19,43 @@ from scipy.stats import norm
 #    Greene 19.3.3, 7th edition.
 #    """
 
+def _olsen_reparam(params):
+    """
+    Go from true parameters to gamma and theta of Olsen
+
+    gamma = beta/sigma
+    theta = 1/sigma
+    """
+    beta, sigma  = params[:-1], params[-1]
+    theta = 1./sigma
+    gamma = beta/sigma
+    return gamma, theta
+
+def _reparam_olsen(params):
+    """
+    Go from gamma and theta of Olsen to beta and sigma
+
+    sigma = 1/theta
+    beta = gamma/theta
+    """
+    gamma, theta  = params[:-1], params[-1]
+    sigma = 1./theta
+    beta = gamma/theta
+    return beta, sigma
+
+def dlogcdf(X):
+    """
+    Returns a _2d_ array norm.pdf(X)/norm.cdf(X)
+    """
+    return (norm.pdf(X)/norm.cdf(X))
+
 class Tobit(base.LikelihoodModel):
     def __init__(self, endog, exog, left=True, right=True):
         super(Tobit, self).__init__(endog, exog)
         # set up censoring
         self._transparams = False # need to exp(sigma) to keep positive in fit
         self._init_censored(left, right)
+        self._set_loglike()
 
     def _init_censored(self, left, right):
         if left is False and right is False:
@@ -55,7 +86,25 @@ class Tobit(base.LikelihoodModel):
         self._center_endog = endog[center_idx]
         self._center_exog = exog[center_idx]
 
-    def score(self, params):
+    def _set_loglike(self):
+        """
+        Use this so we don't have a lot of if checks in each loglike call
+        """
+        left, right = self.left, self.right
+        if np.isfinite(left) and np.isinf(right): # left-censored
+            self.loglike = self._loglike_olsen_left
+            self.score = self._score_left
+            #self.hessian = self._hessian_left
+        elif np.isinf(left) and np.isfinite(right): # right-censored
+            self.loglike = self._loglike_right
+            #self.score = self._score_right
+            #self.hessian = self._hessian_right
+        else: # left and right censored
+            self.loglike = self._loglike_both
+            #self.score = self._score_both
+            #self.hessian = self._hessian_both
+
+    #def score(self, params):
         #NOTE: it might be easier to just use the numerical differentiation
         #they're almost exactly the same here
         # reparam for olsen
@@ -93,12 +142,35 @@ class Tobit(base.LikelihoodModel):
         #dLdparams = dLdB_left + dLdB_right + dLdB_center
         #dLdsigma = dLdSigma_left + dLdSigma_right + dLdSigma_center
         #return np.r_[dLdparams, dLdsigma]
-        loglike = self.loglike
-        return approx_fprime(params, loglike)
+        #loglike = self.loglike
+        #return approx_fprime(params, loglike)
+
+    def _score_left(self, params):
+        if self._transparams: # given true parameters, re-params to Olsen
+            gamma, theta = _olsen_reparam(params)
+        else: # given olsen parameterization
+            gamma, theta = params[:-1], params[-1]
+
+        # score for gamma
+        left, left_exog = self.left, self._left_exog
+        dlncdf = dlogcdf(left*theta - np.dot(left_exog, gamma))
+        gamma_left = np.dot(left_exog.T, dlncdf)
+
+        center_endog = self._center_endog * theta
+        center_exog = self._center_exog
+        center_resid = (center_endog - np.dot(center_exog, gamma))
+        gamma_center = np.dot(center_exog.T, center_resid)
+        dLdGamma = gamma_center - gamma_left
+
+        # score for theta
+        theta_left = np.sum(left * dlncdf)
+        theta_center = np.sum(1/theta - center_resid * self._center_endog)
+        dLdTheta = theta_center + theta_left
+        return np.r_[dLdGamma, dLdTheta]
 
     def hessian(self, params):
         loglike = self.loglike
-        return approx_hess(params, loglike, epsilon=1e-4)[0]
+        return approx_hess(params, loglike, epsilon=1e-8)[0]
 
     def _loglike_left(self, params, sigma):
         left_exog = (self.left - np.dot(self._left_exog, params)) / sigma
@@ -116,21 +188,26 @@ class Tobit(base.LikelihoodModel):
         #    left_like = -1e4
         return right_like
 
-    def loglike_olsen_left(self, params):
-        theta = params[-1]
-        gamma = params[:-1]
-        left = self.left
+    def _loglike_olsen_left(self, params):
+        if self._transparams:
+            gamma, theta = _olsen_reparam(params)
+        else:
+            gamma, theta = params[:-1], params[-1]
+
+        left, left_exog = self.left, self._left_exog
+        llf_left = np.sum(norm.logcdf(left*theta - np.dot(left_exog, gamma)))
+
         center_endog = self._center_endog * theta
         center_exog = self._center_exog
         llf_center = -.5 * np.sum((np.log(2*np.pi) - np.log(theta**2) + \
                      (center_endog - np.dot(center_exog, gamma))**2))
-        left_exog = self._left_exog
-        llf_left = np.sum(norm.logcdf(left*theta - np.dot(left_exog, gamma)))
         return llf_center + llf_left
 
-    def loglike_olsen_right(self, params):
-        theta = params[-1]
-        gamma = params[:-1]
+    def _loglike_olsen_right(self, params):
+        if self._transparams:
+            gamma, theta = _olsen_reparam(params)
+        else:
+            gamma, theta = params[:-1], params[-1]
         right = self.right
         center_endog = self._center_endog * theta
         center_exog = self._center_exog
@@ -165,24 +242,37 @@ class Tobit(base.LikelihoodModel):
         """
         Get starting parameters from an OLS estimation
         """
+        # use ols as starting values, should scale them by observations
         ols_res = sm.OLS(self._center_endog, self._center_exog).fit()
         params = ols_res.params
         sigma = np.log(ols_res.scale ** .5)
         sigma = ols_res.scale ** .5
         theta = 1/ols_res.scale ** .5
+        nobs = len(self._center_endog)
+        params = params/nobs
         start_params = np.r_[params/sigma, theta]
-        #start_params = np.r_[30,1, theta]
+
+        #use null model as starting values
+        #endog = self.endog
+        #left = self.left
+        #right = self.right
+        #res = Tobit(endog, np.ones_like(endog), left=left, right=right).fit(method='bfgs', start_params = [1,1])
+
+        #params = np.r_[ols_res.params, ols_res.scale ** .5]
+        #NOTE: is this a local minimum??
+        #params = np.r_[[0] * 8,res.params]
+        #gamma, theta = _olsen_reparam(params)
+        #start_params = np.r_[gamma, theta]
         return start_params
 
     def fit(self, start_params = None, **kwargs):
-        self.loglike = self.loglike_olsen_right
-        self._transparams = True
+        self._transparams = False # working with reparameterized
         if start_params is None:
             start_params = self._get_start_params()
-        else:
-            start_params[-1] = np.log(start_params[-1])
+        #else:
+        #    start_params[-1] = np.log(start_params[-1])
         mlefit = super(Tobit, self).fit(start_params=start_params, **kwargs)
-        self._transparams = False
+        self._transparams = True # will now get true params
         return TobitResults(self, mlefit)
 
 class TobitResults(base.LikelihoodModelResults):
@@ -191,9 +281,9 @@ class TobitResults(base.LikelihoodModelResults):
         # re-parameterize from exponential
         #sigma = np.exp(mlefit.params[-1])
         # re-parameterize from Olsen
-        sigma = 1/mlefit.params[-1]
-        self.model_params = mlefit.params[:-1]/mlefit.params[-1]
-        mlefit.params[-1] = sigma
+        beta, sigma = _reparam_olsen(mlefit.params)
+        self.model_params = beta
+        mlefit.params = np.r_[beta, sigma]
         self.__dict__.update(mlefit.__dict__)
         self.scale = sigma # overwrite from mlefit
         self._model_stats() # attach model statistics
@@ -270,13 +360,13 @@ def webuse(data, baseurl='http://www.stata-press.com/data/r11/'):
 if __name__ == "__main__":
     import pandas
     import scikits.statsmodels.api as sm
-    dta = webuse('auto')
-    df = pandas.DataFrame.from_records(dta)
-    df['rep78'] = df['rep78'].astype(float)
-    df.ix[df['rep78'] == -999, 'rep78'] = np.nan
-    df['weight'] = df['weight'].astype(float) #TODO: fix StataReader for ints
-    exog = sm.add_constant(df['weight'] / 1000, prepend=True)
-    endog = df['mpg']
+    #dta = webuse('auto')
+    #df = pandas.DataFrame.from_records(dta)
+    #df['rep78'] = df['rep78'].astype(float)
+    #df.ix[df['rep78'] == -999, 'rep78'] = np.nan
+    #df['weight'] = df['weight'].astype(float) #TODO: fix StataReader for ints
+    #exog = sm.add_constant(df['weight'] / 1000, prepend=True)
+    #endog = df['mpg']
     #auto_mod2 = Tobit(endog, exog, left=17, right=False).fit(method='bfgs', maxiter=1000)
 
 
@@ -288,11 +378,11 @@ if __name__ == "__main__":
     #bse[-1] = bse[-1] * mod.scale
 
     #check olsen reparams for non-zero censoring
-    auto_mod2 = Tobit(endog, exog, left=False, right=24).fit(method='bfgs')
-    self = auto_mod2.model
-    gamma = auto_mod2.model_params / auto_mod2.scale
-    theta = 1 / auto_mod2.scale
-    params = np.r_[gamma, theta]
+    #auto_mod2 = Tobit(endog, exog, left=False, right=24).fit(method='bfgs')
+    #self = auto_mod2.model
+    #gamma = auto_mod2.model_params / auto_mod2.scale
+    #theta = 1 / auto_mod2.scale
+    #params = np.r_[gamma, theta]
 
     #mod3 = Tobit(endog, exog, left=17, right=24).fit(method='bfgs')
 
@@ -303,6 +393,9 @@ if __name__ == "__main__":
     endog = data.endog
     exog = sm.add_constant(data.exog, prepend=False)
     #mod = Tobit(endog, exog, left=0, right=False).fit(method='bfgs')
+
+    # well this is interesting...
+
     #NOTE: newton's method doesn't work well, I think this is due to the
     #Hessian approximation
     #fair_mod = Tobit(endog, exog, left=0, right=False).fit(method='newton')
@@ -312,3 +405,24 @@ if __name__ == "__main__":
     # now since we take the exponent in the likelihood of tobit, we have
     # f(x) = exp(log(x)) == exp(x)*1/x, so
     #bse[-1] = bse[-1] * mod.scale
+
+    # params from stata
+    true_params = np.array([-1.530712955415909, -.1051385059749894, .1282901151942823, -.0277671231250877, -.9434969317125149, -.085975029751076, .3128387422989903, .0142119678926689, 7.836529294458556, 4.498874144181835])
+
+    # ok
+    print 'Newton'
+    mod1 = Tobit(endog, exog, left=0, right=False).fit(method='newton')
+
+    # nooooooooo
+    print 'NM'
+    mod2 = Tobit(endog, exog, left=0, right=False).fit(method='nm',
+                    maxiter=1500)
+    print 'BFGS'
+    mod3 = Tobit(endog, exog, left=0, right=False).fit(method='bfgs')
+    print 'Powell'
+    mod4 = Tobit(endog, exog, left=0, right=False).fit(method='powell')
+    print 'CG'
+    mod5 = Tobit(endog, exog, left=0, right=False).fit(method='cg',
+                                maxiter=1000)
+    print 'NCG'
+    mod6 = Tobit(endog, exog, left=0, right=False).fit(method='ncg')
