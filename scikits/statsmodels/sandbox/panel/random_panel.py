53027cd1ee834d9f4c99f8d02fa02c79a456cdc8
Josef Perktold <josef.pktd@gmail.com>
2011-12-18 02:03:28 -0500

    ENH: panel first example working      add GroupSorted with groupiter

--- scikits/statsmodels/sandbox/panel/random_panel.py
+++ scikits/statsmodels/sandbox/panel/random_panel.py
@@ -31,6 +31,7 @@ class PanelSample(object):
         self.n_groups = n_groups
         self.k_vars = k_vars
         self.corr_structure = corr_structure
+        self.groups = np.repeat(np.arange(n_groups), nobs_i)
 
         self.group_indices = np.arange(n_groups+1) * nobs_i #check +1
 
@@ -62,15 +63,23 @@ class PanelSample(object):
 
         random = self.random_state
 
-        noise = np.empty(self.nobs, np.float64)
-        noise.fill(np.nan)
-        for ii in range(self.n_groups):
-            #print ii,
-            idx, idxupp = self.group_indices[ii:ii+2]
-            #print idx, idxupp
-            mean_i = self.group_means[ii]
-            noise[idx:idxupp] = random.multivariate_normal(
-                                    mean_i * np.ones(self.nobs_i), self.cov)
+        use_balanced = True
+        if use_balanced: #much faster for balanced case
+            noise = np.random.multivariate_normal(np.zeros(nobs_i),
+                                                  self.cov,
+                                                  size=n_groups).ravel()
+            #need to add self.group_means
+            noise += np.repeat(self.group_means, nobs_i)
+        else:
+            noise = np.empty(self.nobs, np.float64)
+            noise.fill(np.nan)
+            for ii in range(self.n_groups):
+                #print ii,
+                idx, idxupp = self.group_indices[ii:ii+2]
+                #print idx, idxupp
+                mean_i = self.group_means[ii]
+                noise[idx:idxupp] = random.multivariate_normal(
+                                        mean_i * np.ones(self.nobs_i), self.cov)
 
         endog = self.y_true + noise
         return endog
@@ -89,3 +98,27 @@ if __name__ == '__main__':
     noise = y - dgp.y_true
     print np.corrcoef(y.reshape(-1,n_groups, order='F'))
     print np.corrcoef(noise.reshape(-1,n_groups, order='F'))
+
+    from panel_short import ShortPanelGLS
+    mod = ShortPanelGLS(y, dgp.exog, dgp.groups)
+    res = mod.fit()
+    print res.params
+    print res.bse
+    #Now what?
+    #res.resid is of transformed mode
+    #np.corrcoef(res.resid.reshape(-1,n_groups, order='F'))
+    y_pred = np.dot(mod.exog, res.params)
+    resid = y - y_pred
+    print np.corrcoef(resid.reshape(-1,n_groups, order='F'))
+    print resid.std()
+    err = y_pred - dgp.y_true
+    print err.std()
+    #OLS standard errors are too small
+    mod.res_pooled.params
+    mod.res_pooled.bse
+    #heteroscedasticity robust doesn't help
+    mod.res_pooled.HC1_se
+    #compare with cluster robust se
+    import scikits.statsmodels.sandbox.panel.sandwich_covariance as sw
+    print sw.cov_cluster(mod.res_pooled, dgp.groups.astype(int))[1]
+    #not bad, pretty close to panel estimator
