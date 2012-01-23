529454ee412e2c3391125642f5e7fc0e9de48167
Skipper Seabold <jsseabold@gmail.com>
2012-01-22 19:51:52 -0500

    ENH: Add a frequency inference function

--- scikits/statsmodels/tsa/base/datetools.py
+++ scikits/statsmodels/tsa/base/datetools.py
@@ -213,3 +213,29 @@ def dates_from_range(start, end=None, length=None):
     """
     dates = date_range_str(start, end, length)
     return dates_from_str(dates)
+
+def _add_datetimes(dates):
+    return reduce(lambda x, y: y+x, dates)
+
+def _infer_freq(dates):
+    timedelta = datetime.timedelta
+    nobs = min(len(dates), 6)
+    if nobs == 1:
+        raise ValueError("Cannot infer frequency from one date")
+    diff = np.diff(dates[:nobs])
+    delta = _add_datetimes(diff)
+    nobs -= 1 # after diff
+    if delta == timedelta(nobs): #greedily assume 'D'
+        return 'D'
+    elif delta == timedelta(nobs + 2):
+        return 'B'
+    elif delta == timedelta(7*nobs):
+        return 'W'
+    elif delta >= timedelta(28*nobs) and delta <= timedelta(31*nobs):
+        return 'M'
+    elif delta >= timedelta(90*nobs) and delta <= timedelta(92*nobs):
+        return 'Q'
+    elif delta >= timedelta(365 * nobs) and delta <= timedelta(366 * nobs):
+        return 'A'
+    else:
+        return
