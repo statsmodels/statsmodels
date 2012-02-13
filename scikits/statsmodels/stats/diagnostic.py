dddd19cf95a38e4afba8b4d79f1f05416378a33e
Josef Perktold <josef.pktd@gmail.com>
2012-02-13 12:25:43 -0500

    REF: test_diagnostic import module from stats not from sandbox (diagnostic code is still in sandbox)

--- scikits/statsmodels/stats/diagnostic.py
+++ scikits/statsmodels/stats/diagnostic.py
@@ -2,4 +2,4 @@
 from scikits.statsmodels.sandbox.stats.diagnostic import (
     acorr_ljungbox, breaks_cusumolsresid, breaks_hansen, CompareCox, CompareJ,
     compare_cox, compare_j, het_breushpagan, HetGoldfeldQuandt, het_goldfeldquandt,
-    het_white, recursive_olsresiduals)
+    het_white, recursive_olsresiduals, acorr_breush_godfrey)
