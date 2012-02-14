2a33424b0a85f87bd7e8093a100a7a2ef5192f18
Josef Perktold <josef.pktd@gmail.com>
2012-02-14 12:21:19 -0500

    ENH: add ARCH test with tests agains R:FinTS

--- scikits/statsmodels/stats/diagnostic.py
+++ scikits/statsmodels/stats/diagnostic.py
@@ -2,6 +2,6 @@
 from scikits.statsmodels.sandbox.stats.diagnostic import (
     acorr_ljungbox, breaks_cusumolsresid, breaks_hansen, CompareCox, CompareJ,
     compare_cox, compare_j, het_breushpagan, HetGoldfeldQuandt,
-    het_goldfeldquandt,
+    het_goldfeldquandt, het_arch,
     het_white, recursive_olsresiduals, acorr_breush_godfrey,
     linear_harvey_collier, linear_rainbow)
