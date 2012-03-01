7a4cfaca8f37da95000430eaafd74a0f6f8a706b
Skipper Seabold <jsseabold@gmail.com>
2012-02-29 22:36:03 -0500

    BUG: Handle pandas data with only one regressor

--- scikits/statsmodels/base/data.py
+++ scikits/statsmodels/base/data.py
@@ -150,7 +150,7 @@ class PandasData(ModelData):
             return self._orig_endog.index
 
     def attach_columns(self, result):
-        if result.squeeze().ndim == 1:
+        if result.squeeze().ndim <= 1:
             return Series(result, index=self.xnames)
         else: # for e.g., confidence intervals
             return DataFrame(result, index=self.xnames)
