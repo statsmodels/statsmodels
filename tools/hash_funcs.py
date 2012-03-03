3816063967e03bc7b0cd3b7c95e74291ced04138
Skipper Seabold <jsseabold@gmail.com>
2012-03-03 12:37:58 -0500

    ENH: Fix directory in hash funcs.

--- tools/hash_funcs.py
+++ tools/hash_funcs.py
@@ -2,6 +2,10 @@
 A collection of utilities to see if new ReST files need to be automatically
 generated from certain files in the project (examples, datasets).
 """
+import os
+import pickle
+
+file_path = os.path.dirname(__file__)
 
 def get_hash(f):
     """
@@ -17,12 +21,12 @@ def update_hash_dict(filehash, filename):
     Opens the pickled hash dictionary, adds an entry, and dumps it back.
     """
     try:
-        with open(root+'/hash_dict.pickle','r') as f:
+        with open(file_path+'/hash_dict.pickle','r') as f:
             hash_dict = pickle.load(f)
-    except:
+    except IOError as err:
         hash_dict = {}
     hash_dict.update({filename : filehash})
-    with open(os.path.join(file_path,'hash_dict.pickle','w')) as f:
+    with open(os.path.join(file_path,'hash_dict.pickle'),'w') as f:
         pickle.dump(hash_dict, f)
 
 def check_hash(rawfile, filename):
@@ -30,9 +34,9 @@ def check_hash(rawfile, filename):
     Returns True if hash does not match the previous one.
     """
     try:
-        with open(root+'/hash_dict.pickle','r') as f:
+        with open(file_path+'/hash_dict.pickle','r') as f:
             hash_dict = pickle.load(f)
-    except:
+    except IOError as err:
         hash_dict = {}
     try:
         checkhash = hash_dict[filename]
