775aad1a9904feffd01bb5ca52080356f358ce51
Skipper Seabold <jsseabold@gmail.com>
2012-03-06 10:33:05 -0500

    REF: Move tsa setup responsibility to tsa

---
+++ statsmodels/tsa/setup.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+import os.path
+
+base_path = os.path.abspath(os.path.dirname(__file__))
+
+def configuration(parent_package='', top_path=None):
+    from numpy.distutils.misc_util import (Configuration,
+                            get_numpy_include_dirs)
+    config = Configuration('tsa', parent_package, top_path)
+
+    #config.add_subpackage('kalmanf')
+
+    config.add_data_files('vector_ar/data/*.dat')
+
+    return config
+
+if __name__ == '__main__':
+    from numpy.distutils.core import setup
+    setup(**configuration(top_path='').todict())
