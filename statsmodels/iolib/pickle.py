07213f5d53f18dd483a0bc87cae85b3f7f59a0b3
Josef Perktold <josef.pktd@gmail.com>
2012-03-18 09:46:12 -0400

    TST: move pickle test

--- statsmodels/iolib/pickle.py
+++ statsmodels/iolib/pickle.py
@@ -41,32 +41,7 @@ def load_pickle(fname):
     fin = _get_file_obj(fname, 'rb')
     return pickle.load(fin)
 
-def test_pickle():
-    import tempfile
-    from numpy.testing import assert_equal
-    tmpdir = tempfile.mkdtemp(prefix='pickle')
-    a = range(10)
-    save_pickle(a, tmpdir+'/res.pkl')
-    b = load_pickle(tmpdir+'/res.pkl')
-    assert_equal(a, b)
 
-    #cleanup, tested on Windows
-    try:
-        import os
-        os.remove(tmpdir+'/res.pkl')
-        os.rmdir(tmpdir)
-    except (OSError, IOError):
-        pass
-    assert not os.path.exists(tmpdir)
-
-    #test with file handle
-    import StringIO
-    fh = StringIO.StringIO()
-    save_pickle(a, fh)
-    fh.seek(0,0)
-    c = load_pickle(fh)
-    fh.close()
-    assert_equal(a,b)
 
 
 if __name__ == '__main__':
