1c1cebbdc09c418721989fce78423c32a617cacb
Josef Perktold <josef.pktd@gmail.com>
2012-03-23 12:14:37 -0400

    BUG: py25 iter_compat  (I thought I had changed this already, but where?)

--- statsmodels/compatnp/iter_compat.py
+++ statsmodels/compatnp/iter_compat.py
@@ -16,43 +16,55 @@ try:
     pass
 except AttributeError:
     #python 3.2
-    zip_longest = itertools.zip_longest
-    pass
-except AttributeError:
-    #python 2.5
-    def zip_longest(*args):
-        '''python 2.5 version for transposing a list of lists
-
-        adds None for lists of shorter length, may not have the same
-        behavior as python 2.6 izip_longest or python 3.2 zip_longest for
-        other cases
-
-        Parameters
-        ----------
-        args : sequence of iterables
-            iterables that will be combined in transposed way
-
-        Returns
-        -------
-        it : iterator
-            iterator that generates tuples
-
-        Examples
-        --------
-        >>> lili = [['a0', 'b0', 'c0', 'd0'], ['a1', 'b1', 'c1'],
-                    ['a2', 'b2', 'c2', 'd2'], ['a3', 'b3', 'c3', 'd3'],
-                    ['a4', 'b4']]
-        >>> list(izip_longest(*lili))
-        [('a0', 'a1', 'a2', 'a3', 'a4'), ('b0', 'b1', 'b2', 'b3', 'b4'),
-         ('c0', 'c1', 'c2', 'c3', None), ('d0', None, 'd2', 'd3', None)]
-
-        '''
-        return itertools.imap(None, *args)
+    try:
+        zip_longest = itertools.zip_longest
         pass
+    except AttributeError:
+        #python 2.5
+        def zip_longest(*args, **kwds):
+            '''python 2.5 version for transposing a list of lists
+
+            adds None for lists of shorter length, may not have the same
+            behavior as python 2.6 izip_longest or python 3.2 zip_longest for
+            other cases
+
+            Parameters
+            ----------
+            args : sequence of iterables
+                iterables that will be combined in transposed way
+
+            Returns
+            -------
+            it : iterator
+                iterator that generates tuples
+
+            Examples
+            --------
+            >>> lili = [['a0', 'b0', 'c0', 'd0'], ['a1', 'b1', 'c1'],
+                        ['a2', 'b2', 'c2', 'd2'], ['a3', 'b3', 'c3', 'd3'],
+                        ['a4', 'b4']]
+            >>> list(izip_longest(*lili))
+            [('a0', 'a1', 'a2', 'a3', 'a4'), ('b0', 'b1', 'b2', 'b3', 'b4'),
+             ('c0', 'c1', 'c2', 'c3', None), ('d0', None, 'd2', 'd3', None)]
+
+            '''
+
+            # izip_longest('ABCD', 'xy', fillvalue='-') --> Ax By C- D-
+            fillvalue = kwds.get('fillvalue')
+            def sentinel(counter = ([fillvalue]*(len(args)-1)).pop):
+                yield counter()         # yields the fillvalue, or raises IndexError
+            fillers = itertools.repeat(fillvalue)
+            iters = [itertools.chain(it, sentinel(), fillers) for it in args]
+            try:
+                for tup in itertools.izip(*iters):
+                    yield tup
+            except IndexError:
+                pass
+
 
 try:
     from itertools import combinations
-except AttributeError:
+except ImportError:
     #from python 2.6 documentation
     def combinations(iterable, r):
         # combinations('ABCD', 2) --> AB AC AD BC BD CD
