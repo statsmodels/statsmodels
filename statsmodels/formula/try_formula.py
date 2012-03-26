91f5db6ddf6e26cec27917109689c200498dc85f
Skipper Seabold <jsseabold@gmail.com>
2012-03-26 13:07:36 -0400

    ENH: Add example for injecting user transform

--- statsmodels/formula/try_formula.py
+++ statsmodels/formula/try_formula.py
@@ -17,3 +17,19 @@ dta["SUCCESS"] = endog
 endog = dta.pop("SUCCESS")
 exog = dta
 mod = sm.GLM(endog, exog, formula=formula, family=sm.families.Binomial()).fit()
+
+# try passing a formula object, using user-injected code
+
+def double_it(x):
+    return 2*x
+
+# What is the correct entry point for this? Should users be able to inject
+# code into default_env or similar? I don't see a way to do this yet using
+# the approach I have been using, it should be an argument to Desc
+from charlton.builtins import builtins
+builtins['double_it'] = double_it
+
+formula = 'SUCCESS ~ double_it(LOWINC) + PERASIAN + PERBLACK + PERHISP + '
+formula += 'PCTCHRT '
+formula += '+ PCTYRRND + PERMINTE*AVYRSEXP*AVSALK + PERSPENK*PTRATIO*PCTAF'
+mod2 = sm.GLM(endog, exog, formula=formula, family=sm.families.Binomial()).fit()
