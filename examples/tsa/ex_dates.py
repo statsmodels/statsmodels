ab32623517d3eb9b173b58f2786357dafaf82282
Skipper Seabold <jsseabold@gmail.com>
2012-04-03 09:23:17 -0400

    EX: Remove scikits.ts and larry from example

--- examples/tsa/ex_dates.py
+++ examples/tsa/ex_dates.py
@@ -11,24 +11,15 @@ import pandas
 data = sm.datasets.sunspots.load()
 
 # Right now an annual date series must be datetimes at the end of the year.
-# We can use scikits.timeseries and datetime to create this array.
 
-import datetime
-import scikits.timeseries as ts
-dates = ts.date_array(start_date=1700, length=len(data.endog), freq='A')
-
-# To make an array of datetime types, we need an integer array of ordinals
-
-#.. from datetime import datetime
-#.. dt_dates = dates.toordinal().astype(int)
-#.. dt_dates = np.asarray([datetime.fromordinal(i) for i in dt_dates])
-dt_dates = dates.tolist()
+from datetime import datetime
+dates = sm.tsa.datetools.date_from_range('1700', length=len(data.endog))
 
 # Using Pandas
 # ------------
 
 # Make a pandas TimeSeries or DataFrame
-endog = pandas.Series(data.endog, index=dt_dates)
+endog = pandas.TimeSeries(data.endog, index=dt_dates)
 
 # and instantiate the model
 ar_model = sm.tsa.AR(endog, freq='A')
@@ -54,18 +45,3 @@ print ar_res._data.predict_dates
 # This attribute only exists if predict has been called. It holds the dates
 # associated with the last call to predict.
 #..TODO: should this be attached to the results instance?
-
-# Using scikits.timeseries
-# ------------------------
-
-ts_data = ts.time_series(data.endog, dates=dates)
-ts_ar_model = sm.tsa.AR(ts_data, freq='A')
-ts_ar_res = ts_ar_model.fit(maxlag=9)
-
-# Using Larry
-# -----------
-
-import la
-larr = la.larry(data.endog, [dt_dates])
-la_ar_model = sm.tsa.AR(larr, freq='A')
-la_ar_res = la_ar_model.fit(maxlag=9)
