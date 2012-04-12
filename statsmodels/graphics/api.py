261d1a2cc99046efc0cb9d1931735e1674296b13
Skipper Seabold <jsseabold@gmail.com>
2012-04-12 09:40:44 -0400

    ENH: Add plots to api

--- statsmodels/graphics/api.py
+++ statsmodels/graphics/api.py
@@ -1,3 +1,6 @@
 from .functional import fboxplot, rainbowplot
 from .gofplots import qqplot
 from .boxplots import violinplot, beanplot
+from .regressionplots import abline_plot
+from .factorplots import interaction_plot
+from .plottools import rainbow
