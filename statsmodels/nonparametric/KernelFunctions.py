98d32e6e432d332f72ba8765b8637376175ca57f
George Panterov <econgpanterov@gmail.com>
2012-04-15 17:27:32 -0400

    Added new kernel functison and likelihood cv

---
+++ statsmodels/nonparametric/KernelFunctions.py
@@ -0,0 +1,116 @@
+# This module adds to kernels that are able to handle
+# categorical variables (both ordered and unordered)
+
+
+# In addition, this module contains kernel functions
+# This is a slight deviation from the current approach in statsmodels.
+# nonparametric.kernels where each kernel is a class object
+
+# Having kernel functions rather than classes makes extension to a multivariate
+# kernel density estimation much easier
+
+# NOTE: As it is, this module does not interact with the existing API
+
+import numpy as np
+
+def AitchisonAitken(h,Xi,x,c):
+    """
+    Returns the value of the Aitchison and Aitken's (1976) Kernel. Used for
+    unordered discrete random variable
+
+    Parameters
+    ----------
+    h : float
+        The bandwidth used to estimate the value of the kernel function
+    Xi : int
+        The value of a training point
+    x: int
+        The value at which the kernel density is being estimated
+    c: int
+        The number of possible values that the variable can take
+    
+    Returns
+    -------
+    kernel_value : float
+        The value of the kernel function
+        
+    References
+    ----------
+    Nonparametric econometrics : theory and practice / Qi Li and Jeffrey Scott Racine.
+    
+    """
+    if Xi == x:
+        kernel_value = 1-h
+    else:
+        kernel_value = h/(c-1)
+    return kernel_value
+
+
+
+def WangRyzin (h, Xi, x, c):
+    """
+    Returns the value of the Wang and van Ryzin's (1981) Kernel. Used for
+    ordered discrete random variable
+    
+    Parameters
+    ----------
+    h : float
+        The bandwidth used to estimate the value of the kernel function
+    Xi : int
+        The value of a training point
+    x: int
+        The value at which the kernel density is being estimated
+    c: int
+        The number of possible values that the variable can take
+    
+    Returns
+    -------
+    kernel_value : float
+        The value of the kernel function
+        
+    References
+    ----------
+    Nonparametric econometrics : theory and practice / Qi Li and Jeffrey Scott Racine.
+    
+    """
+    h=float(h)
+    
+    if Xi == x:
+        kernel_value = 1-h
+    else:
+        kernel_value =  0.5*(1-h)*h**abs(Xi-x)
+
+    return kernel_value
+
+def Epanechnikov (h, Xi, x):
+
+    """
+    Returns the value of the Epanechnikov (1969) Kernel. Used for
+    continuous random variables
+    
+    Parameters
+    ----------
+    h : float
+        The bandwidth used to estimate the value of the kernel function
+    Xi : array float
+        The value of a training point
+    x: int
+        The value at which the kernel density is being estimated
+    Returns
+    -------
+    kernel_value : float
+        The value of the kernel function
+        
+    References
+    ----------
+    Racine, J. "Nonparametric econometrics: A Primer" : Foundations and Trends in Econometrics.
+    
+    """
+    Xi=np.asarray(Xi)
+    h = float(h)
+    n = len(Xi)
+    z = (Xi - x)/h
+    InDom = (-np.sqrt(5) <= z) * (z <= np.sqrt(5))
+    kernel_value = InDom*(0.75*(1-0.2*z**2)*(1/np.sqrt(5)))
+ 
+    return kernel_value
--- statsmodels/nonparametric/bandwidths.py
+++ statsmodels/nonparametric/bandwidths.py
@@ -1,6 +1,11 @@
 import numpy as np
 from scipy.stats import scoreatpercentile as sap
 
+import KernelFunctions as kf
+
+kernel_func=dict(ordered=kf.WangRyzin, unordered=kf.AitchisonAitken,
+                 continuous=kf.Epanechnikov)
+                 
 #from scipy.stats import norm
 
 def _select_sigma(X):
@@ -17,6 +22,87 @@ def _select_sigma(X):
 #                             axis=axis), axis=axis)/normalize
     IQR = (sap(X, 75) - sap(X, 25))/normalize
     return np.minimum(np.std(X, axis=0, ddof=1), IQR)
+
+class LeaveOneOut(object):
+    # Written by Skipper
+    """
+    Generator to give leave one out views on X
+
+    Parameters
+    ----------
+    X : array-like
+        1d array
+
+    Examples
+    --------
+    >>> X = np.arange(10)
+    >>> loo = LeaveOneOut(X)
+    >>> for x in loo:
+    ...    print x
+
+    Notes
+    -----
+    A little lighter weight than sklearn LOO. We don't need test index.
+    Also passes views on X, not the index.
+    """
+    def __init__(self, X):
+        self.X = np.asarray(X)
+
+    def __iter__(self):
+        X = self.X
+        n = len(X)
+        for i in xrange(n):
+            index = np.ones(n, dtype=np.bool)
+            index[i] = False
+            yield X[index]
+
+
+def likelihood_cv(h,x,var_type):
+    """
+    Returns the leave one out log likelihood
+
+    Parameters
+    ----------
+    h : float
+        The bandwdith paremteter value (smoothing parameter)
+    x : arraylike
+        The training data
+    var_type : str
+        Defines the type of variable. Can take continuous, ordered or unordered
+
+    Returns
+    -------
+    L : float
+        The (negative of the) log likelihood value
+
+    References
+    ---------
+    Nonparametric econometrics : theory and practice / Qi Li and Jeffrey Scott Racine.
+     (p.16)
+    """
+    var_type=var_type.lower()
+    mykern=kernel_func[var_type]
+
+    n=len(x)
+    LOO=LeaveOneOut(X)
+    L=0
+    i=0
+    for X_j in LOO:
+        f_i=sum(mykern(h,X_j,X[i]))*1/((n-1)*h)
+        
+        i+=1
+        L+=np.log(f_i)
+    return -L
+
+from scipy.optimize import fmin
+def bw_likelihood_cv(x,var_type):
+    """
+    Returns the bandwidth parameter which maximizes the leave one out likelihood
+    """
+    
+    h0 = 1.0
+    bw = fmin (likelihood_cv, h0,(x,var_type))
+    return bw
 
 
 ## Univariate Rule of Thumb Bandwidths ##
