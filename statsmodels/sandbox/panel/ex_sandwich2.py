68d204cc8757efd7ad80231eab99c66fb3297e91
Josef Perktold <josef.pktd@gmail.com>
2012-05-16 11:27:54 -0400

    REF: clean up 2 sandwich examples

--- statsmodels/sandbox/panel/ex_sandwich2.py
+++ statsmodels/sandbox/panel/ex_sandwich2.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
-"""
+"""Cluster robust standard errors for OLS
 
 Created on Fri Dec 16 12:52:13 2011
-
 Author: Josef Perktold
 """
 
@@ -10,23 +9,32 @@ import numpy as np
 from numpy.testing import assert_almost_equal
 
 import statsmodels.api as sm
-
 import statsmodels.sandbox.panel.sandwich_covariance as sw
-import statsmodels.sandbox.panel.sandwich_covariance_generic as swg
-
 
 #http://www.ats.ucla.edu/stat/stata/seminars/svy_stata_intro/srs.dta
 import statsmodels.iolib.foreign as dta
 
-srs = dta.genfromdta("srs.dta")
+try:
+    srs = dta.genfromdta("srs.dta")
+    print 'using local file'
+except IOError:
+    import urllib
+    urllib.urlretrieve('http://www.ats.ucla.edu/stat/stata/seminars/svy_stata_intro/srs.dta', 'srs.dta')
+    print 'downloading file'
+    srs = dta.genfromdta("srs.dta")
+#    from statsmodels.tools.tools import webuse
+#    srs = webuse('srs', 'http://www.ats.ucla.edu/stat/stata/seminars/svy_stata_intro/')
+#    #does currently not cache file
+
 y = srs['api00']
+#older numpy don't reorder
 #x = srs[['growth', 'emer', 'yr_rnd']].view(float).reshape(len(y), -1)
 #force sequence
 x = np.column_stack([srs[ii] for ii in ['growth', 'emer', 'yr_rnd']])
 group = srs['dnum']
 
 #xx = sm.add_constant(x, prepend=True)
-xx = sm.add_constant(x, prepend=False) #for Stata compatibility
+xx = sm.add_constant(x, prepend=False) #const at end for Stata compatibility
 
 #remove nan observation
 mask = (xx!=-999.0).all(1)   #nan code in dta file
@@ -35,12 +43,16 @@ y = y[mask]
 xx = xx[mask]
 group = group[mask]
 
+#run OLS
+
 res_srs = sm.OLS(y, xx).fit()
-print res_srs.params
-print res_srs.bse
+print 'params    ', res_srs.params
+print 'bse_OLS   ', res_srs.bse
+
+#get cluster robust standard errors and compare with STATA
 
 bse_cr = sw.cov_cluster(res_srs, group.astype(int))[1]
-print bse_cr
+print 'bse_rob   ', bse_cr
 
 res_stata = np.rec.array(
      [ ('growth', '|', -0.1027121, 0.22917029999999999, -0.45000000000000001, 0.65500000000000003, -0.55483519999999997, 0.34941109999999997),
@@ -51,5 +63,9 @@ res_stata = np.rec.array(
              ('bse', '<f8'), ('tvalues', '<f8'), ('pvalues', '<f8'),
              ('cilow', '<f8'), ('ciupp', '<f8')])
 
-print bse_cr - res_stata.bse
+print 'diff Stata', bse_cr - res_stata.bse
 assert_almost_equal(bse_cr, res_stata.bse, decimal=6)
+
+#We see that in this case the robust standard errors of the parameter estimates
+#are larger than those of OLS by 8 to 35 %
+print bse_cr/res_srs.bse - 1
