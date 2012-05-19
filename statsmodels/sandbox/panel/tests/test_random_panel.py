803e1aaf3394ac62598b4506319a00fe65c21e2d
Josef Perktold <josef.pktd@gmail.com>
2012-05-18 20:00:34 -0400

    TST: make test for panel_short and panel robust covariance (not compared to other packages)

--- statsmodels/sandbox/panel/tests/test_random_panel.py
+++ statsmodels/sandbox/panel/tests/test_random_panel.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
+"""Test for short_panel and panel sandwich
 
 Created on Fri May 18 13:05:47 2012
 
@@ -9,96 +9,114 @@ moved example from main of random_panel
 """
 
 import numpy as np
+from numpy.testing import assert_almost_equal
+import numpy.testing as npt
+import statsmodels.tools.eval_measures as em
+from statsmodels.stats.moment_helpers import cov2corr, se_cov
+from statsmodels.regression.linear_model import OLS
+
 from statsmodels.sandbox.panel.panel_short import ShortPanelGLS, ShortPanelGLS2
 from statsmodels.sandbox.panel.random_panel import PanelSample
 import statsmodels.sandbox.panel.correlation_structures as cs
+import statsmodels.stats.sandwich_covariance as sw
+
+def assert_maxabs(actual, expected, value):
+    npt.assert_array_less(em.maxabs(actual, expected, None), value)
 
 
-examples = ['ex1']
-
-
-if 'ex1' in examples:
+def test_short_panel():
+    #this checks that some basic statistical properties are satisfied by the
+    #results, not verified results against other packages
     nobs = 100
     nobs_i = 5
     n_groups = nobs // nobs_i
     k_vars = 3
 
-#    dgp = PanelSample(nobs, k_vars, n_groups, corr_structure=cs.corr_equi,
-#                      corr_args=(0.6,))
-#    dgp = PanelSample(nobs, k_vars, n_groups, corr_structure=cs.corr_ar,
-#                      corr_args=([1, -0.95],))
     dgp = PanelSample(nobs, k_vars, n_groups, corr_structure=cs.corr_arma,
                       corr_args=([1], [1., -0.9],), seed=377769)
-    print 'seed', dgp.seed
+    #print 'seed', dgp.seed
     y = dgp.generate_panel()
     noise = y - dgp.y_true
-    print np.corrcoef(y.reshape(-1,n_groups, order='F'))
-    print np.corrcoef(noise.reshape(-1,n_groups, order='F'))
 
-    mod = ShortPanelGLS2(y, dgp.exog, dgp.groups)
-    res = mod.fit()
-    print res.params
-    print res.bse
-    #Now what?
-    #res.resid is of transformed model
-    #np.corrcoef(res.resid.reshape(-1,n_groups, order='F'))
-    y_pred = np.dot(mod.exog, res.params)
-    resid = y - y_pred
-    print np.corrcoef(resid.reshape(-1,n_groups, order='F'))
-    print resid.std()
-    err = y_pred - dgp.y_true
-    print err.std()
-    #OLS standard errors are too small
-    mod.res_pooled.params
-    mod.res_pooled.bse
-    #heteroscedasticity robust doesn't help
-    mod.res_pooled.HC1_se
-    #compare with cluster robust se
-    import statsmodels.stats.sandwich_covariance as sw
-    print sw.se_cov(sw.cov_cluster(mod.res_pooled, dgp.groups.astype(int)))
-    #not bad, pretty close to panel estimator
-    #and with Newey-West Hac
-    print sw.se_cov(sw.cov_nw_panel(mod.res_pooled, 4, mod.group.groupidx))
-    #too small, assuming no bugs,
-    #see Peterson assuming it refers to same kind of model
-    print dgp.cov
+    #test dgp
 
+    dgp_cov_e = np.array(
+              [[ 1.    ,  0.9   ,  0.81  ,  0.729 ,  0.6561],
+               [ 0.9   ,  1.    ,  0.9   ,  0.81  ,  0.729 ],
+               [ 0.81  ,  0.9   ,  1.    ,  0.9   ,  0.81  ],
+               [ 0.729 ,  0.81  ,  0.9   ,  1.    ,  0.9   ],
+               [ 0.6561,  0.729 ,  0.81  ,  0.9   ,  1.    ]])
+
+    npt.assert_almost_equal(dgp.cov, dgp_cov_e, 13)
+
+    cov_noise = np.cov(noise.reshape(-1,n_groups, order='F'))
+    corr_noise = cov2corr(cov_noise)
+    npt.assert_almost_equal(corr_noise, dgp.cov, 1)
+
+    #estimate panel model
     mod2 = ShortPanelGLS(y, dgp.exog, dgp.groups)
     res2 = mod2.fit_iterative(2)
-    print res2.params
-    print res2.bse
-    #both implementations produce the same results:
-    from numpy.testing import assert_almost_equal
-    assert_almost_equal(res.params, res2.params, decimal=14)
-    assert_almost_equal(res.bse, res2.bse, decimal=14)
-    mod5 = ShortPanelGLS(y, dgp.exog, dgp.groups)
-    res5 = mod5.fit_iterative(5)
-    print res5.params
-    print res5.bse
+
+
+    #whitened residual should be uncorrelated
+    corr_wresid = np.corrcoef(res2.wresid.reshape(-1,n_groups, order='F'))
+    assert_maxabs(corr_wresid, np.eye(5), 0.1)
+
+    #residual should have same correlation as dgp
+    corr_resid = np.corrcoef(res2.resid.reshape(-1,n_groups, order='F'))
+    assert_maxabs(corr_resid, dgp.cov, 0.1)
+
+    assert_almost_equal(res2.resid.std(),1, decimal=1)
+
+    y_pred = np.dot(mod2.exog, res2.params)
+    assert_almost_equal(res2.fittedvalues, y_pred, 13)
+
+
+    #compare with OLS
+
+    res2_ols = mod2._fit_ols()
+    npt.assert_(mod2.res_pooled is res2_ols)
+
+    res2_ols = mod2.res_pooled  #TODO: BUG: requires call to _fit_ols
+
     #fitting once is the same as OLS
     #note: I need to create new instance, otherwise it continuous fitting
     mod1 = ShortPanelGLS(y, dgp.exog, dgp.groups)
     res1 = mod1.fit_iterative(1)
-    res_ols = mod1._fit_ols()
-    assert_almost_equal(res1.params, res_ols.params, decimal=14)
-    assert_almost_equal(res1.bse, res_ols.bse, decimal=14)
 
-    import pandas as pa
-    #pandas.DataFrame doesn't do inplace append
-    se = pa.DataFrame(res_ols.bse[None,:], index=['OLS'])
-    se = se.append(pa.DataFrame(res5.bse[None,:], index=['PGLSit5']))
-    clbse = sw.se_cov(sw.cov_cluster(mod.res_pooled, dgp.groups.astype(int)))
-    se = se.append(pa.DataFrame(clbse[None,:], index=['OLSclu']))
-    pnwse = sw.se_cov(sw.cov_nw_panel(mod.res_pooled, 4, mod.group.groupidx))
-    se = se.append(pa.DataFrame(pnwse[None,:], index=['OLSpnw']))
-    print se
-    #list(se.index)
-    from statsmodels.iolib.table import SimpleTable
-    headers = [str(i) for i in se.columns]
-    stubs=list(se.index)
-#    print SimpleTable(np.round(np.asarray(se), 4),
-#                      headers=headers,
-#                      stubs=stubs)
-    print SimpleTable(np.asarray(se), headers=headers, stubs=stubs,
-                      txt_fmt=dict(data_fmts=['%10.4f']),
-                      title='Standard Errors')
+    assert_almost_equal(res1.params, res2_ols.params, decimal=13)
+    assert_almost_equal(res1.bse, res2_ols.bse, decimal=13)
+
+    res_ols = OLS(y, dgp.exog).fit()
+    assert_almost_equal(res1.params, res_ols.params, decimal=13)
+    assert_almost_equal(res1.bse, res_ols.bse, decimal=13)
+
+
+    #compare with old version
+    mod_old = ShortPanelGLS2(y, dgp.exog, dgp.groups)
+    res_old = mod_old.fit()
+
+    assert_almost_equal(res2.params, res_old.params, decimal=13)
+    assert_almost_equal(res2.bse, res_old.bse, decimal=13)
+
+
+    mod5 = ShortPanelGLS(y, dgp.exog, dgp.groups)
+    res5 = mod5.fit_iterative(5)
+
+    #make sure it's different
+    npt.assert_(em.maxabs(res5.bse, res2.bse) > 0.009)
+
+
+    clubse = se_cov(sw.cov_cluster(mod2.res_pooled, dgp.groups.astype(int)))
+    pnwbse = se_cov(sw.cov_nw_panel(mod2.res_pooled, 4, mod2.group.groupidx))
+    bser = np.vstack((res2.bse, res5.bse, clubse, pnwbse))
+    bser_mean = np.mean(bser, axis=0)
+
+    #cov_cluster close to robust and PanelGLS
+    #is up to 24% larger than mean of bser
+    npt.assert_array_less(0, clubse / bser_mean - 1)
+    npt.assert_array_less(clubse / bser_mean - 1, 0.25)
+    #cov_nw_panel close to robust and PanelGLS
+    npt.assert_array_less(pnwbse / bser_mean - 1, 0.1)
+    #OLS underestimates bse, robust at least 60% larger
+    npt.assert_array_less(0.6, bser_mean / res_ols.bse  - 1)
