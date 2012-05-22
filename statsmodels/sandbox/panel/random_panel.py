1f995b0096511449c5c792304e8be35acb087213
Josef Perktold <josef.pktd@gmail.com>
2012-05-21 21:08:55 -0400

    ENH/REF: add cov_nw_groupsum, various adjustments to random_panel example (some behavior I don't understand)

--- statsmodels/sandbox/panel/random_panel.py
+++ statsmodels/sandbox/panel/random_panel.py
@@ -24,9 +24,43 @@ class PanelSample(object):
 
     allows various within correlation structures, but no random intercept yet
 
+    Parameters
+    ----------
+    nobs : int
+        total number of observations
+    k_vars : int
+        number of explanatory variables to create in exog, including constant
+    n_groups int
+        number of groups in balanced sample
+    exog : None or ndarray
+        default is None, in which case a exog is created
+    within : bool
+        If True (default), then the exog vary within a group. If False, then
+        only variation across groups is used.
+        TODO: this option needs more work
+    corr_structure : ndarray or ??
+        Default is np.eye.
+    corr_args : tuple
+        arguments for the corr_structure
+    scale : float
+        scale of noise, standard deviation of normal distribution
+    seed : None or int
+        If seed is given, then this is used to create the random numbers for
+        the sample.
+
+    Notes
+    -----
+    The behavior for panel robust covariance estimators seems to differ by
+    a large amount by whether exog have mostly within group or across group
+    variation. I do not understand why this should be the case from the theory,
+    and this would warrant more investigation.
+
+    This is just used in one example so far and needs more usage to see what
+    will be useful to add.
+
     '''
 
-    def __init__(self, nobs, k_vars, n_groups, exog=None,
+    def __init__(self, nobs, k_vars, n_groups, exog=None, within=True,
                  corr_structure=np.eye, corr_args=(), scale=1, seed=None):
 
 
@@ -42,7 +76,17 @@ class PanelSample(object):
         self.group_indices = np.arange(n_groups+1) * nobs_i #check +1
 
         if exog is None:
-            t = np.repeat(np.linspace(-1,1,nobs_i), n_groups)
+            if within:
+                #t = np.tile(np.linspace(-1,1,nobs_i), n_groups)
+                t = np.tile(np.linspace(0, 2, nobs_i), n_groups)
+                #rs2 = np.random.RandomState(9876)
+                #t = 1 + 0.3 * rs2.randn(nobs_i * n_groups)
+                #mix within and across variation
+                #t += np.repeat(np.linspace(-1,1,nobs_i), n_groups)
+            else:
+                #no within group variation,
+                t = np.repeat(np.linspace(-1,1,nobs_i), n_groups)
+
             exog = t[:,None]**np.arange(k_vars)
 
         self.exog = exog
