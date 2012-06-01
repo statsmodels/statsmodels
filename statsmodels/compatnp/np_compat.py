a1f9a3b61c1e3487f2dd39dcf70c4f37cfe2368f
Josef Perktold <josef.pktd@gmail.com>
2012-05-31 20:57:41 -0400

    BUG: fix type in version check, tested by forcing path not with numpy 1.6.2

--- statsmodels/compatnp/np_compat.py
+++ statsmodels/compatnp/np_compat.py
@@ -49,7 +49,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import numpy as np
 
-if np.version < '1.6.2':
+if np.__version__ < '1.6.2':
     npc_unique = np.unique
 else:
 
