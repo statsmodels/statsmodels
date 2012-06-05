fd83574f224b480b9341d59be10f23f837c02e7e
Josef Perktold <josef.pktd@gmail.com>
2012-06-05 13:07:01 -0400

    ENH: tools/topy.R with sur example, new function cat_items

---
+++ tools/ex_sur.R
@@ -0,0 +1,42 @@
+data <- read.csv('E:\\path_to_repo\\statsmodels\\datasets\\grunfeld\\grunfeld.csv')
+
+data <- data[data$firm %in% c('General Motors','Chrysler','General Electric','Westinghouse','US Steel'),]
+attach(data)
+library('plm')
+library('systemfit')
+panel <- plm.data(data,c('firm','year'))
+formula <- invest ~ value + capital
+SUR <- systemfit(formula,method='SUR',data=panel)
+f <- fitted(SUR)
+ff <- c(f[,'Chrysler'],f[,'General.Electric'],f[,'General.Motors'],f[,'US.Steel'],f[,'Westinghouse'])
+
+# save results to python module
+source("E:\\path_to_repo\\tools\\topy.R")
+
+#translation table for names  (could be dict in python)
+translate = list(coefficients="params",
+                 coefCov="cov_params",
+                 residCovEst="resid_cov_est",
+                 residCov="resid_cov",
+                 df_residual="df_resid",
+                 fitted_values="fittedvalues"
+                 )
+
+fname = "tmp_sur_0.py"
+append = FALSE #TRUE
+
+#redirect output to file
+sink(file=fname, append=append)
+
+cat("\nsur = Bunch()\n")
+
+cat_items(SUR, prefix="sur.", blacklist=c("eq", "control"), trans=translate)
+
+equations = SUR[["eq"]]
+for (ii in c(1:length(equations))) {
+equ_name = paste("\nsur.equ", ii, sep="")
+cat(equ_name); cat(" = Bunch()\n")
+cat_items(equations[[ii]], prefix=paste(equ_name, ".", sep=""), trans=translate)
+}
+
+sink()
---
+++ tools/topy.R
@@ -0,0 +1,60 @@
+
+cat_items <- function(object, prefix="", blacklist=NULL, trans=list())    {
+#print items into python expression for defining variables
+#example cat_items(names(object), blacklist=c("eq"))
+#
+#currently limited type inference, mainly numerical
+
+items = names(object)
+for (name in items) {
+	if (is.element(name, blacklist)) next
+	#cat(name); cat("\n")
+	item = object[[name]]
+
+	#fix name
+	#Skipper's sanitize name
+	name_ <- gsub("\\.", "_", name) # make name pythonic
+      #translate name
+	newname = trans[[name]]
+      if (!is.null(newname)) {
+          name_ = newname}
+	name_ = paste(prefix, name_, sep="")
+
+	if (is.numeric(item)) {
+		if (!is.null(names(item))) {    #named list, class numeric ?
+               mkarray(as.matrix(item), name_); cat("\n")
+                  if (!is.null(dimnames(item))) write_dimnames(item, prefix=name_)
+		}
+		else if (class(item) == 'matrix') {
+               mkarray(item, name_); cat("\n")
+                  if (!is.null(dimnames(item))) write_dimnames(item, prefix=name_)
+		}
+            else if (class(item) == 'numeric') {   #scalar
+                  cat(name_); cat(" = "); cat(item); cat("\n")
+		}
+      }
+	else if (is.character(item)) {
+            #assume string doesn't contain single quote
+            cat(name_); cat(" = '"); cat(item); cat("'\n")
+      }
+	else {
+            cat(name_); cat(" = '''"); cat(deparse(item)); cat("'''"); cat("\n")
+      }
+
+}
+} #end function
+
+write_dimnames <- function(mat, prefix="") {
+if (prefix != "") {
+    prefix = paste(prefix, "_", sep="")
+}
+dimn = list("rownames", "colnames", "thirdnames")  #up to 3 dimension ?
+for (ii in c(1:length(dimnames(mat)))) {
+cat(paste(prefix, dimn[[ii]], sep=""))
+cat (" = [");
+for (dname in dimnames(mat)[[ii]]) {
+cat(dname); cat(", ")
+}
+cat("]\n")
+}
+}
