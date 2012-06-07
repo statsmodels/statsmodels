bba3fbb54540aadd0ef046e34b93f6e9c2672041
Josef Perktold <josef.pktd@gmail.com>
2012-06-06 23:00:21 -0400

    typo in sandbox gof_new example

--- statsmodels/sandbox/distributions/gof_new.py
+++ statsmodels/sandbox/distributions/gof_new.py
@@ -684,3 +684,19 @@ if __name__ == '__main__':
     [0.1545, 0.10009999999999999, 0.049000000000000002, 0.023, 0.0104]
     >>>
     '''
+
+    #test equality of loop, vectorized, batch-vectorized
+    np.random.seed(8765679)
+    resu1 = bootstrap(NewNorm(), args=(0,1), nobs=nobs, nrep=100,
+                      value=0.576/(1 + 4./nobs - 25./nobs**2))
+    np.random.seed(8765679)
+    tmp = [bootstrap(NewNorm(), args=(0,1), nobs=nobs, nrep=1) for _ in range(100)]
+    resu2 = (np.array(tmp) > 0.576/(1 + 4./nobs - 25./nobs**2)).mean()
+    np.random.seed(8765679)
+    tmp = [bootstrap(NewNorm(), args=(0,1), nobs=nobs, nrep=1,
+                     value=0.576/ (1 + 4./nobs - 25./nobs**2),
+                     batch_size=10) for _ in range(10)]
+    resu3 = np.array(tmp).mean()
+    from numpy.testing import assert_almost_equal, assert_array_almost_equal
+    assert_array_almost_equal(resu1, resu2, 15)
+    assert_array_almost_equal(resu2, resu3, 15)
