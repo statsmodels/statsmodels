0400b68e24e4fe8a8f554b809fd8e31154927b85
Josef Perktold <josef.pktd@gmail.com>
2012-06-08 18:16:39 -0400

    REF move code ex_gof2

---
+++ statsmodels/sandbox/examples/ex_gof2.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+
+Created on Fri Jun 08 16:13:47 2012
+
+Author: Josef Perktold
+"""
+
+ge = GOFExpon(rvs)
+print ge.get_test()  #default is 'a2'
+print ge.get_test('w2')
+print ge.get_test('u2')
+rvsn = np.random.randn(50)
+print GOFExpon(rvsn**2).get_test()
+
+#rvsm = (np.random.randn(50, 2) * [1, 2]).sum(1)
+rvsm = (np.random.randn(50, 2) * [1, 2]).ravel()
+gn = GOFNormal(rvsm)
+print gn.get_test()
+from statsmodels.stats import diagnostic as dia
+print dia.normal_ad(rvsm)
+print gn.get_test('d')
+print dia.lillifors(rvsm)
+
+
