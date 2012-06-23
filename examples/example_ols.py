24e64f6427afbc8fa8ceb34359d0585c89e1efdc
Vincent Arel-Bundock <varel@umich.edu>
2012-06-23 19:13:05 -0400

    More verbose example_ols.py

--- examples/example_ols.py
+++ examples/example_ols.py
@@ -1,20 +1,29 @@
 """Ordinary Least Squares
 """
 
-from statsmodels.datasets.longley import load
+# Load modules and the Longley dataset which ships with statsmodels
 import statsmodels.api as sm
 import numpy as np
-
+from statsmodels.datasets.longley import load
 data = load()
+
+# Print the first 5 observations of the Longley exogenous variables matrix
+# (i.e. independent variables) and the endogenous variable (i.e. dependent
+# variable).
+print data.exog[:5,:]
+print data.endog[:5]
+
+# Our model needs an intercept, so we add a column with a constant
 data.exog = sm.tools.add_constant(data.exog)
 
+# Fit the model and print summary
 ols_model = sm.OLS(data.endog, data.exog)
 ols_results = ols_model.fit()
+print ols_results.summary()
 
-# the Longley dataset is well known to have high multicollinearity
-# one way to find the condition number is as follows
-
-# normalize the independent variables to have unit length, Greene 4.9
+# The Longley dataset is well known to have high multicollinearity. One way to
+# find the condition number is to normalize the independent variables to have
+# unit length (see Greene 4.9).
 norm_x = np.ones_like(data.exog)
 for i in range(int(ols_model.df_model)):
     norm_x[:,i] = data.exog[:,i]/np.linalg.norm(data.exog[:,i])
@@ -22,17 +31,11 @@ norm_xtx = np.dot(norm_x.T,norm_x)
 eigs = np.linalg.eigvals(norm_xtx)
 collin = np.sqrt(eigs.max()/eigs.min())
 print collin
-# clearly there is a big problem with multicollinearity
-# the rule of thumb is any number of 20 requires attention
 
-# for instance, consider the longley dataset with the last observation dropped
+# There seems to be a big problem with multicollinearity; the rule of thumb is
+# that any number above 20 requires attention. We consider the Longley dataset
+# with the last observation dropped, and we compare the coefficient estimates
+# in this new model to our original ones. The changes are considerable.
 ols_results2 = sm.OLS(data.endog[:-1], data.exog[:-1,:]).fit()
-
-# all of our coefficients change considerably in percentages
-# of the original coefficients
 print "Percentage change %4.2f%%\n"*7 % tuple([i for i in ols_results.params/ols_results2.params*100 - 100])
-
-
-
-
 
