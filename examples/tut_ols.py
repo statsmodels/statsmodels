b8afb9c632a8785e7fa94879e7d0a6e2eba04f0b
Vincent Arel-Bundock <varel@umich.edu>
2012-06-23 20:59:43 -0400

    Cleaner and more verbose tot_ols.py

--- examples/tut_ols.py
+++ examples/tut_ols.py
@@ -2,106 +2,117 @@
 '''
 
 import numpy as np
-#from scipy import stats
 import statsmodels.api as sm
 import matplotlib
-#..matplotlib.use('Qt4Agg')#, warn=True)   #for Spyder
 import matplotlib.pyplot as plt
 from statsmodels.sandbox.regression.predstd import wls_prediction_std
+#..from scipy import stats
+#..matplotlib.use('Qt4Agg')#, warn=True)   #for Spyder
 
-#fix a seed for these examples
+# Fix a random seed for these examples
 np.random.seed(9876789)
 
-# OLS non-linear curve but linear in parameters
-# ---------------------------------------------
+#OLS non-linear curve but linear in parameters
+#---------------------------------------------
 
+# Artificial data: non-linear relationship between x and y 
 nsample = 50
 sig = 0.5
-x1 = np.linspace(0, 20, nsample)
-X = np.c_[x1, np.sin(x1), (x1-5)**2, np.ones(nsample)]
+x = np.linspace(0, 20, nsample)
+X = np.c_[x, np.sin(x), (x-5)**2, np.ones(nsample)]
 beta = [0.5, 0.5, -0.02, 5.]
 y_true = np.dot(X, beta)
 y = y_true + sig * np.random.normal(size=nsample)
 
-plt.figure()
-plt.plot(x1, y, 'o', x1, y_true, 'b-')
-
+# Fit OLS model and print results
 res = sm.OLS(y, X).fit()
+print res.summary()
+
+# Extract other quantities of interest
 print res.params
 print res.bse
 print res.predict()
+
+# Draw a plot to compare the true relationship to OLS predictions. Confidence
+# intervals around the predictions are built using the ``wls_prediction_std``
+# command.
+plt.figure()
+plt.plot(x, y, 'o', x, y_true, 'b-')
 prstd, iv_l, iv_u = wls_prediction_std(res)
-plt.plot(x1, res.fittedvalues, 'r--.')
-plt.plot(x1, iv_u, 'r--')
-plt.plot(x1, iv_l, 'r--')
-#@savefig tut_ols_0.png
+plt.plot(x, res.fittedvalues, 'r--.')
+plt.plot(x, iv_u, 'r--')
+plt.plot(x, iv_l, 'r--')
 plt.title('blue: true,   red: OLS')
-
-print res.summary()
-
 
 #OLS with dummy variables
 #------------------------
 
-sig = 1.
-#suppose observations from 3 groups
-xg = np.zeros(nsample, int)
-xg[20:40] = 1
-xg[40:] = 2
-print xg
-dummy = (xg[:,None] == np.unique(xg)).astype(float)
-#use group 0 as benchmark
-X = np.c_[x1, dummy[:,1:], np.ones(nsample)]
+# Artificial data: 3 groups will be modelled using dummy variables. Group 0 is
+# the omitted/benchmark category.
+nsample = 50
+groups = np.zeros(nsample, int)
+groups[20:40] = 1
+groups[40:] = 2
+dummy = (groups[:,None] == np.unique(groups)).astype(float)
+X = np.c_[x, dummy[:,1:], np.ones(nsample)]
 beta = [1., 3, -3, 10]
 y_true = np.dot(X, beta)
-y = y_true + sig * np.random.normal(size=nsample)
+y = y_true + np.random.normal(size=nsample)
 
+# Inspect the data
+print X[:5,:]
+print y[:5]
+print groups
+print dummy[:5,:]
 
+# Fit OLS model, print results and other quantities of interest
 res2 = sm.OLS(y, X).fit()
+print res.summary()
 print res2.params
 print res2.bse
 print res.predict()
-prstd, iv_l, iv_u = wls_prediction_std(res2)
 
+# Draw a plot to compare the true relationship to OLS predictions. 
+prstd, iv_l, iv_u = wls_prediction_std(res2)
 plt.figure()
-plt.plot(x1, y, 'o', x1, y_true, 'b-')
-plt.figure()
-plt.plot(x1, y, 'o', x1, y_true, 'b-')
-plt.plot(x1, res2.fittedvalues, 'r--.')
-plt.plot(x1, iv_u, 'r--')
-plt.plot(x1, iv_l, 'r--')
-#@savefig tut_ols_1.png
+plt.plot(x, y, 'o', x, y_true, 'b-')
+plt.plot(x, res2.fittedvalues, 'r--.')
+plt.plot(x, iv_u, 'r--')
+plt.plot(x, iv_l, 'r--')
 plt.title('blue: true,   red: OLS')
 
-print res.summary()
+#Joint hypothesis tests
+#----------------------
 
-R = [[0, 1, 0, 0],
-     [0, 0, 1, 0]]
+#F test
+#^^^^^^
 
-# F test joint hypothesis R * beta = 0
-# i.e. coefficient on both dummy variables equal zero
+# We want to test the hypothesis that both coefficients on the dummy variables
+# are equal to zero, that is, :math:`R \times \beta = 0`. An F test leads us to
+# strongly reject the null hypothesis of identical constant in the 3 groups:
+
+R = [[0, 1, 0, 0], [0, 0, 1, 0]]
+print R
 print res2.f_test(R)
-# strongly rejected Null of identical constant in 3 groups
-#..<F test: F=124.19050615860911, p=2.87411973729e-019, df_denom=46, df_num=2>
-help(res2.f_test)
+print float(res2.f_test(R).pvalue)
 
-# t test for Null hypothesis effects of 2nd and 3rd group add to zero
+#T test
+#^^^^^^
+
+# We want to test the null hypothesis that the effects of the 2nd and 3rd
+# groups add to zero. The T-test allows us to reject the Null (but note the
+# one-sided p-value): 
 R = [0, 1, -1, 0]
 print res2.t_test(R)
-# don't reject Null at 5% confidence level (note one sided p-value)
-#..<T test: effect=1.0363792917100714, sd=0.52675137730463362, t=1.9674923243925513, p=0.027586676754860262, df_denom=46>
 
+#Small group effects
+#^^^^^^^^^^^^^^^^^^^ 
 
-# OLS with small group effects
-
+# If we generate artificial data with smaller group effects, the T test can no
+# longer reject the Null hypothesis: 
 beta = [1., 0.3, -0.0, 10]
 y_true = np.dot(X, beta)
-y = y_true + sig * np.random.normal(size=nsample)
+y = y_true + np.random.normal(size=nsample)
 res3 = sm.OLS(y, X).fit()
 print res3.f_test(R)
-# don't reject Null of identical constant in 3 groups
-#..<F test: F=1.9715385826285652, p=0.15083366806, df_denom=46, df_num=2>
 
-
-#..plt.draw()
-#..plt.show()
