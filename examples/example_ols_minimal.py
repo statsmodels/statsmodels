cedfe99ac9c8eef2bd84af6f28625e6819939cdb
Vincent Arel-Bundock <varel@umich.edu>
2012-06-23 20:03:09 -0400

    More verbose example_ols_minimal.py

--- examples/example_ols_minimal.py
+++ examples/example_ols_minimal.py
@@ -4,16 +4,20 @@
 import numpy as np
 import statsmodels.api as sm
 
+# Artificial data: :math:`y_i = 1 + .1 * x_i + 1 * x_i^2 + e_i`, with :math:`e\sim N(0,1)`
 nsample = 100
-x = np.linspace(0,10, 100)
+x = np.linspace(0, 10, 100)
 X = sm.add_constant(np.column_stack((x, x**2)))
 beta = np.array([1, 0.1, 10])
-y = np.dot(X, beta) + np.random.normal(size=nsample)
+e = np.random.normal(size=nsample)
+y = np.dot(X, beta) + e
 
-results = sm.OLS(y, X).fit()
+# Inspect data
+print X[:5,:]
+print y[:5]
+
+# Describe, fit, and summarize OLS model
+model = sm.OLS(y, X)
+results = model.fit()
 print results.summary()
-
-
-
-
 
