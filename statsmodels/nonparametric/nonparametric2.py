b4815c91b244f054c63f08c4c9bdfd4b4188aa1d
George Panterov <econgpanterov@gmail.com>
2012-06-27 16:10:31 -0400

    pep8 on nonparametric2.py and np_tools.py

--- statsmodels/nonparametric/nonparametric2.py
+++ statsmodels/nonparametric/nonparametric2.py
@@ -39,16 +39,15 @@ class Generic_KDE ():
         The default values for bw and bwmethod are None. The user must specify either a value for bw
         or bwmethod but not both.
         """
-
-
-        self.bw_func = dict(normal_reference = self._normal_reference, cv_ml = self._cv_ml, cv_ls = self._cv_ls)
+        self.bw_func = dict(normal_reference=self._normal_reference,
+                            cv_ml=self._cv_ml, cv_ls=self._cv_ls)
         if bw is None:
             bwfunc = self.bw_func['normal_reference']
             return bwfunc()
 
         if type(bw) != str:  # The user provided an actual bandwidth estimate
             return np.asarray(bw)
-        else: # The user specified a bandwidth selection method e.g. 'normal-reference'
+        else:  # The user specified a bandwidth selection method e.g. 'normal-reference'
             bwfunc = self.bw_func[bw]
             return bwfunc()
 
@@ -58,21 +57,23 @@ class Generic_KDE ():
         """
         c = 1.06
         X = np.std(self.all_vars, axis=0)
-        return c*X*self.N**(-1./(4+np.size(self.all_vars, axis=1)))
+        return c * X * self.N ** (- 1. / (4 + np.size(self.all_vars, axis=1)))
 
-    def _cv_ml (self):
+    def _cv_ml(self):
         """
         Returns the cross validation maximum likelihood bandwidth parameter
         """
 
-        h0 = self._normal_reference() # the initial value for the optimization is the normal_reference
-        bw = opt.fmin(self.loo_likelihood, x0 = h0, args = (np.log,), maxiter = 1e3, maxfun = 1e3,disp = 0)
+        h0 = self._normal_reference()  # the initial value for the optimization is the normal_reference
+        bw = opt.fmin(self.loo_likelihood, x0=h0, args=(np.log, ),
+                      maxiter=1e3, maxfun=1e3, disp=0)
         return bw
 
 #TODO: Add the least squares cross validation bandwidth method
-    def _cv_ls (self):
+    def _cv_ls(self):
         h0 = self._normal_reference()
-        bw = opt.fmin(self.IMSE, x0 = h0, maxiter = 1e3, maxfun = 1e3,disp = 0)
+        bw = opt.fmin(self.IMSE, x0=h0, maxiter=1e3,
+                      maxfun=1e3, disp=0)
         return np.abs(bw)  # Getting correct but negative values for bw. from time to time . Why?
 
     def loo_likelihood(self):
@@ -126,17 +127,16 @@ class UKDE(Generic_KDE):
     array([ 0.39967419,  0.38423292])
 
     """
-    def __init__(self, tdat, var_type, bw = None):
+    def __init__(self, tdat, var_type, bw=None):
 
         self.tdat = np.column_stack(tdat)
         self.all_vars = self.tdat
-        self.N,self.K = np.shape(self.tdat)
+        self.N, self.K = np.shape(self.tdat)
         self.var_type = var_type
         assert self.K == len(self.var_type)
         self.bw = self.compute_bw(bw)
 
-
-    def loo_likelihood(self, bw, func = lambda x:x):
+    def loo_likelihood(self, bw, func=lambda x: x):
         """
         Returns the leave-one-out likelihood for the data
         Parameters
@@ -148,12 +148,13 @@ class UKDE(Generic_KDE):
         i = 0
         L = 0
         for X_j in LOO:
-            f_i = tools.GPKE(bw, tdat = -X_j, edat = -self.tdat[i, :], var_type = self.var_type)
+            f_i = tools.GPKE(bw, tdat=-X_j, edat=-self.tdat[i, :],
+                             var_type=self.var_type)
             i += 1
             L += func(f_i)
         return -L
 
-    def pdf(self, edat = None):
+    def pdf(self, edat=None):
         """
         Returns the probability density function
 
@@ -163,20 +164,40 @@ class UKDE(Generic_KDE):
             Evaluation data.
             If unspecified, the training data is used
         """
-        if edat is None: edat = self.tdat
-        return tools.GPKE(self.bw, tdat = self.tdat, edat = edat, var_type = self.var_type)/self.N
+        if edat is None:
+            edat = self.tdat
+        return tools.GPKE(self.bw, tdat=self.tdat, edat=edat,
+                          var_type=self.var_type) / self.N
 
     def IMSE(self, bw):
         """
         Returns the First term from the Integrated Mean Square Error
         Integrate [ f_hat(x) - f(x)]^2 dx
         """
-        F=0
+        print "running..."
+        F = 0
         for i in range(self.N):
-            k_bar_sum = tools.GPKE (bw, tdat = -self.tdat, edat = -self.tdat[i, :],
-                                    var_type = self.var_type, ckertype = 'gauss_convolution', okertype = 'wangryzin_convolution')
+            k_bar_sum = tools.GPKE(bw, tdat=-self.tdat, edat=-self.tdat[i, :],
+                                    var_type=self.var_type, ckertype='gauss_convolution', okertype='wangryzin_convolution',
+                                    ukertype='aitchisonaitken_convolution')
             F += k_bar_sum
-        return (F/(self.N**2) + self.loo_likelihood(bw)*2/((self.N)*(self.N - 1)))  # there is a + because loo_likelihood returns the negative
+        # there is a + because loo_likelihood returns the negative
+        return (F / (self.N ** 2) + self.loo_likelihood(bw) * 2 / ((self.N) * (self.N - 1)))
+
+    def cdf(self, val):
+        val = np.squeeze(val)
+
+        if self.K == 1:
+            func = tools.IntegrateSingle
+            n_edat = np.size(val)
+        elif self.K == 2:
+            func = tools.IntegrateDbl
+        return func(val, self.pdf)
+##        res=[]
+##        for i in val:
+##            res.append( func(val))
+##        return res
+
 
 class CKDE(Generic_KDE):
     """
@@ -184,7 +205,6 @@ class CKDE(Generic_KDE):
     Calculates P(X_1,X_2,...X_n | Y_1,Y_2...Y_m) = P(X_1, X_2,...X_n, Y_1, Y_2,..., Y_m)/P(Y_1, Y_2,..., Y_m)
     The conditional density is by definition the ratio of the two unconditional densities
     http://en.wikipedia.org/wiki/Conditional_probability_distribution
-
 
     Parameters
     ----------
@@ -230,20 +250,20 @@ class CKDE(Generic_KDE):
     print "The bandwdith is: ", dens_c.bw
     """
 
-    def __init__ (self, tydat, txdat, dep_type, indep_type, bw):
+    def __init__(self, tydat, txdat, dep_type, indep_type, bw):
 
         self.tydat = np.column_stack(tydat)
         self.txdat = np.column_stack(txdat)
-        self.N,self.K_dep = np.shape(self.tydat)
+        self.N, self.K_dep = np.shape(self.tydat)
         self.K_indep = np.shape(self.txdat)[1]
-        self.all_vars = np.concatenate((self.tydat, self.txdat), axis = 1)
-        self.dep_type = dep_type; self.indep_type = indep_type
+        self.all_vars = np.concatenate((self.tydat, self.txdat), axis=1)
+        self.dep_type = dep_type
+        self.indep_type = indep_type
         assert len(self.dep_type) == self.K_dep
         assert len(self.indep_type) == self.K_indep
-
         self.bw = self.compute_bw(bw)
 
-    def loo_likelihood(self, bw, func = lambda x:x):
+    def loo_likelihood(self, bw, func=lambda x: x):
         """
         Returns the leave-one-out likelihood for the data
         """
@@ -253,14 +273,16 @@ class CKDE(Generic_KDE):
         L = 0
         for Y_j in yLOO:
             X_j = xLOO.next()
-            f_yx = tools.GPKE(bw, tdat = -Y_j, edat=-self.all_vars[i,:], var_type = (self.dep_type + self.indep_type))
-            f_x = tools.GPKE(bw[self.K_dep::], tdat = -X_j, edat=-self.txdat[i, :], var_type = self.indep_type)
-            f_i = f_yx/f_x
+            f_yx = tools.GPKE(bw, tdat=-Y_j, edat=-self.all_vars[i, :],
+                              var_type=(self.dep_type + self.indep_type))
+            f_x = tools.GPKE(bw[self.K_dep::], tdat=-X_j,
+                             edat=-self.txdat[i, :], var_type=self.indep_type)
+            f_i = f_yx / f_x
             i += 1
             L += func(f_i)
-        return -L
+        return - L
 
-    def pdf(self,eydat = None, exdat = None):
+    def pdf(self, eydat=None, exdat=None):
         """
         Returns the probability density function
 
@@ -272,74 +294,73 @@ class CKDE(Generic_KDE):
         exdat: array-like
             Evaluation data for the independent variables
         """
-
-        if eydat is None: eydat = self.all_vars
-        if exdat is None: exdat = self.txdat
-
-        f_yx = tools.GPKE(self.bw,tdat=np.concatenate((self.tydat, self.txdat), axis=1), edat = eydat, var_type = (self.dep_type + self.indep_type))
-        f_x = tools.GPKE(self.bw[self.K_dep::], tdat = self.txdat, edat = exdat, var_type = self.indep_type)
-        return (f_yx/f_x)
-
+        if eydat is None:
+            eydat = self.all_vars
+        if exdat is None:
+            exdat = self.txdat
+        f_yx = tools.GPKE(self.bw, tdat=np.concatenate((self.tydat, self.txdat), axis=1),
+                          edat=eydat, var_type=(self.dep_type + self.indep_type))
+        f_x = tools.GPKE(self.bw[self.K_dep::], tdat=self.txdat,
+                         edat=exdat, var_type=self.indep_type)
+        return (f_yx / f_x)
 
     def IMSE_slow(self, bw):
         print "Starting"
         # Still runs very slow. Try to vectorize.
         zLOO = tools.LeaveOneOut(self.all_vars)
         l = 0
-        
         CV = 0
         for Z in zLOO:
-            
-            X = Z[:,self.K_dep::]
-            Y = Z[:,0:self.K_dep]
-            
+            X = Z[:, self.K_dep::]
+            Y = Z[:, 0:self.K_dep]
             G = 0
-            for i in range(self.N-1):
-                
-                
-                K_Xi_Xl = tools.GPKE2(bw[self.K_dep::], tdat = -X[i,:], edat = -self.txdat[l, :], var_type = self.indep_type)
-                K_Xj_Xl = tools.GPKE2(bw[self.K_dep::], tdat = -X, edat = -self.txdat[l, :], var_type = self.indep_type)
-                K2_Yi_Yj = tools.GPKE2(bw[0:self.K_dep], tdat = -Y, edat = -Y[i,:], var_type = self.dep_type,
-                             ckertype = 'gauss_convolution', okertype = 'wangryzin_convolution')
+            for i in range(self.N - 1):
+                K_Xi_Xl = tools.GPKE2(bw[self.K_dep::], tdat=-X[i, :],
+                                      edat=-self.txdat[l, :], var_type=self.indep_type)
+                K_Xj_Xl = tools.GPKE2(bw[self.K_dep::], tdat=-X,
+                                      edat=-self.txdat[l, :], var_type=self.indep_type)
+                K2_Yi_Yj = tools.GPKE2(bw[0:self.K_dep], tdat=-Y,
+                                       edat=-Y[i, :], var_type=self.dep_type,
+                             ckertype='gauss_convolution', okertype='wangryzin_convolution',
+                                       ukertype='aitchisonaitken_convolution')
                 G += np.sum(K_Xi_Xl * K_Xj_Xl * K2_Yi_Yj)
-            G = G/self.N**2
-            f_X_Y = tools.GPKE(bw, tdat = -Z, edat = -self.all_vars[l,:], var_type = (self.dep_type + self.indep_type))/float(self.N)
-            m_x = tools.GPKE(bw[self.K_dep::], tdat = -X, edat = -self.txdat[l, :], var_type = self.indep_type)/float(self.N)
-            
+            G = G / self.N ** 2
+            f_X_Y = tools.GPKE(bw, tdat=-Z, edat=-self.all_vars[l, :],
+                               var_type=(self.dep_type + self.indep_type)) / float(self.N)
+            m_x = tools.GPKE(bw[self.K_dep::], tdat=-X, edat=-self.txdat[l, :],
+                             var_type=self.indep_type) / float(self.N)
             l += 1
-            CV += (G/m_x**2) - 2*(f_X_Y/m_x) 
-        return CV/float(self.N)
-    
+            CV += (G / m_x ** 2) - 2 * (f_X_Y / m_x)
+        return CV / float(self.N)
+
     def IMSE(self, bw):
         print "Starting"
         # Still runs very slow. Try to vectorize.
         zLOO = tools.LeaveOneOut(self.all_vars)
         l = 0
-        
         CV = 0
         for Z in zLOO:
-            
-            X = Z[:,self.K_dep::]
-            Y = Z[:,0:self.K_dep]
-            
-            Expander = np.ones((self.N-1,1))
-            Ye_L = np.kron(Y,Expander)
-            Ye_R = np.kron(Expander,Y)
-
-            Xe_L = np.kron(X,Expander)
-            Xe_R = np.kron(Expander,X)            
-            
-                
-                
-            K_Xi_Xl = tools.GPKE3(bw[self.K_dep::], tdat = Xe_L, edat = self.txdat[l, :], var_type = self.indep_type)
-            K_Xj_Xl = tools.GPKE3(bw[self.K_dep::], tdat = Xe_R, edat = self.txdat[l, :], var_type = self.indep_type)
-            K2_Yi_Yj = tools.GPKE3(bw[0:self.K_dep], tdat = Ye_L, edat = Ye_R, var_type = self.dep_type,
-                             ckertype = 'gauss_convolution', okertype = 'wangryzin_convolution')
+            X = Z[:, self.K_dep::]
+            Y = Z[:, 0:self.K_dep]
+            Expander = np.ones((self.N - 1, 1))
+            Ye_L = np.kron(Y, Expander)
+            Ye_R = np.kron(Expander, Y)
+            Xe_L = np.kron(X, Expander)
+            Xe_R = np.kron(Expander, X)            
+            K_Xi_Xl = tools.GPKE3(bw[self.K_dep::], tdat=Xe_L,
+                                  edat=self.txdat[l, :], var_type=self.indep_type)
+            K_Xj_Xl = tools.GPKE3(bw[self.K_dep::], tdat=Xe_R,
+                                  edat=self.txdat[l, :], var_type=self.indep_type)
+            K2_Yi_Yj = tools.GPKE3(bw[0:self.K_dep], tdat=Ye_L,
+                                   edat=Ye_R, var_type=self.dep_type,
+                             ckertype='gauss_convolution', okertype='wangryzin_convolution',
+                                   ukertype='aitchisonaitken_convolution')
             G = np.sum(K_Xi_Xl * K_Xj_Xl * K2_Yi_Yj)
-            G = G/self.N**2
-            f_X_Y = tools.GPKE(bw, tdat = -Z, edat = -self.all_vars[l,:], var_type = (self.dep_type + self.indep_type))/float(self.N)
-            m_x = tools.GPKE(bw[self.K_dep::], tdat = -X, edat = -self.txdat[l, :], var_type = self.indep_type)/float(self.N)
-            
+            G = G / self.N ** 2
+            f_X_Y = tools.GPKE(bw, tdat=-Z, edat=-self.all_vars[l, :],
+                               var_type=(self.dep_type + self.indep_type)) / float(self.N)
+            m_x = tools.GPKE(bw[self.K_dep::], tdat=-X, edat=-self.txdat[l, :],
+                             var_type=self.indep_type) / float(self.N)
             l += 1
-            CV += (G/m_x**2) - 2*(f_X_Y/m_x) 
-        return CV/float(self.N)
+            CV += (G / m_x ** 2) - 2 * (f_X_Y / m_x)
+        return CV / float(self.N)
--- statsmodels/nonparametric/np_tools.py
+++ statsmodels/nonparametric/np_tools.py
@@ -1,12 +1,28 @@
 import numpy as np
 import KernelFunctions as kf
 import scipy.optimize as opt
+from scipy.integrate import *
 
-kernel_func=dict(wangryzin = kf.WangRyzin, aitchisonaitken = kf.AitchisonAitken,
-                 epanechnikov = kf.Epanechnikov, gaussian = kf.Gaussian,
-                 gauss_convolution = kf.Gaussian_Convolution, wangryzin_convolution = kf.WangRyzin_Convolution)
 
-Convolution_Kernels = dict (gauss_convolution = kf.Gaussian_Convolution, wangryzin_convolution = kf.WangRyzin_Convolution)
+kernel_func = dict(wangryzin=kf.WangRyzin, aitchisonaitken=kf.AitchisonAitken,
+                 epanechnikov=kf.Epanechnikov, gaussian=kf.Gaussian,
+                 gauss_convolution=kf.Gaussian_Convolution,
+                 wangryzin_convolution=kf.WangRyzin_Convolution,
+                 aitchisonaitken_convolution=kf.AitchisonAitken_Convolution)
+
+Convolution_Kernels = dict(gauss_convolution=kf.Gaussian_Convolution,
+                           wangryzin_convolution=kf.WangRyzin_Convolution)
+
+
+def IntegrateSingle(val, pdf):
+    f1 = lambda x: pdf(edat=np.asarray(x))
+    return quad(f1, -np.Inf, val)[0]
+
+
+def IntegrateDbl(val, pdf):
+    f2 = lambda x, y: pdf(edat=np.asarray([y, x]))
+    return dblquad(f2, -np.Inf, val[0], lambda x: -np.Inf, lambda x: val[1])[0]
+
 
 class LeaveOneOut(object):
     # Written by Skipper
@@ -38,14 +54,15 @@ class LeaveOneOut(object):
         N, K = np.shape(X)
 
         for i in xrange(N):
-            index = np.ones(N, dtype = np.bool)
+            index = np.ones(N, dtype=np.bool)
             index[i] = False
             yield X[index, :]
 
-def GPKE(bw, tdat, edat, var_type, ckertype = 'gaussian', okertype = 'wangryzin', ukertype = 'aitchisonaitken'):
+def GPKE(bw, tdat, edat, var_type, ckertype='gaussian',
+         okertype='wangryzin', ukertype='aitchisonaitken'):
     """
     Returns the non-normalized Generalized Product Kernel Estimator
-    
+
     Parameters
     ----------
     bw: array-like
@@ -62,46 +79,45 @@ def GPKE(bw, tdat, edat, var_type, ckertype = 'gaussian', okertype = 'wangryzin'
         The kernel used for the ordered discrete variables
     ukertype: str
         The kernel used for the unordered discrete variables
-        
     """
     var_type = np.asarray(list(var_type))
     iscontinuous = np.where(var_type == 'c')[0]
     isordered = np.where(var_type == 'o')[0]
     isunordered = np.where(var_type == 'u')[0]
-    
+
     if tdat.ndim > 1:
         N, K = np.shape(tdat)
     else:
         K = 1
         N = np.shape(tdat)[0]
         tdat = tdat.reshape([N, K])
-    
+
     if edat.ndim > 1:
         N_edat = np.shape(edat)[0]
     else:
         N_edat = 1
         edat = edat.reshape([N_edat, K])
-    
-    bw = np.reshape(np.asarray(bw), (K,))  #must remain 1-D for indexing to work
+
+    bw = np.reshape(np.asarray(bw), (K,))  # must remain 1-D for indexing to work
     dens = np.empty([N_edat, 1])
-       
+
     for i in xrange(N_edat):
-        
+
         Kval = np.concatenate((
         kernel_func[ckertype](bw[iscontinuous], tdat[:, iscontinuous], edat[i, iscontinuous]),
-        kernel_func[okertype](bw[isordered], tdat[:, isordered],edat[i, isordered]),
+        kernel_func[okertype](bw[isordered], tdat[:, isordered], edat[i, isordered]),
         kernel_func[ukertype](bw[isunordered], tdat[:, isunordered], edat[i, isunordered])
         ), axis=1)
-        
-        dens[i] = np.sum(np.prod(Kval, axis = 1))*1./(np.prod(bw[iscontinuous]))
+
+        dens[i] = np.sum(np.prod(Kval, axis=1)) * 1. / (np.prod(bw[iscontinuous]))
     return dens
 
 
-
-def GPKE2(bw, tdat, edat, var_type, ckertype = 'gaussian', okertype = 'wangryzin', ukertype = 'aitchisonaitken'):
+def GPKE2(bw, tdat, edat, var_type, ckertype='gaussian',
+          okertype='wangryzin', ukertype='aitchisonaitken'):
     """
     Returns the non-normalized Generalized Product Kernel Estimator
-    
+
     Parameters
     ----------
     bw: array-like
@@ -118,93 +134,84 @@ def GPKE2(bw, tdat, edat, var_type, ckertype = 'gaussian', okertype = 'wangryzin
         The kernel used for the ordered discrete variables
     ukertype: str
         The kernel used for the unordered discrete variables
-        
+
     """
     var_type = np.asarray(list(var_type))
     iscontinuous = np.where(var_type == 'c')[0]
     isordered = np.where(var_type == 'o')[0]
     isunordered = np.where(var_type == 'u')[0]
     K = len(var_type)
-    if tdat.ndim == 1 and K==1:  # one variable many observations
+    if tdat.ndim == 1 and K == 1:  # one variable many observations
         N = np.size(tdat)
         #N_edat = np.size(edat)
-    elif tdat.ndim == 1 and K>1:
+    elif tdat.ndim == 1 and K > 1:
         N = 1
-        
+
     else:
-        N, K = np.shape (tdat)
-        
+        N, K = np.shape(tdat)
     tdat = tdat.reshape([N, K])
-    
-        
-    if edat.ndim ==1 and K >1: # one obs many vars
+    if edat.ndim == 1 and K > 1:  # one obs many vars
         N_edat = 1
-    elif edat.ndim == 1 and K ==1: # one obs one var
+    elif edat.ndim == 1 and K == 1:  # one obs one var
         N_edat = np.size(edat)
-    
-    else: 
-        N_edat = np.shape(edat)[0] # ndim >1 so many obs many vars
+
+    else:
+        N_edat = np.shape(edat)[0]  # ndim >1 so many obs many vars
         assert np.shape(edat)[1] == K
-       
+
     edat = edat.reshape([N_edat, K])
-        
-    
-    bw = np.reshape(np.asarray(bw), (K,))  #must remain 1-D for indexing to work
-    #dens = np.empty([N, 1])
-       
+
+    bw = np.reshape(np.asarray(bw), (K, ))  # must remain 1-D for indexing to work
+    # dens = np.empty([N, 1])
     for i in xrange(N_edat):
-        
+
         Kval = np.concatenate((
         kernel_func[ckertype](bw[iscontinuous], tdat[:, iscontinuous], edat[i, iscontinuous]),
-        kernel_func[okertype](bw[isordered], tdat[:, isordered],edat[i, isordered]),
+        kernel_func[okertype](bw[isordered], tdat[:, isordered], edat[i, isordered]),
         kernel_func[ukertype](bw[isunordered], tdat[:, isunordered], edat[i, isunordered])
         ), axis=1)
-        
-        dens = np.prod(Kval, axis = 1)*1./(np.prod(bw[iscontinuous]))
+
+        dens = np.prod(Kval, axis=1) * 1. / (np.prod(bw[iscontinuous]))
     return dens
 
-def GPKE3(bw, tdat, edat, var_type, ckertype = 'gaussian', okertype = 'wangryzin', ukertype = 'aitchisonaitken'):
+
+def GPKE3(bw, tdat, edat, var_type, ckertype='gaussian',
+          okertype='wangryzin', ukertype='aitchisonaitken'):
+    # Rename !
 
     var_type = np.asarray(list(var_type))
     iscontinuous = np.where(var_type == 'c')[0]
     isordered = np.where(var_type == 'o')[0]
     isunordered = np.where(var_type == 'u')[0]
     K = len(var_type)
-    if tdat.ndim == 1 and K==1:  # one variable many observations
+    if tdat.ndim == 1 and K == 1:  # one variable many observations
         N = np.size(tdat)
         #N_edat = np.size(edat)
-    elif tdat.ndim == 1 and K>1:
+    elif tdat.ndim == 1 and K > 1:
         N = 1
-        
+
     else:
-        N, K = np.shape (tdat)
-        
+        N, K = np.shape(tdat)
+
     tdat = tdat.reshape([N, K])
-    
-        
-    if edat.ndim ==1 and K >1: # one obs many vars
+
+    if edat.ndim == 1 and K > 1:  # one obs many vars
         N_edat = 1
-    elif edat.ndim == 1 and K ==1: # one obs one var
+    elif edat.ndim == 1 and K == 1:  # one obs one var
         N_edat = np.size(edat)
-    
-    else: 
-        N_edat = np.shape(edat)[0] # ndim >1 so many obs many vars
+    else:
+        N_edat = np.shape(edat)[0]  # ndim >1 so many obs many vars
         assert np.shape(edat)[1] == K
-       
+
     edat = edat.reshape([N_edat, K])
-        
-    
-    bw = np.reshape(np.asarray(bw), (K,))  #must remain 1-D for indexing to work
-    #dens = np.empty([N, 1])
-       
-    
-        
+
+    bw = np.reshape(np.asarray(bw), (K, ))  # must remain 1-D for indexing to work
+    # dens = np.empty([N, 1])
     Kval = np.concatenate((
     kernel_func[ckertype](bw[iscontinuous], tdat[:, iscontinuous], edat[:, iscontinuous]),
-    kernel_func[okertype](bw[isordered], tdat[:, isordered],edat[:, isordered]),
+    kernel_func[okertype](bw[isordered], tdat[:, isordered], edat[:, isordered]),
     kernel_func[ukertype](bw[isunordered], tdat[:, isunordered], edat[:, isunordered])
     ), axis=1)
-    
-    dens = np.prod(Kval, axis = 1)*1./(np.prod(bw[iscontinuous]))
+
+    dens = np.prod(Kval, axis=1) * 1. / (np.prod(bw[iscontinuous]))
     return dens
- 
