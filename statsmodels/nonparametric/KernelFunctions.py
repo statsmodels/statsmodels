79850082ad7a9c567fccd46bbcb0b16c42a8a324
George Panterov <econgpanterov@gmail.com>
2012-06-27 14:45:49 -0400

    pep8 on KernelFunctions.py

--- statsmodels/nonparametric/KernelFunctions.py
+++ statsmodels/nonparametric/KernelFunctions.py
@@ -13,7 +13,8 @@
 
 import numpy as np
 
-def AitchisonAitken(h,Xi,x):
+
+def AitchisonAitken(h, Xi, x, num_levels=False):
     """
     Returns the value of the Aitchison and Aitken's (1976) Kernel. Used for
     unordered discrete random variable
@@ -28,40 +29,53 @@ def AitchisonAitken(h,Xi,x):
         The value at which the kernel density is being estimated
     c: 1D array of length K
         The number of possible values that each variable can take
-    
     Returns
     -------
     kernel_value : K-dimensional array
-        The value of the kernel function at each training point for each var
-        
+    The value of the kernel function at each training point for each var        
     References
     ----------
     Nonparametric econometrics : theory and practice / Qi Li and Jeffrey Scott Racine.
-    
     """
-    Xi=np.asarray(Xi)
-    N = np.shape(Xi)[0]
-    if Xi.ndim>1:
-        K=np.shape(Xi)[1]
-    else:
-        K=1
-
-    if K==0: return Xi
-    h = np.asarray(h, dtype = float)
-    c=np.asarray([len(np.unique(Xi[:,i])) for i in range(K)],dtype=int)
+##    Xi=np.asarray(Xi)
+##    N = np.shape(Xi)[0]
+##    if Xi.ndim>1:
+##        K=np.shape(Xi)[1]
+##    else:
+##        K=1
+##
+##    if K==0: return Xi
+##    h = np.asarray(h, dtype = float)
+    Xi = np.abs(np.asarray(Xi, dtype=int))
+    x = np.abs(np.asarray(x, dtype=int))
     
+    if Xi.ndim > 1:
+        K = np.shape(Xi)[1]
+        N = np.shape(Xi)[0]
+    elif Xi.ndim == 1:
+        K = 1
+        N = np.shape(Xi)[0]
+    else:  # ndim ==0 so Xi is a single point (number)
+        K = 1
+        N = 1    
+    if K == 0:
+        return Xi
+    
+    h = np.asarray(h, dtype=float)
+    Xi = Xi.reshape([N, K])
+    c = np.asarray([len(np.unique(Xi[:, i])) for i in range(K)],
+                   dtype=int)
+    if num_levels:
+        c = num_levels
     kernel_value = np.empty(Xi.shape)
-    kernel_value.fill(h/(c-1))
-    inDom=(Xi==x) *( 1-h)
-    kernel_value[Xi==x]=inDom[Xi==x]
-    kernel_value=kernel_value.reshape([N,K])
-    
-    
+    kernel_value.fill(h / (c - 1))
+    inDom = (Xi == x) * (1 - h)
+    kernel_value[Xi == x] = inDom[Xi == x]
+    kernel_value = kernel_value.reshape([N, K])
     return kernel_value
 
 
-
-def WangRyzin (h, Xi, x):
+def WangRyzin(h, Xi, x):    
     """
     Returns the value of the Wang and van Ryzin's (1981) Kernel. Used for
     ordered discrete random variable
@@ -74,7 +88,6 @@ def WangRyzin (h, Xi, x):
         The value of the training set
     x: 1D array of length K
         The value at which the kernel density is being estimated
-    
     Returns
     -------
     kernel_value : float
@@ -85,35 +98,29 @@ def WangRyzin (h, Xi, x):
     Nonparametric econometrics : theory and practice / Qi Li and Jeffrey Scott Racine.
     
     """
-    Xi = np.abs(np.asarray(Xi,dtype=int))
-    x = np.abs(np.asarray(x,dtype=int))
-    
-    if Xi.ndim>1:
-        K=np.shape(Xi)[1]
+    Xi = np.abs(np.asarray(Xi, dtype=int))
+    x = np.abs(np.asarray(x, dtype=int))
+    if Xi.ndim > 1:
+        K = np.shape(Xi)[1]
         N = np.shape(Xi)[0]
     elif Xi.ndim == 1:
-        K=1
+        K = 1
         N = np.shape(Xi)[0]
-    else: # ndim ==0 so Xi is a single point (number)
-        K=1
-        N=1
-    
-    if K==0: return Xi
-    
-    h = np.asarray(h,dtype=float)
-    
-    Xi=Xi.reshape([N,K])
-    
-    kernel_value=(0.5*(1-h)*(h**abs(Xi-x)))
-    kernel_value=kernel_value.reshape([N,K])
-    inDom=(Xi==x) *( 1-h)
-    
-    kernel_value[Xi==x]=inDom[Xi==x]
-    
+    else:  # ndim ==0 so Xi is a single point (number)
+        K = 1
+        N = 1
+    if K == 0:
+        return Xi
+    h = np.asarray(h, dtype=float)
+    Xi = Xi.reshape([N, K])
+    kernel_value = (0.5 * (1 - h) * (h ** abs(Xi - x)))
+    kernel_value = kernel_value.reshape([N, K])
+    inDom = (Xi == x) * (1 - h)
+    kernel_value[Xi == x] = inDom[Xi == x]
     return kernel_value
 
-def Epanechnikov (h, Xi, x):
 
+def Epanechnikov(h, Xi, x):
     """
     Returns the value of the Epanechnikov (1969) Kernel. Used for
     continuous random variables
@@ -122,8 +129,8 @@ def Epanechnikov (h, Xi, x):
     ----------
     h : K dimensional array
         The bandwidths used to estimate the value of the kernel function
-    Xi : K dimensional array 
-        The value of the training set
+    Xi : K dimensional array
+    The value of the training set
     x: 1D array of length K
         The value at which the kernel density is being estimated
     Returns
@@ -134,101 +141,123 @@ def Epanechnikov (h, Xi, x):
     References
     ----------
     Racine, J. "Nonparametric econometrics: A Primer" : Foundations and Trends in Econometrics.
-    
-    """
-    
-    Xi=np.asarray(Xi)
-    x=np.asarray(x)
-    h = np.asarray(h,dtype=float)
-    
+    """    
+    Xi = np.asarray(Xi)
+    x = np.asarray(x)
+    h = np.asarray(h, dtype=float)
     N = np.shape(Xi)[0]
-    if Xi.ndim>1:
-        K=np.shape(Xi)[1]
+    if Xi.ndim > 1:
+        K = np.shape(Xi)[1]
     else:
-        K=1
-    if K==0: return Xi
-    
-    z = (Xi - x)/h
+        K = 1
+    if K == 0:
+        return Xi
+    z = (Xi - x) / h
     InDom = (-np.sqrt(5) <= z) * (z <= np.sqrt(5))
-    kernel_value = InDom*(0.75*(1-0.2*z**2)*(1/np.sqrt(5)))
-    kernel_value=kernel_value.reshape([N,K])
-
-    # NOTE: There is a slight discrepancy between this formulation of the Epanechnikov kernel and
-    #       the one in kernels.py.
+    kernel_value = InDom * (0.75 * (1 - 0.2 * z ** 2) * (1 / np.sqrt(5)))
+    kernel_value = kernel_value.reshape([N, K])
+    # NOTE: There is a slight discrepancy between this
+    # formulation of the Epanechnikov kernel and
+    # the one in kernels.py.
     # TODO: Check Silverman and reconcile the discrepancy
-
     return kernel_value
 
-def Gaussian(h,Xi,x):
-    Xi=np.asarray(Xi)
-    x=np.asarray(x)
-    h = np.asarray(h,dtype=float)
-    
+
+def Gaussian(h, Xi, x):
+    Xi = np.asarray(Xi)
+    x = np.asarray(x)
+    h = np.asarray(h, dtype=float)
     N = np.shape(Xi)[0]
-    if Xi.ndim>1:
-        K=np.shape(Xi)[1]
+    if Xi.ndim > 1:
+        K = np.shape(Xi)[1]
     else:
-        K=1
-    if K==0: return Xi
-    z = (Xi - x)/h
-    kernel_value=(1./np.sqrt(2*np.pi))*np.exp(-z**2/2.)
-    kernel_value=kernel_value.reshape([N, K])
+        K = 1
+    if K == 0:
+        return Xi
+    z = (Xi - x) / h
+    kernel_value = (1. / np.sqrt(2 * np.pi)) * np.exp(- z ** 2 / 2.)
+    kernel_value = kernel_value.reshape([N, K])
     return kernel_value
 
 
-def Gaussian_Convolution(h,Xi,x):
+def Gaussian_Convolution(h, Xi, x):
     """
     Calculates the Gaussian Convolution Kernel
     """
-    
-    Xi=np.asarray(Xi)
-    x=np.asarray(x)
-    h = np.asarray(h,dtype=float)
-    
+    Xi = np.asarray(Xi)
+    x = np.asarray(x)
+    h = np.asarray(h, dtype=float)
     N = np.shape(Xi)[0]
-    if Xi.ndim>1:
-        K=np.shape(Xi)[1]
+    if Xi.ndim > 1:
+        K = np.shape(Xi)[1]
     else:
-        K=1
-    if K==0: return Xi
-    z = (Xi - x)/h
-    kernel_value=(1./np.sqrt(4*np.pi))*np.exp(-z**2/4.)
-    kernel_value=kernel_value.reshape([N,K])
+        K = 1
+    if K == 0:
+        return Xi
+    z = (Xi - x) / h
+    kernel_value = (1. / np.sqrt(4 * np.pi)) * np.exp(- z ** 2 / 4.)
+    kernel_value = kernel_value.reshape([N, K])
     return kernel_value
 
-def WangRyzin_Convolution (h, Xi, Xj):
+
+def WangRyzin_Convolution(h, Xi, Xj):
     # This is the equivalent of the convolution case with the Gaussian Kernel
     # However it is not exactly convolution. Think of a better name
     # References http://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=4&ved=0CGUQFjAD&url=http%3A%2F%2Feconweb.tamu.edu%2Fli%2FUncond1.pdf&ei=2THUT5i7IIOu8QSvmrXlAw&usg=AFQjCNH4aGzQbKT22sLBbZqHtPOyeFXNIQ
-    Xi = np.abs(np.asarray(Xi, dtype = int))
-    Xj = np.abs(np.asarray(Xj, dtype = int))
-    h = np.asarray(h,dtype=float)
-    
-    
+    Xi = np.abs(np.asarray(Xi, dtype=int))
+    Xj = np.abs(np.asarray(Xj, dtype=int))
+    h = np.asarray(h, dtype=float)
     if Xi.ndim > 1:
-        K=np.shape(Xi)[1]
+        K = np.shape(Xi)[1]
         N = np.shape(Xi)[0]
     elif Xi.ndim == 1:
-        K=1
+        K = 1
         N = np.shape(Xi)[0]
-    else: # ndim ==0 so Xi is a single point (number)
-        K=1
-        N=1
- 
-    
+    else:  # ndim ==0 so Xi is a single point (number)
+        K = 1
+        N = 1
     if K == 0:
         return Xi
     Xi = Xi.reshape([N, K])
     Xj = Xj.reshape((K, ))
     h = h.reshape((K, ))
-    Dom_x = [np.unique(Xi[:,i]) for i in range(K)]
-    
-    Ordered = np.empty([N,K])
+    Dom_x = [np.unique(Xi[:, i]) for i in range(K)]
+    Ordered = np.empty([N, K])
     for i in range(K):
         Sigma_x = 0
-        
+        # TODO: Think about vectorizing this for optimal performance
         for x in Dom_x[i]:
-            Sigma_x += WangRyzin (h[i], Xi[:, i], int(x))*WangRyzin(h[i], Xj[i], int(x))          
-        Ordered[:,i] = Sigma_x[:,0]
+            Sigma_x += WangRyzin(h[i], Xi[:, i],
+                                  int(x)) * WangRyzin(h[i], Xj[i], int(x))
+            Ordered[:, i] = Sigma_x[:, 0]
     return Ordered
-        
+
+
+def AitchisonAitken_Convolution(h, Xi, Xj):
+    Xi = np.abs(np.asarray(Xi, dtype=int))
+    Xj = np.abs(np.asarray(Xj, dtype=int))
+    h = np.asarray(h, dtype=float)
+    if Xi.ndim > 1:
+        K = np.shape(Xi)[1]
+        N = np.shape(Xi)[0]
+    elif Xi.ndim == 1:
+        K = 1
+        N = np.shape(Xi)[0]
+    else:  # ndim ==0 so Xi is a single point (number)
+        K = 1
+        N = 1   
+    if K == 0:
+        return Xi
+    Xi = Xi.reshape([N, K])
+    Xj = Xj.reshape((K, ))
+    h = h.reshape((K, ))
+    Dom_x = [np.unique(Xi[:, i]) for i in range(K)]
+    Ordered = np.empty([N, K])
+    for i in range(K):
+        Sigma_x = 0
+        # TODO: This can be vectorized
+        for x in Dom_x[i]:
+            Sigma_x += AitchisonAitken (h[i], Xi[:, i], int(x),
+                                        num_levels=len(Dom_x[i])) * AitchisonAitken(h[i], Xj[i], int(x), num_levels=len(Dom_x[i]))       
+        Ordered[:, i] = Sigma_x[:, 0]
+    return Ordered
