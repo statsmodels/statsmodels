f5a07f1436e1913d89318d53d8276918d8bff479
Nathaniel J. Smith <njs@pobox.com>
2012-07-11 19:42:01 -0400

    DOC: Fix js again for nested quotes etc.

--- docs/source/_static/scripts.js
+++ docs/source/_static/scripts.js
@@ -14,9 +14,12 @@ function cleanUpText(codebox){
 return codebox;
 }
 
-function cleanQuotes(text){
-    /// Replace all single and double quotes with one escaped backslash
-    return text.replace(/(["'])/g, "\\\1");
+function htmlescape(text){
+    return (text.replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&#39;"))
 }
 
 function scrapeText(codebox){
@@ -25,10 +28,10 @@ function scrapeText(codebox){
     var newlines = new Array();
     $.each(lines, function() {
         if (this.match(/^In \[\d+]: /)){
-            newlines.push(cleanQuotes(this.replace(/^(\s)*In \[\d+]: /,"")));
+            newlines.push(this.replace(/^(\s)*In \[\d+]: /,""));
         }
         else if (this.match(/^(\s)*\.+:/)){
-            newlines.push(cleanQuotes(this.replace(/^(\s)*\.+: /,"")));
+            newlines.push(this.replace(/^(\s)*\.+: /,""));
         }
 
     }
@@ -41,10 +44,9 @@ $(document).ready(
     // grab all code boxes
     var ipythoncode = $(".highlight-ipython");
     $.each(ipythoncode, function() {
-        //var codebox = cleanUpText($(this).text());
         var codebox = scrapeText($(this).text());
         // give them a facebox pop-up with plain text code   
-        $(this).append('<span style="text-align:left; display:block; margin-top:-10px; margin-left:10px; font-size:75%"><a href="javascript: jQuery.facebox(\'<textarea cols=80 rows=10 readonly style=margin:5px onmouseover=javascript:this.select();>'+codebox+'</textarea>\');">View Code</a></span>');
+        $(this).append('<span style="text-align:left; display:block; margin-top:-10px; margin-left:10px; font-size:75%"><a href="javascript: jQuery.facebox(\'<textarea cols=80 rows=10 readonly style=margin:5px onmouseover=javascript:this.select();>'+htmlescape(htmlescape(codebox))+'</textarea>\');">View Code</a></span>');
         $(this,"textarea").select();
     });
 });
