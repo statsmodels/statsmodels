616d95667b7f46062e34d3ff4e28b65ab0e49c03
George Panterov <econgpanterov@gmail.com>
2012-07-12 00:29:14 -0400

    made test_nonparametric2.py pep 8 compliant

--- statsmodels/nonparametric/tests/test_nonparametric2.py
+++ statsmodels/nonparametric/tests/test_nonparametric2.py
@@ -3,6 +3,9 @@ import numpy.testing as npt
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 import statsmodels.nonparametric as nparam
+#import nonparametric2 as nparam
+#reload(nparam)
+
 
 class MyTest(object):
     def setUp(self):
@@ -13,43 +16,50 @@ class MyTest(object):
         self.c1 = np.random.normal(size=(N, 1))
         self.c2 = np.random.normal(10, 1, size=(N, 1))
         self.c3 = np.random.normal(10, 2, size=(N, 1))
-        self.noise = np.random.normal(size=(N,1))
-        b0 = 0.3; b1 =  1.2; b2 = 3.7 # regression coefficients
+        self.noise = np.random.normal(size=(N, 1))
+        b0 = 0.3
+        b1 = 1.2
+        b2 = 3.7  # regression coefficients
         self.y = b0 + b1 * self.c1 + b2 * self.c2 + self.noise
 
         # Italy data from R's np package (the first 50 obs) R>> data (Italy)
 
-        self.Italy_gdp = [  8.556,  12.262,   9.587,   8.119,   5.537,   6.796,   8.638,
-         6.483,   6.212,   5.111,   6.001,   7.027,   4.616,   3.922,
-         4.688,   3.957,   3.159,   3.763,   3.829,   5.242,   6.275,
-         8.518,  11.542,   9.348,   8.02 ,   5.527,   6.865,   8.666,
-         6.672,   6.289,   5.286,   6.271,   7.94 ,   4.72 ,   4.357,
-         4.672,   3.883,   3.065,   3.489,   3.635,   5.443,   6.302,
-         9.054,  12.485,   9.896,   8.33 ,   6.161,   7.055,   8.717,
-         6.95 ]
+        self.Italy_gdp = \
+        [8.556, 12.262, 9.587, 8.119, 5.537, 6.796, 8.638,
+         6.483, 6.212, 5.111, 6.001, 7.027, 4.616, 3.922,
+         4.688, 3.957, 3.159, 3.763, 3.829, 5.242, 6.275,
+         8.518, 11.542, 9.348, 8.02, 5.527, 6.865, 8.666,
+         6.672, 6.289, 5.286, 6.271, 7.94, 4.72, 4.357,
+         4.672, 3.883, 3.065, 3.489, 3.635, 5.443, 6.302,
+         9.054, 12.485, 9.896, 8.33, 6.161, 7.055, 8.717,
+         6.95]
 
-        self.Italy_year = [1951, 1951, 1951, 1951, 1951, 1951, 1951, 1951, 1951, 1951, 1951,
+        self.Italy_year = \
+        [1951, 1951, 1951, 1951, 1951, 1951, 1951, 1951, 1951, 1951, 1951,
        1951, 1951, 1951, 1951, 1951, 1951, 1951, 1951, 1951, 1951, 1952,
        1952, 1952, 1952, 1952, 1952, 1952, 1952, 1952, 1952, 1952, 1952,
        1952, 1952, 1952, 1952, 1952, 1952, 1952, 1952, 1952, 1953, 1953,
        1953, 1953, 1953, 1953, 1953, 1953]
-        
-        # OECD panel data from NP  R>> data(oecdpanel)
-        self.growth = [-0.0017584 ,  0.00740688,  0.03424461,  0.03848719,  0.02932506,
-        0.03769199,  0.0466038 ,  0.00199456,  0.03679607,  0.01917304,
-       -0.00221   ,  0.00787269,  0.03441118, -0.0109228 ,  0.02043064,
-       -0.0307962 ,  0.02008947,  0.00580313,  0.00344502,  0.04706358,
-        0.03585851,  0.01464953,  0.04525762,  0.04109222, -0.0087903 ,
-        0.04087915,  0.04551403,  0.036916  ,  0.00369293,  0.0718669 ,
-        0.02577732, -0.0130759 , -0.01656641,  0.00676429,  0.08833017,
-        0.05092105,  0.02005877,  0.00183858,  0.03903173,  0.05832116,
-        0.0494571 ,  0.02078484,  0.09213897,  0.0070534 ,  0.08677202,
-        0.06830603, -0.00041   ,  0.0002856 ,  0.03421225, -0.0036825 ]
 
-        self.oecd = [0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+        # OECD panel data from NP  R>> data(oecdpanel)
+        self.growth = \
+        [-0.0017584, 0.00740688, 0.03424461, 0.03848719, 0.02932506,
+        0.03769199, 0.0466038,  0.00199456, 0.03679607, 0.01917304,
+       -0.00221, 0.00787269, 0.03441118, -0.0109228, 0.02043064,
+       -0.0307962, 0.02008947, 0.00580313, 0.00344502, 0.04706358,
+        0.03585851, 0.01464953, 0.04525762, 0.04109222, -0.0087903,
+        0.04087915, 0.04551403, 0.036916, 0.00369293, 0.0718669,
+        0.02577732, -0.0130759, -0.01656641, 0.00676429, 0.08833017,
+        0.05092105, 0.02005877,  0.00183858, 0.03903173, 0.05832116,
+        0.0494571, 0.02078484,  0.09213897, 0.0070534, 0.08677202,
+        0.06830603, -0.00041, 0.0002856, 0.03421225, -0.0036825]
+
+        self.oecd = \
+        [0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
        0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0,
        0, 0, 0, 0]
-        
+
+
 class TestUKDE(MyTest):
 
     def test_pdf_mixeddata_CV_LS(self):
@@ -57,7 +67,6 @@ class TestUKDE(MyTest):
                              bw='cv_ls')
         npt.assert_allclose(dens_u.bw, [0.709195, 0.087333, 0.092500],
                             atol=1e-6)
-        
 
         # Matches R to 3 decimals; results seem more stable than with R.
         # Can be checked with following code:
@@ -83,121 +92,129 @@ class TestUKDE(MyTest):
         # Test ML cross-validation
         dens_ml = nparam.UKDE(tdat=[self.c1, self.o, self.c2], var_type='coc',
                              bw='cv_ml')
-        
+
     def test_pdf_continuous(self):
         # Test for only continuous data
-        
-        dens = nparam.UKDE (tdat = [self.growth, self.Italy_gdp], var_type = 'cc',
-                            bw = 'cv_ls')
-        sm_result = np.squeeze(dens.pdf()[0:5])  # take the first data points from the training set
+        dens = nparam.UKDE(tdat=[self.growth, self.Italy_gdp],
+                            var_type='cc', bw='cv_ls')
+        # take the first data points from the training set
+        sm_result = np.squeeze(dens.pdf()[0:5])
         R_result = [1.6202284, 0.7914245, 1.6084174, 2.4987204, 1.3705258]
 
         ## CODE TO REPRODUCE THE RESULTS IN R
         ## library(np)
         ## data(oecdpanel)
         ## data (Italy)
-        ## bw <-npudensbw(formula = ~oecdpanel$growth[1:50] + Italy$gdp[1:50], bwmethod ='cv.ls')
+        ## bw <-npudensbw(formula = ~oecdpanel$growth[1:50] + Italy$gdp[1:50],
+        ## bwmethod ='cv.ls')
         ## fhat <- fitted(npudens(bws=bw))
         ## fhat[1:5]
-        
-        npt.assert_allclose(sm_result, R_result, atol = 1e-3)
-        
-        
+        npt.assert_allclose(sm_result, R_result, atol=1e-3)
+
     def test_pdf_ordered(self):
         # Test for only ordered data
-        dens = nparam.UKDE (tdat = [self.oecd], var_type ='o', bw = 'cv_ls')
+        dens = nparam.UKDE(tdat=[self.oecd], var_type='o', bw='cv_ls')
         sm_result = np.squeeze(dens.pdf()[0:5])
         R_result = [0.7236395, 0.7236395, 0.2763605, 0.2763605, 0.7236395]
-        npt.assert_allclose(sm_result, R_result, atol = 1e-1)  # lower tol here. only 2nd decimal
-        
-        
-    def test_unordered_CV_LS(self):
-        dens = nparam.UKDE(tdat=[self.growth, self.oecd], var_type = 'cu', bw ='cv_ls')
-        R_result = [0.0052051, 0.05835941]
-        npt.assert_allclose(dens.bw, R_result, atol = 1e-2)
-        
+        # lower tol here. only 2nd decimal
+        npt.assert_allclose(sm_result, R_result, atol=1e-1)
 
-    def test_continuous_cdf(self,edat=None):
-        dens = nparam.UKDE (tdat = [self.Italy_gdp, self.growth], var_type = 'cc',
-                            bw = 'cv_ml')
+    def test_unordered_CV_LS(self):
+        dens = nparam.UKDE(tdat=[self.growth, self.oecd],
+                           var_type='cu', bw='cv_ls')
+        R_result = [0.0052051, 0.05835941]
+        npt.assert_allclose(dens.bw, R_result, atol=1e-2)
+
+    def test_continuous_cdf(self, edat=None):
+        dens = nparam.UKDE(tdat=[self.Italy_gdp, self.growth],
+                            var_type='cc', bw='cv_ml')
         sm_result = dens.cdf()[0:5]
-        R_result = [0.192180770,0.299505196,0.557303666,
+        R_result = [0.192180770, 0.299505196, 0.557303666,
                     0.513387712, 0.210985350]
-        npt.assert_allclose(sm_result, R_result, atol = 1e-3)
+        npt.assert_allclose(sm_result, R_result, atol=1e-3)
+
     def test_mixeddata_cdf(self, edat=None):
-        dens = nparam.UKDE(tdat = [self.Italy_gdp, self.oecd], var_type='cu',
+        dens = nparam.UKDE(tdat=[self.Italy_gdp, self.oecd], var_type='cu',
                            bw='cv_ml')
         sm_result = dens.cdf()[0:5]
-        R_result =  [0.54700010, 0.65907039, 0.89676865, 0.74132941, 0.25291361]
-        npt.assert_allclose(sm_result, R_result, atol = 1e-3)
-         
+        R_result = [0.54700010, 0.65907039, 0.89676865, 0.74132941, 0.25291361]
+        npt.assert_allclose(sm_result, R_result, atol=1e-3)
+
 
 class TestCKDE(MyTest):
-    def test_mixeddata_CV_LS (self):
-        dens_ls = nparam.CKDE(tydat=[self.Italy_gdp],txdat=[self.Italy_year],
-                                           dep_type='c',indep_type='o',bw='cv_ls')
-        npt.assert_allclose(dens_ls.bw, [1.6448, 0.2317373], atol=1e-3)  # Values from the estimation in R with the same data
-        
-    def test_continuous_CV_ML (self):
 
-        dens_ml = nparam.CKDE (tydat=[self.Italy_gdp],txdat=[self.growth],
-                                           dep_type='c',indep_type='c',bw='cv_ml')
-        npt.assert_allclose(dens_ml.bw, [0.5341164,0.04510836], atol = 1e-3) # Results from R
+    def test_mixeddata_CV_LS(self):
+        dens_ls = nparam.CKDE(tydat=[self.Italy_gdp],
+                              txdat=[self.Italy_year],
+                              dep_type='c', indep_type='o', bw='cv_ls')
+        # Values from the estimation in R with the same data
+        npt.assert_allclose(dens_ls.bw, [1.6448, 0.2317373], atol=1e-3)
 
-    def test_unordered_CV_LS (self):
-        dens_ls = nparam.CKDE(tydat=[self.oecd],txdat=[self.growth],
-                                           dep_type='u',indep_type='c',bw='cv_ls')
+    def test_continuous_CV_ML(self):
+        dens_ml = nparam.CKDE(tydat=[self.Italy_gdp],
+                               txdat=[self.growth], dep_type='c',
+                               indep_type='c', bw='cv_ml')
+        # Results from R
+        npt.assert_allclose(dens_ml.bw, [0.5341164, 0.04510836], atol=1e-3)
+
+    def test_unordered_CV_LS(self):
+        dens_ls = nparam.CKDE(tydat=[self.oecd],
+                              txdat=[self.growth],
+                              dep_type='u', indep_type='c', bw='cv_ls')
         print dens_ls.bw
         print "Test completed"
-        
-        
 
     def test_pdf_continuous(self):
-        dens = nparam.CKDE (tydat = [self.growth], txdat = [self.Italy_gdp],
-                            dep_type ='c', indep_type ='c', bw='cv_ml')
+        dens = nparam.CKDE(tydat=[self.growth], txdat=[self.Italy_gdp],
+                            dep_type='c', indep_type='c', bw='cv_ml')
         sm_result = np.squeeze(dens.pdf()[0:5])
         R_result = [11.97964, 12.73290, 13.23037, 13.46438, 12.22779]
-        npt.assert_allclose(sm_result, R_result, atol = 1e-3)
-        
-    
+        npt.assert_allclose(sm_result, R_result, atol=1e-3)
+
     def test_pdf_mixeddata(self):
-        dens = nparam.CKDE(tydat=[self.Italy_gdp],txdat=[self.Italy_year],
-                                           dep_type='c',indep_type='o',bw='cv_ls')
+        dens = nparam.CKDE(tydat=[self.Italy_gdp],
+                           txdat=[self.Italy_year], dep_type='c',
+                           indep_type='o', bw='cv_ls')
         sm_result = np.squeeze(dens.pdf()[0:5])
-        
         R_result = [0.08469226, 0.01737731, 0.05679909, 0.09744726, 0.15086674]
 
         ## CODE TO REPRODUCE IN R
         ## library(np)
         ## data (Italy)
-        ## bw <- npcdensbw(formula = Italy$gdp[1:50]~ordered(Italy$year[1:50]),bwmethod='cv.ls')
+        ## bw <- npcdensbw(formula =
+        ## Italy$gdp[1:50]~ordered(Italy$year[1:50]),bwmethod='cv.ls')
         ## fhat <- fitted(npcdens(bws=bw))
         ## fhat[1:5]
-        npt.assert_allclose(sm_result, R_result, atol = 1e-3)
+        npt.assert_allclose(sm_result, R_result, atol=1e-3)
 
     def test_continuous_normal_ref(self):
         # test for normal reference rule of thumb with continuous data
-        
-        dens_nm = nparam.CKDE(tydat=[self.Italy_gdp],txdat=[self.growth],
-                                           dep_type='c',indep_type='c',bw='normal_reference')
+        dens_nm = nparam.CKDE(tydat=[self.Italy_gdp],
+                              txdat=[self.growth], dep_type='c',
+                              indep_type='c', bw='normal_reference')
         sm_result = dens_nm.bw
-        R_result = [1.283532,0.01535401]
-        npt.assert_allclose(sm_result, R_result, atol = 1e-1)  # Here we need a smaller tolerance.check!
+        R_result = [1.283532, 0.01535401]
+        # Here we need a smaller tolerance.check!
+        npt.assert_allclose(sm_result, R_result, atol=1e-1)
 
     def test_continuous_cdf(self):
         dens_nm = nparam.CKDE(tydat=[self.Italy_gdp],
-                              txdat=[self.growth], dep_type='c',indep_type='c',bw='normal_reference')
+                              txdat=[self.growth],
+                              dep_type='c',
+                              indep_type='c', bw='normal_reference')
         sm_result = dens_nm.cdf()[0:5]
         R_result = [0.81304920, 0.95046942, 0.86878727, 0.71961748, 0.38685423]
-        npt.assert_allclose(sm_result, R_result, atol = 1e-3)
-        
+        npt.assert_allclose(sm_result, R_result, atol=1e-3)
+
     def test_mixeddata_cdf(self):
-        dens = nparam.CKDE(tydat=[self.Italy_gdp],txdat=[self.Italy_year],
-                                           dep_type='c',indep_type='o',bw='cv_ls')
+        dens = nparam.CKDE(tydat=[self.Italy_gdp],
+                           txdat=[self.Italy_year],
+                           dep_type='c', indep_type='o', bw='cv_ls')
         sm_result = dens.cdf()[0:5]
         R_result = [0.8118257, 0.9724863, 0.8843773, 0.7720359, 0.4361867]
-        npt.assert_allclose(sm_result, R_result, atol = 1e-3)
-        
+        npt.assert_allclose(sm_result, R_result, atol=1e-3)
+
+
 class TestReg(MyTest):
     def test_ordered_CVLC(self):
         model = nparam.Reg(tydat=[self.Italy_gdp], txdat=[self.Italy_year],
@@ -210,18 +227,18 @@ class TestReg(MyTest):
 
         sm_R2 = model.R2()
         R_R2 = 0.1435323
-        
-        
+
         ## CODE TO REPRODUCE IN R
         ## library(np)
         ## data(Italy)
         ## attach(Italy)
         ## bw <- npregbw(formula=gdp[1:50]~ordered(year[1:50]))
-        npt.assert_allclose(sm_bw, R_bw, atol = 1e-2)
-        npt.assert_allclose(sm_mean, R_mean, atol = 1e-2)
-        npt.assert_allclose(sm_R2, R_R2, atol = 1e-2)
+        npt.assert_allclose(sm_bw, R_bw, atol=1e-2)
+        npt.assert_allclose(sm_mean, R_mean, atol=1e-2)
+        npt.assert_allclose(sm_R2, R_R2, atol=1e-2)
+
     def test_continuousdata_CVLC(self):
-        model = nparam.Reg(tydat=[self.y], txdat=[self.c1,self.c2],
+        model = nparam.Reg(tydat=[self.y], txdat=[self.c1, self.c2],
                            var_type='cc', bw='cv_lc')
         # Bandwidth
         sm_bw = model.bw
@@ -232,9 +249,7 @@ class TestReg(MyTest):
         # R-Squared
         sm_R2 = model.R2()
         R_R2 = 0.956381720885
-        
-        npt.assert_allclose(sm_bw, R_bw, atol = 1e-2)
-        npt.assert_allclose(sm_mean, R_mean, atol = 1e-2)
-        npt.assert_allclose(sm_R2, R_R2, atol = 1e-2)
-        
-    
+
+        npt.assert_allclose(sm_bw, R_bw, atol=1e-2)
+        npt.assert_allclose(sm_mean, R_mean, atol=1e-2)
+        npt.assert_allclose(sm_R2, R_R2, atol=1e-2)
