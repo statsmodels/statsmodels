ee2cd70e32be704f40fa47cddb021d964c44263d
George Panterov <econgpanterov@gmail.com>
2012-07-11 23:36:51 -0400

    made np_tools.py pep 8 compliant

--- statsmodels/nonparametric/np_tools.py
+++ statsmodels/nonparametric/np_tools.py
@@ -5,12 +5,14 @@ from scipy.integrate import *
 
 
 kernel_func = dict(wangryzin=kf.WangRyzin, aitchisonaitken=kf.AitchisonAitken,
-                 gaussian=kf.Gaussian, gauss_convolution=kf.Gaussian_Convolution,
+                 gaussian=kf.Gaussian,
+                   gauss_convolution=kf.Gaussian_Convolution,
                  wangryzin_convolution=kf.WangRyzin_Convolution,
                  aitchisonaitken_convolution=kf.AitchisonAitken_Convolution,
                    gaussian_cdf=kf.Gaussian_cdf,
                    aitchisonaitken_cdf=kf.AitchisonAitken_cdf,
                    wangryzin_cdf=kf.WangRyzin_cdf)
+
 
 class LeaveOneOut(object):
     # Written by Skipper
@@ -45,13 +47,15 @@ class LeaveOneOut(object):
             index = np.ones(N, dtype=np.bool)
             index[i] = False
             yield X[index, :]
-            
+
+
 def _get_type_pos(var_type):
     var_type = np.asarray(list(var_type))
     iscontinuous = np.where(var_type == 'c')[0]
     isordered = np.where(var_type == 'o')[0]
     isunordered = np.where(var_type == 'u')[0]
     return iscontinuous, isordered, isunordered
+
 
 def _get_shape(tdat, edat, var_type):
     var_type = np.asarray(list(var_type))
@@ -69,15 +73,14 @@ def _get_shape(tdat, edat, var_type):
     elif edat.ndim == 1 and K == 1:  # one obs one var
         N_edat = np.size(edat)
     else:
-        if np.shape(edat)[0]==K:
+        if np.shape(edat)[0] == K:
             N_edat = np.shape(edat)[1]
         else:
             N_edat = np.shape(edat)[0]  # ndim >1 so many obs many vars
         assert np.shape(edat)[1] == K
     return K, N, N_edat
-    
 
-    
+
 def GPKE(bw, tdat, edat, var_type, ckertype='gaussian',
          okertype='wangryzin', ukertype='aitchisonaitken', tosum=True):
     """
@@ -90,7 +93,8 @@ def GPKE(bw, tdat, edat, var_type, ckertype='gaussian',
     tdat: 1D or 2d array
         The training data
     edat: 1d array
-        The evaluation points at which the kernel estimation is performed
+        The evaluation points at which the kernel
+        estimation is performed
     var_type: str
         The variable type (continuous, ordered, unordered)
     ckertype: str
@@ -110,12 +114,16 @@ def GPKE(bw, tdat, edat, var_type, ckertype='gaussian',
 
     The formula for the multivariate kernel estimator for the pdf is:
 
-    .. math:: f(x)=\frac{1}{nh_{1}...h_{q}}\sum_{i=1}^{n}K\left(\frac{X_{i}-x}{h}\right)
+    .. math:: f(x)=\frac{1}{nh_{1}...h_{q}}\sum_{i=1}^
+    {n}K\left(\frac{X_{i}-x}{h}\right)
 
     where
-    
-    .. math:: K\left(\frac{X_{i}-x}{h}\right)=k\left(\frac{X_{i1}-x_{1}}{h_{1}}\right)\times k\left(\frac{X_{i2}-x_{2}}{h_{2}}\right)\times...\times k\left(\frac{X_{iq}-x_{q}}{h_{q}}\right)
-    
+
+    .. math:: K\left(\frac{X_{i}-x}{h}\right)=k\left(
+    \frac{X_{i1}-x_{1}}{h_{1}}\right)\times k\left(
+    \frac{X_{i2}-x_{2}}{h_{2}}\right)\times...\times
+    k\left(\frac{X_{iq}-x_{q}}{h_{q}}\right)
+
     """
     iscontinuous, isordered, isunordered = _get_type_pos(var_type)
     tdat = np.asarray(tdat)
@@ -125,23 +133,26 @@ def GPKE(bw, tdat, edat, var_type, ckertype='gaussian',
 
     tdat = tdat.reshape([N, K])
     edat = edat.reshape([N_edat, K])
-
-    bw = np.reshape(np.asarray(bw), (K,))  # must remain 1-D for indexing to work
+    # must remain 1-D for indexing to work
+    bw = np.reshape(np.asarray(bw), (K,))
     dens = np.empty([N, N_edat])
 
     for i in xrange(N_edat):
 
         Kval = np.concatenate((
-        kernel_func[ckertype](bw[iscontinuous], tdat[:, iscontinuous], edat[i, iscontinuous]),
-        kernel_func[okertype](bw[isordered], tdat[:, isordered], edat[i, isordered]),
-        kernel_func[ukertype](bw[isunordered], tdat[:, isunordered], edat[i, isunordered])
-        ), axis=1)
+        kernel_func[ckertype](bw[iscontinuous],
+                              tdat[:, iscontinuous], edat[i, iscontinuous]),
+        kernel_func[okertype](bw[isordered], tdat[:, isordered],
+                              edat[i, isordered]),
+        kernel_func[ukertype](bw[isunordered], tdat[:, isunordered],
+                              edat[i, isunordered])), axis=1)
 
-        dens[:,i] = np.prod(Kval, axis=1) * 1. / (np.prod(bw[iscontinuous]))
+        dens[:, i] = np.prod(Kval, axis=1) * 1. / (np.prod(bw[iscontinuous]))
     if tosum:
         return np.sum(dens, axis=0)
     else:
         return dens
+
 
 def PKE(bw, tdat, edat, var_type, ckertype='gaussian',
           okertype='wangryzin', ukertype='aitchisonaitken'):
@@ -186,15 +197,16 @@ def PKE(bw, tdat, edat, var_type, ckertype='gaussian',
 
     tdat = tdat.reshape([N, K])
     edat = edat.reshape([N_edat, K])
-
-    bw = np.reshape(np.asarray(bw), (K,))  # must remain 1-D for indexing to work
+    # must remain 1-D for indexing to work
+    bw = np.reshape(np.asarray(bw), (K,))
 
     Kval = np.concatenate((
-    kernel_func[ckertype](bw[iscontinuous], tdat[:, iscontinuous], edat[:, iscontinuous]),
-    kernel_func[okertype](bw[isordered], tdat[:, isordered], edat[:, isordered]),
-    kernel_func[ukertype](bw[isunordered], tdat[:, isunordered], edat[:, isunordered])
-    ), axis=1)
+    kernel_func[ckertype](bw[iscontinuous], tdat[:, iscontinuous],
+                          edat[:, iscontinuous]),
+    kernel_func[okertype](bw[isordered], tdat[:, isordered],
+                          edat[:, isordered]),
+    kernel_func[ukertype](bw[isunordered], tdat[:, isunordered],
+                          edat[:, isunordered])), axis=1)
 
     dens = np.prod(Kval, axis=1) * 1. / (np.prod(bw[iscontinuous]))
     return dens
-
