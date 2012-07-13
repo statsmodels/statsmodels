2a72d5ff38573dc46d4fd1ae42b910e389fa2b00
George Panterov <econgpanterov@gmail.com>
2012-07-12 22:40:40 -0400

    small fix to adjust_shape

--- statsmodels/nonparametric/np_tools.py
+++ statsmodels/nonparametric/np_tools.py
@@ -68,6 +68,8 @@ def adjust_shape(dat, K):
     elif dat.ndim == 1 and K == 1:  # one obs one var
         N = len(dat)
     else:
+        if np.shape(dat)[0] == K and np.shape(dat)[1] != K:
+            dat = dat.T
         N = np.shape(dat)[0]  # ndim >1 so many obs many vars
         assert np.shape(dat)[1] == K
     dat = np.reshape(dat, (N, K))
