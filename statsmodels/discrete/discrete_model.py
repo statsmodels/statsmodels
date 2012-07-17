746c5ed69ef6861c9b43043471bf8ada9e43c0ab
langmore <ianlangmore@gmail.com>
2012-07-17 18:59:01 -0400

    Did not use -a in last commit

--- statsmodels/discrete/discrete_model.py
+++ statsmodels/discrete/discrete_model.py
@@ -1068,6 +1068,9 @@ class MNLogit(MultinomialModel):
     -----
     See developer notes for further information on `MNLogit` internals.
     """
+    def fit_L1(self):
+        print "Fitting"
+
     def pdf(self, eXB):
         """
         NotImplemented
