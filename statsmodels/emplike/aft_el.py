578633d9ac2de096b9dc491e46f48a7d84c46fe9
Justin Grana <jg3705a@student.american.edu>
2012-07-26 13:52:34 -0400

    Added Documentation References and cleaned functions

--- statsmodels/emplike/aft_el.py
+++ statsmodels/emplike/aft_el.py
@@ -4,23 +4,33 @@ Accelerated Failure Time (AFT) Model with empirical likelihood inference.
 
 AFT regression analysis is applicable when the researcher has access
 to a randomly right censored dependent variable, a matrix of exogenous
-variables and an indicatior variable (delta) that takes a value fo 0 if the
+variables and an indicatior variable (delta) that takes a value of 0 if the
 observation is censored and 1 otherwise.
 
 AFT References
 --------------
 
-Koul et. al
+Koul, Susarla and Ryzin. (1981).  "Regression Analysis with Randomly
+Right-Censored Data." Ann. Statistics. Vol. 9. N. 6. 1276-1288
 
-Stute
+
+Stute, W. (1993). "Consistent Estimation Under Random Censorship when
+Covariables are Present." Journal of Multivariate Analysis.
+Vol. 45. Iss. 1. 89-103
 
 EL and AFT References
 ---------------------
 
-Zhou
+Zhou, Kim And Bathke. "Empirical Likelihood Analysis for the Heteroskedastic
+Accelerated Failure Time Model." Manuscript:
+URL: www.ms.uky.edu/~mai/research/CasewiseEL20080724.pdf
 
-Qin
 
+Li, G and Wang, Q.(2003). "Empirical Likelihood Regression Analysis for Right
+Censored Data." Statistica Sinica 13. 51-68.
+
+Qin, G and Jing, B. (2001). "Empirical Likelihood for Censored Linear
+Regression." Scandanavian Journal of Statistics. Vol 28. Iss. 4. 661-673.
 """
 
 import numpy as np
@@ -56,15 +66,31 @@ class emplikeAFT(object):
             ties[i]=1 if endog[i]==endog[i+1] and
             censors[i]=censors[i+1]
         """
+        nobs = int(self.nobs)
+        endog_idx = endog[np.arange(nobs - 1)] == (
+            endog[np.arange(nobs - 1) + 1])
+        censors_idx = censors[np.arange(nobs - 1)] == (
+            censors[np.arange(nobs - 1) + 1])
+        indic_ties = endog_idx * censors_idx  # Both true
+        return np.int_(indic_ties)
 
-        indic_ties = np.zeros(int(self.nobs))
-        for obs_num in range(int(self.nobs - 1))[::-1]:
-            if self.endog[obs_num] == self.endog[obs_num + 1] and\
-               self.censors[obs_num] == self.censors[obs_num + 1]:
-                indic_ties[obs_num] = 1
-        return indic_ties
+    def _km_w_ties(self, tie_indic, untied_km):
+        """
+        Computes KM estimator value at each observation, taking into acocunt
+        ties in the data.
 
-    def km_w_ties(self, tie_indic, untied_km):
+        Parameters:
+        ----------
+
+        tie_indic: 1d array
+            Indicates if the i'th observation is the same as the ith +1
+
+        untied_km: 1d array
+            Km estimates at each observation assuming no ties.
+
+        """
+        # TODO: Vectorize, even though it is only 1 pass through for any
+        # function call
         num_same = 1
         idx_nums = []
         for obs_num in np.arange(int(self.nobs - 1))[::-1]:
@@ -78,7 +104,6 @@ class emplikeAFT(object):
                 untied_km[idx_nums] = untied_km[idx_nums]
                 num_same = 1
                 idx_nums = []
-
         return untied_km.reshape(self.nobs, 1)
 
     def _make_km(self, endog, censors):
@@ -100,20 +125,27 @@ class emplikeAFT(object):
 
         Kaplan Meier estimate for each observation
 
-        """
+        Notes
+        -----
 
+        This function makes calls to _is_tied and km_w_ties to handle ties in
+        the data.If a censored observation and an uncensored observation has
+        the same value, it is assumed that the uncensored happened first.
+
+        """
+        nobs = self.nobs
         if self.km_est == 'koul':
-            num = (self.nobs - (np.arange(self.nobs) + 1.) + 1.)
-            denom = ((self.nobs - (np.arange(self.nobs) + 1.) + 2.))
-        if self.km_est == 'qin':
-            num = (self.nobs - (np.arange(self.nobs) + 1.))
-            denom = ((self.nobs - (np.arange(self.nobs) + 1.) + 1.))
-        km = (num / denom).reshape(self.nobs, 1)
+            num = (nobs - (np.arange(nobs) + 1.) + 1.)
+            denom = ((nobs - (np.arange(nobs) + 1.) + 2.))
+        if self.km_est == 'li':
+            num = (nobs - (np.arange(nobs) + 1.))
+            denom = ((nobs - (np.arange(nobs) + 1.) + 1.))
+        km = (num / denom).reshape(nobs, 1)
         km = km ** np.abs(censors - 1.)
         km = np.cumprod(km)  # If no ties, this is kaplan-meier
         tied = self._is_tied(endog, censors)
-        wtd_km = self.km_w_ties(tied, km)
-        return  (censors / wtd_km).reshape(self.nobs, 1)
+        wtd_km = self._km_w_ties(tied, km)
+        return  (censors / wtd_km).reshape(nobs, 1)
 
     def fit(self, km_est='koul', wt_method='koul', last_uncensored=False):
         """
@@ -126,7 +158,7 @@ class emplikeAFT(object):
         km: string, optional
             Version of Kaplan-Meier estimate to use.  If 'koul', uses the
             estimator in Koul (1981).  This ensures no divide by 0 errors.
-            If 'qin,' uses the estimate in Qin (2001) and Jing (2003).
+            If 'li,' uses the estimate in Qin (2001) and Li (2003).
             If max(endog)  is censored and km='Qin', estimation and inference
             is impossible.  See Owen (2001). Default is 'koul'
 
@@ -156,6 +188,13 @@ class emplikeAFT(object):
             self.modif_censors[-1] = self.censors[-1]
         wts = self._make_km(self.endog, self.modif_censors)
         if wt_method == 'koul':
-            return OLS(self.endog * wts, self.exog).fit()
+            res = OLS(self.endog * wts, self.exog).fit()
         if wt_method == 'stute':
-            return WLS(self.endog, self.exog, wts).fit()
+            res = WLS(self.endog, self.exog, wts).fit()
+            res.bse = None      # Inference will be done with EL.
+        res.conf_int = None
+        res.t = None
+        res.pvalues = None
+        res.f_test = None
+        res.fvalue = None
+        return res
