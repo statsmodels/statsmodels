4377fc3c0c28f8073ff40bfb83458c0c49c56877
Josef Perktold <josef.pktd@gmail.com>
2012-07-26 05:31:23 -0400

    ENH/BUG LTS class fixed, now works but only slightly faster

--- statsmodels/examples/ex_lts2.py
+++ statsmodels/examples/ex_lts2.py
@@ -18,16 +18,21 @@ from statsmodels.robust.tests.results.results_lts import (
         endog_stackloss, exog_stackloss, endog_wood, exog_wood,
         endog_aircraft, exog_aircraft, endog_salin, exog_salin)
 
+min_outl_wood = [ 0,  3,  4,  5,  6,  7, 18]  #zero-based index
 
 np.random.seed(963678)
 t0 = time.time()
 bestw = lts(endog_wood, exog_wood, k_trimmed=None, max_nstarts=100, max_nrefine=20, max_exact=0)
 t1 = time.time()
-print bestw[0], np.nonzero(bestw[-1])[0] + 1, t1-t0
+outl = np.nonzero(bestw[-1])[0]
+print bestw[0], outl + 1, t1-t0, (outl == min_outl_wood).all(), bestw[1].n_est_calls
+
 
 np.random.seed(963678)
 t2 = time.time()
 mod_lts = LTS(endog_wood, exog_wood)
-bestw2 = mod_lts.fit(random_search_options=dict(max_nstarts=100))
+bestw2 = mod_lts.fit(random_search_options=dict(max_nstarts=100, n_keep=10))
 t3 = time.time()
-print bestw2[0].ssr, np.nonzero(~bestw2[-1])[0] + 1, t3-t2
+outl = np.nonzero(~bestw2[-1])[0]
+print bestw2[0].ssr, outl + 1, t3-t2, (outl == min_outl_wood).all(), mod_lts.temp.n_est_calls
+print "len(mod_lts.temp.best_stage1)", len(mod_lts.temp.best_stage1)
