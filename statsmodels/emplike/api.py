d77919b934e4507afa3149bf2d56558128678d77
Justin Grana <jg3705a@student.american.edu>
2012-07-28 16:51:16 -0400

    Uploaded heart data and MC problem example.  See mailinglist on July 28, subject: Issues with AFT estimation.
    Conflicts:
    
    	statsmodels/emplike/aft_el.py
    	statsmodels/emplike/api.py

--- statsmodels/emplike/api.py
+++ statsmodels/emplike/api.py
@@ -4,4 +4,9 @@ api for empirical likelihood
 """
 
 from .descriptive import DescStat
+<<<<<<< HEAD
 from .elregress import ElLinReg, ElOriginRegress, ANOVA
+=======
+from .elregress import ElLinReg, ElOriginRegress
+from .aft_el import emplikeAFT
+>>>>>>> 47fd6b0... Uploaded heart data and MC problem example.  See mailinglist on July 28, subject: Issues with AFT estimation.
