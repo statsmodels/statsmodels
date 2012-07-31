f46ae12dabac35042f45374323f62d41e9893f89
langmore <ianlangmore@gmail.com>
2012-07-31 19:06:01 -0400

    Re-wrote l1_slsqp to look like l1_cvxopt

--- l1_demo/demo.py
+++ l1_demo/demo.py
@@ -13,7 +13,7 @@ def main():
     Demonstrates l1 regularization for MNLogit model.
     """
     ## Commonly adjusted params
-    N = 1000000  # Number of data points
+    N = 10000 # Number of data points
     num_targets = 2  # Targets are the dependent variables
     num_nonconst_covariates = 3 # For every target
     num_zero_params = 1 # For every target
