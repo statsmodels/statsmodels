6dc6916d8eabdc2959fc61b56fe32716ffa1514e
Skipper Seabold <jsseabold@gmail.com>
2012-08-01 18:46:46 -0400

    STY: Some code cleanup

--- statsmodels/tools/numdiff.py
+++ statsmodels/tools/numdiff.py
@@ -33,15 +33,12 @@ Author : josef-pkt
 License : BSD
 
 '''
-
-
-
 import numpy as np
 
+#TODO: do not hard-code this
 EPS = 2.2204460492503131e-016
 
-#from scipy.optimize
-def approx_fprime(xk,f,epsilon=1e-8,*args):
+def approx_fprime(xk, f, epsilon=1e-8, *args):
     f0 = f(*((xk,)+args))
     grad = np.zeros((len(xk),), float)
     ei = np.zeros((len(xk),), float)
@@ -62,28 +59,23 @@ def approx_fprime1(xk, f, epsilon=1e-12, args=(), centered=False):
     f : function
         `*((xk,)+args)` returning either one value or 1d array
     epsilon : float
-        stepsize, TODO add default
-    *args : tuple
+        stepsize
+    args : tuple
         tuple of additional arguments for function f
+    centered : bool
+        Whether central difference should be returned. If not, does forward
+        differencing.
 
     Returns
     -------
     grad : array
         gradient or Jacobian, evaluated with single step forward differencing
-
-    Notes
-    -----
-
-    todo:
-    * add scaled stepsize
-    ss- should scaled stepsize be an option in the gradient or should the
-    gradient be scaled in within an optimization framework?
     '''
+    #TODO:  add scaled stepsize
     f0 = f(*((xk,)+args))
     nobs = len(np.atleast_1d(f0)) # it could be a scalar
     grad = np.zeros((nobs,len(xk)), float)
     ei = np.zeros((len(xk),), float)
-    #centered = False
     if not centered:
         for k in range(len(xk)):
             ei[k] = epsilon
@@ -130,9 +122,6 @@ def approx_hess(xk, f, epsilon=None, args=()):#, returngrad=True):
         g[i] = f(*((xk+ee[i,:],)+args))
 
     hess = np.outer(h,h)
-##    print hess.shape,
-##    print 'H=', hess
-##    print 'h=', hess
     # Compute "double" forward step
     for i in range(n):
         for j in range(i,n):
@@ -192,7 +181,7 @@ def approx_hess3(x0, f, epsilon=None, args=()):
 
     return hess
 
-def approx_fhess_p(x0,p,fprime,epsilon,*args):
+def approx_fhess_p(x0, p, fprime, epsilon, *args):
     """
     Approximate the Hessian when the Jacobian is available.
 
@@ -211,11 +200,9 @@ def approx_fhess_p(x0,p,fprime,epsilon,*args):
     f1 = fprime(*((x0,)+args))
     return (f2 - f1)/epsilon
 
-#copied from try_gradient_complexsteps, which contains notes and some examples for testing
-#renamed from complex_step_grad to approx_fprime_cs, change order of arguments
-#from Guilherme P. de Freitas, numpy mailing list
 def approx_fprime_cs(x0, f, args=(), h=1.0e-20):
-    '''calculate gradient or Jacobian with complex step derivative approximation
+    '''
+    Calculate gradient or Jacobian with complex step derivative approximation
 
     Parameters
     ----------
@@ -229,6 +216,9 @@ def approx_fprime_cs(x0, f, args=(), h=1.0e-20):
     partials : ndarray
        array of partial derivatives, Gradient or Jacobian
     '''
+    #From Guilherme P. de Freitas, numpy mailing list
+    #May 04 2010 thread "Improvement of performance"
+    #http://mail.scipy.org/pipermail/numpy-discussion/2010-May/050250.html
     dim = np.size(x0) #TODO: What's the assumption on the shape here?
     increments = np.identity(dim) * 1j * h
     #TODO: see if this can be vectorized, but usually dim is small
@@ -284,26 +274,15 @@ def approx_hess_cs2(x0, f, epsilon=None, args=()):
     for i in range(n):
         for j in range(i,n):
             hess[i,j] = (f(*((x0 + 1j*ee[i,:] + ee[j,:],)+args))
-                         - f(*((x0 + 1j*ee[i,:] - ee[j,:],)+args))).imag/2./hess[i,j]
+                - f(*((x0 + 1j*ee[i,:] - ee[j,:],)+args))).imag/2./hess[i,j]
             hess[j,i] = hess[i,j]
 
     return hess
 
 
-def fun(beta, x):
-    return np.dot(x, beta).sum(0)
-
-def fun1(beta, y, x):
-    #print beta.shape, x.shape
-    xb = np.dot(x, beta)
-    return (y-xb)**2 #(xb-xb.mean(0))**2
-
-def fun2(beta, y, x):
-    #print beta.shape, x.shape
-    return fun1(beta, y, x).sum(0)
-
-
 if __name__ == '__main__':
+
+
     import statsmodels.api as sm
     from scipy.optimize.optimize import approx_fhess_p
     import numpy as np
@@ -317,7 +296,20 @@ if __name__ == '__main__':
     score = mod.score
     hess = mod.hessian
 
-# below is Josef's scratch work
+    # below is Josef's scratch work
+
+
+    def fun(beta, x):
+        return np.dot(x, beta).sum(0)
+
+    def fun1(beta, y, x):
+        #print beta.shape, x.shape
+        xb = np.dot(x, beta)
+        return (y-xb)**2 #(xb-xb.mean(0))**2
+
+    def fun2(beta, y, x):
+        #print beta.shape, x.shape
+        return fun1(beta, y, x).sum(0)
 
     nobs = 200
     x = np.arange(nobs*3).reshape(nobs,-1)
@@ -375,88 +367,3 @@ if __name__ == '__main__':
     #assert_almost_equal(hessnd, he[0])
     gnd = nd.Gradient(lambda a: fun2(a, y, x))
     gradnd = gnd(xk)
-'''
->>> hnd = nd.Hessian(lambda a: fun2(a, x))
->>> hnd(xk)
-array([[ 216.87702746,   -3.41892545,    1.87887281],
-       [  -3.41892545,  180.76379116,  -13.74326021],
-       [   1.87887281,  -13.74326021,  198.5155617 ]])
->>> he
-(array([[ 216.87702746,   -3.41892545,    1.87887281],
-       [  -3.41892545,  180.76379116,  -13.74326021],
-       [   1.87887281,  -13.74326021,  198.5155617 ]]), array([ 2.35204474,  1.92684939,  2.11920745]))
->>> hnd = nd.Gradient(lambda a: fun2(a, x))
->>> hnd(xk)
-array([  0.00000000e+00,   1.40036521e-14,  -2.59014117e-14])
->>> hnd((1.2, 1.2, 1.2))
-array([ 41.58106741,  34.51076432,  38.95952468])
->>> approx_fprime1(xk,fun1,epsilon,*args).sum(0)
-array([  1.08438310e-04,   9.03821177e-05,   9.92578403e-05])
->>> approx_fprime1((1.2, 1.2, 1.2),fun1,epsilon,*args).sum(0)
-array([ 41.58117585,  34.5108547 ,  38.95962393])
->>> approx_fprime((1.2, 1.2, 1.2),fun2,epsilon,*args).sum(0)
-115.05165448078003
->>> approx_fprime((1.2, 1.2, 1.2),fun2,epsilon,*args)
-array([ 41.58117584,  34.5108547 ,  38.95962393])
->>> epsilon
-9.9999999999999995e-007
->>> approx_hess(np.array([1.2, 1.2, 1.2]),fun2,epsilon,*args)
-(3, 3) H= [[  1.00000000e-12   1.00000000e-12   1.00000000e-12]
- [  1.00000000e-12   1.00000000e-12   1.00000000e-12]
- [  1.00000000e-12   1.00000000e-12   1.00000000e-12]]
-h= [[  1.00000000e-12   1.00000000e-12   1.00000000e-12]
- [  1.00000000e-12   1.00000000e-12   1.00000000e-12]
- [  1.00000000e-12   1.00000000e-12   1.00000000e-12]]
-(array([[ 216.87718291,   -3.41771056,    1.87938554],
-       [  -3.41771056,  180.76384836,  -13.74189651],
-       [   1.87938554,  -13.74189651,  198.51498226]]), array([ 41.58117585,  34.51085471,  38.95962394]))
-
-
->>> hnd = nd.Hessian(lambda a: fun2(a, x))
->>> hnd((1.2, 1.2, 1.2))
-array([[ 216.87702746,   -3.41892545,    1.87887281],
-       [  -3.41892545,  180.76379116,  -13.74326021],
-       [   1.87887281,  -13.74326021,  198.5155617 ]])
->>> hnd(xk)
-array([[ 216.87702746,   -3.41892545,    1.87887281],
-       [  -3.41892545,  180.76379116,  -13.74326021],
-       [   1.87887281,  -13.74326021,  198.5155617 ]])
->>> j = approx_fprime1((1.2, 1.2, 1.2),fun1,epsilon,*args)
->>> np.dot(j.T,j)
-array([[ 90.37418663,  23.81355855,  33.2936759 ],
-       [ 23.81355855,  49.01569714,  16.70507137],
-       [ 33.2936759 ,  16.70507137,  82.24708274]])
-
-
->>> heb = approx_hess(xk,fun2,-1e-6,*args)
-(3, 3) H= [[  1.00000000e-12   1.00000000e-12   1.00000000e-12]
- [  1.00000000e-12   1.00000000e-12   1.00000000e-12]
- [  1.00000000e-12   1.00000000e-12   1.00000000e-12]]
-h= [[  1.00000000e-12   1.00000000e-12   1.00000000e-12]
- [  1.00000000e-12   1.00000000e-12   1.00000000e-12]
- [  1.00000000e-12   1.00000000e-12   1.00000000e-12]]
->>> hef
-(array([[ 216.87707189,   -3.41926487,    1.87860838],
-       [  -3.41926487,  180.76329321,  -13.74345082],
-       [   1.87860838,  -13.74345082,  198.51542631]]), array([  1.08438369e-04,   9.03821462e-05,   9.92579352e-05]))
->>> heb
-(array([[ 216.87729394,   -3.41848772,    1.87905247],
-       [  -3.41848772,  180.76384832,  -13.7433398 ],
-       [   1.87905247,  -13.7433398 ,  198.51575938]]), array([ -1.08438258e-04,  -9.03819242e-05,  -9.92578242e-05]))
->>> (hef[1]+heb[1])/2.
-array([  5.55111512e-11,   1.11022302e-10,   5.55111512e-11])
->>> (hef[0]+heb[0])/2.
-array([[ 216.87718291,   -3.41887629,    1.87883042],
-       [  -3.41887629,  180.76357077,  -13.74339531],
-       [   1.87883042,  -13.74339531,  198.51559284]])
->>> hnd = nd.Hessian(lambda a: fun2(a, x))
->>> hnd(xk)
-array([[ 216.87702746,   -3.41892545,    1.87887281],
-       [  -3.41892545,  180.76379116,  -13.74326021],
-       [   1.87887281,  -13.74326021,  198.5155617 ]])
->>> j = approx_fprime1((1.2, 1.2, 1.2),fun1,epsilon,*args)
->>> np.dot(j.T,j)
-array([[ 90.37418663,  23.81355855,  33.2936759 ],
-       [ 23.81355855,  49.01569714,  16.70507137],
-       [ 33.2936759 ,  16.70507137,  82.24708274]])
-'''
