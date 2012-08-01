a8d90f7eeb43c8b25c106fba3176fb394d9e3f0a
Alexandre Crayssac <alexandre.crayssac@gmail.com>
2012-08-01 20:11:16 +0200

    Fixed bug in DoF corrections

--- statsmodels/examples/example_sysreg3.py
+++ statsmodels/examples/example_sysreg3.py
@@ -16,8 +16,8 @@ eq1 = {'endog' : y, 'exog' : x1, 'indep_endog' : [1]}
 eq2 = {'endog' : y, 'exog' : x2, 'indep_endog' : [1]}
 sys = [eq1, eq2]
 
-mod2sls = Sys2SLS(sys, dfk='dfk1')
-res2sls = mod2sls.fit()
+#mod2sls = Sys2SLS(sys, dfk="dfk1")
+#res2sls = mod2sls.fit()
 #print res2sls.summary()
 
 mod3sls = Sys3SLS(sys)
