9b5cac261c9466ceb6c47023b197b00c10d64252
Skipper Seabold <jsseabold@gmail.com>
2012-08-02 14:45:37 -0400

    ENH: Add summary method for discrete margins

--- statsmodels/discrete/discrete_margins.py
+++ statsmodels/discrete/discrete_margins.py
@@ -212,7 +212,7 @@ def margeff_cov_params_dummy(model, cov_margins, params, exog, dummy_ind,
         if dfdb.ndim >= 2: # for overall
             dfdb = dfdb.mean(0)
         if J > 1:
-            K = dfdb.shape[1] / (J-1) # assumes there's a constant
+            K = dfdb.shape[1] / (J-1)
             cov_margins[i::K, :] = dfdb
         else:
             cov_margins[i, :] = dfdb # how each F changes with change in B
@@ -426,10 +426,75 @@ class DiscreteMargins(object):
         return np.asarray(zip(lower, upper))
 
     def summary(self, alpha=.05):
-        raise NotImplementedError
+        """
+        """
+        results = self.results
+        model = results.model
+        title = model.__class__.__name__ + " Marginal Effects"
+        method = self.margeff_options['method']
+        top_left = [('Dep. Variable:', [model.endog_names]),
+                ('Method:', [method]),
+                ('At:', [self.margeff_options['at']]),]
 
-    def get_margeff(self, at='overall', method='dydx', atexog=None, dummy=False,
-            count=False):
+        from statsmodels.iolib.summary import (Summary, summary_params,
+                                                table_extend)
+        exog_names = model.exog_names[:] # copy
+        smry = Summary()
+
+        # sigh, we really need to hold on to this in _data...
+        _, const_idx = _get_const_index(model.exog)
+        if const_idx is not None:
+            exog_names.pop(const_idx)
+
+        J = int(getattr(model, "J", 1))
+        if J > 1:
+            yname, yname_list = results._get_endog_name(model.endog_names,
+                                                None, all=True)
+        else:
+            yname = model.endog_names
+            yname_list = [yname]
+
+        smry.add_table_2cols(self, gleft=top_left, gright=[],
+                yname=yname, xname=exog_names, title=title)
+
+        #NOTE: add_table_params is not general enough yet for margeff
+        # could use a refactor with getattr instead of hard-coded params
+        # tvalues etc.
+        table = []
+        conf_int = self.conf_int(alpha)
+        margeff = self.margeff
+        margeff_se = self.margeff_se
+        tvalues = self.tvalues
+        pvalues = self.pvalues
+        if J > 1:
+            for eq in range(J):
+                restup = (results, margeff[:,eq], margeff_se[:,eq],
+                          tvalues[:,eq], pvalues[:,eq], conf_int[:,:,eq])
+                tble = summary_params(restup, yname=yname_list[eq],
+                              xname=exog_names, alpha=alpha, use_t=False,
+                              skip_header=True)
+                tble.title = yname_list[eq]
+                # overwrite coef with method name
+                header = ['', _transform_names[method], 'std err', 'z',
+                        'P>|z|', '[%3.1f%% Conf. Int.]' % (100-alpha*100)]
+                tble.insert_header_row(0, header)
+                #from IPython.core.debugger import Pdb; Pdb().set_trace()
+                table.append(tble)
+
+            table = table_extend(table, keep_headers=True)
+        else:
+            restup = (results, margeff, margeff_se, tvalues, pvalues, conf_int)
+            table = summary_params(restup, yname=yname, xname=exog_names,
+                    alpha=alpha, use_t=False, skip_header=True)
+            header = ['', _transform_names[method], 'std err', 'z',
+                        'P>|z|', '[%3.1f%% Conf. Int.]' % (100-alpha*100)]
+            table.insert_header_row(0, header)
+
+        smry.tables.append(table)
+        return smry
+
+    def get_margeff(self, at='overall', method='dydx', atexog=None,
+                          dummy=False, count=False):
         """Get marginal effects of the fitted model.
 
         Parameters
