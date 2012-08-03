28f03823ccf2f36c5d707f39285aff68d7a09578
langmore <ianlangmore@gmail.com>
2012-08-03 18:07:34 -0400

    Made demo much nicer

--- l1_demo/demo.py
+++ l1_demo/demo.py
@@ -12,6 +12,11 @@ def main():
     """
     Demonstrates l1 regularization for MNLogit model.
 
+    How to use this demo
+    --------------------
+    Adjust the "Commonly adjusted params" at the top, then run with 
+        $ python demo.py
+
     The Story
     ---------
     The maximum likelihood (ML) solution works well when the number of data 
@@ -27,11 +32,17 @@ def main():
     The l1_cvxopt_cp solver is part of CVXOPT.  It works well even for larger
         data sizes.
     """
+    ##########################################################################
     #### Commonly adjusted params
     N = 500 # Number of data points
     num_targets = 3  # Targets are the dependent variables
     num_nonconst_covariates = 10 # For every target
     num_zero_params = 8 # For every target
+    print_long_results = False
+    get_l1_slsqp_results = True
+    get_l1_cvxopt_results = True
+    save_results = False
+    load_old_results = False
     # The regularization parameter
     # Here we scale it with N for simplicity.  In practice, you should
     # use cross validation to pick alpha
@@ -44,6 +55,7 @@ def main():
     # matrix (during the l1_cvxopt_cp fit)
     cor_length = 2 
     noise_level = 0.2  # As a fraction of the "signal"
+    ##########################################################################
 
     #### Make the arrays
     exog = get_exog(N, num_nonconst_covariates, cor_length) 
@@ -52,52 +64,65 @@ def main():
     if num_zero_params:
         true_params[-num_zero_params:, :] = 0
     endog = get_multinomial_endog(num_targets, true_params, exog, noise_level)
-    #### Use these lines to save results and try again with new alpha
-    #sp.save('endog.npy', endog)
-    #sp.save('exog.npy', exog)
-    #sp.save('true_params.npy', true_params)
-    #endog = sp.load('endog.npy')
-    #exog = sp.load('exog.npy')
-    #true_params = sp.load('true_params.npy')
+    if save_results:
+        sp.save('endog.npy', endog)
+        sp.save('exog.npy', exog)
+        sp.save('true_params.npy', true_params)
+    if load_old_results:
+        endog = sp.load('endog.npy')
+        exog = sp.load('exog.npy')
+        true_params = sp.load('true_params.npy')
+
     #### Train the models
+    print_str = '=========== Brief Result Printout ============'
     model = sm.MNLogit(endog, exog)
+    # Get ML results
     results_ML = model.fit(method='newton')
-    start_params = results_ML.params.ravel(order='F')
-    #results_l1_slsqp = model.fit(method='l1_slsqp', alpha=alpha, maxiter=70, 
-    #        start_params=start_params, trim_params=True, retall=True)
-    results_l1_cvxopt_cp = model.fit(method='l1_cvxopt_cp', alpha=alpha, 
-            maxiter=50, start_params=start_params, trim_params=True, 
-            retall=True, feastol=1e-5)
-    #### Compute MSE
     MSE_ML = get_MSE(results_ML, true_params)
-    #MSE_l1_slsqp = get_MSE(results_l1_slsqp, true_params)
-    MSE_l1_cvxopt_cp = get_MSE(results_l1_cvxopt_cp, true_params)
+    print_str += '\n ML mean square error = %.4f'%MSE_ML
+    # Get l1 results
+    start_params = results_ML.params.ravel(order='F')
+    if get_l1_slsqp_results:
+        results_l1_slsqp = model.fit(method='l1_slsqp', alpha=alpha, 
+                maxiter=70, start_params=start_params, trim_params=True, 
+                retall=True)
+        MSE_l1_slsqp = get_MSE(results_l1_slsqp, true_params)
+        print_str += '\n l1_slsqp mean square error = %.4f'%MSE_l1_slsqp
+    if get_l1_cvxopt_results:
+        results_l1_cvxopt_cp = model.fit(method='l1_cvxopt_cp', alpha=alpha, 
+                maxiter=50, start_params=start_params, trim_params=True, 
+                retall=True, feastol=1e-5)
+        MSE_l1_cvxopt_cp = get_MSE(results_l1_cvxopt_cp, true_params)
+        print_str += '\n l1_cvxopt_cp mean square error = %.4f'%MSE_l1_cvxopt_cp
     #### Prints results
-    #print "MSEs:  ML = %.4f,  l1_slsqp = %.4f,  l1_cvxopt_cp = %.4f"%(
-    #        MSE_ML, MSE_l1_slsqp, MSE_l1_cvxopt_cp)
-    print "MSEs:  ML = %.4f,  l1_cvxopt_cp = %.4f"%(
-            MSE_ML, MSE_l1_cvxopt_cp)
-    print "The true parameters are \n%s"%true_params
-    #print "\nML had a MSE of %f and the parameters are \n%s"%(
-    #        MSE_ML, results_ML.params)
-    #print "\nl1_slsqp had a MSE of %f and the parameters are \n%s"%(
-    #        MSE_l1_slsqp, results_l1_slsqp.params)
-    print "\nl1_cvxopt_cp had a MSE of %f and the parameters are \n%s"%(
-            MSE_l1_cvxopt_cp, results_l1_cvxopt_cp.params)
-    #print "\n"
-    #print "\nThe ML fit results are"
-    #print results_ML.summary()
-    #print "\nThe l1_slsqp fit results are"
-    #print results_l1_slsqp.summary()
-    #print "\nThe l1_cvxopt_cp fit results are"
-    #print results_l1_cvxopt_cp.summary()
+    print_str += '\n========== More detail ============='
+    print_str += "\nTrue parameters: \n%s"%true_params
+    if print_long_results:
+        if get_l1_slsqp_results:
+            print_str += '\n' + results_l1_slsqp.summary().as_text()
+        if get_l1_cvxopt_results:
+            print_str += '\n' + results_l1_cvxopt_cp.summary().as_text()
+    else:
+        if get_l1_slsqp_results:
+            print_str += '\nThe l1_slsqp params are %s'%results_l1_slsqp.params
+        if get_l1_cvxopt_results:
+            print_str += '\nThe l1_cvxopt_cp params are %s'%results_l1_cvxopt_cp.params
+    print print_str
+
 
 def get_MSE(results, true_params):
+    """
+    Gets the (normalized) mean square error.
+    """
     raw_MSE = sp.sqrt(((results.params - true_params)**2).sum()) 
     param_norm = sp.sqrt((true_params**2).sum())
     return raw_MSE / param_norm
 
 def get_multinomial_endog(num_targets, true_params, exog, noise_level):
+    """
+    Gets an endogenous response that is consistent with the true_params,
+        perturbed by noise at noise_level.
+    """
     N = exog.shape[0]
     ### Create the probability of entering the different classes, 
     ### given exog and true_params
