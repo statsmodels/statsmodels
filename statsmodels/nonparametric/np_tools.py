a6be4fe23a5691ff98781f3cba6c505fd5e0a2e5
Ralf Gommers <ralf.gommers@googlemail.com>
2012-08-04 16:24:54 +0200

    MAINT: rename KernelFunctions.py --> kernels.py
    Also remove some more unused imports.

--- statsmodels/nonparametric/np_tools.py
+++ statsmodels/nonparametric/np_tools.py
@@ -1,16 +1,17 @@
 import numpy as np
-import KernelFunctions as kf
+
+from . import kernels
 
 
-kernel_func = dict(wangryzin=kf.WangRyzin,
-                   aitchisonaitken=kf.AitchisonAitken,
-                   gaussian=kf.Gaussian,
-                   gauss_convolution=kf.Gaussian_Convolution,
-                   wangryzin_convolution=kf.WangRyzin_Convolution,
-                   aitchisonaitken_convolution=kf.AitchisonAitken_Convolution,
-                   gaussian_cdf=kf.Gaussian_cdf,
-                   aitchisonaitken_cdf=kf.AitchisonAitken_cdf,
-                   wangryzin_cdf=kf.WangRyzin_cdf)
+kernel_func = dict(wangryzin=kernels.WangRyzin,
+                   aitchisonaitken=kernels.AitchisonAitken,
+                   gaussian=kernels.Gaussian,
+                   gauss_convolution=kernels.Gaussian_Convolution,
+                   wangryzin_convolution=kernels.WangRyzin_Convolution,
+                   aitchisonaitken_convolution=kernels.AitchisonAitken_Convolution,
+                   gaussian_cdf=kernels.Gaussian_cdf,
+                   aitchisonaitken_cdf=kernels.AitchisonAitken_cdf,
+                   wangryzin_cdf=kernels.WangRyzin_cdf)
 
 
 class LeaveOneOut(object):
