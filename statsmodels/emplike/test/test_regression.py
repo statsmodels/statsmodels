4f2214b6bfb0e905474b3b7a62d182e8744fbf8f
Justin Grana <jg3705a@student.american.edu>
2012-08-10 16:18:28 -0400

    Fixed test to match new function names.

--- statsmodels/emplike/test/test_regression.py
+++ statsmodels/emplike/test/test_regression.py
@@ -31,31 +31,31 @@ class TestRegressionPowell(GenRes):
         super(TestRegressionPowell, self).__init__()
 
     def test_hypothesis_beta0(self):
-        beta0res = self.res1.hy_test_beta([-30], [0], print_weights=1)
-        assert_almost_equal(beta0res[:2], self.res2.hy_test_beta0[:2], 4)
-        assert_almost_equal(beta0res[2], self.res2.hy_test_beta0[2], 4)
+        beta0res = self.res1.test_beta([-30], [0], print_weights=1)
+        assert_almost_equal(beta0res[:2], self.res2.test_beta0[:2], 4)
+        assert_almost_equal(beta0res[2], self.res2.test_beta0[2], 4)
 
     def test_hypothesis_beta1(self):
-        beta1res = self.res1.hy_test_beta([.5], [1], print_weights=1)
-        assert_almost_equal(beta1res[:2], self.res2.hy_test_beta1[:2], 4)
-        assert_almost_equal(beta1res[2], self.res2.hy_test_beta1[2], 4)
+        beta1res = self.res1.test_beta([.5], [1], print_weights=1)
+        assert_almost_equal(beta1res[:2], self.res2.test_beta1[:2], 4)
+        assert_almost_equal(beta1res[2], self.res2.test_beta1[2], 4)
 
     def test_hypothesis_beta2(self):
-        beta2res = self.res1.hy_test_beta([1], [2], print_weights=1)
-        assert_almost_equal(beta2res[:2], self.res2.hy_test_beta2[:2], 4)
-        assert_almost_equal(beta2res[2], self.res2.hy_test_beta2[2], 4)
+        beta2res = self.res1.test_beta([1], [2], print_weights=1)
+        assert_almost_equal(beta2res[:2], self.res2.test_beta2[:2], 4)
+        assert_almost_equal(beta2res[2], self.res2.test_beta2[2], 4)
 
     def test_hypothesis_beta3(self):
-        beta3res = self.res1.hy_test_beta([0], [3], print_weights=1)
-        assert_almost_equal(beta3res[:2], self.res2.hy_test_beta3[:2], 4)
-        assert_almost_equal(beta3res[2], self.res2.hy_test_beta3[2], 4)
+        beta3res = self.res1.test_beta([0], [3], print_weights=1)
+        assert_almost_equal(beta3res[:2], self.res2.test_beta3[:2], 4)
+        assert_almost_equal(beta3res[2], self.res2.test_beta3[2], 4)
 
     def test_ci_beta0(self):
         beta0ci = self.res1.ci_beta(0)
         lower_lim = beta0ci[0]
         upper_lim = beta0ci[1]
-        ul_pval = self.res1.hy_test_beta([upper_lim], [0])[0]
-        ll_pval = self.res1.hy_test_beta([lower_lim], [0])[0]
+        ul_pval = self.res1.test_beta([upper_lim], [0])[1]
+        ll_pval = self.res1.test_beta([lower_lim], [0])[1]
         assert_almost_equal(ul_pval, .050000, 4)
         assert_almost_equal(ll_pval, .050000, 4)
 
@@ -63,8 +63,8 @@ class TestRegressionPowell(GenRes):
         beta1ci = self.res1.ci_beta(1)
         lower_lim = beta1ci[0]
         upper_lim = beta1ci[1]
-        ul_pval = self.res1.hy_test_beta([upper_lim], [1])[0]
-        ll_pval = self.res1.hy_test_beta([lower_lim], [1])[0]
+        ul_pval = self.res1.test_beta([upper_lim], [1])[1]
+        ll_pval = self.res1.test_beta([lower_lim], [1])[1]
         assert_almost_equal(ul_pval, .050000, 4)
         assert_almost_equal(ll_pval, .050000, 4)
 
@@ -72,8 +72,8 @@ class TestRegressionPowell(GenRes):
         beta2ci = self.res1.ci_beta(2)
         lower_lim = beta2ci[0]
         upper_lim = beta2ci[1]
-        ul_pval = self.res1.hy_test_beta([upper_lim], [2])[0]
-        ll_pval = self.res1.hy_test_beta([lower_lim], [2])[0]
+        ul_pval = self.res1.test_beta([upper_lim], [2])[1]
+        ll_pval = self.res1.test_beta([lower_lim], [2])[1]
         assert_almost_equal(ul_pval, .050000, 4)
         assert_almost_equal(ll_pval, .050000, 4)
 
@@ -81,8 +81,8 @@ class TestRegressionPowell(GenRes):
         beta3ci = self.res1.ci_beta(3)
         lower_lim = beta3ci[0]
         upper_lim = beta3ci[1]
-        ul_pval = self.res1.hy_test_beta([upper_lim], [3])[0]
-        ll_pval = self.res1.hy_test_beta([lower_lim], [3])[0]
+        ul_pval = self.res1.test_beta([upper_lim], [3])[1]
+        ll_pval = self.res1.test_beta([lower_lim], [3])[1]
         assert_almost_equal(ul_pval, .050000, 4)
         assert_almost_equal(ll_pval, .050000, 4)
 
@@ -90,7 +90,7 @@ class TestRegressionPowell(GenRes):
 class TestRegressionNM(GenRes):
     """
     All confidence intervals are tested by conducting a hypothesis
-    tests at the confidence interval values since hy_test_beta
+    tests at the confidence interval values since test_beta
     is already tested against Matlab
 
     See Also
@@ -103,33 +103,33 @@ class TestRegressionNM(GenRes):
         super(TestRegressionNM, self).__init__()
 
     def test_hypothesis_beta0(self):
-        beta0res = self.res1.hy_test_beta([-30], [0], print_weights=1,
+        beta0res = self.res1.test_beta([-30], [0], print_weights=1,
                                           method='nm')
-        assert_almost_equal(beta0res[:2], self.res2.hy_test_beta0[:2], 4)
-        assert_almost_equal(beta0res[2], self.res2.hy_test_beta0[2], 4)
+        assert_almost_equal(beta0res[:2], self.res2.test_beta0[:2], 4)
+        assert_almost_equal(beta0res[2], self.res2.test_beta0[2], 4)
 
     def test_hypothesis_beta1(self):
-        beta1res = self.res1.hy_test_beta([.5], [1], print_weights=1,
+        beta1res = self.res1.test_beta([.5], [1], print_weights=1,
                                           method='nm')
-        assert_almost_equal(beta1res[:2], self.res2.hy_test_beta1[:2], 4)
-        assert_almost_equal(beta1res[2], self.res2.hy_test_beta1[2], 4)
+        assert_almost_equal(beta1res[:2], self.res2.test_beta1[:2], 4)
+        assert_almost_equal(beta1res[2], self.res2.test_beta1[2], 4)
 
     def test_hypothesis_beta2(self):
-        beta2res = self.res1.hy_test_beta([1], [2], print_weights=1,
+        beta2res = self.res1.test_beta([1], [2], print_weights=1,
                                           method='nm')
-        assert_almost_equal(beta2res[:2], self.res2.hy_test_beta2[:2], 4)
-        assert_almost_equal(beta2res[2], self.res2.hy_test_beta2[2], 4)
+        assert_almost_equal(beta2res[:2], self.res2.test_beta2[:2], 4)
+        assert_almost_equal(beta2res[2], self.res2.test_beta2[2], 4)
 
     def test_hypothesis_beta3(self):
-        beta3res = self.res1.hy_test_beta([0], [3], print_weights=1,
+        beta3res = self.res1.test_beta([0], [3], print_weights=1,
                                           method='nm')
-        assert_almost_equal(beta3res[:2], self.res2.hy_test_beta3[:2], 4)
-        assert_almost_equal(beta3res[2], self.res2.hy_test_beta3[2], 4)
+        assert_almost_equal(beta3res[:2], self.res2.test_beta3[:2], 4)
+        assert_almost_equal(beta3res[2], self.res2.test_beta3[2], 4)
 
     def test_ci_beta0(self):
         """
         All confidence intervals are tested by conducting a hypothesis
-        tests at the confidence interval values since hy_test_beta
+        tests at the confidence interval values since test_beta
         is already tested against Matlab
 
         See Also
@@ -143,8 +143,8 @@ class TestRegressionNM(GenRes):
         # ^ doesn't converge at default bounds
         lower_lim = beta0ci[0]
         upper_lim = beta0ci[1]
-        ul_pval = self.res1.hy_test_beta([upper_lim], [0], method='nm')[0]
-        ll_pval = self.res1.hy_test_beta([lower_lim], [0], method='nm')[0]
+        ul_pval = self.res1.test_beta([upper_lim], [0], method='nm')[1]
+        ll_pval = self.res1.test_beta([lower_lim], [0], method='nm')[1]
         assert_almost_equal(ul_pval, .050000, 4)
         assert_almost_equal(ll_pval, .050000, 4)
 
@@ -152,8 +152,8 @@ class TestRegressionNM(GenRes):
         beta1ci = self.res1.ci_beta(1, method='nm')
         lower_lim = beta1ci[0]
         upper_lim = beta1ci[1]
-        ul_pval = self.res1.hy_test_beta([upper_lim], [1], method='nm')[0]
-        ll_pval = self.res1.hy_test_beta([lower_lim], [1], method='nm')[0]
+        ul_pval = self.res1.test_beta([upper_lim], [1], method='nm')[1]
+        ll_pval = self.res1.test_beta([lower_lim], [1], method='nm')[1]
         assert_almost_equal(ul_pval, .050000, 4)
         assert_almost_equal(ll_pval, .050000, 4)
 
@@ -161,8 +161,8 @@ class TestRegressionNM(GenRes):
         beta2ci = self.res1.ci_beta(2, method='nm')
         lower_lim = beta2ci[0]
         upper_lim = beta2ci[1]
-        ul_pval = self.res1.hy_test_beta([upper_lim], [2], method='nm')[0]
-        ll_pval = self.res1.hy_test_beta([lower_lim], [2], method='nm')[0]
+        ul_pval = self.res1.test_beta([upper_lim], [2], method='nm')[1]
+        ll_pval = self.res1.test_beta([lower_lim], [2], method='nm')[1]
         assert_almost_equal(ul_pval, .050000, 4)
         assert_almost_equal(ll_pval, .050000, 4)
 
@@ -170,8 +170,8 @@ class TestRegressionNM(GenRes):
         beta3ci = self.res1.ci_beta(3)
         lower_lim = beta3ci[0]
         upper_lim = beta3ci[1]
-        ul_pval = self.res1.hy_test_beta([upper_lim], [3], method='nm')[0]
-        ll_pval = self.res1.hy_test_beta([lower_lim], [3], method='nm')[0]
+        ul_pval = self.res1.test_beta([upper_lim], [3], method='nm')[1]
+        ll_pval = self.res1.test_beta([lower_lim], [3], method='nm')[1]
         assert_almost_equal(ul_pval, .050000, 4)
         assert_almost_equal(ll_pval, .050000, 4)
 
