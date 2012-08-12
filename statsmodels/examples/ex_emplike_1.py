4987c8afb0c5fa046e26c215c1b60225a647bdd7
Justin Grana <jg3705a@student.american.edu>
2012-08-12 19:40:39 -0400

    Fixed examples for all methods in descriptive.py.

--- statsmodels/examples/ex_emplike_1.py
+++ statsmodels/examples/ex_emplike_1.py
@@ -1,43 +1,35 @@
 """
-
-Here is the midterm report and a test script for my empirical likelihood
-GSOC project.  Please see my July 11 blog post at
-http://landopystat.blogspot.com/ for a narrative of my GSOC experience.
-
-For this script to work, install statsmodels from the emplike_reg branch.
-
-This script can be ran feature by feature or all at once. However, plt.show()
-is at the end of the file so to see the plots with the rest of the script
-commented out, make sure to type plt.show().
+This is a basic tutorial on how to conduct basic empirical likelihood
+inference for descriptive statistics.  If matplotlib is installed
+it also generates plots.
 
 """
 import numpy as np
-from matplotlib import pyplot as plt
 import statsmodels.api as sm
 print 'Welcome to El'
-np.random.seed(634) # No significance of the seed, just my favorite number
+np.random.seed(634)  # No significance of the seed.
 # Let's first generate some univariate data.
 univariate = np.random.standard_normal(30)
 
 # Now let's play with it
 # Initiate an empirical likelihood descriptive statistics instance
-eldescriptive=sm.emplike.DescStatUV(univariate)
+eldescriptive = sm.emplike.DescStat(univariate)
 
 # Empirical likelihood is (typically) a  method of inference,
 # not estimation.  Therefore, there is no attribute eldescriptive.mean
 # However, we can check the mean:
-eldescriptive_mean= eldescriptive.endog.mean() #.42
+eldescriptive_mean = eldescriptive.endog.mean()  #.42
 
 #Let's conduct a hypothesis test to see if the mean is 0
 print 'Hypothesis test results for the mean:'
-print eldescriptive.test_mean(0)
+print  eldescriptive.test_mean(0)
 
 
-# The first value is the pvalue and the second is  -2 *log-likelihood ratio,
-# which is distributed chi2.
+# The first value is is  -2 *log-likelihood ratio, which is distributed
+#chi2.  The second value is the p-value.
 
 # Let's see what the variance is:
-eldescriptive_var = eldescriptive.endog.var() # 1.01
+eldescriptive_var = eldescriptive.endog.var()  # 1.01
 
 #Let's test if the variance is 1:
 print 'Hypothesis test results for the variance:'
@@ -48,9 +40,13 @@ print 'Hypothesis test results for Skewness:'
 print eldescriptive.test_skew(0)
 print 'Hypothesis test results for the Kurtosis:'
 print eldescriptive.test_kurt(0)
-
 # Note that the skewness and Kurtosis take longer.  This is because
-# we have to optimizae over the nuisance parameters (mean, variance).
+# we have to optimize over the nuisance parameters (mean, variance).
+
+# We can also test for the joint skewness and kurtoses
+print ' Joint Skewness-Kurtosis test'
+eldescriptive.test_joint_skew_kurt(0, 0)
+
 
 # Let's try and get some confidence intervals
 print 'Confidence interval for the mean'
@@ -61,35 +57,25 @@ print 'Confidence interval for skewness'
 print eldescriptive.ci_skew()
 print 'Confidence interval for kurtosis'
 print eldescriptive.ci_kurt()
-# Note in 'Finding Ci for the variance' output.
-# This is because we need to optimize over the variance and we need to
-# set limits.  However, if we have a general idea, we cans et those and
-# speed up the optimization.
-print eldescriptive.ci_kurt(var_min=.7, var_max=1.5, mu_min=.2, mu_max=.7)
-# In this case, since it was fast to find the CI for the variance, adding
-# in the optional parameters didn't help much.  With more difficult data
-# to analyze, this can be very helpful.
+
 
 # if matplotlib is installed, we can get a contour plot for the mean
 # and variance.
-plt.figure(1)
-eldescriptive.mean_var_contour(-.5, 1.2, .2, 2.5, .1, .1)
-
-# The mean is on the X acis and Variance is on the Y.  I jsut realized
-# that either those should be labeled or mentioned in the documentation
+mean_variance_contour = eldescriptive.plot_contour(-.5, 1.2, .2, 2.5, .05, .05)
+# This returns a figure instance.  Just type mean_var_contour.show()
+# to see the plot.
 
 # Once you close the plot, we can start some multivariate analysis.
 
-x1 = np.random.exponential(2, (30,1))
-x2 = 2 * x1 +np.random.chisquare(4, (30,1))
+x1 = np.random.exponential(2, (30, 1))
+x2 = 2 * x1 + np.random.chisquare(4, (30, 1))
 mv_data = np.concatenate((x1, x2), axis=1)
-mv_elmodel = sm.emplike.DescStatMV(mv_data)
-# For multivariate data, the only attributes are mv_test_mean,
-# mv mean contour and ci_corr and test_corr.  Maybe the multivariate
-# functions should be their own class.
+mv_elmodel = sm.emplike.DescStat(mv_data)
+# For multivariate data, the only methods are mv_test_mean,
+# mv mean contour and ci_corr and test_corr.
 
-# Let test the hypthesis that x1 has a mean of 2 and x2 has a mean of 7
-'Multivaraite mean hypothesis test'
+# Let's test the hypthesis that x1 has a mean of 2 and x2 has a mean of 7
+print 'Multivaraite mean hypothesis test'
 print mv_elmodel.mv_test_mean(np.array([2, 7]))
 
 # Now let's get the confidence interval for correlation
@@ -101,13 +87,11 @@ print mv_elmodel.ci_corr()
 print 'Hypothesis test for correlation'
 print mv_elmodel.test_corr(.7)
 
-#Finally, let's create a contour plot for the means of the data
-plt.figure(2)
-mv_elmodel.mv_mean_contour(0,4, 4,12, .15,.15, plot_dta=1)
+# Finally, let's create a contour plot for the means of the data
+means_contour = mv_elmodel.mv_mean_contour(1, 3, 6,9, .15,.15, plot_dta=1)
+# This also returns a fig so we can type mean_contour.show() to see the figure
+# Sometimes, the data is very dispersed and we would like to see the confidence
+# intervals without the plotted data.  Let's see the difference when we set
+# plot_dta=0
 
-
-
-# Again, I suppose I need labels or something in the documentation to
-# let the end user know which axis is which.
-
-plt.show()
+means_contour2 = mv_elmodel.mv_mean_contour(1, 3, 6,9, .05,.05, plot_dta=0)
