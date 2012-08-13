b0e83eaef2be61b173068573dc05aad7e4c5e26f
Justin Grana <jg3705a@student.american.edu>
2012-08-13 16:41:38 -0400

    EX: Examples for AFT empirical likelihood model

---
+++ statsmodels/examples/ex_emplike_3.py
@@ -0,0 +1,26 @@
+"""
+This script provides a tutorial on how to use estimate and conduct
+inference in an accelerated failure time model using empirical likelihood.
+
+We will be using the Stanford Heart Transplant data
+
+"""
+
+import statsmodels.api as sm
+import numpy as np
+
+data = sm.datasets.heart.load()
+# Note this data has endog, exog and censors
+# We will take the log (base 10) of the endogenous survival times
+
+model = sm.emplike.emplikeAFT(np.log10(data.endog),
+                              sm.add_constant(data.exog, prepend=1),
+                              data.censors)
+
+# We need to fit the model to get the parameters
+fitted = model.fit()
+print fitted.params()
+test1 = fitted.test_beta([4],[0])  # Test that the intercept is 4
+print test1
+test2 = fitted.test_beta([-.05], [0]) # Test that the slope is -.05
+print test2
