bfc3829882bc7c95ef668fbedd39a5f49f3f688c
Justin Grana <jg3705a@student.american.edu>
2012-08-12 20:07:55 -0400

    Fixed examples for empirical likelihood estimationa and inference for linear regression models.

--- statsmodels/examples/ex_emplike_2.py
+++ statsmodels/examples/ex_emplike_2.py
@@ -1,5 +1,6 @@
 """
-Second test script.  Used to demonstrate EL regression.
+This script is a basic tutorial on how to conduct empirical
+likelihood estimation and inference in linear regression models.
 
 I suggest running this script line by line because there will be many
 optimization results printed.  Nevertheless, it can be ran all at once.
@@ -9,68 +10,75 @@ import numpy as np
 import statsmodels.api as sm
 
 # Let's generate some regression data
-np.random.seed(100)
-X = np.random.standard_normal((40,3))
+np.random.seed(100)  # no significance of the seed
+X = np.random.standard_normal((40, 3))
 X = sm.add_constant(X, prepend=1)
-beta = np.array([[1],[2],[3],[4]])
-y= np.dot(X, beta).reshape(40,1) + np.random.standard_normal((40,1))
+beta = np.array([[1], [2], [3], [4]])
+y = np.dot(X, beta).reshape(40, 1) + np.random.standard_normal((40, 1))
 
 # There are no distributional assumptions on the error.  I just chose
 # normal errors to demonstrate.
 
 print 'Lets play with EL Regression'
-elmodel= sm.emplike.ElLinReg(y, X)
 
-# Let's test the hypothesis that beta1 (the first slope parameter) is 0
-print 'Example of optimization difficulties'
-opt_diff =  elmodel.hy_test_beta([0], [1])
-print opt_diff
-# Note a p value of 0 and an enormous LLR.  The p-value is close to accurate
-# but the LLR is way to high.  This is because 0 is not in the convex hull of
-# the data so the optimization cannot find the accurate llr.  I'm still
-# struggling with the topology theory but hope to add some insightful
-# documentation regarding this phenomenon.
+"""
+In a model with an intercept, there are two ways to conduct
+EL inference.  the first is to call the ElLinReg instance directly.
+The second is to access EL inference through OLS results.  We will go
+through both, first by directly using EL and then via OLS.
+"""
+
+elmodel = sm.emplike.ElLinReg(y, X)
+
 
 # Let's test if the intercept is 0
 print 'Intercept test'
-test0_1 = elmodel.hy_test_beta([0], [0])
+test0_1 = elmodel.test_beta([0], [0])
 print test0_1
 # Let's test if beta3 is 4
 print 'beta3 test'
-test1 = elmodel.hy_test_beta([4], [3])
+test1 = elmodel.test_beta([4], [3])
 print test1
 # Lets test the hypothesis that beta3=4 and beta2=3
 print 'joint beta test'
-test2 = elmodel.hy_test_beta([3,4], [2,3])
+test2 = elmodel.test_beta([3, 4], [2, 3])
 print test2
-# Although the log likelihoods are the same, the p-values are
-# different due to the increased degree of freedom.
 
 # Let's get the confidence intervals for the parameters
+print 'Confidence Interval for Beta1'
 ci_beta1 = elmodel.ci_beta(1)
-# The optimization results are the results of optimizing out the nuisance
-# parameters at each various values of beta.  With a significance level of
-# .05, the function is looking for the value of beta 1 that gives a llr
-# of 3.84
 print ci_beta1
+
+"""
+Now we'll see how to conduct EL inference through the OLS instance
+"""
+
+model = sm.OLS(y, X)
+fitted = model.fit()
+print 'The confidence interval for Beta 1 through OLS'
+print fitted.conf_int_el(1)
+print 'Test the hypothesis that Beta 3 = 3, it''s true value'
+print fitted.eltest([3], [2])  # Remember 0 indexing so Beta3 is parameter2
 
 #Now lets check out regression through the origin
 
-originx= np.random.standard_normal((30,3))
-originbeta=np.array([[1],[2],[3]])
-originy= np.dot(originx, originbeta) + np.random.standard_normal((30,1))
+originx = np.random.standard_normal((30, 3))
+originbeta = np.array([[1], [2], [3]])
+originy = np.dot(originx, originbeta) + np.random.standard_normal((30, 1))
 
-originmodel= sm.emplike.ElOriginRegress(originy, originx)
+originmodel = sm.emplike.ElOriginRegress(originy, originx)
 # Since in this case, parameter estimates are different then in OLS,
 # we need to fit the model.
 
-originmodel.fit()
+originfit = originmodel.fit()
 # Now, originmodel has new attributes
-print 'Some attributes'
 
-print originmodel.params
-print originmodel.mse_model
-print originmodel.rsquared
+print 'The fitted parameters'
+print originfit.params
+print 'The MSE'
+print originfit.mse_model
+print 'The R-squared'
+print originfit.rsquared
 
 # Note that the first element of param is 0 and there are 4 params.  That is
 # because the first param is the intercept term.  This is noted in the
@@ -79,10 +87,13 @@ print originmodel.rsquared
 # Now that the model is fitted, we can do some inference.
 
 print 'Test beta1 =1'
-hy_test_beta1= originmodel.hy_test_beta_origin(1,1)
-print hy_test_beta1
+test_beta1 = originfit.test_beta_origin([1], [1])
+print test_beta1
 
-# and a confidence interval
+# A confidence interval for Beta1. In this case, a user supplied minimum and
+# maximum is required.  This is because the optimization can easily get stuck
+# at very unrealistic values for the log-likelihood if the starting values for
+# the brent solver are too extreme.
 print 'confidence interval for beta2'
-ci_beta2= originmodel.ci_beta_origin(1, 1.5, .2)
+ci_beta2 = originfit.ci_beta_origin(1, 1.5, .2)
 print ci_beta2
