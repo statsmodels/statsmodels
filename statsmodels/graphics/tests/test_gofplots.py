1c064bf51c39c2bf2b541fd2e8abb8b0daf724c3
Paul Hobson <pmhobson@gmail.com>
2012-08-16 12:52:08 -0700

    expanded tests to reflect modifcations to the api and expanded coverage

--- statsmodels/graphics/tests/test_gofplots.py
+++ statsmodels/graphics/tests/test_gofplots.py
@@ -2,7 +2,8 @@ import numpy as np
 from numpy.testing import dec
 
 import statsmodels.api as sm
-from statsmodels.graphics.gofplots import qqplot
+from statsmodels.graphics.gofplots import qqplot, qqline, ProbPlot
+from scipy import stats
 
 
 try:
@@ -22,7 +23,31 @@ def test_qqplot():
     data.exog = sm.add_constant(data.exog)
     mod_fit = sm.OLS(data.endog, data.exog).fit()
     res = mod_fit.resid
-    fig1 = sm.qqplot(res)
-    fig2 = sm.qqplot(res, prob=True)
+    fig = sm.qqplot(res)
+
+    plt.close('all')
+
+@dec.skipif(not have_matplotlib)
+def test_ProbPlot():
+    #just test that it runs
+    data = sm.datasets.longley.load()
+    data.exog = sm.add_constant(data.exog)
+    mod_fit = sm.OLS(data.endog, data.exog).fit()
+    res = sm.ProbPlot(mod_fit.resid, stats.t, distargs=(4,))
+    fig1 = res.qqplot()
+    fig2 = res.ppplot()
+    fig3 = res.probplot()
+
+    plt.close('all')
+
+@dec.skipif(not have_matplotlib)
+def test_qqline():
+    #just test that it runs
+    data = sm.datasets.longley.load()
+    data.exog = sm.add_constant(data.exog)
+    mod_fit = sm.OLS(data.endog, data.exog).fit()
+    res = mod_fit.resid
+    for line in ['r', 'q', '45', 's']
+        fig = sm.qqplot(res, line=line)
 
     plt.close('all')
