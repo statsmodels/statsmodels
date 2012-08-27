b77a2255d88f9f5a780ad0ce7ad5c72f35bd7a43
Justin Grana <jg3705a@student.american.edu>
2012-08-27 16:43:47 -0400

    Added cancer data

--- statsmodels/datasets/__init__.py
+++ statsmodels/datasets/__init__.py
@@ -2,7 +2,7 @@
 Datasets module
 """
 #__all__ = filter(lambda s:not s.startswith('_'),dir())
-import anes96, committee, ccard, copper, cpunish, elnino, grunfeld, longley, \
+import anes96, cancer, committee, ccard, copper, cpunish, elnino, grunfeld, longley, \
         macrodata, nile, randhie, scotland, spector, stackloss, star98, \
         strikes, sunspots
 
