95945250dee6831173e203c36fbdf7349b71a747
langmore <ianlangmore@gmail.com>
2012-09-07 12:14:31 -0400

    Fixed typo in discrete_model

--- statsmodels/discrete/discrete_model.py
+++ statsmodels/discrete/discrete_model.py
@@ -1815,7 +1815,7 @@ class L1BinaryResults(BinaryResults):
     --------------
     nnz_params : Integer
         The number of nonzero parameters in the model.  Train with 
-        trim_parms==True or else numerical error will distort this.
+        trim_params==True or else numerical error will distort this.
     """
     def __init__(self, model, bnryfit):
         super(L1BinaryResults, self).__init__(model, bnryfit)
@@ -1902,7 +1902,7 @@ class L1MultinomialResults(MultinomialResults):
     --------------
     nnz_params : Integer
         The number of nonzero parameters in the model.  Train with 
-        trim_parms==True or else numerical error will distort this.
+        trim_params==True or else numerical error will distort this.
     """
     def __init__(self, model, mlefit):
         super(L1MultinomialResults, self).__init__(model, mlefit)
