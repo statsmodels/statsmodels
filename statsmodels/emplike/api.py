ac89661165d0cccedb05303d9ee1ca73d5fbfcea
Justin Grana <jg3705a@student.american.edu>
2012-09-13 14:19:13 -0400

    Edited api to import ANOVA

--- statsmodels/emplike/api.py
+++ statsmodels/emplike/api.py
@@ -6,4 +6,4 @@ api for empirical likelihood
 
 from .descriptive import DescStat, DescStatUV, DescStatMV
 from .originregress import ELOriginRegress
-#from .elregress import ElReg, ElOriginRegress, ANOVA
+from .elanova import ANOVA
