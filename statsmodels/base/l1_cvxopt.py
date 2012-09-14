a421d7a53444b5c1c31bb6a3c38ed28986b57190
langmore <ianlangmore@gmail.com>
2012-09-14 14:53:59 -0400

    Updated cvxopt and moved something around in slsqp

--- statsmodels/base/l1_cvxopt.py
+++ statsmodels/base/l1_cvxopt.py
@@ -3,12 +3,13 @@ Holds files for l1 regularization of LikelihoodModel, using cvxopt.
 """
 import numpy as np
 import pdb
+import l1_solvers_common
 from cvxopt import solvers, matrix
 # pdb.set_trace
 
 
 def _fit_l1_cvxopt_cp(
-        f, score, start_params, args, kwargs, disp=None, maxiter=100,
+        f, score, start_params, args, kwargs, disp=False, maxiter=100,
         callback=None, retall=False, full_output=False, hess=None):
     """
     Solve the l1 regularized problem using cvxopt.solvers.cp
@@ -48,23 +49,23 @@ def _fit_l1_cvxopt_cp(
     start_params = np.array(start_params).ravel('F')
 
     ## Extract arguments
-    # K is total number of covariates, possibly including a leading constant.
-    K = len(start_params)
+    # k_params is total number of covariates, possibly including a leading constant.
+    k_params = len(start_params)
+    # The start point
+    x0 = np.append(start_params, np.fabs(start_params))
+    x0 = matrix(x0, (2 * k_params, 1))
     # The regularization parameter
     alpha = np.array(kwargs['alpha']).ravel('F')
     # Make sure it's a vector
-    alpha = alpha * np.ones(K)
+    alpha = alpha * np.ones(k_params)
     assert alpha.min() >= 0
-    # The start point
-    x0 = np.append(start_params, np.fabs(start_params))
-    x0 = matrix(x0, (2 * K, 1))
 
     ## Wrap up functions for cvxopt
-    f_0 = lambda x: objective_func(f, x, K, alpha, *args)
-    Df = lambda x: fprime(score, x, K, alpha)
-    G = get_G(K)  # Inequality constraint matrix, Gx \leq h
-    h = matrix(0.0, (2 * K, 1))  # RHS in inequality constraint
-    H = lambda x, z: hessian_wrapper(hess, x, z, K)
+    f_0 = lambda x: objective_func(f, x, k_params, alpha, *args)
+    Df = lambda x: fprime(score, x, k_params, alpha)
+    G = get_G(k_params)  # Inequality constraint matrix, Gx \leq h
+    h = matrix(0.0, (2 * k_params, 1))  # RHS in inequality constraint
+    H = lambda x, z: hessian_wrapper(hess, x, z, k_params)
 
     ## Define the optimization function
     def F(x=None, z=None):
@@ -76,7 +77,7 @@ def _fit_l1_cvxopt_cp(
             return f_0(x), Df(x), H(x, z)
 
     ## Convert optimization settings to cvxopt form
-    solvers.options['show_progress'] = retall
+    solvers.options['show_progress'] = disp
     solvers.options['maxiters'] = maxiter
     if 'abstol' in kwargs:
         solvers.options['abstol'] = kwargs['abstol']
@@ -89,21 +90,26 @@ def _fit_l1_cvxopt_cp(
 
     ### Call the optimizer
     results = solvers.cp(F, G, h)
+    x = np.asarray(results['x']).ravel()
+    params = x[:k_params]
 
     ### Post-process
-    trim_params = kwargs.setdefault('trim_params', True)
-    if trim_params:
-        trim_tol = kwargs.setdefault('trim_tol', 'auto')
-        results = do_trim_params(
-            results, K, alpha, trim_tol, score)
-    else:
-        results['trimmed'] = np.asarray([False] * K)
+    # QC
+    QC_tol = kwargs['QC_tol']
+    QC_verbose = kwargs['QC_verbose']
+    passed, QC_dict = l1_solvers_common.QC_results(
+        params, alpha, score, QC_tol, QC_verbose)
+    # Possibly trim
+    trim_mode = kwargs['trim_mode']
+    size_trim_tol = kwargs['size_trim_tol']
+    auto_trim_tol = kwargs['auto_trim_tol']
+    params, trimmed = l1_solvers_common.do_trim_params(
+        params, k_params, alpha, score, passed, trim_mode, size_trim_tol,
+        auto_trim_tol)
 
     ### Pack up return values for statsmodels
     # TODO These retvals are returned as mle_retvals...but the fit wasn't ML
     if full_output:
-        x = np.asarray(results['x']).ravel()
-        params = x[:K]
         fopt = f_0(x)
         gopt = float('nan')  # Objective is non-differentiable
         hopt = float('nan')
@@ -112,10 +118,10 @@ def _fit_l1_cvxopt_cp(
             else results['status']
         retvals = {
             'fopt': fopt, 'converged': converged, 'iterations': iterations,
-            'gopt': gopt, 'hopt': hopt, 'trimmed': results['trimmed']}
+            'gopt': gopt, 'hopt': hopt, 'trimmed': trimmed}
     else:
         x = np.array(results['x']).ravel()
-        params = x[:K]
+        params = x[:k_params]
 
     ### Return results
     if full_output:
@@ -124,82 +130,37 @@ def _fit_l1_cvxopt_cp(
         return params
 
 
-def do_trim_params(results, K, alpha, trim_tol, score):
-    """
-    If trim_tol == 'auto', then trim params if the derivative in that direction
-        is significantly smaller than alpha.  Theory says the nonzero params
-        should have magnitude equal to alpha at a minimum.
-
-    If trim_tol is a float, then trim (set = 0) params that are within trim_tol
-        of zero.  
-
-    In all cases, if alpha[i] == 0, then don't trim the ith param.  
-    In all cases, do nothing with the dummy variables.
-    """
-    ## Extract params from the results
-    x_arr = np.array(results['x'])
-    ## Trim the small params
-    trimmed = [False] * K  
-    # Don't bother triming the dummy variables 'u'
-    if trim_tol == 'auto':
-        fprime = score(x_arr[:K].ravel())
-        for i in xrange(K):
-            if alpha[i] != 0:
-                # TODO Magic number !!
-                magic_tol = 0.03
-                if alpha[i] - abs(fprime[i]) > magic_tol:
-                    x_arr[i] = 0.0
-                    trimmed[i] = True
-                # If fprime is too big, then we didn't converge properly 
-                # and we shouldn't trust the automatic trimming
-                elif alpha[i] - abs(fprime[i]) < -magic_tol:
-                    raise Exception(
-                        "Unable to trim params automatically with "\
-                        "this low optimization accuracy")
-    else:
-        for i in xrange(K):
-            if alpha[i] != 0:
-                if abs(x_arr[i]) < trim_tol:
-                    x_arr[i] = 0.0
-                    trimmed[i] = True
-    ## Replenish results
-    results['x'] = matrix(x_arr)
-    results['trimmed'] = np.array(trimmed)
-    ## Return
-    return results
-
-
-def objective_func(f, x, K, alpha, *args):
+def objective_func(f, x, k_params, alpha, *args):
     """
     The regularized objective function.
     """
     x_arr = np.asarray(x)
-    params = x_arr[:K].ravel()
-    u = x_arr[K:]
+    params = x_arr[:k_params].ravel()
+    u = x_arr[k_params:]
     # Call the numpy version
     objective_func_arr = f(params, *args) + (alpha * u).sum()
     # Return
     return matrix(objective_func_arr)
 
 
-def fprime(score, x, K, alpha):
+def fprime(score, x, k_params, alpha):
     """
     The regularized derivative.
     """
     x_arr = np.asarray(x)
-    params = x_arr[:K].ravel()
+    params = x_arr[:k_params].ravel()
     # Call the numpy version
     # The derivative just appends a vector of constants
     fprime_arr = np.append(score(params), alpha)
     # Return
-    return matrix(fprime_arr, (1, 2 * K))
+    return matrix(fprime_arr, (1, 2 * k_params))
 
 
-def get_G(K):
+def get_G(k_params):
     """
     The linear inequality constraint matrix.
     """
-    I = np.eye(K)
+    I = np.eye(k_params)
     A = np.concatenate((-I, -I), axis=1)
     B = np.concatenate((I, -I), axis=1)
     C = np.concatenate((A, B), axis=0)
@@ -207,7 +168,7 @@ def get_G(K):
     return matrix(C)
 
 
-def hessian_wrapper(hess, x, z, K):
+def hessian_wrapper(hess, x, z, k_params):
     """
     Wraps the hessian up in the form for cvxopt.
 
@@ -215,10 +176,10 @@ def hessian_wrapper(hess, x, z, K):
         Since our constraints are linear, this part is all zeros.
     """
     x_arr = np.asarray(x)
-    params = x_arr[:K].ravel()
+    params = x_arr[:k_params].ravel()
     zh_x = np.asarray(z[0]) * hess(params)
     zero_mat = np.zeros(zh_x.shape)
     A = np.concatenate((zh_x, zero_mat), axis=1)
     B = np.concatenate((zero_mat, zero_mat), axis=1)
     zh_x_ext = np.concatenate((A, B), axis=0)
-    return matrix(zh_x_ext, (2 * K, 2 * K))
+    return matrix(zh_x_ext, (2 * k_params, 2 * k_params))
