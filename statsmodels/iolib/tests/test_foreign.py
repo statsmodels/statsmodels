8b18e23b723c2583b0a2d550e19af440c6dd3de2
Skipper Seabold <jsseabold@gmail.com>
2012-10-08 11:34:03 -0400

    TST: Add some StataWriter tests

--- statsmodels/iolib/tests/test_foreign.py
+++ statsmodels/iolib/tests/test_foreign.py
@@ -2,12 +2,15 @@
 Tests for iolib/foreign.py
 """
 
+from StringIO import StringIO
 from numpy.testing import *
 import numpy as np
 import statsmodels.api as sm
 import os
 from statsmodels.iolib.foreign import StataWriter, genfromdta
 from statsmodels.datasets import macrodata
+from pandas import DataFrame
+import pandas.util.testing as ptesting
 
 # Test precisions
 DECIMAL_4 = 4
@@ -36,8 +39,7 @@ def test_genfromdta_pandas():
     res1 = res1.astype(float)
     assert_frame_equal(res1, dta)
 
-def test_stata_writer():
-    from StringIO import StringIO
+def test_stata_writer_structured():
     buf = StringIO()
     dta = macrodata.load().data
     dtype = dta.dtype
@@ -48,6 +50,43 @@ def test_stata_writer():
     buf.seek(0)
     dta2 = genfromdta(buf)
     assert_array_equal(dta, dta2)
+
+def test_stata_writer_array():
+    buf = StringIO()
+    dta = macrodata.load().data
+    dta = DataFrame.from_records(dta)
+    dta.columns = ["v%d" % i for i in range(1,15)]
+    writer = StataWriter(buf, dta.values)
+    writer.write_file()
+    buf.seek(0)
+    dta2 = genfromdta(buf)
+    dta = dta.to_records(index=False)
+    assert_array_equal(dta, dta2)
+
+def test_stata_writer_missing():
+    pass
+
+def test_stata_writer_pandas():
+    buf = StringIO()
+    dta = macrodata.load().data
+    dtype = dta.dtype
+    #as of 0.9.0 pandas only supports i8 and f8
+    dta = dta.astype(np.dtype([('year', 'i8'),
+                               ('quarter', 'i8')] + dtype.descr[2:]))
+    dta = DataFrame.from_records(dta)
+    writer = StataWriter(buf, dta)
+    writer.write_file()
+    buf.seek(0)
+    dta2 = genfromdta(buf)
+    ptesting.assert_frame_equal(dta.reset_index(), DataFrame.from_records(dta2))
+
+
+def test_stata_writer_unicode():
+    # make sure to test with characters outside the latin-1 encoding
+    pass
+
+def test_stata_writer_datetime():
+    pass
 
 
 if __name__ == "__main__":
