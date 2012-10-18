93b75ebc0f1a061bab12556a5cf7d5b2b1310aa5
Ralf Gommers <ralf.gommers@googlemail.com>
2012-10-18 20:46:13 +0200

    MAINT: further simplify nonparametric kernels.

--- statsmodels/nonparametric/kernels.py
+++ statsmodels/nonparametric/kernels.py
@@ -11,30 +11,17 @@ kernel density estimation much easier.
 NOTE: As it is, this module does not interact with the existing API
 """
 
+
 import numpy as np
 from scipy.special import erf
 
 
-def _get_shape_and_transform(h, Xi, x=None):
-    """
-    Utility function to transform arrays and check shape parameters.
-    """
-    # More than one variable with more than one observations
-    if Xi.ndim > 1:
-        K = Xi.shape[1]
-        N = Xi.shape[0]
-    elif Xi.ndim == 1:  # One variable with many observations
-        K = 1
-        N = Xi.shape[0]
-    else:  # ndim ==0 so Xi is a single point (number)
-        K = 1
-        N = 1
-
-    Xi = Xi.reshape([N, K])
-    return h, Xi, x, N, K
+#TODO:
+# - make sure we only receive int input for wang-ryzin and aitchison-aitken
+# - Check for the scalar Xi case everywhere
 
 
-def aitchison_aitken(h, Xi, x, num_levels=False):
+def aitchison_aitken(h, Xi, x, num_levels=None):
     """
     The Aitchison-Aitken kernel, used for unordered discrete random variables.
 
@@ -69,14 +56,10 @@ def aitchison_aitken(h, Xi, x, num_levels=False):
     .. [2] Racine, Jeff. "Nonparametric Econometrics: A Primer," Foundation
            and Trends in Econometrics: Vol 3: No 1, pp1-88., 2008.
     """
-    h, Xi, x, N, K = _get_shape_and_transform(h, Xi, x)
-    x = np.asarray(x, dtype=int)
-    if num_levels:
-        c = num_levels
-    else:
-        c = np.asarray([len(np.unique(Xi[:, i])) for i in range(K)], dtype=int)
+    if num_levels is None:
+        num_levels = np.asarray(np.unique(Xi).size)
 
-    kernel_value = np.tile(h / (c - 1), (N, 1))
+    kernel_value = np.ones(Xi.size) * h / (num_levels - 1)
     idx = Xi == x
     kernel_value[idx] = (idx * (1 - h))[idx]
     return kernel_value
@@ -140,7 +123,6 @@ def gaussian(h, Xi, x):
         The value of the kernel function at each training point for each var.
 
     """
-    h, Xi, x, N, K = _get_shape_and_transform(h, Xi, x)
     z = (Xi - x) / h
     kernel_value = (1. / np.sqrt(2 * np.pi)) * np.exp(- z ** 2 / 2.)
     return kernel_value
@@ -148,7 +130,6 @@ def gaussian(h, Xi, x):
 
 def gaussian_convolution(h, Xi, x):
     """ Calculates the Gaussian Convolution Kernel """
-    h, Xi, x, N, K = _get_shape_and_transform(h, Xi, x)
     z = (Xi - x) / h
     kernel_value = (1. / np.sqrt(4 * np.pi)) * np.exp(- z ** 2 / 4.)
     return kernel_value
@@ -158,101 +139,50 @@ def wang_ryzin_convolution(h, Xi, Xj):
     # This is the equivalent of the convolution case with the Gaussian Kernel
     # However it is not exactly convolution. Think of a better name
     # References
-    Xi = Xi.astype(int)
-    Xj = np.asarray(Xj, dtype=int)
-
-    Ordered = np.zeros((Xi.size, 1))
+    ordered = np.zeros((Xi.size, 1))
     for x in np.unique(Xi):
-        Ordered += wang_ryzin(h, Xi, x) * wang_ryzin(h, Xj, x)
+        ordered += wang_ryzin(h, Xi, x) * wang_ryzin(h, Xj, x)
 
-    return Ordered
+    return ordered
 
 
 def aitchison_aitken_convolution(h, Xi, Xj):
-    h, Xi, x, N, K = _get_shape_and_transform(h, Xi)
-    Xi = np.abs(Xi.astype(int))
-    Xj = np.abs(np.asarray(Xj, dtype=int))
-    Dom_x = [np.unique(Xi[:, i]) for i in range(K)]
-    Ordered = np.empty([N, K])
-    for i in range(K):
-        Sigma_x = np.zeros((N, 1))
-        num_levels = Dom_x[i].size
-        for x in Dom_x[i]:
-            Sigma_x += aitchison_aitken(h[i], Xi[:, i], x,
-                                        num_levels=num_levels) * \
-                       aitchison_aitken(h[i], Xj[i], x,
-                                        num_levels=num_levels)
+    Xi_vals = np.unique(Xi)
+    ordered = np.zeros((Xi.size, 1))
+    num_levels = Xi_vals.size
+    for x in Xi_vals:
+        ordered += aitchison_aitken(h, Xi, x, num_levels=num_levels) * \
+                   aitchison_aitken(h, Xj, x, num_levels=num_levels)
 
-        Ordered[:, i] = Sigma_x[:, 0]
-
-    return Ordered
+    return ordered
 
 
 def gaussian_cdf(h, Xi, x):
-    h, Xi, x, N, K = _get_shape_and_transform(h, Xi, x)
-    cdf = 0.5 * h * (1 + erf((x - Xi) / (h * np.sqrt(2))))
-    return cdf
+    return 0.5 * h * (1 + erf((x - Xi) / (h * np.sqrt(2))))
 
 
 def aitchison_aitken_cdf(h, Xi, x_u):
-    Xi = np.abs(np.asarray(Xi, dtype=int))
-    if Xi.ndim > 1:
-        K = np.shape(Xi)[1]
-        N = np.shape(Xi)[0]
-    elif Xi.ndim == 1:
-        K = 1
-        N = np.shape(Xi)[0]
-    else:  # ndim ==0 so Xi is a single point (number)
-        K = 1
-        N = 1
+    x_u = int(x_u)
+    Xi_vals = np.unique(Xi)
+    ordered = np.zeros(Xi.size)
+    num_levels = Xi_vals.size
+    for x in Xi_vals:
+        if x <= x_u:  #FIXME: why a comparison for unordered variables?
+            ordered += aitchison_aitken(h, Xi, x, num_levels=num_levels)
 
-    h = np.asarray(h, dtype=float)
-    Xi = Xi.reshape([N, K])
-    Dom_x = [np.unique(Xi[:, i]) for i in range(K)]
-    Ordered = np.empty([N, K])
-    for i in range(K):
-        Sigma_x = 0
-        for x in Dom_x[i]:
-            if x <= x_u:
-                Sigma_x += aitchison_aitken(h, Xi[:, i], int(x),
-                                           num_levels=len(Dom_x[i]))
-
-        Ordered[:, i] = Sigma_x[:, 0]
-
-    return Ordered
+    return ordered
 
 
 def wang_ryzin_cdf(h, Xi, x_u):
-    Xi = np.abs(np.asarray(Xi, dtype=int))
-    h = np.asarray(h, dtype=float)
-    if Xi.ndim > 1:
-        K = np.shape(Xi)[1]
-        N = np.shape(Xi)[0]
-    elif Xi.ndim == 1:
-        K = 1
-        N = np.shape(Xi)[0]
-    else:  # ndim ==0 so Xi is a single point (number)
-        K = 1
-        N = 1
+    ordered = np.zeros(Xi.size)
+    for x in np.unique(Xi):
+        if x <= x_u:
+            ordered += wang_ryzin(h, Xi, x)
 
-    Xi = Xi.reshape([N, K])
-    h = h.reshape((K, ))
-    Dom_x = [np.unique(Xi[:, i]) for i in range(K)]
-    Ordered = np.empty([N, K])
-    for i in range(K):
-        Sigma_x = 0
-        for x in Dom_x[i]:
-            if x <= x_u:
-                Sigma_x += wang_ryzin(h[i], Xi[:, i], x)
-
-        Ordered[:, i] = Sigma_x[:, 0]
-    return Ordered
+    return ordered
 
 def d_gaussian(h, Xi, x):
     # The derivative of the Gaussian Kernel
-    Xi = np.asarray(Xi)
-    x = np.asarray(x)
-    h = np.asarray(h, dtype=float)
     z = (Xi - x) / h
     value = np.exp(-z ** 2 / 2.) * (Xi - x) / (np.sqrt(2 * np.pi) * h ** 2)
     value = 2 * ( x - Xi) * gaussian(h, Xi, x) / (h ** 2)
@@ -264,10 +194,10 @@ def aitchison_aitken_reg(h, Xi, x):
 
     Suggested by Li and Racine.
     """
-    h, Xi, x, N, K = _get_shape_and_transform(h, Xi, x)
-    kernel_value = np.ones((N,K))
-    inDom = (Xi != x) * h
-    kernel_value[Xi != x] = inDom[Xi != x]
+    kernel_value = np.ones(Xi.size)
+    ix = Xi != x
+    inDom = ix * h
+    kernel_value[ix] = inDom[ix]
     return kernel_value
 
 
@@ -277,6 +207,5 @@ def wang_ryzin_reg(h, Xi, x):
 
     Suggested by Li and Racine in [1] ch.4
     """
-    h, Xi, x, N, K = _get_shape_and_transform(h, Xi, x)
-    kernel_value = h ** abs(Xi - x)
-    return kernel_value
+    return h ** abs(Xi - x)
+
