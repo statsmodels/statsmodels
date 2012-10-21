e8f0e882fdf81d9f6c4b223922d743428b65eea7
Ralf Gommers <ralf.gommers@googlemail.com>
2012-10-21 21:17:34 +0200

    MAINT: some more cleanups.

--- statsmodels/nonparametric/np_tools.py
+++ statsmodels/nonparametric/np_tools.py
@@ -51,12 +51,10 @@ class LeaveOneOut(object):
 
 
 def _get_type_pos(var_type):
-    var_type = np.asarray(list(var_type))
-    #iscontinuous = np.where(var_type == 'c')[0]
-    iscontinuous = np.array([c == 'c' for c in var_type])
-    isordered = np.where(var_type == 'o')[0]
-    isunordered = np.where(var_type == 'u')[0]
-    return iscontinuous, isordered, isunordered
+    ix_cont = np.array([c == 'c' for c in var_type])
+    ix_ord = np.array([c == 'o' for c in var_type])
+    ix_unord = np.array([c == 'u' for c in var_type])
+    return ix_cont, ix_ord, ix_unord
 
 
 def adjust_shape(dat, K):
