edcdfa20c449602aef0fdc9545b6d105fd731acc
Josef Perktold <josef.pktd@gmail.com>
2012-10-31 15:07:23 -0400

    ENH: Add add_lowess plot function

--- statsmodels/graphics/regressionplots.py
+++ statsmodels/graphics/regressionplots.py
@@ -16,6 +16,7 @@ import numpy as np
 from statsmodels.regression.linear_model import OLS
 from statsmodels.sandbox.regression.predstd import wls_prediction_std
 from statsmodels.graphics import utils
+from statsmodels.nonparametric import lowess
 
 
 __all__ = ['plot_fit', 'plot_regress_exog', 'plot_partregress', 'plot_ccpr',
@@ -26,6 +27,32 @@ def _high_leverage(results):
     #TODO: replace 1 with k_constant
     return 2. * (results.df_model + 1)/results.nobs
 
+def add_lowess(ax, lines_idx=0, frac=.2, **lowess_kwargs)
+    """
+    Add Lowess line to a plot.
+
+    Parameters
+    ----------
+    ax : matplotlib Axes instance
+        The Axes to which to add the plot
+    lines_idx : int
+        This is the line on the existing plot to which you want to add
+        a smoothed lowess line.
+    frac : float
+        The fraction of the points to use when doing the lowess fit.
+    lowess_kwargs
+        Additional keyword arguments are passes to lowess.
+
+    Returns
+    -------
+    fig : matplotlib Figure instance
+        The figure that holds the instance.
+    """
+    y0 = ax.get_lines()[lines_idx]._y
+    x0 = ax.get_lines()[lines_idx]._x
+    lres = lowess(y0, x0, frac=frac, **lowess_kwargs)
+    ax.plot(lres[:,0], lres[:,1], 'r', lw=1.5)
+    return fig
 
 def plot_fit(results, exog_idx, y_true=None, ax=None, **kwargs):
     """Plot fit against one regressor.
