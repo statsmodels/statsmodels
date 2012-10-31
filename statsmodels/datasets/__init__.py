549ccc8d426526d463a7e9438c95b648161474bf
Skipper Seabold <jsseabold@gmail.com>
2012-10-31 19:31:17 -0400

    ENH: Add get_rdatasets for dl'ing and caching from Rdatasets package

--- statsmodels/datasets/__init__.py
+++ statsmodels/datasets/__init__.py
@@ -5,3 +5,4 @@ Datasets module
 from . import (anes96, cancer, committee, ccard, copper, cpunish, elnino,
                grunfeld, longley, macrodata, nile, randhie, scotland, spector,
                stackloss, star98, strikes, sunspots, fair, heart, statecrime)
+from utils import get_rdataset, get_data_home, clear_data_home
--- statsmodels/datasets/utils.py
+++ statsmodels/datasets/utils.py
@@ -1,7 +1,18 @@
-import os
+import shutil
+from os import environ
+from os import makedirs
+from os.path import basename
+from os.path import expanduser
+from os.path import exists
+from os.path import expanduser
+from os.path import join
+from StringIO import StringIO
 import time
+import httplib2
+
 import numpy as np
 from numpy import genfromtxt, array
+
 
 class Dataset(dict):
     def __init__(self, **kw):
@@ -86,3 +97,140 @@ def process_recarray_pandas(data, endog_idx=0, exog_idx=None, dtype=None,
     dataset = Dataset(data=data, names=list(names), endog=endog, exog=exog,
                       endog_name=endog_name, exog_name=exog_name)
     return dataset
+
+def _maybe_reset_index(data):
+    """
+    All the Rdatasets have the integer row.labels from R if there is no
+    real index. Strip this for a zero-based index
+    """
+    from pandas import Index
+    if data.index.equals(Index(range(1,len(data)+1))):
+        data = data.reset_index(drop=True)
+    return data
+
+def _get_rdatasets_name(dataname):
+    """
+    Grabs a list of all csv files and tries to match versus one in a case
+    insensitive way. Could be extended to do fuzzy matching
+    """
+    from urllib2 import urlopen
+    from HTMLParser import HTMLParser
+    base_url = ("https://github.com/vincentarelbundock/Rdatasets/tree/"
+                "master/csv")
+    html = urlopen(base_url).read()
+    class MyHTMLParser(HTMLParser):
+        result = None
+        def handle_starttag(self, tag, attrs):
+            if tag == "a":
+                for name, value in attrs:
+                    if name == "href" and value.endswith('.csv'):
+                        #from IPython.core.debugger import Pdb; Pdb().set_trace()
+                        base = basename(value)
+                        if base.lower() == dataname + ".csv":
+                            self.result = base[:-4]
+                            raise StopIteration
+
+    parser = MyHTMLParser()
+    try:
+        parser.feed(html)
+    except StopIteration:
+        pass
+    result = parser.result
+    return result
+
+def _open_w_404_handling(connection, base_url, dataname):
+    url = base_url + "%s.csv" % dataname
+    response, data = connection.request(url)
+    if response.status == 404:
+        # try a little harder to find the dataset
+        new_dataname = _get_rdatasets_name(dataname)
+        if new_dataname:
+            url = base_url + "%s.csv" % new_dataname
+            response, data = connection.request(url)
+        else:
+            raise
+        if response.status == 404:
+            raise
+
+    # python 3 compatibility
+    import sys
+    if sys.version[0] == '3':  # pragma: no cover
+        data = data.decode('ascii', errors='strict')
+    return StringIO(data)
+
+def _get_data(base_url, dataname, cache):
+    if cache is False:
+        # do not do any caching or load from cache
+        cache = None
+    elif cache is True: # use default dir for cache
+        cache = get_data_home(None)
+    else:
+        cache = get_data_home(cache)
+
+    connection = httplib2.Http(cache)
+    data = _open_w_404_handling(connection, base_url, dataname)
+    return data
+
+def get_rdataset(dataname, cache=False):
+    """
+    Parameters
+    ----------
+    dataname : str
+        The name of the dataset you want to download
+    cache : bool or str
+        If True, will download this data into the STATSMODELS_DATA folder.
+        The default location is a folder called statsmodels_data in the
+        user home folder. Otherwise, you can specify a path to a folder to
+        use for caching the data. If False, the data will not be cached.
+
+    Returns
+    -------
+    dataframe : pandas.DataFrame
+        A dataframe containing the requested dataset.
+
+    Notes
+    -----
+    If the R dataset has an integer index. This is reset to be zero-based.
+    Otherwise the index is preserved. While the function will do its best not
+    to be case-sensitive. If you want to use the caching facilities, you need
+    to give the case-sensitive name of the dataset.
+    """
+    #NOTE: use raw github bc html site might not be most up to date
+    base_url = ("https://raw.github.com/vincentarelbundock/Rdatasets/master/"
+                "csv/")
+    data = _get_data(base_url, dataname, cache)
+    from pandas import read_csv
+    data = read_csv(data, index_col=0)
+    data = _maybe_reset_index(data)
+    return data
+
+### The below function were taken from sklearn
+
+def get_data_home(data_home=None):
+    """Return the path of the scikit-learn data dir.
+
+    This folder is used by some large dataset loaders to avoid
+    downloading the data several times.
+
+    By default the data dir is set to a folder named 'statsmodels_data'
+    in the user home folder.
+
+    Alternatively, it can be set by the 'STATSMODELS_DATA' environment
+    variable or programatically by giving an explit folder path. The
+    '~' symbol is expanded to the user home folder.
+
+    If the folder does not already exist, it is automatically created.
+    """
+    if data_home is None:
+        data_home = environ.get('STATSMODELS_DATA',
+                               join('~', 'statsmodels_data'))
+    data_home = expanduser(data_home)
+    if not exists(data_home):
+        makedirs(data_home)
+    return data_home
+
+
+def clear_data_home(data_home=None):
+    """Delete all the content of the data home cache."""
+    data_home = get_data_home(data_home)
+    shutil.rmtree(data_home)
