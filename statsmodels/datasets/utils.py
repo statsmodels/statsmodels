4b6a665a5900f0a0ecf672e70aa23c01bfe182e4
Skipper Seabold <jsseabold@gmail.com>
2012-11-14 15:14:58 -0500

    STY: Comment indents

--- statsmodels/datasets/utils.py
+++ statsmodels/datasets/utils.py
@@ -19,8 +19,8 @@ class Dataset(dict):
     def __init__(self, **kw):
         dict.__init__(self,kw)
         self.__dict__ = self
-# Some datasets have string variables. If you want a raw_data attribute you
-# must create this in the dataset's load function.
+        # Some datasets have string variables. If you want a raw_data
+        # attribute you must create this in the dataset's load function.
         try: # some datasets have string variables
             self.raw_data = self.data.view((float, len(self.names)))
         except:
