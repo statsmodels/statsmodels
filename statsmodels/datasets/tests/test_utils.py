d9549167cb444d3e27180bf57082d861adc06778
Skipper Seabold <jsseabold@gmail.com>
2012-11-15 10:27:02 -0500

    TST: Skip test on Py3. Works but can't test both

--- statsmodels/datasets/tests/test_utils.py
+++ statsmodels/datasets/tests/test_utils.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from statsmodels.datasets import get_rdataset
 from numpy.testing import assert_
 
@@ -6,5 +7,13 @@ cur_dir = os.path.dirname(os.path.abspath(__file__))
 
 def test_get_rdataset():
     # smoke test
-    duncan = get_rdataset("Duncan", "car", cache=cur_dir)
-    assert_(duncan.from_cache)
+    if sys.version_info[0] >= 3:
+        #NOTE: there's no way to test both since the cached files were
+        #created with Python 2.x, they're strings, but Python 3 expects
+        #bytes and the index file path is hard-coded so both can't live
+        #side by side
+        pass
+        #duncan = get_rdataset("Duncan-py3", "car", cache=cur_dir)
+    else:
+        duncan = get_rdataset("Duncan", "car", cache=cur_dir)
+        assert_(duncan.from_cache)
