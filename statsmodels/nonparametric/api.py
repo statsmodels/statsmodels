d06fe4b98c319537bc0ed888bd988a822e488dc5
Ralf Gommers <ralf.gommers@googlemail.com>
2012-11-17 15:07:50 +0100

    MAINT: empty out __init__.py and fix examples to use statsmodels.api

--- statsmodels/nonparametric/api.py
+++ statsmodels/nonparametric/api.py
@@ -1,3 +1,7 @@
 from kde import KDE
 from smoothers_lowess import lowess
 import bandwidths
+
+from kernel_density import \
+    KDEMultivariate, KDEMultivariateConditional, EstimatorSettings
+from kernel_regression import Reg, CensoredReg
