c37eb06abab4d5da544b6fe8635f5d971baed9e6
Skipper Seabold <jsseabold@gmail.com>
2012-11-19 17:23:23 -0500

    TST: Test patsy DesignMatrix with exog == None

--- statsmodels/tools/tests/test_data.py
+++ statsmodels/tools/tests/test_data.py
@@ -39,3 +39,13 @@ def test_dataframe():
     np.testing.assert_equal(vals, df.values)
     np.testing.assert_equal(rnames.tolist(), df.index.tolist())
     np.testing.assert_equal(cnames, df.columns.tolist())
+
+def test_patsy_577():
+    X = np.random.random((10, 2))
+    df = pandas.DataFrame(X, columns=["var1", "var2"])
+    from patsy import dmatrix
+    endog = dmatrix("var1 - 1", df)
+    np.testing.assert_(data._is_using_patsy(endog, None))
+    exog = dmatrix("var2 - 1", df)
+    np.testing.assert_(data._is_using_patsy(endog, exog))
+
