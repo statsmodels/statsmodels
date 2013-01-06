78d687b8f63c7235371b389362a057463a4e2a06
Josef Perktold <josef.pktd@gmail.com>
2013-01-06 16:23:31 -0500

    DOC: docstrings dgp_examples

--- statsmodels/nonparametric/dgp_examples.py
+++ statsmodels/nonparametric/dgp_examples.py
@@ -29,10 +29,44 @@ def fg2(x):
     '''
     return np.sin(2 * x) + 2 * np.exp(-16 * x**2)
 
+doc = {'description':
+'''Base Class for Univariate non-linear example
+
+    Does not work on it's own.
+    needs additional at least self.func
+''',
+'ref': ''}
 
 class _UnivariateFanGijbels(object):
-    '''Fan and Gijbels example function 1
-    '''
+    __doc__ = '''%(description)s
+
+    Parameters
+    ----------
+    nobs : int
+        number of observations to simulate
+    x : None or 1d array
+        If x is given then it is used for the exogenous variable instead of
+        creating a random sample
+    distr_x : None or distribution instance
+        Only used if x is None. The rvs method is used to create a random
+        sample of the exogenous (explanatory) variable.
+    distr_noise : None or distribution instance
+        The rvs method is used to create a random sample of the errors.
+
+    Attributes
+    ----------
+    x : ndarray, 1-D
+        exogenous or explanatory variable. x is sorted.
+    y : ndarray, 1-D
+        endogenous or response variable
+    y_true : ndarray, 1-D
+        expected values of endogenous or response variable, i.e. values of y
+        without noise
+    func : callable
+        underlying function (defined by subclass)
+
+    %(ref)s
+    ''' #% doc
 
     def __init__(self, nobs=200, x=None, distr_x=None, distr_noise=None):
 
@@ -54,6 +88,23 @@ class _UnivariateFanGijbels(object):
 
 
     def plot(self, scatter=True, ax=None):
+        '''plot the mean function and optionally the scatter of the sample
+
+        Parameters
+        ----------
+        scatter: bool
+            add scatterpoints of sample to plot
+        ax : None or matplotlib axis instance
+            If None, then a matplotlib.pyplot figure is created, otherwise
+            the given axis, ax, is used.
+
+        Returns
+        -------
+        fig : matplotlib figure
+            This is either the created figure instance or the one associated
+            with ax if ax is given.
+
+        '''
         if ax is None:
             import matplotlib.pyplot as plt
             fig = plt.figure()
@@ -66,7 +117,24 @@ class _UnivariateFanGijbels(object):
         ax.plot(xx, self.func(xx), lw=2, color='b', label='dgp mean')
         return ax.figure
 
+doc = {'description':
+'''Fan and Gijbels example function 1
+
+linear trend plus a hump
+''',
+'ref':
+'''
+References
+----------
+Fan, Jianqing, and Irene Gijbels. 1992. "Variable Bandwidth and Local
+Linear Regression Smoothers."
+The Annals of Statistics 20 (4) (December): 2008-2036. doi:10.2307/2242378.
+
+'''}
+
 class UnivariateFanGijbels1(_UnivariateFanGijbels):
+    __doc__ = _UnivariateFanGijbels.__doc__ % doc
+
 
     def __init__(self, nobs=200, x=None, distr_x=None, distr_noise=None):
         self.s_x = 1.
@@ -76,7 +144,14 @@ class UnivariateFanGijbels1(_UnivariateFanGijbels):
                                              distr_x=distr_x,
                                              distr_noise=distr_noise)
 
+doc['description'] =\
+'''Fan and Gijbels example function 2
+
+sin plus a hump
+'''
+
 class UnivariateFanGijbels2(_UnivariateFanGijbels):
+    __doc__ = _UnivariateFanGijbels.__doc__ % doc
 
     def __init__(self, nobs=200, x=None, distr_x=None, distr_noise=None):
         self.s_x = 1.
