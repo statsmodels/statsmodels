42a9bbff0c89d3f173c6e55c1382af4a84a45a48
Josef Perktold <josef.pktd@gmail.com>
2013-01-06 10:50:05 -0500

    DOC: move examples out of dgp_example

---
+++ statsmodels/examples/ex_kernel_regression_dgp.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+
+Created on Sun Jan 06 09:50:54 2013
+
+Author: Josef Perktold
+"""
+
+if __name__ == '__main__':
+
+    from statsmodels.nonparametric.api import KernelReg
+    import statsmodels.nonparametric.dgp_examples as dgp
+    import matplotlib.pyplot as plt
+
+    from scipy import stats
+
+    funcs = [dgp.UnivariateFanGijbels1(),
+             dgp.UnivariateFanGijbels2(),
+             dgp.UnivariateFanGijbels1EU(),
+             dgp.UnivariateFanGijbels2(distr_x=stats.uniform(-2, 4))
+             ]
+
+    res = []
+    fig = plt.figure()
+    for i,func in enumerate(funcs):
+        #f = func()
+        f = func
+        model = KernelReg(endog=[f.y], exog=[f.x], reg_type='ll',
+                          var_type='c', bw='cv_ls')
+        mean, mfx = model.fit()
+        ax = fig.add_subplot(2, 2, i+1)
+        f.plot(ax=ax)
+        ax.plot(f.x, mean, color='r', lw=2, label='est. mean')
+        ax.legend(loc='upper left')
+        res.append((model, mean, mfx))
+
+    fig.suptitle('Kernel Regression')
+    fig.show()
