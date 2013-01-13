092d6bf3290e37555b7183ec8cf4dd63bab91c16
Josef Perktold <josef.pktd@gmail.com>
2013-01-13 15:05:32 -0500

    REF: cleanup initial script, remove experimental parts

--- statsmodels/stats/power.py
+++ statsmodels/stats/power.py
@@ -17,16 +17,7 @@ beta   0.8 0.8
 """
 
 import numpy as np
-from scipy import stats
-
-#power
-d = 0.68  #effect size
-nobs = 20  #observations
-alpha = 0.05
-alpha_two = alpha / 2.
-crit = stats.t.isf(alpha_two, nobs-1) #critical value at df=nobs
-pow_t = stats.nct(nobs-1, d*np.sqrt(nobs)).sf(stats.t.isf(alpha_two, nobs-1))
-pow_f = stats.ncf(1, nobs-1, d**2*nobs).sf(stats.f.isf(alpha, 1, nobs-1))
+from scipy import stats, optimize
 
 
 def ttest_power(effect_size, nobs, alpha, df=None, alternative='two-sided'):
@@ -46,84 +37,12 @@ def ttest_power(effect_size, nobs, alpha, df=None, alternative='two-sided'):
     pow_ = stats.nct(df, d*np.sqrt(nobs)).sf(stats.t.isf(alpha_, df))
     return pow_
 
-
-from scipy import optimize
-
-def solve_power_nobs(effect_size, alpha, beta):
-    '''solve for required sample size for t-test to obtain power beta
-    '''
-
-    pow_n = lambda nobs: ttest_power(effect_size, nobs, alpha) - beta
-    return optimize.fsolve(pow_n, 10).item()
-
-effect_size, alpha, beta = 0.5, 0.05, 0.8
-nobs_p = solve_power_nobs(effect_size, alpha, beta)
-print nobs_p
-print ttest_power(effect_size, nobs_p, alpha), beta
-
-
-#generic implementation
-#----------------------
-
-#to be reused for other tests
-
-def ttest_power_id(effect_size=None, nobs=None, alpha=None, beta=None,
-                   df=None, alternative='two-sided'):
-    '''identity for power calculation, should be zero
-
-    '''
-    return ttest_power(effect_size=effect_size, nobs=nobs, alpha=alpha,
-                       df=df, alternative=alternative) - beta
-
-#import functools as ft   #too limited
-
+#module global for now
 start_ttp = dict(effect_size=0.01, nobs=10., alpha=0.15, beta=0.6,
                  nobs1=10, ratio=1)
-#TODO: nobs1 and rati are for ttest_ind,
+#TODO: nobs1 and ratio are for ttest_ind,
 #      need start_ttp for each test/class separately, added default start_value
 #possible rootfinding problem for effect_size, starting small seems to work
-
-def solve_ttest_power(**kwds):
-    '''solve for any one of the parameters of a t-test
-
-    for t-test the keywords are:
-        effect_size, nobs, alpha, beta
-
-    exactly one needs to be `None`, all others need numeric values
-
-    '''
-    #TODO: maybe use explicit kwds,
-    #      nicer but requires inspect? and not generic across tests
-    #TODO: use explicit calculation for beta=None
-    key = [k for k,v in kwds.iteritems() if v is None]
-    #print kwds, key;
-    if len(key) != 1:
-        raise ValueError('need exactly one keyword that is None')
-    key = key[0]
-
-    def func(x):
-        kwds[key] = x
-        return ttest_power_id(**kwds)
-
-    #start_value = start_ttp.get(key, 0.9)
-    #TODO: I'm using the following so I get a warning when start_ttp is not defined
-    try:
-        start_value = start_ttp[key]
-    except KeyError:
-        start_value = 0.9
-        print 'Warning: using default start_value'
-
-    return optimize.fsolve(func, start_value).item() #scalar
-
-
-print '\nroundtrip - root with respect to all variables'
-print '\n       calculated, desired'
-
-print 'nobs  ', solve_ttest_power(effect_size=effect_size, nobs=None, alpha=alpha, beta=beta), nobs_p
-print 'effect', solve_ttest_power(effect_size=None, nobs=nobs_p, alpha=alpha, beta=beta), effect_size
-
-print 'alpha ', solve_ttest_power(effect_size=effect_size, nobs=nobs_p, alpha=None, beta=beta), alpha
-print 'beta  ', solve_ttest_power(effect_size=effect_size, nobs=nobs_p, alpha=alpha, beta=None), beta
 
 
 #class based implementation
@@ -204,47 +123,51 @@ class TTestIndPower(Power):
 
 tt_solve_power = TTestPower().solve_power
 
-ttest_pow = TTestPower()
-print '\nroundtrip - root with respect to all variables'
-print '\n       calculated, desired'
+if __name__ == '__main__':
+    effect_size, alpha, beta = 0.5, 0.05, 0.8
 
-print 'nobs  ', ttest_pow.solve_power(effect_size=effect_size, nobs=None, alpha=alpha, beta=beta), nobs_p
-print 'effect', ttest_pow.solve_power(effect_size=None, nobs=nobs_p, alpha=alpha, beta=beta), effect_size
+    ttest_pow = TTestPower()
+    print '\nroundtrip - root with respect to all variables'
+    print '\n       calculated, desired'
 
-print 'alpha ', ttest_pow.solve_power(effect_size=effect_size, nobs=nobs_p, alpha=None, beta=beta), alpha
-print 'beta  ', ttest_pow.solve_power(effect_size=effect_size, nobs=nobs_p, alpha=alpha, beta=None), beta
+    nobs_p = ttest_pow.solve_power(effect_size=effect_size, nobs=None, alpha=alpha, beta=beta)
+    print 'nobs  ', nobs_p
+    print 'effect', ttest_pow.solve_power(effect_size=None, nobs=nobs_p, alpha=alpha, beta=beta), effect_size
 
-print '\nroundtrip - root with respect to all variables'
-print '\n       calculated, desired'
+    print 'alpha ', ttest_pow.solve_power(effect_size=effect_size, nobs=nobs_p, alpha=None, beta=beta), alpha
+    print 'beta  ', ttest_pow.solve_power(effect_size=effect_size, nobs=nobs_p, alpha=alpha, beta=None), beta
 
-print 'nobs  ', tt_solve_power(effect_size=effect_size, nobs=None, alpha=alpha, beta=beta), nobs_p
-print 'effect', tt_solve_power(effect_size=None, nobs=nobs_p, alpha=alpha, beta=beta), effect_size
+    print '\nroundtrip - root with respect to all variables'
+    print '\n       calculated, desired'
 
-print 'alpha ', tt_solve_power(effect_size=effect_size, nobs=nobs_p, alpha=None, beta=beta), alpha
-print 'beta  ', tt_solve_power(effect_size=effect_size, nobs=nobs_p, alpha=alpha, beta=None), beta
+    print 'nobs  ', tt_solve_power(effect_size=effect_size, nobs=None, alpha=alpha, beta=beta), nobs_p
+    print 'effect', tt_solve_power(effect_size=None, nobs=nobs_p, alpha=alpha, beta=beta), effect_size
 
-print '\none sided'
-nobs_p1 = tt_solve_power(effect_size=effect_size, nobs=None, alpha=alpha, beta=beta, alternative='1s')
-print 'nobs  ', nobs_p1
-print 'effect', tt_solve_power(effect_size=None, nobs=nobs_p1, alpha=alpha, beta=beta, alternative='1s'), effect_size
-print 'alpha ', tt_solve_power(effect_size=effect_size, nobs=nobs_p1, alpha=None, beta=beta, alternative='1s'), alpha
-print 'beta  ', tt_solve_power(effect_size=effect_size, nobs=nobs_p1, alpha=alpha, beta=None, alternative='1s'), beta
+    print 'alpha ', tt_solve_power(effect_size=effect_size, nobs=nobs_p, alpha=None, beta=beta), alpha
+    print 'beta  ', tt_solve_power(effect_size=effect_size, nobs=nobs_p, alpha=alpha, beta=None), beta
 
-#start_ttp = dict(effect_size=0.01, nobs1=10., alpha=0.15, beta=0.6)
+    print '\none sided'
+    nobs_p1 = tt_solve_power(effect_size=effect_size, nobs=None, alpha=alpha, beta=beta, alternative='1s')
+    print 'nobs  ', nobs_p1
+    print 'effect', tt_solve_power(effect_size=None, nobs=nobs_p1, alpha=alpha, beta=beta, alternative='1s'), effect_size
+    print 'alpha ', tt_solve_power(effect_size=effect_size, nobs=nobs_p1, alpha=None, beta=beta, alternative='1s'), alpha
+    print 'beta  ', tt_solve_power(effect_size=effect_size, nobs=nobs_p1, alpha=alpha, beta=None, alternative='1s'), beta
 
-ttind_solve_power = TTestIndPower().solve_power
+    #start_ttp = dict(effect_size=0.01, nobs1=10., alpha=0.15, beta=0.6)
 
-print '\nroundtrip - root with respect to all variables'
-print '\n       calculated, desired'
+    ttind_solve_power = TTestIndPower().solve_power
 
-nobs_p2 = ttind_solve_power(effect_size=effect_size, nobs1=None, alpha=alpha, beta=beta)
-print 'nobs  ', nobs_p2
-print 'effect', ttind_solve_power(effect_size=None, nobs1=nobs_p2, alpha=alpha, beta=beta), effect_size
+    print '\nroundtrip - root with respect to all variables'
+    print '\n       calculated, desired'
 
-print 'alpha ', ttind_solve_power(effect_size=effect_size, nobs1=nobs_p2, alpha=None, beta=beta), alpha
-print 'beta  ', ttind_solve_power(effect_size=effect_size, nobs1=nobs_p2, alpha=alpha, beta=None), beta
-print 'ratio  ', ttind_solve_power(effect_size=effect_size, nobs1=nobs_p2, alpha=alpha, beta=beta, ratio=None), 1
+    nobs_p2 = ttind_solve_power(effect_size=effect_size, nobs1=None, alpha=alpha, beta=beta)
+    print 'nobs  ', nobs_p2
+    print 'effect', ttind_solve_power(effect_size=None, nobs1=nobs_p2, alpha=alpha, beta=beta), effect_size
 
-print '\ncheck ratio'
-print 'smaller beta', ttind_solve_power(effect_size=effect_size, nobs1=nobs_p2, alpha=alpha, beta=0.7, ratio=None), '< 1'
-print 'larger beta ', ttind_solve_power(effect_size=effect_size, nobs1=nobs_p2, alpha=alpha, beta=0.9, ratio=None), '> 1'
+    print 'alpha ', ttind_solve_power(effect_size=effect_size, nobs1=nobs_p2, alpha=None, beta=beta), alpha
+    print 'beta  ', ttind_solve_power(effect_size=effect_size, nobs1=nobs_p2, alpha=alpha, beta=None), beta
+    print 'ratio  ', ttind_solve_power(effect_size=effect_size, nobs1=nobs_p2, alpha=alpha, beta=beta, ratio=None), 1
+
+    print '\ncheck ratio'
+    print 'smaller beta', ttind_solve_power(effect_size=effect_size, nobs1=nobs_p2, alpha=alpha, beta=0.7, ratio=None), '< 1'
+    print 'larger beta ', ttind_solve_power(effect_size=effect_size, nobs1=nobs_p2, alpha=alpha, beta=0.9, ratio=None), '> 1'
