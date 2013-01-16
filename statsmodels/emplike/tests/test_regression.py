d720548533a4b6b044bd5e4ee827b9cb6a71f60f
Justin Grana <jg3705a@student.american.edu>
2013-01-16 00:34:34 -0500

    TST: Made changes as suggested by Josef in issue 622

--- statsmodels/emplike/tests/test_regression.py
+++ statsmodels/emplike/tests/test_regression.py
@@ -19,8 +19,7 @@ class GenRes(object):
 class TestRegressionPowell(GenRes):
     """
     All confidence intervals are tested by conducting a hypothesis
-    tests at the confidence interval values since hy_test_beta
-    is already tested against Matlab
+    tests at the confidence interval values.
 
     See Also
     --------
@@ -31,12 +30,14 @@ class TestRegressionPowell(GenRes):
     def __init__(self):
         super(TestRegressionPowell, self).__init__()
 
+    @slow
     def test_hypothesis_beta0(self):
         beta0res = self.res1.el_test([-30], [0], return_weights=1,
                                      method='powell')
         assert_almost_equal(beta0res[:2], self.res2.test_beta0[:2], 4)
         assert_almost_equal(beta0res[2], self.res2.test_beta0[2], 4)
 
+    @slow
     def test_hypothesis_beta1(self):
         beta1res = self.res1.el_test([.5], [1], return_weights=1,
                                      method='powell')
@@ -55,53 +56,35 @@ class TestRegressionPowell(GenRes):
         assert_almost_equal(beta3res[:2], self.res2.test_beta3[:2], 4)
         assert_almost_equal(beta3res[2], self.res2.test_beta3[2], 4)
 
+    # Confidence interval results obtained through hypothesis testing in Matlab
     def test_ci_beta0(self):
-        beta0ci = self.res1.conf_int_el(0, lower_bound=-52.8, upper_bound=-24.1,
-                                        method='powell')
-        lower_lim = beta0ci[0]
-        upper_lim = beta0ci[1]
-        ul_pval = self.res1.el_test([upper_lim], [0])[1]
-        ll_pval = self.res1.el_test([lower_lim], [0])[1]
-        assert_almost_equal(ul_pval, .050000, 4)
-        assert_almost_equal(ll_pval, .050000, 4)
+        beta0ci = self.res1.conf_int_el(0, lower_bound=-52.9,
+                                        upper_bound=-24.1, method='powell')
+        assert_almost_equal(beta0ci, self.res2.test_ci_beta0, 3)
+        #  Slightly lower precision.  CI was obtained from nm method.
 
     def test_ci_beta1(self):
         beta1ci = self.res1.conf_int_el(1, lower_bound=.418, upper_bound=.986,
                                         method='powell')
-        lower_lim = beta1ci[0]
-        upper_lim = beta1ci[1]
-        ul_pval = self.res1.el_test([upper_lim], [1])[1]
-        ll_pval = self.res1.el_test([lower_lim], [1])[1]
-        assert_almost_equal(ul_pval, .050000, 4)
-        assert_almost_equal(ll_pval, .050000, 4)
+        assert_almost_equal(beta1ci, self.res2.test_ci_beta1, 4)
+
     @slow
     def test_ci_beta2(self):
         beta2ci = self.res1.conf_int_el(2, lower_bound=.59,
                                     upper_bound=2.2, method='powell')
-        lower_lim = beta2ci[0]
-        upper_lim = beta2ci[1]
-        ul_pval = self.res1.el_test([upper_lim], [2])[1]
-        ll_pval = self.res1.el_test([lower_lim], [2])[1]
-        assert_almost_equal(ul_pval, .050000, 4)
-        assert_almost_equal(ll_pval, .050000, 4)
+        assert_almost_equal(beta2ci, self.res2.test_ci_beta2, 5)
 
     @slow
     def test_ci_beta3(self):
         beta3ci = self.res1.conf_int_el(3, lower_bound=-.39, upper_bound=.01,
                                         method='powell')
-        lower_lim = beta3ci[0]
-        upper_lim = beta3ci[1]
-        ul_pval = self.res1.el_test([upper_lim], [3])[1]
-        ll_pval = self.res1.el_test([lower_lim], [3])[1]
-        assert_almost_equal(ul_pval, .050000, 4)
-        assert_almost_equal(ll_pval, .050000, 4)
+        assert_almost_equal(beta3ci, self.res2.test_ci_beta3, 6)
 
 
 class TestRegressionNM(GenRes):
     """
     All confidence intervals are tested by conducting a hypothesis
-    tests at the confidence interval values since el_test
-    is already tested against Matlab
+    tests at the confidence interval values.
 
     See Also
     --------
@@ -124,18 +107,23 @@ class TestRegressionNM(GenRes):
         assert_almost_equal(beta1res[:2], self.res2.test_beta1[:2], 4)
         assert_almost_equal(beta1res[2], self.res2.test_beta1[2], 4)
 
+    @slow
     def test_hypothesis_beta2(self):
         beta2res = self.res1.el_test([1], [2], return_weights=1,
                                           method='nm')
         assert_almost_equal(beta2res[:2], self.res2.test_beta2[:2], 4)
         assert_almost_equal(beta2res[2], self.res2.test_beta2[2], 4)
 
+    @slow
     def test_hypothesis_beta3(self):
         beta3res = self.res1.el_test([0], [3], return_weights=1,
                                           method='nm')
         assert_almost_equal(beta3res[:2], self.res2.test_beta3[:2], 4)
         assert_almost_equal(beta3res[2], self.res2.test_beta3[2], 4)
 
+    #  Confidence interval results obtained through hyp testing in Matlab
+
+    @slow
     def test_ci_beta0(self):
         """
         All confidence intervals are tested by conducting a hypothesis
@@ -149,37 +137,17 @@ class TestRegressionNM(GenRes):
 
         """
         beta0ci = self.res1.conf_int_el(0, method='nm')
-        lower_lim = beta0ci[0]
-        upper_lim = beta0ci[1]
-        ul_pval = self.res1.el_test([upper_lim], [0], method='nm')[1]
-        ll_pval = self.res1.el_test([lower_lim], [0], method='nm')[1]
-        assert_almost_equal(ul_pval, .050000, 4)
-        assert_almost_equal(ll_pval, .050000, 4)
+        assert_almost_equal(beta0ci, self.res2.test_ci_beta0, 6)
 
+    @slow
     def test_ci_beta1(self):
         beta1ci = self.res1.conf_int_el(1, method='nm')
-        lower_lim = beta1ci[0]
-        upper_lim = beta1ci[1]
-        ul_pval = self.res1.el_test([upper_lim], [1], method='nm')[1]
-        ll_pval = self.res1.el_test([lower_lim], [1], method='nm')[1]
-        assert_almost_equal(ul_pval, .050000, 4)
-        assert_almost_equal(ll_pval, .050000, 4)
+        assert_almost_equal(beta1ci, self.res2.test_ci_beta1, 6)
 
     def test_ci_beta2(self):
         beta2ci = self.res1.conf_int_el(2, method='nm')
-        lower_lim = beta2ci[0]
-        upper_lim = beta2ci[1]
-        ul_pval = self.res1.el_test([upper_lim], [2], method='nm')[1]
-        ll_pval = self.res1.el_test([lower_lim], [2], method='nm')[1]
-        assert_almost_equal(ul_pval, .050000, 4)
-        assert_almost_equal(ll_pval, .050000, 4)
-
+        assert_almost_equal(beta2ci, self.res2.test_ci_beta2, 6)
 
     def test_ci_beta3(self):
         beta3ci = self.res1.conf_int_el(3, method='nm')
-        lower_lim = beta3ci[0]
-        upper_lim = beta3ci[1]
-        ul_pval = self.res1.el_test([upper_lim], [3], method='nm')[1]
-        ll_pval = self.res1.el_test([lower_lim], [3], method='nm')[1]
-        assert_almost_equal(ul_pval, .050000, 4)
-        assert_almost_equal(ll_pval, .050000, 4)
+        assert_almost_equal(beta3ci, self.res2.test_ci_beta3, 6)
