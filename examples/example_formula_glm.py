2f65866da6f807a77a1a97294da0dd7a75f4ba36
dengemann <denis.engemann@gmail.com>
2013-02-09 16:52:47 +0100

    ENH: missing commit

--- examples/example_formula_glm.py
+++ examples/example_formula_glm.py
@@ -8,13 +8,13 @@ star98 = sm.datasets.star98.load_pandas().data
 formula = ('SUCCESS ~ LOWINC + PERASIAN + PERBLACK + PERHISP + PCTCHRT '
            '+ PCTYRRND + PERMINTE*AVYRSEXP*AVSALK + PERSPENK*PTRATIO*PCTAF')
 
-dta = star98[["NABOVE", "NBELOW", "LOWINC", "PERASIAN", "PERBLACK", "PERHISP",
-              "PCTCHRT", "PCTYRRND", "PERMINTE", "AVYRSEXP", "AVSALK",
-              "PERSPENK", "PTRATIO", "PCTAF"]]
+dta = star98[['NABOVE', 'NBELOW', 'LOWINC', 'PERASIAN', 'PERBLACK', 'PERHISP',
+              'PCTCHRT', 'PCTYRRND', 'PERMINTE', 'AVYRSEXP', 'AVSALK',
+              'PERSPENK', 'PTRATIO', 'PCTAF']]
 
-endog = dta["NABOVE"] / (dta["NABOVE"] + dta.pop("NBELOW"))
-del dta["NABOVE"]
-dta["SUCCESS"] = endog
+endog = dta['NABOVE'] / (dta['NABOVE'] + dta.pop('NBELOW'))
+del dta['NABOVE']
+dta['SUCCESS'] = endog
 
 mod = sm.GLM.from_formula(formula=formula, data=dta,
                           family=sm.families.Binomial()).fit()
