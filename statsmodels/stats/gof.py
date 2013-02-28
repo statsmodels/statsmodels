20c6b01b25116b6257fb5cdb45701b0c2aed98c4
Josef Perktold <josef.pktd@gmail.com>
2013-02-28 14:17:42 -0500

    ENH: add options to chisquare_effect size

--- statsmodels/stats/gof.py
+++ statsmodels/stats/gof.py
@@ -433,7 +433,7 @@ def chisquare_power(effect_size, nobs, n_bins, alpha=0.05, ddof=0):
     return power
 
 
-def chisquare_effectsize(probs0, probs1, axis=0):
+def chisquare_effectsize(probs0, probs1, correction=None, cohen=True, axis=0):
     '''effect size for a chisquare goodness-of-fit test
 
     Parameters
@@ -446,6 +446,19 @@ def chisquare_effectsize(probs0, probs1, axis=0):
         and broadcast in the other dimensions
         Both probs0 and probs1 are normalized to add to one (in the ``axis``
         dimension).
+    correction : None or tuple (nobs, df)
+        If None, then the effect size is the chisquare statistic divide by
+        the number of observations.
+        I the correction is a tuple (nobs, df), then the effectsize is 
+        corrected to have less bias and a smaller variance. 
+        Pederson and Johnson (1990) as referenced in McLaren et all. (1994)
+    cohen : bool
+        If True, then the square root is returned as in the definition of the
+        effect size by Cohen (1977), If False, then the original effect size
+        is returned.
+    axis : int
+        If the probability arrays broadcast to more than 1 dimension, then
+        this is the axis over which the sums are taken.
 
     Returns
     -------
@@ -457,5 +470,15 @@ def chisquare_effectsize(probs0, probs1, axis=0):
     probs1 = np.asarray(probs1, float)
     probs0 = probs0 / probs0.sum(axis)
     probs1 = probs1 / probs1.sum(axis)
+    
+    d2 = ((probs1 - probs0)**2 / probs0).sum(axis)
+    
+    if correction is not None:
+        nobs, df = correction
+        diff = ((probs1 - probs0) / probs0).sum(axis)
+        d2 = (d2 * nobs - diff - df) / (nobs - 1.) 
 
-    return np.sqrt(((probs1 - probs0)**2 / probs0).sum(axis))
+    if cohen:
+        return np.sqrt(d2)
+    else:
+        return d2
