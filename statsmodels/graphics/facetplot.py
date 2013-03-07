661cb2aa08af87dd73d1d04cb0cd2244957bc0fe
Enrico Giampieri <enrico.giampieri@unibo.it>
2013-03-07 02:32:27 +0100

    extended the docstrings of both autoplot and facet_plot, implementing boxplots and points for all combinations

--- statsmodels/graphics/facetplot.py
+++ statsmodels/graphics/facetplot.py
@@ -92,8 +92,8 @@ def autoplot(x, y=None, ax=None, kind=None, *args, **kwargs):
 
     the args and kwargs are redirected to the plot function
 
-    Params
-    ======
+    Parameters
+    ==========
     x : array, list or pandas.Series
         The main array to be plotted
     y : same as x, optional
@@ -108,14 +108,19 @@ def autoplot(x, y=None, ax=None, kind=None, *args, **kwargs):
     where the first element is the default option:
         numerical exogenous:
             numerical endogenous:
-                'ellipse', 'points', 'lines', 'hexbin'
+                'ellipse', 'points', 'lines', 'hexbin', 'boxplot'
             categorical endogenous:
                 'boxplot', 'points'
         categorical exogenous:
             numerical endogenous:
                 'violinplot', 'points', 'boxplot', 'beanplot'
             categorical endogenous:
-                'mosaic', 'points', 'matrix'
+                'mosaic', 'points', 'matrix', 'boxplot'
+
+    As can be seen the points and boxplot version can be applied in any
+    case. It should be noted that even if you can, this doesn't mean
+    that you should: The function will not stop the user from creating
+    meaningless plots
 
     Returns
     =======
@@ -124,6 +129,26 @@ def autoplot(x, y=None, ax=None, kind=None, *args, **kwargs):
     See Also
     ========
     facet_plot: create a figure with subplots of x and y divided by category
+
+    Examples
+    ========
+    Similarly to the facet_plot we create an example dataset and see some
+    example application
+    >>> import pylab as plt
+    >>> N = 300
+    >>> data = pd.DataFrame({
+    ...     'int_1': plt.randint(0, 5, size=N),
+    ...     'int_2': plt.randint(0, 5, size=N),
+    ...     'float_1': plt.randn(N),
+    ...     'float_2': plt.randn(N),
+    ...     'cat_1': ['aeiou'[i] for i in plt.randint(0, 5, size=N)],
+    ...     'cat_2': ['BCDF'[i] for i in plt.randint(0, 4, size=N)]})
+
+    and do some example plot
+    >>> autoplot(data.float_1, data.float_2)
+    >>> autoplot(data.int_2, data.float_1, kind='boxplot')
+    >>> autoplot(data.float_1, data.int_2, kind='ellipse')
+    >>> autoplot(data.cat_1, data.cat_2, kind='mosaic')
     """
     fig, ax = utils.create_mpl_ax(ax)
     available_plots = ['ellipse', 'lines', 'points', 'hexbin',
@@ -166,6 +191,12 @@ def autoplot(x, y=None, ax=None, kind=None, *args, **kwargs):
                 kwargs.setdefault('cmap', plt.cm.binary)
                 kwargs.setdefault('gridsize', 20)
                 ax.hexbin(x, y, *args, **kwargs)
+            elif kind in ['boxplot']:
+                data = pd.DataFrame({'x': x, 'y': y})
+                levels = list(data.groupby('x')['y'])
+                level_v = [v for k, v in levels]
+                level_k = [k for k, v in levels]
+                ax.boxplot(level_v, *args, **kwargs)
             else:
                 raise plot_error
             #add the level to the scatterplot
@@ -175,11 +206,11 @@ def autoplot(x, y=None, ax=None, kind=None, *args, **kwargs):
                 _make_ellipse(mean, cov, ax, 0.95, 'gray')
                 _make_ellipse(mean, cov, ax, 0.50, 'blue')
                 _make_ellipse(mean, cov, ax, 0.05, 'purple')
-            #this gives some problem I don'tunderstand about a fixedlocator
-            #if y.dtype == int:
-            #    ax.set_yticks([int(i) for i in ax.get_yticks()])
-            #if x.dtype == int:
-            #    ax.set_xticks([int(i) for i in ax.get_xticks()])
+            # this gives some problem I don'tunderstand about a fixedlocator
+            if y.dtype == int:
+                ax.set_yticks([int(i) for i in ax.get_yticks()])
+            if x.dtype == int:
+                ax.set_xticks([int(i) for i in ax.get_xticks()])
         # the endog is categorical, do a horizontal boxplot
         else:
             data = pd.DataFrame({'x': x, 'f': y})
@@ -239,7 +270,7 @@ def autoplot(x, y=None, ax=None, kind=None, *args, **kwargs):
                 data = pd.DataFrame({x_name: x, y_name: y})
                 mosaicplot.mosaic(data, index=[x_name, y_name],
                         ax=ax, *args, **kwargs)
-            elif kind in ['points', 'matrix']:
+            elif kind in ['points', 'matrix', 'boxplot']:
                 ylevels = y.unique()
                 y_ticks = range(1, 1 + len(ylevels))
                 num4levely = {k: v for k, v in zip(ylevels, y_ticks)}
@@ -252,6 +283,11 @@ def autoplot(x, y=None, ax=None, kind=None, *args, **kwargs):
                 if kind in ['points']:
                     kwargs.setdefault('alpha', 0.33)
                     ax.scatter(_jitter(x), _jitter(y), *args, **kwargs)
+                elif kind in ['boxplot']:
+                    values = [[num4levely[s] for s in v] for v in level_v]
+                    kwargs.setdefault('notch', True)
+                    ax.boxplot(values, *args, **kwargs)
+                    ax.set_xticklabels(level_k)
                 else:
                     image = np.zeros((len(y_ticks), len(x_ticks)))
                     for _x, _y in zip(x, y):
@@ -261,7 +297,6 @@ def autoplot(x, y=None, ax=None, kind=None, *args, **kwargs):
                     extent = (x_ticks[0] - 0.5, x_ticks[-1] + 0.5,
                         y_ticks[0] - 0.5, y_ticks[-1] + 0.5)
                     kwargs.setdefault('extent', extent)
-
                     ax.imshow(image, *args, **kwargs)
                 ax.set_yticks(y_ticks)
                 ax.set_yticklabels(level_k_y)
@@ -335,13 +370,12 @@ def _array4name(name, data):
         # to do monovariate plots this should return a series
         # the dataframe is kept for future implementation of
         # multivariate plots
-        if len(dmatrix) == 1:
+        if len(dmatrix.columns) == 1:
             value = pd.Series(dmatrix[dmatrix.columns[0]])
-            value.name = name
+            #value.name = name
         else:
             value = dmatrix
             value.name = name
-        print value
     return value
 
 
@@ -360,7 +394,8 @@ def _select_rowcolsize(num_of_categories):
     return row_num, col_num
 
 
-def facet_plot(formula, data, subset=None, kind=None, *args, **kwargs):
+def facet_plot(formula, data, subset=None, kind=None,
+    drop_na=True, *args, **kwargs):
     """make a faceted plot of two variables divided into categories
 
     the formula should follow the sintax of the faceted plot:
@@ -373,9 +408,109 @@ def facet_plot(formula, data, subset=None, kind=None, *args, **kwargs):
 
     All the factor will be treated as patsy formulas, but are limited to
     monovariate endogenous and exogenous variables.
+
+    Parameters
+    ==========
+    formula: string formula like
+        The plot description using the patsy formula syntax
+    data: dict like object or pandas.Dataframe
+        The dataframe over which the formula should be evaluated
+    subset: boolean array, optional
+        The True position indicates which rows of the dataframe should
+        be used (it should be used to select only a part of the dataset)
+    kind: string, optional
+        The kind of plot that should be done on the graphs. If not
+        specified will try and guess the best one.
+    drop_na: boolean, optional
+        Drop the nan values in the dataframe,defaul to True
+
+    the args and kwargs will be redirected to the specific plot
+
+    the valid kind of plot is riassumed in this list,
+    where the first element is the default option:
+        numerical exogenous:
+            numerical endogenous:
+                'ellipse', 'points', 'lines', 'hexbin', 'boxplot'
+            categorical endogenous:
+                'boxplot', 'points'
+        categorical exogenous:
+            numerical endogenous:
+                'violinplot', 'points', 'boxplot', 'beanplot'
+            categorical endogenous:
+                'mosaic', 'points', 'matrix', 'boxplot'
+
+    Returns
+    =======
+    fig: the created matplotlib figure
+
+    See Also
+    ========
+    autoplot: the function that does all the heavy lifting of guessing
+        and that can be used as a standalone function
+
+    The function is concettually based on the lattice library of R
+        http://www.statmethods.net/advgraphs/trellis.html
+        http://cran.r-project.org/web/packages/lattice/index.html
+
+    Examples
+    ========
+    Let's create a very simple dataset to work on
+    >>> import pylab as plt
+    >>> N = 300
+    >>> data = pd.DataFrame({
+    ...     'int_1': plt.randint(0, 5, size=N),
+    ...     'int_2': plt.randint(0, 5, size=N),
+    ...     'float_1': plt.randn(N),
+    ...     'float_2': plt.randn(N),
+    ...     'cat_1': ['aeiou'[i] for i in plt.randint(0, 5, size=N)],
+    ...     'cat_2': ['BCDF'[i] for i in plt.randint(0, 4, size=N)]})
+
+    basic facet_plots for variuos combinations
+    >>> facet_plot('int_1', data)
+    >>> facet_plot('float_1', data)
+    >>> facet_plot('int_1 | cat_1', data)
+    >>> facet_plot('int_1 ~ float_1 | cat_1', data)
+    >>> facet_plot('float_1 ~ float_1 | cat_2', data)
+    >>> facet_plot('int_1 ~ cat_1', data)
+
+    parameters can be passed to the underlying plot function
+    >>> facet_plot('float_1', data, facecolor='r', alpha=1.0)
+
+    the split can be done even in terms of integers values
+    >>> facet_plot('float_1 ~ float_2 | int_1', data)
+
+    formulas can be used in the definition of the model
+    >>> facet_plot('I(float_1 + float_2) ~ int_1', data)
+
+    Multiple categorical variable are supported,
+    using the cartesian product of the levels
+    >>> facet_plot('float_1 | cat_1 cat_2', data)
+
+    of any combination of level as categorical variables
+    >>> facet_plot('float_1 | cat_1 + cat_2', data)
+    >>> facet_plot('float_1 | cat_1 * cat_2', data)
+    >>> facet_plot('I(float_1*4) ~ I(float_2 + 3)', data)
+
+    At last, the possibility to insert a certain kind of plot
+    for example, using all the available types on a combination
+    of categorical values
+    >>> facet_plot('cat_1 ~ cat_2 | int_1', data, kind='points')
+    >>> facet_plot('cat_1 ~ cat_2 | int_1', data, kind='mosaic')
+    >>> facet_plot('cat_1 ~ cat_2 | int_1', data, kind='matrix')
+    >>> facet_plot('cat_1 ~ cat_2', data, kind='boxplot')
+
+    and these are the solution for the double numerical values
+    >>> facet_plot('float_1 ~ float_2 | cat_1', data, kind='points')
+    >>> facet_plot('float_1 ~ float_2 | cat_1', data, kind='ellipse')
+    >>> facet_plot('float_1 ~ float_2 | cat_1', data, kind='hexbin')
+    >>> facet_plot('float_1 ~ float_2 | cat_1', data, kind='lines')
+    >>> facet_plot('float_1 ~ int_1 | cat_1', data, kind='boxplot')
     """
     fig = plt.figure()
     y, x, facet = _formula_split(formula)
+    data = pd.DataFrame(data)
+    if drop_na:
+        data = data.dropna()
     # if a subset is specified use it to trim the dataframe
     if subset:
         data = data[subset]
@@ -394,7 +529,6 @@ def facet_plot(formula, data, subset=None, kind=None, *args, **kwargs):
             idx -= row_num * col_num - L
         my_row = idx // col_num
         my_col = idx % col_num
-
         ax = fig.add_subplot(row_num, col_num, idx + 1,
                              sharex=base_ax, sharey=base_ax)
         # all the subplots share the same axis with the first one being
@@ -406,130 +540,49 @@ def facet_plot(formula, data, subset=None, kind=None, *args, **kwargs):
         value_y = _array4name(y, value) if y else None
         # launch the autoplot
         autoplot(value_x, value_y, ax, kind=kind, *args, **kwargs)
-        #remove the extremal ticks to remove overlaps
+        # remove the extremal ticks to remove overlaps
+        # if the ticks have been fixed it generate a fixedLocator
+        # that gives error, so just skip if it fails
         if (value_y is not None and value_y.dtype != object) or value_y is None:
-            ax.locator_params(prune='both', axis='y')
+            try:
+                ax.locator_params(prune='both', axis='y')
+            except AttributeError:
+                pass
         if value_x.dtype != object:
-            ax.locator_params(prune='both', axis='x')
+            try:
+                ax.locator_params(prune='both', axis='x')
+            except AttributeError:
+                pass
         # remove the superfluos info base on the columns
         if my_col:
             ax.set_ylabel('')
             plt.setp(ax.get_yticklabels(), visible=False)
         # show the x labels only if it's on the last line
-        # or in the one before that and is not hidden by the
-        # last row
-        # Can be simplified moving the x axes on the top
-        #TODO: move the x axis on the top
-        if my_row != row_num - 1:# and not (my_row == row_num - 2
-                                 #       and 0 < L % col_num <= my_col):
+        if my_row != row_num - 1:
             ax.set_xlabel('')
             plt.setp(ax.get_xticklabels(), visible=False)
-
         ax.set_title(level)
-        #ax.set_title("{:.0f}, {:.0f}, {:.0f}".format(idx, my_row, my_col))
     fig.canvas.set_window_title(formula)
     fig.subplots_adjust(wspace=0, hspace=0.0)
     fig.tight_layout()
     return fig
 
 if __name__ == '__main__':
-    N = 20
-    data = pd.DataFrame({'int_1': plt.randint(0, 5, size=N),
-                         'int_2': plt.randint(0, 5, size=N),
-                         'float_1': plt.randn(N),
-                         'float_2': plt.randn(N),
-                         'cat_1': ['lizard'] * 7 + ['dog'] * 7 + ['newt'] * 6,
-                         'cat_2': (['men'] * 4 + ['women'] * 11
-                                  + ['men'] * 5)})
+    N = 300
+    data = pd.DataFrame({
+         'int_1': plt.randint(0, 5, size=N),
+         'int_2': plt.randint(0, 5, size=N),
+         'float_1': plt.randn(N),
+         'float_2': plt.randn(N),
+         'cat_1': ['aeiou'[i] for i in plt.randint(0, 5, size=N)],
+         'cat_2': ['BCDF'[i] for i in plt.randint(0, 4, size=N)]})
 
-    affair = datasets.fair.load_pandas().data
-    print affair.columns
-    rate_marriage = {1: '1 very poor', 2: '2 poor', 3: '3 fair',
-    4: '4 good', 5: '5 very good'}
-    l = lambda s: rate_marriage[s]
-    affair['rate_marriage'] = affair['rate_marriage'].apply(l)
-    religious = {1: '1 not', 2: '2 mildly', 3: '3 fairly', 4: '4 strongly'}
-    affair['religious'] = affair['religious'].apply(lambda s: religious[s])
-    occupation = {1: 'student',
-        2: 'farming, agriculture; semi-skilled, or unskilled worker',
-        3: 'white-colloar',
-        4: ('teacher counselor social worker, nurse; artist, writers; '
-            'technician, skilled worker'),
-        5: 'managerial, administrative, business',
-        6: 'professional with advanced degree'}
-    affair['occupation'] = affair['occupation'].apply(lambda s: occupation[s])
-    l = lambda s: occupation[s]
-    affair['occupation_husb'] = affair['occupation_husb'].apply(l)
-    affair['age'] = affair['age'].apply(int)
-    affair['yrs_married'] = affair['yrs_married'].apply(int)
-    affair['cheated'] = affair.affairs > 0
-
-    #_autoplot(data.int_1)
-    #_autoplot(data.cat_1)
-    #_autoplot(data.float_1)
-    #_autoplot(data.float_1, data.float_2)
-    #_autoplot(data.float_1, data.int_2)
-    #_autoplot(data.float_1, data.cat_2)
-    #_autoplot(data.int_1, data.float_2)
-    #_autoplot(data.int_1, data.int_2)
-    #_autoplot(data.int_1, data.cat_2)
-    #_autoplot(data.cat_1, data.float_2)
-    #_autoplot(data.cat_1, data.int_2)
-    #_autoplot(data.cat_1, data.cat_2)
     assert _formula_split('y ~ x | f') == ('y', 'x', 'f')
     assert _formula_split('y ~ x') == ('y', 'x', None)
     assert _formula_split('x | f') == (None, 'x', 'f')
     assert _formula_split('x') == (None, 'x', None)
 
-    # basic facet_plots for variuos combinations
-    #facet_plot('int_1 | cat_1', data)
-    #facet_plot('int_1 ~ float_1 | cat_1', data)
-    #facet_plot('int_1 ~ float_1 | cat_2', data)
-    #facet_plot('int_1 ~ float_1', data)
-    #facet_plot('int_1', data)
-    #facet_plot('float_1', data, facecolor='r', alpha=1.0)
-    #facet_plot('cat_1 ~ cat_2', data)
-    #facet_plot('float_1 ~ float_2 | cat_1', data)
-
-    # it can split even for integer levels!
-    #facet_plot('float_1 ~ float_2 | int_1', data)
-
-    #multiple classes for the categorical
-    #facet_plot('float_1 | cat_1 cat_2', data)
-
-    #categorical with patsy
-    #facet_plot('float_1 | cat_1 + cat_2', data)
-
-    #categorical with patsy version 2
-    #facet_plot('float_1 | cat_1 * cat_2', data)
-
-    #facet_plot('I(float_1*4) ~ I(float_2 + 3)', data)
-
-    #facet_plot('yrs_married ~ religious', affair)
-    #facet_plot('yrs_married ~ religious | educ', affair)
-
-    # test for the different kind of plots
-    #facet_plot('yrs_married ~ age | religious', affair, kind='scatter')
-    #facet_plot('yrs_married ~ age | religious', affair, kind='points')
-    #facet_plot('yrs_married ~ age | religious', affair, kind='ellipse')
-    #facet_plot('rate_marriage ~ age | religious', affair, kind='boxplot')
-    #facet_plot('rate_marriage ~ age | religious', affair, kind='points')
-    #facet_plot('age ~ rate_marriage | religious', affair, kind='boxplot')
-    #facet_plot('age ~ rate_marriage | religious', affair, kind='beanplot')
-    #facet_plot('age ~ rate_marriage | religious', affair, kind='violinplot')
-    #facet_plot('age ~ rate_marriage | religious', affair, kind='points')
-    #facet_plot('rate_marriage ~ religious', affair, kind='mosaic')
-    #facet_plot('rate_marriage ~ religious', affair, kind='points')
-    #facet_plot('rate_marriage ~ religious', affair, kind='matrix')
-    import pandas as pd
-    import pylab as plt
-    base_cat = 'abc'
-    f = 10
-    data = pd.DataFrame({'x': plt.randn(f * len(base_cat)),
-                         'y': plt.randn(f * len(base_cat)),
-                         'z': plt.randn(f * len(base_cat)),
-                         'c': list(base_cat * f)})
-    facet_plot('y ~ x + z', data, kind='lines')
-
-    #autoplot(data['y'],data['x'])
+    #facet_plot('cat_1 ~ cat_2', data, kind='boxplot')
+    autoplot(data.int_2, data.float_1, kind='boxplot')
+    autoplot(data.float_1, data.int_2, kind='ellipse')
     plt.show()
