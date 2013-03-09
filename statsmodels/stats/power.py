0f4c3099651e467e8aee44488f8a064437762590
Josef Perktold <josef.pktd@gmail.com>
2013-03-09 18:01:11 -0500

    REF/TST NormalIndPower allow ratio=0 for one-sample, test classes with root testing

--- statsmodels/stats/power.py
+++ statsmodels/stats/power.py
@@ -369,14 +369,16 @@ class NormalIndPower(Power):
             number of observations of sample 1. The number of observations of
             sample two is ratio times the size of sample 1,
             i.e. ``nobs2 = nobs1 * ratio``
+            ``ratio`` can be set to zero in order to get the power for a
+            one sample test.
         alpha : float in interval (0,1)
             significance level, e.g. 0.05, is the probability of a type I
             error, that is wrong rejections if the Null Hypothesis is true.
         ratio : float
             ratio of the number of observations in sample 2 relative to
             sample 1. see description of nobs1
-            The default for ratio is 1; to solve for ration given the other
-            arguments it has to be explicitely set to None.
+            The default for ratio is 1; to solve for ratio given the other
+            arguments it has to be explicitly set to None.
         alternative : string, 'two-sided' (default) or 'one-sided'
             extra argument to choose whether the power is calculated for a
             two-sided (default) or one sided test.
@@ -391,9 +393,12 @@ class NormalIndPower(Power):
 
         '''
 
-        nobs2 = nobs1*ratio
-        #equivalent to nobs = n1*n2/(n1+n2)=n1*ratio/(1+ratio)
-        nobs = 1./ (1. / nobs1 + 1. / nobs2)
+        if ratio > 0:
+            nobs2 = nobs1*ratio
+            #equivalent to nobs = n1*n2/(n1+n2)=n1*ratio/(1+ratio)
+            nobs = 1./ (1. / nobs1 + 1. / nobs2)
+        else:
+            nobs = nobs1
         return normal_power(effect_size, nobs, alpha, alternative=alternative)
 
     #method is only added to have explicit keywords and docstring
@@ -415,6 +420,8 @@ class NormalIndPower(Power):
             number of observations of sample 1. The number of observations of
             sample two is ratio times the size of sample 1,
             i.e. ``nobs2 = nobs1 * ratio``
+            ``ratio`` can be set to zero in order to get the power for a
+            one sample test.
         alpha : float in interval (0,1)
             significance level, e.g. 0.05, is the probability of a type I
             error, that is wrong rejections if the Null Hypothesis is true.
@@ -426,7 +433,7 @@ class NormalIndPower(Power):
             ratio of the number of observations in sample 2 relative to
             sample 1. see description of nobs1
             The default for ratio is 1; to solve for ration given the other
-            arguments it has to be explicitely set to None.
+            arguments it has to be explicitly set to None.
         alternative : string, 'two-sided' (default) or 'one-sided'
             extra argument to choose whether the power is calculated for a
             two-sided (default) or one sided test.
