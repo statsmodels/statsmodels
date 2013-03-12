8aa3df96debe0d3581fca840615a19bf63268a72
Vincent Arel-Bundock <varel@umich.edu>
2013-03-11 23:17:32 -0400

    quantreg formula

--- statsmodels/formula/api.py
+++ statsmodels/formula/api.py
@@ -18,3 +18,5 @@ from statsmodels.discrete.discrete_model import Probit
 probit = Probit.from_formula
 from statsmodels.discrete.discrete_model import Poisson
 poisson = Poisson.from_formula
+from statsmodels.regression.quantreg import QuantReg
+quantreg = QuantReg.from_formula
