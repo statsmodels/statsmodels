be997807ad5983a6553cedbc940e7148bab12d80
Skipper Seabold <jsseabold@gmail.com>
2013-04-10 00:18:41 -0400

    REF: Remove cython call

--- statsmodels/tsa/kalmanf/setup.py
+++ statsmodels/tsa/kalmanf/setup.py
@@ -1,19 +1,10 @@
 #! /usr/bin/env python
 
-import os.path
-
-cur_dir = os.path.abspath(os.path.dirname(__file__))
-
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import (Configuration,
                                            get_numpy_include_dirs)
 
     config = Configuration('kalmanf', parent_package, top_path)
-
-    # This function tries to create C files from the given .pyx files.  If
-    # it fails, we build the checked-in .c files.
-    cython(['kalman_loglike.pyx'], working_path=cur_dir)
-
     config.add_extension('kalman_loglike',
                      sources=['kalman_loglike.c'],
                      include_dirs=[get_numpy_include_dirs()])
