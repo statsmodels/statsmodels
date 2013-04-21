979e1fb08511d6d03d3fab3c8e4db85775ef366a
Josef Perktold <josef.pktd@gmail.com>
2013-04-21 09:19:55 -0400

    TST/DOC first test and example for proportion tests

---
+++ statsmodels/examples/ex_proportion.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+
+Created on Sun Apr 21 07:59:26 2013
+
+Author: Josef Perktold
+"""
+
+import numpy as np
+import statsmodels.stats.proportion as sms
+import statsmodels.stats.weightstats as smw
+
+from numpy.testing import assert_almost_equal
+
+
+# Region, Eyes, Hair, Count
+ss = '''\
+1 blue  fair   23  1 blue  red     7  1 blue  medium 24
+1 blue  dark   11  1 green fair   19  1 green red     7
+1 green medium 18  1 green dark   14  1 brown fair   34
+1 brown red     5  1 brown medium 41  1 brown dark   40
+1 brown black   3  2 blue  fair   46  2 blue  red    21
+2 blue  medium 44  2 blue  dark   40  2 blue  black   6
+2 green fair   50  2 green red    31  2 green medium 37
+2 green dark   23  2 brown fair   56  2 brown red    42
+2 brown medium 53  2 brown dark   54  2 brown black  13'''
+
+dta0 = np.array(ss.split()).reshape(-1,4)
+dta = np.array(map(tuple, dta0.tolist()), dtype=[('Region', int), ('Eyes', 'S6'), ('Hair', 'S6'), ('Count', int)])
+
+xfair = np.repeat([1,0], [228, 762-228])
+
+# comparing to SAS last output at
+# http://support.sas.com/documentation/cdl/en/procstat/63104/HTML/default/viewer.htm#procstat_freq_sect028.htm
+# confidence interval for tost
+ci01 = smw.confint_ztest(xfair, alpha=0.1)
+assert_almost_equal(ci01,  [0.2719, 0.3265], 4)
+res = smw.ztost(xfair, 0.18, 0.38)
+
+assert_almost_equal(res[1][0], 7.1865, 4)
+assert_almost_equal(res[2][0], -4.8701, 4)
+
+nn = np.arange(200, 351)
+pow_z = sms.power_ztost_prop(0.5, 0.72, nn, 0.6, alpha=0.05)
+pow_bin = sms.power_ztost_prop(0.5, 0.72, nn, 0.6, alpha=0.05, dist='binom')
+import matplotlib.pyplot as plt
+plt.plot(nn, pow_z[0], label='normal')
+plt.plot(nn, pow_bin[0], label='binomial')
+plt.legend(loc='lower right')
+plt.title('Proportion Equivalence Test: Power as function of sample size')
+plt.xlabel('Number of Observations')
+plt.ylabel('Power')
+
+plt.show()
+
--- statsmodels/stats/tests/test_proportion.py
+++ statsmodels/stats/tests/test_proportion.py
@@ -10,8 +10,10 @@ import numpy as np
 from numpy.testing import assert_almost_equal
 
 from statsmodels.stats.proportion import confint_proportion
+import statsmodels.stats.proportion as smprop
 
-
+class Holder(object):
+    pass
 
 def test_confint_proportion():
     from results.results_proportion import res_binom, res_binom_methods
@@ -34,6 +36,51 @@ def test_confint_proportion():
 
             assert_almost_equal(ci, [res_low, res_upp], decimal=6,
                                 err_msg=repr(case) + method)
+
+class CheckProportionMixin(object):
+    def test_proptest(self):
+        pt = smprop.proportions_chisquare(self.n_success, self.nobs, value=None)
+        assert_almost_equal(pt[0], self.res_prop_test.statistic, decimal=10)
+        assert_almost_equal(pt[1], self.res_prop_test.p_value, decimal=10)
+
+    def test_pairwiseproptest(self):
+        ppt = smprop.proportions_chisquare_allpairs(self.n_success, self.nobs,
+                                  value=None, multitest_method=None)
+        assert_almost_equal(ppt[0], self.res_ppt_pvals_raw)
+        ppt = smprop.proportions_chisquare_allpairs(self.n_success, self.nobs,
+                                  value=None, multitest_method='h')
+        assert_almost_equal(ppt[0], self.res_ppt_pvals_raw)
+
+class TestProportion(CheckProportionMixin):
+    def setup(self):
+        self.n_success = np.array([ 73,  90, 114,  75])
+        self.nobs = np.array([ 86,  93, 136,  82])
+
+        self.res_ppt_pvals_raw = np.array([
+                 0.00533824886503131, 0.8327574849753566, 0.1880573726722516,
+                 0.002026764254350234, 0.1309487516334318, 0.1076118730631731
+                ])
+        res_ppt_pvals_holm = np.array([
+                 0.02669124432515654, 0.8327574849753566, 0.4304474922526926,
+                 0.0121605855261014, 0.4304474922526926, 0.4304474922526926
+                ])
+
+        res_prop_test = Holder()
+        res_prop_test.statistic = 11.11938768628861
+        res_prop_test.parameter = 3
+        res_prop_test.p_value = 0.011097511366581344
+        res_prop_test.estimate = np.array([
+             0.848837209302326, 0.967741935483871, 0.838235294117647,
+             0.9146341463414634
+            ]).reshape(4,1, order='F')
+        res_prop_test.null_value = '''NULL'''
+        res_prop_test.conf_int = '''NULL'''
+        res_prop_test.alternative = 'two.sided'
+        res_prop_test.method = '4-sample test for equality of proportions ' + \
+                               'without continuity correction'
+        res_prop_test.data_name = 'smokers2 out of patients'
+        self.res_prop_test = res_prop_test
+
 
 if __name__ == '__main__':
     test_confint_proportion()
