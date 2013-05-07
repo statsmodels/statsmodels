ccaf823b755004143f7aa4fefef74b4efde83d87
tylerhartley <tyleha@gmail.com>
2013-05-07 12:20:28 -0400

    Added some dashed lines around the master group, x and y label arguments, and made comparison_name optional so you could just plot all groups in black without preference. for pull request #809

--- statsmodels/sandbox/stats/multicomp.py
+++ statsmodels/sandbox/stats/multicomp.py
@@ -94,29 +94,29 @@ from statsmodels.graphics import utils
 
 
 qcrit = '''
-	2 		3 		4 		5 		6 		7 		8 		9 		10
-5 	3.64 5.70 	4.60 6.98 	5.22 7.80 	5.67 8.42 	6.03 8.91 	6.33 9.32 	6.58 9.67 	6.80 9.97 	6.99 10.24
-6 	3.46 5.24 	4.34 6.33 	4.90 7.03 	5.30 7.56 	5.63 7.97 	5.90 8.32 	6.12 8.61 	6.32 8.87 	6.49 9.10
-7 	3.34 4.95 	4.16 5.92 	4.68 6.54 	5.06 7.01 	5.36 7.37 	5.61 7.68 	5.82 7.94 	6.00 8.17 	6.16 8.37
-8 	3.26 4.75 	4.04 5.64 	4.53 6.20 	4.89 6.62 	5.17 6.96 	5.40 7.24       5.60 7.47 	5.77 7.68 	5.92 7.86
-9 	3.20 4.60 	3.95 5.43 	4.41 5.96 	4.76 6.35 	5.02 6.66 	5.24 6.91       5.43 7.13 	5.59 7.33 	5.74 7.49
-10 	3.15 4.48 	3.88 5.27 	4.33 5.77 	4.65 6.14 	4.91 6.43 	5.12 6.67       5.30 6.87 	5.46 7.05 	5.60 7.21
-11 	3.11 4.39 	3.82 5.15 	4.26 5.62 	4.57 5.97 	4.82 6.25 	5.03 6.48	5.20 6.67 	5.35 6.84 	5.49 6.99
-12 	3.08 4.32 	3.77 5.05 	4.20 5.50 	4.51 5.84 	4.75 6.10 	4.95 6.32	5.12 6.51 	5.27 6.67 	5.39 6.81
-13 	3.06 4.26 	3.73 4.96 	4.15 5.40 	4.45 5.73 	4.69 5.98 	4.88 6.19	5.05 6.37 	5.19 6.53 	5.32 6.67
-14 	3.03 4.21 	3.70 4.89 	4.11 5.32 	4.41 5.63 	4.64 5.88 	4.83 6.08	4.99 6.26 	5.13 6.41 	5.25 6.54
-15 	3.01 4.17 	3.67 4.84 	4.08 5.25 	4.37 5.56 	4.59 5.80 	4.78 5.99	4.94 6.16 	5.08 6.31 	5.20 6.44
-16 	3.00 4.13 	3.65 4.79 	4.05 5.19 	4.33 5.49 	4.56 5.72 	4.74 5.92	4.90 6.08 	5.03 6.22 	5.15 6.35
-17 	2.98 4.10 	3.63 4.74 	4.02 5.14 	4.30 5.43 	4.52 5.66 	4.70 5.85	4.86 6.01 	4.99 6.15 	5.11 6.27
-18 	2.97 4.07 	3.61 4.70 	4.00 5.09 	4.28 5.38 	4.49 5.60 	4.67 5.79	4.82 5.94 	4.96 6.08 	5.07 6.20
-19 	2.96 4.05 	3.59 4.67 	3.98 5.05 	4.25 5.33 	4.47 5.55 	4.65 5.73	4.79 5.89 	4.92 6.02 	5.04 6.14
-20 	2.95 4.02 	3.58 4.64 	3.96 5.02 	4.23 5.29 	4.45 5.51 	4.62 5.69	4.77 5.84 	4.90 5.97 	5.01 6.09
-24 	2.92 3.96 	3.53 4.55 	3.90 4.91 	4.17 5.17 	4.37 5.37 	4.54 5.54	4.68 5.69 	4.81 5.81 	4.92 5.92
-30 	2.89 3.89 	3.49 4.45 	3.85 4.80 	4.10 5.05 	4.30 5.24 	4.46 5.40	4.60 5.54 	4.72 5.65 	4.82 5.76
-40 	2.86 3.82 	3.44 4.37 	3.79 4.70 	4.04 4.93 	4.23 5.11 	4.39 5.26	4.52 5.39 	4.63 5.50 	4.73 5.60
-60 	2.83 3.76 	3.40 4.28 	3.74 4.59 	3.98 4.82 	4.16 4.99 	4.31 5.13	4.44 5.25 	4.55 5.36 	4.65 5.45
-120 	2.80 3.70 	3.36 4.20 	3.68 4.50 	3.92 4.71 	4.10 4.87 	4.24 5.01	4.36 5.12 	4.47 5.21 	4.56 5.30
-infinity 	2.77 3.64 	3.31 4.12 	3.63 4.40 	3.86 4.60 	4.03 4.76 	4.17 4.88 	4.29 4.99 	4.39 5.08 	4.47 5.16
+  2     3     4     5     6     7     8     9     10
+5   3.64 5.70   4.60 6.98   5.22 7.80   5.67 8.42   6.03 8.91   6.33 9.32   6.58 9.67   6.80 9.97   6.99 10.24
+6   3.46 5.24   4.34 6.33   4.90 7.03   5.30 7.56   5.63 7.97   5.90 8.32   6.12 8.61   6.32 8.87   6.49 9.10
+7   3.34 4.95   4.16 5.92   4.68 6.54   5.06 7.01   5.36 7.37   5.61 7.68   5.82 7.94   6.00 8.17   6.16 8.37
+8   3.26 4.75   4.04 5.64   4.53 6.20   4.89 6.62   5.17 6.96   5.40 7.24       5.60 7.47   5.77 7.68   5.92 7.86
+9   3.20 4.60   3.95 5.43   4.41 5.96   4.76 6.35   5.02 6.66   5.24 6.91       5.43 7.13   5.59 7.33   5.74 7.49
+10  3.15 4.48   3.88 5.27   4.33 5.77   4.65 6.14   4.91 6.43   5.12 6.67       5.30 6.87   5.46 7.05   5.60 7.21
+11  3.11 4.39   3.82 5.15   4.26 5.62   4.57 5.97   4.82 6.25   5.03 6.48 5.20 6.67   5.35 6.84   5.49 6.99
+12  3.08 4.32   3.77 5.05   4.20 5.50   4.51 5.84   4.75 6.10   4.95 6.32 5.12 6.51   5.27 6.67   5.39 6.81
+13  3.06 4.26   3.73 4.96   4.15 5.40   4.45 5.73   4.69 5.98   4.88 6.19 5.05 6.37   5.19 6.53   5.32 6.67
+14  3.03 4.21   3.70 4.89   4.11 5.32   4.41 5.63   4.64 5.88   4.83 6.08 4.99 6.26   5.13 6.41   5.25 6.54
+15  3.01 4.17   3.67 4.84   4.08 5.25   4.37 5.56   4.59 5.80   4.78 5.99 4.94 6.16   5.08 6.31   5.20 6.44
+16  3.00 4.13   3.65 4.79   4.05 5.19   4.33 5.49   4.56 5.72   4.74 5.92 4.90 6.08   5.03 6.22   5.15 6.35
+17  2.98 4.10   3.63 4.74   4.02 5.14   4.30 5.43   4.52 5.66   4.70 5.85 4.86 6.01   4.99 6.15   5.11 6.27
+18  2.97 4.07   3.61 4.70   4.00 5.09   4.28 5.38   4.49 5.60   4.67 5.79 4.82 5.94   4.96 6.08   5.07 6.20
+19  2.96 4.05   3.59 4.67   3.98 5.05   4.25 5.33   4.47 5.55   4.65 5.73 4.79 5.89   4.92 6.02   5.04 6.14
+20  2.95 4.02   3.58 4.64   3.96 5.02   4.23 5.29   4.45 5.51   4.62 5.69 4.77 5.84   4.90 5.97   5.01 6.09
+24  2.92 3.96   3.53 4.55   3.90 4.91   4.17 5.17   4.37 5.37   4.54 5.54 4.68 5.69   4.81 5.81   4.92 5.92
+30  2.89 3.89   3.49 4.45   3.85 4.80   4.10 5.05   4.30 5.24   4.46 5.40 4.60 5.54   4.72 5.65   4.82 5.76
+40  2.86 3.82   3.44 4.37   3.79 4.70   4.04 4.93   4.23 5.11   4.39 5.26 4.52 5.39   4.63 5.50   4.73 5.60
+60  2.83 3.76   3.40 4.28   3.74 4.59   3.98 4.82   4.16 4.99   4.31 5.13 4.44 5.25   4.55 5.36   4.65 5.45
+120   2.80 3.70   3.36 4.20   3.68 4.50   3.92 4.71   4.10 4.87   4.24 5.01 4.36 5.12   4.47 5.21   4.56 5.30
+infinity  2.77 3.64   3.31 4.12   3.63 4.40   3.86 4.60   4.03 4.76   4.17 4.88   4.29 4.99   4.39 5.08   4.47 5.16
 '''
 
 res = [line.split() for line in qcrit.replace('infinity','9999').split('\n')]
@@ -667,16 +667,24 @@ class TukeyHSDResults(object):
                                       #you maybe shouldn't be using Multiple Comparisons anyways...        
         self.halfwidths = (self.q_crit/np.sqrt(2))*w 
 
-    def plot_intervals(self, comparison_name, ax=None, figsize=(10,6)):
+    def plot_intervals(self, comparison_name=None, ax=None, figsize=(10,6), xlabel=None, ylabel=None):
         """Perform the plotting, color coding to visualize Hochberg intervals
 
-        :comparison_name: a string master group name that others should be compared to
+        :comparison_name: (optional) if provided, plot_intervals will color code all groups that are
+            significantly different from the comparison_name red, and will color code insig-
+            nificant groups gray. Otherwise, all intervals will just be plotted in black.
+        :ax: (optional) a matplotlib axis on which to attach the plot
         :figsize: (optional) tupe of the size of the figure generated
+        :xlabel: a str used as x axis label
+        :ylabel: a str used for y axis label
+
+        :returns: the figure containing the plots
         """
         import matplotlib.pyplot as plt
 
         fig, ax1 = utils.create_mpl_ax(ax)
-        fig.set_size_inches(figsize)
+        if figsize != None:
+            fig.set_size_inches(figsize)
         if getattr(self, 'halfwidths', None) == None:
             raise AttributeError, "Perform compute_intervals before plotting"
         means = self._multicomp.groupstats.groupmean
@@ -687,33 +695,44 @@ class TukeyHSDResults(object):
         minrange = [means[i]-self.halfwidths[i] for i in range(len(means))]
         maxrange = [means[i]+self.halfwidths[i] for i in range(len(means))]
         
-        for i in range(len(means)):
-            if self.groupsunique[i] == comparison_name: continue
-        
-            if min(maxrange[i], maxrange[midx]) - max(minrange[i], minrange[midx]) < 0:
-                sigidx.append(i)
-            else:
-                nsigidx.append(i)
-                
-        #Plot the master comparison
-        ax1.errorbar(means[midx], midx, xerr=self.halfwidths[midx], marker='o', linestyle='None', color='b', ecolor='b')
-        #Plot those that are significantly different
-        if len(sigidx) > 0:
-            ax1.errorbar(means[sigidx], sigidx, xerr=self.halfwidths[sigidx], marker='o', linestyle='None', color='r', ecolor='r')
-        #Plot those that are not significantly different
-        if len(nsigidx) > 0:
-            ax1.errorbar(means[nsigidx], nsigidx, xerr=self.halfwidths[nsigidx], marker='o', linestyle='None', color='0.5', ecolor='0.5')
-        
-        #ax1.set_autoscale_on(True)
+        if comparison_name == None:
+            ax1.set_title('Multiple Comparisons Between Groups')
+            ax1.errorbar(means, range(len(means)), xerr=self.halfwidths, marker='o', 
+                            linestyle='None', color='k', ecolor='k')
+
+        else:   
+            for i in range(len(means)):
+                if self.groupsunique[i] == comparison_name: continue
+                if min(maxrange[i], maxrange[midx]) - max(minrange[i], minrange[midx]) < 0:
+                    sigidx.append(i)
+                else:
+                    nsigidx.append(i) 
+            #Plot the master comparison
+            ax1.set_title('Multiple Comparisons against %s'%comparison_name)
+            ax1.errorbar(means[midx], midx, xerr=self.halfwidths[midx], marker='o', linestyle='None', 
+                                color='b', ecolor='b')
+            ax1.plot([minrange[midx]]*2, [-1, self._multicomp.ngroups], linestyle='--', color='0.7')
+            ax1.plot([maxrange[midx]]*2, [-1, self._multicomp.ngroups], linestyle='--', color='0.7')
+            #Plot those that are significantly different
+            if len(sigidx) > 0:
+                ax1.errorbar(means[sigidx], sigidx, xerr=self.halfwidths[sigidx], marker='o', 
+                                linestyle='None', color='r', ecolor='r')
+            #Plot those that are not significantly different
+            if len(nsigidx) > 0:
+                ax1.errorbar(means[nsigidx], nsigidx, xerr=self.halfwidths[nsigidx], marker='o', 
+                                linestyle='None', color='0.5', ecolor='0.5')
+            
         r = np.max(maxrange) - np.min(minrange) 
-        p = ax1.set_ylim([-1, self._multicomp.ngroups])
-        p = ax1.set_xlim([np.min(minrange)-r/10., np.max(maxrange)+r/10.]) 
-        p = ax1.set_title('Multiple Comparisons against %s'%comparison_name)
+        ax1.set_ylim([-1, self._multicomp.ngroups])
+        ax1.set_xlim([np.min(minrange)-r/10., np.max(maxrange)+r/10.]) 
         ax1.set_yticklabels(np.insert(self.groupsunique, 0, ''))
+        ax1.set_xlabel(xlabel if xlabel != None else '')
+        ax1.set_ylabel(ylabel if ylabel != None else '')
         return fig
         
 
-    def compute_plot_intervals(self, comparison_name, q_crit=None, S=None, figsize=(10, 6), ax=None):
+    def compute_plot_intervals(self, comparison_name=None, q_crit=None, S=None, ax=None, figsize=(10, 6), xlabel=None,
+                                        ylabel=None):
         """Compute and plot each group mean along with their CIs to visually identify significant differences.
 
         Multiple comparison tests are nice, but lack a good way to be visualized. If you have, say, 6 groups, 
@@ -732,8 +751,8 @@ class TukeyHSDResults(object):
         :S: (optional) the group RMSE 
         :figsize: (optional) the size of the plotted figure
         """
-        self.compute_intervals(q_crit=q_crit, S=S)
-        return self.plot_intervals(comparison_name, figsize=figsize, ax=ax)
+        self.compute_intervals(q_crit, S)
+        return self.plot_intervals(comparison_name, ax, figsize, xlabel, ylabel)
 
 
 
@@ -887,12 +906,11 @@ class MultiComparison(object):
                             useranks=False)
 
         means = self.groupstats.groupmean
-        nobs = self.groupstats.groupnobs
         #var_ = self.groupstats.groupvarwithin() #possibly an error in varcorrection in this case
         var_ = np.var(self.groupstats.groupdemean(), ddof=len(means))
         #res contains: 0:(idx1, idx2), 1:reject, 2:meandiffs, 3: std_pairs, 4:confint, 5:q_crit,
         #6:df_total, 7:reject2 
-        res = tukeyhsd(means, nobs, var_, df=None, alpha=alpha, q_crit=None)
+        res = tukeyhsd(means, self.nobs, var_, df=None, alpha=alpha, q_crit=None)
 
         resarr = np.array(zip(res[0][0], res[0][1],
                                   np.round(res[2],4),
