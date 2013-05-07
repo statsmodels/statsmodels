1785fab73440f8f976728571c03964d1143b81ba
Chad Fulton <chad@chadfulton.com>
2013-05-07 04:03:18 -0700

    Add 3 new unit tests for arima_process: arma_acovf, arma_acf, arma_generate_sample

--- statsmodels/tsa/tests/test_arima_process.py
+++ statsmodels/tsa/tests/test_arima_process.py
@@ -4,8 +4,8 @@ from numpy.testing import (assert_array_almost_equal, assert_almost_equal,
                            assert_equal)
 
 
-from statsmodels.tsa.arima_process import (arma_impulse_response,
-                        lpol_fiar, lpol_fima)
+from statsmodels.tsa.arima_process import (arma_generate_sample, arma_acovf,
+                        arma_acf, arma_impulse_response, lpol_fiar, lpol_fima)
 from statsmodels.sandbox.tsa.fftarma import ArmaFft
 
 from results.results_process import armarep  #benchmarkdata
@@ -20,6 +20,66 @@ malist = [[1.],
           [1, -0.9],
           [1,  0.9, -0.3]]
 
+
+def test_arma_acovf():
+    # Check for specific AR(1)
+    N = 20;
+    phi = 0.9;
+    sigma = 1;
+    # rep 1: from module function
+    rep1 = arma_acovf([1, -phi], [1], N);
+    # rep 2: manually
+    rep2 = [1.*sigma*phi**i/(1-phi**2) for i in range(N)];
+    assert_almost_equal(rep1, rep2, 7); # 7 is max precision here
+
+
+def test_arma_acf():
+    # Check for specific AR(1)
+    N = 20;
+    phi = 0.9;
+    sigma = 1;
+    # rep 1: from module function
+    rep1 = arma_acf([1, -phi], [1], N);
+    # rep 2: manually
+    acovf = np.array([1.*sigma*phi**i/(1-phi**2) for i in range(N)])
+    rep2 = acovf / (1./(1-phi**2));
+    assert_almost_equal(rep1, rep2, 8); # 8 is max precision here
+
+
+def _manual_arma_generate_sample(ar, ma, eta):
+    T = len(eta);
+    ar = ar[::-1];
+    ma = ma[::-1];
+    p,q = len(ar), len(ma);
+    rep2 = [0]*max(p,q); # initialize with zeroes
+    for t in range(T):
+        yt = eta[t];
+        if p:
+            yt += np.dot(rep2[-p:], ar);
+        if q:
+            # left pad shocks with zeros
+            yt += np.dot([0]*(q-t) + list(eta[max(0,t-q):t]), ma);
+        rep2.append(yt);
+    return np.array(rep2[max(p,q):]);
+
+def test_arma_generate_sample():
+    # Test that this generates a true ARMA process
+    # (amounts to just a test that scipy.signal.lfilter does what we want)
+    T = 100;
+    dists = [np.random.randn]
+    for dist in dists:
+        np.random.seed(1234);
+        eta = dist(T);
+        for ar in arlist:
+            for ma in malist:
+                # rep1: from module function
+                np.random.seed(1234);
+                rep1 = arma_generate_sample(ar, ma, T, distrvs=dist);
+                # rep2: "manually" create the ARMA process
+                ar_params = -1*np.array(ar[1:]);
+                ma_params = np.array(ma[1:]);
+                rep2 = _manual_arma_generate_sample(ar_params, ma_params, eta)
+                assert_array_almost_equal(rep1, rep2, 13);
 
 
 def test_fi():
@@ -68,6 +128,9 @@ def test_armafft():
 
 
 if __name__ == '__main__':
+    test_arma_acovf()
+    test_arma_acf()
+    test_arma_generate_sample()
     test_fi()
     test_arma_impulse_response()
     test_spectrum()
