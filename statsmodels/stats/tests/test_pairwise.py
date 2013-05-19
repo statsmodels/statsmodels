256e711fcff5cb1a6e391efe881acfc693194120
tylerhartley <tyleha@gmail.com>
2013-05-18 18:00:15 -0700

    added basic smoke tests for hochberg interval using new data set. Not sure if new data set must be configured as previous ones (i.e. as recfromtxt arrays).

--- statsmodels/stats/tests/test_pairwise.py
+++ statsmodels/stats/tests/test_pairwise.py
@@ -112,6 +112,19 @@ ss5 = '''\
 1 - 2	-4.600	-8.249	-0.951	***
 1 - 3	-0.260	-3.909	3.389	'''
 
+cylinders = np.array([8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 4, 8, 8, 8, 8, 8, 8, 8, 8, 8, 4, 6, 6, 6, 4, 4, 
+                    4, 4, 4, 4, 6, 8, 8, 8, 8, 4, 4, 4, 4, 8, 8, 8, 8, 6, 6, 6, 6, 4, 4, 4, 4, 6, 6, 
+                    6, 6, 4, 4, 4, 4, 4, 8, 4, 6, 6, 8, 8, 8, 8, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 
+                    4, 4, 4, 4, 4, 4, 4, 6, 6, 4, 6, 4, 4, 4, 4, 4, 4, 4, 4])
+cyl_labels = np.array(['USA', 'USA', 'USA', 'USA', 'USA', 'USA', 'USA', 'USA', 'USA', 'USA', 'France', 
+    'USA', 'USA', 'USA', 'USA', 'USA', 'USA', 'USA', 'USA', 'USA', 'Japan', 'USA', 'USA', 'USA', 'Japan', 
+    'Germany', 'France', 'Germany', 'Sweden', 'Germany', 'USA', 'USA', 'USA', 'USA', 'USA', 'Germany', 
+    'USA', 'USA', 'France', 'USA', 'USA', 'USA', 'USA', 'USA', 'USA', 'USA', 'USA', 'USA', 'USA', 'Germany', 
+    'Japan', 'USA', 'USA', 'USA', 'USA', 'Germany', 'Japan', 'Japan', 'USA', 'Sweden', 'USA', 'France', 
+    'Japan', 'Germany', 'USA', 'USA', 'USA', 'USA', 'USA', 'USA', 'USA', 'USA', 'USA', 'USA', 'USA', 'USA', 
+    'Germany', 'Japan', 'Japan', 'USA', 'USA', 'Japan', 'Japan', 'Japan', 'Japan', 'Japan', 'Japan', 'USA', 
+    'USA', 'USA', 'USA', 'Japan', 'USA', 'USA', 'USA', 'Germany', 'USA', 'USA', 'USA'])
+
 #accommodate recfromtxt for python 3.2, requires bytes
 ss = asbytes(ss)
 ss2 = asbytes(ss2)
@@ -138,6 +151,8 @@ def get_thsd(mci, alpha=0.05):
     #print resi[4]
     var2 = (mci.groupstats.groupvarwithin() * (nobs - 1)).sum() \
                                                         / (nobs - 1).sum()
+    print nobs, (nobs - 1).sum()
+    print mci.groupstats.groupvarwithin()
     assert_almost_equal(var_, var2, decimal=14)
     return resi
 
@@ -154,7 +169,7 @@ class CheckTuckeyHSDMixin(object):
         assert_equal(self.res.reject, self.reject2)
 
     def test_group_tukey(self):
-        res_t = get_thsd(self.mc,alpha=self.alpha)
+        res_t = get_thsd(self.mc, alpha=self.alpha)
         assert_almost_equal(res_t[4], self.confint2, decimal=2)
 
     def test_shortcut_function(self):
@@ -218,6 +233,35 @@ class TestTuckeyHSD3(CheckTuckeyHSDMixin):
         self.meandiff2 = sas_['mean']
         self.confint2 = sas_[['lower','upper']].view(float).reshape((3,2))
         self.reject2 = sas_['sig'] == asbytes('***')
+
+class TestTuckeyHSD4(CheckTuckeyHSDMixin):
+
+    @classmethod
+    def setup_class(self):
+        #unbalanced case verified in Matlab
+        self.endog = cylinders
+        self.groups = cyl_labels
+        self.alpha = 0.05
+        self.setup_class_()
+        self.res.compute_intervals()
+
+        #from Matlab
+        self.halfwidth2 = np.array([1.5228335685980883, 0.9794949704444682, 0.78673802805533644, 
+                                    2.3321237694566364, 0.57355135882752939])
+        self.meandiff2 = np.array([0.22222222222222232, 0.13333333333333375, 0.0, 2.2898550724637685, 
+                            -0.088888888888888573, -0.22222222222222232, 2.0676328502415462, 
+                            -0.13333333333333375, 2.1565217391304348, 2.2898550724637685])
+        self.confint2 = np.array([-2.32022210717, 2.76466655161, -2.247517583, 2.51418424967, 
+                            -3.66405224956, 3.66405224956, 0.113960166573, 4.46574997835, 
+                            -1.87278583908, 1.6950080613, -3.529655688, 3.08521124356, 0.568180988881, 
+                            3.5670847116, -3.31822643175, 3.05155976508, 0.951206924521, 3.36183655374,
+                             -0.74487911754, 5.32458926247]).reshape(10,2)
+        self.reject2 = np.array([False, False, False,  True, False, False,  True, False,  True, False])
+
+    def test_hochberg_intervals(self):
+        assert_almost_equal(self.res.halfwidths, self.halfwidth2, 14)
+
+
 
 if __name__ == '__main__':
     import statsmodels.sandbox.stats.multicomp as multi #incomplete refactoring
