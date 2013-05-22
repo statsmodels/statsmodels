fix ztest incorrect variance for 2sample, add tests

statsmodels/stats/tests/test_weightstats.py
@@ -19,7 +19,7 @@ import numpy as np
 from scipy import stats
 from numpy.testing import assert_almost_equal, assert_equal, assert_allclose
 from statsmodels.stats.weightstats import \
-                DescrStatsW, CompareMeans, ttest_ind
+                DescrStatsW, CompareMeans, ttest_ind, ztest
 
 class Holder(object):
     pass
@@ -372,3 +372,17 @@ def test_ztest_ztost():
     res1 = d2.ztost_mean(0.4, 0.6)
     res2 = smprop.proportions_ztost(15, 20., 0.4, 0.6)
     assert_almost_equal(res1[0], res2[0], decimal=12)
+
+    x2 = [0, 1]
+    w2 = [10, 10]
+    #d2 = DescrStatsW(x1, np.array(w1)*21./20)
+    d2 = DescrStatsW(x2, w2)
+    res1 = ztest(d1.asrepeats(), d2.asrepeats())
+    res2 = smprop.proportions_chisquare(np.asarray([15, 10]),
+                                        np.asarray([20., 20]))
+    #TODO: check this is this difference expected?, see test_proportion
+    assert_allclose(res1[1], res2[1], rtol=0.03)
+
+    res1a = CompareMeans(d1, d2).ztest_ind()
+    assert_allclose(res1a[1], res2[1], rtol=0.03)
+    assert_almost_equal(res1a, res1, decimal=12)

statsmodels/stats/weightstats.py
@@ -1010,6 +1010,12 @@ def ttost_paired(x1, x2, low, upp, transform=None, weights=None):
 
 def ztest(x1, x2=None, value=0, alternative='2-sided', usevar='pooled'):
     '''test for mean based on normal distribution, one or two samples
+
+    In the case of two samples, the samples are assumed to be independent.
+
+    usevar not implemented, is always pooled in two sample case
+    use CompareMeans instead.
+    TODO: this should delegate to CompareMeans like ttest_ind
     '''
     #usevar is not used, always pooled
     x1 = np.asarray(x1)
@@ -1021,7 +1027,8 @@ def ztest(x1, x2=None, value=0, alternative='2-sided', usevar='pooled'):
         nobs2 = x2.shape[0]
         x2_mean = x2.mean(0)
         x2_var = x2.var(0)
-        var_pooled = (nobs1 * x1_var + nobs2 * x2_var) / (nobs1 + nobs2)
+        var_pooled = (nobs1 * x1_var + nobs2 * x2_var) / (nobs1 + nobs2 - 2)
+        var_pooled *= (1. / nobs1 + 1. / nobs2)
     else:
         var_pooled = x1_var / nobs1
         x2_mean = 0
@@ -1037,6 +1044,8 @@ def confint_ztest(x1, x2=None, value=0, alpha=0.05, alternative='2-sided',
     Notes
     -----
     checked only for 1 sample case
+
+    usevar not implemented, is always pooled in two sample case
     '''
     #usevar is not used, always pooled
     # mostly duplicate code from ztest
@@ -1050,6 +1059,7 @@ def confint_ztest(x1, x2=None, value=0, alpha=0.05, alternative='2-sided',
         x2_mean = x2.mean(0)
         x2_var = x2.var(0)
         var_pooled = (nobs1 * x1_var + nobs2 * x2_var) / (nobs1 + nobs2)
+        var_pooled *= (1. / nobs1 + 1. / nobs2)
     else:
         var_pooled = x1_var / nobs1
         x2_mean = 0