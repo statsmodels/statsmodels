2f79da3d6ad798c26bdca567c44db88fa8f9d051
Josef Perktold <josef.pktd@gmail.com>
2013-05-26 19:33:44 -0400

    REF remove old lowess.py again (was readded during rebase)

--- statsmodels/nonparametric/lowess.py
+++
@@ -1,5 +0,0 @@
-import warnings
-warnings.warn('the module lowess is deprecated and will be removed in 0.5.'
-              'The new module name is smoothers_lowess, and lowess (function)'
-              'can be imported now through statsmodels.api')
-from smoothers_lowess import *
