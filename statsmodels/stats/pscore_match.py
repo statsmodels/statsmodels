dce673cb1999b6dda30cdda8361d0e827c178cf6
Javier Burroni <javier.burroni@gmail.com>
2013-05-29 16:41:01 -0300

    more on pscore

--- statsmodels/stats/pscore_match.py
+++ statsmodels/stats/pscore_match.py
@@ -13,23 +13,34 @@ class PropensityScoreMatch(object):
     def __init__(self, assigment_index, covariates, treatment_objective_variable):
         self.assigment_index = assigment_index
         self.covariates = covariates
-        self.tratment_objective_variable = tratment_objective_variable
+        self.treatment_objective_variable = treatment_objective_variable
         
     def fit(self):
-        result = PScoreMatchResult()
-        p_mod = sm.Logit(self.assigment_index, self.covariates)
-        p_res = mod.fit()
-        result.propensity_estimation(p_res)
-        self.scores = p_res.predict(self.covariates)
+        self.result = PScoreMatchResult()
+        self.compute_pscore()
         self.stratify()
         
-    def check_balance_for(strata):
+    def compute_pscore(self):
+        p_mod = sm.Logit(self.assigment_index, self.covariates)
+        p_res = p_mod.fit()
+        self.scores = p_res.predict(self.covariates)
+        self.result.propensity_estimation = p_res
+        
+    def compute_stratas(self, strata = None):
+        if not(strata is None) and self.check_balance_for(strata):
+            return [strata,]
+        half = (self.scores.max() + self.scores.min())/2
+        left, right = self.scores < half, self.scores > half
+        return self.compute_stratas(left) + self.compute_stratas(right)
+            
+        
+        
+    def check_balance_for(self, strata):
         endog, exog = self.assigment_index[strata], sm.add_constant(self.covariates[strata], prepend = True)
-        om = sm.OM(endog, exog)
+        om = sm.OLS(endog, exog)
         res= om.fit()
-        return (res.f_pvalue < 0.05) and (result.pvalues < 0.05).all() 
+        return (res.f_pvalue > 0.05) and (result.pvalues > 0.05).all() 
         
         
 class PScoreMatchResult(object):
-    def propensity_estimation(self, result):
-        self.propensity_estimation = result
+    pass
