013d32124ee6dccfa93c51c77092025ad93dbad0
Vincent Arel-Bundock <varel@umich.edu>
2013-06-27 00:05:50 -0400

    ENH: summary_params use_t

--- statsmodels/iolib/summary2.py
+++ statsmodels/iolib/summary2.py
@@ -289,8 +289,8 @@ def summary_model(results):
             pass
     return out
 
-def summary_params(results, alpha=.05, params=None, bse=None, tvalues=None,
-        pvalues=None, confint=None):
+def summary_params(results, yname=None, xname=None, alpha=.05, use_t=True,
+                   skip_header=False, float_format="%.4f"):
     '''create a summary table of parameters from results instance
 
     Parameters
@@ -298,27 +298,51 @@ def summary_params(results, alpha=.05, params=None, bse=None, tvalues=None,
     res : results instance
         some required information is directly taken from the result
         instance
+    yname : string or None
+        optional name for the endogenous variable, default is "y"
+    xname : list of strings or None
+        optional names for the exogenous variables, default is "var_xx"
     alpha : float
         significance level for the confidence intervals
+    use_t : bool
+        indicator whether the p-values are based on the Student-t
+        distribution (if True) or on the normal distribution (if False)
+    skip_headers : bool
+        If false (default), then the header row is added. If true, then no
+        header row is added.
+    float_format : string
+        float formatting options (e.g. ".3g")
 
     Returns
     -------
-    params_table : DataFrame instance
+    params_table : SimpleTable instance
     '''
-    #Parameters part of the summary table
-    vals = [params, bse, tvalues, pvalues, confint]
-    if any([x is None for x in vals]):
+
+    if isinstance(results, tuple):
+        results, params, std_err, tvalues, pvalues, conf_int = results
+    else:
         params = results.params
         bse = results.bse
         tvalues = results.tvalues
         pvalues = results.pvalues
-        confint = results.conf_int(alpha)
+        conf_int = results.conf_int(alpha)
+
     data = np.array([params, bse, tvalues, pvalues]).T
-    data = np.hstack([data, confint])
+    data = np.hstack([data, conf_int])
     data = pd.DataFrame(data)
-    data.columns = ['Coef.', 'Std.Err.', 't', 'P>|t|',
-                    '[' + str(alpha/2), str(1-alpha/2) + ']']
-    data.index = results.model.exog_names
+
+    if use_t:
+        data.columns = ['Coef.', 'Std.Err.', 't', 'P>|t|',
+                        '[' + str(alpha/2), str(1-alpha/2) + ']']
+    else:
+        data.columns = ['Coef.', 'Std.Err.', 'z', 'P>|z|',
+                        '[' + str(alpha/2), str(1-alpha/2) + ']']
+
+    if not xname:
+        data.index = results.model.exog_names
+    else:
+        data.index = xname
+
     return data
 
 
