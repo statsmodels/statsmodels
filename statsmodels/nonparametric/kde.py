Fixes to incorrect calculation of variance, and removal of alternative calculation method.

statsmodels/nonparametric/kde.py
@@ -311,16 +311,6 @@ class KDEUnivariate(object):
 
     #     return (x-cdf[index-1])*1.0/(cdf[index]-cdf[index-1])*(support[index]-support[index-1])+ support[index-1]
 
-    def variance_alt(self, point):
-        """
-        Evaluates the variance of the kernel estimator according
-        to the approximate formula
-         v = 1/n * (1/h^2 sum(K(x-X_i/h)^2) - fhat(x,h)^2)
-        """
-
-        _checkisfit(self)
-        return self.kernel.variance(self.endog, point, self.bw)
-
     def variance(self, point):
         """
         Evaluates the variance of the kernel estimator according
@@ -331,8 +321,7 @@ class KDEUnivariate(object):
         """
 
         _checkisfit(self)
-        fh = self.evaluate(point)
-        return 1./(len(self.endog*self.bw))*1/(np.sqrt(np.pi)*2)*fh-1./(len(self.endog))*fh**2
+        return self.kernel.density_variance(self.endog, point)
 
     def evaluate(self, point):
         """

statsmodels/sandbox/nonparametric/kernels.py
@@ -189,29 +189,23 @@ class CustomKernel(object):
         else:
             return np.nan
 
-    def variance(self, xs, x, H):
+    def density_variance(self, xs, x):
+        """Returns an estimate of the density estimate for a point x based on
+        x-values xs. Uses the approximate variance formula:
+        $var(f(x))=\frac{1}{nh}R(K)-\frac{f(x)}{n}$
+        """
 
         xs = np.asarray(xs)
-        n = len(xs) # before inDomain?
+        n = len(xs)
         xs = self.inDomain( xs, xs, x )[0]
         if xs.ndim == 1:
-            xs = xs[:,None]
+            xs = xs[:, None]
         if len(xs)>0:
             h = self.h
-            w =  np.mean(1/H**2 * self((xs-x)/h)**2 - self.density(xs,x)**2, axis=0)
-            return w
-        else:
-            return np.nan
-
-        n = len(xs)
-        #xs = self.inDomain( xs, xs, x )[0]
-        if len(xs)>0:  ## Need to do product of marginal distributions
-            #w = np.sum([self(self._Hrootinv * (xx-x).T ) for xx in xs])/n
-            #vectorized doesn't work:
-            t1 = 1./h**2 * self(((xs-x)/h)**2) #transposed
-            t2 =  - self.density(xs,x)**2
-            w = np.mean(t1-t2)
-            #w = np.mean([self(xd) for xd in ((xs-x) * self._Hrootinv)] ) #transposed
+            # Note 05/07/13 - _L2Norm actually seems to store the square of the
+            # norm. This is equal to R(K).
+            R = self._L2Norm
+            w =  (1./h * self.density(xs, x) * R - self.density(xs, x)**2)/n
             return w
         else:
             return np.nan
@@ -400,6 +394,7 @@ class Gaussian(CustomKernel):
         CustomKernel.__init__(self, shape = lambda x: 0.3989422804014327 *
                         np.exp(-x**2/2.0), h = h, domain = None, norm = 1.0)
         self._L2Norm = 1.0/(2.0*np.sqrt(np.pi))
+
 
     def smooth(self, xs, ys, x):
         """Returns the kernel smoothing estimate for point x based on x-values