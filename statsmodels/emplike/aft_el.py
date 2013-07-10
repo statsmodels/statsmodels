db68dc256bfa7535ba152e4b4bea6e0176f8b930
Justin Grana <jg3705a@student.american.edu>
2013-07-10 10:45:19 -0400

    STY: Changed import structure so tests can be run in place.  Before, tests were importsing statsmodels.api which lead to an error since kalman_loglike wasn't built in place.

--- statsmodels/emplike/aft_el.py
+++ statsmodels/emplike/aft_el.py
@@ -29,16 +29,17 @@ Statistics. 14:3, 643-656.
 """
 
 import numpy as np
-from statsmodels.api import OLS, WLS, add_constant
+from statsmodels.regression.linear_model import OLS, WLS
+from statsmodels.tools import add_constant
 #from elregress import ElReg
 from scipy import optimize
 from scipy.stats import chi2
-from descriptive import _OptFuncts
+from descriptive import _OptFuncts_mixin
 # ^ this will change when descriptive gets merged
 import warnings
 
 
-class OptAFT(_OptFuncts):
+class OptAFT(_OptFuncts_mixin):
     """
     Provides optimization functions used in estimating and conducting
     inference in an AFT model.
