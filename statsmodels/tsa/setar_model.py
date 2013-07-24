0503128e1d729e05f6290e7a18f26d6f0cbe231e
Chad Fulton <chad@chadfulton.com>
2013-07-24 16:01:11 -0700

    Implemented hyper parameter grid search selection

--- statsmodels/tsa/setar_model.py
+++ statsmodels/tsa/setar_model.py
@@ -42,7 +42,15 @@ class SETAR(tsbase.TimeSeriesModel):
         The maximum delay parameter to consider if a grid search is used. If
         left blank, it is set to be the ar_order.
     threshold_grid_size : integer, optional
-        The number of elements in the threshold grid if a grid search is used.
+        The approximate number of elements in the threshold grid if a grid
+        search is used.
+
+
+    Notes
+    -----
+    threshold_grid_size is only approximate because it uses values from the
+    threshold variable itself, approximately evenly spaced, and there may be a
+    few more elements in the grid search than requested
     """
 
     # TODO are there too many parameters here?
@@ -132,12 +140,131 @@ class SETAR(tsbase.TimeSeriesModel):
 
         return OLS(endog, exog).fit()
 
+    def _grid_search_objective(self, delay, thresholds, XX, resids):
+        """
+        Objective function to maximize in SETAR(2) hyperparameter grid search
+
+        Corresponds to f_2(\gamma, d) in Hansen (1999), but extended to any
+        number of thresholds.
+        """
+        endog, exog = self.build_datasets(
+            delay, thresholds, order=len(thresholds)+1
+        )
+
+        # Intermediate calculations
+        k = self.ar_order+1
+        X1 = exog[:, :-k]
+        X = self.exog[self.nobs_initial:]
+        X1X1 = X1.T.dot(X1)
+        XX1 = X.T.dot(X1)
+        Mn = np.linalg.inv(
+            X1X1 - XX1.T.dot(XX).dot(XX1)
+        )
+
+        # Return objective
+        return resids.T.dot(X1).dot(Mn).dot(X1.T).dot(resids)
+
+    def _select_hyperparameters_grid(self, thresholds, threshold_grid_size,
+                                     XX, resids, delay_grid=None):
+
+        if delay_grid is None:
+            delay_grid = range(2, self.max_delay+1)
+
+        max_obj = 0
+        params = (None, None)
+        # Iterate over possible delay values
+        for delay in delay_grid:
+
+            # Build the appropriate threshold grid given delay
+            threshold_var = np.unique(np.sort(self.endog[:-delay]))
+            nobs = len(threshold_var)
+            indices = np.arange(self.min_regime_num,
+                                nobs - self.min_regime_num,
+                                max(np.floor(nobs / threshold_grid_size), 1),
+                                dtype=int)
+            threshold_grid = threshold_var[indices]
+
+            # Iterate over possible threshold values
+            for threshold in threshold_grid:
+                try:
+                    obj = self._grid_search_objective(
+                        delay, np.sort([threshold] + thresholds),
+                        XX, resids
+                    )
+                    if obj > max_obj:
+                        max_obj = obj
+                        params = (delay, threshold)
+                # Some threshold values don't allow enough values in each
+                # regime; we just need to not select those thresholds
+                except InvalidRegimeError:
+                    pass
+
+        return params, max_obj
+
     def select_hyperparameters(self):
         """
         Select delay and threshold hyperparameters via grid search
         """
 
-        raise NotImplementedError
+        # Cache calculations
+        endog = self.endog[self.nobs_initial:]
+        exog = self.exog[self.nobs_initial:]
+        XX = np.linalg.inv(exog.T.dot(exog))    # (X'X)^{-1}
+        resids = endog - np.dot(                # SETAR(1) residuals
+            exog,
+            XX.dot(exog.T.dot(endog))
+        )
+
+        # Get threshold grid size
+        threshold_grid_size = self.threshold_grid_size
+
+        # Estimate the delay and an initial value for the dominant threshold
+        thresholds = []
+        params, min_obj = self._select_hyperparameters_grid(
+            thresholds, threshold_grid_size, XX, resids
+        )
+        delay = params[0]
+        thresholds.append(params[1])
+
+        # Get remaining thresholds
+        for i in range(2, self.order):
+
+            # Get initial estimate of next threshold
+            params, min_obj = self._select_hyperparameters_grid(
+                thresholds, threshold_grid_size, XX, resids,
+                delay_grid=[delay]
+            )
+            thresholds.append(params[1])
+
+            # Iterate threshold selection to convergence
+            proposed = thresholds[:]
+            iteration = 0
+            maxiter = 100
+            while True:
+                iteration += 1
+
+                # Recalculate each threshold individually, holding the others
+                # constant, starting at the first threshold
+                for j in range(i):
+                    params, min_obj = self._select_hyperparameters_grid(
+                        thresholds[:j] + thresholds[j+1:],
+                        threshold_grid_size, XX, resids,
+                        delay_grid=[delay]
+                    )
+                    proposed[j] = params[1]
+
+                # If the recalculation produced no change, we've converged
+                if proposed == thresholds:
+                    break
+                # If convergence is happening fast enough
+                if iteration > maxiter:
+                    print ('Warning: Maximum number of iterations has been '
+                           'exceeded.')
+                    break
+
+                thresholds = proposed[:]
+
+        return delay, np.sort(thresholds)
 
 
 class SETARResults:
