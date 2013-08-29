29aa33bf204e25013b6eff62eab1bee70a7a77ac
Ana Martinez Pardo <anamartinezpardo@gmail.com>
2013-08-30 00:14:19 +0200

    Summary improved and fixed some pep8 minor errors.
     * new in summary: method, iterations, elapsed time, num. cases,
            num. and frequencies of alternatives.
    
    Note than from previous commit, when a specific alternative
    variable isn't present on all utilities, no longer need to do
    previous work like:
      df ['vrble_alternative'] = df ['variable'] * (choice_index == 0)

--- statsmodels/discrete/dcm_clogit.py
+++ statsmodels/discrete/dcm_clogit.py
@@ -16,10 +16,11 @@ Train, K. `Discrete Choice Methods with Simulation`.
 
 import numpy as np
 import pandas as pd
-from statsmodels.base.model import GenericLikelihoodModel, GenericLikelihoodModelResults
+from statsmodels.base.model import (GenericLikelihoodModel,
+                                    GenericLikelihoodModelResults)
 import statsmodels.api as sm
-
 import time
+
 
 class CLogit(GenericLikelihoodModel):
     __doc__ = """
@@ -35,10 +36,10 @@ class CLogit(GenericLikelihoodModel):
         default and should be added by the user.
     V: dict
         a dictionary with the names of the explanatory variables for the
-        utility function for each alternative. Variables with common
-        coefficients have to be first in each array.
-        For explanatory variables, choose an alternative and drop all on it.
-        This is for identification.
+        utility function for each alternative.
+        Alternative specific variables (common coefficients) have to be first.
+        For specific variables (various coefficients), choose an alternative and
+        drop all specific variables on it.
     ncommon : int
         number of explanatory variables with common coefficients.
 
@@ -83,22 +84,20 @@ class CLogit(GenericLikelihoodModel):
         self.exog_data = exog_data
         self.V = V
         self.ncommon = ncommon
-        super(CLogit, self).__init__(endog = endog_data , **kwds)
-
-    def initialize(self):
 
         self.J = len(self.V)
-        self.nobs = self.endog.shape[0]/self.J
+        self.nobs = endog_data.shape[0] / self.J
 
         # Endog_bychoices
-        self.endog_bychoices = self.endog.reshape(-1, self.J)
+        self.endog_bychoices = endog_data.values.reshape(-1, self.J)
         # Exog_bychoices
         exog_bychoices = []
         exog_bychoices_names = []
-        self.choice_index = np.arange(self.J*self.nobs) % self.J
+        self.choice_index = np.arange(self.J * self.nobs) % self.J
 
         for ii, key in enumerate(iter(self.V)):
-            (exog_bychoices.append(self.exog_data[self.V[key]][self.choice_index==ii]
+            (exog_bychoices.append(self.exog_data[self.V[key]]
+                                    [self.choice_index == ii]
                                     .values.reshape(self.nobs, -1)))
 
         for key in self.V:
@@ -107,43 +106,49 @@ class CLogit(GenericLikelihoodModel):
         self.exog_bychoices = exog_bychoices
 
         # Betas
-        beta_not_common = ([len(exog_bychoices_names[ii])-self.ncommon
+        beta_not_common = ([len(exog_bychoices_names[ii]) - self.ncommon
                             for ii in range(self.J)])
-        zi = np.r_[[self.ncommon], self.ncommon + np.array(beta_not_common).cumsum()]
+        zi = np.r_[[self.ncommon], self.ncommon + np.array(beta_not_common)\
+                    .cumsum()]
         z = np.arange(max(zi))
-        beta_ind = [np.r_[np.arange(self.ncommon), z[zi[ii]:zi[ii+1]]]
-                               for ii in range(len(zi)-1)] # index of betas
+        beta_ind = [np.r_[np.arange(self.ncommon), z[zi[ii]:zi[ii + 1]]]
+                               for ii in range(len(zi) - 1)]  # index of betas
         beta_ind = beta_ind
         self.beta_ind = beta_ind
         beta_ind_str = ([map(str, (np.r_[np.arange(self.ncommon),
-                                        z[zi[ii]:zi[ii+1]]]).tolist())
-                               for ii in range(len(zi)-1)])# str index of betas
+                                        z[zi[ii]:zi[ii + 1]]]).tolist())
+                               for ii in range(len(zi) - 1)])  # str index of betas
 
-        print 'Coefficients: '
         betas = {}
 
         for sublist in range(self.J):
             aa = []
             for ii in range(len(exog_bychoices_names[sublist])):
-                aa.append( beta_ind_str[sublist][ii]
-                          +"_"+ exog_bychoices_names[sublist][ii] )
-            betas [sublist] = aa
+                aa.append(beta_ind_str[sublist][ii]
+                          + "_" + exog_bychoices_names[sublist][ii])
+            betas[sublist] = aa
 
         for key in betas:
-            print '{0} => {1:10}'.format(key, betas[key])
+            print '{0} => {1:10}'.format(V.keys()[key], betas[key])
 
-        # Exog matrix
-        pieces =[]
+        # Exog
+        pieces = []
         Vkeys = []
         for ii in range(self.J):
             pieces.append(pd.DataFrame(exog_bychoices[ii], columns=betas[ii]))
-            Vkeys.append(ii+1)
+            Vkeys.append(ii + 1)
 
-        exog_matrix_all = (pd.concat(pieces, axis=0,keys = Vkeys, names =
-                           ['choice', 'nobs'])
-                           .fillna(value=0).sortlevel(1).reset_index())
+        self.exog_matrix_all = (pd.concat(pieces, axis = 0, keys = Vkeys,
+                                     names =['choice', 'nobs'])
+                           .fillna(value = 0).sortlevel(1).reset_index())
 
-        self.exog_matrix = exog_matrix_all.iloc [:, 2:]
+        self.exog_matrix = self.exog_matrix_all.iloc[:, 2:]
+
+        print 'Parameters to estimate: '
+        print self.exog_matrix.columns.tolist()
+
+        super(CLogit, self).__init__(endog = endog_data, exog = self.exog_matrix,
+                                        **kwds)
 
         self.K = len(self.exog_matrix.columns)
         self.df_model = self.K
@@ -175,11 +180,10 @@ class CLogit(GenericLikelihoodModel):
 
         Notes
         -----
-        .. math:: \\frac{\\exp\\left(\\beta_{j}^{\\prime}x_{i}\\right)}{\\sum_{k=0}^{J}\\exp\\left(\\beta_{k}^{\\prime}x_{i}\\right)}
+        .. math:: \\frac{\\exp\\left(\\beta_{j}^{\\prime}x_{i}\\right){\\sum_{k=0}^{J}\\exp\\left(\\beta_{k}^{\\prime}x_{i}\\right)}
         """
         eXB = np.exp(X)
-        return  eXB/eXB.sum(1)[:, None]
-
+        return  eXB / eXB.sum(1)[:, None]
 
     def loglike(self, params):
 
@@ -209,7 +213,36 @@ class CLogit(GenericLikelihoodModel):
 
         return loglike.sum()
 
-    def scoreX(self, params):
+    def loglikeobs(self, params):
+        """
+        Log-likelihood for each observation.
+
+        Parameters
+        ----------
+        params : array-like
+            The parameters of the model.
+
+        Returns
+        -------
+        loglike : ndarray (nobs,)
+            The log likelihood for each observation of the model evaluated
+            at `params`. See Notes
+
+        Notes
+        ------
+        .. math:: \\ln L_{i}=\\sum_{j=0}^{J}d_{ij}\\ln\\left(\\frac{\\exp\\left(\\beta_{j}^{\\prime}x_{i}\\right)}{\\sum_{k=0}^{J}\\exp\\left(\\beta_{k}^{\\prime}x_{i}\\right)}\\right)
+
+        for observations :math:`i=1,...,n`
+
+        where :math:`d_{ij}=1` if individual `i` chose alternative `j` and 0
+        if not.
+        """
+
+        xb = self.xbetas(params)
+
+        return  (self.endog_bychoices * np.log(self.cdf(xb))).sum(1)
+
+    def score(self, params):
         """
         Score/gradient matrix for conditional logit model log-likelihood
 
@@ -232,17 +265,10 @@ class CLogit(GenericLikelihoodModel):
 
         for :math:`j=1,...,J`
         """
-        # TODO
-#       Rgradient = np.array([ -1.60028400e-09,  -2.46809100e-09,
-#                                  4.05103000e-11, 1.79633500e-09,
-#                                  3.78327600e-11,  -9.05671600e-11])
-#        gc,ttme,air:(intercept),hinc_air,train:(intercept),bus:(intercept)
 
-#        firsterm = (self.endog_bychoices - self.cdf(self.xbetas(params))).flatten('F')
-#
-#        return np.dot(firsterm.T, self.exog_matrix.values)
-
-
+        firstterm = (self.endog_bychoices - self.cdf(self.xbetas(params)))\
+                    .reshape(-1, 1)
+        return  np.dot(firstterm.T, self.exog).flatten()
 
     def jac(self, params):
         """
@@ -267,16 +293,12 @@ class CLogit(GenericLikelihoodModel):
 
         for :math:`j=1,...,J`, for observations :math:`i=1,...,n`
 
-        The jac is (nobs , K) but is returned as a flattened array.
-        The Jacobian has the observations in rows and the flatteded array of
-        derivatives in columns.
         """
 
-#        firsterm = (self.endog_bychoices - self.cdf(self.xbetas(params))).flatten('F')
-#
-#        return (firsterm [:, None] * self.exog_matrix.values).flatten()
+        firsterm = (self.endog_bychoices - self.cdf(self.xbetas(params)))\
+                    .reshape(-1, 1)
 
-        return NotImplementedError
+        return (firsterm * self.exog)
 
     def hessianX(self, params):
         """
@@ -303,18 +325,17 @@ class CLogit(GenericLikelihoodModel):
         :math:`\\boldsymbol{1}\\left(j=l\\right)` equals 1 if `j` = `l` and 0
         otherwise.
         """
-        return NotImplementedError
+        return NotImplement
 
-
-    def fit(self, start_params=None, maxiter=10000, maxfun=5000, method="newton",
-            full_output=1, disp=1, callback=None,**kwds):
+    def fit(self, start_params=None, maxiter=10000, maxfun=5000,
+            method="newton", full_output=1, disp=1, callback=None, **kwds):
         """
         Fits CLogit() model using maximum likelihood.
         In a model linear the log-likelihood function of the sample, is
         global concave for β parameters, which facilitates its numerical
         maximization (McFadden, 1973).
         Fixed Method = Newton, because it'll find the maximum in a few
-        iterations. Newton method require a likelihood function, a score/gradient,
+        iterations. Newton method require a likelihood function, a gradient,
         and a Hessian. Since analytical solutions are known, we give it.
         Initial parameters estimates from the standard logit
         Returns
@@ -326,28 +347,29 @@ class CLogit(GenericLikelihoodModel):
 
         if start_params is None:
             print 'Estimating initial parameters..'
-            logit_mod = sm.Logit(self.endog , self.exog_matrix)
+            logit_mod = sm.Logit(self.endog, self.exog_matrix)
             logit_res = logit_mod.fit()
-            start_params  = logit_res.params.values
+            start_params = logit_res.params.values
             print start_params
         else:
             start_params = np.asarray(start_params)
 
         start_time = time.time()
         print 'Estimating model..'
-        model_fit =  super(CLogit, self).fit(start_params=start_params, method=method,
-                                    maxiter=maxiter, maxfun=maxfun,**kwds)
+        model_fit = super(CLogit, self).fit(start_params=start_params,
+                        method=method, maxiter=maxiter, maxfun=maxfun, **kwds)
         end_time = time.time()
         self.elapsed_time = end_time - start_time
+
         print("the elapsed time was %g seconds" % (self.elapsed_time))
         return model_fit
 
 ### Results Class ###
 
+
 class CLogitResults (GenericLikelihoodModelResults):
 
-    # TODO on summary: frequencies of alternatives, McFadden R^2, Likelihood
-    #   ratio test, method, iterations.
+    # TODO on summary: McFadden R^2, Likelihood ratio test
 
     def __init__(self, model, mlefit, **kwds):
 
@@ -355,7 +377,9 @@ class CLogitResults (GenericLikelihoodModelResults):
         self.mlefit = mlefit
         self.nobs_bychoice = model.nobs
         self.__dict__.update(mlefit.__dict__)
-        self.xname = model.exog_matrix.columns.tolist()
+        self.alt = model.V.keys()
+        self.freq_alt = (model.endog_bychoices[:, ].sum(0) / model.nobs)\
+                        .tolist()
 
     def __getstate__(self):
         try:
@@ -365,16 +389,12 @@ class CLogitResults (GenericLikelihoodModelResults):
             pass
         return self.__dict__
 
-    def summary(self, yname=None, xname=None, title=None, alpha=.05,
-                yname_list=None):
+
+    def summary(self, title= None, alpha=.05):
         """Summarize the Clogit Results
 
         Parameters
         -----------
-        yname : string, optional
-            Default is `y`
-        xname : list of strings, optional
-            Default is `var_##` for ## in p the number of regressors
         title : string, optional
             Title for the top table. If not None, then this replaces the
             default title
@@ -384,7 +404,7 @@ class CLogitResults (GenericLikelihoodModelResults):
         Returns
         -------
         smry : Summary instance
-            this holds the summary tables and text, which can be printed or
+            this holds the summary tables and text, which caclon be printed or
             converted to various output formats.
 
         See Also
@@ -395,40 +415,54 @@ class CLogitResults (GenericLikelihoodModelResults):
         """
 
         top_left = [('Dep. Variable:', None),
-                     ('Model:', [self.model.__class__.__name__]),
-                     ('Method:', ['MLE']),
-                     ('Date:', None),
-                     ('Time:', None),
-                     ('Converged:', ["%s" % self.mle_retvals['converged']]),
-                     ('Iterations:', ["%s" % self.mle_retvals['iterations']]),
-                     ('Elapsed time:' , ['NotImplemented'] )
+                    ('Model:', [self.model.__class__.__name__]),
+                    ('Method:', [self.mle_settings['optimizer']]),
+                    ('Date:', None),
+                    ('Time:', None),
+                    ('Converged:', ["%s" % self.mle_retvals['converged']]),
+                    ('Iterations:', ["%s" % self.mle_retvals['iterations']]),
+                    ('Elapsed time (seg.):',
+                                    ["%10.4f" % self.model.elapsed_time]),
+                    ('Num. alternatives:', [self.model.J])
                       ]
 
-        top_right = [('No. Observations:', [self.nobs_bychoice]),
+        top_right = [('No. Cases:', [self.nobs]),
+                     ('No. Observations:', [self.nobs_bychoice]),
                      ('Df Residuals:', None),
                      ('Df Model:', None),
-                     ('Log-Likelihood:', None),
+                     ('Log-Likelihood:', None)
                      ]
 
         if title is None:
             title = self.model.__class__.__name__ + ' ' + \
-            "class of results"
+            "results"
 
         #boiler plate
-        from statsmodels.iolib.summary import Summary
+        from statsmodels.iolib.summary import Summary, SimpleTable
+
         smry = Summary()
         # for top of table
         smry.add_table_2cols(self, gleft=top_left, gright=top_right,
                              title=title)
-        # for parameters, etc
+
+        # Frequencies of alternatives
+        mydata = [self.freq_alt]
+        myheaders = self.alt
+        mytitle = ("Frequencies of alternatives: ")
+
+        tbl = SimpleTable(mydata, myheaders, title = mytitle,
+                          data_fmts = ["%3.3f"])
+        smry.tables.append(tbl)
+
+        # for parameters
+        # TODO rename parameters
+
         smry.add_table_params(self, alpha=alpha, use_t=False)
 
         return smry
 
 if __name__ == "__main__":
 
-    # example
-    import pandas as pd
     from patsy import dmatrices
 
     url = "http://vincentarelbundock.github.io/Rdatasets/csv/Ecdat/ModeChoice.csv"
@@ -438,33 +472,32 @@ if __name__ == "__main__":
         import urllib
         urllib.urlretrieve(url, "ModeChoice.csv")
     df = pd.read_csv(file_)
-    pd.set_printoptions(max_rows=1000, max_columns=20)
     df.describe()
 
     f = 'mode  ~ ttme+invc+invt+gc+hinc+psize'
     y, X = dmatrices(f, df, return_type='dataframe')
 
-    ### Names of the variables for the utility function for each alternative
-    #Variables with common coefficients have to be first in each array
+    # Names of the variables for the utility function for each alternative
+    # variables with common coefficients have to be first in each array
     V = {
-        "1": ['gc', 'Intercept', 'hinc'],
-        "2": ['gc', 'Intercept', 'hinc'],
-        "3": ['gc', 'Intercept', 'hinc'],
-        "4": ['gc'],
-         }
+        "air": ['gc', 'ttme', 'Intercept', 'hinc'],
+        "train": ['gc', 'ttme', 'Intercept'],
+        "bus ": ['gc', 'ttme', 'Intercept'],
+        "car": ['gc', 'ttme']
+        }
 
-    #Number of common coefficients
-    ncommon= 1
+    # Number of common coefficients
+    ncommon = 2
 
-    ###
+    # Model
+    print 'Example:'
     start_time = time.time()
 
-    clogit_mod  =  CLogit (y, X,  V, ncommon)
-    clogit_res =  clogit_mod.fit()
+    clogit_mod = CLogit(y, X,  V, ncommon)
+    clogit_res = clogit_mod.fit()
 
     end_time = time.time()
     print("the whole elapsed time was %g seconds.") % (end_time - start_time)
 
-    print clogit_mod.exog_matrix.columns.tolist()
     print clogit_res.params
     print CLogitResults(clogit_mod, clogit_res).summary()
