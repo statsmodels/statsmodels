47cb2fbf2f201eb82466c101571e277deea620b0
Josef Perktold <josef.pktd@gmail.com>
2013-10-06 09:51:56 -0400

    TST: IV2SLS add df_model, more unit test

--- statsmodels/sandbox/regression/tests/test_gmm.py
+++ statsmodels/sandbox/regression/tests/test_gmm.py
@@ -7,7 +7,7 @@ Author: Josef Perktold
 """
 
 import numpy as np
-from numpy.testing import assert_allclose, assert_almost_equal
+from numpy.testing import assert_allclose, assert_almost_equal, assert_equal
 
 from statsmodels import iolib
 from statsmodels.tools.tools import add_constant
@@ -52,7 +52,8 @@ def get_griliches76_data():
 # use module global to load only once
 yg_df, xg_df, zg_df = get_griliches76_data()
 
-endog, exog, instrument = map(np.asarray, [yg_df, xg_df, zg_df])
+endog = np.asarray(yg_df, dtype=float)  # TODO: why is yg_df float32
+exog, instrument = map(np.asarray, [xg_df, zg_df])
 
 
 
@@ -236,10 +237,46 @@ class CheckIV2SLS(object):
         assert_allclose(res1.bse, res2.bse, rtol=1e-10, atol=0)
         assert_allclose(res1.bse, res2.bse, rtol=0, atol=1e-11)
 
+        assert_allclose(res1.tvalues, res2.tvalues, rtol=5e-10, atol=0)
+
+
     def test_other(self):
         res1, res2 = self.res1, self.res2
         assert_allclose(res1.rsquared, res2.r2, rtol=1e-7, atol=0)
+        assert_allclose(res1.rsquared_adj, res2.r2_a, rtol=1e-7, atol=0)
+
+        # TODO: why is fvalue different, IV2SLS uses inherited linear
+        #assert_allclose(res1.fvalue, res2.F, rtol=1e-10, atol=0)
+        #assert_allclose(res1.f_pvalue, res2.Fp, rtol=1e-10, atol=0)
+        assert_allclose(np.sqrt(res1.mse_resid), res2.rmse, rtol=1e-10, atol=0)
         assert_allclose(res1.ssr, res2.rss, rtol=1e-10, atol=0)
+        assert_allclose(res1.uncentered_tss, res2.yy, rtol=1e-10, atol=0)
+        assert_allclose(res1.centered_tss, res2.yyc, rtol=1e-10, atol=0)
+        assert_allclose(res1.ess, res2.mss, rtol=1e-9, atol=0)
+
+        assert_equal(res1.df_model, res2.df_m)
+        assert_equal(res1.df_resid, res2.df_r)
+
+        # TODO: llf raise NotImplementedError
+        #assert_allclose(res1.llf, res2.ll, rtol=1e-10, atol=0)
+
+    def test_hypothesis(self):
+        res1, res2 = self.res1, self.res2
+        restriction = np.eye(len(res1.params))
+        res_t = res1.t_test(restriction)
+        assert_allclose(res_t.tvalue, res1.tvalues, rtol=1e-12, atol=0)
+        assert_allclose(res_t.pvalue, res1.pvalues, rtol=1e-12, atol=0)
+        res_f = res1.f_test(restriction[:-1]) # without constant
+        # TODO res1.fvalue problem, see issue #1104
+        #assert_allclose(res_f.fvalue, res1.fvalue, rtol=1e-12, atol=0)
+        assert_allclose(res_f.fvalue, res2.F, rtol=1e-10, atol=0)
+        assert_allclose(res_f.pvalue, res2.Fp, rtol=1e-08, atol=0)
+
+
+    def test_smoke(self):
+        res1 = self.res1
+        # TODO: llf raise NotImplementedError, used in summary
+        #res1.summary()
 
 
 
