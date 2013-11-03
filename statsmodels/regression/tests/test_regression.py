98770ac3ec4e0df40b0abb2549f11e782035f958
Kevin Sheppard <kevin.sheppard@economics.ox.ac.uk>
2013-11-03 13:54:51 +0000

    Made tests tougher

--- statsmodels/regression/tests/test_regression.py
+++ statsmodels/regression/tests/test_regression.py
@@ -557,8 +557,8 @@ class TestOLS_GLS_WLS_equivalence(object):
         cls.results = []
         cls.results.append(OLS(y, X).fit())
         cls.results.append(WLS(y, X, w).fit())
-        cls.results.append(GLS(y, X, 0.5*w).fit())
-        cls.results.append(GLS(y, X, np.diag(2.0*w)).fit())
+        cls.results.append(GLS(y, X, 100*w).fit())
+        cls.results.append(GLS(y, X, np.diag(0.1*w)).fit())
 
     def test_ll(self):
         llf = np.array([r.llf for r in self.results])
