statsmodels/statsmodels

commit 61175498ada40061c97caf89b213997d36378f7d

REF: use robustcov in RLM as default, see #1164

statsmodels/robust/robust_linear_model.py
@@ -401,6 +401,7 @@ class RLMResults(base.LikelihoodModelResults):
         #for remove_data
         self.data_in_cache = ['sresid']
 
+        self.cov_params_default = self.bcov_scaled
         #TODO: "pvals" should come from chisq on bse?
 
     @cache_readonly

commit 87684b7cdc7e231a9059fca87e3bac7b57728445

BUG: RLM bcov_unscaled was broken by 61175498ada40061c97caf89b213997d36378f7d

statsmodels/robust/robust_linear_model.py
@@ -418,7 +418,7 @@ class RLMResults(base.LikelihoodModelResults):
 
     @cache_readonly
     def bcov_unscaled(self):
-        return self.cov_params(scale=1.)
+        return self.normalized_cov_params
 
     @cache_readonly
     def weights(self):