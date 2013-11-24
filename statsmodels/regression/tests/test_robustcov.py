58119dc416d9182401438d0dd4552d4ed00765fb
Josef Perktold <josef.pktd@gmail.com>
2013-11-24 03:04:58 -0500

    TST  test for OLS cov_nw_groupsum

--- statsmodels/regression/tests/test_robustcov.py
+++ statsmodels/regression/tests/test_robustcov.py
@@ -361,3 +361,29 @@ class TestOLSRobustCluster2Large(CheckOLSRobustCluster, CheckOLSRobustNewMixin):
     # skipping see https://github.com/statsmodels/statsmodels/pull/1189#issuecomment-29141741
     def test_fvalue(self):
         pass
+
+
+
+class TestOLSRobustClusterGS(CheckOLSRobustCluster, CheckOLSRobustNewMixin):
+    # compare with `reg cluster`
+
+    def setup(self):
+        res_ols = self.res1.get_robustcov_results('nw-groupsum',
+                                                  time=self.time,
+                                                  maxlags=4,
+                                                  use_correction=False,
+                                                  use_t=True)
+        self.res3 = self.res1
+        self.res1 = res_ols
+        self.bse_robust = res_ols.bse
+        self.cov_robust = res_ols.cov_params()
+        cov1 = sw.cov_nw_groupsum(self.res1, 4, self.time, use_correction=False)
+        se1 =  sw.se_cov(cov1)
+        self.bse_robust2 = se1
+        self.cov_robust2 = cov1
+        self.small = True
+        self.res2 = res2.results_nw_groupsum4
+
+        self.skip_f = True
+        self.rtol = 1e-6
+        self.rtolh = 1e-10
