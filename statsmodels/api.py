9faa25de409dc7d67f50d71a326dbecf543e41cc
Kevin Sheppard <kevin.sheppard@economics.ox.ac.uk>
2014-03-26 23:39:10 +0000

    Rolled back absolute imports in api.py

--- statsmodels/api.py
+++ statsmodels/api.py
@@ -1,33 +1,33 @@
 import statsmodels.iolib as iolib
 import statsmodels.datasets as datasets
 import statsmodels.tools as tools
-from statsmodels.tools.tools import add_constant, categorical
+from .tools.tools import add_constant, categorical
 import statsmodels.regression as regression
-from statsmodels.regression.linear_model import OLS, GLS, WLS, GLSAR
-from statsmodels.regression.quantile_regression import QuantReg
-from statsmodels.genmod.generalized_linear_model import GLM
-from statsmodels.genmod.generalized_estimating_equations import GEE
-from statsmodels.genmod import families
+from .regression.linear_model import OLS, GLS, WLS, GLSAR
+from .regression.quantile_regression import QuantReg
+from .genmod.generalized_linear_model import GLM
+from .genmod.generalized_estimating_equations import GEE
+from .genmod import families
 import statsmodels.robust as robust
-from statsmodels.robust.robust_linear_model import RLM
-from statsmodels.discrete.discrete_model import (Poisson, Logit, Probit,
-                                                 MNLogit, NegativeBinomial)
-from statsmodels.tsa import api as tsa
-from statsmodels.nonparametric import api as nonparametric
+from .robust.robust_linear_model import RLM
+from .discrete.discrete_model import (Poisson, Logit, Probit,
+                                      MNLogit, NegativeBinomial)
+from .tsa import api as tsa
+from .nonparametric import api as nonparametric
 import statsmodels.distributions as distributions
-from statsmodels.__init__ import test
-from statsmodels import version
-from statsmodels.info import __doc__
-from statsmodels.graphics.gofplots import qqplot, qqplot_2samples, qqline, ProbPlot
-from statsmodels.graphics import api as graphics
-from statsmodels.stats import api as stats
-from statsmodels.emplike import api as emplike
+from .__init__ import test
+from . import version
+from .info import __doc__
+from .graphics.gofplots import qqplot, qqplot_2samples, qqline, ProbPlot
+from .graphics import api as graphics
+from .stats import api as stats
+from .emplike import api as emplike
 
-from statsmodels.formula import api as formula
+from .formula import api as formula
 
-from statsmodels.iolib.smpickle import load_pickle as load
+from .iolib.smpickle import load_pickle as load
 
-from statsmodels.tools.print_version import show_versions
+from .tools.print_version import show_versions
 
 import os
 
@@ -35,6 +35,7 @@ chmpath = os.path.join(os.path.dirname(__file__), 'statsmodelsdoc.chm')
 if os.path.exists(chmpath):
     def open_help(chmpath=chmpath):
         from subprocess import Popen
+
         p = Popen(chmpath, shell=True)
 
 del os
