fea525afdc7601bbde792ee3e37641fab9354507
Skipper Seabold <jsseabold@gmail.com>
2014-04-14 14:00:56 -0400

    DOC: Expand documentation notes

--- statsmodels/stats/stattools.py
+++ statsmodels/stats/stattools.py
@@ -21,8 +21,25 @@ def durbin_watson(resids, axis=0):
 
     Returns
     --------
-    Durbin Watson statistic.  This is defined as
-    sum_(t=2)^(T)((e_t - e_(t-1))^(2))/sum_(t=1)^(T)e_t^(2)
+    dw : float, array-like
+
+    The Durbin-Watson statistic.
+
+    Notes
+    -----
+    The null hypothesis of the test is that there is no serial correlation.
+    The Durbin-Watson test statistics is defined as:
+
+    .. math::
+
+       \sum_{t=2}^T((e_t - e_{t-1})^2)/\sum_{t=1}^Te_t^2
+
+    The test statistic is approximately equal to 2*(1-r) where ``r`` is the
+    sample autocorrelation of the residuals. Thus, for r == 0, indicating no
+    serial correlation, the test statistic equals 2. This statistic will
+    always be between 0 and 4. The closer to 0 the statistic, the more
+    evidence for positive serial correlation. The closer to 4, the more
+    evidence for negative serial correlation.
     """
     resids = np.asarray(resids)
     diff_resids = np.diff(resids, 1, axis=axis)
