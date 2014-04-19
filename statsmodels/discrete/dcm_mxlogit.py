06890fe5f9f9a0cde57847f5ad4cafe3f3cc2e3f
Josef Perktold <josef.pktd@gmail.com>
2014-04-19 09:00:45 -0400

    BUG/CLN: mxlogit: fix location of random options                   move example to module, add smoke test

--- statsmodels/discrete/dcm_mxlogit.py
+++ statsmodels/discrete/dcm_mxlogit.py
@@ -59,10 +59,6 @@ from .discrete_model import Logit
 from .halton_sequence import halton
 from .dcm_base import (DiscreteChoiceModel, DiscreteChoiceModelResults)
 
-# TODO remove these, copied from __main__ example
-NORMAL = ['gc']
-draws = 50
-
 DEBUG = False
 
 ### Public Model Classes ####
@@ -147,10 +143,16 @@ class MXLogit(DiscreteChoiceModel):
     def __init__(self, endog_data, exog_data,  V, draws, ncommon,
                  NORMAL,ref_level, name_intercept = None, **kwds):
 
+        self.NORMAL = NORMAL
+        if  NORMAL is not None:
+            self.draws = draws
+            self.num_rparam = len(NORMAL)
+
         super(MXLogit, self).__init__(endog_data, exog_data,  V, ncommon,
                  ref_level, name_intercept = None, **kwds)
 
-        self._initialize()
+        # super is already calling _initialize, don't call it twice
+        #self._initialize()
 
     def _initialize(self):
         """
@@ -159,11 +161,6 @@ class MXLogit(DiscreteChoiceModel):
         # Need a private Model Class for preprocesses the data for this model?
 
         super(MXLogit, self)._initialize()
-
-        self.NORMAL = NORMAL
-        if  NORMAL is not None:
-            self.draws = draws
-            self.num_rparam = len(NORMAL)
 
         self.values_ramdon = []
         self.n_ramdon = []
@@ -490,59 +487,3 @@ class MXLogitResults(DiscreteChoiceModelResults):
                           data_fmts = ["%5.4f"])
         smry.tables.append(tb2)
         return smry
-
-
-if __name__ == "__main__":
-
-    DEBUG = 0
-
-    print('Example:')
-    import statsmodels.api as sm
-    # Loading data as pandas object
-    data = sm.datasets.modechoice.load_pandas()
-    data.endog[:5]
-    data.exog[:5]
-    data.exog['Intercept'] = 1  # include an intercept
-    y, X = data.endog, data.exog
-
-    # Set up model
-    # Names of the variables for the utility function for each alternative
-    # variables with common coefficients have to be first in each array
-    # the order should be the same as sequence in data
-    # ie: row1 -- air data, row2 -- train data, row3 -- bus data, ...
-
-    V = OrderedDict((
-        ('air',   ['gc', 'ttme', 'Intercept', 'hinc']),
-        ('train', ['gc', 'ttme', 'Intercept']),
-        ('bus',   ['gc', 'ttme', 'Intercept']),
-        ('car',   ['gc', 'ttme']))
-        )
-
-    # Number of common coefficients
-    ncommon = 2
-
-    # Random coefficients and distributions (By now, only Normal)
-    # TODO: add Uniform, Triangular and Log-nomal
-
-    NORMAL = ['gc']
-
-    # Number of draws used for simulation
-    # eg: 50 for draf model, 1000 for final model
-    draws = 50
-
-    # Describe model
-    ref_level = 'car'
-    name_intercept = 'Intercept'
-    mxlogit_mod = MXLogit(endog_data = y, exog_data = X,  V = V,
-                          NORMAL = NORMAL, draws = draws,
-                          ncommon = ncommon, ref_level = ref_level,
-                          name_intercept = name_intercept)
-
-    # Fit model
-#    mxlogit_res = mxlogit_mod.fit(method = "bfgs", disp = 1)
-#    print mxlogit_res.params
-#    print mxlogit_res.llf
-
-    # Summarize model
-    # TODO means and standard errors for mixed logit parameters
-    print(mxlogit_mod.summary())
