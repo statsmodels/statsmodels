caf9fb55d93314080128297d55d01695e3d69636
Josef Perktold <josef.pktd@gmail.com>
2014-04-19 06:46:09 -0400

    REF compat 2to3 example

--- statsmodels/discrete/dcm_clogit_examples.py
+++ statsmodels/discrete/dcm_clogit_examples.py
@@ -38,14 +38,14 @@ data.exog[:5]
 data.exog['Intercept'] = 1  # include an intercept
 y, X = data.endog, data.exog
 
-print u"""
+print(u"""
 
 Example 1. Replicate Greene (2003) results.
 TABLE 21.11 Parameter Estimates. Unweighted Sample
     βG       βT      αair        γH         αtrain       αbus
 [-0.015501  -0.09612   5.2074  0.01328757  3.86905293  3.16319074]
 
-"""
+""")
 
 # Names of the variables for the utility function for each alternative
 # variables with common coefficients have to be first in each array
@@ -67,13 +67,13 @@ clogit_mod = CLogit(y, X, V, ncommon,
 clogit_res = clogit_mod.fit()
 
 end_time = time.time()
-print("the whole elapsed time was %g seconds."
-% (end_time - start_time))
+print(("the whole elapsed time was %g seconds."
+% (end_time - start_time)))
 
 # Results
-print clogit_mod.exog_matrix.columns.tolist()
-print clogit_res.params
-print CLogitResults(clogit_mod).summary()
+print(clogit_mod.exog_matrix.columns.tolist())
+print(clogit_res.params)
+print(CLogitResults(clogit_mod).summary())
 
 # hessian = clogit_mod.hessian(clogit_res.params)
 # print hessian
@@ -121,7 +121,7 @@ Log-Likelihood: -199.13
 McFadden R^2:  0.29825
 Likelihood ratio test : chisq = 169.26 (p.value = < 2.22e-16)"""
 
-print  u"""
+print("""
 Summary R results for example 1
 
 air:(intercept) train:(intercept)   bus:(intercept)   gc
@@ -137,9 +137,9 @@ bus:(intercept)          3.883696          6.766574      -17.978427    -21.70683
 gc                     192.371522       -776.604449      -21.706830 -75474.20527 -16841.6889   7780.6315
 ttme                 -1109.978447       -313.751079     -159.840260 -16841.68892 -91446.9712 -43448.0365
 hinc_air              -993.264146        284.426623      144.526736   7780.63148 -43448.0365 -48054.1196
-"""
+""")
 
-print u"""
+print(u"""
 Example 2
 
     *four alternative-specific constants (αair, αtrain, αbus, αcar)
@@ -147,7 +147,7 @@ Example 2
     *one alternative specific variables (invc)
         with a generic coefficient (βinvc)
 
-"""
+""")
 # Names of the variables for the utility function for each alternative
 # variables with common coefficients have to be first in each array
 V = {
@@ -168,13 +168,13 @@ clogit_mod = CLogit(y, X,  V, ncommon,
 clogit_res = clogit_mod.fit()
 
 end_time = time.time()
-print("the whole elapsed time was %g seconds."
-% (end_time - start_time))
+print(("the whole elapsed time was %g seconds."
+% (end_time - start_time)))
 
 # Results
-print clogit_mod.exog_matrix.columns.tolist()
-print clogit_res.params
-print CLogitResults(clogit_mod).summary()
+print(clogit_mod.exog_matrix.columns.tolist())
+print(clogit_res.params)
+print(CLogitResults(clogit_mod).summary())
 
 #hessian2 = clogit_mod2.hessian(clogit_res2.params)
 #print hessian2
@@ -208,7 +208,7 @@ Likelihood ratio test : chisq = 6.4418 (p.value = 0.011147)
 """
 
 
-print u"""
+print("""
 R results
 
     model2$coefficient
@@ -221,12 +221,12 @@ air:(intercept)        -41.485888         17.171385        8.218602   -2022.6771
 train:(intercept)       17.171385        -43.402569        8.885814     -81.87671
 bus:(intercept)          8.218602          8.885814      -25.618418     455.92294
 invc                 -2022.677132        -81.876710      455.922944 -157872.76175
-"""
-print u"""
+""")
+print(u"""
 Example 3
     *one alternative specific variables (gc)
         with a generic coefficient (βG)
-"""
+""")
 
 # Names of the variables for the utility function for each alternative
 # variables with common coefficients have to be first in each array
@@ -248,18 +248,18 @@ clogit_mod = CLogit(y, X,  V, ncommon,
 clogit_res = clogit_mod.fit()
 
 end_time = time.time()
-print("the whole elapsed time was %g seconds."
-% (end_time - start_time))
+print(("the whole elapsed time was %g seconds."
+% (end_time - start_time)))
 
 # Results
-print clogit_mod.exog_matrix.columns.tolist()
-print clogit_res.params
-print CLogitResults(clogit_mod).summary()
+print(clogit_mod.exog_matrix.columns.tolist())
+print(clogit_res.params)
+print(CLogitResults(clogit_mod).summary())
 
 #hessian = clogit_mod.hessian(clogit_res.params)
 #print hessian
 
-print u"""
+print(u"""
 Example 4
     *four alternative-specific constants (αair, αtrain, αbus, αcar)
         αcar dropped for identification
@@ -271,7 +271,7 @@ Example 4
 
 Ui j = αair + αtrain + αbus + βG*gcij
         + γHair*hinci + γHtrain*hinci+ γHtbus*hinci+ εij
-"""
+""")
 
 # Names of the variables for the utility function for each alternative
 # variables with common coefficients have to be first in each array
@@ -293,24 +293,24 @@ clogit_mod = CLogit(y, X,  V, ncommon,
 clogit_res = clogit_mod.fit()
 
 end_time = time.time()
-print("the whole elapsed time was %g seconds."
-% (end_time - start_time))
+print(("the whole elapsed time was %g seconds."
+% (end_time - start_time)))
 
 # Results
-print clogit_mod.exog_matrix.columns.tolist()
-print clogit_res.params
-print CLogitResults(clogit_mod).summary()
+print(clogit_mod.exog_matrix.columns.tolist())
+print(clogit_res.params)
+print(CLogitResults(clogit_mod).summary())
 
 #hessian = clogit_mod.hessian(clogit_res.params)
 #print hessian
 
-print u"""
+print(u"""
 Example 5
     *one individual specific variables (hinc)
         with an alternative specific coefficient (γHair,γHtrain,γHbus,γHcar)
         γHcar dropped for identification
 
-"""
+""")
 # Names of the variables for the utility function for each alternative
 # variables with common coefficients have to be first in each array
 V = {
@@ -331,13 +331,13 @@ clogit_mod = CLogit(y, X,  V, ncommon,
 clogit_res =  clogit_mod.fit()
 
 end_time = time.time()
-print("the whole elapsed time was %g seconds."
-% (end_time - start_time))
+print(("the whole elapsed time was %g seconds."
+% (end_time - start_time)))
 
 # Results
-print clogit_mod.exog_matrix.columns.tolist()
-print clogit_res.params
-print CLogitResults(clogit_mod).summary()
+print(clogit_mod.exog_matrix.columns.tolist())
+print(clogit_res.params)
+print(CLogitResults(clogit_mod).summary())
 
 #hessian = clogit_mod.hessian(clogit_res.params)
 #print hessian
