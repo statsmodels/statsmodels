408957945c942f65ce028168587dff77ec4ed459
Skipper Seabold <jsseabold@gmail.com>
2014-04-30 15:32:39 -0400

    ENH: Add classification performance measures. Closes #1577

--- statsmodels/tools/eval_measures.py
+++ statsmodels/tools/eval_measures.py
@@ -570,7 +570,213 @@ def hqic_sigma(sigma2, nobs, df_modelwc, islog=False):
 #def fpe_sigma():
 #    ((nobs + self.df_model) / self.df_resid) ** neqs * np.exp(ld)
 
+# Evaluation measures for classification models
+
+def _pred_table_to_cells(pred_table, true):
+    """
+    Returns true positive, false positive, false negative, true negative
+    """
+    pred_table = np.asarray(pred_table)
+
+    if true == 0:
+        tp, fp, fn, tn = pred_table.flatten()
+    elif true == 1:
+        tn, fn, fp, tp = pred_table.flatten()
+    else:  # pragma: no cover
+        raise ValueError("Only true == [0, 1] supported.")
+
+    return tp, fp, fn, tn
+
+
+def precision(pred_table, true=1):
+    """
+    Computes classification precision given prediction table.
+
+    Binary classification only.
+
+    Parameters
+    ----------
+    pred_table : array-like
+        2 x 2 prediction table (aka confusion matrix). The actual class is
+        assumed to be the rows. The predicted class is in the columns.
+        The "True" category is in the column given by ``true''
+    true : int {0, 1}
+        The column which contains the "True" category.
+
+    Returns
+    -------
+    precision : float
+        The precision of the classification
+
+    Notes
+    -----
+    Precision is analagous to (absence of) Type I errors. The probability
+    that a randomly selected document is classified correctly. I.e., there are
+    no false negatives. Sometimes called positive predictive value.
+    """
+    tp, fp, fn, tn = _pred_table_to_cells(pred_table, true)
+
+    try:
+        return tp / (tp + fp)
+    except ZeroDivisionError:
+        return np.nan
+
+
+def recall(pred_table, true=1):
+    """
+    Computes the classification recall (or sensitivity) of prediction table.
+
+    Binary classification only.
+
+    Parameters
+    ----------
+    pred_table : array-like
+        2 x 2 prediction table (aka confusion matrix). The actual class is
+        assumed to be the rows. The predicted class is in the columns.
+        The "True" category is in the column given by ``true''
+    true : int {0, 1}
+        The column which contains the "True" category.
+
+    Returns
+    -------
+    recall : float
+        The recall of the classification
+
+    Notes
+    -----
+
+    Computes
+
+    .. math::
+
+       tp / (tp + fn)
+
+    Analagous to (absence of) Type II errors. Out of all the ones that are
+    true, how many did you predict as true. I.e., with a recall of 1 there
+    are no false positives. Also called the true positive rate.
+    """
+    tp, fp, fn, tn = _pred_table_to_cells(pred_table, true)
+
+    try:
+        return tp / (tp + fn)
+    except ZeroDivisionError:
+        return np.nan
+
+sensitivity = recall
+
+
+def specificity(pred_table, true=1):
+    """
+    Computes the classification specificity of prediction table.
+
+    Binary classification only.
+
+    Parameters
+    ----------
+    pred_table : array-like
+        2 x 2 prediction table (aka confusion matrix). The actual class is
+        assumed to be the rows. The predicted class is in the columns.
+        The "True" category is in the column given by ``true''
+    true : int {0, 1}
+        The column which contains the "True" category.
+
+    Returns
+    -------
+    specificity : float
+        The specificity of the classification
+
+    Notes
+    -----
+
+    Computes
+
+    .. math::
+
+       tn / (tn + fp)
+
+    Analagous to (absence of) type I errors. Out of all the ones that are
+    negative, how many did you predict as negative. I.e., with a specificty
+    of 1 there are no false positives. Sometimes called the true negative
+    rate.
+    """
+    if true == 1:
+        true = 0
+    elif true == 0:
+        true = 1
+    return recall(pred_table, true)
+
+
+def accuracy(pred_table, true=1):
+    """
+    Compuates the classification accuracy of a prediction table.
+
+    Binary classification only.
+
+    Parameters
+    ----------
+    pred_table : array-like
+        2 x 2 prediction table (aka confusion matrix). The actual class is
+        assumed to be the rows. The predicted class is in the columns.
+        The "True" category is in the column given by ``true''
+    true : int {0, 1}
+        The column which contains the "True" category.
+
+    Returns
+    -------
+    accuracy : float
+        The accuracy of the classification
+
+    Notes
+    -----
+
+    Computes
+
+    .. math::
+
+       (tp + tn) / (tp + tn + fp + fn)
+
+    Sometimes called Rand Accuracy.
+    """
+    tp, fp, fn, tn = _pred_table_to_cells(pred_table, true)
+    if tp == tn == fp == fn == 0:  # pragma: no cover
+        raise ValueError("Prediction table cannot be all zero")
+    return (tp + tn) / (tp + tn + fp + fn)
+
+
+def fscore_measure(pred_table, b=1, true=1):
+    """
+    Return the F-b measure for a prediction table
+
+    Binary classification only.
+
+    Parameters
+    ----------
+    pred_table : array-like
+        2 x 2 prediction table (aka confusion matrix). The actual class is
+        assumed to be the rows. The predicted class is in the columns.
+        The "True" category is in the column given by ``true''
+    b : float
+        b should be positive. It indicates the how much more importance is
+        attached to recall than precision. The default is 1, giving the
+        traditional F-measure. For example, if a user attached half as much
+        importance to recall as precision, b = .5. If twice as much, b = 2.
+    true : int {0, 1}
+        The column which contains the "True" category.
+
+    Notes
+    -----
+    Computes
+
+    .. math::
+
+       (1 + b^2)*(precision * recall)/(b^2 * precision + recall)
+    """
+    r = recall(pred_table, true)
+    p = precision(pred_table, true)
+    return (1 + b**2) * r*p/(b**2 * p + r)
+
 
 __all__ = [maxabs, meanabs, medianabs, medianbias, mse, rmse, stde, vare,
            aic, aic_sigma, aicc, aicc_sigma, bias, bic, bic_sigma,
-           hqic, hqic_sigma, iqr]
+           hqic, hqic_sigma, iqr, precision, recall, accuracy, fscore_measure,
+           specificity, sensitivity]
