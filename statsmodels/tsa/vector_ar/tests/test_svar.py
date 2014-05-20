794c1c54d7dc37adcb9fc64e8e7cd8430728f69b
Josef Perktold <josef.pktd@gmail.com>
2014-05-19 23:20:55 -0400

    TST: SVAR add tests for params and bse

--- statsmodels/tsa/vector_ar/tests/test_svar.py
+++ statsmodels/tsa/vector_ar/tests/test_svar.py
@@ -4,7 +4,7 @@ Test SVAR estimation
 
 import statsmodels.api as sm
 from statsmodels.tsa.vector_ar.svar_model import SVAR
-from numpy.testing import assert_almost_equal, assert_equal
+from numpy.testing import assert_almost_equal, assert_equal, assert_allclose
 from .results import results_svar
 import numpy as np
 import numpy.testing as npt
@@ -25,8 +25,26 @@ class TestSVAR(object):
         B = np.asarray([['E', 0, 0], [0, 'E', 0], [0, 0, 'E']])
         results = SVAR(data, svar_type='AB', A=A, B=B).fit(maxlags=3)
         cls.res1 = results
-        cls.res2 = results_svar.SVARdataResults()
+        #cls.res2 = results_svar.SVARdataResults()
+        from .results import results_svar_st
+        cls.res2 = results_svar_st.results_svar1_small
+
+
+    def _reformat(self, x):
+        return x[[1, 4, 7, 2, 5, 8, 3, 6, 9, 0], :].ravel("F")
+
+
     def test_A(self):
         assert_almost_equal(self.res1.A, self.res2.A, DECIMAL_4)
+
+
     def test_B(self):
         assert_almost_equal(self.res1.B, self.res2.B, DECIMAL_4)
+
+
+    def test_basic(self):
+        res1 = self.res1
+        res2 = self.res2
+        assert_allclose(self._reformat(res1.params), res2.b_var, atol=1e-12)
+        bse_st = np.sqrt(np.diag(res2.V_var))
+        assert_allclose(self._reformat(res1.bse), bse_st, atol=1e-12)
