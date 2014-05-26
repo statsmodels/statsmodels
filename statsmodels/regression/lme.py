Fixed xrange probem in profile; fixed complex number problem in Hessian add random seed

statsmodels/regression/lme.py
@@ -36,6 +36,10 @@ linear mixed effects models for repeated measures data".  Journal of
 the American Statistical Association. Volume 83, Issue 404, pages
 1014-1022.
 
+See also this more recent document:
+
+http://econ.ucsb.edu/~doug/245a/Papers/Mixed%20Effects%20Implement.pdf
+
 All the likelihood, gradient, and Hessian calculations closely follow
 Lindstrom and Bates.
 
@@ -304,9 +308,9 @@ class MixedLM(base.LikelihoodModel):
 
         # Set the random effect parameter names
         if isinstance(self.exog_re, pd.DataFrame):
-            self.exog_re_names = self.exog_re.columns
+            self.exog_re_names = list(self.exog_re.columns)
         else:
-            self.exog_re_names = ["RE%d" % (k+1) for k in
+            self.exog_re_names = ["Z%d" % (k+1) for k in
                                   range(self.exog_re.shape[1])]
 
     def set_random(self, re_formula, data):
@@ -510,14 +514,32 @@ class MixedLM(base.LikelihoodModel):
 
     def hessian(self, params):
         """
-        Hessian of log-likelihood evaluated at `params`.  Note that
-        this uses either `cov_re` or its square root (L) depending on
-        the value of `use_sqrt`.  `hessian_full` is an analytic
-        implementation of the Hessian with respect to cov_re.
-        """
-        from statsmodels.tools.numdiff import approx_hess_cs
-        return approx_hess_cs(params, self.loglike)
+        Hessian of log-likelihood evaluated at `params`, calculated
+        numerically.
 
+        Parameters
+        ----------
+        params : array-like
+            The model parameters, packed into a 1d array.
+
+        Returns
+        -------
+        The Hessian matrix of the log likelihood function, evaluated
+        at params.
+
+        Notes
+        -----
+        The Hessian is calculated with respect to either the lower
+        triangle of `cov_re` or its lower triangular square root (L)
+        depending on the value of `use_sqrt`.
+
+        `hessian_full` is an analytic Hessian calculation, always
+        calculated with respect to cov_re
+        """
+
+        from statsmodels.tools.numdiff import approx_hess_cs
+        hmat = approx_hess_cs(params, self.loglike)
+        return hmat.real
 
     def _unpack(self, params, sym=True):
         """
@@ -546,7 +568,7 @@ class MixedLM(base.LikelihoodModel):
         re_params = params[self.k_fe:]
 
         # Unpack the covariance matrix of the random effects
-        cov_re = np.zeros((self.k_re, self.k_re), dtype=np.float64)
+        cov_re = np.zeros((self.k_re, self.k_re), dtype=params.dtype)
         ix = np.tril_indices(self.k_re)
         cov_re[ix] = re_params
 
@@ -1374,6 +1396,8 @@ class MixedLM(base.LikelihoodModel):
                 try:
                     fit_args = dict(kwargs)
                     fit_args["retall"] = hist is not None
+                    if "disp" not in fit_args:
+                        fit_args["disp"] = False
                     # Only bfgs seems to work for some reason.
                     fit_args["method"] = "bfgs"
                     rslt = super(MixedLM, self).fit(start_params=params_prof, **fit_args)
@@ -1659,10 +1683,10 @@ class MixedLMResults(base.LikelihoodModelResults):
         for i in range(self.k_re):
             for j in range(i + 1):
                 if i == j:
-                    names.append(self.model.exog_re_names[i])
+                    names.append(self.model.exog_re_names[i] + " RE")
                 else:
                     names.append(self.model.exog_re_names[j] + " x " +
-                                 self.model.exog_re_names[i])
+                                 self.model.exog_re_names[i] + " RE")
                 sdf[jj, 0] = self.cov_re[i, j]
                 sdf[jj, 1] = np.sqrt(self.sig2) * self.bse[jj]
                 jj += 1
@@ -1717,7 +1741,7 @@ class MixedLMResults(base.LikelihoodModelResults):
         # Need to permute the variables so that the profiled variable
         # is first.
         exog_re_li_save = [x.copy() for x in model.exog_re_li]
-        ix = range(pr)
+        ix = list(range(pr))
         ix[0] = re_ix
         ix[re_ix] = 0
         for k in range(len(model.exog_re_li)):