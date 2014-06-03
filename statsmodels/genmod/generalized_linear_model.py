5fdb88ab834c5813738481ddb61063fa11c4abdc
Kerby Shedden <kshedden@umich.edu>
2014-06-03 04:48:03 -0400

    Added missing offset to starting value calculation

--- statsmodels/genmod/generalized_linear_model.py
+++ statsmodels/genmod/generalized_linear_model.py
@@ -434,7 +434,7 @@ class GLM(base.LikelihoodModel):
         if start_params is None:
             mu = self.family.starting_mu(self.endog)
         else:
-            mu = self.family.fitted(np.dot(wlsexog, start_params))
+            mu = self.family.fitted(np.dot(wlsexog, start_params) + offset)
         eta = self.family.predict(mu)
         dev = self.family.deviance(self.endog, mu)
         if np.isnan(dev):
