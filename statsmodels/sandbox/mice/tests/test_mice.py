69e4743e8cef87a5ced70c92a99d57acadedaa46
Frank Cheng <frcheng@umich.edu>
2014-06-17 00:32:10 -0400

    short update
    work on test_new_imputer

--- statsmodels/sandbox/mice/tests/test_mice.py
+++ statsmodels/sandbox/mice/tests/test_mice.py
@@ -1,18 +1,20 @@
 import numpy as np
 import pandas as pd
 from statsmodels.sandbox.mice import mice
+import statsmodels.api as sm
 
 class TestMice(object):
+    def __init__(self):
+        self.formula = "X2~X3+X4"
+        np.random.seed(1325)
 
     def test_get_data_from_formula(self):
-        formula = "X2~X3+X4"
-        np.random.seed(1325)
         data = np.random.normal(size=(10,4))
         data[8:, 1] = np.nan
         df = pd.DataFrame(data, columns=["X1", "X2", "X3", "X4"])
         imp_dat = mice.ImputedData(df)
-        endog_obs, exog_obs, exog_miss = imp_dat.get_data_from_formula(formula)
-        endog_obs, exog_obs, exog_miss = imp_dat.get_data_from_formula(formula)
+        endog_obs, exog_obs, exog_miss = imp_dat.get_data_from_formula(self.formula)
+        endog_obs, exog_obs, exog_miss = imp_dat.get_data_from_formula(self.formula)
         endog_obs = np.asarray(endog_obs).flatten()
         exog_obs = np.asarray(exog_obs)[:,1:]
         exog_miss = np.asarray(exog_miss)[:,1:]
@@ -23,11 +25,10 @@ class TestMice(object):
         np.testing.assert_almost_equal(exog_miss, test_exog_miss)
         np.testing.assert_almost_equal(endog_obs, test_endog_obs)
 
-    def test_get_data_from_formula(self):
-
-    def test_get_data_from_formula(self):
-
-    def test_get_data_from_formula(self):
+    def test_new_imputer(self):
+        md = sm.OLS.from_formula(self.formula, df)
+        mdf = md.fit()
+        mdf.params
 
 if  __name__=="__main__":
 
