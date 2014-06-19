4a000a5010370036a3b3798ab2066d521e84aa2a
Kerby Shedden <kshedden@umich.edu>
2014-06-19 10:25:48 -0400

    Use seed when generating data for tests

--- statsmodels/sandbox/tests/phreg_gentests.py
+++ statsmodels/sandbox/tests/phreg_gentests.py
@@ -8,6 +8,9 @@ After updating the test data sets, use R to run the survival.R script
 to update the R results.
 """
 
+# The current data may not reflect this seed
+np.random.seed(5234)
+
 # Loop over pairs containing (sample size, number of variables).
 for (n,p) in (20,1), (50,1), (50,2), (100,5), (1000,10):
 
@@ -17,28 +20,30 @@ for (n,p) in (20,1), (50,1), (50,2), (100,5), (1000,10):
     expected_survival_time = np.exp(-lpred)
 
     # Survival times are exponential
-    survival_time = -expected_survival_time*\
-        np.log(np.random.uniform(size=5*n))
+    survival_time = -np.log(np.random.uniform(size=5*n))
+    survival_time *= expected_survival_time
 
     # Set this to get a reasonable amount of censoring
     expected_censoring_time = np.mean(expected_survival_time)
 
     # Theses are the observation times.
-    censoring_time = -expected_censoring_time*\
-        np.log(np.random.uniform(size=5*n))
+    censoring_time = -np.log(np.random.uniform(size=5*n))
+    censoring_time *= expected_censoring_time
 
     # Entry times
-    entry_time = -0.5*expected_censoring_time*\
-        np.log(np.random.uniform(size=5*n))
+    entry_time = -np.log(np.random.uniform(size=5*n))
+    entry_time *= 0.5*expected_censoring_time
 
     # 1=failure (death), 0=no failure (no death)
     status = 1*(survival_time <= censoring_time)
 
     # The censoring time of the failure time, whichever comes first
     time = np.where(status==1, survival_time, censoring_time)
+
+    # Round time so that we have ties
     time = np.around(time, decimals=1)
 
-    # Only take cases where the entry time is before the failulre or
+    # Only take cases where the entry time is before the failure or
     # censoring time.  Take exactly n such cases.
     ii = np.flatnonzero(entry_time < time)
     ii = ii[np.random.permutation(len(ii))[0:n]]
