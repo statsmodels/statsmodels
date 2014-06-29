d1429824cf4d2e9eff5430d25bdc9cf96789c797
Josef Perktold <josef.pktd@gmail.com>
2014-06-28 21:20:04 -0400

    REF: remove try ...except (no other changes)

--- statsmodels/base/data.py
+++ statsmodels/base/data.py
@@ -86,58 +86,54 @@ class ModelData(object):
             self.const_idx = None
             self.k_constant = 0
         else:
-            try:  # detect where the constant is
-                check_implicit = False
-                const_idx = np.where(self.exog.ptp(axis=0) == 0)[0].squeeze()
-                self.k_constant = const_idx.size
+            # detect where the constant is
+            check_implicit = False
+            const_idx = np.where(self.exog.ptp(axis=0) == 0)[0].squeeze()
+            self.k_constant = const_idx.size
 
-                if self.k_constant == 1:
-                    if self.exog[:, const_idx].mean() != 0:
-                        self.const_idx = const_idx
-                    else:
-                        # we only have a zero column and no other constant
-                        check_implicit = True
-                elif self.k_constant > 1:
-                    # we have more than one constant column
-                    # look for ones
-                    values = []  # keep values if we need != 0
-                    for idx in const_idx:
-                        value = self.exog[:, idx].mean()
-                        if value == 1:
-                            self.k_constant = 1
-                            self.const_idx = const_idx[idx]
-                            break
-                        values.append(value)
-                    else:
-                        # we didn't break, no column of ones
-                        pos = (np.array(values) != 0)
-                        if pos.any():
-                            # take the first nonzero column
-                            self.k_constant = 1
-                            self.const_idx = const_idx[pos.argmax()]
-                        else:
-                            # only zero columns
-                            check_implicit = True
-                elif self.k_constant == 0:
-                    check_implicit = True
+            if self.k_constant == 1:
+                if self.exog[:, const_idx].mean() != 0:
+                    self.const_idx = const_idx
                 else:
-                    # shouldn't be here
-                    pass
+                    # we only have a zero column and no other constant
+                    check_implicit = True
+            elif self.k_constant > 1:
+                # we have more than one constant column
+                # look for ones
+                values = []  # keep values if we need != 0
+                for idx in const_idx:
+                    value = self.exog[:, idx].mean()
+                    if value == 1:
+                        self.k_constant = 1
+                        self.const_idx = const_idx[idx]
+                        break
+                    values.append(value)
+                else:
+                    # we didn't break, no column of ones
+                    pos = (np.array(values) != 0)
+                    if pos.any():
+                        # take the first nonzero column
+                        self.k_constant = 1
+                        self.const_idx = const_idx[pos.argmax()]
+                    else:
+                        # only zero columns
+                        check_implicit = True
+            elif self.k_constant == 0:
+                check_implicit = True
+            else:
+                # shouldn't be here
+                pass
 
-                if check_implicit:
-                    # look for implicit constant
-                    # Compute rank of augmented matrix
-                    augmented_exog = np.column_stack(
-                                (np.ones(self.exog.shape[0]), self.exog))
-                    rank_augm = np_matrix_rank(augmented_exog)
-                    rank_orig = np_matrix_rank(self.exog)
-                    self.k_constant = int(rank_orig == rank_augm)
-                    self.const_idx = None
-
-            except:  # should be an index error but who knows, means no const
-                raise
+            if check_implicit:
+                # look for implicit constant
+                # Compute rank of augmented matrix
+                augmented_exog = np.column_stack(
+                            (np.ones(self.exog.shape[0]), self.exog))
+                rank_augm = np_matrix_rank(augmented_exog)
+                rank_orig = np_matrix_rank(self.exog)
+                self.k_constant = int(rank_orig == rank_augm)
                 self.const_idx = None
-                self.k_constant = 0
+
 
     @classmethod
     def _drop_nans(cls, x, nan_mask):
