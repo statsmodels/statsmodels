pmm, normalized covariance bugfix

statsmodels/discrete/discrete_model.py
@@ -729,7 +729,7 @@ class CountModel(DiscreteModel):
         else:
             return np.dot(exog, params[:exog.shape[1]]) + exposure + offset
 
-    def get_distribution(self, params, exog=None, scale=None):
+    def get_distribution(self, params, exog=None, model_class=None, scale=None):
         """
         Return a scipy.stats.distributions object corresponding to the `endog`
         of this model.

statsmodels/sandbox/mice/mice.py
@@ -43,6 +43,8 @@ import random
 import statsmodels
 from statsmodels.tools.decorators import cache_readonly
 from scipy import stats
+import copy
+import sys
 
 class ImputedData(object):
     __doc__= """
@@ -73,7 +75,7 @@ class ImputedData(object):
             self.columns[c] = MissingDataInfo(self.data[c])
         self.data = self.data.fillna(self.data.mean())
 
-    def new_imputer(self, endog_name, method="gaussian", formula=None, model_class=None,
+    def new_imputer(self, endog_name, method="gaussian", k_pmm=1, formula=None, model_class=None,
                     init_args={}, fit_args={}, scale_method="fix",
                     scale_value=None):
         """
@@ -106,7 +108,7 @@ class ImputedData(object):
         if formula is None:
             formula = endog_name + " ~ " + " + ".join(
                             [x for x in self.data.columns if x != endog_name])
-        return Imputer(formula, model_class, self, method=method, init_args=init_args,
+        return Imputer(formula, model_class, self, method=method, k_pmm=1, init_args=init_args,
                        fit_args=fit_args, scale_method=scale_method,
                        scale_value=scale_value)
 
@@ -246,7 +248,7 @@ class Imputer(object):
             raise NotImplementedError
         p = len(params)
         params += np.dot(covmat_sqrt,
-                         np.random.normal(0, mdf.scale * scale_per, p))
+                         np.random.normal(0, np.sqrt(mdf.scale * scale_per), p))
         return params, scale_per
 
     def impute_asymptotic_bayes(self):
@@ -268,7 +270,7 @@ class Imputer(object):
         new_endog = new_rv.rvs(size=len(exog_miss))
         self.data.store_changes(self.endog_name, new_endog)
 
-    def impute_pmm(self, pmm_neighbors=1.):
+    def impute_pmm(self, pmm_neighbors=1):
         """
         Use predictive mean matching to simulate data.
 
@@ -292,25 +294,67 @@ class Imputer(object):
         pendog_obs = md.predict(params, exog_obs)
         pendog_miss = md.predict(params, exog_miss)
         
-        imputed_miss = []
-        for val in pendog_miss:
-            dist = abs(pendog_obs - val)
-            dist_ind = np.argsort(dist, axis=0)
-            imputed_miss.append(random.choice(np.array(
-            endog_obs)[dist_ind[0:pmm_neighbors][0]]))
+#        imputed_miss = []
+#        for val in pendog_miss:
+#            dist = abs(pendog_obs - val)
+#            dist_ind = np.argsort(dist, axis=0)
+#            imputed_miss.append(random.choice(np.array(
+#            endog_obs)[dist_ind[0:pmm_neighbors][0]]))
         
-#        ii = np.argsort(pendog_obs, axis=0)
-#        pendog_obs = pendog_obs[ii]
-#        oendog = endog_obs.iloc[ii,:]
-#        # Get indices of predicted endogs of nonmissing observations that are
-#        # close to those of missing observations
-#        ix = np.searchsorted(pendog_obs, pendog_miss)
-#        # Select a random draw of observed endogenous variable from a set
-#        # window around the closest predicted value
-#        ix += np.random.randint(-pmm_neighbors / 2, pmm_neighbors / 2, size=len(ix))
+        ii = np.argsort(pendog_obs, axis=0)
+        pendog_obs = pendog_obs[ii]
+        oendog = endog_obs.iloc[ii,:]
+        # Get indices of predicted endogs of nonmissing observations that are
+        # close to those of missing observations
+        ix = np.searchsorted(pendog_obs, pendog_miss)
+        np.clip(ix, 0, len(pendog_obs) - 1, out=ix)
+        ix_list = []
+        for i in range(len(pendog_miss)):
+            k = 0
+            count_low = 0
+            count_high = 0
+            ix_list.append([])
+            upper = pendog_obs[ix[i]]
+            lower = pendog_obs[ix[i] - 1]
+            target = pendog_miss[i]
+            ixs = ix_list[len(ix_list) - 1]
+            limit_low = False
+            limit_high = False
+            while k < pmm_neighbors:
+                if limit_low:
+                    count_high += 1
+                    ixs.append(ix[i] - 1 + count_high)
+                elif limit_high:
+                    count_low += 1
+                    ixs.append(ix[i] - count_low)
+                elif abs(target - upper) >= abs(target - lower):
+                    count_low += 1
+                    if ix[i] - 1 - count_low >= 0:
+                        ixs.append(ix[i] - count_low)
+                        target = copy.copy(lower)
+                        lower = pendog_obs[ix[i] - 1 - count_low]
+                    else:
+                        ixs.append(ix[i] - 1 + count_high)
+                        limit_low = True
+                elif abs(target - upper) < abs(target - lower):
+                    count_high += 1
+                    if ix[i] + count_high < len(pendog_obs):
+                        ixs.append(ix[i] - 1 + count_high)
+                        target = copy.copy(upper)
+                        upper = pendog_obs[ix[i] + count_high]
+                    else:
+                        ixs.append(ix[i] - count_low)
+                        limit_high = True
+                k += 1                    
+            ixs = np.clip(ixs, 0, len(oendog))
+            ixs = random.choice(ixs)
+        # Select a random draw of observed endogenous variable from a set
+        # window around the closest predicted value
+#        ix += np.random.randint(int(-pmm_neighbors / 2.), int(pmm_neighbors / 2.) + 1, size=len(ix))
 #        np.clip(ix, 0, len(oendog), out=ix)
 #        imputed_miss = np.array(oendog.iloc[ix,:])
-        
+        ix_list = np.squeeze(ix_list)
+        imputed_miss = np.array(oendog.iloc[ix_list,:])
         self.data.store_changes(self.endog_name, imputed_miss)
 
     def impute_bootstrap(self):
@@ -554,8 +598,8 @@ class MICE(object):
         within_g = np.mean(cov_list, axis=0)
         # Used MLE rather than method of moments between group covariance
         between_g = np.cov(np.array(params_list).T, bias=1)
-        cov_params = within_g + (1 + 1. / float(self.num_ds)) * between_g
-        rslt = MICEResults(self, params, cov_params)
+        cov_params = within_g * scale + (1 + 1. / float(self.num_ds)) * between_g
+        rslt = MICEResults(self, params, cov_params / scale)
         rslt.scale = scale
         return rslt
 

statsmodels/sandbox/mice/tests/test_mice.py
@@ -76,7 +76,7 @@ class TestMice(object):
         imputer = mice.Imputer(self.formula, sm.OLS, mice.ImputedData(df))
         imputer.impute_asymptotic_bayes()
         np.testing.assert_almost_equal(np.asarray(imputer.data.data['X2'][8:]),
-                                       np.asarray([-0.82292821, -0.22632992]))
+                                       np.asarray([-0.83679515, -0.22187195]))
 
     def test_impute_pmm(self):
         np.random.seed(1325)
@@ -94,18 +94,18 @@ class TestMice(object):
         data[8:, 1] = np.nan
         df = pd.DataFrame(data, columns=["X1", "X2", "X3", "X4"])
         impdata = mice.ImputedData(df)
-        m = impdata.new_imputer("X2", scale_method="perturb_chi2")
+        m = impdata.new_imputer("X2", scale_method="perturb_chi2", method="pmm")
         impcomb = mice.MICE("X2 ~ X1 + X3", sm.OLS, [m])
-        impcomb.run(method="pmm")
+        impcomb.run()
         p1 = impcomb.combine()
-        np.testing.assert_almost_equal(p1.params, np.asarray([0.30651575,
-                                                              0.2264856 ,
-                                                              0.03370901]))
-        np.testing.assert_almost_equal(p1.scale, 0.64051079487931539)
+        np.testing.assert_almost_equal(p1.params, np.asarray([0.31562745, 
+                                                              0.19100593,
+                                                              0.0133906 ]))
+        np.testing.assert_almost_equal(p1.scale, 0.61329459165397548)
         np.testing.assert_almost_equal(p1.cov_params(), np.asarray([
-        [ 0.08157686,  0.03109555, -0.0017935 ],
-        [ 0.03109555,  0.06462905, -0.00655131],
-        [-0.0017935 , -0.00655131,  0.06580208]]))
+        [  7.88964139e-02,   2.70237318e-02,  -3.64367780e-03],
+        [  2.70237318e-02,   7.23763179e-02,  -1.76877602e-05],
+        [ -3.64367780e-03,  -1.76877602e-05,   6.80019669e-02]]))
 
     def test_overall(self):
         """
@@ -151,11 +151,11 @@ class TestMice(object):
         r_pooled_se = np.sqrt(np.asarray(np.mean(cov) + (1 + 1 / 5.) * np.var(params)))
         r_pooled_params = np.asarray(np.mean(params))
         impdata = mice.ImputedData(data)
-        m1 = impdata.new_imputer("x2")
-        m2 = impdata.new_imputer("x3")
-        m3 = impdata.new_imputer("x1", model_class=sm.Logit)
-        impcomb = mice.MICE("x1 ~ x2 + x3", sm.Logit,[m1,m2,m3])
-        impcomb.run(method="pmm")
+        m1 = impdata.new_imputer("x2", method="pmm")
+        m2 = impdata.new_imputer("x3", method="pmm")
+        m3 = impdata.new_imputer("x1", model_class=sm.Logit, method="pmm")
+        impcomb = mice.MICE("x1 ~ x2 + x3", sm.Logit, [m1,m2,m3])
+        impcomb.run()
         p1 = impcomb.combine()
         np.testing.assert_allclose(p1.params, r_pooled_params, rtol=0.5)
         np.testing.assert_allclose(np.sqrt(np.diag(p1.cov_params())), r_pooled_se, rtol=1)