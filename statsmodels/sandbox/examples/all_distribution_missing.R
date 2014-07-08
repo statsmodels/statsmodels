6e0df099eb226e194a4ddc5a51406d1ab5b99baa
Frank Cheng <frcheng@umich.edu>
2014-07-08 01:01:40 -0400

    comments on R script

--- statsmodels/sandbox/examples/all_distribution_missing.R
+++ statsmodels/sandbox/examples/all_distribution_missing.R
@@ -1,27 +1,29 @@
+require(mvtnorm)  
+set.seed(100)
   setwd("C:/Users/Frank/Documents/GitHub/statsmodels/statsmodels/sandbox/examples")
   size = 1000
   cor = 0.9
   mu = rep(0,3)
-  sig = matrix(cor, nrow=3, ncol=3) + diag(3) * (1-cor)
+  sig = matrix(cor, nrow=3, ncol=3) + diag(3) * (1-cor) #Generate covariance matrix for normal
   draws = rmvnorm(n=size, mean=mu, sigma=sig)
-  unidraws = pnorm(draws)
-  normdraws = draws[,1]
-  berndraws = qbinom(1-unidraws[,2], 1, 0.75)
-  poisdraws = qpois(unidraws[,3], 5)
-  y<--1+1*berndraws-1*normdraws+1*poisdraws+rnorm(size,0,1) 
+  unidraws = pnorm(draws) #Apply probability integral transform to get uniform draws
+  normdraws = draws[,1] #Draw from original normal
+  berndraws = qbinom(1-unidraws[,2], 1, 0.75) #Inverse Bernoulli cdf using uniform draws
+  poisdraws = qpois(unidraws[,3], 5) #Inverse Poisson cdf using uniform draws
+  y<--1+1*berndraws-1*normdraws+1*poisdraws+rnorm(size,0,1) #Simulate linear regression to get conditional behavior in missingness rate
   
-  alpha.1<-exp(-16+2*y-normdraws)/(1+exp(-16+2*y-normdraws));
-  alpha.2<-exp(-3.5+.7*y)/(1+exp(-3.5+.7*y));
-  alpha.3<-exp(-6+1.2*y-berndraws)/(1+exp(-6+1.2*y-berndraws));
+  alpha.1<-exp(-16+2*y-normdraws)/(1+exp(-16+2*y-normdraws))
+  alpha.2<-exp(-3.5+.7*y)/(1+exp(-3.5+.7*y))
+  alpha.3<-exp(-6+1.2*y-berndraws)/(1+exp(-6+1.2*y-berndraws))
   
   
-  r.berndraws.mar<-rbinom(N,1,prob=alpha.1)
-  r.normdraws.mar<-rbinom(N,1,prob=alpha.2)
-  r.poisdraws.mar<-rbinom(N,1,prob=alpha.3)
+  r.berndraws.mar<-rbinom(size,1,prob=alpha.1)
+  r.normdraws.mar<-rbinom(size,1,prob=alpha.2)
+  r.poisdraws.mar<-rbinom(size,1,prob=alpha.3)
   berndraws.mar<-berndraws*(1-r.berndraws.mar)+r.berndraws.mar*99999  #berndraws.mar=berndraws if not missing, 99999 if missing
   normdraws.mar<-normdraws*(1-r.normdraws.mar)+r.normdraws.mar*99999
   poisdraws.mar<-poisdraws*(1-r.poisdraws.mar)+r.poisdraws.mar*99999
-  berndraws.mar[berndraws.mar==99999]=NA                  #change 99999 to NA (R's notation for missing)
+  berndraws.mar[berndraws.mar==99999]=NA #change 99999 to NA (R's notation for missing)
   normdraws.mar[normdraws.mar==99999]=NA
   poisdraws.mar[poisdraws.mar==99999]=NA
   
--- statsmodels/sandbox/examples/ex_mice_multiple.py
+++ statsmodels/sandbox/examples/ex_mice_multiple.py
@@ -2,16 +2,18 @@ import pandas as pd
 import statsmodels.api as sm
 from statsmodels.sandbox.mice import mice
 import os
+import numpy as np
 #
 #cur_dir = os.getcwd()
 #fn = os.path.join(cur_dir,"missingdata.csv")
+np.random.seed(1325)
 data = pd.read_csv("missingfull.csv")
 data.columns = ['x1','x2','x3']
 impdata = mice.ImputedData(data)
-m1 = impdata.new_imputer("x2", scale_method="perturb_chi2", method="pmm")
-m2 = impdata.new_imputer("x3", model_class=sm.Poisson, scale_method="perturb_chi2", method="pmm")
-m3 = impdata.new_imputer("x1", model_class=sm.Logit, scale_method="perturb_chi2", method="pmm")
-impcomb = mice.MICE("x1 ~ x2 + x3", sm.Logit,[m1,m2,m3])
-impcomb.run()
+m1 = impdata.new_imputer("x2", method="pmm")
+m2 = impdata.new_imputer("x3", model_class=sm.Poisson, method="pmm")
+m3 = impdata.new_imputer("x1", model_class=sm.Logit, method="pmm")
+impcomb = mice.MICE("x1 ~ x2 + x3", sm.Logit,[m3,m1,m2])
+impcomb.run(2, 5)
 p1 = impcomb.combine()
 print p1.summary()
