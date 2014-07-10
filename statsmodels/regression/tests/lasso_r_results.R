f43e2fb58e1e55b6f2627aa38acac439f52ad27e
Kerby Shedden <kshedden@umich.edu>
2014-07-10 18:11:58 -0400

    Forgot one file

--- statsmodels/regression/tests/lasso_r_results.R
+++ statsmodels/regression/tests/lasso_r_results.R
@@ -3,35 +3,38 @@ library(R2nparray)
 
 # Run the glmnet lasso (elastic net) on all the test data sets
 
-files = list.files(path="results", pattern="lasso_data_...csv")
+data = read.csv("results/lasso_data.csv", header=FALSE)
 
-rslt = list()
 ik = 0
+rslt = list()
 
-for (file in files) {
+for (n in c(100, 200, 300)) {
+    for (p in c(2, 3, 5)) {
 
-    data = read.csv(paste("results", file, sep="/"), header=FALSE)
+        endog = data[1:n, 1]
+        exog = data[1:n, 2:(p+1)]
+        exog = as.matrix(exog)
 
-    endog = data[,1]
-    exog = data[,2:dim(data)[2]]
-    exog = as.matrix(exog)
+        endog = (endog - mean(endog)) / sd(endog)
+        for (k in 1:p) {
+            exog[,k] = exog[,k] - mean(exog[,k])
+            exog[,k] = exog[,k] / sd(exog[,k])
+        }
 
-    for (alpha in c(0, 0.5, 1)) {
+        for (alpha in c(0, 0.5, 1)) {
 
-        fit = glmnet(exog, endog, intercept=FALSE, standardize=FALSE, alpha=alpha)
+            fit = glmnet(exog, endog, intercept=FALSE, standardize=FALSE, alpha=alpha)
+            ii = length(fit$lambda) * c(0.3, 0.5, 0.7)
+            ii = round(ii)
 
-        ii = length(fit$lambda) * c(0.3, 0.5, 0.7)
-        ii = round(ii)
-
-        for (q in c(0.3, 0.5, 0.7)) {
-            ii = round(q * length(fit$lambda))
-            coefs = coef(fit, s=fit$lambda[ii])
-            coefs = coefs[2:length(coefs)]
-            rname = sprintf("rslt_%d", ik)
-            ik = ik + 1
-            fix = substr(file, 12, 13)
-            fix = as.integer(fix)
-            rslt[[rname]] = c(fix, alpha, fit$lambda[ii], coefs)
+            for (q in c(0.3, 0.5, 0.7)) {
+                ii = round(q * length(fit$lambda))
+                coefs = coef(fit, s=fit$lambda[ii])
+                coefs = coefs[2:length(coefs)]
+                rname = sprintf("rslt_%d", ik)
+                ik = ik + 1
+                rslt[[rname]] = c(n, p, alpha, fit$lambda[ii], coefs)
+            }
         }
     }
 }
