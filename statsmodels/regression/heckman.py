a39eb2f1c59ecf395abeb83bc5322bf95de3d037
Bertram Ieong <bert9bert@users.noreply.github.com>
2014-08-25 22:08:07 -0500

    STY: renamed select_exog to exog_select, moved fitting routines into private methods, renamed fitted to results

--- statsmodels/regression/heckman.py
+++ statsmodels/regression/heckman.py
@@ -21,17 +21,17 @@ class Heckman(base.LikelihoodModel):
 
     Attributes
     ----------
-    select_exog : ndarray
+    exog_select : ndarray
         Data for the selection equation
     """
 
-    def __init__(self, endog, exog, select_exog):
+    def __init__(self, endog, exog, exog_select):
         #TODO: add code to take care of missing data in X and Z
         #TODO: add type checking and shape checking
         #TODO: make sure that selection equation contains at least one more unique var than in reg eqn
 
         # store data
-        self.select_exog = select_exog
+        self.exog_select = exog_select
         self.treated = np.asarray(~np.isnan(endog))
         super(Heckman, self).__init__(endog, exog)
 
@@ -55,17 +55,17 @@ class Heckman(base.LikelihoodModel):
                 self.xname = None
 
         try:
-            self.zname = [v for v in select_exog.columns]
+            self.zname = [v for v in exog_select.columns]
         except AttributeError:
             try:
-                self.zname = select_exog.name
+                self.zname = exog_select.name
             except AttributeError:
                 self.zname = None
 
     def initialize(self):
         self.wendog = self.endog
         self.wexog = self.exog
-        self.wselect_exog = self.select_exog
+        self.wexog_select = self.exog_select
 
     def whiten(self, data):
         """
@@ -100,76 +100,90 @@ class Heckman(base.LikelihoodModel):
         X = np.asarray(self.exog)
         X = X[self.treated,:]
 
-        Z = np.asarray(self.select_exog)
+        Z = np.asarray(self.exog_select)
 
         ## fit
-        if method=='2step':
-            ## Step 1
-            step1model = sm.Probit(self.treated, Z)
-            step1res = step1model.fit(disp=False)
-            step1_fitted = np.atleast_2d(step1res.fittedvalues).T
-            step1_varcov = step1res.cov_params()
-
-            inverse_mills = norm.pdf(step1_fitted)/norm.cdf(step1_fitted)
-
-            ## Step 2
-            W = np.hstack((X, inverse_mills[self.treated] ) )
-            step2model = sm.OLS(Y, W)
-            step2res = step2model.fit()
-
-            params = step2res.params[:-1]
-            betaHat_inverse_mills = step2res.params[-1]
-
-
-            ## Compute standard errors
-            # Compute estimated error variance of censored regression
-            delta = np.multiply(inverse_mills, inverse_mills + step1_fitted)[self.treated]
-
-            sigma2Hat = step2res.resid.dot(step2res.resid) / self.nobs_uncensored + \
-                (betaHat_inverse_mills**2 * sum(delta)) / self.nobs_uncensored
-            sigma2Hat = sigma2Hat[0]
-            sigmaHat = np.sqrt(sigma2Hat)
-            rhoHat = betaHat_inverse_mills / sigmaHat
-
-            # compute standard errors of beta estimates of censored regression
-            R = np.zeros([self.nobs_uncensored, self.nobs_uncensored])
-            for i in range(self.nobs_uncensored):
-                R[i,i] = 1 - rhoHat**2 * delta[i]
-
-            D = np.zeros([self.nobs_uncensored, self.nobs_uncensored])
-            for i in range(self.nobs_uncensored):
-                D[i,i] = delta[i]
-
-
-            Q = rhoHat**2 * (W.T.dot(D).dot(Z[self.treated])).dot(step1_varcov).dot(Z[self.treated].T.dot(D).dot(W))
-
-            normalized_varcov_all = np.linalg.inv(W.T.dot(W)).dot(W.T.dot(R).dot(W)+Q).dot(np.linalg.inv(W.T.dot(W)))
-            normalized_varcov = normalized_varcov_all[:-1,:-1]
-
-            varcov_all = sigma2Hat * normalized_varcov_all
-            varcov = varcov_all[:-1,:-1]
-
-            stderr_all = np.sqrt(np.diag(varcov_all))
-            stderr = stderr_all[:-1]
-            stderr_betaHat_inverse_mills = stderr_all[-1]
-
-
-            ## store results
-            fitted = HeckmanResults(self, params, normalized_varcov, sigma2Hat,
-                select_res=step1res,
-                param_inverse_mills=betaHat_inverse_mills, stderr_inverse_mills=stderr_betaHat_inverse_mills,
-                var_reg_error=sigma2Hat, corr_eqnerrors=rhoHat)
-
+        if method=='twostep':
+            results = self._fit_twostep(Y, X, Z)
         elif method=='mle':
-            raise ValueError("Invalid choice for estimation method. MLE estimation may be implemented at a later time.")
+            results = self._fit_mle(Y, X, Z)
         else:
             raise ValueError("Invalid choice for estimation method.")
 
 
         ## return fitted Heckman model
-        return fitted
+        return results
 
 
+    def _fit_twostep(self, Y, X, Z):
+        ########################################################################
+        # PRIVATE METHOD
+        # Fits using Heckman two-step from Heckman (1979).
+        ########################################################################
+
+        ## Step 1
+        step1model = sm.Probit(self.treated, Z)
+        step1res = step1model.fit(disp=False)
+        step1_fitted = np.atleast_2d(step1res.fittedvalues).T
+        step1_varcov = step1res.cov_params()
+
+        inverse_mills = norm.pdf(step1_fitted)/norm.cdf(step1_fitted)
+
+        ## Step 2
+        W = np.hstack((X, inverse_mills[self.treated] ) )
+        step2model = sm.OLS(Y, W)
+        step2res = step2model.fit()
+
+        params = step2res.params[:-1]
+        betaHat_inverse_mills = step2res.params[-1]
+
+
+        ## Compute standard errors
+        # Compute estimated error variance of censored regression
+        delta = np.multiply(inverse_mills, inverse_mills + step1_fitted)[self.treated]
+
+        sigma2Hat = step2res.resid.dot(step2res.resid) / self.nobs_uncensored + \
+            (betaHat_inverse_mills**2 * sum(delta)) / self.nobs_uncensored
+        sigma2Hat = sigma2Hat[0]
+        sigmaHat = np.sqrt(sigma2Hat)
+        rhoHat = betaHat_inverse_mills / sigmaHat
+
+        # compute standard errors of beta estimates of censored regression
+        R = np.zeros([self.nobs_uncensored, self.nobs_uncensored])
+        for i in range(self.nobs_uncensored):
+            R[i,i] = 1 - rhoHat**2 * delta[i]
+
+        D = np.zeros([self.nobs_uncensored, self.nobs_uncensored])
+        for i in range(self.nobs_uncensored):
+            D[i,i] = delta[i]
+
+
+        Q = rhoHat**2 * (W.T.dot(D).dot(Z[self.treated])).dot(step1_varcov).dot(Z[self.treated].T.dot(D).dot(W))
+
+        normalized_varcov_all = np.linalg.inv(W.T.dot(W)).dot(W.T.dot(R).dot(W)+Q).dot(np.linalg.inv(W.T.dot(W)))
+        normalized_varcov = normalized_varcov_all[:-1,:-1]
+
+        varcov_all = sigma2Hat * normalized_varcov_all
+        varcov = varcov_all[:-1,:-1]
+
+        stderr_all = np.sqrt(np.diag(varcov_all))
+        stderr = stderr_all[:-1]
+        stderr_betaHat_inverse_mills = stderr_all[-1]
+
+
+        ## store results
+        results = HeckmanResults(self, params, normalized_varcov, sigma2Hat,
+            select_res=step1res,
+            param_inverse_mills=betaHat_inverse_mills, stderr_inverse_mills=stderr_betaHat_inverse_mills,
+            var_reg_error=sigma2Hat, corr_eqnerrors=rhoHat)
+
+        return results
+
+
+    def _fit_mle(self, Y, X, Z):
+        raise ValueError("Invalid choice for estimation method."
+            " MLE estimation may be implemented at a later time.")
+        return None
 
 
 class HeckmanResults(base.LikelihoodModelResults):
@@ -269,7 +283,7 @@ class HeckmanResults(base.LikelihoodModelResults):
         # add top info
         top_left = [('Dep. Variable:', None),
                     ('Model:', None),
-                    ('Method:', ['Heckman 2-Step']),
+                    ('Method:', ['Heckman Two-Step']),
                     ('Date:', None),
                     ('Time:', None),
                     ('No. Total Obs.:', ["%#i" % self.model.nobs_total]),
