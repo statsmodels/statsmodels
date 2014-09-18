179beba89651bff6de6c58f4435b21ed55d09938
Josef Perktold <josef.pktd@gmail.com>
2014-09-18 10:51:06 -0400

    BUG: summary2 use float_format when creating `_simple_tables` see #1964

--- statsmodels/iolib/summary2.py
+++ statsmodels/iolib/summary2.py
@@ -541,7 +541,7 @@ def _df_to_simpletable(df, align='r', float_format="%.4f", header=True,
 
 def _simple_tables(tables, settings, pad_col=None, pad_index=None):
     simple_tables = []
-    float_format = '%.4f'
+    float_format = settings[1]['float_format']
     if pad_col is None:
         pad_col = [0] * len(tables)
     if pad_index is None:
