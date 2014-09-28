dec35ee0d3cf013026cd69c1ff282b0486856e5a
Evgeni Burovski <evgeni@burovski.me>
2014-09-28 21:28:36 +0100

    MAINT: distributions: tweak the __init__

--- statsmodels/distributions/__init__.py
+++ statsmodels/distributions/__init__.py
@@ -1,6 +1,3 @@
-from empirical_distribution import ECDF, monotone_fn_inverter, StepFunction
-from edgeworth import ExpandedNormal
-
-from statsmodels import NoseWrapper as Tester
-test = Tester().test
+from .empirical_distribution import ECDF, monotone_fn_inverter, StepFunction
+from .edgeworth import ExpandedNormal
 
