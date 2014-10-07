47401c7ccd89456bbc3177f976194e885123b985
Skipper Seabold <jsseabold@gmail.com>
2014-10-07 10:09:13 -0400

    DOC: Silence fit.

--- docs/source/plots/arma_predict_plot.py
+++ docs/source/plots/arma_predict_plot.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 dta = sm.datasets.sunspots.load_pandas().data[['SUNACTIVITY']]
 dta.index = pd.DatetimeIndex(start='1700', end='2009', freq='A')
-res = sm.tsa.ARMA(dta, (3, 0)).fit()
+res = sm.tsa.ARMA(dta, (3, 0)).fit(disp=0)
 fig, ax = plt.subplots()
 ax = dta.ix['1950':].plot(ax=ax)
 res.plot_predict('1990', '2012', dynamic=True, ax=ax,
