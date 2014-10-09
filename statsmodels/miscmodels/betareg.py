27a4d783181a8e5a0bb13b9bc4d3bc578acb9e54
Brent Pedersen <bpederse@gmail.com>
2014-10-08 20:31:53 -0600

    rename link_phi to link_precision and Z to exog_precision

--- statsmodels/miscmodels/betareg.py
+++ statsmodels/miscmodels/betareg.py
@@ -41,12 +41,12 @@ _init_example = """
     >>> print(rslt.summary())
 
     We can also specify a formula and a specific structure and use the
-    identity-link for phi.
+    identity-link for precision.
 
     >>> from sm.families.links import identity
     >>> Z = patsy.dmatrix('~ temp', dat, return_type='dataframe')
     >>> mod = Beta.from_formula('iyield ~ C(batch, Treatment(10)) + temp',
-    ...                         dat, Z=Z, link_phi=identity())
+    ...                         dat, exog_precision=Z, link_precision=identity())
 
     In the case of proportion-data, we may think that the precision depends on
     the number of measurements. E.g for sequence data, on the number of
@@ -62,12 +62,11 @@ class Beta(GenericLikelihoodModel):
 
     """Beta Regression.
 
-    This implementation uses `phi` as a precision parameter equal to
-    `a + b` from the Beta parameters.
+    This implementation uses a `precision` parameter
     """
 
-    def __init__(self, endog, exog, Z=None, link=Logit(),
-            link_phi=sm.families.links.Log(), **kwds):
+    def __init__(self, endog, exog, exog_precision=None, link=Logit(),
+            link_precision=sm.families.links.Log(), **kwds):
         """
         Parameters
         ----------
@@ -81,12 +80,12 @@ class Beta(GenericLikelihoodModel):
             the number of regressors. An intercept is not included by
             default and should be added by the user. See
             `statsmodels.tools.add_constant`.
-        Z : array-like
-            2d array of variables for the precision phi.
+        exog_precision : array-like
+            2d array of variables for the precision.
         link : link
             Any link in sm.families.links for `exog`
-        link_phi : link
-            Any link in sm.families.links for `Z`
+        link_precision : link
+            Any link in sm.families.links for `exog_precision`
 
         Examples
         --------
@@ -99,20 +98,23 @@ class Beta(GenericLikelihoodModel):
         """.format(example=_init_example)
         etmp = np.array(endog)
         assert np.all((0 < etmp) & (etmp < 1))
-        if Z is None:
-            extra_names = ['phi']
-            Z = np.ones((len(endog), 1), dtype='f')
+        if exog_precision is None:
+            extra_names = ['precision']
+            exog_precision = np.ones((len(endog), 1), dtype='f')
         else:
             extra_names = ['precision-%s' % zc for zc in \
-                        (Z.columns if hasattr(Z, 'columns') else range(1, Z.shape[1] + 1))]
+                    (exog_precision.columns \
+                    if hasattr(exog_precision, 'columns') 
+                    else range(1, exog_precision.shape[1] + 1))]
+
         kwds['extra_params_names'] = extra_names
 
         super(Beta, self).__init__(endog, exog, **kwds)
         self.link = link
-        self.link_phi = link_phi
+        self.link_precision = link_precision
 
-        self.Z = Z
-        assert len(self.Z) == len(self.endog)
+        self.exog_precision = exog_precision
+        assert len(self.exog_precision) == len(self.endog)
 
     def nloglikeobs(self, params):
         """
@@ -124,7 +126,7 @@ class Beta(GenericLikelihoodModel):
         params : np.ndarray
             Parameter estimates
         """
-        return -self._ll_br(self.endog, self.exog, self.Z, params)
+        return -self._ll_br(self.endog, self.exog, self.exog_precision, params)
 
     def fit(self, start_params=None, maxiter=100000, maxfun=5000, disp=False,
             method='bfgs', **kwds):
@@ -147,20 +149,21 @@ class Beta(GenericLikelihoodModel):
         if start_params is None:
             start_params = sm.GLM(self.endog, self.exog, family=Binomial()
                                  ).fit(disp=False).params
-            start_params = np.append(start_params, [0.5] * self.Z.shape[1])
+            start_params = np.append(start_params, [0.5] \
+                    * self.exog_precision.shape[1])
 
         return super(Beta, self).fit(start_params=start_params,
                                         maxiter=maxiter, maxfun=maxfun,
                                         method=method, disp=disp, **kwds)
 
     def _ll_br(self, y, X, Z, params):
-        nz = self.Z.shape[1]
+        nz = Z.shape[1]
 
         Xparams = params[:-nz]
         Zparams = params[-nz:]
 
         mu = self.link.inverse(np.dot(X, Xparams))
-        phi = self.link_phi.inverse(np.dot(Z, Zparams))
+        phi = self.link_precision.inverse(np.dot(Z, Zparams))
         # TODO: derive a and b and constrain to > 0?
 
         if np.any(phi <= np.finfo(float).eps): return np.array(-np.inf)
@@ -178,7 +181,7 @@ if __name__ == "__main__":
     Z = patsy.dmatrix('~ temp', dat, return_type='dataframe')
     # using other precison params with
     m = Beta.from_formula('iyield ~ C(batch, Treatment(10)) + temp', dat,
-            Z=Z, link_phi=sm.families.links.identity())
+            exog_precision=Z, link_precision=sm.families.links.identity())
     print(m.fit().summary())
 
     fex = pd.read_csv('foodexpenditure.csv')
@@ -189,6 +192,6 @@ if __name__ == "__main__":
     dev = pd.read_csv('methylation-test.csv')
     Z = patsy.dmatrix('~ age', dev, return_type='dataframe')
     m = Beta.from_formula('methylation ~ gender + CpG', dev,
-            Z=Z,
-            link_phi=sm.families.links.identity())
+            exog_precision=Z,
+            link_precision=sm.families.links.identity())
     print(m.fit().summary())
