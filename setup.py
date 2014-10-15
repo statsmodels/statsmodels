7473965c9c71e2bc89951fda7f75bc2f780b3b3e
Skipper Seabold <jsseabold@gmail.com>
2014-10-15 16:28:18 -0400

    RLS: Set version number to 0.6.0rc1

--- setup.py
+++ setup.py
@@ -159,8 +159,8 @@ def check_dependency_versions(min_versions):
 MAJ = 0
 MIN = 6
 REV = 0
-ISRELEASED = False
-VERSION = '%d.%d.%d' % (MAJ,MIN,REV)
+ISRELEASED = True
+VERSION = '%d.%d.%d-rc1' % (MAJ,MIN,REV)
 
 classifiers = [ 'Development Status :: 4 - Beta',
               'Environment :: Console',
