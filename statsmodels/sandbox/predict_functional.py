5b8c3bc13954e432a6cfc3c84ce942cc884941e7
Kerby Shedden <kshedden@umich.edu>
2015-01-11 10:37:24 -0500

    Added simultaneous confidence bands

--- statsmodels/sandbox/predict_functional.py
+++ statsmodels/sandbox/predict_functional.py
@@ -2,6 +2,7 @@ import statsmodels.api as sm
 import pandas as pd
 import patsy
 import numpy as np
+import warnings
 
 """
 A predict-like function that constructs means and pointwise confidence
@@ -31,6 +32,18 @@ def _make_formula_exog(result, focus_var, summaries, values, num_points):
     exog = model.data.frame
 
     colnames = summaries.keys() + values.keys() + [focus_var]
+
+    # Check for variables whose values are not set either through
+    # `values` or `summaries`.  Since the model data frame can contain
+    # extra variables not referenced in the formula RHS, this may not
+    # be a problem, so just warn.  There is no obvious way to extract
+    # from a formula all the variable names that it references.
+    varl = set(exog.columns.tolist()) - set([model.endog_names])
+    unmatched = varl - set(colnames)
+    unmatched = list(unmatched)
+    if len(unmatched) > 0:
+        warnings.warn("%s in data frame but not in summaries or values."
+                      % ", ".join(["'%s'" % x for x in unmatched]))
 
     fexog = pd.DataFrame(index=range(num_points), columns=colnames)
 
@@ -70,6 +83,15 @@ def _make_exog(result, focus_var, summaries, values, num_points):
 
     exog = np.zeros((num_points, model_exog.shape[1]))
 
+    # Check for variables whose values are not set either through
+    # `values` or `summaries`.
+    colnames = values.keys() + summaries.keys() + [focus_var]
+    unmatched = set(exog_names) - set(colnames)
+    unmatched = list(unmatched)
+    if len(unmatched) > 0:
+        warnings.warn("%s in model but not in `summaries` or `values`."
+                      % ", ".join(["'%s'" % x for x in unmatched]))
+
     # The values of the 'focus variable' are a sequence of percentiles
     pctls = np.linspace(0, 100, num_points).tolist()
     ix = exog_names.index(focus_var)
@@ -89,9 +111,10 @@ def _make_exog(result, focus_var, summaries, values, num_points):
     return exog, fvals
 
 
-def predict_functional(result, focus_var, summaries, values, num_points=10, **kwargs):
+def predict_functional(result, focus_var, summaries=None, values=None,
+                       cvrg_prob=0.95, simultaneous=False, num_points=10, **kwargs):
     """
-    Returns predictions of a fitted model relative to a given 'focus variable'.
+    Returns predictions of a fitted model as a function of a given covariate.
 
     The value of the focus variable varies along a sequence of its
     quantiles, calculated from the data used to fit the model.  The
@@ -111,20 +134,27 @@ def predict_functional(result, focus_var, summaries, values, num_points=10, **kw
         model, to obtain a value at which the variable is held fixed.
     values : dict-like
         Values at which a given non-focus variable is held fixed.
+    cvrg_prob : float
+        The coverage probability.
+    simultaneous : bool
+        If true, the confidence band is simultaneous, otherwise it is
+        pointwise.
     num_points : integer
-        The number of rows of the resulting data frame.
+        The number of equally-spaced quantile points where the
+        prediction is made.
     kwargs :
-        Arguments such as `linear` passed to the `predict` method.
+        Arguments passed to the `predict` method.
 
     Returns
     -------
     pred : array-like
         The predicted mean values.
-    ci : array-like
-        An array with two columns, containing respectively the lower and upper
-        confidence limit.
+    cb : array-like
+        An array with two columns, containing respectively the lower
+        and upper limits of a confidence band.
     fvals : array-like
-        The values of the focus variable at which the prediction is made.
+        The values of the focus variable at which the prediction is
+        made.
 
     Notes
     -----
@@ -134,8 +164,8 @@ def predict_functional(result, focus_var, summaries, values, num_points=10, **kw
     These are conditional means, based on specified values of the
     non-focus variables.  They are not marginal (unconditional) means.
 
-    Example
-    -------
+    Examples
+    --------
     Fit a model using a formula in which the predictors are age
     (modeled with splines), ethnicity (which is categorical), gender,
     and income.  Then we obtain the fitted mean values as a function
@@ -147,7 +177,7 @@ def predict_functional(result, focus_var, summaries, values, num_points=10, **kw
     >>> mode = lambda x : x.value_counts().argmax()
     >>> summaries = {'income': np.mean, ethnicity=mode}
     >>> values = {'gender': 'female'}
-    >>> pr = predict_focus(result, 'age', summaries, values)
+    >>> pr, cb, x = predict_functional(result, 'age', summaries, values)
 
     Fit a model using arrays.  Plot the means as a function of x3,
     holding x1 fixed at its mean value in the data used to fit the
@@ -157,14 +187,20 @@ def predict_functional(result, focus_var, summaries, values, num_points=10, **kw
     >>> result = model.fit()
     >>> summaries = {'x1': np.mean}
     >>> values = {'x2': 1}
-    >>> pr = predict_focus(result, 'x3', summaries, values)
+    >>> pr, cb, x = predict_functional(result, 'x3', summaries, values)
     """
+
+    if summaries is None:
+        summaries = {}
+    if values is None:
+        values = {}
 
     ky = set(values.keys()) & set(summaries.keys())
     if len(ky) > 0:
         raise ValueError("%s included in both `values` and `summaries`" %
                          ", ".join(ky))
 
+    # Branch depending on whether the model was fit with a formula.
     if hasattr(result.model.data, "frame"):
         dexog, fexog, fvals = _make_formula_exog(result, focus_var, summaries, values, num_points)
     else:
@@ -173,21 +209,130 @@ def predict_functional(result, focus_var, summaries, values, num_points=10, **kw
 
     pred = result.predict(exog=fexog, **kwargs)
     t_test = result.t_test(dexog)
-    ci = t_test.conf_int()
 
-    return pred, ci, fvals
+    if simultaneous:
+        sd = t_test.sd
+        cb = np.zeros((num_points, 2))
 
-def predict_functional_glm(result, focus_var, summaries, values, num_points=10, **kwargs):
+        # Scheffe's method
+        from scipy.stats.distributions import f as fdist
+        df1 = result.model.exog.shape[1]
+        df2 = result.model.exog.shape[0] - df1
+        qf = fdist.cdf(cvrg_prob, df1, df2)
+        fx = sd * np.sqrt(df1 * qf)
+        cb[:, 0] = pred - fx
+        cb[:, 1] = pred + fx
 
-    if not hasattr(result.model, "family"):
-        raise ValueError("result must be a fitted GLM or GEE results instance")
+    else:
+        cb = t_test.conf_int(alpha=1-cvrg_prob)
 
-    pred, ci, fvals = predict_functional(result, focus_var, summaries, values, num_points=10, linear=True)
+    return pred, cb, fvals
 
-    link = result.family.link
-    pred = link.inverse(pred)
-    ci = link.inverse(ci)
 
-    return pred, ci, fvals
+def _glm_basic_scr(result, exog, cvrg_prob):
+    """
+    The basic SCR from (Sun et al. Annals of Statistics 2000).
+
+    Parameters
+    ----------
+    result : results instance
+        The fitted GLM results instance
+    exog : array-like
+        The exog values spanning the interval
+    cvrg_prob : float
+        Coverage probability.
+
+    Returns
+    -------
+    An array with two columns, containing the lower and upper
+    confidence bounds, respectively.
+
+    Notes
+    -----
+    The rows of `exog` should be a sequence of covariate values
+    obtained by taking one 'free variable' x and varying it over an
+    interval.  The matrix `exog` is thus the basis functions and any
+    other covariates evaluated as x varies.
+    """
+
+    model = result.model
+    n = model.exog.shape[0]
+
+    # Get the Hessian without recomputing.
+    cov = result.cov_params()
+    hess = np.linalg.inv(cov)
+
+    # Proposition 3.1 of Sun et al.
+    A = hess / n
+    B = np.linalg.cholesky(A).T # Upper Cholesky triangle
+
+    # The variance and SD of the linear predictor at each row of exog.
+    sigma2 = (np.dot(exog, cov) * exog).sum(1)
+    sigma = np.sqrt(sigma2)
+
+    # Calculate kappa_0 (formula 42 from Sun et al)
+    bz = np.linalg.solve(B.T, exog.T).T
+    bz /= np.sqrt(n)
+    bz /= sigma[:, None]
+    bzd = np.diff(bz, 1, axis=0)
+    bzdn = (bzd**2).sum(1)
+    kappa_0 = np.sqrt(bzdn).sum()
+
+    from scipy.stats.distributions import norm
+
+    # The root of this function is the multiplier for the confidence
+    # band, see Sun et al. equation 35.
+    def func(c):
+        return kappa_0 * np.exp(-c**2/2) / np.pi + 2*(1 - norm.cdf(c)) - (1 - cvrg_prob)
+
+    from scipy.optimize import brentq
+
+    c, rslt = brentq(func, 1, 10, full_output=True)
+    if rslt.converged == False:
+        raise ValueError("Root finding error in basic SCR")
+
+    return sigma, c
+
+def predict_functional_glm(result, focus_var, summaries=None, values=None, cvrg_prob=0.95,
+                           simultaneous=False, num_points=10, linear=False, **kwargs):
+
+    if summaries is None:
+        summaries = {}
+    if values is None:
+        values = {}
+
+    ky = set(values.keys()) & set(summaries.keys())
+    if len(ky) > 0:
+        raise ValueError("%s included in both `values` and `summaries`" %
+                         ", ".join(ky))
+
+    # Branch depending on whether the model was fit with a formula.
+    if hasattr(result.model.data, "frame"):
+        dexog, fexog, fvals = _make_formula_exog(result, focus_var, summaries, values, num_points)
+    else:
+        exog, fvals = _make_exog(result, focus_var, summaries, values, num_points)
+        dexog, fexog = exog, exog
+
+    kwargs_pred = kwargs.copy()
+    kwargs_pred.update({"linear": True})
+    pred = result.predict(exog=fexog, **kwargs_pred)
+    t_test = result.t_test(dexog)
+
+    if simultaneous:
+
+        sigma, c = _glm_basic_scr(result, exog, cvrg_prob)
+        cb = np.zeros((exog.shape[0], 2))
+        cb[:, 0] = pred - c*sigma
+        cb[:, 1] = pred + c*sigma
+
+    else:
+        cb = t_test.conf_int(alpha=1-cvrg_prob)
+
+    if not linear:
+        link = result.family.link
+        pred = link.inverse(pred)
+        cb = link.inverse(cb)
+
+    return pred, cb, fvals
 
 predict_functional_glm.__doc__ = predict_functional.__doc__.replace("predict_focus", "predict_focus_glm")
--- statsmodels/sandbox/tests/test_predict_functional.py
+++ statsmodels/sandbox/tests/test_predict_functional.py
@@ -31,7 +31,7 @@ def formula_example(plt, pdf):
     x4 = np.random.randint(0, 5, size=n)
     x4 = np.asarray(["ABCDE"[i] for i in x4])
     x5 = np.random.normal(size=n)
-    y = x2**2 + (x4 == "B") + (x4 == "B") * x2**2 + x5 + np.random.normal(size=n)
+    y = 0.3*x2**2 + (x4 == "B") + 0.1*(x4 == "B")*x2**2 + x5 + np.random.normal(size=n)
 
     df = pd.DataFrame({"y": y, "x1": x1, "x2": x2, "x3": x3, "x4": x4, "x5": x5})
 
@@ -45,10 +45,10 @@ def formula_example(plt, pdf):
     summaries = {"x1": np.mean, "x3": pctl(0.75), "x5": np.mean}
 
     values = {"x4": "B"}
-    pr1, ci1, fvals1 = predict_functional(result, "x2", summaries, values, 10)
+    pr1, ci1, fvals1 = predict_functional(result, "x2", summaries, values)
 
     values = {"x4": "C"}
-    pr2, ci2, fvals2 = predict_functional(result, "x2", summaries, values, 10)
+    pr2, ci2, fvals2 = predict_functional(result, "x2", summaries, values)
 
     plt.clf()
     fig = plt.figure()
@@ -73,9 +73,73 @@ def formula_example(plt, pdf):
     plt.figlegend(ha, lb, "center right")
     plt.xlabel("Focus variable", size=15)
     plt.ylabel("Fitted mean", size=15)
-    plt.title("Using formula")
+    plt.title("Using formula, pointwise CB")
     close_or_save(pdf, fig)
 
+
+
+def scb_example(plt, pdf):
+
+    np.random.seed(473)
+    n = 100
+    x = np.random.normal(size=(n,4))
+    x[:, 0] = 1
+
+    for fam_name in "poisson", "binomial", "gaussian":
+
+        if fam_name == "poisson":
+            y = np.random.poisson(20, size=n)
+            fam = sm.families.Poisson()
+            true_mean = 20
+            true_lp = np.log(20)
+        elif fam_name == "binomial":
+            y = 1 * (np.random.uniform(size=n) < 0.5)
+            fam = sm.families.Binomial()
+            true_mean = 0.5
+            true_lp = 0
+        elif fam_name == "gaussian":
+            y = np.random.normal(size=n)
+            fam = sm.families.Gaussian()
+            true_mean = 0
+            true_lp = 0
+
+        model = sm.GLM(y, x, family=fam)
+        result = model.fit()
+
+        # CB is for linear predictor or mean response
+        for linear in False, True:
+
+            true = true_lp if linear else true_mean
+
+            values = {'const': 1, "x2": 0}
+            summaries = {"x3": np.mean}
+            pred1, cb1, fvals1 = predict_functional_glm(result, "x1",
+                        values=values, summaries=summaries, linear=linear)
+            pred2, cb2, fvals2 = predict_functional_glm(result, "x1",
+                        values=values, summaries=summaries, simultaneous=True,
+                        linear=linear)
+
+            plt.clf()
+            fig = plt.figure()
+            ax = plt.axes([0.1, 0.1, 0.58, 0.8])
+            plt.plot(fvals1, pred1, '-', color='black', label='Estimate')
+            plt.plot(fvals1, true * np.ones(len(pred1)), '-', color='purple',
+                     label='Truth')
+            plt.plot(fvals1, cb1[:, 0], color='blue', label='Pointwise CB')
+            plt.plot(fvals1, cb1[:, 1], color='blue')
+            plt.plot(fvals2, cb2[:, 0], color='green', label='Simultaneous CB')
+            plt.plot(fvals2, cb2[:, 1], color='green')
+            ha, lb = ax.get_legend_handles_labels()
+            leg = plt.figlegend(ha, lb, "center right")
+            leg.draw_frame(False)
+            plt.xlabel("Focus variable", size=15)
+            if linear:
+                plt.ylabel("Linear predictor", size=15)
+            else:
+                plt.ylabel("Fitted mean", size=15)
+            plt.title("Using formula, %s family, constant mean" % fam_name.capitalize())
+
+            close_or_save(pdf, fig)
 
 
 def glm_formula_example(plt, pdf):
@@ -102,10 +166,10 @@ def glm_formula_example(plt, pdf):
     summaries = {"x2": np.mean}
 
     values = {"x3": "B"}
-    pr1, ci1, fvals1 = predict_functional_glm(result, "x1", summaries, values, 10)
+    pr1, ci1, fvals1 = predict_functional_glm(result, "x1", summaries, values)
 
     values = {"x3": "C"}
-    pr2, ci2, fvals2 = predict_functional_glm(result, "x1", summaries, values, 10)
+    pr2, ci2, fvals2 = predict_functional_glm(result, "x1", summaries, values)
 
     plt.clf()
     fig = plt.figure()
@@ -116,7 +180,7 @@ def glm_formula_example(plt, pdf):
     plt.figlegend(ha, lb, "center right")
     plt.xlabel("Focus variable", size=15)
     plt.ylabel("Fitted mean", size=15)
-    plt.title("Using formula (GLM)")
+    plt.title("Using formula, Binomial GLM")
     close_or_save(pdf, fig)
 
     plt.clf()
@@ -130,7 +194,7 @@ def glm_formula_example(plt, pdf):
     plt.figlegend(ha, lb, "center right")
     plt.xlabel("Focus variable", size=15)
     plt.ylabel("Fitted mean", size=15)
-    plt.title("Using formula (GLM)")
+    plt.title("Using formula, Binomial GLM, pointwise CB")
     close_or_save(pdf, fig)
 
 
@@ -153,10 +217,10 @@ def noformula_example(plt, pdf):
 
     summaries = {"x3": pctl(0.75)}
     values = {"x2": 1}
-    pr1, ci1, fvals1 = predict_functional(result, "x1", summaries, values, 10)
+    pr1, ci1, fvals1 = predict_functional(result, "x1", summaries, values)
 
     values = {"x2": -1}
-    pr2, ci2, fvals2 = predict_functional(result, "x1", summaries, values, 10)
+    pr2, ci2, fvals2 = predict_functional(result, "x1", summaries, values)
 
     plt.clf()
     fig = plt.figure()
@@ -176,9 +240,13 @@ def noformula_example(plt, pdf):
     ax = plt.axes([0.1, 0.1, 0.7, 0.8])
     plt.plot(fvals1, pr1, '-', label='x2=1', lw=4, alpha=0.6)
     plt.fill_between(fvals1, ci1[:, 0], ci1[:, 1], color='grey')
+    plt.plot(fvals1, pr2, '-', label='x2=1', lw=4, alpha=0.6)
+    plt.fill_between(fvals2, ci2[:, 0], ci2[:, 1], color='grey')
+    ha, lb = ax.get_legend_handles_labels()
+    plt.figlegend(ha, lb, "center right")
     plt.xlabel("Focus variable", size=15)
     plt.ylabel("Fitted mean", size=15)
-    plt.title("Not using formula")
+    plt.title("Not using formula, pointwise CB")
     close_or_save(pdf, fig)
 
 
@@ -191,6 +259,7 @@ def test_all():
     else:
         pdf = None
 
+    scb_example(plt, pdf)
     formula_example(plt, pdf)
     glm_formula_example(plt, pdf)
     noformula_example(plt, pdf)
