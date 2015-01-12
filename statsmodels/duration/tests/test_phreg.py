e6ece6f52e9b2925ca8f09fa90c09c029dfd6c7d
Kerby Shedden <kshedden@umich.edu>
2015-01-12 11:06:08 -0500

    Initial commit to fix issue 2178

--- statsmodels/duration/tests/test_phreg.py
+++ statsmodels/duration/tests/test_phreg.py
@@ -167,6 +167,39 @@ class TestPHReg(object):
         assert_allclose(rslt1.bse, rslt2.bse)
         assert_allclose(rslt1.bse, rslt3.bse)
 
+    def test_predict_formula(self):
+
+        n = 100
+        np.random.seed(34234)
+        time = 50 * np.random.uniform(size=n)
+        status = np.random.randint(0, 2, n).astype(np.float64)
+        exog = np.random.uniform(1, 2, size=(n, 2))
+
+        df = pd.DataFrame({"time": time, "status": status,
+                           "exog1": exog[:, 0], "exog2": exog[:, 1]})
+
+        fml = "time ~ 0 + exog1 + np.log(exog2) + exog1*exog2"
+        model1 = PHReg.from_formula(fml, df, status=status)
+        result1 = model1.fit()
+
+        from patsy import dmatrix
+        dfp = dmatrix(model1.data.orig_exog.design_info.builder, df)
+
+        pr1 = result1.predict()
+        pr2 = result1.predict(exog=df)
+        pr3 = model1.predict(result1.params, exog=dfp) # No standard errors
+        pr4 = model1.predict(result1.params, cov_params=result1.cov_params(), exog=dfp)
+
+        prl = (pr1, pr2, pr3, pr4)
+        for i in range(4):
+            for j in range(i):
+                assert_allclose(prl[i].predicted_values, prl[j].predicted_values)
+
+        prl = (pr1, pr2, pr4)
+        for i in range(3):
+            for j in range(i):
+                assert_allclose(prl[i].standard_errors, prl[j].standard_errors)
+
     def test_offset(self):
 
         np.random.seed(34234)
