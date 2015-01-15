580ec8b8b545f55ab6ca17af1de795aa0f17e900
Chad Fulton <chad@chadfulton.com>
2015-01-15 11:07:20 -0800

    REF: cov_params no longer use delta method

--- statsmodels/tsa/statespace/mlemodel.py
+++ statsmodels/tsa/statespace/mlemodel.py
@@ -567,6 +567,20 @@ class MLEResults(FilterResults, tsbase.TimeSeriesModelResults):
 
     @cache_readonly
     def cov_params(self):
+        hessian = self.model.hessian(
+            self._params, set_params=False, transformed=True,
+            initial_state=self.initial_state,
+            initial_state_cov=self.initial_state_cov
+        )
+
+        # Reset the matrices to the saved parameters (since they were
+        # overwritten in the hessian call)
+        self.model.update(self.model.params)
+
+        return -np.linalg.inv(hessian*self.nobs)
+
+    @cache_readonly
+    def cov_params_delta(self):
         # Uses Delta method (method of propagation of errors)
 
         unconstrained = self.model.untransform_params(self._params)
