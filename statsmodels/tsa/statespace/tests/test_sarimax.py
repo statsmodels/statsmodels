e4527e9a7e16a27ab95c4cade63476059a7ba1e6
Josef Perktold <josef.pktd@gmail.com>
2015-02-13 22:32:54 -0500

    BUG use model.orig_endog not model.data.orig_endog (already transformed)

--- statsmodels/tsa/statespace/tests/test_sarimax.py
+++ statsmodels/tsa/statespace/tests/test_sarimax.py
@@ -618,8 +618,9 @@ class SARIMAXCoverageTest(object):
 
         kwargs = self.model._get_init_kwds()
         endog = mod1.endog.squeeze() # test failures
-        endog = mod1.data.orig_endog
-        model2 = sarimax.SARIMAX(endog, mod1.exog, **kwargs)
+        endog = mod1.orig_endog
+        exog = mod1.orig_exog
+        model2 = sarimax.SARIMAX(endog, exog, **kwargs)
         model2.update(self.true_params)
         res1 = self.model.filter()
         res2 = model2.filter()
