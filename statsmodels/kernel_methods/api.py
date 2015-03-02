efbcea45b6322635cf31257e6066f917f248e26e
Pierre Barbier de Reuille <pierre.barbierdereuille@gmail.com>
2015-03-02 14:09:04 +0100

    Added new module for kernel methods.
    This takes over from the kde/draft branch, with renaming of the modules
    in progress.

---
+++ statsmodels/kernel_methods/api.py
@@ -0,0 +1,4 @@
+from .kde import KDE  # noqa
+from . import bandwidths  # noqa
+from . import _kde1d_methods, _kdend_methods, _kdenc_methods, _kde_multivariate  # noqa
+from . import grid, kernels  # noqa
