7db1a04c3c9b6165dc700849442fe84c34cc9b7b
Josef <josef.pktd@gmail.com>
2015-03-13 13:15:39 -0400

    try sphinx source encoding (doesn't help for autosummary)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,7 +58,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The encoding of source files.
-#source_encoding = 'utf-8-sig'
+source_encoding = 'utf-8-sig'
 
 # The master toctree document.
 master_doc = 'index'
