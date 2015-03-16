3ec8e06101ee643505ee39401b4fb89429135adf
Pierre Barbier de Reuille <pierre.barbierdereuille@gmail.com>
2015-03-16 19:04:29 +0100

    Added KDEnD tests
    Also some corrected in grid to pass test with ancient numpy
    Re-organised KDE tests

--- statsmodels/kernel_methods/tests/kde_utils.py
+++ statsmodels/kernel_methods/tests/kde_utils.py
@@ -1,16 +1,18 @@
 import numpy as np
 from .. import kde_methods as km
 from ..kde_utils import namedtuple
-from ...compat.python import range
 from scipy import stats
 from .. import kernels
-from .. import kde
 
 def generate(dist, N, low, high):
     start = dist.cdf(low)
     end = dist.cdf(high)
     xs = np.linspace(1 - start, 1 - end, N)
     return dist.isf(xs)
+
+def generate_nd(dist, N):
+    np.random.seed(1)
+    return dist.rvs(N)
 
 def setupClass_norm(cls):
     """
@@ -20,7 +22,7 @@ def setupClass_norm(cls):
     cls.sizes = [128, 256, 512]
     cls.vs = [generate(cls.dist, s, -5, 5) for s in cls.sizes]
     cls.args = {}
-    cls.weights = [cls.dist.pdf(v) for v in cls.vs]
+    cls.weights = [cls.dist.rvs(v) for v in cls.sizes]
     cls.adjust = [1 - ws for ws in cls.weights]
     cls.xs = np.r_[-5:5:512j]
     cls.lower = -5
@@ -40,101 +42,49 @@ def setupClass_lognorm(cls):
     cls.upper = 20
     cls.methods = methods_log
 
+def setupClass_normnd(cls, ndim):
+    """
+    Seting the class for a nD normal distribution
+    """
+    cls.dist = stats.multivariate_normal(cov=np.eye(ndim))
+    cls.sizes = [32, 64, 128]
+    cls.vs = [generate_nd(cls.dist, s) for s in cls.sizes]
+    cls.weights = [cls.dist.pdf(v) for v in cls.vs]
+    cls.adjust = [1 - ws for ws in cls.weights]
+    cls.xs = [np.r_[-5:5:512j]] * ndim
+    cls.lower = [-5] * ndim
+    cls.upper = [5] * ndim
+    cls.methods = methods_nd
+    cls.args = {}
+
 test_method = namedtuple('test_method',
                          ['instance', 'accuracy', 'grid_accuracy',
                           'normed_accuracy', 'bound_low', 'bound_high'])
 
-methods = [ test_method(km.Unbounded1D, 1e-5, 1e-4, 1e-5, False, False)
-          , test_method(km.Reflection1D, 1e-5, 1e-4, 1e-5, True, True)
-          , test_method(km.Cyclic1D, 1e-5, 1e-3, 1e-4, True, True)
-          , test_method(km.Renormalization, 1e-5, 1e-4, 1e-2, True, True)
-          , test_method(km.LinearCombination, 1e-1, 1e-1, 1e-1, True, False)
+methods = [test_method(km.Unbounded1D, 1e-5, 1e-4, 1e-5, False, False)
+          ,test_method(km.Reflection1D, 1e-5, 1e-4, 1e-5, True, True)
+          ,test_method(km.Cyclic1D, 1e-5, 1e-3, 1e-4, True, True)
+          ,test_method(km.Renormalization, 1e-5, 1e-4, 1e-2, True, True)
+          ,test_method(km.LinearCombination, 1e-1, 1e-1, 1e-1, True, False)
           ]
 methods_log = [test_method(km.TransformKDE1D(km.LogTransform), 1e-5, 1e-4, 1e-5, True, False)]
 
+methods_nd = [test_method(km.Cyclic, 1e-5, 1e-4, 1e-5, True, True)
+             ,test_method(km.Cyclic, 1e-5, 1e-4, 1e-5, False, False)
+             ,test_method(km.KDEnDMethod, 1e-5, 1e-4, 1e-5, False, False)
+             ]
+
 test_kernel = namedtuple('test_kernel', ['cls', 'precision_factor', 'var', 'positive'])
 
-kernels1d = [ test_kernel(kernels.normal1d, 1, 1, True)
-            , test_kernel(kernels.tricube, 1, 1, True)
-            , test_kernel(kernels.Epanechnikov, 10, 1, True)
-            , test_kernel(kernels.normal_order4, 10, 0, False)  # Bad for precision because of high frequencies
-            , test_kernel(kernels.Epanechnikov_order4, 1000, 0, False)  # Bad for precision because of high frequencies
+kernels1d = [test_kernel(kernels.normal1d, 1, 1, True)
+            ,test_kernel(kernels.tricube, 1, 1, True)
+            ,test_kernel(kernels.Epanechnikov, 10, 1, True)
+            ,test_kernel(kernels.normal_order4, 10, 0, False)  # Bad for precision because of high frequencies
+            ,test_kernel(kernels.Epanechnikov_order4, 1000, 0, False)  # Bad for precision because of high frequencies
             ]
 
-kernelsnc = [ test_kernel(kernels.AitchisonAitken, 1, 1, True)
-            , test_kernel(kernels.WangRyzin, 1, 1, True)
+kernelsnc = [test_kernel(kernels.AitchisonAitken, 1, 1, True)
+            ,test_kernel(kernels.WangRyzin, 1, 1, True)
             ]
 
-kernelsnd = [ test_kernel(kernels.normal, 1, 1, True) ]
-
-class KDETester(object):
-
-    def createKDE(self, data, method, **args):
-        all_args = dict(self.args)
-        all_args.update(args)
-        k = kde.KDE(data, **all_args)
-        if method.instance is None:
-            del k.method
-        else:
-            k.method = method.instance
-        if method.bound_low:
-            k.lower = self.lower
-        else:
-            del k.lower
-        if method.bound_high:
-            k.upper = self.upper
-        else:
-            del k.upper
-        return k
-
-    def test_methods(self):
-        for m in self.methods:
-            for i in range(len(self.sizes)):
-                k = self.createKDE(self.vs[i], m)
-                yield self.method_works, k, m, '{0}_{1}'.format(k.method, i)
-
-    def test_grid_methods(self):
-        for m in self.methods:
-            for i in range(len(self.sizes)):
-                k = self.createKDE(self.vs[i], m)
-                yield self.grid_method_works, k, m, '{0}_{1}'.format(k.method, i)
-
-    def test_weights_methods(self):
-        for m in self.methods:
-            for i in range(len(self.sizes)):
-                k = self.createKDE(self.vs[i], m)
-                k.weights = self.weights[i]
-                yield self.method_works, k, m, 'weights_{0}_{1}'.format(k.method, i)
-
-    def test_weights_grid_methods(self):
-        for m in self.methods:
-            for i in range(len(self.sizes)):
-                k = self.createKDE(self.vs[i], m)
-                k.weights = self.weights[i]
-                yield self.grid_method_works, k, m, 'weights_{0}_{1}'.format(k.method, i)
-
-    def test_adjust_methods(self):
-        for m in self.methods:
-            k = self.createKDE(self.vs[0], m)
-            k.adjust = self.adjust[0]
-            yield self.method_works, k, m, 'adjust_{0}_{1}'.format(k.method, 0)
-
-    def test_adjust_grid_methods(self):
-        for m in self.methods:
-            k = self.createKDE(self.vs[0], m)
-            k.adjust = self.adjust[0]
-            yield self.grid_method_works, k, m, 'adjust_{0}_{1}'.format(k.method, 0)
-
-    def kernel_works_(self, k):
-        self.kernel_works(k, 'default')
-
-    def test_kernels(self):
-        for k in kernels1d:
-            yield self.kernel_works_, k
-
-    def grid_kernel_works_(self, k):
-        self.grid_kernel_works(k, 'default')
-
-    def test_grid_kernels(self):
-        for k in kernels1d:
-            yield self.grid_kernel_works_, k
+kernelsnd = [test_kernel(kernels.normal, 1, 1, True)]
