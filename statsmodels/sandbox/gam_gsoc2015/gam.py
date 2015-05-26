6603bdc491cafdb577193dbc1d1af924344c0cd0
unknown <lpugginX@IRDEVIMP002.ger.corp.intel.com>
2015-05-26 16:14:18 +0100

    b splines derivative

--- statsmodels/sandbox/gam_gsoc2015/gam.py
+++ statsmodels/sandbox/gam_gsoc2015/gam.py
@@ -4,19 +4,94 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+from scipy.interpolate import splev
 
-n = 2000
+n = 200
 data = pd.DataFrame()
 x = np.linspace(-1, 1, n)
 d = {"x": x}
-dm = dmatrix("bs(x, df=20, degree=2, include_intercept=True)", d)
+dm = dmatrix("bs(x, df=5, degree=2, include_intercept=True)", d)
 
 
-z = np.linspace(-2, 0, n)
-d["z"] = z
-y = x * x +  x + z
+def _R_compat_quantile(x, probs):
+    #return np.percentile(x, 100 * np.asarray(probs))
+    probs = np.asarray(probs)
+    quantiles = np.asarray([np.percentile(x, 100 * prob)
+                            for prob in probs.ravel(order="C")])
+    return quantiles.reshape(probs.shape, order="C")
+
+
+## from patsy splines.py
+def _eval_bspline_basis(x, knots, degree):
+    try:
+        from scipy.interpolate import splev
+    except ImportError: # pragma: no cover
+        raise ImportError("spline functionality requires scipy")
+    # 'knots' are assumed to be already pre-processed. E.g. usually you
+    # want to include duplicate copies of boundary knots; you should do
+    # that *before* calling this constructor.
+    knots = np.atleast_1d(np.asarray(knots, dtype=float))
+    assert knots.ndim == 1
+    knots.sort()
+    degree = int(degree)
+    x = np.atleast_1d(x)
+    if x.ndim == 2 and x.shape[1] == 1:
+        x = x[:, 0]
+    assert x.ndim == 1
+    # XX FIXME: when points fall outside of the boundaries, splev and R seem
+    # to handle them differently. I don't know why yet. So until we understand
+    # this and decide what to do with it, I'm going to play it safe and
+    # disallow such points.
+    if np.min(x) < np.min(knots) or np.max(x) > np.max(knots):
+        raise NotImplementedError("some data points fall outside the "
+                                  "outermost knots, and I'm not sure how "
+                                  "to handle them. (Patches accepted!)")
+    # Thanks to Charles Harris for explaining splev. It's not well
+    # documented, but basically it computes an arbitrary b-spline basis
+    # given knots and degree on some specificed points (or derivatives
+    # thereof, but we don't use that functionality), and then returns some
+    # linear combination of these basis functions. To get out the basis
+    # functions themselves, we use linear combinations like [1, 0, 0], [0,
+    # 1, 0], [0, 0, 1].
+    # NB: This probably makes it rather inefficient (though I haven't checked
+    # to be sure -- maybe the fortran code actually skips computing the basis
+    # function for coefficients that are zero).
+    # Note: the order of a spline is the same as its degree + 1.
+    # Note: there are (len(knots) - order) basis functions.
+    n_bases = len(knots) - (degree + 1)
+    basis = np.empty((x.shape[0], n_bases), dtype=float)
+    der_basis = np.empty((x.shape[0], n_bases), dtype=float)
+    for i in range(n_bases):
+        coefs = np.zeros((n_bases,))
+        coefs[i] = 1
+        basis[:, i] = splev(x, (knots, coefs, degree))
+        der_basis[:, i] = splev(x, (knots, coefs, degree), der=1)
 
 
 
-plt.plot(dm)
+    return basis, der_basis
+
+
+
+df = 5
+degree = 2
+order = degree + 1
+n_inner_knots = df - order
+lower_bound = np.min(x)
+upper_bound = np.max(x)
+knot_quantiles = np.linspace(0, 1, n_inner_knots + 2)[1:-1]
+inner_knots = _R_compat_quantile(x, knot_quantiles)
+all_knots = np.concatenate(([lower_bound, upper_bound] * order, inner_knots))
+
+basis, der_basis = _eval_bspline_basis(x, all_knots, degree)
+
+
+plt.plot(dm[:, 1:])
+plt.show()
+
+
+plt.plot(basis)
+plt.show()
+
+plt.plot(der_basis)
 plt.show()
