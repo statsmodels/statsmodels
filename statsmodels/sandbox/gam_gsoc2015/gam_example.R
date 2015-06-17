127b35f51815ac1384940bec18ea71b29736ec12
donbeo <lucapuggio@gmail.com>
2015-06-18 00:56:13 +0100

    ip

--- statsmodels/sandbox/gam_gsoc2015/gam_example.R
+++ statsmodels/sandbox/gam_gsoc2015/gam_example.R
@@ -1,35 +1,47 @@
 ## This files contains the R code to generate some test for the GAM function ##
 ## Documentation for MGCV is available at: http://cran.r-project.org/web/packages/mgcv/mgcv.pdf
 
+library('pracma')
 library('mgcv')
 #library('gam')
-n = 10000
+n = 100
 x = seq(from = -1, to = 1, length.out = n)
 
 funz = function(x){
-  2 * x * x * x - x
+  2 * x * x * x - x + rnorm(n = n, mean = 0, sd = .1)
 }
+
+
 
 y = funz(x)
 data = data.frame('y'=y, 'x'=x)
 
 # for mgcv
-g = gam(y~s(x, k = 10, bs = "cr"), data = data, scale = 80)
-
-# for gam
-#g = gam(y~s(x, spar = 2), data = data)
-
+g = gam(y~s(x, k = 10, bs = "cr"), data = data, scale = 1)
 
 y_gam = predict(g, newdata = data)
-plot(x, y_gam, type='l', col='blue')
+plot(x, y_gam, type='l', col='blue', ylim=c(min(y), max(y)))
 points(x, y)
 
-new_data = data.frame('x'=seq(from = -1, to = 1, length.out = 100))
-new_data$y = funz(new_data$x)
 
-y_est = predict(g, newdata = new_data)
-plot(new_data$x, y_est, ylim=c(-1, 1), type='l')
-points(new_data$x, new_data$y)
+y_est = predict(g, newdata = data)
+plot(data$x, y_est, ylim=c(min(y), max(y)), type='l')
+points(data$x, data$y)
 
-new_data$y_est = y_est
-write.csv(new_data, '/home/donbeo/Documents/statsmodels/statsmodels/sandbox/gam_gsoc2015/tests/prediction_from_mgcv.csv')
+data$y_est = y_est
+
+### GAM BINOMIAL ### 
+
+mu = mean(y)
+ybin = y
+ybin[y>mu] = 1
+ybin[y<=mu] = 0
+data$ybin = ybin
+gb = gam(ybin~s(x, k = 10, bs = "cr"), data = data, family = 'binomial', scale=10)
+
+data$ybin_est = predict(gb, newdata = data)
+
+plot(data$x, sigmoid(data$ybin_est), 'l', ylim=c(-1,2))
+points(data$x, data$ybin)
+
+write.csv(data, '/home/donbeo/Documents/statsmodels/statsmodels/sandbox/gam_gsoc2015/tests/prediction_from_mgcv.csv')
