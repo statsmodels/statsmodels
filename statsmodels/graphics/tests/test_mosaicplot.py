ef25c10cd8c82c999201ed079c22fedb2a816216
Josef Perktold <josef.pktd@gmail.com>
2015-06-24 20:53:34 -0400

    TST: add smoke test

--- statsmodels/graphics/tests/test_mosaicplot.py
+++ statsmodels/graphics/tests/test_mosaicplot.py
@@ -196,6 +196,28 @@ def test_axes_labeling():
     fig.suptitle("correct alignment of the axes labels")
     #pylab.show()
 
+@dec.skipif(not have_matplotlib or pandas_old)
+def test_mosaic_empty_cells():
+    # SMOKE test  see #2286
+    import pandas as pd
+
+    mydata = pd.DataFrame({'id2': {64: 'Angelica',
+                                   65: 'DXW_UID', 66: 'casuid01',
+                                   67: 'casuid01', 68: 'EC93_uid',
+                                   69: 'EC93_uid', 70: 'EC93_uid',
+                                   60: 'DXW_UID',  61: 'AtmosFox',
+                                   62: 'DXW_UID', 63: 'DXW_UID'},
+                           'id1': {64: 'TGP',
+                                   65: 'Retention01', 66: 'default',
+                                   67: 'default', 68: 'Musa_EC_9_3',
+                                   69: 'Musa_EC_9_3', 70: 'Musa_EC_9_3',
+                                   60: 'default', 61: 'default',
+                                   62: 'default', 63: 'default'}})
+
+    ct = pd.crosstab(mydata.id1, mydata.id2)
+    fig, vals = mosaic(ct.T.unstack())
+    fig, vals = mosaic(mydata, ['id1','id2'])
+
 
 eq = lambda x, y: assert_(np.allclose(x, y))
 
