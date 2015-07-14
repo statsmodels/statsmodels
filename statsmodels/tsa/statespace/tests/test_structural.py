2d0ccb8ec78f68e1bf7a6a000b90f36ed55ef63c
Chad Fulton <chad@chadfulton.com>
2015-07-14 16:32:45 -0700

    TST/CLEAN: Cleanup lines, remove python assert

--- statsmodels/tsa/statespace/tests/test_structural.py
+++ statsmodels/tsa/statespace/tests/test_structural.py
@@ -15,7 +15,7 @@ from statsmodels.datasets import macrodata
 from statsmodels.tsa.statespace import structural
 from .results import results_structural
 from statsmodels.tools import add_constant
-from numpy.testing import assert_almost_equal, assert_raises, assert_allclose
+from numpy.testing import assert_equal, assert_almost_equal, assert_raises, assert_allclose
 from nose.exc import SkipTest
 
 
@@ -34,8 +34,10 @@ def run_ucm(name):
 
         assert_allclose(res.llf, true['llf'])
 
+
 def test_irregular():
     run_ucm('irregular')
+
 
 def test_fixed_intercept():
     warnings.simplefilter("always")
@@ -43,19 +45,24 @@ def test_fixed_intercept():
         run_ucm('fixed_intercept')
         message = ("Specified model does not contain a stochastic element;"
                    " irregular component added.")
-        assert(str(w[0].message) == message)
+        assert_equal(str(w[0].message), message)
+
 
 def test_deterministic_constant():
     run_ucm('deterministic_constant')
 
+
 def test_random_walk():
     run_ucm('random_walk')
+
 
 def test_local_level():
     run_ucm('local_level')
 
+
 def test_fixed_slope():
     run_ucm('fixed_slope')
+
 
 def test_fixed_slope():
     warnings.simplefilter("always")
@@ -63,22 +70,28 @@ def test_fixed_slope():
         run_ucm('fixed_slope')
         message = ("Specified model does not contain a stochastic element;"
                    " irregular component added.")
-        assert(str(w[0].message) == message)
+        assert_equal(str(w[0].message), message)
+
 
 def test_deterministic_trend():
     run_ucm('deterministic_trend')
 
+
 def test_random_walk_with_drift():
     run_ucm('random_walk_with_drift')
+
 
 def test_local_linear_deterministic_trend():
     run_ucm('local_linear_deterministic_trend')
 
+
 def test_local_linear_trend():
     run_ucm('local_linear_trend')
 
+
 def test_smooth_trend():
     run_ucm('smooth_trend')
+
 
 def test_random_trend():
     run_ucm('random_trend')
