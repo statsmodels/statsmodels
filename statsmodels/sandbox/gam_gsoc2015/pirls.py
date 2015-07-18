4f265c04f0ff999f4bbc388f23b35e2ed03fa862
donbeo <lucapuggio@gmail.com>
2015-07-18 14:54:07 +0100

    update pirls

--- statsmodels/sandbox/gam_gsoc2015/pirls.py
+++ statsmodels/sandbox/gam_gsoc2015/pirls.py
@@ -7,8 +7,9 @@ import sklearn
 from sklearn.linear_model import LinearRegression
 import pandas as pd
 from numpy.testing import assert_allclose
-from statsmodels.genmod.generalized_linear_model import GLM
+from statsmodels.genmod.generalized_linear_model import GLM, GLMResults, GLMResultsWrapper
 import statsmodels.regression.linear_model as lm
+import scipy as sp
 
 def equally_spaced_knots(x, n_knots):
     x_min = x.min()
@@ -34,7 +35,6 @@ def splines_x(x, xk):
             xs[i, j+2] = s_ij
     return xs
 
-
 def splines_s(xk):
     q = len(xk) + 2
     s = np.zeros(shape=(q, q))
@@ -43,13 +43,11 @@ def splines_s(xk):
             s[i+2, j+2] = rk(x1, x2)
     return s
 
-
 def get_sqrt(x):
-    '''
-
+    """
     :param x:
     :return: b the sqrt of the matrix x. np.dot(b.T, b) = x
-    '''
+    """
     u, s, v = np.linalg.svd(x)
     sqrt_s = np.sqrt(s)
     b = np.dot(u, np.dot(np.diag(sqrt_s), v))
@@ -58,42 +56,82 @@ def get_sqrt(x):
 class GamPirls(GLM):
 
     def _fit_pirls(self, y, spl_x, spl_s, alpha, start_params=None, maxiter=100, tol=1e-8,
-                  scale=None, cov_type='nonrobust', cov_kwds=None,
-                  use_t=None,):
-
-        rs = get_sqrt(alpha * spl_s) # TODO: this should handle also multivariate analysis i.e. spl_s = [s1, s2, s3...]
+                   scale=None, cov_type='nonrobust', cov_kwds=None, use_t=None, weights=None):
 
         n_samples, n_columns = spl_x.shape
-        x1 = np.vstack([spl_x, rs]) # augmented x
+
+        if isinstance(spl_s, list):
+            alpha_spl_s = [spl_s[i] * alpha[i] for i in range(len(spl_s))]
+            alpha_spl_s = sp.linalg.block_diag(*alpha_spl_s)
+        else:
+            alpha_spl_s = alpha * spl_s
+
+        rs = get_sqrt(alpha_spl_s)
+
+        x1 = np.vstack([spl_x, rs])  # augmented x
         n_samp1es_x1 = x1.shape[0]
-        y1 = np.array([0] * n_samp1es_x1) # augmented y
+        y1 = np.array([0] * n_samp1es_x1)  # augmented y
         y1[:n_samples] = y
 
-        params = np.zeros(shape=(n_columns,))
-        params[0] = 1
+        if weights is None:
+            self.weights = np.array([1./n_samp1es_x1] * n_samp1es_x1)  # TODO: should the weight be of size n_samples_x1?
+                                                                       # Probably we have to use equation 4.22 from the Wood's
+                                                                       # book and replace WLS with OLS
+
+        if start_params is None:
+            params = np.zeros(shape=(n_columns,))
+            params[0] = 1
+        else:
+            params = start_params
+
+        lin_pred = np.dot(x1, params)[:n_samples]
+        self.mu = self.family.fitted(lin_pred)
+        dev = self.family.deviance(self.endog, self.mu)
+
+        history = dict(params=[None, params], deviance=[np.inf, dev])
+        converged = False
+        criterion = history['deviance']
+
         new_norm = 0
         old_norm = 1
-        for i in range(maxiter):
-            print('iteration=', i, 'old_norm=', old_norm, 'new_norm=', new_norm)
+        iteration = 0
+        for iteration in range(maxiter):
             if np.abs(new_norm - old_norm) < tol:
                 break
+
             lin_pred = np.dot(x1, params)[:n_samples]
-            mu = self.family.fitted(lin_pred)
+            self.mu = self.family.fitted(lin_pred)
             z = np.zeros(shape=(n_samp1es_x1,))
-            z[:n_samples] = (y - mu) / mu + lin_pred # TODO: review this and the following line
-            lm_results = lm.OLS(z, x1).fit()
-            lin_pred = np.dot(spl_x, lm_results.params)
+            z[:n_samples] = (y - self.mu) / self.mu + lin_pred # TODO: review this and the following line
+            wls_results = lm.WLS(z, x1, self.weights).fit() # TODO: should weights be used?
+            lin_pred = np.dot(spl_x, wls_results.params)
 
             new_mu = self.family.fitted(lin_pred)
 
             old_norm = new_norm
             new_norm = np.linalg.norm((z[:n_samples] - new_mu))
-        return lm_results.params
 
+        self.scale = 0 # TODO: check the right value scale
+        self.data_weights = 0 # TODO: add support for weights
+        glm_results = GLMResults(self, wls_results.params,
+                                 wls_results.normalized_cov_params,
+                                 self.scale,
+                                 cov_type=cov_type, cov_kwds=cov_kwds,
+                                 use_t=use_t)
 
-data = pd.read_csv('/home/donbeo/Documents/statsmodels/statsmodels/sandbox/gam_gsoc2015/tests/results/gam_PIRLS_results.csv')
+        glm_results.method = "PIRLS"
+        history['iteration'] = iteration + 1
+        glm_results.fit_history = history
+        glm_results.converged = converged
+        return GLMResultsWrapper(glm_results)
 
+'''
+import os
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(cur_dir, "tests/results", "gam_PIRLS_results.csv")
+data = pd.read_csv(file_path)
 
+print('Univariate GAM ')
 X = data['x'].as_matrix()
 Y = data['y'].as_matrix()
 
@@ -104,12 +142,12 @@ SPL_X = splines_x(X, XK)
 
 assert_allclose(spl_x_R, SPL_X)
 
-spl_s_R =[[0, 0, 0.000000000,  0.000000000,  0.000000000,  0.000000000],
-          [0,    0,  0.000000000,  0.000000000,  0.000000000,  0.000000000],
-          [0,    0,  0.001400000,  0.000200000, -0.001133333, -0.001000000],
-          [0,    0,  0.000200000,  0.002733333,  0.001666667, -0.001133333],
-          [0,    0, -0.001133333,  0.001666667,  0.002733333,  0.000200000],
-          [0,    0, -0.001000000, -0.001133333,  0.000200000,  0.001400000]]
+spl_s_R = [[0,    0,  0.000000000,  0.000000000,  0.000000000,  0.000000000],
+           [0,    0,  0.000000000,  0.000000000,  0.000000000,  0.000000000],
+           [0,    0,  0.001400000,  0.000200000, -0.001133333, -0.001000000],
+           [0,    0,  0.000200000,  0.002733333,  0.001666667, -0.001133333],
+           [0,    0, -0.001133333,  0.001666667,  0.002733333,  0.000200000],
+           [0,    0, -0.001000000, -0.001133333,  0.000200000,  0.001400000]]
 SPL_S = splines_s(xk=XK)
 assert_allclose(spl_s_R, SPL_S, atol=4.e-10)
 
@@ -119,15 +157,48 @@ b = get_sqrt(SPL_S)
 
 assert_allclose(np.dot(b.T, b), SPL_S)
 
-link = lambda x:x
+for i, alpha in enumerate([0, .1, 10, 200]):
 
-
-gam = GamPirls(Y, SPL_X)
-params = gam._fit_pirls(Y, SPL_X, SPL_S, 1)
-Y_EST = np.dot(SPL_X, params)
-
-plt.plot(X, Y, '.')
-plt.plot(X, Y_EST, '.')
+    gam = GamPirls(Y, SPL_X)
+    gam_results = gam._fit_pirls(Y, SPL_X, SPL_S, alpha)
+    Y_EST = np.dot(SPL_X, gam_results.params)
+    plt.subplot(2, 2, i+1)
+    plt.title('Alpha=' + str(alpha))
+    plt.plot(X, Y, '.')
+    plt.plot(X, Y_EST, '.')
 plt.show()
 
 
+
+print('multivariate gam')
+X2 = np.random.uniform(0, 1, len(Y))
+XK2 = [0, 0.5, 1]
+SPL_X2 = splines_x(X2, XK2)
+SPL_S2 = splines_s(xk=XK2)
+
+SPL_X_FULL = np.hstack([SPL_X, SPL_X2])
+SPL_S_FULL = [SPL_S, SPL_S2]
+
+Y = X2*X2 + X*X*X + np.random.normal(0, .2, len(Y))
+gam = GamPirls(Y, SPL_X_FULL)
+alpha = 0
+gam_results = gam._fit_pirls(Y, SPL_X_FULL, SPL_S_FULL, alpha=[alpha]*2)
+
+n_var1 = SPL_X.shape[1]
+n_var2 = SPL_X2.shape[1]
+
+plt.subplot(3, 1, 1)
+plt.plot(X, np.dot(SPL_X, gam_results.params[:n_var1]), '.', label='Estimated')
+plt.plot(X, X*X*X, '.', label='Real')
+plt.legend(loc='best')
+plt.subplot(3, 1, 2)
+plt.plot(X2, np.dot(SPL_X2, gam_results.params[n_var1:]), '.', label='Estimated')
+plt.plot(X2, X2*X2, '.', label='Real')
+plt.legend(loc='best')
+plt.subplot(3, 1, 3)
+plt.plot(Y, '.')
+plt.plot(np.dot(SPL_X_FULL, gam_results.params), '.')
+plt.show()
+
+
+'''
---
+++ statsmodels/sandbox/gam_gsoc2015/tests/test_gam_pirls.py
@@ -0,0 +1,52 @@
+__author__ = 'Luca Puggini: <lucapuggio@gmail.com>'
+__date__ = '18/07/15'
+
+
+from statsmodels.sandbox.gam_gsoc2015.pirls import GamPirls, splines_x, splines_s, get_sqrt
+import os
+import pandas as pd
+from numpy.testing import assert_allclose
+import numpy as np
+
+def test_splines_x():
+    cur_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(cur_dir, "results", "gam_PIRLS_results.csv")
+    data = pd.read_csv(file_path)
+
+    x = data['x'].as_matrix()
+    y = data['y'].as_matrix()
+    xk = np.array([0.2, .4, .6, .8])
+
+    spl_x_R = data[['spl_x.1', 'spl_x.2', 'spl_x.3', 'spl_x.4', 'spl_x.5', 'spl_x.6']].as_matrix()
+    spl_x = splines_x(x, xk)
+
+    assert_allclose(spl_x_R, spl_x)
+
+def test_spl_s():
+
+    xk = np.array([0.2, .4, .6, .8])
+    # matrix from R
+    spl_s_R = [[0,    0,  0.000000000,  0.000000000,  0.000000000,  0.000000000],
+               [0,    0,  0.000000000,  0.000000000,  0.000000000,  0.000000000],
+               [0,    0,  0.001400000,  0.000200000, -0.001133333, -0.001000000],
+               [0,    0,  0.000200000,  0.002733333,  0.001666667, -0.001133333],
+               [0,    0, -0.001133333,  0.001666667,  0.002733333,  0.000200000],
+               [0,    0, -0.001000000, -0.001133333,  0.000200000,  0.001400000]]
+    spl_s = splines_s(xk=xk)
+    assert_allclose(spl_s_R, spl_s, atol=4.e-10)
+
+
+def test_gest_sqrt():
+
+    xk = np.array([0.2, .4, .6, .8])
+    # matrix from R
+    spl_s = splines_s(xk=xk)
+    b = get_sqrt(spl_s)
+    assert_allclose(np.dot(b.T, b), spl_s)
+
+
+'''
+test_gest_sqrt()
+test_spl_s()
+test_splines_x()
+'''
