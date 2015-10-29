655cf7d40c927ecf11bd390cea47f84591290188
Chad Fulton <chad@chadfulton.com>
2015-10-29 12:57:32 -0700

    TST: Add (failing) test confirming #2683.

--- statsmodels/tsa/statespace/tests/results/results_structural.py
+++ statsmodels/tsa/statespace/tests/results/results_structural.py
@@ -230,6 +230,11 @@ lltrend_cycle_seasonal_reg_ar1 = {
         {'level': 'lltrend', 'autoregressive': 1, 'cycle': True,
          'stochastic_cycle': True, 'seasonal': 4, 'autoregressive': 1,
          'exog': True, 'mle_regression': False, 'freq':'MS'},
+        # Minutely frequency dataset
+        {'level': 'lltrend', 'autoregressive': 1, 'cycle': True,
+         'stochastic_cycle': True, 'seasonal': 4, 'autoregressive': 1,
+         'exog': True, 'mle_regression': False, 'freq':'T',
+         'cycle_period_bounds': (1.5*12, 12*12)},
     ],
     'params': [0.0001, 0.01, 0.06, 0.0001, 0.0001, 0.1, 2*pi / 10, 0.2],
     'llf': -168.5258709,
