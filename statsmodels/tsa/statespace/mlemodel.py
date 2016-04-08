663bfadf188c1f627515050f8160bc920a207324
Chad Fulton <chad@chadfulton.com>
2016-04-07 23:27:42 -0700

    REF: Expose more smoothing variables in results.

--- statsmodels/tsa/statespace/mlemodel.py
+++ statsmodels/tsa/statespace/mlemodel.py
@@ -1593,8 +1593,10 @@ class MLEResults(tsbase.TimeSeriesModelResults):
         # References of filter and smoother output
         for name in ['filtered_state', 'filtered_state_cov', 'predicted_state',
                      'predicted_state_cov', 'forecasts', 'forecasts_error',
-                     'forecasts_error_cov', 'smoothed_state',
-                     'smoothed_state_cov', 'smoothed_state_autocov',
+                     'forecasts_error_cov', 'scaled_smoothed_estimator',
+                     'scaled_smoothed_estimator_cov', 'smoothing_error',
+                     'smoothed_state', 'smoothed_state_cov',
+                     'smoothed_state_autocov',
                      'smoothed_measurement_disturbance',
                      'smoothed_state_disturbance',
                      'smoothed_measurement_disturbance_cov',
