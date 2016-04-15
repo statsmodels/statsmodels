645517891176783638c5c4b5feb484805cdcb4b4
Brendan Condon <brendan.condon@gmail.com>
2016-04-15 11:22:59 +0100

    Fixed test_remove_data_pickle bug.

--- statsmodels/base/tests/test_shrink_pickle.py
+++ statsmodels/base/tests/test_shrink_pickle.py
@@ -51,6 +51,9 @@ class RemoveDataPickle(object):
         self.l_max = 20000
 
     def test_remove_data_pickle(self):
+
+        import pandas as pd
+
         if winoldnp:
             raise SkipTest
         results = self.results
@@ -72,7 +75,12 @@ class RemoveDataPickle(object):
         #remove data arrays, check predict still works
         results.remove_data()
         pred2 = results.predict(xf, **pred_kwds)
-        np.testing.assert_equal(pred2, pred1)
+
+        if ((isinstance(pred1, pd.Series) and isinstance(pred2, pd.Series)) or
+            (isinstance(pred1, pd.DataFrame) and isinstance(pred2, pd.DataFrame))):
+            assert_(pred1.equals(pred2))
+        else:
+            np.testing.assert_equal(pred2, pred1)
 
         #pickle, unpickle reduced array
         res, l = check_pickle(results._results)
@@ -86,7 +94,12 @@ class RemoveDataPickle(object):
 
 
         pred3 = results.predict(xf, **pred_kwds)
-        np.testing.assert_equal(pred3, pred1)
+
+        if ((isinstance(pred1, pd.Series) and isinstance(pred3, pd.Series)) or
+            (isinstance(pred1, pd.DataFrame) and isinstance(pred3, pd.DataFrame))):
+            assert_(pred1.equals(pred3))
+        else:
+            np.testing.assert_equal(pred3, pred1)
 
     def test_remove_data_docstring(self):
         assert_(self.results.remove_data.__doc__ is not None)
