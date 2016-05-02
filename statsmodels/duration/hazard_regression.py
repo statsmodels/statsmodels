c01726f546b5bba2ceadd21361c16b737684b500
lbybee <leland.bybee@gmail.com>
2016-05-02 16:40:55 -0500

    fix bunch

--- statsmodels/duration/hazard_regression.py
+++ statsmodels/duration/hazard_regression.py
@@ -1233,7 +1233,8 @@ class PHReg(model.LikelihoodModel):
             class bunch:
                 predicted_values = None
                 standard_errors = None
-                ret_val = bunch()
+            
+            ret_val = bunch()
 
             ret_val.predicted_values = predicted_values
 
