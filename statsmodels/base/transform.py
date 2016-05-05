03e7fd0b2f67364c9d43d59e7a96d62cc9f6dc8a
Niels Wouda <nielswouda@gmail.com>
2016-05-05 11:39:05 +0200

    Guerrero

--- statsmodels/base/transform.py
+++ statsmodels/base/transform.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.optimize import minimize
+from scipy.optimize import minimize_scalar
 
 
 class BoxCox(object):
@@ -32,7 +32,7 @@ class BoxCox(object):
             raise ValueError("Non-positive x.")
 
         if lmbda is None:
-            lmbda = self._est_lambda(x, method)
+            lmbda = self._est_lambda(x, method=method)
 
         if np.isclose(lmbda, 0.):
             y = np.log(x)
@@ -52,8 +52,9 @@ class BoxCox(object):
             The lmbda parameter used to transform the series.
         method : {'naive', 'normal'}
             Indicates the method to be used in the untransformation. Defaults
-            to 'naive', which reverses the transformation. 'normal' is an optimal
-            inversion, assuming the series is normally distributed.
+            to 'naive', which reverses the transformation. 'normal' yields an
+            optimal back-transform, assuming the series is normally distributed.
+
         Returns
         -------
         y : array_like
@@ -71,15 +72,49 @@ class BoxCox(object):
 
         return y
 
-    def _est_lambda(self, x, method='guerrero'):
+    def _est_lambda(self, x, low=None, up=None, R=2, method='guerrero'):
         """
         Computes an estimate for the lambda parameter in the Box-Cox
         transformation using method.
-        """
-        if method == 'guerrero':
-            return minimize(self.__guerrero_cv, 0.,
-                            method='Nelder-Mead',
-                            options={'maxiter': 100})
 
-    def __guerrero_cv():
-        pass  # TODO
+        TODO: for specific methods, the number of arguments may increase
+        enormously. Think about a more elegant solution.
+        """
+        if low is None:
+            low = -1
+        if up is None:
+            up = 3
+
+        if method == 'guerrero':
+            res = minimize_scalar(self.__guerrero_cv,
+                                   bounds=(low, up),
+                                   args=(x, R),
+                                   method='bounded',
+                                   options={'maxiter': 100})
+            return res.x
+
+    def __guerrero_cv(self, lmbda, x, R, **kwargs):
+        """
+        Computes guerrero's coefficient of variation. If no seasonality
+        is present in the data, R is set to 2 (p. 40, comment).
+
+        NOTE: Seasonality-specific auxiliaries *should* provide their own
+        seasonality parameter.
+        """
+        nobs = len(x)
+        groups = int(nobs / R)
+
+        # remove the first n < R observations from consideration.
+        grouped_data = np.reshape(x[nobs - (groups * R): nobs], (R, groups))
+
+        mean = np.mean(grouped_data, 1)
+        sd = np.std(grouped_data, 1)
+
+        rat = np.divide(sd, np.power(mean, 1 - lmbda))  # eq. 6, p. 40
+        return np.std(rat) / np.mean(rat)
+
+
+if __name__ == "__main__":
+    bc = BoxCox()
+    x = np.arange(1, 100) + np.abs(np.random.rand(99) * 100)
+    print(bc.transform_boxcox(x))
