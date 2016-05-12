54051627d144d03ef052b1e6147a9b8782170626
ssktotoro <kcwang@ymail.com>
2016-05-12 15:16:07 -0400

    fixes undefined var data

--- statsmodels/tsa/tests/test_seasonal.py
+++ statsmodels/tsa/tests/test_seasonal.py
@@ -83,7 +83,7 @@ class TestDecompose:
 
     def test_pandas(self):
         res_add = seasonal_decompose(self.data, freq=4)
-        freq_override_data = self.data.index = DatetimeIndex(start='1/1/1951', periods=len(data), freq='A')
+        freq_override_data = self.data.index = DatetimeIndex(start='1/1/1951', periods=len(self.data), freq='A')
         res_add_override = seasonal_decompose(freq_override_data, freq=4)
         seasonal = [62.46, 86.17, -88.38, -60.25, 62.46, 86.17, -88.38,
                     -60.25, 62.46, 86.17, -88.38, -60.25, 62.46, 86.17,
