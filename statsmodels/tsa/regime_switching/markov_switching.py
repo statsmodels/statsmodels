40049b6ab12a8f110b77b39da6544c9e9ffda347
Chad Fulton <chad@chadfulton.com>
2016-06-07 14:26:27 -0700

    COMPAT: OrderedDict in Py2.6

--- statsmodels/tsa/regime_switching/markov_switching.py
+++ statsmodels/tsa/regime_switching/markov_switching.py
@@ -10,7 +10,7 @@ from __future__ import division, absolute_import, print_function
 import warnings
 import numpy as np
 import pandas as pd
-from collections import OrderedDict
+from statsmodels.compat.collections import OrderedDict
 
 from scipy.misc import logsumexp
 from scipy.stats import norm
