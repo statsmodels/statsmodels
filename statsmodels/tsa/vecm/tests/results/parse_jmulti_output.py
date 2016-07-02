260bc3b7ca64007d36a6e8879d0e09a1853cde16
vegcev <aleksandar.karakas@student.tugraz.at>
2016-07-02 17:42:02 +0200

    parse JMulTi VAR output

--- statsmodels/tsa/vecm/tests/results/parse_jmulti_output.py
+++ statsmodels/tsa/vecm/tests/results/parse_jmulti_output.py
@@ -19,57 +19,73 @@ def load_results_jmulti(dataset):
         file = dataset.__str__()+'_'+source+'_'+deterministic_terms+'.txt'
         file = os.path.join(os.path.dirname(os.path.realpath(__file__)), file)
         # sections in jmulti output:
-        section_header = ["Lagged endogenous term", # Gamma
-                          "Deterministic term",     # c, s, lt
-                          "Loading coefficients",   # alpha
-                          "Estimated cointegration relation", # beta
-                          "VAR REPRESENTATION"]     # end of vecm representation
-        # the following "sections" will serve as key for the corresponding result values
+        section_header = ["Lagged endogenous term",  # Gamma
+                          "Deterministic term",      # c, s, lt
+                          "Loading coefficients",    # alpha
+                          "Estimated cointegration relation",  # beta
+                          "Legend",
+                          "Lagged endogenous term",  # VAR representation
+                          "Deterministic term"]      # VAR representation
+        # the following "sections" will serve as key for the corresponding
+        # result values
         sections = ["Gamma", 
                     "C",     # Here C as well as linear trend coefficients are collected.
                              # Later, the linear trend coefficients are stripped from C.
                     "alpha", 
-                    "beta"]
-        if deterministic_terms == '': 
+                    "beta",
+                    "Legend",
+                    "VAR A",  # VAR parameter matrices
+                    "VAR deterministic"]  # VAR deterministic terms
+        if deterministic_terms == '':
             del(section_header[1])
             del(sections[1])
+            del(section_header[-1])
+            del(sections[-1])
         results = dict.fromkeys(sections)
 
         section = -1
         result = []
-        col_len = 0
-        
-
-        # parse information about \alpha, \beta, \Gamma, and deterministic term coefficients
+        rows = 0
+        started_reading_section = False
+        start_end_mark = "-----"
+        # parse information about \alpha, \beta, \Gamma, deterministic of VECM
+        # and A_i and deterministic of corresponding VAR:
         for line in open(file):
-            if section_header[section+1] in line:
-                section += 1
-                #print("result: "+str(result))
-                #print("### section: "+str(section))
-                if section == 0:
-                    continue
-                results[sections[section-1]] = np.array(result)
-                result = []
-                if section == len(sections):
-                    break
-            regex_number = re.compile("\s-?\d+\.\d+")
-            matrix_col = re.findall(regex_number, line)
-            if matrix_col == []:
-                #print("No values found, continue.")
+            if section == -1 and section_header[section+1] not in line:
                 continue
-            if result == []:
-                col_len = len(matrix_col)
-                result = [[] for i in range(col_len)]
-            for i in range(col_len):
-                #print(i)
-                result[i].append(float(matrix_col[i]))
-        
+            if section < len(section_header)-1 \
+                    and section_header[section+1] in line:  # new section
+                section += 1
+                continue
+            if not started_reading_section:
+                if line.startswith(start_end_mark):
+                    started_reading_section = True
+                continue
+            if started_reading_section:
+                if line.startswith(start_end_mark):
+                    results[sections[section]] = np.array(result)
+                    result = []
+                    started_reading_section = False
+                    continue
+                regex_number = re.compile("\s-?\d+\.\d+")
+                matrix_col = re.findall(regex_number, line)
+                if matrix_col == []:  # No values found in this line==>continue
+                    continue
+                if result == []:
+                    rows = len(matrix_col)
+                    result = [[] for i in range(rows)]
+                for i in range(rows):
+                    # print(i)
+                    result[i].append(float(matrix_col[i]))
+        # delete "Legend"-section of JMulTi:
+        del results["Legend"]
 
-        # parse the information regarding \Sigma_u
+        # parse  information regarding \Sigma_u
         sigmau_file = dataset.__str__()+'_'+source+'_'+deterministic_terms+'_Sigmau'+'.txt'
         sigmau_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), sigmau_file)
         rows_to_parse = 0
-        regex_number = re.compile("\s+\S+e\S+") # all numbers of Sigma_u in notation with e (e.g. 2.283862e-05)
+        # all numbers of Sigma_u in notation with e (e.g. 2.283862e-05)
+        regex_number = re.compile("\s+\S+e\S+")
         sigmau_section_reached = False
         for line in open(sigmau_file):
             if not sigmau_section_reached and not 'Covariance:' in line:
@@ -80,7 +96,6 @@ def load_results_jmulti(dataset):
                 rows_to_parse = len(row) # Sigma_u is quadratic ==> #rows==#cols
                 Sigma_u = np.empty((rows_to_parse, rows_to_parse))
             row = re.findall(regex_number, line)
-            print(row)
             rows_to_parse -= 1
             Sigma_u[rows_to_parse] = row # rows are added in reverse order
             if rows_to_parse == 0:
