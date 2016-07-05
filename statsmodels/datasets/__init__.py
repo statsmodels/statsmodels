cc94ebd1c570dacb955c8488940214c3a8a3346e
vegcev <aleksandar.karakas@student.tugraz.at>
2016-07-05 18:38:14 +0200

    fixed testing problems in Python 2 and maybe found solution for failing test with seasonal terms.

--- statsmodels/datasets/__init__.py
+++ statsmodels/datasets/__init__.py
@@ -3,7 +3,8 @@ Datasets module
 """
 #__all__ = filter(lambda s:not s.startswith('_'),dir())
 from . import (anes96, cancer, committee, ccard, copper, cpunish, elnino,
-               engel, grunfeld, longley, macrodata, modechoice, nile, randhie,
-               scotland, spector, stackloss, star98, strikes, sunspots, fair,
-               heart, statecrime, co2, fertility, china_smoking)
+               engel, grunfeld, interest_inflation, longley, macrodata,
+               modechoice, nile, randhie, scotland, spector, stackloss, 
+               star98, strikes, sunspots, fair, heart, statecrime, co2,
+               fertility, china_smoking)
 from .utils import get_rdataset, get_data_home, clear_data_home, webuse, check_internet
