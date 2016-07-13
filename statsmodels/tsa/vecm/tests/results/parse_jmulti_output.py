27216171174f0610d6a7e4a5a69bf2145453e293
vegcev <aleksandar.karakas@student.tugraz.at>
2016-07-13 20:43:17 +0200

    introducing basic VECMResults class
    - VECMResults offers estimated coefficients, standard errors, t-values, (approximated) p-values, and loglikelihood
    - these values are now all parsed from JMulTi output
    - tests for the newly introduced values were written (still 25/89 are failing but the main reason might be that JMulTi is treating constant deterministic terms differently (in the cointegration relation). The option for treating constants in the same way as JMulTi should be implemented next.

--- statsmodels/tsa/vecm/tests/results/parse_jmulti_output.py
+++ statsmodels/tsa/vecm/tests/results/parse_jmulti_output.py
@@ -4,16 +4,45 @@ import os
 import numpy as np
 
 
-deterministic_terms_list = ['', 'c', 'cs', 'clt', 'lt']
+deterministic_terms_list = ["", "c", "cs", "clt", "lt"]
+
+debug_mode = True
+def print_debug_output(results, dt):
+        print("\n\n\nDETERMINISTIC TERMS: " + dt)
+        alpha = results["est"]["alpha"]
+        print("alpha:")
+        print(str(type(alpha)) + str(alpha.shape))
+        print(alpha)
+        print("se: ")
+        print(results["se"]["alpha"])
+        print("t: ")
+        print(results["t"]["alpha"])
+        print("p: ")
+        print(results["p"]["alpha"])
+        beta = results["est"]["beta"]
+        print("beta:")
+        print(str(type(beta)) + str(beta.shape))
+        print(beta)
+        Gamma = results["est"]["Gamma"]
+        print("Gamma:")
+        print(str(type(Gamma)) + str(Gamma.shape))
+        print(Gamma)
+        if dt:
+            C = results["est"]["C"]
+            print("C:")
+            print(str(type(C)) + str(C.shape))
+            print(C)
+            print("se: ")
+            print(results["se"]["C"])
 
 
 def load_results_jmulti(dataset):
-    source = 'jmulti'
+    source = "jmulti"
 
     results_for_different_det_terms = dict.fromkeys(deterministic_terms_list)
         
-    for deterministic_terms in deterministic_terms_list:
-        file = dataset.__str__()+'_'+source+'_'+deterministic_terms+'.txt'
+    for dt in deterministic_terms_list:
+        file = dataset.__str__()+"_"+source+"_"+dt+".txt"
         file = os.path.join(os.path.dirname(os.path.realpath(__file__)), file)
         # sections in jmulti output:
         section_header = ["Lagged endogenous term",  # Gamma
@@ -26,22 +55,33 @@ def load_results_jmulti(dataset):
         # the following "sections" will serve as key for the corresponding
         # result values
         sections = ["Gamma", 
-                    "C",     # Here C as well as linear trend coefficients are collected.
-                             # Later, the linear trend coefficients are stripped from C.
-                    "alpha", 
+                    "C",     # Here all deterministic term coefficients are
+                             # collected. (const and linear trend which belong
+                             # to cointegration relation as well as seasonal
+                             # components which are outside the cointegration
+                             # relation. Later, we will strip the terms related
+                             # to the cointegration relation from C.
+                    "alpha",
                     "beta",
                     "Legend",
                     "VAR A",  # VAR parameter matrices
                     "VAR deterministic"]  # VAR deterministic terms
-        if deterministic_terms == '':
+        if dt == "":
             del(section_header[1])
             del(sections[1])
             del(section_header[-1])
             del(sections[-1])
-        results = dict.fromkeys(sections)
-
+        results = dict()
+        results["est"] = dict.fromkeys(sections)
+        results["se"] = dict.fromkeys(sections)
+        results["t"] = dict.fromkeys(sections)
+        results["p"] = dict.fromkeys(sections)
         section = -1
         result = []
+        result_se = []
+        result_t = []
+        result_p = []
+
         rows = 0
         started_reading_section = False
         start_end_mark = "-----"
@@ -60,55 +100,95 @@ def load_results_jmulti(dataset):
                 continue
             if started_reading_section:
                 if line.startswith(start_end_mark):
-                    results[sections[section]] = np.array(result)
+                    if result == []:  # no results collected in section "Legend"
+                        started_reading_section = False
+                        continue
+                    results["est"][sections[section]] = np.column_stack(result)
                     result = []
+                    results["se"][sections[section]] = np.column_stack(result_se)
+                    result_se = []
+                    results["t"][sections[section]] = np.column_stack(result_t)
+                    result_t = []
+                    results["p"][sections[section]] = np.column_stack(result_p)
+                    result_p = []
                     started_reading_section = False
                     continue
-                regex_number = re.compile("\s-?\d+\.\d+")
-                matrix_col = re.findall(regex_number, line)
-                if matrix_col == []:  # No values found in this line==>continue
-                    continue
-                if result == []:
-                    rows = len(matrix_col)
-                    result = [[] for i in range(rows)]
-                for i in range(rows):
-                    result[i].append(float(matrix_col[i]))
+                str_number = "-?\d+\.\d{3}"
+                regex_est = re.compile(str_number + "[^\)\]\}]")
+                est_col = re.findall(regex_est, line)
+                # standard errors in parantheses in JMulTi output:
+                regex_se = re.compile("\(" + str_number + "\)")
+                se_col = re.findall(regex_se, line)
+                # t-values in brackets in JMulTi output:
+                regex_t_value = re.compile("\[" + str_number + "\]")
+                t_col = re.findall(regex_t_value, line)
+                # p-values in braces in JMulTi output:
+                regex_p_value = re.compile("\{" + str_number + "\}")
+                p_col = re.findall(regex_p_value, line)
+                if result == [] and est_col != []:
+                    rows = len(est_col)
+                if est_col != []:
+                    est_col = [float(el) for el in est_col]
+                    result.append(est_col)
+                elif se_col != []:
+                    for i in range(rows):
+                        se_col[i] = se_col[i].replace("(", "").replace(")", "")
+                    se_col = [float(el) for el in se_col]
+                    result_se.append(se_col)
+                elif t_col != []:
+                    for i in range(rows):
+                        t_col[i] = t_col[i].replace("[", "").replace("]", "")
+                    t_col = [float(el) for el in t_col]
+                    result_t.append(t_col)
+                elif p_col != []:
+                    for i in range(rows):
+                        p_col[i] = p_col[i].replace("{", "").replace("}", "")
+                    p_col = [float(el) for el in p_col]
+                    result_p.append(p_col)
         # delete "Legend"-section of JMulTi:
-        del results["Legend"]
-
+        del results["est"]["Legend"]
+        del results["se"]["Legend"]
+        del results["t"]["Legend"]
+        del results["p"]["Legend"]
         # parse  information regarding \Sigma_u
-        sigmau_file = dataset.__str__()+'_'+source+'_'+deterministic_terms+'_Sigmau'+'.txt'
+        sigmau_file = dataset.__str__()+"_"+source+"_"+dt+"_Sigmau"+".txt"
         sigmau_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), sigmau_file)
         rows_to_parse = 0
         # all numbers of Sigma_u in notation with e (e.g. 2.283862e-05)
-        regex_number = re.compile("\s+\S+e\S+")
+        regex_est = re.compile("\s+\S+e\S+")
         sigmau_section_reached = False
         for line in open(sigmau_file):
-            if not sigmau_section_reached and not 'Covariance:' in line:
+            if line.startswith("Log Likelihood:"):
+                line = line.split("Log Likelihood:")[1]
+                results["log_like"] = float(re.findall(regex_est, line)[0])
+            if not sigmau_section_reached and not "Covariance:" in line:
                 continue
-            if 'Covariance:' in line:
+            if "Covariance:" in line:
                 sigmau_section_reached = True
-                row = re.findall(regex_number, line)
+                row = re.findall(regex_est, line)
                 rows_to_parse = len(row)  # Sigma_u quadratic ==> #rows==#cols
                 Sigma_u = np.empty((rows_to_parse, rows_to_parse))
-            row = re.findall(regex_number, line)
+            row = re.findall(regex_est, line)
             rows_to_parse -= 1
             Sigma_u[rows_to_parse] = row # rows are added in reverse order
             if rows_to_parse == 0:
                 break
-        results["Sigma_u"] = Sigma_u[::-1]
+        results["est"]["Sigma_u"] = Sigma_u[::-1]
 
-        results_for_different_det_terms[deterministic_terms] = results
-        if 'lt' in deterministic_terms:
-            C = results_for_different_det_terms[deterministic_terms]["C"]
-            lt = (C[:,-1])[:, None] # take last col and make it 2-dimensional
+        if debug_mode:
+            print_debug_output(results, dt)
+
+        results_for_different_det_terms[dt] = results
+        if 'lt' in dt:
+            C = results_for_different_det_terms[dt]["est"]["C"]
+            lt = (C[:, -1])[:, None]  # take last col and make it 2-dimensional
             C = C[:, :-1]
             if C.shape[1] == 0:
-                del results_for_different_det_terms[deterministic_terms]["C"]
+                del results_for_different_det_terms[dt]["est"]["C"]
             else:
-                results_for_different_det_terms[deterministic_terms]["C"] = C
-            results_for_different_det_terms[deterministic_terms]["lin_trend"] = lt
-    
+                results_for_different_det_terms[dt]["est"]["C"] = C
+            results_for_different_det_terms[dt]["est"]["lin_trend"] = lt
+
     return results_for_different_det_terms
 
 if __name__ == "__main__":
