e4d419e205483312d1f696c481ac5e60c4ca892f
vegcev <aleksandar.karakas@student.tugraz.at>
2016-07-26 22:36:24 +0200

    Add det. terms inside coint. relation, fix Python 2.6 bug, 224/226 tests pass
    - Deterministic terms are now declared by passing the following strings as
      argument:
        -- "co" and "ci" for constant terms outside and inside the cointegration
           respectively. "ci" was called "cc" until the last commit but "cc"
           would imply that a linear trend is called "lc" which would have the
           disadvantage of finding "cc" in the string "lcco" when the user
           actually wants to declare "lc" (see below) and "co" as deterministic
           terms.
        -- "lo" and "li" for linear trends outside and inside the cointegration
           relation respectively. "li" is new.
    - Reference output for deterministic terms was added. (I have found the option
      in JMulTi to have deterministic terms inside the cointegration relation.)
    - Based on this reference output, new test cases were written. Currently the
      following deterministic terms combinations are tested:
        -- "", "co", "colo", "ci", "cili" and
        -- "", "co", "colo", "ci", "cili" all with deterministic seasonal terms.
      Now: 226 tests of which 2 are failing.
    - The Python 2.6 specific error which was the only the only fail after the
      last push should be gone now (data import for testing has been rewritten).
      (The last push solved the problems in Python 2.7 but one problem with
      Python 2.6 had remained obviously.)

--- statsmodels/datasets/interest_inflation/data.py
+++ statsmodels/datasets/interest_inflation/data.py
@@ -1,36 +1,39 @@
 """(West) German interest and inflation rate 1972-1998"""
 
+from numpy import recfromtxt, column_stack, array
+from pandas import DataFrame
+
+from statsmodels.datasets.utils import Dataset
+from os.path import dirname, abspath, pardir, join
+
 __docformat__ = 'restructuredtext'
 
-COPYRIGHT   = """...""" # TODO
-TITLE       = __doc__
-SOURCE      = """
+COPYRIGHT = """..."""  # TODO
+TITLE = __doc__
+SOURCE = """
 http://www.jmulti.de/download/datasets/e6.dat
 """
 
-DESCRSHORT  = """(West) German interest and inflation rate 1972Q2 - 1998Q4"""
+DESCRSHORT = """(West) German interest and inflation rate 1972Q2 - 1998Q4"""
 
-DESCRLONG   = """West German (until 1990) / German (afterwards) interest and inflation rate 1972Q2 - 1998Q4"""
+DESCRLONG = """West German (until 1990) / German (afterwards) interest and
+inflation rate 1972Q2 - 1998Q4
+"""
 
 
-NOTE        = """::
+NOTE = """::
     Number of Observations - 107
 
     Number of Variables - 2
 
     Variable name definitions::
 
-        year      - 1972q1 - 1998q4
+        year      - 1972q2 - 1998q4
         quarter   - 1-4
         Dp        - Delta log gdp deflator
         R         - nominal long term interest rate
 """
 
-from numpy import recfromtxt, column_stack, array
-from pandas import DataFrame
-
-from statsmodels.datasets.utils import Dataset
-from os.path import dirname, abspath
 
 def load():
     """
@@ -43,24 +46,28 @@ def load():
 
     Notes
     -----
-    The interest_inflation Dataset instance does not contain endog and exog attributes.
+    The interest_inflation Dataset instance does not contain endog and exog
+    attributes.
     """
     data = _get_data()
     names = data.dtype.names
     dataset = Dataset(data=data, names=names)
     return dataset
 
+
 def load_pandas():
     dataset = load()
     dataset.data = DataFrame(dataset.data)
     return dataset
 
+
 def _get_data():
     filepath = dirname(abspath(__file__))
-    with open(filepath + '/E6.csv', 'rb') as f:
+    with open(join(filepath, 'E6.csv'), 'rb') as f:
         data = recfromtxt(f, delimiter=",",
                           names=True, dtype=float)
         return data
+
         
 def __str__():
     return "e6"
