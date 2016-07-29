9c91733eec0b90396322f6fc7ce884954f5aea90
Brendan Condon <brendan.condon@gmail.com>
2016-07-29 19:46:00 +0100

    Checks for col or row vector and transposes to maintain consistency.

--- statsmodels/tools/data_interface.py
+++ statsmodels/tools/data_interface.py
@@ -7,6 +7,7 @@ from patsy.design_info import DesignMatrix
 NUMPY_TYPES = [np.ndarray, np.float64]
 PANDAS_TYPES = [pd.Series, pd.DataFrame]
 
+
 class DataInterface(object):
 
     def __init__(self, permitted_types, internal_type=np.ndarray, data=None, external_type=None, model=None,
@@ -14,11 +15,15 @@ class DataInterface(object):
 
         self.permitted_types = permitted_types
         self.internal_type = internal_type
-        self.columns = getattr(data, 'columns', None)
-        self.name = getattr(data, 'name', None)
-        self.ndim = getattr(data, 'ndim', None)
         self.model = model
         self.use_formula = use_formula
+
+        self.columns = None
+        self.name = None
+        self.dtype = None
+        self.index = None
+        self.ndim = None
+        self.is_col_vector = None
 
         if external_type is not None:
             self.external_type = external_type
@@ -29,21 +34,45 @@ class DataInterface(object):
         else:
             self.external_type = np.ndarray
 
-        self.dtype = get_dtype(data)
+    def init_data_interface(self, data):
+
+        self.columns = getattr(data, 'columns', None)
+        self.name = getattr(data, 'name', None)
+        self.dtype = get_dtypes(data)
+        self.ndim = get_ndim(data)
+        self.is_col_vector = is_col_vector(data)
+
+    def to_transpose(self, data):
+
+        data_ndim = get_ndim(data)
+        data_col_vector = is_col_vector(data)
+
+        if self.ndim == 1 and data_ndim == 1:
+            if self.is_col_vector == data_col_vector:
+                return False
+            else:
+                return True
+        else:
+            return False
 
     def to_statsmodels(self, data):
 
         if data is None:
             return None
+        else:
+            self.init_data_interface(data)
 
-        elif self.use_formula and self.model is not None and hasattr(self.model, 'formula'):
+        if self.use_formula and self.model is not None and hasattr(self.model, 'formula'):
             return dmatrix(self.model.data.design_info.builder, data)
 
         elif type(data) in self.permitted_types:
             return data
 
         elif self.internal_type == np.ndarray:
-             return self.to_numpy_array(data)
+            return self.to_numpy_array(data)
+
+        elif self.internal_type in PANDAS_TYPES:
+            self.to_pandas(data)
 
         else:
             raise TypeError('Type conversion to {} from {} is not possible.'.format(self.internal_type, type(data)))
@@ -51,6 +80,7 @@ class DataInterface(object):
     def from_statsmodels(self, data):
 
         from_type = type(data)
+        self.index = getattr(data, 'index', None)
 
         if data is None:
             return None
@@ -59,10 +89,20 @@ class DataInterface(object):
             return data
 
         elif from_type in NUMPY_TYPES:
-            return self.from_numpy_array(data)
+            data_to_return = self.from_numpy_array(data)
+
+            if self.to_transpose(data_to_return):
+                return data_to_return.squeeze()
+            else:
+                return data_to_return
 
         elif from_type in PANDAS_TYPES:
-            return self.from_pandas(data)
+            data_to_return = self.from_pandas(data)
+
+            if self.to_transpose(data_to_return):
+                return
+            else:
+                return data_to_return
 
         else:
             raise TypeError('Type conversion from {} to {} is not possible.'.format(from_type, self.external_type))
@@ -90,9 +130,8 @@ class DataInterface(object):
         else:
             try:
                 return np.asarray(data)
-            except:
+            except TypeError:
                 TypeError('Type conversion to numpy from {} is not possible.'.format(from_type))
-
 
     def to_pandas(self, data):
 
@@ -105,9 +144,10 @@ class DataInterface(object):
             np_data = self.to_numpy_array(data)
 
             if np_data.ndim == 1:
-                return pd.Series(np_data, name=self.name)
+                return pd.Series(np_data, name=self.name, dtype=self.dtype)
+
             else:
-                return pd.DataFrame(np_data, columns=self.columns)
+                return pd.DataFrame(np_data, columns=self.columns, dtype=self.dtype)
 
     def from_numpy_array(self, data):
 
@@ -130,13 +170,15 @@ class DataInterface(object):
             ndim = getattr(data, 'ndim', None)
 
             if ndim in [1, None]:
-                index = getattr(data, 'index', None)
+                return pd.Series(data=data, index=self.index, dtype=self.dtype)
 
-                return pd.Series(data=data, index=index)
             elif self.ndim == ndim:
-                return pd.DataFrame(data=data, columns=self.columns, dtype=self.dtype)
+                df = pd.DataFrame(data=data, columns=self.columns, index=self.index)
+                return apply_dtype_to_df(df, self.dtype)
+
             else:
-                return pd.DataFrame(data=data, dtype=self.dtype)
+                df = pd.DataFrame(data=data, dtype=self.dtype, index=self.index)
+                return apply_dtype_to_df(df, self.dtype)
 
         else:
             return data
@@ -149,49 +191,133 @@ class DataInterface(object):
             return data
 
         elif self.external_type == np.ndarray:
-            return  data.values
+            return data.values
 
         elif from_type == pd.Series and self.external_type == pd.DataFrame:
             return data.to_frame()
 
         elif from_type == pd.DataFrame and self.external_type == pd.Series:
             if data.ndim == 1:
-                return pd.Series(data.values, index=data.index, name=data.columns[0])
+                return pd.Series(data.values, index=data.index, name=data.columns[0], dtype=self.dtype)
             else:
-                raise TypeError('Cannot convert multi dimentional DataFrame to a Series')
+                raise TypeError('Cannot convert multi dimensional DataFrame to a Series')
 
         elif self.external_type == list:
             return data.values.tolist()
+
+        else:
+            return data
 
 
 NumPyInterface = partial(DataInterface, [np.ndarray])
 PandasInterface = partial(DataInterface, [np.ndarray, pd.Series, pd.DataFrame])
 
-def get_dtype(data):
+
+def get_ndim(data):
+    if type(data) == pd.Series:
+        return 1
+
+    if type(data) == pd.DataFrame:
+        if safe_get(data.shape, 1, None) == 1 and safe_get(data.shape, 0, None) > 1:
+            return 1
+
+        elif safe_get(data.shape, 1, None) > 1 and safe_get(data.shape, 0, None) == 1:
+            return 1
+
+        else:
+            return data.ndim
+
+    try:
+        data = np.asarray(data)
+        data_ndim = data.ndim
+        data_squeeze_ndim = data.squeeze().ndim
+
+        if data_ndim > 1 and data_squeeze_ndim == 1:
+            return 1
+        else:
+            return data_ndim
+
+    except:
+        return None
+
+
+def is_col_vector(data):
+    if type(data) == pd.Series:
+        return False
+
+    if type(data) == pd.DataFrame:
+        if safe_get(data.shape, 1, None) == 1:
+            return True
+        else:
+            return False
+
+    try:
+        data = np.asarray(data)
+        data_ndim = data.ndim
+        data_squeeze_ndim = data.squeeze().ndim
+
+        if data_ndim > 1 and data_squeeze_ndim == 1:
+            return True
+        else:
+            return False
+    except:
+        raise ValueError('Cannot determine if the vector is a column')
+
+
+def transpose(data):
+
+    transpose_type = type(data)
+
+    if is_col_vector(data):
+
+        if transpose_type == np.ndarray:
+            return data.squeeze()
+
+        elif transpose_type == pd.DataFrame:
+            return data.T.squeeze()
+
+        else:
+            raise TypeError('Cannot transpose {} into a row vector'.format(transpose_type))
+
+    else:
+
+        if transpose_type == np.ndarray:
+            return data[np.newaxis].T
+
+        elif transpose_type == pd.Series:
+            data_col = data.values[np.newaxis].T
+            return pd.DataFrame(data_col, index=data.index)
+
+        elif transpose_type == pd.DataFrame:
+            return data.T
+
+        else:
+            raise TypeError('Cannot transpose {} into a column vector'.format(transpose_type))
+
+
+def safe_get(data, index, default):
+    try:
+        return data[index]
+
+    except IndexError:
+        return default
+
+
+def get_dtypes(data):
 
     if hasattr(data, 'dtype'):
         return data.dtype
+
     elif hasattr(data, 'dtypes'):
         return data.dtypes
+
     else:
         return None
 
-def is_1d(data):
 
-    try:
-        if np.asarray(data).ndim > 1 and np.asarray(data).squeeze().ndim == 1:
-            return False
-        elif np.asarray(data).ndim == 1:
-            return True
-    except:
-        pass
+def apply_dtype_to_df(df, dtypes):
 
-    try:
-        if data.ndim > 1:
-            return False
-        elif data.ndim == 1:
-            return True
-    except:
-        pass
+    for col, dtype in zip(df.columns, dtypes.values):
+        df[col] = df[col].astype(dtype)
 
-    raise ValueError('Cannot determine number of dimensions')
+    return df
--- statsmodels/tools/tests/test_data_interface.py
+++ statsmodels/tools/tests/test_data_interface.py
@@ -1,5 +1,8 @@
-from statsmodels.tools.data_interface import NumPyInterface
+
+from statsmodels.tools.data_interface import NumPyInterface, get_ndim, is_col_vector, transpose
+from pandas.util.testing import assert_frame_equal
 import pandas as pd
+import numpy as np
 
 
 def test_list_numpy():
@@ -30,3 +33,65 @@ def test_data_frame_numpy():
     numpy_to_data_frame = data_frame_interface.from_statsmodels(data_frame_to_numpy)
 
     assert data_frame.equals(numpy_to_data_frame)
+
+def test_ndim():
+
+    list_row_vector = [1, 2, 3]
+    list_col_vector = [[1], [2], [3]]
+    list_matrix = [[1, 2, 3], [4, 5, 6]]
+
+    np_row_vector = np.array(list_row_vector)
+    np_col_vector = np.array(list_col_vector)
+    np_matrix = np.array(list_matrix)
+
+    pd_row_vector = pd.Series(np_row_vector)
+    pd_row_vector2 = pd.DataFrame([np_row_vector])
+    pd_col_vector = pd.DataFrame(np_col_vector)
+    pd_matrix = pd.DataFrame(np_matrix)
+
+
+    assert get_ndim(list_row_vector) == 1
+    assert get_ndim(list_col_vector) == 1
+    assert get_ndim(list_matrix) == 2
+
+    assert get_ndim(np_row_vector) == 1
+    assert get_ndim(np_col_vector) == 1
+    assert get_ndim(np_matrix) == 2
+
+    assert get_ndim(pd_row_vector) == 1
+    assert get_ndim(pd_row_vector2) == 1
+    assert get_ndim(pd_col_vector) == 1
+    assert get_ndim(pd_matrix) == 2
+
+    assert is_col_vector(list_row_vector) == False
+    assert is_col_vector(list_col_vector) == True
+    assert is_col_vector(list_matrix) == False
+
+    assert is_col_vector(np_row_vector) == False
+    assert is_col_vector(np_col_vector) == True
+    assert is_col_vector(np_matrix) == False
+
+    assert is_col_vector(pd_row_vector) == False
+    assert is_col_vector(pd_row_vector2) == False
+    assert is_col_vector(pd_col_vector) == True
+    assert is_col_vector(pd_matrix) == False
+
+def test_transpose():
+
+    list_row_vector = [1, 2, 3]
+    list_col_vector = [[1], [2], [3]]
+
+    np_row_vector = np.array(list_row_vector, dtype=np.int64)
+    np_col_vector = np.array(list_col_vector, dtype=np.int64)
+
+    pd_row_vector = pd.Series(np_row_vector)
+    pd_row_vector2 = pd.DataFrame([np_row_vector])
+    pd_col_vector = pd.DataFrame(np_col_vector)
+
+    np.testing.assert_equal(transpose(np_row_vector), np_col_vector)
+    np.testing.assert_equal(transpose(np_col_vector), np_row_vector)
+
+    assert pd_row_vector.equals(transpose(pd_col_vector))
+    assert pd_col_vector.equals(transpose(pd_row_vector))
+
+    assert_frame_equal(pd_col_vector, transpose(pd_row_vector2))
