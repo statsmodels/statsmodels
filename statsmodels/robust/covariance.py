bdfa449a378f8f6c957cafc3e78d06cd66811e02
Josef <josef.pktd@gmail.com>
2016-10-08 16:56:37 -0400

    ENH: add Tyler robust covariance (experimental version)

--- statsmodels/robust/covariance.py
+++ statsmodels/robust/covariance.py
@@ -3,10 +3,39 @@
 Author: Josef Perktold
 License: BSD-3
 
+
+---  for Tyler
+copied from "M:\josef\eclipsegworkspace\statsmodels-git\local_scripts\local_scripts\try_cov_tyler.py"
+
+Created on Tue Nov 18 11:53:19 2014
+
+Author: Josef Perktold
+License: BSD-3
+
+
+based on iteration in equ. (14) in Soloveychik and Wiesel
+
+Soloveychik, I., and A. Wiesel. 2014. “Tyler’s Covariance Matrix Estimator in
+Elliptical Models With Convex Structure.”
+IEEE Transactions on Signal Processing 62 (20): 5251–59.
+doi:10.1109/TSP.2014.2348951.
+
+see also related articles by Frahm (which are not easy to read,
+too little explanation, too many strange font letters)
+
+shrinkage version is based on article
+
+Chen, Yilun, A. Wiesel, and A.O. Hero. 2011. “Robust Shrinkage Estimation of
+High-Dimensional Covariance Matrices.”
+IEEE Transactions on Signal Processing 59 (9): 4097–4107.
+doi:10.1109/TSP.2011.2138698.
+
+
 """
 
 import numpy as np
 from scipy import linalg, stats
+from statsmodels import robust
 import statsmodels.robust as robust
 
 # shorthand functions
@@ -17,6 +46,8 @@ class Holder(object):
     def __init__(self, **kwds):
         self.__dict__.update(kwds)
 
+
+### GK and OGK ###
 
 def _weight_mean(x, c):
     x = np.asarray(x)
@@ -213,3 +244,252 @@ def cov_ogk(data, maxiter=2, scale_func=mad, cov_func=cov_gk,
                  transf0=transf0)
 
     return res
+
+### Tyler ###
+
+
+
+def cov_tyler(data, start_cov=None, normalize=False, maxiter=100, eps=1e-13):
+    """Tyler's M-estimator for normalized covariance (scatter)
+
+    The underlying (population) mean of the data is assumed to be zero.
+
+    Parameters
+    ----------
+    data : ndarray
+        data array with observations in rows and variables in columns
+    start_cov : None or ndarray
+        starting covariance for iterative solution
+    normalize : bool
+        If True, then the scatter matrix is normalized to have trace equalt
+        to the number of columns in the data.
+    maxiter : int
+        maximum number of iterations to find the solution
+    eps : float
+        convergence criterion. The maximum absolute distance needs to be
+        smaller than eps for convergence.
+
+    Returns
+    -------
+    scatter : ndarray
+        estimate of the scatter matrix
+    iter : int
+        number of iterations used in finding a solution. If iter is less than
+        maxiter, then the iteration converged.
+    """
+    x = np.asarray(data)
+    nobs, k_vars = x.shape
+    kn = k_vars * 1. / nobs
+    if start_cov is not None:
+        c = start_cov
+    else:
+        c = np.diag(robust.mad(x, center=0)**2)
+
+    #Tyler's M-estimator of shape (scatter) matrix
+    for i in range(maxiter):
+        c_inv = np.linalg.pinv(c)
+        c_old = c
+        # this could be vectorized but could use a lot of memory
+        # TODO:  try to work in vectorized batches
+        c = kn * sum(np.outer(xi, xi) / np.inner(xi, c_inv.dot(xi)) for xi in x)
+        diff = np.max(np.abs(c - c_old))
+        if diff < eps:
+            break
+    if normalize:
+        c /= np.trace(c) / k_vars
+
+    return c, i
+
+
+def cov_tyler_regularized(data, start_cov=None, normalize=False,
+                          shrinkage_factor=None,
+                          maxiter=100, eps=1e-13):
+    """Tyler's M-estimator for normalized covariance (scatter)
+
+    The underlying (population) mean of the data is assumed to be zero.
+
+    Parameters
+    ----------
+    data : ndarray
+        data array with observations in rows and variables in columns.
+    start_cov : None or ndarray
+        starting covariance for iterative solution
+    normalize : bool
+        If True, then the scatter matrix is normalized to have trace equalt
+        to the number of columns in the data.
+    shrinkage_factor : None or float in [0, 1]
+        Shrinkage for the scatter estimate. If it is zero, then no shrinkage
+        is performed. If it is None, then the shrinkage factor will be
+        determined by a plugin estimator
+    maxiter : int
+        maximum number of iterations to find the solution
+    eps : float
+        convergence criterion. The maximum absolute distance needs to be
+        smaller than eps for convergence.
+
+    Returns
+    -------
+    scatter : ndarray
+        estimate of the scatter matrix
+    iter : int
+        number of iterations used in finding a solution. If iter is less than
+        maxiter, then the iteration converged.
+    shrinkage_factor : float
+        shrinkage factor that was used in the estimation. This will be the
+        same as the function argument if it was not None.
+
+    Notes
+    -----
+    If the shrinkage factor is None, then a plugin is used as described in
+    Chen and Wiesel 2011. The required trace for a pilot scatter estimate is
+    obtained by the covariance rescaled by MAD estimate for the variance.
+
+    """
+    x = np.asarray(data)
+    nobs, k_vars = x.shape
+    kn = k_vars * 1. / nobs
+
+    # calculate MAD only once if needed
+    if start_cov is None or shrinkage_factor is None:
+        scale_mad = robust.mad(x, center=0)
+
+    corr = None
+    if shrinkage_factor is None:
+        # maybe some things here are redundant
+        xd = x / x.std(0) #scale_mad
+        corr = xd.T.dot(xd)
+        corr * np.outer(scale_mad, scale_mad)
+        corr *= k_vars / np.trace(corr)
+        tr = np.trace(corr.dot(corr))
+
+        n, k = nobs, k_vars
+        # Chen and Wiesel 2011 equation (13)
+        sf = k*k + (1 - 2./k) * tr
+        sf /= (k*k - n*k - 2*n) + (n + 1 + 2. * (n - 1.) / k) * tr
+        shrinkage_factor = sf
+
+    if start_cov is not None:
+        c = start_cov
+    else:
+        c = np.diag(scale_mad**2)
+
+    identity = np.eye(k_vars)
+
+    for i in range(maxiter):
+        c_inv = np.linalg.pinv(c)
+        c_old = c
+        # this could be vectorized but could use a lot of memory
+        # TODO:  try to work in vectorized batches
+        c0 = kn * sum(np.outer(xi, xi) / np.inner(xi, c_inv.dot(xi)) for xi in x)
+        if shrinkage_factor != 0:
+            c = (1 - shrinkage_factor) * c0 + shrinkage_factor * identity
+        else:
+            c = c0
+
+        c *= k_vars / np.trace(c)
+
+        diff = np.max(np.abs(c - c_old))
+        if diff < eps:
+            break
+
+    return c, i, shrinkage_factor, corr
+
+
+def cov_tyler_pairs_regularized(data_iterator, start_cov=None, normalize=False,
+                          shrinkage_factor=None, nobs=None, k_vars=None,
+                          maxiter=100, eps=1e-13):
+    """Tyler's M-estimator for normalized covariance (scatter)
+
+    The underlying (population) mean of the data is assumed to be zero.
+
+    experimental, calculation of startcov and shrinkage factor doesn't work
+    This is intended for cluster robust and HAC covariance matrices that need
+    to iterate over pairs of observations that are correlated.
+
+    Parameters
+    ----------
+    data_iterator : restartable iterator
+        needs to provide three elements xi, xj and w
+    start_cov : None or ndarray
+        starting covariance for iterative solution
+    normalize : bool
+        If True, then the scatter matrix is normalized to have trace equalt
+        to the number of columns in the data.
+    shrinkage_factor : None or float in [0, 1]
+        Shrinkage for the scatter estimate. If it is zero, then no shrinkage
+        is performed. If it is None, then the shrinkage factor will be
+        determined by a plugin estimator
+    maxiter : int
+        maximum number of iterations to find the solution
+    eps : float
+        convergence criterion. The maximum absolute distance needs to be
+        smaller than eps for convergence.
+
+    Returns
+    -------
+    scatter : ndarray
+        estimate of the scatter matrix
+    iter : int
+        number of iterations used in finding a solution. If iter is less than
+        maxiter, then the iteration converged.
+    shrinkage_factor : float
+        shrinkage factor that was used in the estimation. This will be the
+        same as the function argument if it was not None.
+
+    Notes
+    -----
+    If the shrinkage factor is None, then a plugin is used as described in
+    Chen and Wiesel 2011. The required trace for a pilot scatter estimate is
+    obtained by the covariance rescaled by MAD estimate for the variance.
+
+    """
+    x = data_iterator
+    #x = np.asarray(data)
+    #nobs, k_vars = x.shape
+
+    # calculate MAD only once if needed
+    if start_cov is None or shrinkage_factor is None:
+        scale_mad = sm.robust.mad(x, center=0)
+
+    corr = None
+    if shrinkage_factor is None:
+        # maybe some things here are redundant
+        xd = x / x.std(0) #scale_mad
+        corr = xd.T.dot(xd)
+        corr * np.outer(scale_mad, scale_mad)
+        corr *= k_vars / np.trace(corr)
+        tr = np.trace(corr.dot(corr))
+
+        n, k = nobs, k_vars
+        # Chen and Wiesel 2011 equation (13)
+        sf = k*k + (1 - 2./k) * tr
+        sf /= (k*k - n*k - 2*n) + (n + 1 + 2. * (n - 1.) / k) * tr
+        shrinkage_factor = sf
+
+    if start_cov is not None:
+        c = start_cov
+    else:
+        c = np.diag(scale_mad**2)
+
+    identity = np.eye(k_vars)
+    kn = k_vars * 1. / nobs
+    for i in range(maxiter):
+        c_inv = np.linalg.pinv(c)
+        c_old = c
+        # this could be vectorized but could use a lot of memory
+        # TODO:  try to work in vectorized batches
+        # weights is a problem if iterator should be ndarray
+        #c0 = kn * sum(np.outer(xi, xj) / np.inner(xi, c_inv.dot(xj)) for xi, xj in x)
+        c0 = kn * sum(np.outer(xij[0], xij[1]) / np.inner(xij[0], c_inv.dot(xij[1])) for xij in x)
+        if shrinkage_factor != 0:
+            c = (1 - shrinkage_factor) * c0 + shrinkage_factor * identity
+        else:
+            c = c0
+
+        c *= k_vars / np.trace(c)
+
+        diff = np.max(np.abs(c - c_old))
+        if diff < eps:
+            break
+
+    return c, i, shrinkage_factor#, corr
