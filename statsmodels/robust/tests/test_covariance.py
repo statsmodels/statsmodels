1a2d3c9180d0525bd461069a0a39817728f6ba19
Josef <josef.pktd@gmail.com>
2016-10-08 16:56:37 -0400

    ENH: add Tyler robust covariance (experimental version)

--- statsmodels/robust/tests/test_covariance.py
+++ statsmodels/robust/tests/test_covariance.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy import linalg
 from numpy.testing import assert_allclose, assert_equal
 
+from statsmodels import robust
 import statsmodels.robust.covariance as robcov
 
 
@@ -20,7 +21,18 @@ def test_robcov_SMOKE():
     xtx = x.T.dot(x)
     cov_emp = np.cov(x.T)
 
-
+    # GK, OGK
     robcov.cov_gk1(x[:, 0], x[:,2])
     robcov.cov_gk(x)
     robcov.cov_ogk(x)
+
+    # Tyler
+    robcov.cov_tyler(x)
+    robcov.cov_tyler_regularized(x, shrinkage_factor=0.1)
+
+    x2 = np.array([x, x])
+    x2_ = np.rollaxis(x2, 1)
+    robcov.cov_tyler_pairs_regularized(x2_,
+                                start_cov=np.diag(robust.mad(x)**2),
+                                shrinkage_factor=0.1,
+                                nobs=x.shape[0], k_vars=x.shape[1])
