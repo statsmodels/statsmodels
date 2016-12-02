1179a4f9d8e464c17d7f57a547adf41a1c3a57ef
Yichuan Liu <yichuanliu2004@gmail.com>
2016-12-02 17:50:25 -0500

    Repeated measures ANOVA

--- statsmodels/anova/anova.py
+++ statsmodels/anova/anova.py
@@ -2,48 +2,137 @@
 
 Author: Yichuan Liu
 """
-
+import pandas as pd
 import numpy as np
+from statsmodels.regression.linear_model import OLS
 from scipy import stats
+import patsy
+from statsmodels.iolib import summary2
 
-def long_to_wide(x, factor1, factor2=None, subject=None):
-    raise NotImplementedError
 
-def anova_r1(x):
+def _ols_exclude_term(y, x, terms):
     """
-    One-way repeated measures ANOVA
+    Linear regression excluding terms in x
 
-    .. [1] http://www.statisticshell.com/docs/onewayrmhand.pdf
     Parameters
     ----------
-    x : 2-D array
-        Each row is a subject and Each column is a time level
-    factor
-    subject
+    y : array
+        Dependent variable
+    x : patsy.DesignMatrix
+        Independent variables
+    terms : a list of strings
 
     Returns
     -------
+    residual sum of squares and degree of freedom
 
     """
-    ssw = x.var(axis=1).sum() * (x.shape[1] - 1)
-    dfw = x.shape[0]  * (x.shape[1] - 1)
+    term_slices = x.design_info.term_name_slices
+    ind = np.array([True]*x.shape[1])
+    for term in terms:
+        s = term_slices[term]
+        ind[s] = False
+    x = x[:, ind]
+    model = OLS(y, x)
+    results = model.fit()
+    return (results.ssr, results.df_resid)
 
-    ssm = x.mean(axis=0).var() * x.shape[0] * (x.shape[1] - 1)
-    dfm = x.shape[1] - 1
 
-    ssr = ssw - ssm
-    dfr = dfw - dfm
+class ANOVA(object):
+    """
+    ANOVA using least square regression
 
-    msm = ssm / dfm
-    msr = ssr / dfr
+    The full model regression residual sum of squared is
+    used to compared with reduced model for calculating the
+    within subject effect sum of squared
 
-    f = msm / msr
-    pval = stats.f.sf(f, dfm, dfr)
+    Between subject effect is not yet supported
 
-    return (ssm, ssr, f, dfm, dfr, pval)
+    Parameters
+    ----------
+    data : DataFrame
+    dv : string
+        Dependent variable
+    within : a list of string(s)
+        The within-subject factors
+    between : a list of string(s)
+        The between-subject factors
+    subject : string
+        Specify the subject id
 
-def anova_r2(x, factor1, factor2, subject=None):
-    raise NotImplementedError
+    Returns
+    -------
+    ANOVAResults
 
-def anova_b1r1(x, between, within, subject=None):
-    raise NotImplementedError
+    """
+
+    def __init__(self, data, dv, within=None, between=None, subject=None):
+        self.data = data
+        self.dv = dv
+        self.within = within
+        self.between = between
+        if between is not None:
+            raise NotImplementedError('Between subject effect not '
+                                      'yet supported!')
+        self.subject = subject
+
+    def fit(self):
+        factors = self.within + [self.subject]
+        factors = ['C(%s, Sum)' % i for i in factors]
+        y = self.data[self.dv].values
+        x = patsy.dmatrix('*'.join(factors), data=self.data)
+        term_last = [':'.join(factors)]
+        term_slices = x.design_info.term_name_slices
+        ssr, df_resid = _ols_exclude_term(y, x, term_last)
+
+        anova_table = pd.DataFrame(
+            {'F Value':[], 'Num DF':[], 'Den DF':[], 'Pr > F':[]})
+        for key in term_slices:
+            if self.subject not in key and key != 'Intercept':
+                ssr1, df_resid1 = _ols_exclude_term(
+                    y, x, term_last + [key])
+                df1 = df_resid1 - df_resid
+                msm = (ssr1 - ssr) / df1
+                if key == ':'.join(factors[:-1]):
+                    msr = ssr / df_resid
+                    df2 = df_resid
+                else:
+                    ssr1, df_resid1 = _ols_exclude_term(
+                        y, x, term_last + [key +
+                                           ':C(%s, Sum)' % self.subject])
+                    df2 = df_resid1 - df_resid
+                    msr = (ssr1 - ssr) / df2
+
+                F = msm / msr
+                p = stats.f.sf(F, df1, df2)
+                term = key.replace('C(', '').replace(', Sum)', '')
+                anova_table.loc[term, 'F Value'] = F
+                anova_table.loc[term, 'Num DF'] = df1
+                anova_table.loc[term, 'Den DF'] = df2
+                anova_table.loc[term, 'Pr > F'] = p
+        self.anova_table = anova_table.iloc[:, [1, 2, 0, 3]]
+        return ANOVAResults(self)
+
+
+class ANOVAResults(object):
+    """
+    ANOVA results class
+
+    Attributes
+    ----------
+    anova_table : DataFrame
+    """
+    def __init__(self, anova):
+        self.anova_table = anova.anova_table
+
+    def __str__(self):
+        return self.summary().__str__()
+
+    def summary(self, contrast_L=False, transform_M=False):
+        summ = summary2.Summary()
+        summ.add_title('ANOVA')
+        summ.add_df(self.anova_table)
+
+        return summ
+
+
--- statsmodels/anova/tests/test_anova.py
+++ statsmodels/anova/tests/test_anova.py
@@ -1,20 +1,45 @@
 import pandas as pd
-from ..anova import anova_b1r1, anova_r1, anova_r2
+from ..anova import ANOVA
+from numpy.testing import assert_array_almost_equal
 
-X = pd.DataFrame([[1.17, 1.78, 1.29, 1.29],
-                  [1.77, 1.98, 1.99, 1.99],
-                  [1.49, 1.69, 1.79, 1.59],
-                  [0.65, 0.99, 0.69, 1.09],
-                  [1.58, 1.70, 1.89, 1.89],
-                  [3.13, 3.15, 2.99, 3.09],
-                  [2.09, 1.88, 2.09, 2.49],
-                  [0.62, 0.65, 0.65, 0.69],
-                  [5.89, 5.99, 5.99, 6.99],
-                  [4.46, 4.84, 4.99, 5.15]])
+DV = [7, 3, 6, 6, 5, 8, 6, 7,
+      7, 11, 9, 11, 10, 10, 11, 11,
+      8, 14, 10, 11, 12, 10, 11, 12,
+      16, 7, 11, 9, 10, 11, 8, 8,
+      16, 10, 13, 10, 10, 14, 11, 12,
+      24, 29, 10, 22, 25, 28, 22, 24]
+id = [1, 2, 3, 4, 5, 6, 7, 8,
+      1, 2, 3, 4, 5, 6, 7, 8,
+      1, 2, 3, 4, 5, 6, 7, 8,
+      1, 2, 3, 4, 5, 6, 7, 8,
+      1, 2, 3, 4, 5, 6, 7, 8,
+      1, 2, 3, 4, 5, 6, 7, 8]
+A =  ['a','a','a','a','a','a','a','a',
+      'a','a','a','a','a','a','a','a',
+      'a','a','a','a','a','a','a','a',
+      'b','b','b','b','b','b','b','b',
+      'b','b','b','b','b','b','b','b',
+      'b','b','b','b','b','b','b','b']
+B =  ['a','a','a','a','a','a','a','a',
+      'b','b','b','b','b','b','b','b',
+      'c','c','c','c','c','c','c','c',
+      'a','a','a','a','a','a','a','a',
+      'b','b','b','b','b','b','b','b',
+      'c','c','c','c','c','c','c','c']
 
-def test_anova_r1():
+data = pd.DataFrame([id, A, B, DV], index=['id', 'A', 'B', 'DV']).T
+
+
+def test_two_factors_repeated_measures_anova():
     """
-    .. [1] https://ww2.coastal.edu/kingw/statistics/R-tutorials/repeated.html
+    Results reproduces R `ezANOVA` function from library ez
     """
-
-    print(anova_r1(X))
+    dv = 'DV'
+    within  = ['A', 'B']
+    subject = 'id'
+    df = ANOVA(data, 'DV', within=['A', 'B'], subject='id').fit()
+    a = [[1, 7, 40.14159, 3.905263e-04],
+         [2, 14, 29.21739, 1.007549e-05],
+         [2, 14, 17.10545, 1.741322e-04]]
+    assert_array_almost_equal(df.anova_table.iloc[:,[1, 2, 0, 3]].values,
+                              a, decimal=5)
