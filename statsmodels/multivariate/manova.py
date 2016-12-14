4650cad573d266c669685d37ae6d2973045882ef
Yichuan Liu <yichuanliu2004@gmail.com>
2016-12-14 16:12:10 -0500

    Simplify MANOVA

--- statsmodels/multivariate/manova.py
+++ statsmodels/multivariate/manova.py
@@ -10,91 +10,10 @@ from statsmodels.base.model import Model
 import numpy as np
 from numpy.linalg import matrix_rank, qr
 from statsmodels.iolib import summary2
-from .multivariate_ols import _multivariate_test, _hypotheses_doc
+from .multivariate_ols import _multivariate_ols_test, _hypotheses_doc
 from .multivariate_ols import MultivariateTestResults
+from .multivariate_ols import _multivariate_ols_fit
 __docformat__ = 'restructuredtext en'
-
-
-def _manova_fit(x, y):
-    """
-    For a MANOVA problem y = x * params
-    where y is dependent variables, x is independent variables
-    Perform QR decomposition of x to calculate the sums of squares
-    and cross-products (SSCP) of residuals
-    These are necessary for performing MANOVA hypotheses tests
-
-    No acture
-
-    Parameters
-    ----------
-    x : array-like, each column is a independent variable
-    y : array-like, each column is a dependent variable
-
-    Returns
-    -------
-    a tuple of matrices or values necessary for hypotheses testing
-
-    .. [1] ftp://public.dhe.ibm.com/software/analytics/spss/documentation/statistics/20.0/en/client/Manuals/IBM_SPSS_Statistics_Algorithms.pdf
-
-    """
-    nobs, k_endog = y.shape
-    nobs1, k_exog= x.shape
-    if nobs != nobs1:
-        raise ValueError('x(n=%d) and y(n=%d) should have the same number of '
-                         'rows!' % (nobs1, nobs))
-
-    # Calculate the matrices necessary for hypotheses testing
-    df_resid = nobs - k_exog
-
-    q, r = qr(x)
-    u = q.T.dot(y)
-    sscpr = np.subtract(y.T.dot(y), u.T.dot(u))
-    fittedvalues = u
-    return (df_resid, fittedvalues, sscpr)
-
-
-def _manova_test(hypotheses, fit_results, exog_names, endog_names):
-    def fn(L, M):
-        df_resid, u, sscpr = fit_results
-        t1 = L.dot(u)
-        q = matrix_rank(t1)
-        t1 = t1.dot(M)
-        H = t1.T.dot(t1)
-
-        # E = M'(Y'Y - B'(X'X)B)M
-        E = M.T.dot(sscpr).dot(M)
-        return E, H, q, df_resid
-
-    return _multivariate_test(hypotheses, exog_names, endog_names, fn)
-
-_manova_test.__doc__ = (
-    """
-    MANOVA hypotheses testing
-
-    For y = x * params, where y is dependent variables and x is independent
-    variables, testing L * params * M = 0 where L is the contast matrix for
-    hypotheses testing and M is the transformation matrix for transforming the
-    dependent variables in y.
-
-    Algorithm:
-        H = (L * u * M)' * (L * u * M)
-        E = M' * sscpr * M
-
-    .. [1] ftp://public.dhe.ibm.com/software/analytics/spss/documentation/statistics/20.0/en/client/Manuals/IBM_SPSS_Statistics_Algorithms.pdf
-
-    Parameters
-    ----------
-    """ + _hypotheses_doc +
-    """
-    fittedvalues : tuple
-        Output of ``fit_manova``
-    exog_names : a list of string
-    endog_names : a list of string
-
-    Returns
-    -------
-    a dict of manova tables
-    """)
 
 
 class MANOVA(Model):
@@ -105,40 +24,35 @@ class MANOVA(Model):
     Parameters
     ----------
     endog : array-like
-        Dependent variables (DV). A n_sample x n_y_var array where n_sample is
-        the number of observations and n_y_var is the number of DV.
+        Dependent variables. A nobs x k_endog array where nobs is
+        the number of observations and k_endog is the number of dependent
+         variables.
 
     exog : array-like
-        Independent variables (IV). A n_sample x n_x_var array where n is the
-        number of observations and n_x_var is the number of IV. An intercept is
-        not included by default and should be added by the user (models
-        specified using a formula include an intercept by default)
+        Independent variables. A nobs x k_exog array where nobs is the
+        number of observations and k_exog is the number of independent
+        variables. An intercept is not included by default and should be added
+        by the user (models specified using a formula include an intercept by
+        default)
 
     .. [1] ftp://public.dhe.ibm.com/software/analytics/spss/documentation/statistics/20.0/en/client/Manuals/IBM_SPSS_Statistics_Algorithms.pdf
 
     Attributes
     -----------
-    df_resid : float
-        The number of observation `n` minus the number of IV `q`.
-    sscpr : array
-        Sums of squares and cross-products of residuals
     endog : array
         See Parameters.
     exog : array
         See Parameters.
-    design_info : patsy.DesignInfo
-        Contain design info for the independent variables if model is
-        constructed using `from_formula`
 
     """
     def __init__(self, endog, exog, missing='none', hasconst=None, **kwargs):
         super(MANOVA, self).__init__(endog, exog, **kwargs)
-        self.fittedmod = _manova_fit(self.exog, self.endog)
+        self._fittedmod = _multivariate_ols_fit(self.endog, self.exog)
 
     def mv_test(self, hypotheses=None):
         if hypotheses is None:
             if (hasattr(self, 'data') and self.data is not None and
-                        self.data.design_info is not None):
+                        hasattr(self.data, 'design_info')):
                 terms = self.data.design_info.term_name_slices
                 hypotheses = []
                 for key in terms:
@@ -152,8 +66,8 @@ class MANOVA(Model):
                     L[i] = 1
                     hypotheses.append([name, L, None])
 
-        results = _manova_test(hypotheses, self.fittedmod,self.exog_names,
-                               self.endog_names)
+        results = _multivariate_ols_test(hypotheses, self._fittedmod,
+                                         self.exog_names, self.endog_names)
 
         return MultivariateTestResults(results)
     mv_test.__doc__ = (
@@ -170,7 +84,7 @@ class MANOVA(Model):
 
         Returns
         -------
-        results: MANOVAResults
+        results: MultivariateTestResults
 
         """
     )
--- statsmodels/multivariate/tests/test_manova.py
+++ statsmodels/multivariate/tests/test_manova.py
@@ -73,58 +73,6 @@ def test_manova_sas_example():
                         0.4109, decimal=4)
 
 
-def test_compare_r_lm_anova_output_dogs_data():
-    ''' Testing  with 2 between-subject effect
-    Compares with R lm anova output
-    '''
-    data = pd.DataFrame([['Morphine', 'N', .04, .20, .10, .08],
-                         ['Morphine', 'N', .02, .06, .02, .02],
-                         ['Morphine', 'N', .07, 1.40, .48, .24],
-                         ['Morphine', 'N', .17, .57, .35, .24],
-                         ['Morphine', 'Y', .10, .09, .13, .14],
-                         ['placebo', 'Y', .07, .07, .06, .07],
-                         ['placebo', 'Y', .05, .07, .06, .07],
-                         ['placebo', 'N', .03, .62, .31, .22],
-                         ['placebo', 'N', .03, 1.05, .73, .60],
-                         ['placebo', 'N', .07, .83, 1.07, .80],
-                         ['Trimethaphan', 'N', .09, 3.13, 2.06, 1.23],
-                         ['Trimethaphan', 'Y', .10, .09, .09, .08],
-                         ['Trimethaphan', 'Y', .08, .09, .09, .10],
-                         ['Trimethaphan', 'Y', .13, .10, .12, .12],
-                         ['Trimethaphan', 'Y', .06, .05, .05, .05]],
-                        columns=['Drug', 'Depleted', 'Histamine0',
-                                 'Histamine1',
-                                 'Histamine3', 'Histamine5'])
-
-    for i in range(2,6):
-        data.iloc[:, i] = np.log(data.iloc[:, i])
-
-    mod = MANOVA.from_formula(
-        'Histamine0 + Histamine1 + Histamine3 + Histamine5 ~ Drug * Depleted',
-        data)
-    r = mod.mv_test()
-    a = [[6.63472514e-03, 4, 6, 2.24583217e+02, 1.16241802e-06],
-         [9.93365275e-01, 4, 6, 2.24583217e+02, 1.16241802e-06],
-         [1.49722144e+02, 4, 6, 2.24583217e+02, 1.16241802e-06],
-         [1.49722144e+02, 4, 6, 2.24583217e+02, 1.16241802e-06]]
-    assert_array_almost_equal(r['Intercept']['stat'].values, a, decimal=6)
-    a = [[0.09711919, 8., 12.,        3.31325352, 0.03054676],
-         [1.32559483, 8., 14.,        3.43975859, 0.02101755],
-         [4.94409805, 8., 6.63157895, 3.53952474, 0.06115465],
-         [3.79813274, 4., 7.,         6.64673229, 0.01558302]]
-    assert_array_almost_equal(r['Drug']['stat'].values, a, decimal=6)
-    a = [[1.15958333e-01, 4, 6, 1.14356810e+01, 5.69444657e-03],
-         [8.84041667e-01, 4, 6, 1.14356810e+01, 5.69444657e-03],
-         [7.62378732e+00, 4, 6, 1.14356810e+01, 5.69444657e-03],
-         [7.62378732e+00, 4, 6, 1.14356810e+01, 5.69444657e-03]]
-    assert_array_almost_equal(r['Depleted']['stat'].values, a, decimal=6)
-    a = [[0.15234366,  8., 12.,        2.34307678, 0.08894239],
-         [1.13013353,  8., 14.,        2.27360606, 0.08553213],
-         [3.70989596,  8., 6.63157895, 2.65594824, 0.11370285],
-         [3.1145597,   4., 7.,         5.45047947, 0.02582767]]
-    assert_array_almost_equal(r['Drug:Depleted']['stat'].values, a, decimal=6)
-
-
 def test_manova_test_input_validation():
     mod = MANOVA.from_formula('Basal + Occ + Max ~ Loc', data=X)
     hypothesis = [('test', np.array([[1, 1, 1]]), None)]
