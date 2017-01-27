62e5127f879e56459ae23964ec8b559d8979368b
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2017-01-27 20:55:35 +0000

    TST: Switch to pytest
    Switch travis and appveyor to pytest

---
+++ conftest.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption("--skip-slow", action="store_true",
+                     help="skip slow tests")
+    parser.addoption("--only-slow", action="store_true",
+                     help="run only slow tests")
+
+
+def pytest_runtest_setup(item):
+    if 'slow' in item.keywords and item.config.getoption("--skip-slow"):
+        pytest.skip("skipping due to --skip-slow")
+
+    if 'slow' not in item.keywords and item.config.getoption("--only-slow"):
+        pytest.skip("skipping due to --only-slow")
