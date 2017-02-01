fc52175a0b5c2db3066d9eac43e502dbe19e5209
Josef Perktold <josef.pktd@gmail.com>
2017-02-01 15:33:13 -0500

    Merge pull request #3406 from has2k1/fix-prediction-results-no-wieghts
    BUG: Fix GLM prediction with an exog and no weights
    (cherry picked from commit 71da899ffdafa672d15cb99abd5ae2fdffe3bada)

--- statsmodels/genmod/_prediction.py
+++ statsmodels/genmod/_prediction.py
@@ -220,12 +220,15 @@ def get_prediction_glm(self, exog=None, transform=True, weights=None,
 
     link_deriv = self.model.family.link.inverse_deriv(linpred.predicted_mean)
     var_pred_mean = link_deriv**2 * (exog * np.dot(covb, exog.T).T).sum(1)
+    var_resid = self.scale  # self.mse_resid / weights
 
     # TODO: check that we have correct scale, Refactor scale #???
-    var_resid = self.scale / weights # self.mse_resid / weights
     # special case for now:
     if self.cov_type == 'fixed scale':
-        var_resid = self.cov_kwds['scale'] / weights
+        var_resid = self.cov_kwds['scale']
+
+    if weights is not None:
+        var_resid /= weights
 
     dist = ['norm', 't'][self.use_t]
     return PredictionResults(predicted_mean, var_pred_mean, var_resid,
