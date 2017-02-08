348f223a307b059c9f9e7c07c7093dc344a1574e
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2017-02-08 21:08:43 +0000

    CLN: Remove 0.7 deprecations
    Remove all 0.7 scheduled deprecations
    Rmove tests that use deprecated features
    Remove doc links

--- statsmodels/discrete/discrete_model.py
+++ statsmodels/discrete/discrete_model.py
@@ -1142,9 +1142,6 @@ class Poisson(CountModel):
         L = np.exp(np.dot(X,params) + offset + exposure)
         return (self.endog - L)[:,None] * X
 
-    jac = np.deprecate(score_obs, 'jac', 'score_obs', "Use score_obs method."
-                       " jac will be removed in 0.7")
-
     def hessian(self, params):
         """
         Poisson model Hessian matrix of the loglikelihood
@@ -1343,9 +1340,6 @@ class Logit(BinaryModel):
         X = self.exog
         L = self.cdf(np.dot(X, params))
         return (y - L)[:,None] * X
-
-    jac = np.deprecate(score_obs, 'jac', 'score_obs', "Use score_obs method."
-                       " jac will be removed in 0.7")
 
     def hessian(self, params):
         """
@@ -1558,9 +1552,6 @@ class Probit(BinaryModel):
         # clip to get rid of invalid divide complaint
         L = q*self.pdf(q*XB)/np.clip(self.cdf(q*XB), FLOAT_EPS, 1 - FLOAT_EPS)
         return L[:,None] * X
-
-    jac = np.deprecate(score_obs, 'jac', 'score_obs', "Use score_obs method."
-                       " jac will be removed in 0.7")
 
     def hessian(self, params):
         """
@@ -1805,9 +1796,6 @@ class MNLogit(MultinomialModel):
                                                   params))[:,1:]
         #NOTE: might need to switch terms if params is reshaped
         return (firstterm[:,:,None] * self.exog[:,None,:]).reshape(self.exog.shape[0], -1)
-
-    jac = np.deprecate(score_obs, 'jac', 'score_obs', "Use score_obs method."
-                       " jac will be removed in 0.7")
 
     def hessian(self, params):
         """
@@ -2251,9 +2239,6 @@ class NegativeBinomial(CountModel):
     def score_obs(self, params):
         sc = approx_fprime_cs(params, self.loglikeobs)
         return sc
-
-    jac = np.deprecate(score_obs, 'jac', 'score_obs', "Use score_obs method."
-                       " jac will be removed in 0.7")
 
     def fit(self, start_params=None, method='bfgs', maxiter=35,
             full_output=1, disp=1, callback=None,
