065551656b5eab87cd399a97d6439400e687f7f3
Chad Fulton <chad@chadfulton.com>
2017-02-26 15:53:49 -0500

    REF: Replace RecursiveLS ad-hoc concentrate

--- statsmodels/regression/recursive_ls.py
+++ statsmodels/regression/recursive_ls.py
@@ -108,6 +108,9 @@ class RecursiveLS(MLEModel):
         # Use univariate filtering by default
         self.ssm.filter_univariate = True
 
+        # Concentrate the scale out of the likelihood function
+        self.ssm.filter_concentrated = True
+
         # Setup the state space representation
         self['design'] = np.zeros((self.k_endog, self.k_states, self.nobs))
         self['design', 0] = self.exog[:, :, None].T
@@ -118,6 +121,7 @@ class RecursiveLS(MLEModel):
         # Notice that the filter output does not depend on the measurement
         # variance, so we set it here to 1
         self['obs_cov', 0, 0] = 1.
+        self['transition'] = np.eye(self.k_states)
 
         # Linear constraints are technically imposed by adding "fake" endog
         # variables that are used during filtering, but for all model- and
@@ -138,17 +142,12 @@ class RecursiveLS(MLEModel):
         -------
         RecursiveLSResults
         """
-        # Get the smoother results with an arbitrary measurement variance
         smoother_results = self.smooth(return_ssm=True)
-        # Compute the MLE of sigma2 (see Harvey, 1989 equation 4.2.5)
-        d = max(smoother_results.nobs_diffuse, self.loglikelihood_burn)
-        resid = smoother_results.standardized_forecasts_error[0, d:]
-        sigma2 = np.inner(resid, resid) / (self.nobs - d)
 
-        # Now construct a results class, where the params are the final
-        # estimates of the regression coefficients
-        self['obs_cov', 0, 0] = sigma2
-        return self.smooth()
+        with self.ssm.fixed_scale(smoother_results.scale):
+            res = self.smooth()
+
+        return res
 
     def filter(self, return_ssm=False, **kwargs):
         # Get the state space output
