a504883be4bc7fe4c7441b3a7fee6933bb74ee6e
Kerby Shedden <kshedden@umich.edu>
2017-02-27 23:41:03 -0500

    Remove non-ASCII character

--- statsmodels/duration/_kernel_estimates.py
+++ statsmodels/duration/_kernel_estimates.py
@@ -146,8 +146,8 @@ def _kernel_survfunc(time, status, exog, kfunc, freq_weights):
     ----------
     Zeng, Donglin 2004. Estimating Marginal Survival Function by
     Adjusting for Dependent Censoring Using Many Covariates. The
-    Annals of Statistics 32 (4):
-    1533-55. doi:10.1214/009053604000000508.
+    Annals of Statistics 32 (4): 1533 55.
+    doi:10.1214/009053604000000508.
     http://arxiv.org/pdf/math/0409180.pdf
     """
 
