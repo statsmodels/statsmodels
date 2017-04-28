d8f90d59020ee55fd9977759d045bc7ad7ad4123
Brock Mendel <jbrockmendel@gmail.com>
2017-04-28 15:16:53 -0700

    Dont import from numpy, just call into np; add shebang

--- statsmodels/tsa/adfvalues.py
+++ statsmodels/tsa/adfvalues.py
@@ -1,5 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from scipy.stats import norm
-from numpy import array, polyval, inf, asarray
 import numpy as np
 
 __all__ = ['mackinnonp','mackinnoncrit']
@@ -9,7 +10,7 @@ __all__ = ['mackinnonp','mackinnoncrit']
 
 tau_star_nc = [-1.04, -1.53, -2.68, -3.09, -3.07, -3.77]
 tau_min_nc = [-19.04,-19.62,-21.21,-23.25,-21.63,-25.74]
-tau_max_nc = [inf,1.51,0.86,0.88,1.05,1.24]
+tau_max_nc = [np.inf,1.51,0.86,0.88,1.05,1.24]
 tau_star_c = [-1.61, -2.62, -3.13, -3.47, -3.78, -3.93]
 tau_min_c = [-18.83,-18.86,-23.48,-28.07,-25.96,-23.27]
 tau_max_c = [2.74,0.92,0.55,0.61,0.79,1]
@@ -20,14 +21,14 @@ tau_star_ctt = [-3.21,-3.51,-3.81,-3.83,-4.12,-4.63]
 tau_min_ctt = [-17.17,-21.1,-24.33,-24.03,-24.33,-28.22]
 tau_max_ctt = [0.54,0.79,1.08,1.43,3.49,1.92]
 
-small_scaling = array([1,1,1e-2])
+small_scaling = np.array([1,1,1e-2])
 tau_nc_smallp = [ [0.6344,1.2378,3.2496],
                   [1.9129,1.3857,3.5322],
                   [2.7648,1.4502,3.4186],
                   [3.4336,1.4835,3.19],
                   [4.0999,1.5533,3.59],
                   [4.5388,1.5344,2.9807]]
-tau_nc_smallp = asarray(tau_nc_smallp)*small_scaling
+tau_nc_smallp = np.asarray(tau_nc_smallp)*small_scaling
 
 tau_c_smallp = [ [2.1659,1.4412,3.8269],
                  [2.92,1.5012,3.9796],
@@ -35,7 +36,7 @@ tau_c_smallp = [ [2.1659,1.4412,3.8269],
                  [3.9673,1.4777,2.6315],
                  [4.5509,1.5338,2.9545],
                  [5.1399,1.6036,3.4445]]
-tau_c_smallp = asarray(tau_c_smallp)*small_scaling
+tau_c_smallp = np.asarray(tau_c_smallp)*small_scaling
 
 tau_ct_smallp = [ [3.2512,1.6047,4.9588],
                   [3.6646,1.5419,3.6448],
@@ -43,7 +44,7 @@ tau_ct_smallp = [ [3.2512,1.6047,4.9588],
                   [4.5844,1.5338,2.8796],
                   [5.0722,1.5634,2.9472],
                   [5.53,1.5914,3.0392]]
-tau_ct_smallp = asarray(tau_ct_smallp)*small_scaling
+tau_ct_smallp = np.asarray(tau_ct_smallp)*small_scaling
 
 tau_ctt_smallp = [ [4.0003,1.658,4.8288],
                    [4.3534,1.6016,3.7947],
@@ -51,16 +52,16 @@ tau_ctt_smallp = [ [4.0003,1.658,4.8288],
                    [5.214,1.6077,3.3449],
                    [5.6481,1.6274,3.3455],
                    [5.9296,1.5929,2.8223]]
-tau_ctt_smallp = asarray(tau_ctt_smallp)*small_scaling
+tau_ctt_smallp = np.asarray(tau_ctt_smallp)*small_scaling
 
-large_scaling = array([1,1e-1,1e-1,1e-2])
+large_scaling = np.array([1,1e-1,1e-1,1e-2])
 tau_nc_largep = [ [0.4797,9.3557,-0.6999,3.3066],
                   [1.5578,8.558,-2.083,-3.3549],
                   [2.2268,6.8093,-3.2362,-5.4448],
                   [2.7654,6.4502,-3.0811,-4.4946],
                   [3.2684,6.8051,-2.6778,-3.4972],
                   [3.7268,7.167,-2.3648,-2.8288]]
-tau_nc_largep = asarray(tau_nc_largep)*large_scaling
+tau_nc_largep = np.asarray(tau_nc_largep)*large_scaling
 
 tau_c_largep = [ [1.7339,9.3202,-1.2745,-1.0368],
                  [2.1945,6.4695,-2.9198,-4.2377],
@@ -68,7 +69,7 @@ tau_c_largep = [ [1.7339,9.3202,-1.2745,-1.0368],
                  [3.0387,4.5452,-3.3666,-4.1921],
                  [3.5049,5.2098,-2.9158,-3.3468],
                  [3.9489,5.8933,-2.5359,-2.721]]
-tau_c_largep = asarray(tau_c_largep)*large_scaling
+tau_c_largep = np.asarray(tau_c_largep)*large_scaling
 
 tau_ct_largep = [ [2.5261,6.1654,-3.7956,-6.0285],
                   [2.85,5.272,-3.6622,-5.1695],
@@ -76,7 +77,7 @@ tau_ct_largep = [ [2.5261,6.1654,-3.7956,-6.0285],
                   [3.652,5.9758,-2.7483,-3.2081],
                   [4.0712,6.6428,-2.3464,-2.546],
                   [4.4735,7.1757,-2.0681,-2.1196] ]
-tau_ct_largep = asarray(tau_ct_largep)*large_scaling
+tau_ct_largep = np.asarray(tau_ct_largep)*large_scaling
 
 tau_ctt_largep = [ [3.0778,4.9529,-4.1477,-5.9359],
                    [3.4713,5.967,-3.2507,-4.2286],
@@ -84,7 +85,7 @@ tau_ctt_largep = [ [3.0778,4.9529,-4.1477,-5.9359],
                    [4.2736,7.6199,-2.1534,-2.4026],
                    [4.6679,8.2618,-1.822,-1.9147],
                    [5.0009,8.3735,-1.6994,-1.6928]]
-tau_ctt_largep = asarray(tau_ctt_largep)*large_scaling
+tau_ctt_largep = np.asarray(tau_ctt_largep)*large_scaling
 
 
 #NOTE: The Z-statistic is used when lags are included to account for
@@ -104,28 +105,28 @@ z_star_ctt = [-20.7,-25.3,-29.9,-34.4,-38.5,-44.2]
 # p = norm.cdf(d_0 + d_1*log(abs(z)) + d_2*log(abs(z))**2 + d_3*log(abs(z))**3
 # there is no Z-min, ie., it is well-behaved in the left tail
 
-z_nc_smallp = array([[.0342, -.6376,0,-.03872],
+z_nc_smallp = np.array([[.0342, -.6376,0,-.03872],
                     [1.3426,-.7680,0,-.04104],
                     [3.8607,-2.4159,.51293,-.09835],
                     [6.1072,-3.7250,.85887,-.13102],
                     [7.7800,-4.4579,1.00056,-.14014],
                     [4.0253, -.8815,0,-.04887]])
 
-z_c_smallp = array([[2.2142,-1.7863,.32828,-.07727],
+z_c_smallp = np.array([[2.2142,-1.7863,.32828,-.07727],
                    [1.1662,.1814,-.36707,0],
                    [6.6584,-4.3486,1.04705,-.15011],
                    [3.3249,-.8456,0,-.04818],
                    [4.0356,-.9306,0,-.04776],
                    [13.9959,-8.4314,1.97411,-.22234]])
 
-z_ct_smallp = array([ [4.6476,-2.8932,0.5832,-0.0999],
+z_ct_smallp = np.array([ [4.6476,-2.8932,0.5832,-0.0999],
                       [7.2453,-4.7021,1.127,-.15665],
                       [3.4893,-0.8914,0,-.04755],
                       [1.6604,1.0375,-0.53377,0],
                       [2.006,1.1197,-0.55315,0],
                       [11.1626,-5.6858,1.21479,-.15428]])
 
-z_ctt_smallp = array([ [3.6739,-1.1549,0,-0.03947],
+z_ctt_smallp = np.array([ [3.6739,-1.1549,0,-0.03947],
                        [3.9783,-1.0619,0,-0.04394],
                        [2.0062,0.8907,-0.51708,0],
                        [4.9218,-1.0663,0,-0.04691],
@@ -135,8 +136,8 @@ z_ctt_smallp = array([ [3.6739,-1.1549,0,-0.03947],
 # These are well-behaved in the right tail.
 # the approximation function is
 # p = norm.cdf(d_0 + d_1 * z + d_2*z**2 + d_3*z**3 + d_4*z**4)
-z_large_scaling = array([1,1e-1,1e-2,1e-3,1e-5])
-z_nc_largep = array([ [0.4927,6.906,13.2331,12.099,0],
+z_large_scaling = np.array([1,1e-1,1e-2,1e-3,1e-5])
+z_nc_largep = np.array([ [0.4927,6.906,13.2331,12.099,0],
                       [1.5167,4.6859,4.2401,2.7939,7.9601],
                       [2.2347,3.9465,2.2406,0.8746,1.4239],
                       [2.8239,3.6265,1.6738,0.5408,0.7449],
@@ -144,7 +145,7 @@ z_nc_largep = array([ [0.4927,6.906,13.2331,12.099,0],
                       [3.729,3.0611,0.9579,0.2087,0.1943]])
 z_nc_largep *= z_large_scaling
 
-z_c_largep = array([ [1.717,5.5243,4.3463,1.6671,0],
+z_c_largep = np.array([ [1.717,5.5243,4.3463,1.6671,0],
                      [2.2394,4.2377,2.432,0.9241,0.4364],
                      [2.743,3.626,1.5703,0.4612,0.567],
                      [3.228,3.3399,1.2319,0.3162,0.3482],
@@ -152,7 +153,7 @@ z_c_largep = array([ [1.717,5.5243,4.3463,1.6671,0],
                      [4.0379,2.8735,0.7694,0.1433,0.1146]])
 z_c_largep *= z_large_scaling
 
-z_ct_largep = array([ [2.7117,4.5731,2.2868,0.6362,0.5],
+z_ct_largep = np.array([ [2.7117,4.5731,2.2868,0.6362,0.5],
                       [3.0972,4.0873,1.8982,0.5796,0.7384],
                       [3.4594,3.6326,1.4284,0.3813,0.4325],
                       [3.806,3.2634,1.0689,0.2402,0.2304],
@@ -160,7 +161,7 @@ z_ct_largep = array([ [2.7117,4.5731,2.2868,0.6362,0.5],
                       [4.4497,2.7534,0.6582,0.1089,0.0773]])
 z_ct_largep *= z_large_scaling
 
-z_ctt_largep = array([ [3.4671,4.3476,1.9231,0.5381,0.6216],
+z_ctt_largep = np.array([ [3.4671,4.3476,1.9231,0.5381,0.6216],
                        [3.7827,3.9421,1.5699,0.4093,0.4485],
                        [4.052,3.4947,1.1772,0.2642,0.2502],
                        [4.3311,3.1625,0.9126,0.1775,0.1462],
@@ -215,7 +216,7 @@ def mackinnonp(teststat, regression="c", N=1, lags=None):
 #above is only for z stats
     else:
         tau_coef = eval("tau_" + regression + "_largep["+str(N-1)+"]")
-    return norm.cdf(polyval(tau_coef[::-1], teststat))
+    return norm.cdf(np.polyval(tau_coef[::-1], teststat))
 
 # These are the new estimates from MacKinnon 2010
 # the first axis is N -1
@@ -225,7 +226,7 @@ def mackinnonp(teststat, regression="c", N=1, lags=None):
 tau_nc_2010 = [[ [-2.56574,-2.2358,-3.627,0], # N = 1
                  [-1.94100,-0.2686,-3.365,31.223],
                  [-1.61682, 0.2656, -2.714, 25.364]]]
-tau_nc_2010 = asarray(tau_nc_2010)
+tau_nc_2010 = np.asarray(tau_nc_2010)
 
 tau_c_2010 = [[ [-3.43035,-6.5393,-16.786,-79.433], # N = 1, 1%
                 [-2.86154,-2.8903,-4.234,-40.040],  # 5 %
@@ -263,7 +264,7 @@ tau_c_2010 = [[ [-3.43035,-6.5393,-16.786,-79.433], # N = 1, 1%
               [ [-6.63790,-50.2095,-124.156,579.622],# N = 12
                 [-6.11279,-36.2681,-32.505,314.802],
                 [-5.83724,-29.9864,-2.686,184.116]]]
-tau_c_2010 = asarray(tau_c_2010)
+tau_c_2010 = np.asarray(tau_c_2010)
 
 tau_ct_2010 = [[ [-3.95877,-9.0531,-28.428,-134.155],   # N = 1
                  [-3.41049,-4.3904,-9.036,-45.374],
@@ -301,7 +302,7 @@ tau_ct_2010 = [[ [-3.95877,-9.0531,-28.428,-134.155],   # N = 1
                [ [-6.83488,-54.7119,-139.800,736.376],  # N = 12
                  [-6.31127,-39.9676,-37.021,406.051],
                  [-6.03650,-33.2381,-6.606,317.776]]]
-tau_ct_2010 = asarray(tau_ct_2010)
+tau_ct_2010 = np.asarray(tau_ct_2010)
 
 tau_ctt_2010 = [[ [-4.37113,-11.5882,-35.819,-334.047], # N = 1
                   [-3.83239,-5.9057,-12.490,-118.284],
@@ -339,7 +340,7 @@ tau_ctt_2010 = [[ [-4.37113,-11.5882,-35.819,-334.047], # N = 1
                 [ [-7.02582,-59.6037,-166.368,989.879], # N = 12
                   [-6.50353,-44.0797,-47.242,543.889],
                   [-6.22941,-36.9673,-10.868,418.414]]]
-tau_ctt_2010 = asarray(tau_ctt_2010)
+tau_ctt_2010 = np.asarray(tau_ctt_2010)
 
 tau_2010s = {
   'nc': tau_nc_2010,
@@ -348,7 +349,7 @@ tau_2010s = {
   'ctt': tau_ctt_2010,
   }
 
-def mackinnoncrit(N=1, regression ="c", nobs=inf):
+def mackinnoncrit(N=1, regression ="c", nobs=np.inf):
     """
     Returns the critical values for cointegrating and the ADF test.
 
@@ -386,7 +387,7 @@ def mackinnoncrit(N=1, regression ="c", nobs=inf):
     reg = regression
     if reg not in ['c', 'ct', 'nc', 'ctt']:
         raise ValueError("regression keyword %s not understood" % reg)
-    if nobs is inf:
+    if nobs is np.inf:
         tau2010 = tau_2010s[reg][N-1, :, 0]
         return tau2010
     else:
