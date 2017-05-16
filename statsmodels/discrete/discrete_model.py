6a58ea8e8a5d116cbdc1cb0916cdb855469c9483
Brock Mendel <jbrockmendel@gmail.com>
2017-05-16 14:30:13 -0700

    Notice that we have equivalent expressions in nb1 and nb2 blocks; combine them outside the if/else

--- statsmodels/discrete/discrete_model.py
+++ statsmodels/discrete/discrete_model.py
@@ -2084,12 +2084,11 @@ class NegativeBinomial(CountModel):
             # Then a1 is mu/alpha.
             # `prob` then simplifies to 1/(alpha+1)            
             dparams = exog*a1*npdg
-            dalpha = ((y - mu)*prob - a1*npdg).sum() / alpha
         else: # nb2
             # In this case a1 is 1/alpha            
             dparams = exog*a1 * (y-mu)/(mu+a1)
-            dalpha = -(npdg*a1 - (y-mu)*prob).sum() / alpha
-
+        
+        dalpha = -(npdg*a1 - (y-mu)*prob).sum() / alpha
         # Multiply/divide by constants above outside the sum() to reduce
         # floating point error.
         if self._transparams:
