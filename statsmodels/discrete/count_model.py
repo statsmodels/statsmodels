cb844c8c8bae5349ebbda6822317cc3efa4212c9
Evgeny Zhurko <evgeny.zhurko@gmail.com>
2017-06-12 17:55:31 +0300

    Implemented llf for genericZeroInflated model

---
+++ statsmodels/discrete/count_model.py
@@ -0,0 +1,209 @@
+from __future__ import division
+
+__all__ = ["PoissonZeroInflated"]
+
+from statsmodels.discrete.discrete_model import CountModel
+
+class GenericZeroInflated(CountModel):
+    __doc__ = """
+    Generiz Zero Inflated model for count data
+
+    %(params)s
+    %(extra_params)s
+
+    Attributes
+    -----------
+    endog : array
+        A reference to the endogenous response variable
+    exog : array
+        A reference to the exogenous design.
+    """ % {'params' : base._model_params_doc,
+           'extra_params' :
+           """offset : array_like
+        Offset is added to the linear prediction with coefficient equal to 1.
+    exposure : array_like
+        Log(exposure) is added to the linear prediction with coefficient
+        equal to 1.
+
+    """ + base._missing_param_doc}
+    def __init__(self, endog, exog, exog_infl=None, offset=None,
+                       exposure=None, missing='none', **kwargs):
+        super(GenericZeroInflated, self).__init__(endog, exog, offset=offset,
+                                                  exposure=exposure,
+                                                  missing=missing, **kwargs)
+
+        if exog_infl is None:
+            exog_infl = np.ones((endog.size, 1))
+        self.k_exog = exog.shape[1]
+        self.k_inflate = exog_infl.shape[1]
+
+    def loglike(self, params):
+        """
+        Loglikelihood of Generic Zero Inflated model
+
+        Parameters
+        ----------
+        params : array-like
+            The parameters of the model.
+
+        Returns
+        -------
+        loglike : float
+            The log-likelihood function of the model evaluated at `params`.
+            See notes.
+
+        Notes
+        --------
+        .. math:: \\ln L=\\sum_{i=1}^{n}\\left[-\\lambda_{i}+y_{i}x_{i}^{\\prime}\\beta-\\ln y_{i}!\\right]
+        """
+        return np.sum(self.loglikeobs(params))
+
+    def loglikeobs(self, params):
+        """
+        Loglikelihood for observations of Generic Zero Inflated model
+
+        Parameters
+        ----------
+        params : array-like
+            The parameters of the model.
+
+        Returns
+        -------
+        loglike : ndarray (nobs,)
+            The log likelihood for each observation of the model evaluated
+            at `params`. See Notes
+
+        Notes
+        --------
+        .. math:: \\ln L_{i}=\\left[-\\lambda_{i}+y_{i}x_{i}^{\\prime}\\beta-\\ln y_{i}!\\right]
+
+        for observations :math:`i=1,...,n`
+
+        """
+        params_infl = params[self.k_exog:]
+        params_main = params[:self.k_exog]
+
+        y = self.endog
+        w = self.model_infl.predict(params_infl)
+        llf_main = self.main_model.loglikeobs(params_main)
+
+        llf = np.zeros_like(y)
+        llf[y == 0] = (np.log(w[y == 0] +
+            (1 - w[y == 0]) * np.exp(llf_main[y == 0])))
+        llf[y != 0] = np.log(1 - w[y != 0]) + llf_main[y != 0]
+
+        return llf
+
+    def fit(self, start_params=None, method='newton', maxiter=35,
+            full_output=1, disp=1, callback=None, **kwargs):
+        if start_params is None:
+            offset = getattr(self, "offset", 0) + getattr(self, "exposure", 0)
+            if np.size(offset) == 1 and offset == 0:
+                offset = None
+            mod_poi = Poisson(self.endog, self.exog, offset=offset)
+            start_params = mod_poi.fit(disp=0).params
+            start_params = np.append(start_params, np.ones(self.k_inflate))
+        mlefit = super(GenericZeroInflated, self).fit(start_params=start_params,
+                        maxiter=maxiter, disp=disp,
+                        full_output=full_output, callback=lambda x:x,
+                        **kwargs)
+
+        #gpfit = GeneralizedPoissonResults(self, mlefit._results)
+        #result = GeneralizedPoissonResultsWrapper(gpfit)
+        result = mlefit
+
+        if cov_kwds is None:
+            cov_kwds = {}
+
+        result._get_robustcov_results(cov_type=cov_type,
+                                      use_self=True, use_t=use_t, **cov_kwds)
+        return result
+
+    fit.__doc__ = DiscreteModel.fit.__doc__
+
+    def score(self, params):
+        """
+        Generic Zero Inflated model score (gradient) vector of the log-likelihood
+
+        Parameters
+        ----------
+        params : array-like
+            The parameters of the model
+
+        Returns
+        -------
+        score : ndarray, 1-D
+            The score vector of the model, i.e. the first derivative of the
+            loglikelihood function, evaluated at `params`
+
+        Notes
+        -----
+        .. math:: \\frac{\\partial\\ln L}{\\partial\\beta}=\\sum_{i=1}^{n}\\left(y_{i}-\\lambda_{i}\\right)x_{i}
+
+        where the loglinear model is assumed
+
+        .. math:: \\ln\\lambda_{i}=x_{i}\\beta
+        """
+        return approx_fprime(params, self.loglike)
+
+    def hessian(self, params):
+        """
+        Generic Zero Inflated model Hessian matrix of the loglikelihood
+
+        Parameters
+        ----------
+        params : array-like
+            The parameters of the model
+
+        Returns
+        -------
+        hess : ndarray, (k_vars, k_vars)
+            The Hessian, second derivative of loglikelihood function,
+            evaluated at `params`
+
+        Notes
+        -----
+        .. math:: \\frac{\\partial^{2}\\ln L}{\\partial\\beta\\partial\\beta^{\\prime}}=-\\sum_{i=1}^{n}\\lambda_{i}x_{i}x_{i}^{\\prime}
+
+        where the loglinear model is assumed
+
+        .. math:: \\ln\\lambda_{i}=x_{i}\\beta
+
+        """
+        return approx_hess(params, self.loglike)
+
+class PoissonZeroInflated(GenericZeroInflated):
+    """
+    Poisson Zero Inflated model for count data
+
+    %(params)s
+    %(extra_params)s
+
+    Attributes
+    -----------
+    endog : array
+        A reference to the endogenous response variable
+    exog : array
+        A reference to the exogenous design.
+    """ % {'params' : base._model_params_doc,
+           'extra_params' :
+           """offset : array_like
+        Offset is added to the linear prediction with coefficient equal to 1.
+    exposure : array_like
+        Log(exposure) is added to the linear prediction with coefficient
+        equal to 1.
+
+    """ + base._missing_param_doc}
+    def __init__(self, endog, exog, exog_infl=None, offset=None,
+                       exposure=None, missing='none', **kwargs):
+        super(PoissonZeroInflated, self).__init__(endog, exog, offset=offset,
+                                                  exog_infl=exog_infl,
+                                                  exposure=exposure,
+                                                  missing=missing, **kwargs)
+        self.main_model = Poisson(endog, exog)
+        self.model_infl = Logit(np.zeros(exog_infl.shape[0]), exog_infl)
+
+
+if __name__=="__main__":
+    import numpy as np
+    import statsmodels.api as sm
