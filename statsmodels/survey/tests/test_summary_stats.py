12bd1289b40bc309c1185fb29ad433f3830a41bd
jarvmiller <jarvmiller@gmail.com>
2017-06-27 16:45:45 -0400

    added bootstrap, will change test file accordingly

--- statsmodels/survey/tests/test_summary_stats.py
+++ statsmodels/survey/tests/test_summary_stats.py
@@ -1,18 +1,32 @@
-import summary_stats
-import unittest
+import summary_stats as ss
 import pandas as pd
 import numpy as np
+from numpy.testing import (assert_almost_equal, assert_equal, assert_array_less,
+                           assert_raises, assert_allclose)
 
-# df = pd.read_csv("examples/survey_df.csv")
-# data = df.copy()
-# n, p = data.shape
-# svy = SurveyDesign(strata=np.ones(n), cluster=df["dnum"], weights=df["pw"])
+strata = np.r_[1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0]
+cluster = np.r_[0, 0, 2, 2, 3, 3, 4, 4, 4, 6, 6, 6]
+weights = np.r_[1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2].astype(np.float64)
+data = np.asarray([[1, 3, 2, 5, 4, 1, 2, 3, 4, 6, 9, 5],
+                   [5, 3, 2, 1, 4, 7, 8, 9, 5, 4, 3, 5]], dtype=np.float64).T
+design = ss.SurveyDesign(strata, cluster, weights)
 
-# class TestSummary(unittest.TestCase):
-#     def test_intialization(self):
-#         np.testing.assert_equal(data["dnum"].reshape(n,1), svy.cluster)
-#         np.testing.assert_equal(data["pw"].reshape(n, 1), svy.prob_weights)
+def test_design():
+    assert_equal(design.ncs, np.array([2, 3]))
+    assert_equal(design.sfclust, np.array([0, 0, 1, 1, 1]))
+    assert(len(np.unique(design.sclust)), len(np.unique(cluster)))
 
+def test_mean():
+    avg = ss.SurveyMean(design, data, 'jack')
+    assert(np.allclose(avg.est, np.r_[3.777778, 4.722222]))
+    assert(np.allclose(avg.vc, np.r_[0.9029327, 1.061515]))
 
-# suite = unittest.TestLoader().loadTestsFromTestCase(TestSummary)
-# unittest.TextTestRunner(verbosity=2).run(suite)
+def test_total():
+    tot = ss.SurveyTotal(design, data, 'jack')
+
+    # assert(np.allclose(tot.est, np.r_[68., 85.]))
+    # # # throws an error. tot.vc yields array([ 19.79898987,  15.71623365])
+    # # # so im rounding until I can get the stata results
+    # assert(np.allclose(np.round(tot.vc, 3), np.r_[19.799, 15.716]))
+
+ss.SurveyQuantile(design, data, [.25, .50])
