ea1d37df9309cbf2a72b77a6539fadaf682acd5e
jarvmiller <jarvmiller@gmail.com>
2017-07-05 12:50:40 -0400

    potential confusion about replicate weights, tried to add but check is necessary. Need to finish testing by the end of next week

--- statsmodels/survey/tests/test_summary_stats.py
+++ statsmodels/survey/tests/test_summary_stats.py
@@ -16,19 +16,27 @@ def test_design():
     assert_equal(design.sfclust, np.array([0, 0, 1, 1, 1]))
     assert(len(np.unique(design.sclust)) == len(np.unique(cluster)))
 
-def test_mean():
+def test_mean_jack():
     avg = ss.SurveyMean(design, data, 'jack')
     assert_allclose(avg.est, np.r_[3.777778, 4.722222])
     assert_allclose(avg.vc, np.r_[0.9029327, 1.061515],  rtol=1e-5, atol=0)
 
-def test_total():
+# def test_mean_boot():
+#     avg = ss.SurveyMean(design, data, 'boot')
+#     assert_allclose(avg.est, np.r_[0, 0])
+#     assert_allclose(avg.vc, np.r_[0, 0], rtol=1e-5, atol=0)
+
+def test_total_jack():
     tot = ss.SurveyTotal(design, data, 'jack')
     assert_allclose(tot.est, np.r_[68, 85])
-    # # # throws an error. tot.vc yields array([ 19.79898987,  15.71623365])
-    # # # so im rounding until I can get the stata results
     assert_allclose(tot.vc, np.r_[19.79899, 15.71623],  rtol=1e-5, atol=0)
 
-def quant_test():
+# def test_total_boot():
+#     tot = ss.SurveyTotal(design, data, 'boot')
+#     assert_allclose(tot.est, np.r_[68, 85])
+#     assert_allclose(tot.vc, np.r_[19.79899, 15.71623],  rtol=1e-5, atol=0)
+
+def test_quantile_jack():
     quant = ss.SurveyQuantile(design, data, [.1, .25, .33, .5, .75, .99], 'jack')
     assert_allclose(quant.est[0], np.r_[1, 2, 2, 3.5, 5, 9])
     ## change 7 to 6 to accommodate w/ stata
@@ -38,3 +46,14 @@ def quant_test():
     med = ss.SurveyMedian(design, data, 'jack')
     assert_equal(quant.est[0][-3], med.est[0][0])
     assert_equal(quant.est[1][-3], med.est[1][0])
+
+# def test_quantile_boot():
+#     quant = ss.SurveyQuantile(design, data, [.1, .25, .33, .5, .75, .99], 'boot')
+#     assert_allclose(quant.est[0], np.r_[1, 2, 2, 3.5, 5, 9])
+#     ## change 7 to 6 to accommodate w/ stata
+#     assert_allclose(quant.est[1], np.r_[2, 3, 3, 4.5, 7, 9])
+
+#     # ensure that median yields same result
+#     med = ss.SurveyMedian(design, data, 'boot')
+#     assert_equal(quant.est[0][-3], med.est[0][0])
+#     assert_equal(quant.est[1][-3], med.est[1][0])
