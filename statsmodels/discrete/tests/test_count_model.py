579caed43c958a7fb0fe9912776b91b43dca551c
Evgeny Zhurko <evgeny.zhurko@gmail.com>
2017-07-18 18:13:59 +0300

    Added test with offset for ZIP

--- statsmodels/discrete/tests/test_count_model.py
+++ statsmodels/discrete/tests/test_count_model.py
@@ -73,6 +73,37 @@ class TestZeroInflatedModel_probit(object):
     def test_bic(self):
         assert_allclose(self.res1.aic, self.res2.aic, atol=1e-1, rtol=1e-1)
 
+class TestZeroInflatedModel_offset(object):
+    @classmethod
+    def setupClass(cls):
+        data = sm.datasets.randhie.load()
+        cls.endog = data.endog
+        exog = sm.add_constant(data.exog[:,1:4], prepend=False)
+        exog_infl = sm.add_constant(data.exog[:,0], prepend=False)
+        cls.res1 = sm.PoissonZeroInflated(data.endog, exog,
+            exog_infl=exog_infl, offset=data.exog[:,7]).fit(method='newton', maxiter=500)
+        res2 = RandHIE()
+        res2.zero_inflated_poisson_offset()
+        cls.res2 = res2
+
+    def test_params(self):
+        assert_allclose(self.res1.params, self.res2.params, atol=1e-5, rtol=1e-5)
+
+    def test_llf(self):
+        assert_allclose(self.res1.llf, self.res2.llf, atol=1e-5, rtol=1e-5)
+
+    def test_conf_int(self):
+        assert_allclose(self.res1.conf_int(), self.res2.conf_int, atol=1e-3, rtol=1e-5)
+
+    def test_bse(self):
+        assert_allclose(self.res1.bse, self.res2.bse, atol=1e-3)
+
+    def test_aic(self):
+        assert_allclose(self.res1.aic, self.res2.aic, atol=1e-2, rtol=1e-2)
+
+    def test_bic(self):
+        assert_allclose(self.res1.aic, self.res2.aic, atol=1e-1, rtol=1e-1)
+
 
 if __name__ == "__main__":
     import nose
