eb745f7e0e69ed03d19c87c02a067e50a8efe251
Jacob Kimmel <jacobkimmel@gmail.com>
2017-07-26 11:50:45 -0700

    lilliefors test range fix

--- statsmodels/stats/tests/test_lilliefors.py
+++ statsmodels/stats/tests/test_lilliefors.py
@@ -8,13 +8,13 @@ class TestLilliefors(object):
 
     def test_normal(self):
         np.random.seed(3975)
-        x_n = stats.norm.rvs(size=5000)
+        x_n = stats.norm.rvs(size=500)
         d_ks, p = lilliefors(x_n, dist='norm')
         assert_(p > 0.05)
 
     def test_expon(self):
         np.random.seed(3975)
-        x_e = stats.expon.rvs(size=5000)
+        x_e = stats.expon.rvs(size=500)
         d_ks, p = lilliefors(x_e, dist='exp')
         assert_(p > 0.05)
 
