97212a5c8b64e4c1f5e33835163748f46de4166f
jarvmiller <jarvmiller@gmail.com>
2017-08-10 15:41:51 -0400

    updated SurveyModel. stderr close to truth but off (not by a constant)

--- statsmodels/survey/survey_model.py
+++ statsmodels/survey/survey_model.py
@@ -1,7 +1,46 @@
+"""
+Methods for creating models for survey data.
+
+The main classes are:
+
+  * SurveyModel : Creates and fits the specified model. Returns
+  estimates and stderr
+
+Note: This makes use of survey weights, which is supplied to
+the 'freq_weights' parameter in StatsModel's GLM. SurveyModel can
+only be utilized by models w/ a freq_weight or weight parameter
+"""
+
 from __future__ import division
 import numpy as np
-class SurveyModel(object):
 
+class SurveyModel(object):
+    """
+
+    Parameters
+    -------
+    design : Instance of class SurveyDesign
+    model_class : Instance of class GLM
+    init_args : Dictionary of arguments
+        when initializing the model
+    fit_args : Dictionary of arguments
+        when fitting the model
+
+    Attributes
+    ----------
+    design : Instance of class SurveyDesign
+    model : Instance of class GLM
+    init_args : Dictionary of arguments
+        when initializing the model
+    fit_args : Dictionary of arguments
+        when fitting the model
+    params : (p, ) array
+        Array of coefficients of model
+    vcov : (p, p) array
+        Covariance matrix
+    stderr : (p, ) array
+        Standard error of cofficients
+    """
     def __init__(self, design, model_class, init_args={}, fit_args={}):
         self.design = design
         self.model = model_class
@@ -24,47 +63,121 @@ class SurveyModel(object):
                     array[self.design.ii[s], :] -= array[self.design.ii[s],
                                                          :].mean(0)
         else:
-            raise ValueError("Centering option not implemented")
+            raise ValueError("Centering option %s not implemented" %center_by)
         return array
 
     def _stata_linearization_vcov(self, X, y):
-        model = self.model(y, X, **self.init_args)
-        # doing model.fit() to get the hessian
-        model.fit(**self.fit_args)
+        """
+        Get the linearized covariance matrix using STATA's methodology
 
-        hessian = model.hessian(self.params)
+        Parameters
+        ----------
+        X : array-like
+            A n x p array where 'n' is the number of observations and 'p'
+            is the number of regressors. An intercept is not included by
+            deault and should be added by the user
+        y : array-like
+            1d array of the response variable
+
+        Returns
+        -------
+        vcov : (p,p) array
+            The covariance matrix
+
+        Notes
+        -----
+        This uses a 'sandwich' method, where three matrices are multiplied
+        together. The outer parts of the 'sandwich' is the inverse of the
+        negative hessian. The inside is the design-based variance of the
+        estimation of a total, ie np.dot(weights, observations) where
+        'observations' is the derivative of the log pseudolikelihood w.r.t
+        theta (X*Beta) multiplied by the data
+
+        Reference
+        ---------
+        http://www.stata.com/manuals13/svyvarianceestimation.pdf
+        """
+
+        # model = self.model(y, X, **self.init_args)
+
+        # doing model.fit() to get the hessian
+        # self.initialized_model.fit(**self.fit_args)
+        hessian = self.initialized_model.hessian(self.params, observed=True)
         hess_inv = np.linalg.inv(hessian)
 
         lin_pred = np.dot(X, self.params)
-        idl = model.family.link.inverse_deriv(lin_pred)
-        # d_hat is the matrix of partial derivatives of the link function
-        # w.r.t self.params.
-        d_hat = (X * idl[:, None])
+        #  derivative of the log pseudolikelihood w.r.t x_j * beta
+        s = self.initialized_model.family.link.inverse_deriv(lin_pred)
+        # d_hat = s_j * x_j is equivalent to STATA's d_j
+        d_hat = (X * s[:, None])
 
         jdata = []
+        # design-based variance estimator for a total
+        # This is the same as getting linearized variance
+        # of SurveyTotal
         for c in range(self.design.n_clust):
             w = self.design.weights.copy()
-            # but if you're not in that cluster, set as 0
+            # If you're not in that cluster, set tp 0
             w[self.design.clust != c] = 0
+            # calculate the total
             jdata.append(np.dot(w, d_hat))
         jdata = np.asarray(jdata)
-        # we usually deal w/ jdata as nxp
-        # unless w/ ratio, in which 2 columns
+
         if jdata.ndim == 1:
             jdata = jdata[:, None]
         jdata = self._centering(jdata, 'stratum')
+
+        # multiplying by constants
         nh = self.design.clust_per_strat[self.design.strat_for_clust].astype(np.float64)
         mh = np.sqrt(nh / (nh-1))
         fh = np.sqrt(1 - self.design.fpc)
         jdata = fh[:, None] * mh[:, None] * jdata
-        vcov = np.dot(jdata.T, jdata)
 
+        v_hat = np.dot(jdata.T, jdata)
+        vcov = np.dot(np.dot(hess_inv, v_hat), hess_inv.T)
         return vcov
 
-    def _sas_linearization_vcov(self, X, y, technique):
-        model = self.model(y, X, **self.init_args)
+    def _sas_linearization_vcov(self, X, y, lin_method):
+        """
+        Get the linearized covariance matrix using STATA's methodology
+
+        Parameters
+        ----------
+        X : array-like
+            A n x p array where 'n' is the number of observations and 'p'
+            is the number of regressors. An intercept is not included by
+            deault and should be added by the user
+        y : array-like
+            1d array of the response variable
+        lin_method : str
+            Requests the Newton-Raphson algorithm if "Newton"
+            is specified
+
+
+        Returns
+        -------
+        vcov : (p,p) array
+            The covariance matrix
+
+        Notes
+        -----
+        This uses a 'sandwich' method, where three matrices are multiplied
+        together. The outer parts of the 'sandwich' is either the inverse of
+        the negative expected hessian or __(not sure how to describe Q_hat)__.
+        The inside is the design-based variance of the estimation of a total,
+        ie np.dot(weights, observations) where 'observations' is a sort of
+        residual. It is multiplied by an additional factor of (n-1)/(p-1) that
+        is not in STATA's methodology to reduce the small sample bias
+        associated with using the estimated function to calculate deviations
+
+        Reference
+        ---------
+        https://support.sas.com/documentation/cdl/en/statug/63033/HTML/default/viewer.htm#statug_surveylogistic_a0000000364.htm
+        """
+
+        # model = self.model(y, X, **self.init_args)
         lin_pred = np.dot(X, self.params)
-        idl = model.family.link.inverse_deriv(lin_pred)
+        idl = self.initialized_model.family.link.inverse_deriv(lin_pred)
         # d_hat is the matrix of partial derivatives of the link function
         # w.r.t self.params.
         d_hat = (X * idl[:, None]).T
@@ -88,12 +201,11 @@ class SurveyModel(object):
         e = fh[:, None] * mh[:, None] * e
         g_hat = (len(y) - 1) / (len(y) - X.shape[1]) * np.dot(e.T, e)
 
-        if technique=="newton":
+        if lin_method=="newton":
             # TODO: Figure out if hessian should be used when
             # 'jack-sandwich' or 'boot-sandwich' is requested
-            model.fit(**self.fit_args)
-            factor = model.hessian_factor(self.params)
-            q_hat = -np.dot(X.T * factor, X)
+            # self.initialized_model.fit(**self.fit_args)
+            q_hat = self.initialized_model.hessian(self.params, observed=False)
         else:
             cond_inv = np.linalg.inv(np.diag(cond_mean) - np.dot(cond_mean, cond_mean.T))
             q_hat = np.dot(w * d_hat, np.dot(cond_inv, d_hat.T))
@@ -101,36 +213,38 @@ class SurveyModel(object):
         vcov = np.dot(q_hat_inv, np.dot(g_hat, q_hat_inv))
         return vcov
 
-    def _get_jackknife_vcov(self, X, y):
+    def _jackknife_vcov(self, X, y):
         replicate_params = []
         for c in range(self.design.n_clust):
             w = self.design.get_rep_weights(cov_method='jack', c=c)
-            self.init_args["weights"] = w
+            self.init_args["freq_weights"] = w
             replicate_params.append(self._get_params(y, X))
         replicate_params = np.asarray(replicate_params)
         self.replicate_params = self._centering(replicate_params)
-        try:
+
+        if hasattr(self.design, 'clust_per_strat'):
             nh = self.design.clust_per_strat[self.design.strat_for_clust].astype(np.float64)
             mh = np.sqrt((nh - 1) / nh)
             self.replicate_params = mh[:, None] * self.replicate_params
-        except AttributeError:
+        else:
             nh = self.design.rep_weights.shape[1]
             mh = np.sqrt((nh - 1) / nh)
             self.replicate_params *= mh
+
         vcov = np.dot(self.replicate_params.T, self.replicate_params)
         return vcov
 
-    def fit(self, y, X, cov_method='jack', center_by='est', technique=None):
+    def fit(self, y, X, cov_method='jack', center_by='est', lin_method=None):
         self.center_by = center_by
-        self.init_args["weights"] = self.design.weights
+        self.init_args["freq_weights"] = self.design.weights
         self.params = self._get_params(y, X)
 
 
         # for now, just working with jackknife to see if it works
         if cov_method == 'jack':
-            self.vcov = self._get_jackknife_vcov(X, y)
+            self.vcov = self._jackknife_vcov(X, y)
         elif cov_method == 'linearized_sas':
-            self.vcov = self._sas_linearization_vcov(X, y, technique)
+            self.vcov = self._sas_linearization_vcov(X, y, lin_method)
         elif cov_method == 'linearized_stata':
             self.vcov = self._stata_linearization_vcov(X, y)
         else:
@@ -141,6 +255,10 @@ class SurveyModel(object):
             self.stderr = np.sqrt(self.vcov)
 
     def _get_params(self, y, X):
-        model = self.model(y, X, **self.init_args)
-        self.result = model.fit(**self.fit_args)
+        # note, can make 'model' into self.initialized_model
+        # so that i dont have to worry about it getting called multiple times
+        # when doing linearized variance
+        # when doing jackknife, it'll get called mult times anyway
+        self.initialized_model = self.model(y, X, **self.init_args)
+        self.result = self.initialized_model.fit(**self.fit_args)
         return self.result.params
--- statsmodels/survey/tests/test_survey_model.py
+++ statsmodels/survey/tests/test_survey_model.py
@@ -11,21 +11,10 @@ strata = np.r_[0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1]
 cluster = np.r_[0, 0, 1, 1, 2, 2, 3, 3, 3, 4, 4]
 weights = np.r_[1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1].astype(np.float64)
 fpc = np.r_[.5, .5, .5, .5, .5, .5, .1, .1, .1, .1, .1]
-data = np.asarray([[1, 3, 2, 5, 4, 1, 2, 3, 4, 6, 9],
-                   [5, 3, 2, 1, 4, 7, 8, 9, 5, 4, 3],
-                   [3, 2, 1, 5, 6, 7, 4, 2, 1, 6, 4]], dtype=np.float64).T
-# need to get stata results to compare
-y = np.random.choice([0,1], 11)
-X = data[:, [1,2]]
-design = SurveyDesign(strata, cluster, weights)
-# assert_equal(design.clust, np.r_[0, 0, 1, 1, 2, 2, 3, 3, 3, 4, 4])
-model_class = sm.GLM(y, X, family = sm.families.Binomial())
-model_class.fit()
-
-init_args = {'family': sm.families.Binomial()}
-model_class = sm.GLM
-model = SurveyModel(design, model_class=model_class, init_args=init_args)
-model.fit(y, X, cov_method='linearized_stata',center_by='est')
+data = pd.read_csv("~/Downloads/logistic_df.csv")
+y = np.asarray(data['y'])
+X = np.asarray(data[['x1', 'x2']])
+X = sm.add_constant(X)
 
 def test_jack_repw():
     design = ss.SurveyDesign(strata, cluster, weights)
@@ -33,18 +22,26 @@ def test_jack_repw():
     model_class = sm.GLM
     init_args = {'family': sm.families.Binomial()}
     model = smod.SurveyModel(design, model_class=model_class, init_args=init_args)
-    rslt = model.fit(y, X, cov_method='linearized_sas', center_by='est')
-
+    rslt = model.fit(y, X, cov_method='linearized_stata', center_by='stratum')
+    print(model.params)
+    print(model.stderr)
+    raise ValueError('stop here')
     rw = []
     for k in range(5):
         rw.append(design.get_rep_weights(c=k, cov_method='jack'))
     rw = np.asarray(rw).T
-    model = smod.SurveyModel(design, model_class=model_class)
-    model.fit(y, X)
+    model = smod.SurveyModel(design, model_class=model_class, init_args=init_args)
+    model.fit(y, X, cov_method='jack', center_by='stratum')
     design_rw = ss.SurveyDesign(weights = weights, rep_weights=rw)
-    model_rw = smod.SurveyModel(design_rw, model_class=model_class)
+    model_rw = smod.SurveyModel(design_rw, model_class=model_class, init_args=init_args)
     model_rw.fit(y, X, cov_method='jack')
-
+    # print("calculated rep_weights")
+    # print(model.params)
+    # print(model.stderr)
+    # print(" \n supplied rep_weights")
+    # print(model_rw.params)
+    # print(model_rw.stderr)
+    # raise ValueError('stop here')
     assert_allclose(model.params, model_rw.params)
     """
     comparison between stderr fails because model has
