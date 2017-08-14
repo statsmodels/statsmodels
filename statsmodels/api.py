04fbd8ed3c647cc185f8c7e6b3571e54036aee9c
Evgeny Zhurko <evgeny.zhurko@gmail.com>
2017-08-14 11:45:38 +0300

    Refactored ZIGP-p

--- statsmodels/api.py
+++ statsmodels/api.py
@@ -15,7 +15,8 @@ from .discrete.discrete_model import (Poisson, Logit, Probit,
                                       MNLogit, NegativeBinomial,
                                       GeneralizedPoisson,
                                       NegativeBinomialP)
-from .discrete.count_model import (ZeroInflatedPoisson)
+from .discrete.count_model import (ZeroInflatedPoisson,
+                                   ZeroInflatedGeneralizedPoisson)
 from .tsa import api as tsa
 from .duration.survfunc import SurvfuncRight
 from .duration.hazard_regression import PHReg
