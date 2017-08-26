abd296f9d661cb470179cdc823f01b0c212a9c67
Brock Mendel <jbrockmendel@gmail.com>
2017-08-26 11:42:46 -0700

    Fix indentation error

--- statsmodels/tools/tests/test_numdiff.py
+++ statsmodels/tools/tests/test_numdiff.py
@@ -297,26 +297,26 @@ def test_dtypes():
 
 
 def test_tuple_error():
-	  # This is a regression test for a bug that occurred due to a typo
-	  # in approx_hess3.  See GH #3253
-	  # With the typo, this test will raise:
-	  #
-	  #   File "<stdin>", line 1, in <module>
-    #	File "/usr/local/lib/python2.7/site-packages/statsmodels/tools/numdiff.py", line 352, in approx_hess3
+    # This is a regression test for a bug that occurred due to a typo
+    # in approx_hess3.  See GH #3253
+    # With the typo, this test will raise:
+    #
+    #   File "<stdin>", line 1, in <module>
+    #    File "/usr/local/lib/python2.7/site-packages/statsmodels/tools/numdiff.py", line 352, in approx_hess3
     #       - f(*((x - ee[i, :] - ee[j, :],) + args), **kwargs),)
-	  #   TypeError: unsupported operand type(s) for -: 'float' and 'tuple'
+    #   TypeError: unsupported operand type(s) for -: 'float' and 'tuple'
 
-    # The below is copied near-verbatim form #3253
-	  def f(x):
-    	  return np.dot(x, x) / 2.0
+    # The below is copied near-verbatim from #3253
+    def f(x):
+        return np.dot(x, x) / 2.0
 
-	  def f_float(x):
-    	  return float(np.dot(x, x) / 2.0)
+    def f_float(x):
+        return float(np.dot(x, x) / 2.0)
 
-	  # Test array for differentiating
-	  x = np.array([1.0])
+    # Test array for differentiating
+    x = np.array([1.0])
 
-	  numdiff.approx_hess3(x, f_float)
+    numdiff.approx_hess3(x, f_float)
 
 
 
