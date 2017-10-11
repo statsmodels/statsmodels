0789f1becc3589b4c83580603666e3cb34a92265
Brock Mendel <jbrockmendel@gmail.com>
2017-10-10 18:06:40 -0700

    fixup tab-->space4

--- statsmodels/tools/tests/test_decorators.py
+++ statsmodels/tools/tests/test_decorators.py
@@ -8,32 +8,32 @@ from statsmodels.tools.decorators import deprecated_alias
 
 class TestDeprecatedAlias(object):
 
-	@classmethod
-	def setup_class(cls):
-		
-		class Dummy(object):
+    @classmethod
+    def setup_class(cls):
+        
+        class Dummy(object):
 
-			y = deprecated_alias('y', 'x', '0.11.0')
+            y = deprecated_alias('y', 'x', '0.11.0')
 
-			def __init__(self, y):
-				self.x = y
+            def __init__(self, y):
+                self.x = y
 
-		cls.Dummy = Dummy
+        cls.Dummy = Dummy
 
-	def test_get(self):
-		inst = self.Dummy(4)
-		with warnings.catch_warnings(record=True) as record:
-			assert_(inst.y == 4)
+    def test_get(self):
+        inst = self.Dummy(4)
+        with warnings.catch_warnings(record=True) as record:
+            assert_(inst.y == 4)
 
-		assert_(len(record) == 1)
-		assert_('is a deprecated alias' in str(record[0]))
+        assert_(len(record) == 1)
+        assert_('is a deprecated alias' in str(record[0]))
 
-	def test_set(self):
-		inst = self.Dummy(4)
+    def test_set(self):
+        inst = self.Dummy(4)
 
-		with warnings.catch_warnings(record=True) as record:
-			inst.y = 5
+        with warnings.catch_warnings(record=True) as record:
+            inst.y = 5
 
-		assert_(len(record) == 1)
-		assert_('is a deprecated alias' in str(record[0]))
-		assert_(inst.x == 5)
+        assert_(len(record) == 1)
+        assert_('is a deprecated alias' in str(record[0]))
+        assert_(inst.x == 5)
