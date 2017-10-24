82dc88048aee5eaff3233d5dd4da2fd822b02408
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2017-10-24 10:10:29 +0100

    TST: Add test for omitted column
    Add test for #3702 to ensure omitted is not displayed
    
    closes #3702
    closes #1638

--- statsmodels/iolib/tests/test_summary2.py
+++ statsmodels/iolib/tests/test_summary2.py
@@ -1,7 +1,10 @@
 import warnings
-from numpy.testing import assert_equal, assert_
-from statsmodels.regression.linear_model import OLS, add_constant
+
+import numpy as np
+from numpy.testing import assert_equal
+
 from statsmodels.iolib.summary2 import summary_col
+from statsmodels.regression.linear_model import OLS, add_constant
 
 
 class TestSummaryLatex(object):
@@ -35,6 +38,23 @@ x1    & -0.7500  & -1.5769   \\
         actual = summary_col([reg1,reg2]).as_latex()
         actual = '\n%s\n' % actual
         assert_equal(desired, actual)
+
+    def test_summarycol_drop_omitted(self):
+        # gh-3702
+        x = [1, 5, 7, 3, 5]
+        x = add_constant(x)
+        x2 = np.concatenate([x, np.array([[3], [9], [-1], [4], [0]])], 1)
+        y1 = [6, 4, 2, 7, 4]
+        y2 = [8, 5, 0, 12, 4]
+        reg1 = OLS(y1, x).fit()
+        reg2 = OLS(y2, x2).fit()
+        actual = summary_col([reg1, reg2], regressor_order=['const', 'x1'],
+                             drop_omitted=True)
+        assert 'x2' not in str(actual)
+        actual = summary_col([reg1, reg2], regressor_order=['x1'],
+                             drop_omitted=False)
+        assert 'const' in str(actual)
+        assert 'x2' in str(actual)
 
     def test_OLSsummary(self):
         # Test that latex output of regular OLS output still contains
