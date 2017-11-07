b7adb4fe8031b3b8097108eebc4f53a64f15e51b
Joses W. Ho <joseshowh@gmail.com>
2017-11-07 15:21:52 +0800

    example for bland-altman

---
+++ docs/source/plots/graphics-bland_altman.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Test Bland-Altman
+"""
+
+Author: Joses Ho
+
+"""
+
+
+from bland_altman import bland_altman_plot
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Seed the random number generator.
+# This ensures that the results below are reproducible.
+np.random.seed(9999)
+m1 = np.random.random(20)
+m2 = np.random.random(20)
+
+f, ax = plt.subplots(1, figsize =(8,5))
+my_bland_altman_plot = bland_altman_plot(m1, m2, ax = ax)
