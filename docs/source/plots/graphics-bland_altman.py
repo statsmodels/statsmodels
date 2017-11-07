5153fb87f8828387315d50b07640069eb892c1f5
Joses W. Ho <joseshowh@gmail.com>
2017-11-07 16:22:01 +0800

    update tests

--- docs/source/plots/graphics-bland_altman.py
+++ docs/source/plots/graphics-bland_altman.py
@@ -6,8 +6,7 @@ Author: Joses Ho
 
 """
 
-
-from bland_altman import bland_altman_plot
+import statsmodels.api as sm
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -17,5 +16,7 @@ np.random.seed(9999)
 m1 = np.random.random(20)
 m2 = np.random.random(20)
 
-f, ax = plt.subplots(1, figsize =(8,5))
-my_bland_altman_plot = bland_altman_plot(m1, m2, ax = ax)
+f, ax = plt.subplots(1, figsize = (8,5))
+sm.graphics.bland_altman_plot(m1, m2, ax = ax)
+
+plt.show()
