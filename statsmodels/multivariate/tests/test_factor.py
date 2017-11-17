11ffa5d8c6e849d013aa861519d12dc311025e6c
Yichuan Liu <yichuanliu2004@gmail.com>
2017-11-17 10:01:08 -0500

    Structure change. Do plot and rotate in the results class.

--- statsmodels/multivariate/tests/test_factor.py
+++ statsmodels/multivariate/tests/test_factor.py
@@ -3,7 +3,7 @@
 import numpy as np
 import pandas as pd
 from statsmodels.multivariate.factor import Factor
-from numpy.testing import assert_equal
+from numpy.testing import assert_equal, assert_array_almost_equal
 from numpy.testing.decorators import skipif
 
 try:
@@ -39,38 +39,39 @@ def test_example_compare_to_R_output():
                   [0.97113975, 0.17207499],
                   [0.9618705, -0.2004196],
                   [0.37570708, -0.45821379]])
-    # assert_array_almost_equal(mod.loadings, a, decimal=8)
+    assert_array_almost_equal(results.loadings, a, decimal=8)
 
     # Same as R GRArotation
-    results = mod.fit(rotation='varimax')
+    results.rotate('varimax')
     a = np.array([[0.98828898, -0.12587155],
                   [0.97424206, -0.15354033],
                   [0.84418097, -0.502714],
                   [0.20601929, -0.55558235]])
-    # assert_array_almost_equal(mod.loadings, a, decimal=8)
+    assert_array_almost_equal(results.loadings, a, decimal=8)
 
-    results = mod.fit(rotation='quartimax')  # Same as R fa
+    results.rotate('quartimax')  # Same as R fa
     a = np.array([[0.98935598, 0.98242714, 0.94078972, 0.33442284],
                   [0.117190049, 0.086943252, -0.283332952, -0.489159543]])
-    #assert_array_almost_equal(mod.loadings, a.T, decimal=8)
+    assert_array_almost_equal(results.loadings, a.T, decimal=8)
 
-    results = mod.fit(rotation='equamax')  # Not the same as R fa
+    results.rotate('equamax')  # Not the same as R fa
 
-    results = mod.fit(rotation='promax')  # Not the same as R fa
+    results.rotate('promax')  # Not the same as R fa
 
-    results = mod.fit(rotation='biquartimin')  # Not the same as R fa
+    results.rotate('biquartimin')  # Not the same as R fa
 
-    results = mod.fit(rotation='oblimin')  # Same as R fa
+    results.rotate('oblimin')  # Same as R fa
     a = np.array([[1.02834170170, 1.00178840104, 0.71824931384,
                    -0.00013510048],
                   [0.06563421, 0.03096076, -0.39658839, -0.59261944]])
-    #assert_array_almost_equal(mod.loadings, a.T, decimal=8)
+    assert_array_almost_equal(results.loadings, a.T, decimal=8)
 
 
 @skipif(missing_matplotlib)
 def test_plots():
     mod = Factor(X.iloc[:, 1:], 3)
-    mod.fit(rotation='oblimin')
-    mod.plot_scree()
-    fig_loadings = mod.plot_loadings()
+    results = mod.fit()
+    results.rotate('oblimin')
+    results.plot_scree()
+    fig_loadings = results.plot_loadings()
     assert_equal(3, len(fig_loadings))
