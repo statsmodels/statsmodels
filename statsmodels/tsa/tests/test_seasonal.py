7ede34859a4157ef083f8e1a147c5d04c5507739
Josef <josef.pktd@gmail.com>
2018-01-07 15:09:39 -0500

    BUG: seasonal_decompose, fix extrapolation with 1-D closes #4196

--- statsmodels/tsa/tests/test_seasonal.py
+++ statsmodels/tsa/tests/test_seasonal.py
@@ -261,22 +261,26 @@ class TestDecompose:
         assert_equal(trend, expected)
 
     def test_interpolate_trend(self):
-        x = np.arange(6)
-        trend = seasonal_decompose(x, freq=2).trend
+        x = np.arange(12)
+        freq = 4
+        trend = seasonal_decompose(x, freq=freq).trend
         assert_equal(trend[0], np.nan)
 
-        trend = seasonal_decompose(x, freq=2, extrapolate_trend=1).trend
+        trend = seasonal_decompose(x, freq=freq, extrapolate_trend=5).trend
         assert_almost_equal(trend, x)
 
-        trend = seasonal_decompose(x, freq=2, extrapolate_trend='freq').trend
+        trend = seasonal_decompose(x, freq=freq, extrapolate_trend='freq').trend
+        assert_almost_equal(trend, x)
+
+        trend = seasonal_decompose(x[:, None], freq=freq, extrapolate_trend=5).trend
         assert_almost_equal(trend, x)
 
         # 2d case
-        x = np.tile(np.arange(6), (2, 1)).T
-        trend = seasonal_decompose(x, freq=2, extrapolate_trend=1).trend
+        x = np.tile(np.arange(12), (2, 1)).T
+        trend = seasonal_decompose(x, freq=freq, extrapolate_trend=1).trend
         assert_almost_equal(trend, x)
 
-        trend = seasonal_decompose(x, freq=2, extrapolate_trend='freq').trend
+        trend = seasonal_decompose(x, freq=freq, extrapolate_trend='freq').trend
         assert_almost_equal(trend, x)
 
     def test_raises(self):
