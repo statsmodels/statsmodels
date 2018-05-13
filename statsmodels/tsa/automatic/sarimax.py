5d01590876ceb2f9115b4c7a37751128cad372eb
abhijeetpanda12 <abhijeet.panda12@gmail.com>
2018-05-13 08:38:00 +0530

    function definition of order selection

---
+++ statsmodels/tsa/automatic/sarimax.py
@@ -0,0 +1,7 @@
+"""automatic forecasting for SARIMAX models."""
+
+
+def auto_order(endog, criteria='aic', d=0, max_order=(3, 3), spec=None):
+    """Auto order function for SARIMAX models."""
+    print(endog)
+    pass
