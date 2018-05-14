17c697c80b38a072e2393c0d45a4ed052671d6c0
abhijeetpanda12 <abhijeet.panda12@gmail.com>
2018-05-14 14:05:20 +0530

    test file created for sarimax

--- statsmodels/tsa/automatic/tests/test_sarimax.py
+++ statsmodels/tsa/automatic/tests/test_sarimax.py
@@ -1,7 +1,12 @@
-"""automatic forecasting for SARIMAX models."""
+"""unit test for automatic sarimax forecasting."""
 import numpy as np
 import pandas as pd
 import statsmodels.api as sm
+
+# # Dataset
+wpi1 = requests.get('http://www.stata-press.com/data/r12/wpi1.dta').content
+data = pd.read_stata(BytesIO(wpi1))
+data.index = data.t
 
 def auto_order(endog, criteria='aic', d=0, max_order=(3, 3), spec=None):
     """Auto order function for SARIMAX models."""
@@ -11,7 +16,8 @@ def auto_order(endog, criteria='aic', d=0, max_order=(3, 3), spec=None):
             if p == 0 and q == 0:
                 continue
             # fit  the model
-            mod = sm.tsa.statespace.SARIMAX(endog, order=(p, d, q), **spec)
+            # TODO smoke test
+            mod = sm.tsa.statespace.SARIMAX(endog, order=(p, d, q))
             res = mod.fit(disp=False)
             aic_matrix[p, q] = res.aic
     min_aic = aic_matrix.min()
