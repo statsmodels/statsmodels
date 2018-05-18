60021740ae9dec8c641c12fc560c5e599754a3db
Josef <josef.pktd@gmail.com>
2018-05-18 00:19:30 -0400

    DOC/CLN docstring  [skip ci]

--- statsmodels/base/_penalties.py
+++ statsmodels/base/_penalties.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 A collection of smooth penalty functions.
 
@@ -166,7 +167,34 @@ class PseudoHuber(Penalty):
 
 class SCAD(Penalty):
     """
-    The SCAD penalty of Fan and Li
+    The SCAD penalty of Fan and Li.
+
+    The SCAD penalty is linear around zero as a L1 penalty up to threshold tau.
+    The SCAD penalty is constant for values larger than c*tau.
+    The middle segment is quadratic and connect the two segments with a continuous
+    derivative.
+    The penalty is symmetric around zero.
+
+    Parameterization follows Boo, Johnson, Li and Tan 2011.
+    Fan and Li use lambda instead of tau, and a instead of c. Fan and Li
+    recommend setting c=3.7.
+
+    f(x) = { tau |x|                                   if 0 <= |x| < tau
+           { -(|x|^2 - 2 c tau |x| + tau^2) / (2 (c - 1)   if tau <= |x| < c tau
+           { (c + 1) tau^2 / 2                         if c tau <= |x|
+
+
+    References
+    ----------
+    Buu, Anne, Norman J. Johnson, Runze Li, and Xianming Tan. "New variable
+    selection methods for zero‐inflated count data with applications to the
+    substance abuse field."
+    Statistics in medicine 30, no. 18 (2011): 2326-2340.
+
+    Fan, Jianqing, and Runze Li. "Variable selection via nonconcave penalized
+    likelihood and its oracle properties."
+    Journal of the American statistical Association 96, no. 456 (2001):
+    1348-1360.
     """
 
     def __init__(self, tau, c=3.7, weights=None):
@@ -189,10 +217,8 @@ class SCAD(Penalty):
         res[mask1] = tau * p_abs[mask1]
         mask2 = ~mask1 & ~mask3
         p_abs2 = p_abs[mask2]
-        #tmp = (tau * p_abs2**2 - 2 * self.c * p_abs2 + tau**2)
         tmp = (p_abs2**2 - 2 * self.c * tau * p_abs2 + tau**2)
         res[mask2] = -tmp / (2 * (self.c - 1))
-        #res[mask3] = (self.c + 1) / 2. * p_abs[mask3]**2
         res[mask3] = (self.c + 1) * tau**2 / 2.
 
         return (self.weights * res).sum(0)
@@ -204,34 +230,31 @@ class SCAD(Penalty):
         p = np.atleast_1d(params)
         p_abs = np.abs(p)
         p_sign = np.sign(p)
-        res = np.empty(p_abs.shape)#, p_abs.dtype)
+        res = np.empty(p_abs.shape)
         res.fill(np.nan)
 
         mask1 = p_abs < tau
         mask3 = p_abs >= self.c * tau
         mask2 = ~mask1 & ~mask3
         res[mask1] = p_sign[mask1] * tau
-        #tmp = p_sign[mask2] * (tau * p_abs[mask2] - self.c)
         tmp = p_sign[mask2] * (p_abs[mask2] - self.c * tau)
         res[mask2] = -tmp / (self.c - 1)
-        res[mask3] = 0#(self.c + 1)
+        res[mask3] = 0
 
         return self.weights * res
-
 
     def deriv2(self, params):
         """Second derivative of function
 
-        Warning: Not checked, possible problems in multivariate case
-        This returns scalar or vector in same shape as params, not a square Hessian.
-
+        This returns scalar or vector in same shape as params, not a square
+        Hessian. If the return is 1 dimensional, then it is the diagonal of
+        the Hessian.
         """
 
         # 3 segments in absolute value
         tau = self.tau
         p = np.atleast_1d(params)
         p_abs = np.abs(p)
-        p_sign = np.sign(p)
         res = np.zeros(p_abs.shape)
 
         mask1 = p_abs < tau
@@ -244,7 +267,7 @@ class SCAD(Penalty):
 
 class SCADSmoothed(SCAD):
     """
-    The SCAD penalty of Fan and Li, quadratically smoothed around zero
+    The SCAD penalty of Fan and Li, quadratically smoothed around zero.
 
     This follows Fan and Li 2001 equation (3.7).
 
@@ -276,7 +299,6 @@ class SCADSmoothed(SCAD):
         self.aq2 = 0.5 * deriv_c0 / c0
         self.restriction = restriction
 
-
     def func(self, params):
         # TODO: `and np.size(params) > 1` is hack for llnull, need better solution
         if self.restriction is not None and np.size(params) > 1:
@@ -298,7 +320,6 @@ class SCADSmoothed(SCAD):
 
         return (self.weights * value).sum(0)
 
-
     def grad(self, params):
         if self.restriction is not None and np.size(params) > 1:
             params = self.restriction.dot(params)
@@ -317,7 +338,6 @@ class SCADSmoothed(SCAD):
             return weights * value.dot(self.restriction)
         else:
             return weights * value
-
 
     def deriv2(self, params):
         if self.restriction is not None and np.size(params) > 1:
