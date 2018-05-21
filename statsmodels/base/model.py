01ee0cd7e57def726fb554dac49c88d49de08518
Moritz Lotze <lotzemoritz@gmail.com>
2018-05-21 19:26:53 +0200

    FIX clone parameters from model, not from results

--- statsmodels/base/model.py
+++ statsmodels/base/model.py
@@ -2053,7 +2053,7 @@ class ResultMixin(object):
         """
         results = []
         print(self.model.__class__)
-        hascloneattr = True if hasattr(self, 'cloneattr') else False
+        hascloneattr = True if hasattr(self.model, 'cloneattr') else False
         for i in range(nrep):
             rvsind = np.random.randint(self.nobs, size=self.nobs)
             # this needs to set startparam and get other defining attributes
