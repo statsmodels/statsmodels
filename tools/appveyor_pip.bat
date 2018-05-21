46634fea3624e26640749722389808fba3c27919
Matthew Brett <matthew.brett@gmail.com>
2018-05-21 12:17:16 +0100

    Add pip testing in appveyor

---
+++ tools/appveyor_pip.bat
@@ -0,0 +1,7 @@
+# Install packages using pip
+PATH="%PYTHON%:%PYTHON%\Scripts;%PATH%"
+IF Defined SCIPY (
+    pip install numpy scipy==%SCIPY% cython pandas pip nose patsy
+) else (
+    pip install numpy scipy cython pandas pip nose patsy
+)
