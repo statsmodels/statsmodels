74771b0b0a15ea170d5ee7708ffbb12647499bce
abhijeetpanda12 <abhijeet.panda12@gmail.com>
2018-05-22 07:54:00 +0530

    stepwise algorithm added with the test function

--- statsmodels/tsa/automatic/sarimax.py
+++ statsmodels/tsa/automatic/sarimax.py
@@ -3,24 +3,92 @@ import numpy as np
 import pandas as pd
 import statsmodels.api as sm
 
-def auto_order(endog, criteria='aic', d=0, max_order=(3, 3), **spec=None):
+
+def auto_order(
+                endog, criteria='aic', d=0, max_order=(3, 3),
+                stepwise=False, **spec):
     """Auto order function for SARIMAX models."""
-    initp, initq = max_order[0], max_order[1]
-    aic_matrix = np.zeros((initp, initq))
-    aic_matrix[0, 0] = np.inf
-    for p in range(max_order[0]):
-        for q in range(max_order[1]):
-            if p == 0 and q == 0:
-                continue
-            # fit  the model
-            mod = sm.tsa.statespace.SARIMAX(endog, order=(p, d, q), **spec)
+    if not stepwise:
+        aic_matrix = np.zeros((max_order[0], max_order[1]))
+        for p in range(max_order[0]):
+            for q in range(max_order[1]):
+                # fit  the model
+                mod = sm.tsa.statespace.SARIMAX(endog, order=(p, d, q), **spec)
+                res = mod.fit(disp=False)
+                aic_matrix[p, q] = res.aic
+                # print(res.aic)
+        # print(aic_matrix)
+        min_aic = aic_matrix.min()
+        # print(min_aic)
+        p, q = np.where(aic_matrix == min_aic)
+        return p, q
+    else:
+        """stepwise algorithm for auto order"""
+        aic_vals = np.zeros(4)
+        order_init = {
+                        0: [2, 2],
+                        1: [0, 0],
+                        2: [1, 0],
+                        3: [0, 1]
+                    }
+        for model in range(4):
+            mod = sm.tsa.statespace.SARIMAX(
+                endog, order=(order_init[model][0], d, order_init[model][1]),
+                **spec)
             res = mod.fit(disp=False)
-            aic_matrix[p, q] = res.aic
-            print(res.aic)
-#             print(p,q)
-    print(aic_matrix)
-    min_aic = aic_matrix.min()
-    print(min_aic)
-    p, q = np.where(aic_matrix == min_aic)
-    print(p, q)
-    return p, q
+            aic_vals[model] = res.aic
+        print(aic_vals)
+        min_aic = aic_vals.min()
+        model = int(np.where(aic_vals == min_aic)[0])
+        p, q = order_init[model][0], order_init[model][1]
+        # only p varies by +1
+        if(p+1 <= max_order[0]):
+            mod = sm.tsa.statespace.SARIMAX(endog, order=(p+1, d, q), **spec)
+            res = mod.fit(disp=False)
+            # print(res.aic)
+            if (res.aic < min_aic):
+                p += 1
+                min_aic = res.aic
+        # only p varies by -1
+        if(p-1 >= 0):
+            mod = sm.tsa.statespace.SARIMAX(endog, order=(p-1, d, q), **spec)
+            res = mod.fit(disp=False)
+            # print(res.aic)
+            if (res.aic < min_aic):
+                p -= 1
+                min_aic = res.aic
+        # only q varies by +1
+        if(q+1 <= max_order[1]):
+            mod = sm.tsa.statespace.SARIMAX(endog, order=(p, d, q+1), **spec)
+            res = mod.fit(disp=False)
+            # print(res.aic)
+            if (res.aic < min_aic):
+                q += 1
+                min_aic = res.aic
+        # only q varies by -1
+        if(q-1 >= 0):
+            mod = sm.tsa.statespace.SARIMAX(endog, order=(p, d, q-1), **spec)
+            res = mod.fit(disp=False)
+            # print(res.aic)
+            if (res.aic < min_aic):
+                q -= 1
+                min_aic = res.aic
+        # both p and q vary by +1
+        if(p+1 <= max_order[0]) and (q+1 <= max_order[1]):
+            mod = sm.tsa.statespace.SARIMAX(endog, order=(p+1, d, q+1), **spec)
+            res = mod.fit(disp=False)
+            # print(res.aic)
+            if (res.aic < min_aic):
+                p += 1
+                q += 1
+                min_aic = res.aic
+        # both p and q vary by -1
+        if(q-1 >= 0) and (q-1 >= 0):
+            mod = sm.tsa.statespace.SARIMAX(endog, order=(p-1, d, q-1), **spec)
+            res = mod.fit(disp=False)
+            # print(res.aic)
+            if (res.aic < min_aic):
+                p -= 1
+                q -= 1
+                min_aic = res.aic
+        return p, q
--- statsmodels/tsa/automatic/tests/test_sarimax.py
+++ statsmodels/tsa/automatic/tests/test_sarimax.py
@@ -12,12 +12,23 @@ current_path = os.path.dirname(os.path.abspath(__file__))
 macrodata = datasets.macrodata.load_pandas().data
 macrodata.index = pd.PeriodIndex(start='1959Q1', end='2009Q3', freq='Q')
 
+
 def test_non_stepwise():
-    """Auto order function for SARIMAX models."""
-    p, q = sarimax.auto_order(macrodata.infl, d=0)
-    # p, q = sarimax.auto_order(macrodata.infl, d=0, enforce_stationarity=False)
+    """test function for non-stepwise auto_order."""
+    p, q = sarimax.auto_order(macrodata['infl'], d=0)
+#    p, q = sarimax.auto_order(macrodata.infl, d=0, enforce_stationarity=False)
     desired_p = 2
     desired_q = 2
-    assert_equal(p, desired_p)
-    assert_equal(q, desired_q)
+    assert p == desired_p
+    assert q == desired_q
 
+
+def test_stepwise():
+    """test function for stepwise auto_order."""
+    # p, q = sarimax.auto_order(
+    #             macrodata['infl'], stepwise=True, enforce_stationarity=False)
+    p, q = sarimax.auto_order(macrodata['infl'], stepwise=True)
+    desired_p = 3
+    desired_q = 3
+    assert p == desired_p
+    assert q == desired_q
