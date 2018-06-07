ab061c96cf3578e714ac28570279611da672d7f3
thequackdaddy <pquack@gmail.com>
2018-06-07 10:47:50 -0500

    TST: Use newton for pickle tests... appears to be more stable

--- statsmodels/tsa/tests/test_arima.py
+++ statsmodels/tsa/tests/test_arima.py
@@ -2313,8 +2313,8 @@ def test_arma_pickle():
     mod = ARMA(x, (1, 1))
     pkl_mod = cPickle.loads(cPickle.dumps(mod))
 
-    res = mod.fit(trend="c", disp=-1)
-    pkl_res = pkl_mod.fit(trend="c", disp=-1)
+    res = mod.fit(trend="c", disp=-1, solver='newton')
+    pkl_res = pkl_mod.fit(trend="c", disp=-1, solver='newton')
 
     assert_allclose(res.params, pkl_res.params)
     assert_allclose(res.llf, pkl_res.llf)
@@ -2328,8 +2328,8 @@ def test_arima_pickle():
     mod = ARIMA(endog, (1, 1, 1))
     pkl_mod = cPickle.loads(cPickle.dumps(mod))
 
-    res = mod.fit(trend="c", disp=-1)
-    pkl_res = pkl_mod.fit(trend="c", disp=-1)
+    res = mod.fit(trend="c", disp=-1, solver='newton')
+    pkl_res = pkl_mod.fit(trend="c", disp=-1, solver='newton')
 
     assert_allclose(res.params, pkl_res.params)
     assert_allclose(res.llf, pkl_res.llf)
