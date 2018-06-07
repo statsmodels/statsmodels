4e27dfa1a4ebb9ab01ff1fe54fd9097dba43742e
Kerby Shedden <kshedden@umich.edu>
2018-06-07 11:29:49 -0400

    initial commit

--- statsmodels/stats/correlation_tools.py
+++ statsmodels/stats/correlation_tools.py
@@ -11,6 +11,7 @@ import numpy as np
 import scipy.sparse as sparse
 from scipy.sparse.linalg import svds
 from scipy.optimize import fminbound
+import warnings
 
 from statsmodels.tools.tools import Bunch
 from statsmodels.tools.sm_exceptions import (IterationLimitWarning,
@@ -856,6 +857,183 @@ def corr_thresholded(data, minabs=None, max_elt=1e7):
     return cmat
 
 
+class MultivariateKernel(object):
+    """
+    Base class for multivariate kernels.
 
-if __name__ == '__main__':
-    pass
+    Parameters
+    ----------
+    bw : array-like
+        The bandwidths for the components of the kernel.
+
+    Returns
+    -------
+    An instance with `call` method having signature `call(x, loc)`,
+    returning the kernel weights comparing `x` (a 1d ndarray) to each
+    row of `loc` (a 2d ndarray).
+    """
+
+    def call(x):
+        raise NotImplementedError
+
+    def set_bandwidth(self, bw):
+        """
+        Set the bandwidth to the given vector.
+
+        Parameters
+        ----------
+        bw : array-like
+            A vector of non-negative bandwidth values.
+        """
+
+        self.bw = bw
+        self._setup()
+
+    def _setup(self):
+
+        # Precompute the squared bandwidth values.
+        self.bwk = np.prod(self.bw)
+        self.bw2 = self.bw * self.bw
+
+    def set_default_bw(self, loc, bwm=None):
+        """
+        Set default bandwiths based on domain values.
+
+        Parameters
+        ----------
+        loc : array-like
+            Values from the domain to which the kernel will
+            be applied.
+        bwm : scalar, optional
+            A non-negative scalar that is used to multiply
+            the default bandwidth.
+        """
+
+        sd = loc.std(0)
+        q25, q75 = np.percentile(loc, [25, 75], axis=0)
+        iqr = (q75 - q25) / 1.349
+        bw = np.where(iqr < sd, iqr, sd)
+        bw *= 0.9 / loc.shape[0] ** 0.2
+
+        if bwm is not None:
+            bw *= bwm
+
+        # The final bandwidths
+        self.bw = np.asarray(bw, dtype=np.float64)
+
+        self._setup()
+
+
+class GaussianMultivariateKernel(MultivariateKernel):
+    """
+    The Gaussian (squared exponential) multivariate kernel.
+    """
+
+    def call(self, x, loc):
+        return np.exp(-(x - loc)**2 / (2 * self.bw2)).sum(1) / self.bwk
+
+
+def kernel_covariance(exog, loc, group, kernel=None, bw=None):
+    """
+    Use kernel averaging to estimate a multivariate covariance function.
+
+    The goal is to estimate a covariance function C(x, y) =
+    cov(Z(x), Z(y)) where x, y are vectors in R^p (e.g. representing
+    locations in time or space), and Z(.) represents a multivariate
+    process on R^p.
+
+    The data used for estimation can be observed at arbitrary values of the
+    position vector, and there can be multiple independent observations
+    from the process.
+
+    Parameters
+    ----------
+    exog : array-like
+        The rows of exog are realizations of the process obtained at
+        specified points.
+    loc : array-like
+        The rows of loc are the locations (e.g. in space or time) at
+        which the rows of exog are observed.
+    group : array-like
+        The values of group are labels for distinct independent copies
+        of the process.
+    kernel : MultivariateKernel instance, optional
+        An instance of MultivariateKernel, defaults to
+        GaussianMultivariateKernel.
+    bw : array-like or scalar
+        A bandwidth vector, or bandwith multiplier.  If a 1d array, it
+        contains kernel bandwidths for each component of the process, and
+        must have length equal to the number of columns of exog.  If a scalar,
+        bw is a bandwidth multiplier used to adjust the default bandwidth; if
+        None, a default bandwidth is used.
+
+    Returns
+    -------
+    A function C(x, y) which estimates the covariance between
+    values of the process located at x and y.
+
+    References
+    ----------
+    Genton M, W Kleiber (2015).  Cross covariance functions for multivariate
+    geostatics.  Statistical Science 30(2).
+    https://arxiv.org/pdf/1507.08017.pdf
+    """
+
+    exog = np.asarray(exog)
+    loc = np.asarray(loc)
+    group = np.asarray(group)
+
+    if loc.ndim == 1:
+        loc = loc[:, None]
+
+    v = [exog.shape[0], loc.shape[0], len(group)]
+    if min(v) != max(v):
+        msg = "exog, loc, and group must have the same number of rows"
+        raise ValueError(msg)
+
+    # Map from group labels to the row indices in each group.
+    ix = {}
+    for i, g in enumerate(group):
+        if g not in ix:
+            ix[g] = []
+        ix[g].append(i)
+    for g in ix.keys():
+        ix[g] = np.sort(ix[g])
+
+    if kernel is None:
+        kernel = GaussianMultivariateKernel()
+
+    if bw is None:
+        kernel.set_default_bw(loc)
+    elif np.isscalar(bw):
+        kernel.set_default_bw(loc, bwm=bw)
+    else:
+        kernel.set_bandwidth(bw)
+
+    def cov(x, y):
+
+        kx = kernel.call(x, loc)
+        ky = kernel.call(y, loc)
+
+        cm, cw = 0., 0.
+
+        for g, ii in ix.items():
+
+            m = len(ii)
+            j1, j2 = np.indices((m, m))
+            j1 = ii[j1.flat]
+            j2 = ii[j2.flat]
+            w = kx[j1] * ky[j2]
+
+            cm += np.einsum("ij,ik,i->jk", exog[j1, :], exog[j2, :], w)
+            cw += w.sum()
+
+        if cw < 1e-10:
+            msg = ("Effective sample size is 0.  The bandwidth may be too " +
+                   "small, or you are outside the range of your data.")
+            warnings.warn(msg)
+            return np.nan * np.ones_like(cm)
+
+        return cm / cw
+
+    return cov
