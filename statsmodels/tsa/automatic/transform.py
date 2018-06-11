5420d78c05e715a9ceef1629a86d7b6ece0703b7
abhijeetpanda12 <abhijeet.panda12@gmail.com>
2018-06-11 22:12:59 +0530

    code cleaned with minor modifications

--- statsmodels/tsa/automatic/transform.py
+++ statsmodels/tsa/automatic/transform.py
@@ -19,31 +19,29 @@ def create_exog(nobs, d=0, seasonal_periods=1):
                         season[i-1][j] = 1
         for series in season:
             exog.append(series)
+    exog = np.array(exog)
+    exog = exog.transpose()
     return exog
 
 
-def predict_lambda(endog, lambda_range, nobs, d, seasonal_periods, offset=True):
-
-    for val in endog:
-        if val < 0:
-            raise ValueError('Negative values hould not be present in endog')
+def predict_lambda(endog, lambda_range, d, seasonal_periods, offset=True, gridsize=11):
+    nobs = len(endog)
+    if np.any(endog < 0):
+        raise ValueError('Negative values hould not be present in endog')
 
     exog = create_exog(nobs, d=d, seasonal_periods=seasonal_periods)
-    exog = np.array(exog)
-    exog = exog.transpose()
     if offset:
         endog = endog + np.min(endog)/2
 
-    y_dot = gmean(endog)
+    y_gmean = gmean(endog)
 
     min_ssr = np.inf
-    for lambda_val in range(lambda_range[0], lambda_range[1]+1):
+    for lambda_val in np.linspace(lambda_range[0], lambda_range[1], num=gridsize):
         if lambda_val == 0:
-            v = y_dot * np.log(endog)
+            y_transform = y_gmean * np.log(endog)
         else:
-            v = ((endog**lambda_val) - 1)/(lambda_val * (y_dot**lambda_val-1))
-        model = sm.OLS(v[:nobs], exog).fit()
-        predictions = model.predict(exog)
+            y_transform = ((endog ** lambda_val) - 1) / (lambda_val * (y_gmean ** lambda_val - 1))
+        model = sm.OLS(y_transform[:nobs], exog).fit()
     #     print(model.ssr)
         if model.ssr < min_ssr:
             min_ssr = model.ssr
