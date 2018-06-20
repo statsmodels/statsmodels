5bdb7d1ef75e9034c8defac1bbbe9b1598296924
abhijeetpanda12 <abhijeet.panda12@gmail.com>
2018-06-21 00:07:54 +0530

    auto transform unit test fixed

--- statsmodels/tsa/automatic/tests/results/autotransform.R
+++ statsmodels/tsa/automatic/tests/results/autotransform.R
@@ -1,3 +1,15 @@
 library("forecast")
-macrodata <- read.csv('/Users/abhijeet/gsoc/dev/statsmodels/statsmodels/datasets/macrodata/macrodata.csv')
-lambda <- BoxCox.lambda(macrodata['cpi'], method = 'loglik', lower = -1, upper = 2)
+macrodata <- read.csv('../../../../datasets/macrodata/macrodata.csv')
+
+# Here y is not a ts() object, so only is fit with a mean
+y <- macrodata$cpi
+lambda_d0s1 <- BoxCox.lambda(y, method = 'loglik', lower = -1, upper = 2)
+
+# y is now a ts() object, so it now includes a time trend, but no seasonal
+y <- ts(macrodata$cpi)
+lambda_d1s1 <- BoxCox.lambda(y, method = 'loglik', lower = -1, upper = 2)
+
+# y is now a ts() object w/ frequency, so it now includes a time trend
+# and a seasonal
+y <- ts(macrodata$cpi, frequency=12)
+lambda_d1s12 <- BoxCox.lambda(y, method = 'loglik', lower = -1, upper = 2)
--- statsmodels/tsa/automatic/tests/test_transform.py
+++ statsmodels/tsa/automatic/tests/test_transform.py
@@ -9,10 +9,19 @@ from numpy.testing import assert_equal, assert_allclose
 from statsmodels.tsa.automatic import transform
 
 macrodata = datasets.macrodata.load_pandas().data
-
+data = sm.datasets.macrodata.load_pandas().data['cpi']
 
 def test_transform():
-    result = transform.predict_lambda(macrodata['cpi'], (-1, 2), d=0,
-                                      seasonal_periods=1, offset=False,
-                                      gridsize=61)
-    assert_equal(result, 1)
+    lambda_d0s1 = transform.predict_lambda(endog=data, lambda_range=(-1, 2),
+                                           d=0, offset=False,
+                                           seasonal_periods=1, gridsize=61)
+    lambda_d1s1 = transform.predict_lambda(endog=data, lambda_range=(-1, 2),
+                                           d=1, offset=False,
+                                           seasonal_periods=1, gridsize=61)
+    lambda_d1s12 = transform.predict_lambda(endog=data, lambda_range=(-1, 2),
+                                            d=1, offset=False,
+                                            seasonal_periods=12, gridsize=61)
+    assert_allclose(lambda_d0s1, 0.35)
+    assert_allclose(lambda_d1s1, 0.6)
+    assert_allclose(lambda_d1s12, 0.6)
+    # assert_equal(result, 0.35)
