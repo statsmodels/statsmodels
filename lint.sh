43ff3a3e8f0889160ab75c3d0498d4f910276efc
Brock Mendel <jbrockmendel@gmail.com>
2018-07-31 20:59:40 -0700

    revert NameError checks, because those arent important

--- lint.sh
+++ lint.sh
@@ -4,7 +4,7 @@ echo "inside $0"
 
 RET=0
 
-flake8 statsmodels --select=E901,E999,F821,F822,F823,E111,E114
+flake8 statsmodels --select=E901,E999,F822,F823,E111,E114
 if [ $? -ne "0" ]; then
     RET=1
 fi
