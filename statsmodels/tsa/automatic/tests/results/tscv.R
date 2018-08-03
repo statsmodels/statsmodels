8fcbd5c664f61fcfa26bb4529345e252d8575dd4
abhijeetpanda12 <abhijeet.panda12@gmail.com>
2018-08-03 23:38:14 +0530

    TSCV test for CPI data added

--- statsmodels/tsa/automatic/tests/results/tscv.R
+++ statsmodels/tsa/automatic/tests/results/tscv.R
@@ -1,7 +1,11 @@
 library("forecast")
 macrodata <- read.csv('../../../../datasets/macrodata/macrodata.csv')
 x <- ts(macrodata$infl)
+y <- ts(macrodata$cpi)
 far2 <- function(x, h){forecast(Arima(x, order=c(1,0,0), init=c(0.5, 1.5),
                                       optim.control=list(maxit=0)), h=h)}
 e <- tsCV(x, far2, h=1, window=30)
 write.csv(e, file = "result_tscv.csv")
+
+e <- tsCV(y, far2, h=1, window=30)
+write.csv(e, file = "result_tscv_cpi.csv")
--- statsmodels/tsa/automatic/tests/test_tscv.py
+++ statsmodels/tsa/automatic/tests/test_tscv.py
@@ -9,21 +9,34 @@ from numpy.testing import assert_equal, assert_allclose
 macrodata = datasets.macrodata.load_pandas().data
 macrodata.index = pd.PeriodIndex(start='1959Q1', end='2009Q3', freq='Q')
 dataset = macrodata['infl']
+dataset_cpi = macrodata['cpi']
 
 curdir = os.path.dirname(os.path.abspath(__file__))
 data_file = os.path.join(curdir, "results/result_tscv.csv")
 data = pd.read_csv(data_file, header=None)
 
+curdir = os.path.dirname(os.path.abspath(__file__))
+data_file = os.path.join(curdir, "results/result_tscv_cpi.csv")
+data_cpi = pd.read_csv(data_file, header=None)
+
 
 def forecast_fixed_ar1(endog, horizon=1):
-    """Test Function."""
+    """Forecast Function."""
     mod = sm.tsa.SARIMAX(endog, order=(1, 0, 0), trend='c',
                          concentrate_scale=True)
     res = mod.filter([0.75, 0.5])
     return res.forecast(horizon)
 
 
-def test_tscv_unit():
+def test_tscv_unit_1():
+    """Test Function."""
     e = tscv.evaluate(dataset, forecast_fixed_ar1, roll_window=30)
     for i in range(30, 203):
         assert_allclose(float(data.iloc[i, 1]), e[i])
+
+
+def test_tscv_unit_2():
+    """Test Function."""
+    e = tscv.evaluate(dataset_cpi, forecast_fixed_ar1, roll_window=30)
+    for i in range(30, 203):
+        assert_allclose(float(data_cpi.iloc[i, 1]), e[i])
