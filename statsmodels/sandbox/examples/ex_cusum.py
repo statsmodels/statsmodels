2fb1b853922f53e84822dd09d7b270927bd68314
Brock Mendel <jbrockmendel@gmail.com>
2018-08-04 16:47:42 -0700

    Fix and lint for E227

--- statsmodels/sandbox/examples/ex_cusum.py
+++ statsmodels/sandbox/examples/ex_cusum.py
@@ -80,7 +80,7 @@ plt.plot(rresid)
 plt.plot(np.abs(rresid))
 
 print('cusum test reject:')
-print(((rcusum[1:]>rcusumci[1])|(rcusum[1:]<rcusumci[0])).any())
+print(((rcusum[1:]>rcusumci[1]) | (rcusum[1:]<rcusumci[0])).any())
 
 rresid2, rparams2, rypred2, rresid_standardized2, rresid_scaled2, rcusum2, rcusumci2 = \
             recursive_olsresiduals2(res1, skip)
