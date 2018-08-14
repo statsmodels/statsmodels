f5096c625b17e16c822dfc69281e6d8fc8b3d2b6
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2018-08-14 23:10:02 +0100

    BLD: Rebase changes
    Fix difference due to upstream changes

--- tools/travis_conda.sh
+++ tools/travis_conda.sh
@@ -1,18 +1,19 @@
 # Source to configure conda CI builds
-wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 chmod +x miniconda.sh
 ./miniconda.sh -b -p /home/travis/miniconda
 export PATH=/home/travis/miniconda/bin:$PATH
 conda config --set always_yes yes
 conda update --quiet conda
 # Build package list to avoid empty package=versions; only needed for versioned packages
-PKGS="python=${PYTHON}"
-PKGS="${PKGS} numpy"; if [ ${NUMPY} ]; then PKGS="${PKGS}=${NUMPY}"; fi
+PKGS="numpy"; if [ ${NUMPY} ]; then PKGS="${PKGS}=${NUMPY}"; fi
+if [ ${USENUMPYBASE} = true ]; then PKGS="${PKGS} numpy-base=${NUMPY}"; fi
 PKGS="${PKGS} scipy"; if [ ${SCIPY} ]; then PKGS="${PKGS}=${SCIPY}"; fi
 PKGS="${PKGS} patsy"; if [ ${PATSY} ]; then PKGS="${PKGS}=${PATSY}"; fi
 PKGS="${PKGS} pandas"; if [ ${PANDAS} ]; then PKGS="${PKGS}=${PANDAS}"; fi
 PKGS="${PKGS} Cython"; if [ ${CYTHON} ]; then PKGS="${PKGS}=${CYTHON}"; fi
 if [ ${USEMPL} = true ]; then PKGS="${PKGS} matplotlib"; if [ ${MATPLOTLIB} ]; then PKGS="${PKGS}=${MATPLOTLIB}"; fi; fi
-echo conda create --yes --quiet -n statsmodels-test ${PKGS} ${OPTIONAL} pyyaml
-conda create --yes --quiet -n statsmodels-test ${PKGS} ${OPTIONAL} ${DEPEND_ALWAYS}
+if [ ${COVERAGE} = true ]; then export COVERAGE_OPTS=" --cov-config=.travis_coveragerc --cov=statsmodels "; else export COVERAGE_OPTS=""; fi
+echo conda create --yes --quiet -n statsmodels-test python=${PYTHON} ${BLAS} ${PKGS} ${OPTIONAL} ${DEPEND_ALWAYS}
+conda create --yes --quiet -n statsmodels-test python=${PYTHON} ${BLAS} ${PKGS} ${OPTIONAL} ${DEPEND_ALWAYS}
 source activate statsmodels-test
