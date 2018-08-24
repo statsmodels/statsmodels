1775ac1f69dcf83c4ff0958ba81f34d8fb35fe06
Brock Mendel <jbrockmendel@gmail.com>
2018-08-23 19:05:59 -0700

    [TST] make tools.linalg __main__ section into tests

--- lint.sh
+++ lint.sh
@@ -22,6 +22,8 @@ if [ "$LINT" == true ]; then
         statsmodels/regression/mixed_linear_model.py \
         statsmodels/duration/__init__.py \
         statsmodels/regression/recursive_ls.py \
+        statsmodels/tools/linalg.py \
+        statsmodels/tools/tests/test_linalg.py \
         conftest.py
     if [ $? -ne "0" ]; then
         RET=1
