5d7e8c2edd80de90c318eccbeed942367c1e03ca
Brock Mendel <jbrockmendel@gmail.com>
2018-08-24 13:34:44 -0700

    [MAINT] add missing import

--- statsmodels/sandbox/tsa/examples/ex_mle_garch.py
+++ statsmodels/sandbox/tsa/examples/ex_mle_garch.py
@@ -65,15 +65,17 @@ from __future__ import print_function
 import numpy as np
 from numpy.testing import assert_almost_equal
 
+from scipy import optimize
+
 import matplotlib.pyplot as plt
 import numdifftools as ndt
 
 import statsmodels.api as sm
 from statsmodels.sandbox import tsa
 from statsmodels.sandbox.tsa.garch import (generate_kindofgarch,
-                                          generate_garch, generate_gjrgarch,
-                                          garchplot, Garch, Garch0,
-                                          loglike_GARCH11, Arma, miso_lfilter)
+                                           generate_garch, generate_gjrgarch,
+                                           garchplot, Garch, Garch0,
+                                           loglike_GARCH11, Arma, miso_lfilter)
 
 
 nobs = 1000
