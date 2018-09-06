909f72a8972f9d0a34891ef884dd81797a95e910
Brock Mendel <jbrockmendel@gmail.com>
2018-09-06 13:11:06 -0700

    BUG: Fix NameError in grouputils, make __main__ into tests

--- statsmodels/tools/tests/test_grouputils.py
+++ statsmodels/tools/tests/test_grouputils.py
@@ -1,12 +1,16 @@
-from statsmodels.compat.python import PY37
+from statsmodels.compat.python import PY37, lrange
 
 import numpy as np
+from numpy.testing import assert_equal
 import pandas as pd
-from statsmodels.tools.grouputils import Grouping
+from pandas.util import testing as tm
+
+from statsmodels.tools.grouputils import (
+    Grouping, Group, combine_indices, group_sums)
 from statsmodels.tools.tools import categorical
 from statsmodels.datasets import grunfeld, anes96
-from pandas.util import testing as ptesting
 import pytest
+
 
 class CheckGrouping(object):
 
@@ -23,7 +27,7 @@ class CheckGrouping(object):
         sorted_data, index = self.grouping.sort(self.data)
         expected_sorted_data = self.data.sort_index()
 
-        ptesting.assert_frame_equal(sorted_data, expected_sorted_data)
+        tm.assert_frame_equal(sorted_data, expected_sorted_data)
         np.testing.assert_(isinstance(sorted_data, pd.DataFrame))
         np.testing.assert_(not index.equals(self.grouping.index))
 
@@ -42,7 +46,7 @@ class CheckGrouping(object):
         sorted_data, index = self.grouping.sort(series)
 
         expected_sorted_data = series.sort_index()
-        ptesting.assert_series_equal(sorted_data, expected_sorted_data)
+        tm.assert_series_equal(sorted_data, expected_sorted_data)
         np.testing.assert_(isinstance(sorted_data, pd.Series))
         if hasattr(sorted_data, 'equals'):
             np.testing.assert_(not sorted_data.equals(series))
@@ -254,3 +258,53 @@ def test_init_api():
     grouping = Grouping(list_groups)
     np.testing.assert_array_equal(grouping.group_names,
                                   ['group0', 'group1', 'group2'])
+
+
+def test_combine_indices():
+    # Moved from grouputils __main__ section
+    np.random.seed(985367)
+    groups = np.random.randint(0, 2, size=(10, 2))
+    uv, ux, u, label = combine_indices(groups, return_labels=True)
+    uv, ux, u, label = combine_indices(groups, prefix='g1,g2=', sep=',',
+                                       return_labels=True)
+
+    group0 = np.array(['sector0', 'sector1'])[groups[:, 0]]
+    group1 = np.array(['region0', 'region1'])[groups[:, 1]]
+    uv, ux, u, label = combine_indices((group0, group1),
+                                       prefix='sector,region=',
+                                       sep=',',
+                                       return_labels=True)
+    uv, ux, u, label = combine_indices((group0, group1), prefix='', sep='.',
+                                       return_labels=True)
+    group_joint = np.array(label)[uv]
+    group_joint_expected = np.array(['sector1.region0', 'sector0.region1',
+                                     'sector0.region0', 'sector0.region1',
+                                     'sector1.region1', 'sector0.region0',
+                                     'sector1.region0', 'sector1.region0',
+                                     'sector0.region1', 'sector0.region0'],
+                                    dtype='|S15')
+    assert_equal(group_joint, group_joint_expected)
+
+
+@pytest.mark.smoke
+def test_group_sums():
+    # Moved from grouputils __main__ section
+    g = np.array([0, 0, 1, 2, 1, 1, 2, 0])
+
+    group_sums(np.arange(len(g)*3*2).reshape(len(g), 3, 2), g,
+               use_bincount=False).T
+    group_sums(np.arange(len(g)*3*2).reshape(len(g), 3, 2)[:, :, 0], g)
+    group_sums(np.arange(len(g)*3*2).reshape(len(g), 3, 2)[:, :, 1], g)
+
+
+@pytest.mark.smoke
+def test_group_class():
+    # Moved from grouputils __main__ section
+    g = np.array([0, 0, 1, 2, 1, 1, 2, 0])
+
+    x = np.arange(len(g)*3).reshape(len(g), 3, order='F')
+    mygroup = Group(g)
+
+    mygroup.group_int
+    mygroup.group_sums(x)
+    mygroup.labels()
