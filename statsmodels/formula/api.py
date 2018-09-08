779c837bf4c3897907a5228ac1fb86d29b6b3e66
Daniel Saxton <daniel.saxton@gmail.com>
2018-09-08 14:13:30 -0500

    add trailing underscores to temporary imports

--- statsmodels/formula/api.py
+++ statsmodels/formula/api.py
@@ -1,28 +1,28 @@
-import statsmodels.regression.linear_model as lm
-import statsmodels.discrete.discrete_model as dm
-import statsmodels.regression.mixed_linear_model as mlm
-import statsmodels.genmod.generalized_linear_model as glm
-import statsmodels.robust.robust_linear_model as roblm
-import statsmodels.regression.quantile_regression as qr
-import statsmodels.duration.hazard_regression as hr
-import statsmodels.genmod.generalized_estimating_equations as gee
+import statsmodels.regression.linear_model as lm_
+import statsmodels.discrete.discrete_model as dm_
+import statsmodels.regression.mixed_linear_model as mlm_
+import statsmodels.genmod.generalized_linear_model as glm_
+import statsmodels.robust.robust_linear_model as roblm_
+import statsmodels.regression.quantile_regression as qr_
+import statsmodels.duration.hazard_regression as hr_
+import statsmodels.genmod.generalized_estimating_equations as gee_
 
-gls = lm.GLS.from_formula
-wls = lm.WLS.from_formula
-ols = lm.OLS.from_formula
-glsar = lm.GLSAR.from_formula
-mixedlm = mlm.MixedLM.from_formula
-glm = glm.GLM.from_formula
-rlm = roblm.RLM.from_formula
-mnlogit = dm.MNLogit.from_formula
-logit = dm.Logit.from_formula
-probit = dm.Probit.from_formula
-poisson = dm.Poisson.from_formula
-negativebinomial = dm.NegativeBinomial.from_formula
-quantreg = qr.QuantReg.from_formula
-phreg = hr.PHReg.from_formula
-ordinal_gee = gee.OrdinalGEE.from_formula
-nominal_gee = gee.NominalGEE.from_formula
-gee = gee.GEE.from_formula
+gls = lm_.GLS.from_formula
+wls = lm_.WLS.from_formula
+ols = lm_.OLS.from_formula
+glsar = lm_.GLSAR.from_formula
+mixedlm = mlm_.MixedLM.from_formula
+glm = glm_.GLM.from_formula
+rlm = roblm_.RLM.from_formula
+mnlogit = dm_.MNLogit.from_formula
+logit = dm_.Logit.from_formula
+probit = dm_.Probit.from_formula
+poisson = dm_.Poisson.from_formula
+negativebinomial = dm_.NegativeBinomial.from_formula
+quantreg = qr_.QuantReg.from_formula
+phreg = hr_.PHReg.from_formula
+ordinal_gee = gee_.OrdinalGEE.from_formula
+nominal_gee = gee_.NominalGEE.from_formula
+gee = gee_.GEE.from_formula
 
-del lm, dm, mlm, glm, roblm, qr, hr
+del lm_, dm_, mlm_, glm_, roblm_, qr_, hr_, gee_
