294d8316d8fcceebde43e7cc3ba0b2f20f5f1aea
AliHeggie <alastair.heggie@gmail.com>
2018-09-20 09:04:43 +0100

    docstrings & system matrix properties
    Added numpy style docstrings and created new methods/properties to
    create system matrices

--- statsmodels/tsa/statespace/dynamic_regression.py
+++ statsmodels/tsa/statespace/dynamic_regression.py
@@ -1,3 +1,9 @@
+"""
+Univariate time-varying coefficient regression model
+
+Author: Alastair Heggie
+License: Simplified-BSD
+"""
 import pdb
 import numpy as np
 from scipy.signal import lfilter
@@ -8,6 +14,31 @@ from .tools import (constrain_stationary_univariate,
 
 # Construct the model
 class SDR(sm.tsa.statespace.MLEModel):
+    """
+    Univariate time-varying coefficient regression models
+    
+    A model in which the coefficients of the exogonous covariates are 
+    described by a univariate structural model
+
+    Parameters
+    ----------
+    exog : array_like or None, optional
+        Exogenous variables.
+    freq_seasonal: list of dicts or None, optional.
+        Whether (and how) to model seasonal component(s) with trig. functions.
+        If specified, there is one dictionary for each frequency-domain
+        seasonal component.  Each dictionary must have the key, value pair for
+        'period' -- integer and may have a key, value pair for
+        'harmonics' -- integer. If 'harmonics' is not specified in any of the
+        dictionaries, it defaults to the floor of period/2.
+    arma_order : tuple
+        A tuple giving the order of the autoregressive and moving average
+        components of the error term respectively. (0,0) by default.
+    dynamic_regression : bool
+        Whether or not the regression coefficient updates over time. If False
+        then the states corresponding to the regression coefficients have 0
+        variance.
+    """
     def __init__(self, endog, exog, freq_seasonal,arma_order=(0,0),
         dynamic_regression=True):
         self.arma_order = arma_order
@@ -21,7 +52,6 @@ class SDR(sm.tsa.statespace.MLEModel):
         self.enforce_stationarity = True
         self.enforce_invertibility = True
         self.dynamic_regression = dynamic_regression
-
 
         #Get k_exog from size of exog data
         try:
@@ -37,24 +67,59 @@ class SDR(sm.tsa.statespace.MLEModel):
          k_posdef=k_state_cov, initialization='approximate_diffuse')
         self.k_posdef=k_state_cov
         self.k_state_cov=k_state_cov
-
         #Construct the design matrix
+        self.ssm.shapes['design'] = (self.k_endog,self.k_states,self.nobs)
+        self.ssm['design'] = self.initial_design
+        #construct transition matrix
+        self.ssm['transition'] = self.initial_transition
+        #construct selection matrix
+        self.ssm['selection'] = self.initial_selection
+        #construct intercept matrices
+        self.obs_intecept = np.zeros(1)
+        self.ssm['obs_intercept'] = self.obs_intecept
+        self.state_intercept = np.zeros((self.k_states,1))
+        self.ssm['state_intercept'] = self.state_intercept
+        #construct covariance matrices
+        self.obs_cov =  np.zeros(1)
+        self.ssm['obs_cov'] = self.obs_cov
+        self.state_cov = np.zeros((self.k_state_cov,self.k_state_cov))
+        self.ssm['state_cov'] = self.state_cov
+
+    @property
+    def start_params(self):
+        """
+        Starting parameters for maximum likelihood estimation
+        
+        first ar_order terms are ar_params, next ma_order terms are ma params
+        next k_state_cov params are the state covariance, the first of which
+        is the observation covariance which is in the state due to ARMA model
+        """
+        params = np.zeros(self.ar_order + self.ma_order + self.k_state_cov)
+        params[self.ar_order + self.ma_order] = np.nanvar(self.ssm.endog)
+        return params
+
+    @property
+    def initial_design(self):
+        """Initial design matrix"""
+        # Basic design matrix
         design = np.vstack((np.ones(self.nobs),np.zeros((self.r-1,self.nobs)),self.exog.transpose()))
         for ix, h in enumerate(self.freq_seasonal_harmonics):
             series = self.exog[:,ix]
             lines=np.array([series,np.repeat(0,self.nobs)])
             array=np.vstack(tuple(lines for i in range(0,h)))
             design = np.vstack((design,array))
-        self.ssm.shapes['design'] = (self.k_endog,self.k_states,self.nobs)
-        self.ssm['design'] = np.expand_dims(design,axis=0)
+        design = np.expand_dims(design,axis=0)
+        return design
 
-        #construct transition matrix
-        self.transition = np.zeros((self.k_states,self.k_states))
+    @property
+    def initial_transition(self):
+        """Initial transition matrix"""
+        transition = np.zeros((self.k_states,self.k_states))
         #ARMA
-        self.transition[1:self.r,0:self.r-1] = np.eye(self.r-1)
+        transition[1:self.r,0:self.r-1] = np.eye(self.r-1)
         #Exog level
         i = self.r
-        self.transition[i:i+self.k_exog,i:i+self.k_exog] = np.eye(self.k_exog)
+        transition[i:i+self.k_exog,i:i+self.k_exog] = np.eye(self.k_exog)
         #Exog seasonal
         i=self.r + self.k_exog
         for ix, h in enumerate(self.freq_seasonal_harmonics):
@@ -68,52 +133,54 @@ class SDR(sm.tsa.statespace.MLEModel):
                 trans = np.array([[cos_lambda_block, sin_lambda_block],
                                   [-sin_lambda_block, cos_lambda_block]])
                 trans_s = np.s_[i + t:i + t + 2]
-                self.transition[trans_s, trans_s] = trans
+                transition[trans_s, trans_s] = trans
                 t += 2
             i += n
-        self.ssm['transition']=self.transition
-
-        #construct selection matrix
-        self.ssm.selection = np.zeros((self.k_states,self.k_state_cov))
+        return transition
+    
+    @property
+    def initial_selection(self):
+        """Initial selection matrix"""
+        selection = np.zeros((self.k_states,self.k_state_cov))
         #ARMA
         i,j = 0,0
-        self.ssm.selection[i,j] = 1
+        selection[i,j] = 1
         i += self.r
         j += 1
         if self.dynamic_regression:
             #Exog level
-            self.ssm.selection[i:i+self.k_exog,j:j+self.k_exog,0] = np.eye(self.k_exog)
+            selection[i:i+self.k_exog,j:j+self.k_exog] = np.eye(self.k_exog)
             i += self.k_exog
             j += self.k_exog
             #Exog seasonal
             for ix, h in enumerate(self.freq_seasonal_harmonics):
                 select = np.vstack([np.eye(2) for i in range(h)])
-                self.ssm.selection[i:i+2*h,j:j+2,0] = select
+                selection[i:i+2*h,j:j+2] = select
                 i += 2*h
                 j += 2
-        #construct intercept matrices
-        self.obs_intecept = np.zeros(1)
-        self.ssm['obs_intercept'] = self.obs_intecept
-        self.state_intercept = np.zeros((self.k_states,1))
-        self.ssm['state_intercept'] = self.state_intercept
-        #construct covariance matrices
-        self.obs_cov =  np.zeros(1)
-        self.ssm['obs_cov'] = self.obs_cov
-        self.state_cov = np.zeros((self.k_state_cov,self.k_state_cov))
-        self.ssm['state_cov'] = self.state_cov
+        selection = np.expand_dims(selection,axis=2)
+        return selection
 
-
-    @property
-    def start_params(self):
-        # first ar_order terms are ar_params, next ma_order terms are ma params
-        # next k_state_cov params are the state covariance, the first of which
-        # is the observation covariance which is in the state due to ARMA model
-        params = np.zeros(self.ar_order + self.ma_order + self.k_state_cov)
-        params[self.ar_order + self.ma_order] = np.nanvar(self.ssm.endog)
-        return params
-
-    # Describe how parameters enter the model
     def update(self, params, transformed=True, **kwargs):
+        """
+        Update the parameters of the model
+
+        Updates the representation matrices to fill in the new parameter
+        values.
+
+        Parameters
+        ----------
+        params : array_like
+            Array of new parameters.
+        transformed : boolean, optional
+            Whether or not `params` is already transformed. If set to False,
+            `transform_params` is called. Default is True..
+
+        Returns
+        -------
+        params : array_like
+            Array of parameters.
+        """
         ar_params = params[0:self.ar_order]
         ma_params = params[self.ar_order:self.ma_order+self.ar_order]
         state_cov_params = params[self.ma_order+self.ar_order:]
@@ -127,6 +194,24 @@ class SDR(sm.tsa.statespace.MLEModel):
 
 
     def transform_params(self, unconstrained):
+        """
+        Transform unconstrained parameters used by the optimizer to constrained
+        parameters used in likelihood evaluation.
+
+        Used primarily to enforce stationarity of the autoregressive lag
+        polynomial, invertibility of the moving average lag polynomial, and
+        positive variance parameters.
+
+        Parameters
+        ----------
+        unconstrained : array_like
+            Unconstrained parameters used by the optimizer.
+
+        Returns
+        -------
+        constrained : array_like
+            Constrained parameters used in likelihood evaluation.
+        """
         unconstrained = np.array(unconstrained, ndmin=1)
         constrained = np.zeros(unconstrained.shape, unconstrained.dtype)
         # Transform the AR parameters (phi) to be stationary
@@ -157,6 +242,24 @@ class SDR(sm.tsa.statespace.MLEModel):
         return constrained
     
     def untransform_params(self, constrained):
+        """
+        Transform constrained parameters used in likelihood evaluation
+        to unconstrained parameters used by the optimizer
+
+        Used primarily to reverse enforcement of stationarity of the
+        autoregressive lag polynomial and invertibility of the moving average
+        lag polynomial.
+
+        Parameters
+        ----------
+        constrained : array_like
+            Constrained parameters used in likelihood evaluation.
+
+        Returns
+        -------
+        constrained : array_like
+            Unconstrained parameters used by the optimizer.
+        """
         constrained = np.array(constrained, ndmin=1)
         unconstrained = np.zeros(constrained.shape, constrained.dtype)
         # Transform the AR parameters (phi) to be stationary
