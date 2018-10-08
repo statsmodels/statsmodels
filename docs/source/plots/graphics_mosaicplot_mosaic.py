fd199c62bce5376914650ed28574824742ed0f72
jtweeder <31365056+jtweeder@users.noreply.github.com>
2018-10-08 11:00:49 -0500

    whitespace clean up
    incorporating whitespace and comment clean up from linting and codacy review

--- docs/source/plots/graphics_mosaicplot_mosaic.py
+++ docs/source/plots/graphics_mosaicplot_mosaic.py
@@ -10,10 +10,9 @@ data = {'a': 10, 'b': 15, 'c': 16}
 mosaic(data, title='basic dictionary')
 plt.show()
 
-'''
-A more useful example is given by a dictionary with multiple indices.  In this
-case we use a wider gap to a better visual separation of the resulting plot
-'''
+# A more useful example is given by a dictionary with multiple indices.  In
+# this case we use a wider gap to a better visual separation of the resulting
+# plot
 data = {('a', 'b'): 1, ('a', 'c'): 2, ('d', 'b'): 3, ('d', 'c'): 4}
 mosaic(data, gap=0.05, title='complete dictionary')
 plt.show()
@@ -26,25 +25,25 @@ data = pd.Series(rand(8), index=index)
 mosaic(data, title='hierarchical index series')
 plt.show()
 
-'''
-The third accepted data structureis the np array, for which a very simple index
-will be created.
-'''
+# The third accepted data structureis the np array, for which a very simple
+# index will be created.
 rand = np.random.random
 data = 1+rand((2, 2))
 mosaic(data, title='random non-labeled array')
 plt.show()
 
-'''
-If you need to modify the labeling and the coloring you can give a function to
-create the labels and one with the graphical properties starting from the key
-tuple
-'''
+# If you need to modify the labeling and the coloring you can give a function
+# to create the labels and one with the graphical properties starting from the
+# key tuple
+
+
 def props(key):
     return {'color': 'r' if 'a' in key else 'gray'}
 
+
 def labelizer(key):
     return {('a',): 'first', ('b',): 'second', ('c',): 'third'}[key]
+
 
 data = {'a': 10, 'b': 15, 'c': 16}
 mosaic(data, title='colored dictionary', properties=props, labelizer=labelizer)
