40b842c9a7e82d44f9d32df7dc0bfd0a1815ee3e
thequackdaddy <pquack@gmail.com>
2018-10-12 17:07:04 -0500

    ipython 7.0.1 counts magic as the "top" of the file, so you can't have a from __future__ in same cell.
    Try unpinned testpath
    
    Remove unneeded requierments

--- tools/ci/docbuild_install.sh
+++ tools/ci/docbuild_install.sh
@@ -4,11 +4,11 @@
 echo sudo apt-get install graphviz -qq
 sudo apt-get install graphviz -qq
 # Install required packages
-echo conda install sphinx "ipython<7.0" jupyter nbconvert numpydoc tzlocal "testpath<0.4.0" --yes --quiet
-conda install sphinx "ipython<7.0" jupyter nbconvert numpydoc tzlocal "testpath<0.4.0" --yes --quiet
+echo conda install sphinx ipython jupyter nbconvert numpydoc tzlocal --yes --quiet
+conda install sphinx ipython jupyter nbconvert numpydoc tzlocal --yes --quiet
 # doctr and pdr
-echo pip install colorama doctr pandas-datareader
-pip install colorama doctr pandas-datareader
+echo pip install colorama doctr pandas-datareader simplegeneric
+pip install colorama doctr pandas-datareader simplegeneric  # TODO: Remove simplegeneric after rpy2 updated
 # R and dependencies
 echo conda install --channel conda-forge/label/gcc7 rpy2 r-robustbase r-lme4 r-geepack libiconv --yes --quiet
 conda install --channel conda-forge/label/gcc7 rpy2 r-robustbase r-lme4 r-geepack libiconv --yes --quiet
