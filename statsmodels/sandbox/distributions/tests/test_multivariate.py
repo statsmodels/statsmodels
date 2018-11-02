0b5045276db2725d8c0036887774450e53863656
thequackdaddy <pquack@gmail.com>
2018-11-02 12:21:34 -0500

    Change float to e format

--- statsmodels/sandbox/distributions/tests/test_multivariate.py
+++ statsmodels/sandbox/distributions/tests/test_multivariate.py
@@ -70,9 +70,9 @@ class Test_MVN_MVT_prob(object):
         probmvt_R = 0.9522146
         quadkwds = {'epsabs': 1e-08}
         probmvt = mvstdtprob(a2, b, corr2, df, quadkwds=quadkwds)
-        assert_allclose(probmvt_R, probmvt, atol=0.0005)
+        assert_allclose(probmvt_R, probmvt, atol=5e-4)
         probmvn = mvstdnormcdf(a2, b, corr2, maxpts=100000, abseps=1e-5)
-        assert_allclose(probmvn_R, probmvn, atol=0.0001)
+        assert_allclose(probmvn_R, probmvn, atol=1e-4)
 
     def test_mvn_mvt_4(self):
         a, bl = self.a, self.b
