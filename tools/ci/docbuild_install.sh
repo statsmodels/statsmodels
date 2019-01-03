43eaacf425d9327bfec2f17cb60102bf27604779
thequackdaddy <pquack@gmail.com>
2019-01-03 00:07:52 -0600

    Simplify package installation

--- tools/ci/docbuild_install.sh
+++ tools/ci/docbuild_install.sh
@@ -3,12 +3,9 @@
 # Install system dependencies
 echo sudo apt-get install graphviz -qq
 sudo apt-get install graphviz -qq
-# Install required packages
-echo conda install sphinx ipython jupyter nbconvert numpydoc tzlocal --yes --quiet
-conda install sphinx ipython jupyter nbconvert numpydoc tzlocal --yes --quiet
+# Install required packages and R
+echo conda install --channel conda-forge/label/gcc7 sphinx ipython jupyter nbconvert numpydoc tzlocal r-robustbase r-lme4 r-geepack libiconv --yes --quiet
+conda install --channel conda-forge/label/gcc7 sphinx ipython jupyter nbconvert numpydoc tzlocal r-robustbase r-lme4 r-geepack libiconv --yes --quiet
 # doctr and pdr
-echo pip install colorama doctr pandas-datareader simplegeneric
-pip install colorama doctr pandas-datareader simplegeneric  # TODO: Remove simplegeneric after rpy2 updated
-# R and dependencies
-echo conda install --channel conda-forge/label/gcc7 rpy2 r-robustbase r-lme4 r-geepack libiconv --yes --quiet
-conda install --channel conda-forge/label/gcc7 rpy2 r-robustbase r-lme4 r-geepack libiconv --yes --quiet
+echo pip install colorama doctr pandas-datareader rpy2
+pip install colorama doctr pandas-datareader rpy2
