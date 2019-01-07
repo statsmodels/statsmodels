653927d4b49521acffbdd2113550e160071ff4d5
Josef Perktold <josef.pktd@gmail.com>
2019-01-07 10:43:37 -0500

    TST: fix r script for tests, whitespace creation in R2nparray.R

--- tools/R2nparray/R/R2nparray.R
+++ tools/R2nparray/R/R2nparray.R
@@ -186,10 +186,12 @@ mkhtest <- function(ht, name, distr="f") {
 header = "
 import numpy as np
 
+
 class Bunch(dict):
+
     def __init__(self, **kw):
         dict.__init__(self, kw)
-        self.__dict__  = self
+        self.__dict__ = self
 
 "
 write_header = function(){
