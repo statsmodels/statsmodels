8d4eff060dbea79ea225a9103be636c2e70d3fa1
Josef Perktold <josef.pktd@gmail.com>
2019-01-18 14:12:38 -0500

    ENH: use PandasData to get wrapper to work with pandas

--- statsmodels/gam/tests/test_penalized.py
+++ statsmodels/gam/tests/test_penalized.py
@@ -9,7 +9,7 @@ Author: Josef Perktold
 import os
 
 import numpy as np
-from numpy.testing import assert_allclose, assert_equal
+from numpy.testing import assert_allclose, assert_equal, assert_
 import pandas as pd
 
 import patsy
@@ -559,6 +559,7 @@ class TestGAMMPGBSPoisson(CheckGAMMixin):
                                linear=True)
         assert_allclose(linpred, res2.linear_predictors[2:4],
                         rtol=self.rtol_fitted)
+
         assert_equal(predicted.index.values, [2, 3])
         assert_equal(linpred.index.values, [2, 3])
 
@@ -612,7 +613,8 @@ class TestGAMMPGBSPoissonFormula(TestGAMMPGBSPoisson):
         cls.rtol_fitted = 1e-8
         cls.covp_corrfact = 1  # not needed
 
-    def test_names(self):
+    def test_names_wrapper(self):
+        res1a = self.res1a
         xnames = ['Intercept', 'fuel[T.gas]', 'drive[T.fwd]', 'drive[T.rwd]',
                   'weight_s0', 'weight_s1', 'weight_s2', 'weight_s3',
                   'weight_s4', 'weight_s5', 'weight_s6', 'weight_s7',
@@ -620,6 +622,10 @@ class TestGAMMPGBSPoissonFormula(TestGAMMPGBSPoisson):
                   'hp_s0', 'hp_s1', 'hp_s2', 'hp_s3', 'hp_s4', 'hp_s5',
                   'hp_s6', 'hp_s7', 'hp_s8']
 
-        assert_equal(self.res1a.model.exog_names, xnames)
-        assert_equal(self.res1a.model.design_info_linear.column_names,
+        assert_equal(res1a.model.exog_names, xnames)
+        assert_equal(res1a.model.design_info_linear.column_names,
                      xnames[:4])
+
+        assert_equal(res1a.fittedvalues[2:4].index.values, [2, 3])
+        assert_equal(res1a.params.index.values, xnames)
+        assert_(isinstance(res1a.params, pd.Series))
