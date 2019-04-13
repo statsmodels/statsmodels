4a43e094e1d8b8c294ecf3fefd34767097338d92
thequackdaddy <pquack@gmail.com>
2019-04-13 14:12:54 -0500

    Move extras install to before_script
    Install libgfortran3
    
    Apt-get update maybe?

--- tools/ci/docbuild_install.sh
+++ tools/ci/docbuild_install.sh
@@ -1,11 +1,14 @@
 # Additional installation requirements for travis docbuilds
 
 # Install system dependencies
-echo sudo apt-get install graphviz -qq
-sudo apt-get install graphviz -qq
+echo sudo apt-get update
+sudo apt-get update
+
+echo sudo apt-get install graphviz libgfortran3 -qq
+sudo apt-get install graphviz libgfortran3 -qq
 # Install required packages and R
-echo conda install --channel conda-forge/label/gcc7 sphinx jupyter nbconvert numpydoc "tornado<6" r-robustbase r-lme4 r-geepack libiconv rpy2 --yes --quiet
-conda install --channel conda-forge/label/gcc7 sphinx jupyter nbconvert numpydoc "tornado<6" r-robustbase r-lme4 r-geepack libiconv rpy2 --yes --quiet
+echo conda install --channel conda-forge sphinx jupyter nbconvert numpydoc r-robustbase r-lme4 r-geepack libiconv rpy2 --yes --quiet
+conda install --channel conda-forge sphinx jupyter nbconvert numpydoc r-robustbase r-lme4 r-geepack libiconv rpy2 --yes --quiet
 # doctr and pdr
 echo pip install doctr pandas-datareader simplegeneric
 pip install doctr pandas-datareader simplegeneric
