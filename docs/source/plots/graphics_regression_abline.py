28609c07e4f2543f1148515e902d95ecb56a9684
Kevin Sheppard <bashtage@users.noreply.github.com>
2019-05-02 23:11:34 +0100

    Jtweeder graphics addgraphics (#5607)
    * DOC: Add demos with plots to graphics
    
    Add demos with plots to graphics

---
+++ docs/source/plots/graphics_regression_abline.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+'''
+Using the state crime dataset seperately plot the effect of the each
+variable on the on the outcome, murder rate while accounting for the effect
+of all other variables in the model.
+
+'''
+
+import numpy as np
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+
+np.random.seed(12345)
+X = sm.add_constant(np.random.normal(0, 20, size=30))
+y = np.dot(X, [25, 3.5]) + np.random.normal(0, 30, size=30)
+mod = sm.OLS(y, X).fit()
+fig = sm.graphics.abline_plot(model_results=mod)
+ax = fig.axes[0]
+ax.scatter(X[:, 1], y)
+ax.margins(.1)
+plt.show()
