c3ab5045e38cb262490e9be0552daea545d2846e
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2019-05-07 23:25:20 +0100

    BUG: Fix MANOVA when not using formulas
    Fix mv_test so that it works when not using formulas
    Improve MANOVA docstring to help users
    
    closes #4903
    closes #5578

--- statsmodels/multivariate/tests/test_manova.py
+++ statsmodels/multivariate/tests/test_manova.py
@@ -2,9 +2,12 @@
 
 import numpy as np
 import pandas as pd
+import pytest
+from numpy.testing import assert_almost_equal, assert_raises
+
 from statsmodels.multivariate.manova import MANOVA
-from numpy.testing import assert_almost_equal
-from numpy.testing import assert_raises
+from statsmodels.multivariate.multivariate_ols import MultivariateTestResults
+from statsmodels.tools import add_constant
 
 # Example data
 # https://support.sas.com/documentation/cdl/en/statug/63033/HTML/default/
@@ -71,6 +74,74 @@ def test_manova_sas_example():
                         0.6272, decimal=4)
     assert_almost_equal(r['Loc']['stat'].loc["Roy's greatest root", 'Pr > F'],
                         0.4109, decimal=4)
+
+
+def test_manova_no_formula():
+    # Same as previous test only skipping formula interface
+    exog = add_constant(pd.get_dummies(X[['Loc']], drop_first=True))
+    endog = X[['Basal', 'Occ', 'Max']]
+    mod = MANOVA(endog, exog)
+    intercept = np.zeros((1, 3))
+    intercept[0, 0] = 1
+    loc = np.zeros((2, 3))
+    loc[0, 1] = loc[1, 2] = 1
+    hypotheses = [('Intercept', intercept), ('Loc', loc)]
+    r = mod.mv_test(hypotheses)
+    assert_almost_equal(r['Loc']['stat'].loc["Wilks' lambda", 'Value'],
+                        0.60143661, decimal=8)
+    assert_almost_equal(r['Loc']['stat'].loc["Pillai's trace", 'Value'],
+                        0.44702843, decimal=8)
+    assert_almost_equal(r['Loc']['stat'].loc["Hotelling-Lawley trace",
+                                             'Value'],
+                        0.58210348, decimal=8)
+    assert_almost_equal(r['Loc']['stat'].loc["Roy's greatest root", 'Value'],
+                        0.35530890, decimal=8)
+    assert_almost_equal(r['Loc']['stat'].loc["Wilks' lambda", 'F Value'],
+                        0.77, decimal=2)
+    assert_almost_equal(r['Loc']['stat'].loc["Pillai's trace", 'F Value'],
+                        0.86, decimal=2)
+    assert_almost_equal(r['Loc']['stat'].loc["Hotelling-Lawley trace",
+                                             'F Value'],
+                        0.75, decimal=2)
+    assert_almost_equal(r['Loc']['stat'].loc["Roy's greatest root", 'F Value'],
+                        1.07, decimal=2)
+    assert_almost_equal(r['Loc']['stat'].loc["Wilks' lambda", 'Num DF'],
+                        6, decimal=3)
+    assert_almost_equal(r['Loc']['stat'].loc["Pillai's trace", 'Num DF'],
+                        6, decimal=3)
+    assert_almost_equal(r['Loc']['stat'].loc["Hotelling-Lawley trace",
+                                             'Num DF'],
+                        6, decimal=3)
+    assert_almost_equal(r['Loc']['stat'].loc["Roy's greatest root", 'Num DF'],
+                        3, decimal=3)
+    assert_almost_equal(r['Loc']['stat'].loc["Wilks' lambda", 'Den DF'],
+                        16, decimal=3)
+    assert_almost_equal(r['Loc']['stat'].loc["Pillai's trace", 'Den DF'],
+                        18, decimal=3)
+    assert_almost_equal(r['Loc']['stat'].loc["Hotelling-Lawley trace",
+                                             'Den DF'],
+                        9.0909, decimal=4)
+    assert_almost_equal(r['Loc']['stat'].loc["Roy's greatest root", 'Den DF'],
+                        9, decimal=3)
+    assert_almost_equal(r['Loc']['stat'].loc["Wilks' lambda", 'Pr > F'],
+                        0.6032, decimal=4)
+    assert_almost_equal(r['Loc']['stat'].loc["Pillai's trace", 'Pr > F'],
+                        0.5397, decimal=4)
+    assert_almost_equal(r['Loc']['stat'].loc["Hotelling-Lawley trace",
+                                             'Pr > F'],
+                        0.6272, decimal=4)
+    assert_almost_equal(r['Loc']['stat'].loc["Roy's greatest root", 'Pr > F'],
+                        0.4109, decimal=4)
+
+
+@pytest.mark.smoke
+def test_manova_no_formula_no_hypothesis():
+    # Same as previous test only skipping formula interface
+    exog = add_constant(pd.get_dummies(X[['Loc']], drop_first=True))
+    endog = X[['Basal', 'Occ', 'Max']]
+    mod = MANOVA(endog, exog)
+    r = mod.mv_test()
+    assert isinstance(r, MultivariateTestResults)
 
 
 def test_manova_test_input_validation():
