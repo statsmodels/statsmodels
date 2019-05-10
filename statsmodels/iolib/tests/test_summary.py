90c02269606a7c19c12809a520a48057c87f49b7
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2019-05-10 13:47:49 +0100

    BUG: Escape strings for latex output
    Escale string when output is LaTeX
    
    closes #5297

--- statsmodels/iolib/tests/test_summary.py
+++ statsmodels/iolib/tests/test_summary.py
@@ -3,6 +3,24 @@
 
 '''
 from __future__ import print_function
+
+import numpy as np  # noqa: F401
+
+from statsmodels.datasets import macrodata
+from statsmodels.regression.linear_model import OLS
+
+
+def test_escaped_variable_name():
+    # Rename 'cpi' column to 'CPI_'
+    data = macrodata.load(True).data
+    data.rename(columns={'cpi': 'CPI_'}, inplace=True)
+
+    mod = OLS.from_formula('CPI_ ~ 1 + np.log(realgdp)', data=data)
+    res = mod.fit()
+    assert 'CPI\\_' in res.summary().as_latex()
+    assert 'CPI_' in res.summary().as_text()
+
+
 if __name__ == '__main__':
 
     from statsmodels.regression.tests.test_regression import TestOLS
