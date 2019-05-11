b702e803c738e56b47b9730763a55117810bb0de
Brock Mendel <jbrockmendel@gmail.com>
2019-05-11 07:01:37 -0700

    MAINT: remove NameError-having version of levinson_durbin, just keep copied-and-fixed version in stattools

--- statsmodels/tsa/stattools.py
+++ statsmodels/tsa/stattools.py
@@ -903,8 +903,8 @@ def periodogram(X):
     return pergr
 
 
-#copied from nitime and statsmodels\sandbox\tsa\examples\try_ld_nitime.py
-#TODO: check what to return, for testing and trying out returns everything
+# moved from sandbox.tsa.examples.try_ld_nitime, via nitime
+# TODO: check what to return, for testing and trying out returns everything
 def levinson_durbin(s, nlags=10, isacov=False):
     """
     Levinson-Durbin recursion for autoregressive processes
