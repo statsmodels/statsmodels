f82cf520c32bcf34252337eca1d49ed9ff36ab63
Brock Mendel <jbrockmendel@gmail.com>
2019-05-11 07:10:05 -0700

    MAINT: Fix NameErrors, missing import

--- statsmodels/sandbox/stats/stats_mstats_short.py
+++ statsmodels/sandbox/stats/stats_mstats_short.py
@@ -315,7 +315,7 @@ def edf_normal_inverse_transformed(x, alpha=3./8, beta=3./8, axis=0):
     '''rank based normal inverse transformed cdf
     '''
     from scipy import stats
-    ranks = plotting_positions(data, alpha=alpha, beta=alpha, axis=0, masknan=False)
+    ranks = plotting_positions(x, alpha=alpha, beta=alpha, axis=0, masknan=False)
     ranks_transf = stats.norm.ppf(ranks)
     return ranks_transf
 
