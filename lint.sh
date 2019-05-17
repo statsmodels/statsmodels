50984e2da8c3b5f369a7bcc812812428bdf78808
Brock Mendel <jbrockmendel@gmail.com>
2019-05-16 22:07:16 -0700

    CLN: strict linting for stats/tests/results/

--- lint.sh
+++ lint.sh
@@ -60,6 +60,7 @@ if [ "$LINT" == true ]; then
         statsmodels/stats/tests/test_moment_helpers.py \
         statsmodels/stats/tests/test_qsturng.py \
         statsmodels/stats/tests/test_regularized_covariance.py \
+        statsmodels/stats/tests/results/ \
         statsmodels/tools/linalg.py \
         statsmodels/tools/web.py \
         statsmodels/tools/tests/test_linalg.py \
