669fbf57e08f8a3785f7e61ed44253f89f3ff591
Brock Mendel <jbrockmendel@gmail.com>
2019-05-29 15:42:01 -0700

    CLN: descriptivestats

--- statsmodels/tsa/descriptivestats.py
+++ statsmodels/tsa/descriptivestats.py
@@ -7,11 +7,10 @@ Author: josef-pktd
 License: BSD(3clause)
 """
 
-import numpy as np
-from . import stattools as stt
+from . import stattools
 
 
-#todo: check subclassing for descriptive stats classes
+# TODO: check subclassing for descriptive stats classes
 class TsaDescriptive(object):
     '''collection of descriptive statistical methods for time series
 
@@ -32,48 +31,46 @@ class TsaDescriptive(object):
         xdetrended = tsatools.detrend(self.data, order=order)
         return self.__class__(xdetrended, self.label, self.name + '_detrended')
 
-    def fit(self, order=(1,0,1), **kwds):
+    def fit(self, order=(1, 0, 1), **kwds):
         from .arima_model import ARMA
         self.mod = ARMA(self.data)
         self.res = self.mod.fit(order=order, **kwds)
-        #self.estimated_process =
         return self.res
 
     def acf(self, nlags=40):
-        return stt.acf(self.data, nlags=nlags)
+        return stattools.acf(self.data, nlags=nlags)
 
     def pacf(self, nlags=40):
-        return stt.pacf(self.data, nlags=nlags)
+        return stattools.pacf(self.data, nlags=nlags)
 
     def periodogram(self):
-        #doesn't return frequesncies
-        return stt.periodogram(self.data)
+        # doesn't return frequesncies
+        return stattools.periodogram(self.data)
 
     # copied from fftarma.py
     def plot4(self, fig=None, nobs=100, nacf=20, nfreq=100):
         data = self.data
         acf = self.acf(nacf)
         pacf = self.pacf(nacf)
-        w = np.linspace(0, np.pi, nfreq, endpoint=False)
-        spdr = self.periodogram()[:nfreq] #(w)
+        spdr = self.periodogram()[:nfreq]
 
         if fig is None:
             import matplotlib.pyplot as plt
             fig = plt.figure()
-        ax = fig.add_subplot(2,2,1)
+        ax = fig.add_subplot(2, 2, 1)
         namestr = ' for %s' % self.name if self.name else ''
         ax.plot(data)
         ax.set_title('Time series' + namestr)
 
-        ax = fig.add_subplot(2,2,2)
+        ax = fig.add_subplot(2, 2, 2)
         ax.plot(acf)
         ax.set_title('Autocorrelation' + namestr)
 
-        ax = fig.add_subplot(2,2,3)
-        ax.plot(spdr) # (wr, spdr)
+        ax = fig.add_subplot(2, 2, 3)
+        ax.plot(spdr)
         ax.set_title('Power Spectrum' + namestr)
 
-        ax = fig.add_subplot(2,2,4)
+        ax = fig.add_subplot(2, 2, 4)
         ax.plot(pacf)
         ax.set_title('Partial Autocorrelation' + namestr)
 
