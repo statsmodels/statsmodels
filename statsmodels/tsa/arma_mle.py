35c0cc9aae2759662c2b718b0eeb01b7bade4b7c
Brock Mendel <jbrockmendel@gmail.com>
2019-05-29 16:00:21 -0700

    CLN: much of arma_mle.py, many left because of commented-out code

--- statsmodels/tsa/arma_mle.py
+++ statsmodels/tsa/arma_mle.py
@@ -14,11 +14,12 @@ from scipy import signal, optimize
 from statsmodels.base.model import GenericLikelihoodModel
 
 
-#copied from sandbox/regression/mle.py
-#rename until merge of classes is complete
-class Arma(GenericLikelihoodModel):  #switch to generic mle
+# copied from sandbox/regression/mle.py
+# rename until merge of classes is complete
+class Arma(GenericLikelihoodModel):
     """
-    univariate Autoregressive Moving Average model, conditional on initial values
+    univariate Autoregressive Moving Average model, conditional on initial
+    values
 
     The ARMA model is estimated either with conditional Least Squares or with
     conditional Maximum Likelihood. The implementation is
@@ -27,8 +28,8 @@ class Arma(GenericLikelihoodModel):  #switch to generic mle
     Maximum Likelihood and will be more accurate, statistically more efficent
     in small samples.
 
-    In large samples conditional LS, conditional MLE and exact MLE should be very
-    close to each other, they are equivalent asymptotically.
+    In large samples conditional LS, conditional MLE and exact MLE should be
+    very close to each other, they are equivalent asymptotically.
 
     Notes
     -----
@@ -46,24 +47,23 @@ class Arma(GenericLikelihoodModel):  #switch to generic mle
     """
 
     def __init__(self, endog, exog=None):
-        #need to override p,q (nar,nma) correctly
+        # need to override p,q (nar,nma) correctly
         super(Arma, self).__init__(endog, exog)
-        #set default arma(1,1)
+        # set default arma(1, 1)
         self.nar = 1
         self.nma = 1
-        #self.initialize()
 
     def initialize(self):
         pass
 
     def geterrors(self, params):
-        #copied from sandbox.tsa.arima.ARIMA
+        # copied from sandbox.tsa.arima.ARIMA
         p, q = self.nar, self.nma
         ar = np.concatenate(([1], -params[:p]))
         ma = np.concatenate(([1], params[p:p+q]))
 
         #lfilter_zi requires same length for ar and ma
-        maxlag = 1+max(p,q)
+        maxlag = 1+max(p, q)
         armax = np.zeros(maxlag)
         armax[:p+1] = ar
         mamax = np.zeros(maxlag)
@@ -84,26 +84,26 @@ class Arma(GenericLikelihoodModel):  #switch to generic mle
         the params vector
         """
 
-#        #copied from sandbox.tsa.arima.ARIMA
-#        p = self.nar
-#        rhoy = np.concatenate(([1], params[:p]))
-#        rhoe = np.concatenate(([1], params[p:-1]))
-#        errorsest = signal.lfilter(rhoy, rhoe, self.endog)
+        # copied from sandbox.tsa.arima.ARIMA
+        #p = self.nar
+        #rhoy = np.concatenate(([1], params[:p]))
+        #rhoe = np.concatenate(([1], params[p:-1]))
+        #errorsest = signal.lfilter(rhoy, rhoe, self.endog)
         errorsest = self.geterrors(params)
         sigma2 = np.maximum(params[-1]**2, 1e-6)
         axis = 0
         nobs = len(errorsest)
         #this doesn't help for exploding paths
         #errorsest[np.isnan(errorsest)] = 100
-#        llike  =  -0.5 * (np.sum(np.log(sigma2),axis)
-#                          + np.sum((errorsest**2)/sigma2, axis)
-#                          +  nobs*np.log(2*np.pi))
-        llike  =  -0.5 * (nobs*np.log(sigma2)
-                          + np.sum((errorsest**2)/sigma2, axis)
-                          +  nobs*np.log(2*np.pi))
+        #llike = -0.5 * (np.sum(np.log(sigma2), axis)
+        #                  + np.sum((errorsest**2)/sigma2, axis)
+        #                  + nobs*np.log(2*np.pi))
+        llike = -0.5 * (nobs*np.log(sigma2)
+                        + np.sum((errorsest**2)/sigma2, axis)
+                        + nobs*np.log(2*np.pi))
         return llike
 
-    #add for Jacobian calculation  bsejac in GenericMLE, copied from loglike
+    # add for Jacobian calculation  bsejac in GenericMLE, copied from loglike
     def nloglikeobs(self, params):
         """
         Loglikelihood for arma model
@@ -114,46 +114,27 @@ class Arma(GenericLikelihoodModel):  #switch to generic mle
         the params vector
         """
 
-#        #copied from sandbox.tsa.arima.ARIMA
-#        p = self.nar
-#        rhoy = np.concatenate(([1], params[:p]))
-#        rhoe = np.concatenate(([1], params[p:-1]))
-#        errorsest = signal.lfilter(rhoy, rhoe, self.endog)
+        #copied from sandbox.tsa.arima.ARIMA
+        #p = self.nar
+        #rhoy = np.concatenate(([1], params[:p]))
+        #rhoe = np.concatenate(([1], params[p:-1]))
+        #errorsest = signal.lfilter(rhoy, rhoe, self.endog)
         errorsest = self.geterrors(params)
         sigma2 = np.maximum(params[-1]**2, 1e-6)
-        axis = 0
-        nobs = len(errorsest)
         #this doesn't help for exploding paths
         #errorsest[np.isnan(errorsest)] = 100
-#        llike  =  -0.5 * (np.sum(np.log(sigma2),axis)
-#                          + np.sum((errorsest**2)/sigma2, axis)
-#                          +  nobs*np.log(2*np.pi))
-        llike  =  0.5 * (np.log(sigma2)
-                          + (errorsest**2)/sigma2
-                          +  np.log(2*np.pi))
+        #nobs = len(errorsest)
+        #axis = 0
+        #llike = -0.5 * (np.sum(np.log(sigma2), axis)
+        #                  + np.sum((errorsest**2)/sigma2, axis)
+        #                  + nobs*np.log(2*np.pi))
+        llike = 0.5 * (np.log(sigma2)
+                       + (errorsest**2)/sigma2
+                       + np.log(2*np.pi))
         return llike
 
-#use generic instead
-#    def score(self, params):
-#        """
-#        Score vector for Arma model
-#        """
-#        #return None
-#        #print params
-#        jac = ndt.Jacobian(self.loglike, stepMax=1e-4)
-#        return jac(params)[-1]
-
-#use generic instead
-#    def hessian(self, params):
-#        """
-#        Hessian of arma model.  Currently uses numdifftools
-#        """
-#        #return None
-#        Hfun = ndt.Jacobian(self.score, stepMax=1e-4)
-#        return Hfun(params)[-1]
-
-    #copied from arima.ARIMA, needs splitting out of method specific code
-    def fit(self, order=(0,0), start_params=None, method="ls", **optkwds):
+    # copied from arima.ARIMA, needs splitting out of method specific code
+    def fit(self, order=(0, 0), start_params=None, method="ls", **optkwds):
         '''
         Estimate lag coefficients of an ARIMA process.
 
@@ -180,29 +161,29 @@ class Arma(GenericLikelihoodModel):  #switch to generic mle
             unscaled (!) covariance matrix of coefficient estimates
         '''
         if not hasattr(order, '__iter__'):
-            raise ValueError("order must be an iterable sequence.  Got type \
-%s instead" % type(order))
+            raise ValueError("order must be an iterable sequence.  Got type "
+                             "%s instead" % type(order))
 
-        p,q = order
+        p, q = order
         self.nar = p  # needed for geterrors, needs cleanup
         self.nma = q
 
-##        if d > 0:
-##            raise ValueError("Differencing not implemented yet")
-##            # assume no constant, ie mu = 0
-##            # unless overwritten then use w_bar for mu
-##            Y = np.diff(endog, d, axis=0) #TODO: handle lags?
+        #if d > 0:
+        #    raise ValueError("Differencing not implemented yet")
+        #    # assume no constant, ie mu = 0
+        #    # unless overwritten then use w_bar for mu
+        #    Y = np.diff(endog, d, axis=0) #TODO: handle lags?
 
-        x = self.endog.squeeze() # remove the squeeze might be needed later
-#        def errfn( rho):
-#            #rhoy, rhoe = rho
-#            rhoy = np.concatenate(([1], rho[:p]))
-#            rhoe = np.concatenate(([1], rho[p:]))
-#            etahatr = signal.lfilter(rhoy, rhoe, x)
-#            #print rho,np.sum(etahatr*etahatr)
-#            return etahatr
+        #x = self.endog.squeeze() # remove the squeeze might be needed later
+        #def errfn( rho):
+        #    #rhoy, rhoe = rho
+        #    rhoy = np.concatenate(([1], rho[:p]))
+        #    rhoe = np.concatenate(([1], rho[p:]))
+        #    etahatr = signal.lfilter(rhoy, rhoe, x)
+        #    #print rho,np.sum(etahatr*etahatr)
+        #    return etahatr
 
-        #replace with start_params
+        # replace with start_params
         if start_params is None:
             arcoefs0 = 0.5 * np.ones(p)
             macoefs0 = 0.5 * np.ones(q)
@@ -211,26 +192,27 @@ class Arma(GenericLikelihoodModel):  #switch to generic mle
         method = method.lower()
 
         if method == "ls":
-            #update
+            # update
             optim_kwds = dict(ftol=1e-10, full_output=True)
             optim_kwds.update(optkwds)
             #changes: use self.geterrors  (nobs,):
-#            rh, cov_x, infodict, mesg, ier = \
-#               optimize.leastsq(errfn, np.r_[rhoy0, rhoe0],ftol=1e-10,full_output=True)
+            #rh, cov_x, infodict, mesg, ier = \
+            #    optimize.leastsq(errfn, np.r_[rhoy0, rhoe0],ftol=1e-10,full_output=True)
             rh, cov_x, infodict, mesg, ier = \
-               optimize.leastsq(self.geterrors, start_params, **optim_kwds)
-            #TODO: need missing parameter estimates for LS, scale, residual-sdt
-            #TODO: integrate this into the MLE.fit framework?
+                optimize.leastsq(self.geterrors, start_params, **optim_kwds)
+            # TODO: need missing parameter estimates for LS, scale, residual-sdt
+            # TODO: integrate this into the MLE.fit framework?
         elif method == "ssm":
             pass
-        else:  #this is also conditional least squares
+        else:  # this is also conditional least squares
             # fmin_bfgs is slow or doesn't work yet
-            errfnsum = lambda rho : np.sum(self.geterrors(rho)**2)
+            def errfnsum(rho):
+                return np.sum(self.geterrors(rho)**2)
             #xopt, {fopt, gopt, Hopt, func_calls, grad_calls
             optim_kwds = dict(maxiter=2, full_output=True)
             optim_kwds.update(optkwds)
 
-            rh, fopt, gopt, cov_x, _,_, ier = \
+            rh, fopt, gopt, cov_x, _, _, ier = \
                 optimize.fmin_bfgs(errfnsum, start_params, **optim_kwds)
             infodict, mesg = None, None
         self.params = rh
@@ -240,18 +222,22 @@ class Arma(GenericLikelihoodModel):  #switch to generic mle
         self.error_estimate = self.geterrors(rh)
         return rh, cov_x, infodict, mesg, ier
 
-    #renamed and needs check with other fit
-    def fit_mle(self, order=(0,0), start_params=None, method='nm', maxiter=5000, tol=1e-08,
-                **kwds):
-        '''Estimate an ARMA model with given order using Conditional Maximum Likelihood
+    # renamed and needs check with other fit
+    def fit_mle(self, order=(0, 0), start_params=None, method='nm',
+                maxiter=5000, tol=1e-08, **kwds):
+        '''
+        Estimate an ARMA model with given order using Conditional Maximum
+        Likelihood.
 
         Parameters
         ----------
         order : tuple, 2 elements
-            specifies the number of lags(nar, nma) to include, not including lag 0
+            specifies the number of lags(nar, nma) to include, not including
+            lag 0
         start_params : array_like, 1d, (nar+nma+1,)
-            start parameters for the optimization, the length needs to be equal to the
-            number of ar plus ma coefficients plus 1 for the residual variance
+            start parameters for the optimization, the length needs to be equal
+            to the number of ar plus ma coefficients plus 1 for the residual
+            variance
         method : str
             optimization method, as described in LikelihoodModel
         maxiter : int
@@ -270,8 +256,9 @@ class Arma(GenericLikelihoodModel):  #switch to generic mle
         if start_params is None:
             start_params = np.concatenate((0.05*np.ones(nar + nma), [1]))
         mlefit = super(Arma, self).fit(start_params=start_params,
-                maxiter=maxiter, method=method, tol=tol, **kwds)
-        #bug fix: running ls and then mle didn't overwrite this
+                                       maxiter=maxiter, method=method,
+                                       tol=tol, **kwds)
+        # bug fix: running ls and then mle didn't overwrite this
         rh = mlefit.params
         self.params = rh
         self.ar_est = np.concatenate(([1], -rh[:p]))
@@ -279,21 +266,21 @@ class Arma(GenericLikelihoodModel):  #switch to generic mle
         self.error_estimate = self.geterrors(rh)
         return mlefit
 
-    #copied from arima.ARIMA
+    # copied from arima.ARIMA
     def predicted(self, ar=None, ma=None):
         '''past predicted values of time series
         just added, not checked yet
         '''
 
-#        #ar, ma not used, not useful as arguments for predicted pattern
-#        #need it for prediction for other time series, endog
-#        if ar is None:
-#            ar = self.ar_est
-#        if ma is None:
-#            ma = self.ma_est
+        #ar, ma not used, not useful as arguments for predicted pattern
+        #need it for prediction for other time series, endog
+        #if ar is None:
+        #    ar = self.ar_est
+        #if ma is None:
+        #    ma = self.ma_est
         return self.endog - self.error_estimate
 
-    #copied from arima.ARIMA
+    # copied from arima.ARIMA
     def forecast(self, ar=None, ma=None, nperiod=10):
         '''nperiod ahead forecast at the end of the data period
 
@@ -307,7 +294,8 @@ class Arma(GenericLikelihoodModel):  #switch to generic mle
         return signal.lfilter(ma, ar, eta)
 
     def forecast2(self, step_ahead=1, start=None, end=None, endog=None):
-        '''rolling h-period ahead forecast without reestimation, 1 period ahead only
+        '''rolling h-period ahead forecast without reestimation, 1 period ahead
+        only
 
         in construction: uses loop to go over data and
         not sure how to get (finite) forecast polynomial for h-step
@@ -315,12 +303,13 @@ class Arma(GenericLikelihoodModel):  #switch to generic mle
         Notes
         -----
         just the idea:
-        To improve performance with expanding arrays, specify total period by endog
-        and the conditional forecast period by step_ahead
+        To improve performance with expanding arrays, specify total period by
+        endog and the conditional forecast period by step_ahead
 
-        This should be used by/with results which should contain predicted error or
-        noise. Could be either a recursive loop or lfilter with a h-step ahead
-        forecast filter, but then I need to calculate that one. ???
+        This should be used by/with results which should contain predicted
+        error or noise. Could be either a recursive loop or lfilter with a
+        h-step ahead forecast filter, but then I need to calculate that one.
+        ???
 
         further extension: allow reestimation option
 
@@ -329,19 +318,20 @@ class Arma(GenericLikelihoodModel):  #switch to generic mle
         if step_ahead != 1:
             raise NotImplementedError
 
-        p,q = self.nar, self.nma
+        p, q = self.nar, self.nma
         k = 0
         errors = self.error_estimate
         y = self.endog
 
-        #this is for 1step ahead only, still need h-step predictive polynomial
+        # this is for 1step ahead only, still need h-step predictive polynomial
         arcoefs_rev = self.params[k:k+p][::-1]
         macoefs_rev = self.params[k+p:k+p+q][::-1]
 
         predicted = []
         # create error vector iteratively
         for i in range(start, end):
-            predicted.append(sum(arcoefs_rev*y[i-p:i]) + sum(macoefs_rev * errors[i-p:i]))
+            pred = sum(arcoefs_rev*y[i-p:i]) + sum(macoefs_rev * errors[i-p:i])
+            predicted.append(pred)
 
         return np.asarray(predicted)
 
@@ -350,18 +340,20 @@ class Arma(GenericLikelihoodModel):  #switch to generic mle
         '''
 
         from .arima_process import arma2ma
-        p,q = self.nar, self.nma
-        k=0
+        p, q = self.nar, self.nma
+        k = 0
         ar = self.params[k:k+p]
         ma = self.params[k+p:k+p+q]
-        marep = arma2ma(ar,ma, start)[step_ahead+1:]  #truncated ma representation
+        marep = arma2ma(ar, ma, start)[step_ahead+1:]
+        # truncated ma representation
+
         errors = self.error_estimate
         forecasts = np.convolve(errors, marep)
-        return forecasts#[-(errors.shape[0] - start-5):] #get 5 overlapping for testing
+        return forecasts
 
-    #copied from arima.ARIMA
-    #TODO: is this needed as a method at all?
-    #JP: not needed in this form, but can be replace with using the parameters
+    # copied from arima.ARIMA
+    # TODO: is this needed as a method at all?
+    # JP: not needed in this form, but can be replace with using the parameters
     @classmethod
     def generate_sample(cls, ar, ma, nsample, std=1):
         eta = std * np.random.randn(nsample)
