44fe03cbef1fa6d4c7b5b90d43b58d07ddc6ffd9
Kerby Shedden <kshedden@umich.edu>
2019-06-08 17:07:55 -0400

    Dimension reduction for covariance matrices

--- statsmodels/regression/dimred.py
+++ statsmodels/regression/dimred.py
@@ -1,7 +1,8 @@
 import numpy as np
 from statsmodels.base import model
 import statsmodels.base.wrapper as wrap
-
+import pandas as pd
+import warnings
 
 class _DimReductionRegression(model.Model):
     """
@@ -256,3 +257,195 @@ wrap.populate_wrapper(DimReductionResultsWrapper,  # noqa:E305
 SIR = SlicedInverseReg
 PHD = PrincipalHessianDirections
 SAVE = SlicedAverageVarianceEstimation
+
+
+class CovReduce(_DimReductionRegression):
+    """
+    Dimension reduction for a collection of covariance matrices.
+
+    Parameters
+    ----------
+    endog : array-like
+        The dependent variable, treated as group labels
+    exog : array-like
+        The independent variables.
+    dim : integer
+        The dimension of the subspace onto which the covariance
+        matrices are projected.
+
+    Returns
+    -------
+    An orthogonal matrix P such that replacing each group's
+    covariance matrix C with P'CP optimally preserves the
+    differences among these matrices.
+
+    Notes
+    -----
+    This is a likelihood-based dimension reduction procedure based
+    on Wishart models for sample covariance matrices.  The goal
+    is to find a projection matrix P so that C_i | P'C_iP and
+    C_j | P'C_jP are equal in distribution for all i, j, where
+    the C_i are the within-group covariance matrices.
+
+    The model and methodology are as described in Cook and Forzani,
+    but the optimization method follows Edelman et. al.
+
+    References
+    ----------
+    DR Cook, L Forzani (2008).  Covariance reducing models: an alternative
+    to spectral modeling of covariance matrices.  Biometrika 95:4.
+
+    A Edelman, TA Arias, ST Smith (1998).  The geometry of algorithms with
+    orthogonality constraints. SIAM J Matrix Anal Appl.
+    http://math.mit.edu/~edelman/publications/geometry_of_algorithms.pdf
+    """
+
+    def __init__(self, endog, exog, dim):
+
+        super(_DimReductionRegression, self).__init__(endog, exog)
+
+        covs, ns = [], []
+        df = pd.DataFrame(self.exog, index=self.endog)
+        for k, v in df.groupby(df.index):
+            covs.append(v.cov().values)
+            ns.append(v.shape[0])
+
+        self.nobs = len(endog)
+
+        # The marginal covariance
+        covm = 0
+        for i in range(len(covs)):
+           covm += covs[i] * ns[i]
+        covm /= self.nobs
+        self.covm = covm
+
+        self.covs = covs
+        self.ns = ns
+        self.dim = dim
+
+    def loglike(self, params):
+        """
+        Evaluate the log-likelihood
+
+        Parameters
+        ----------
+        params : array-like
+            The projection matrix used to reduce the covariances, flattened
+            to 1d.
+
+        Returns the log-likelihood.
+        """
+
+        p = self.covm.shape[0]
+        proj = params.reshape((p, self.dim))
+
+        c = np.dot(proj.T, np.dot(self.covm, proj))
+        _, ldet = np.linalg.slogdet(c)
+        f = self.nobs * ldet / 2
+
+        for j,c in enumerate(self.covs):
+            c = np.dot(proj.T, np.dot(c, proj))
+            _, ldet = np.linalg.slogdet(c)
+            f -= self.ns[j] * ldet / 2
+
+        return f
+
+    def score(self, params):
+        """
+        Evaluate the score function.
+
+        Parameters
+        ----------
+        params : array-like
+            The projection matrix used to reduce the covariances,
+            flattened to 1d.
+
+        Returns the score function evaluated at 'params'.
+        """
+
+        p = self.covm.shape[0]
+        proj = params.reshape((p, self.dim))
+
+        c0 = np.dot(proj.T, np.dot(self.covm, proj))
+        cP = np.dot(self.covm, proj)
+        g = self.nobs * np.linalg.solve(c0, cP.T).T
+
+        for j, c in enumerate(self.covs):
+            c0 = np.dot(proj.T, np.dot(c, proj))
+            cP = np.dot(c, proj)
+            g -= self.ns[j] * np.linalg.solve(c0, cP.T).T
+
+        return g.ravel()
+
+    def fit(self, start_params=None, maxiter=100, gtol=1e-4):
+        """
+        Fit the covariance reduction model.
+
+        Parameters
+        ----------
+        start_params : array-like
+            Starting value for the projection matrix. May be
+            rectangular, or flattened.
+        maxiter : integer
+            The maximum number of gradient steps to take.
+        gtol : float
+            Convergence criterion for the gradient norm.
+
+        Returns
+        -------
+        An orthogonal p x d matrix P that optimizes the likelihood.
+        """
+
+        p = self.covm.shape[0]
+        d = self.dim
+
+        # Starting value for params
+        if start_params is None:
+            params = np.zeros((p, d))
+            params[0:d, 0:d] = np.eye(d)
+            params = params.ravel()
+        else:
+            params = start_params.ravel()
+
+        llf = self.loglike(params)
+
+        for iter in range(maxiter):
+
+            g = self.score(params)
+            g -= np.dot(g, params) * params / np.dot(params, params)
+
+            if np.sqrt(np.sum(g * g)) < gtol:
+                break
+
+            gm = g.reshape((p, d))
+            u, s, vt = np.linalg.svd(gm, 0)
+
+            paramsm = params.reshape((p, d))
+            pa0 = np.dot(paramsm, vt.T)
+
+            def geo(t):
+                # Parameterize the geodesic path in the direction
+                # of the gradient as a function of t (real).
+                pa = pa0 * np.cos(s * t) + u * np.sin(s * t)
+                return np.dot(pa, vt).ravel()
+
+            # Try to find an uphill step along the geodesic path.
+            step = 2.
+            while step > 1e-10:
+                pa = geo(step)
+                llf1 = self.loglike(pa)
+                if llf1 > llf:
+                    params = pa
+                    llf = llf1
+                    break
+                step /= 2
+
+            if step <= 1e-10:
+                msg = "CovReduce optimization did not converge"
+                warnings.warn(msg)
+                break
+
+        params = params.reshape((p, d))
+        results = DimReductionResults(self, params, eigs=None)
+        results.llf = llf
+        return DimReductionResultsWrapper(results)
