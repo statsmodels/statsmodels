3e36241486aa999efc6b886793b7b0fe20e6e275
jbrockmendel <jbrockmendel@gmail.com>
2019-07-12 08:49:51 -0700

    CLN: remove check for very-old pandas

--- statsmodels/compat/pandas.py
+++ statsmodels/compat/pandas.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 from distutils.version import LooseVersion
 
 import pandas
+from pandas.util._decorators import cache_readonly, deprecate_kwarg
 
 __all__ = ['assert_frame_equal', 'assert_index_equal', 'assert_series_equal',
            'data_klasses', 'frequencies', 'is_numeric_dtype', 'testing',
@@ -24,11 +25,6 @@ except ImportError:
 data_klasses = (pandas.Series, pandas.DataFrame)
 if pandas_lt_25_0:
     data_klasses += (pandas.Panel,)
-
-if version >= '0.20':
-    from pandas.util._decorators import cache_readonly, deprecate_kwarg
-else:
-    from pandas.util.decorators import cache_readonly, deprecate_kwarg
 
 try:
     import pandas.testing as testing
