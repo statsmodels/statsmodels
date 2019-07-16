70dd597df0ca71b44efc806ff865031d7bc4cd12
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2019-07-16 16:36:27 +0100

    MAINT: Ensure seaborn is available for docbuild
    Add seaborn to docbuild

--- tools/ci/docbuild_install.sh
+++ tools/ci/docbuild_install.sh
@@ -10,5 +10,5 @@ sudo apt-get install graphviz libgfortran3 -qq
 echo conda install --channel conda-forge sphinx jupyter nbconvert numpydoc==0.8.0 r-robustbase r-lme4 r-geepack libiconv rpy2 --yes --quiet
 conda install --channel conda-forge sphinx jupyter nbconvert numpydoc==0.8.0 r-robustbase r-lme4 r-geepack libiconv rpy2 --yes --quiet
 # doctr and pdr
-echo pip install doctr pandas-datareader simplegeneric
-pip install doctr pandas-datareader simplegeneric
+echo pip install doctr pandas-datareader simplegeneric seaborn
+pip install doctr pandas-datareader simplegeneric seaborn
