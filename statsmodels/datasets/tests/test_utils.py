fe3a67c427259b0f7c32881b3e393b4556d84949
Kevin Sheppard <bashtage@users.noreply.github.com>
2019-08-09 14:43:52 +0100

    Merge pull request #6093 from bashtage/pickle-dataset-cache-2
    MAINT: Fix new cache name

--- statsmodels/datasets/tests/test_utils.py
+++ statsmodels/datasets/tests/test_utils.py
@@ -44,10 +44,10 @@ def test_get_rdataset_write_read_cache():
     guerry2 = get_rdataset("Guerry", "HistData", cache=cur_dir)
     assert_(guerry2.from_cache is True)
     fn = "raw.githubusercontent.com,vincentarelbundock,Rdatasets,master,csv," \
-         "HistData,Guerry.csv.zip"
+         "HistData,Guerry-v2.csv.zip"
     os.remove(os.path.join(cur_dir, fn))
     fn = "raw.githubusercontent.com,vincentarelbundock,Rdatasets,master,doc," \
-         "HistData,rst,Guerry.rst.zip"
+         "HistData,rst,Guerry-v2.rst.zip"
     os.remove(os.path.join(cur_dir, fn))
 
 
