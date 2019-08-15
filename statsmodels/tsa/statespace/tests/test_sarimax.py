c176bc3928d0f07ba64f62de6f91c03a8bd5ce7c
Chad Fulton <chad@chadfulton.com>
2019-08-15 08:58:09 -0700

    ENH: Improve SARIMAX start_params if too few nobs

--- statsmodels/tsa/statespace/tests/test_sarimax.py
+++ statsmodels/tsa/statespace/tests/test_sarimax.py
@@ -2617,3 +2617,26 @@ def test_apply_results():
 
     assert_allclose(res3.forecast(10, exog=np.ones(10)),
                     res1.forecast(10, exog=np.ones(10)))
+
+
+def test_start_params_small_nobs():
+    # Test that starting parameters work even when nobs is very small, but
+    # issues a warning.
+    endog = np.log(realgdp_results['value']).diff()[1:].values
+
+    # Regular ARMA
+    mod = sarimax.SARIMAX(endog[:4], order=(4, 0, 0))
+    match = ('Too few observations to estimate starting parameters for ARMA'
+             ' and trend.')
+    with pytest.warns(UserWarning, match=match):
+        start_params = mod.start_params
+        assert_allclose(start_params, [0, 0, 0, 0, np.var(endog[:4])])
+
+    # Seasonal ARMA
+    mod = sarimax.SARIMAX(endog[:4], order=(0, 0, 0),
+                          seasonal_order=(1, 0, 0, 4))
+    match = ('Too few observations to estimate starting parameters for'
+             ' seasonal ARMA.')
+    with pytest.warns(UserWarning, match=match):
+        start_params = mod.start_params
+        assert_allclose(start_params, [0, np.var(endog[:4])])
