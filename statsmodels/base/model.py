fc61d2f7f709217a860feba820ef4a14a973f1bf
Rebecca N. Palmer <rebecca_palmer@zoho.com>
2019-09-15 19:30:50 +0100

    DOC: fix docstring formatting of previous commit

--- statsmodels/base/model.py
+++ statsmodels/base/model.py
@@ -2145,8 +2145,12 @@ class LikelihoodModelResults(Results):
     @classmethod
     def load(cls, fname):
         """
-        Load a pickled results instance; use only on trusted files,
-        as unpickling can run arbitrary code.
+        Load a pickled results instance; use only on trusted files.
+
+        Unpickling can run arbitrary code from the file being loaded.
+        This allows it to load a wide range of object types, but is
+        also a security risk: calling this on a malicious file can
+        wipe or take over your system.
 
         Parameters
         ----------
--- statsmodels/iolib/smpickle.py
+++ statsmodels/iolib/smpickle.py
@@ -19,9 +19,12 @@ def save_pickle(obj, fname):
 
 def load_pickle(fname):
     """
-    Load a previously saved object; **use only on trusted files**,
-    as unpickling can run arbitrary code.  (i.e. calling this on a
-    malicious file can wipe or take over your system.)
+    Load a previously saved object; **use only on trusted files**.
+
+    Unpickling can run arbitrary code from the file being loaded.
+    This allows it to load a wide range of object types, but is also
+    a security risk: calling this on a malicious file can wipe or
+    take over your system.
 
     Parameters
     ----------
