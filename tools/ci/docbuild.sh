947301df39108e404f249130ceec2a7a110ea3a9
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2020-01-22 17:43:14 +0000

    DOC: Special docbuild [skip appveyor] Perform a special docbuild

--- tools/ci/docbuild.sh
+++ tools/ci/docbuild.sh
@@ -1,9 +1,6 @@
 # Change to doc directory
 cd "$SRCDIR"/docs
 
-# Run notebooks as tests
-pytest ../statsmodels/examples/tests
-
 set -e
 # Clean up
 echo '================================= Clean ================================='
@@ -45,19 +42,6 @@ else
   exit 1;
 fi;
 
-# Build documentation
-echo '========================================================================'
-echo '=                        Checking Spelling                             ='
-echo '========================================================================'
-echo 'make spelling > doc_spelling.log 2>&1'
-make spelling > /dev/null
-echo '========================================================================'
-echo '=                        Spelling Mistakes                             ='
-echo '========================================================================'
-if [ -f "build/spelling/output.txt" ]; then
-  cat build/spelling/output.txt
-fi;
-
 
 # Deploy with doctr
 cd "$SRCDIR"
@@ -74,11 +58,4 @@ else
   fi;
 fi;
 
-# Script to text examples and build docs on travis
-echo '========================================================================'
-echo '=                        Checking Doc Strings                          ='
-echo '========================================================================'
-
-cd "$SRCDIR"
-pip install numpydoc --upgrade
-python "$SRCDIR"/tools/validate_docstrings.py --errors=GL03,GL04,GL05,GL06,GL07,GL08,GL09,SS01,SS04,SS05,SS06,PR01,PR02,PR03,PR04,PR05,PR06,PR07,PR08,PR10,EX04,RT01,RT02,RT03,RT04,RT05,SA05,EX04 || true
+echo Finished...
