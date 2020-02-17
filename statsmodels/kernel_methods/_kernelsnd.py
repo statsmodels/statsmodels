a6beb43993cf296069fca3ad41864d9c863764d1
Pierre Barbier de Reuille <Pierre Barbier de Reuille pierre.barbierdereuille@gmail.com>
2020-02-17 22:23:21 +0000

    Split kernels.py into _kernels.py and the imports.

--- statsmodels/kernel_methods/_kernelsnd.py
+++
@@ -1,7 +0,0 @@
-"""
-Module containing kernels in nD, except for Gaussian, which is already in
-the main kernels module.
-
-Note: There are no such kernels for now.
-"""
-#from .kernels import KernelnD
