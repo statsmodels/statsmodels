adaefe435e02a51edd572c64ea304362fe520c96
Thomas Brooks <tgbrooks@gmail.com>
2020-02-27 16:20:40 -0500

    Expand exog_predict tests

--- statsmodels/nonparametric/tests/test_lowess.py
+++ statsmodels/nonparametric/tests/test_lowess.py
@@ -120,16 +120,6 @@ class TestLowess(object):
         assert_almost_equal(actual_lowess, actual_lowess1, decimal=13)
         assert_(actual_lowess.dtype is np.dtype(float))
 
-        # Test specifying exog_predict explicitly
-        perm_idx = np.arange(len(x)//2)
-        np.random.shuffle(perm_idx)
-        actual_lowess2 = lowess(y, x, exog_predict = x[perm_idx], return_sorted=False)
-        assert_almost_equal(actual_lowess[perm_idx,1], actual_lowess2, decimal=13)
-
-        actual_lowess3 = lowess(y, x, exog_predict = x[perm_idx], return_sorted=False, it=0)
-        actual_lowess_it0 = lowess(y, x, return_sorted=False, it=0)
-        assert_almost_equal(actual_lowess_it0[perm_idx], actual_lowess3, decimal=13)
-
         # check with nans,  this changes the arrays
         y[[5, 6]] = np.nan
         x[3] = np.nan
@@ -157,10 +147,51 @@ class TestLowess(object):
         yhat = yhat[np.isfinite(yhat)]
         assert_almost_equal(yhat, actual_lowess2[:,1], decimal=13)
 
+    def test_exog_predict(self):
+        rfile = os.path.join(rpath, 'test_lowess_simple.csv')
+        test_data = np.genfromtxt(open(rfile, 'rb'),
+                                  delimiter = ',', names = True)
+        y, x = test_data['y'], test_data['x']
+        target = lowess(y, x, is_sorted=True)
+
+        # Test specifying exog_predict explicitly
+        perm_idx = np.arange(len(x)//2)
+        np.random.shuffle(perm_idx)
+        actual_lowess = lowess(y, x, exog_predict = x[perm_idx], missing='none')
+        assert_almost_equal(target[perm_idx,1], actual_lowess, decimal=13)
+
+        target_it0 = lowess(y, x, return_sorted=False, it=0)
+        actual_lowess2 = lowess(y, x, exog_predict = x[perm_idx], it=0)
+        assert_almost_equal(target_it0[perm_idx], actual_lowess2, decimal=13)
+
+        # Check nans in exog_predict
+        assert_raises(ValueError, lowess, y, x,
+                    exog_predict = np.array([np.nan, 5, 3]),
+                    missing='raise')
+
+        # With is_sorted=True
+        actual_lowess3 = lowess(y, x, exog_predict=x, is_sorted=True)
+        assert_equal(actual_lowess3, target[:,1])
+
+        # check with nans,  this changes the arrays
+        y[[5, 6]] = np.nan
+        x[3] = np.nan
+        target = lowess(y, x, is_sorted=True)
+
         # Test specifying exog_predict explicitly, now with nans
-        perm_idx = np.arange(actual_lowess.shape[0])
-        actual_lowess4 = lowess(y, x, exog_predict = actual_lowess[perm_idx,0], return_sorted=False)
-        assert_almost_equal(actual_lowess[perm_idx,1], actual_lowess4, decimal=13)
+        perm_idx = np.arange(target.shape[0])
+        actual_lowess1 = lowess(y, x, exog_predict = target[perm_idx,0])
+        assert_almost_equal(target[perm_idx,1], actual_lowess1, decimal=13)
+
+        # nans and missing='drop'
+        actual_lowess2 = lowess(y, x, exog_predict = x,
+                                missing='drop')
+        all_finite = np.isfinite(x) & np.isfinite(y)
+        assert_equal(actual_lowess2[all_finite], target[:,1])
+
+        # Dimensional check
+        assert_raises(ValueError, lowess, y, x, exog_predict = np.array([[5],[10]]))
+
 
 
 def test_returns_inputs():
