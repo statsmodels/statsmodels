b8cb27baa4d0ce6291a04f35cd0fec19e404fa50
Bolzano-Weierstrass <thomas.p.marchand@gmail.com>
2020-04-06 15:11:23 +0200

    BUG-TST: corrected variance formula in test_var, still 3 red tests

--- statsmodels/discrete/tests/test_count_model.py
+++ statsmodels/discrete/tests/test_count_model.py
@@ -226,8 +226,21 @@ class TestZeroInflatedPoisson_predict(object):
                         atol=1e-2, rtol=1e-2)
 
     def test_var(self):
-        assert_allclose((self.res.predict().mean() *
-                        self.res._dispersion_factor.mean()),
+        def compute_mixture_var(dispersion_factor, prob_main, mu):
+            # the variance of the mixture is the mixture of the variances
+            # plus a non-negative term accounting for the (weighted) dispersion of the means
+            # see stats.stackexchange #16609 and Casella & Berger's Statistical Inference (Example 10.2.1)
+            prob_infl = 1-prob_main
+            var = (dispersion_factor*(1-prob_infl)*mu).mean()
+            var += (((1-prob_infl)*mu)**2).mean() - (((1-prob_infl)*mu).mean())**2
+            return var
+
+        res = self.res
+        var_fitted = compute_mixture_var(res._dispersion_factor,
+                                         res.predict(which='prob-main'),
+                                         res.predict(which='mean-main'))
+
+        assert_allclose(var_fitted.mean(),
                         self.endog.var(), atol=5e-2, rtol=5e-2)
 
     def test_predict_prob(self):
@@ -329,8 +342,18 @@ class TestZeroInflatedGeneralizedPoisson_predict(object):
                         atol=1e-4, rtol=1e-4)
 
     def test_var(self):
-        assert_allclose((self.res.predict().mean() *
-                        self.res._dispersion_factor.mean()),
+        def compute_mixture_var(dispersion_factor, prob_main, mu):
+            prob_infl = 1-prob_main
+            var = (dispersion_factor*(1-prob_infl)*mu).mean()
+            var += (((1-prob_infl)*mu)**2).mean() - (((1-prob_infl)*mu).mean())**2
+            return var
+
+        res = self.res
+        var_fitted = compute_mixture_var(res._dispersion_factor,
+                                         res.predict(which='prob-main'),
+                                         res.predict(which='mean-main'))
+
+        assert_allclose(var_fitted.mean(),
                         self.endog.var(), atol=0.05, rtol=0.1)
 
     def test_predict_prob(self):
@@ -447,8 +470,18 @@ class TestZeroInflatedNegativeBinomialP_predict(object):
 
     def test_var(self):
         # todo check precision
-        assert_allclose((self.res.predict().mean() *
-                        self.res._dispersion_factor.mean()),
+        def compute_mixture_var(dispersion_factor, prob_main, mu):
+            prob_infl = 1 - prob_main
+            var = (dispersion_factor * (1 - prob_infl) * mu).mean()
+            var += (((1 - prob_infl) * mu) ** 2).mean() - (((1 - prob_infl) * mu).mean()) ** 2
+            return var
+
+        res = self.res
+        var_fitted = compute_mixture_var(res._dispersion_factor,
+                                         res.predict(which='prob-main'),
+                                         res.predict(which='mean-main'))
+
+        assert_allclose(var_fitted.mean(),
                         self.endog.var(), rtol=0.2)
 
     def test_predict_prob(self):
