1c2c11ea1e18ac5f48b3aa969b4b36f9c98bfa67
Giulio Beseghi <giulio.beseghi@ecotricity.co.uk>
2020-04-21 23:54:44 +0100

    Handle pathlib.Path objects

--- statsmodels/iolib/tests/test_pickle.py
+++ statsmodels/iolib/tests/test_pickle.py
@@ -1,4 +1,5 @@
 from io import BytesIO
+import pathlib
 import tempfile
 
 from numpy.testing import assert_equal
@@ -8,16 +9,26 @@ from statsmodels.iolib.smpickle import save_pickle, load_pickle
 
 
 def test_pickle():
-    tmpdir = tempfile.mkdtemp(prefix='pickle')
+    tmpdir = tempfile.mkdtemp(prefix="pickle")
     a = lrange(10)
-    save_pickle(a, tmpdir+'/res.pkl')
-    b = load_pickle(tmpdir+'/res.pkl')
+
+    # test with str
+    path_str = tmpdir + "/res.pkl"
+    save_pickle(a, path_str)
+    b = load_pickle(path_str)
+    assert_equal(a, b)
+
+    # test with pathlib
+    path_pathlib = pathlib.Path(tmpdir) / "res.pkl"
+    save_pickle(a, path_pathlib)
+    b = load_pickle(path_pathlib)
     assert_equal(a, b)
 
     # cleanup, tested on Windows
     try:
         import os
-        os.remove(tmpdir+'/res.pkl')
+
+        os.remove(tmpdir + "/res.pkl")
         os.rmdir(tmpdir)
     except (OSError, IOError):
         pass
