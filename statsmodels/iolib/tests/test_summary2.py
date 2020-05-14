d556254df2208cae880e8c2c09713bd229c14725
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2020-05-14 17:44:30 +0100

    BUG: Fix summary col R2 ordering
    Ensure consistent ordering
    
    closes #6578

--- statsmodels/iolib/tests/test_summary2.py
+++ statsmodels/iolib/tests/test_summary2.py
@@ -9,6 +9,7 @@ from statsmodels.iolib.summary2 import summary_col
 from statsmodels.tools.tools import add_constant
 from statsmodels.regression.linear_model import OLS
 
+
 class TestSummaryLatex(object):
 
     def test_summarycol(self):
@@ -19,42 +20,42 @@ class TestSummaryLatex(object):
 \begin{center}
 \begin{tabular}{lcc}
 \hline
-          &   y I    &   y II    \\
+               &   y I    &   y II    \\
 \midrule
-const     & 7.7500   & 12.4231   \\
-          & (1.1058) & (3.1872)  \\
-x1        & -0.7500  & -1.5769   \\
-          & (0.2368) & (0.6826)  \\
-R-squared & 0.6930   & 0.5202    \\
-          & 0.7697   & 0.6401    \\
+const          & 7.7500   & 12.4231   \\
+               & (1.1058) & (3.1872)  \\
+x1             & -0.7500  & -1.5769   \\
+               & (0.2368) & (0.6826)  \\
+R-squared      & 0.7697   & 0.6401    \\
+R-squared Adj. & 0.6930   & 0.5202    \\
 \hline
 \end{tabular}
 \end{center}
 \end{table}
 '''
-        x = [1,5,7,3,5]
+        x = [1, 5, 7, 3, 5]
         x = add_constant(x)
-        y1 = [6,4,2,7,4]
-        y2 = [8,5,0,12,4]
-        reg1 = OLS(y1,x).fit()
-        reg2 = OLS(y2,x).fit()
-        actual = summary_col([reg1,reg2]).as_latex()
+        y1 = [6, 4, 2, 7, 4]
+        y2 = [8, 5, 0, 12, 4]
+        reg1 = OLS(y1, x).fit()
+        reg2 = OLS(y2, x).fit()
+        actual = summary_col([reg1, reg2]).as_latex()
         actual = '\n%s\n' % actual
         assert_equal(desired, actual)
 
     def test_summarycol_float_format(self):
         # Test for latex output of summary_col object
         desired = r"""
-=====================
-           y I   y II
----------------------
-const     7.7   12.4 
-          (1.1) (3.2)
-x1        -0.7  -1.6 
-          (0.2) (0.7)
-R-squared 0.7   0.5  
-          0.8   0.6  
-=====================
+==========================
+                y I   y II
+--------------------------
+const          7.7   12.4 
+               (1.1) (3.2)
+x1             -0.7  -1.6 
+               (0.2) (0.7)
+R-squared      0.8   0.6  
+R-squared Adj. 0.7   0.5  
+==========================
 Standard errors in
 parentheses.
 """  # noqa:W291
@@ -67,6 +68,11 @@ parentheses.
         actual = summary_col([reg1, reg2], float_format='%0.1f').as_text()
         actual = '%s\n' % actual
         assert_equal(actual, desired)
+
+        starred = summary_col([reg1, reg2], stars=True, float_format='%0.1f')
+        assert "7.7***" in str(starred)
+        assert "12.4**" in str(starred)
+        assert "12.4***" not in str(starred)
 
     def test_summarycol_drop_omitted(self):
         # gh-3702
@@ -112,17 +118,17 @@ parentheses.
         # Test that latex output of regular OLS output still contains
         # multiple tables
 
-        x = [1,5,7,3,5]
+        x = [1, 5, 7, 3, 5]
         x = add_constant(x)
-        y1 = [6,4,2,7,4]
-        reg1 = OLS(y1,x).fit()
+        y1 = [6, 4, 2, 7, 4]
+        reg1 = OLS(y1, x).fit()
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
             actual = reg1.summary().as_latex()
         string_to_find = r'''\end{tabular}
 \begin{tabular}'''
         result = string_to_find in actual
-        assert(result is True)
+        assert (result is True)
 
 
 def test_ols_summary_rsquared_label():
@@ -142,3 +148,13 @@ def test_ols_summary_rsquared_label():
         assert r2_str in str(reg_without_constant.summary2())
     with pytest.warns(UserWarning):
         assert r2_str in str(reg_without_constant.summary())
+
+
+def test_summary_col_r2():
+    # GH 6578
+    y = [1, 1, 4, 2] * 4
+    x = add_constant([1, 2, 3, 4] * 4)
+    mod = OLS(endog=y, exog=x).fit()
+    table = summary_col(results=mod)
+    assert "R-squared  " in str(table)
+    assert "R-squared Adj." in str(table)
