d3b6299a194392757cf3ae97a85baf6f71fcf50e
Josef Perktold <josef.pktd@gmail.com>
2020-06-02 15:06:36 -0400

    TST/DOC/CLN: test_simulate_equivalence, some docstring, code cleanup

--- statsmodels/stats/tests/test_oneway.py
+++ statsmodels/stats/tests/test_oneway.py
@@ -423,3 +423,40 @@ class TestOnewayOLS(object):
         pow_oneway = smpwr.ftest_anova_power(np.sqrt(es_oneway), nobs_t, alpha,
                                              k_groups=k, df=None)
         assert_allclose(pow_ols, pow_oneway, rtol=1e-13)
+
+
+def test_simulate_equivalence():
+    # regression test, needs large k_mc to be reliable
+
+    k_groups = 4
+    k_repl = 10
+    nobs = np.array([10, 12, 13, 15]) * k_repl
+    means = np.array([-1, 0, 0, 1]) * 0.12
+    vars_ = np.array([1, 2, 3, 4])
+    nobs_t = nobs.sum()
+
+    eps = 0.0191 * 10
+    opt_var = ["unequal", "equal", "bf"]
+    k_mc = 100
+    np.random.seed(987126)
+    res_mc = sto.simulate_power_equivalence_oneway(
+        means, nobs, eps, vars_=vars_, k_mc=k_mc, trim_frac=0.1,
+        options_var=opt_var, margin_type="wellek")
+
+    frac_reject = (res_mc.pvalue <= 0.05).sum(0) / k_mc
+    assert_allclose(frac_reject, [0.17, 0.18, 0.14], atol=0.001)
+    # result with k_mc = 10000 is [0.1466, 0.1871, 0.1606]
+    # similar to asy below, but not very close for all
+
+    es_alt_li = []
+    for uv in opt_var:
+        es = effectsize_oneway(means, vars_, nobs, use_var=uv)
+        es_alt_li.append(es)
+
+    # compute asy power as comparison
+    margin = wellek_to_f2(eps, k_groups)
+    pow_ = [power_equivalence_oneway(
+        es_, margin, nobs_t, n_groups=k_groups, df=None, alpha=0.05,
+        margin_type="f2") for es_ in es_alt_li]
+    # regression test numbers
+    assert_allclose(pow_, [0.147749, 0.173358, 0.177412], atol=0.007)
