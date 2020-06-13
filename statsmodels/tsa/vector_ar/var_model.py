6a5ee7c5ff5a02f6b93a6cccc37a4c2833d477bd
Joon Ro <devel.jro@outlook.com>
2020-06-12 17:49:56 -0700

    Remove a blank line which contains whitespace

--- statsmodels/tsa/vector_ar/var_model.py
+++ statsmodels/tsa/vector_ar/var_model.py
@@ -1633,7 +1633,7 @@ class VARResults(VARProcess):
         intercept = self.intercept
         nobs = self.nobs
         nobs_original = nobs + k_ar
-        
+
         ma_coll = np.zeros((repl, steps + 1, neqs, neqs))
 
         def fill_coll(sim):
