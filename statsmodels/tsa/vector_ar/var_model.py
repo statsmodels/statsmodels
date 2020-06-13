8c21dd5a14bf34978cc59edfc426507e6840f1da
Joon Ro <devel.jro@outlook.com>
2020-06-12 17:41:56 -0700

    Simulate correct number of observations #6748
    `nobs` is # of observations without the initial observations (`k_ar`). The irf simulation should include initial observations (`nobs_original = nobs + k_ar`).

--- statsmodels/tsa/vector_ar/var_model.py
+++ statsmodels/tsa/vector_ar/var_model.py
@@ -1632,7 +1632,8 @@ class VARResults(VARProcess):
         sigma_u = self.sigma_u
         intercept = self.intercept
         nobs = self.nobs
-
+        nobs_original = nobs + k_ar
+        
         ma_coll = np.zeros((repl, steps + 1, neqs, neqs))
 
         def fill_coll(sim):
@@ -1643,7 +1644,7 @@ class VARResults(VARProcess):
         for i in range(repl):
             # discard first burn to eliminate correct for starting bias
             sim = util.varsim(coefs, intercept, sigma_u,
-                              seed=seed, steps=nobs+burn)
+                              seed=seed, steps=nobs_original+burn)
             sim = sim[burn:]
             ma_coll[i, :, :, :] = fill_coll(sim)
 
