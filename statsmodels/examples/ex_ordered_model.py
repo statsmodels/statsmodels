b975648ed20edbd8ee815a400b7b3cc3bc1d36ab
Bolzano-Weierstrass <thomas.p.marchand@gmail.com>
2020-08-23 14:25:58 +0200

    STY: flake8 friendly

--- statsmodels/examples/ex_ordered_model.py
+++ statsmodels/examples/ex_ordered_model.py
@@ -14,7 +14,8 @@ from statsmodels.miscmodels.ordinal_model import OrderedModel
 
 nobs, k_vars = 1000, 3
 x = np.random.randn(nobs, k_vars)
-#x = np.column_stack((np.ones(nobs), x))  #constant will be in integration limits
+# x = np.column_stack((np.ones(nobs), x))
+# #constant will be in integration limits
 xb = x.dot(np.ones(k_vars))
 y_latent = xb + np.random.randn(nobs)
 y = np.round(np.clip(y_latent, -2.4, 2.4)).astype(int) + 2
@@ -26,16 +27,17 @@ mod = OrderedModel(y, x)
 start_params = np.ones(k_vars + 4)
 start_params = np.concatenate((np.ones(k_vars), np.arange(4)))
 start_ppf = stats.norm.ppf((np.bincount(y) / len(y)).cumsum())
-start_threshold = np.concatenate((start_ppf[:1], np.log(np.diff(start_ppf[:-1]))))
+start_threshold = np.concatenate((start_ppf[:1],
+                                  np.log(np.diff(start_ppf[:-1]))))
 start_params = np.concatenate((np.zeros(k_vars), start_threshold))
 res = mod.fit(start_params=start_params, maxiter=5000, maxfun=5000)
 print(res.params)
-#res = mod.fit(start_params=res.params, method='bfgs')
+# res = mod.fit(start_params=res.params, method='bfgs')
 res = mod.fit(start_params=start_params, method='bfgs')
 
 print(res.params)
 print(np.exp(res.params[-(mod.k_levels - 1):]).cumsum())
-#print(res.summary())
+# print(res.summary())
 
 predicted = res.model.predict(res.params)
 pred_choice = predicted.argmax(1)
@@ -51,7 +53,8 @@ pred_choice_log = res_log.predict().argmax(1)
 print((y == pred_choice_log).mean())
 print(res_log.summary())
 
-# example form UCLA Stats pages http://www.ats.ucla.edu/stat/stata/dae/ologit.htm
+# example form UCLA Stats pages
+# http://www.ats.ucla.edu/stat/stata/dae/ologit.htm
 # requires downloaded dataset ologit.dta
 
 dataf = pandas.read_stata(r"M:\josef_new\scripts\ologit_ucla.dta")
@@ -61,7 +64,8 @@ res_log2 = OrderedModel(np.asarray(dataf['apply']),
                         np.asarray(dataf[['pared', 'public', 'gpa']], float),
                         distr='logit').fit(method='bfgs')
 
-# this replicates the UCLA example except for different parameterization of par2
+# this replicates the UCLA example except
+# for different parameterization of par2
 res_log3 = OrderedModel(dataf['apply'].values.codes,
                         np.asarray(dataf[['pared', 'public', 'gpa']], float),
                         distr='logit').fit(method='bfgs')
@@ -70,8 +74,9 @@ print(res_log3.summary())
 
 # with ordered probit - not on UCLA page
 print(
-    OrderedModel(dataf['apply'].values.codes, np.asarray(dataf[['pared', 'public', 'gpa']], float), distr='probit').fit(
-        method='bfgs').summary())
+    OrderedModel(dataf['apply'].values.codes,
+                 np.asarray(dataf[['pared', 'public', 'gpa']], float),
+                 distr='probit').fit(method='bfgs').summary())
 
 
 # example with a custom distribution - not on UCLA page
