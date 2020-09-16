4b68e68a92e6bfef155699f1db195ca7df73151c
Hassan Kibirige <has2k1@gmail.com>
2020-09-16 13:49:39 +0300

    TST: Use unused test @parameterize variable
    Pytest @parameterize variable `kernel` was unused in the body of
    the test function.
    
    Reference: #6547

--- statsmodels/nonparametric/tests/test_kernel_density.py
+++ statsmodels/nonparametric/tests/test_kernel_density.py
@@ -423,5 +423,5 @@ def test_all_kernels(kernel, reset_randomstate):
     data = np.random.normal(size=200)
     x_grid = np.linspace(min(data), max(data), 200)
     density = sm.nonparametric.KDEUnivariate(data)
-    density.fit(kernel="gau", fft=False)
+    density.fit(kernel=kernel, fft=False)
     assert isinstance(density.evaluate(x_grid), np.ndarray)
