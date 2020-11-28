ffd0572331cb67af9b115bc872288d46f77ab289
Chad Fulton <chad@chadfulton.com>
2020-11-28 00:04:53 -0500

    TST: Add test for ETSModel get_prediction with one step.

--- statsmodels/tsa/tests/test_exponential_smoothing.py
+++ statsmodels/tsa/tests/test_exponential_smoothing.py
@@ -1008,3 +1008,28 @@ def test_exact_prediction_intervals(austourists_model_fit):
     fit.model = DummyModel("AAA")
     s_AAA = fit._relative_forecast_variance(steps)
     assert_almost_equal(s_AAdA, s_AAA, 2)
+
+
+def test_one_step_ahead(setup_model):
+    model, params, results_R = setup_model
+    model2 = ETSModel(
+        pd.Series(model.endog),
+        seasonal_periods=model.seasonal_periods,
+        error=model.error,
+        trend=model.trend,
+        seasonal=model.seasonal,
+        damped_trend=model.damped_trend,
+    )
+    res = model2.smooth(params)
+
+    fcast1 = res.forecast(steps=1)
+    fcast2 = res.forecast(steps=2)
+    assert_allclose(fcast1.iloc[0], fcast2.iloc[0])
+
+    pred1 = res.get_prediction(start=model2.nobs, end=model2.nobs,
+                               simulate_repetitions=2)
+    pred2 = res.get_prediction(start=model2.nobs, end=model2.nobs + 1,
+                               simulate_repetitions=2)
+    df1 = pred1.summary_frame(alpha=0.05)
+    df2 = pred1.summary_frame(alpha=0.05)
+    assert_allclose(df1.iloc[0, 0], df2.iloc[0, 0])
