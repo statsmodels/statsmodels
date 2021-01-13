9145cdedff6457d71bf4fd98a8949e5e8fca7aed
Pamphile ROY <roy.pamphile@gmail.com>
2021-01-13 13:24:21 +0100

    MAINT: refactor seed management

--- statsmodels/distributions/tests/test_joint_distribution.py
+++ statsmodels/distributions/tests/test_joint_distribution.py
@@ -70,3 +70,4 @@ def test_joint_distribution():
     copula = GaussianCopula(cov=[[1., 0.5], [0.5, 1.]])
     joint_dist = JointDistribution(dists, copula)
     sample = joint_dist.random(512)
+    assert sample.shape == (512, 2)
