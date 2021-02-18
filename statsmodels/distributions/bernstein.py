7e905bee5dd3607b1255ece9b495032c08410917
Josef Perktold <josef.pktd@gmail.com>
2021-02-18 12:24:42 -0500

    ENH: add BernsteinDistribution

---
+++ statsmodels/distributions/bernstein.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 17 15:35:23 2021
+
+Author: Josef Perktold
+License: BSD-3
+
+"""
+
+import numpy as np
+
+from statsmodels.tools.decorators import cache_readonly
+from statsmodels.distributions.tools import (
+        _Grid, cdf2prob_grid, _eval_bernstein_dd)
+
+
+class BernsteinDistribution(object):
+    """Distribution based on Bernstein Polynomials on unit hypercube
+
+
+    Parameters
+    ----------
+    cdf_grid : array_like
+        cdf values on a equal spaced grid of the unit hypercube [0, 1]^d.
+        The dimension of the arrays define how many random variables are
+        included in the multivariate distribution.
+
+
+    """
+
+    def __init__(self, cdf_grid):
+        self.cdf_grid = cdf_grid = np.asarray(cdf_grid)
+        self.k_dim = cdf_grid.ndim
+        self.k_grid = cdf_grid.shape
+        self.k_grid_product = np.product([i-1 for i in self.k_grid])
+        self._grid = _Grid(self.k_grid)
+
+    @cache_readonly
+    def prob_grid(self):
+        return cdf2prob_grid(self.cdf_grid, prepend=None)
+
+    def cdf(self, x):
+        cdf_ = _eval_bernstein_dd(x, self.cdf_grid)
+        return cdf_
+
+    def pdf(self, x):
+        # TODO: check usage of k_grid_product. Should this go into eval?
+        pdf_ = self.k_grid_product * _eval_bernstein_dd(x, self.prob_grid)
+        return pdf_
+
+    def get_marginal(self, idx):
+        """get marginal BernsteinDistribution
+
+        currently only 1-dim margins, `idx` is int
+
+        Status: not verified yet except for uniform margins.
+        This uses the smoothed cdf values in the new marginal distribution and
+        not the grid values corresponding to the margin of the multivariate
+        grid. This might change.
+
+        """
+
+        # univariate
+        if self.k_dim == 1:
+            return self
+
+        x_m = np.ones((self.k_grid[idx], self.k_dim))
+        x_m[:, idx] = self._grid.x_marginal[idx]
+        cdf_m = self.cdf(x_m)
+        bpd_marginal = BernsteinDistribution(cdf_m)
+        return bpd_marginal
---
+++ statsmodels/distributions/tests/test_bernstein.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 17 23:44:18 2021
+
+Author: Josef Perktold
+License: BSD-3
+
+"""
+
+import numpy as np
+from numpy.testing import assert_allclose, assert_array_less
+from scipy import stats
+
+from statsmodels.distributions.copula.api import (
+        CopulaDistribution, ArchimedeanCopula)
+from statsmodels.distributions.copula.api import transforms as tra
+import statsmodels.distributions.tools as dt
+from statsmodels.distributions.bernstein import BernsteinDistribution
+
+
+def test_bernstein_distribution_1d():
+    grid = dt._Grid([501])
+
+    distr = stats.beta(3, 5)
+    cdf_g = distr.cdf(np.squeeze(grid.x_flat))
+    bpd = BernsteinDistribution(cdf_g)
+
+    cdf_bp = bpd.cdf(grid.x_flat)
+    assert_allclose(cdf_bp, cdf_g, atol=0.005)
+    assert_array_less(np.median(np.abs(cdf_bp - cdf_g)), 0.001)
+
+    pdfv = distr.pdf(np.squeeze(grid.x_flat))
+    pdf_bp = bpd.pdf(grid.x_flat)
+    assert_allclose(pdf_bp, pdfv, atol=0.02)
+    assert_array_less(np.median(np.abs(pdf_bp - pdfv)), 0.01)
+
+
+def test_bernstein_distribution_2d():
+    grid = dt._Grid([51, 51])
+
+    cop_tr = tra.TransfFrank
+    args = (2,)
+    ca = ArchimedeanCopula(cop_tr())
+    distr1 = stats.uniform
+    distr2 = stats.uniform
+    cad = CopulaDistribution([distr1, distr2], ca, copargs=args)
+    cdfv = cad.cdf(grid.x_flat, args)
+    cdf_g = cdfv.reshape(grid.k_grid)
+
+    bpd = BernsteinDistribution(cdf_g)
+
+    cdf_bp = bpd.cdf(grid.x_flat)
+    assert_allclose(cdf_bp, cdfv, atol=0.005)
+    assert_array_less(np.median(np.abs(cdf_bp - cdfv)), 0.001)
+
+    grid_eps = dt._Grid([51, 51], eps=1e-8)
+    pdfv = cad.pdf(grid_eps.x_flat)
+    pdf_bp = bpd.pdf(grid_eps.x_flat)
+    assert_allclose(pdf_bp, pdfv, atol=0.01, rtol=0.04)
+    assert_array_less(np.median(np.abs(pdf_bp - pdfv)), 0.05)
+
+    # check marginal cdfs
+    # get marginal cdf
+    xx = np.column_stack((np.linspace(0, 1, 5), np.ones(5)))
+    cdf_m1 = bpd.cdf(xx)
+    assert_allclose(cdf_m1, xx[:, 0], atol=1e-13)
+    xx = np.column_stack((np.ones(5), np.linspace(0, 1, 5)))
+    cdf_m2 = bpd.cdf(xx)
+    assert_allclose(cdf_m2, xx[:, 1], atol=1e-13)
+
+    xx_ = np.linspace(0, 1, 5)
+    xx = xx_[:, None]  # currently requires 2-dim
+    bpd_m1 = bpd.get_marginal(0)
+    cdf_m1 = bpd_m1.cdf(xx)
+    assert_allclose(cdf_m1, xx_, atol=1e-13)
+    pdf_m1 = bpd_m1.pdf(xx)
+    assert_allclose(pdf_m1, np.ones(len(xx)), atol=1e-13)
+
+    bpd_m = bpd.get_marginal(1)
+    cdf_m = bpd_m.cdf(xx)
+    assert_allclose(cdf_m, xx_, atol=1e-13)
+    pdf_m = bpd_m.pdf(xx)
+    assert_allclose(pdf_m, np.ones(len(xx)), atol=1e-13)
