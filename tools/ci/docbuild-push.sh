99b81277cd47b6c9035d831dae906f74da92b3b7
Kevin Sheppard <kevin.sheppard@gmail.com>
2021-07-07 23:59:42 +0100

    MAINT: Get doc push to work

--- tools/ci/docbuild-push.sh
+++ tools/ci/docbuild-push.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 pushd statsmodels.github.io
-echo "Change the remote"
-git remote set-url origin https://${PERSONAL_ACCESS_TOKEN}@github.com/statsmodels/statsmodels.github.io.git
 echo "Pushing"
 git push
 popd
