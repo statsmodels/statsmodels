bf84475860448f9a7ddacb862c14d33abcb5e8b8
Greg McMahan <53842892+gmcmacran@users.noreply.github.com>
2021-07-09 19:22:38 -0500

    Removing white spaces

--- statsmodels/genmod/families/links.py
+++ statsmodels/genmod/families/links.py
@@ -890,27 +890,28 @@ class cloglog(CLogLog):
     """
     pass
 
+
 class LogLog(Logit):
     """
     The log-log transform
-    
+
     LogLog inherits from Logit in order to have access to its _clean method
     for the link and its derivative.
     """
     def __call__(self, p):
         """
         Log-Log transform link function
-        
+
         Parameters
         ----------
         p : ndarray
             Mean parameters
-            
+
         Returns
         -------
         z : ndarray
             The LogLog transform of `p`
-            
+
         Notes
         -----
         g(p) = -log(-log(p))
@@ -921,18 +922,18 @@ class LogLog(Logit):
     def inverse(self, z):
         """
         Inverse of Log-Log transform link function
-        
-        
+
+
         Parameters
         ----------
         z : array_like
             The value of the inverse of the LogLog link function at `p`
-            
+
         Returns
         -------
         p : ndarray
             Mean parameters
-            
+
         Notes
         -----
         g^(-1)(`z`) = exp(-exp(`z`))
@@ -942,17 +943,17 @@ class LogLog(Logit):
     def deriv(self, p):
         """
         Derivative of Log-Log transform link function
-        
+
         Parameters
         ----------
         p : array_like
             Mean parameters
-            
+
         Returns
         -------
         g'(p) : ndarray
             The derivative of the LogLog transform link function
-            
+
         Notes
         -----
         g'(p) = - 1 /p * log(p)
@@ -963,36 +964,53 @@ class LogLog(Logit):
     def deriv2(self, p):
         """
         Second derivative of the Log-Log link function
-        
+
         Parameters
         ----------
         p : array_like
             Mean parameters
-            
+
         Returns
         -------
         g''(p) : ndarray
             The second derivative of the LogLog link function
         """
         p = self._clean(p)
-        d2 = (1 + np.log(p)) / (p * (np.log(p)))**2 
+        d2 = (1 + np.log(p)) / (p * (np.log(p)))**2
         return d2
 
     def inverse_deriv(self, z):
         """
         Derivative of the inverse of the Log-Log transform link function
-        
+
         Parameters
         ----------
         z : array_like
             The value of the inverse of the LogLog link function at `p`
-            
+
         Returns
         -------
         g^(-1)'(z) : ndarray
             The derivative of the inverse of the LogLog link function
         """
         return np.exp(-np.exp(-z) - z)
+    
+    def inverse_deriv2(self, z):
+        """
+        Second derivative of the inverse of the Log-Log transform link function
+
+        Parameters
+        ----------
+        z : array_like
+            The value of the inverse of the LogLog link function at `p`
+
+        Returns
+        -------
+        g^(-1)''(z) : ndarray
+            The second derivative of the inverse of the LogLog link function
+        """
+        return self.inverse_deriv(z) * ( np.exp(-z) - 1)
+    
     
 class loglog(LogLog):
     """
