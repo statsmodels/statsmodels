eb744747f7a91287fc28d878760a113de9cc866a
Pamphile ROY <roy.pamphile@gmail.com>
2021-07-30 14:22:28 +0200

    MAINT: remove ref to old NumPy

--- statsmodels/tools/rng_qrng.py
+++ statsmodels/tools/rng_qrng.py
@@ -1,4 +1,5 @@
 import numbers
+from collections import Sequence
 
 import numpy as np
 import scipy.stats as stats
@@ -9,13 +10,13 @@ def check_random_state(seed=None):
 
     Parameters
     ----------
-    seed : {None, int, `numpy.random.Generator`,
+    seed : {None, int, array_like[ints], `numpy.random.Generator`,
             `numpy.random.RandomState`, `scipy.stats.qmc.QMCEngine`}, optional
 
         If `seed` is None fresh, unpredictable entropy will be pulled
         from the OS and `numpy.random.Generator` is used.
-        If `seed` is an int, a new ``Generator`` instance is used,
-        seeded with `seed`.
+        If `seed` is an int or ``array_like[ints]``, a new ``Generator``
+        instance is used, seeded with `seed`.
         If `seed` is already a ``Generator``, ``RandomState`` or
         `scipy.stats.qmc.QMCEngine` instance then
         that instance is used.
@@ -25,26 +26,18 @@ def check_random_state(seed=None):
 
     Returns
     -------
-    seed : {`numpy.random.Generator`, `numpy.random.RandomState`}
+    seed : {`numpy.random.Generator`, `numpy.random.RandomState`,
+            `scipy.stats.qmc.QMCEngine`}
+
         Random number generator.
 
     """
-    if seed is None or isinstance(seed, (numbers.Integral, np.integer)):
-        if not hasattr(np.random, 'Generator'):
-            # This can be removed once numpy 1.16 is dropped
-            msg = ("NumPy 1.16 doesn't have Generator, use either "
-                   "NumPy >= 1.17 or `seed=np.random.RandomState(seed)`")
-            raise ValueError(msg)
+    if seed is None or isinstance(seed, (numbers.Integral, np.integer, Sequence)):
         return np.random.default_rng(seed)
-    elif isinstance(seed, np.random.RandomState):
+    elif isinstance(seed, (np.random.RandomState, np.random.Generator)):
         return seed
-    elif isinstance(seed, np.random.Generator):
-        # The two checks can be merged once numpy 1.16 is dropped
-        return seed
-    elif isinstance(seed, np.random.Generator):
-        return seed
-    elif hasattr(stats.qmc, 'QMCEngine') and isinstance(seed, stats.qmc.QMCEngine):
+    elif hasattr(stats.qmc, "QMCEngine") and isinstance(seed, stats.qmc.QMCEngine):
         return seed
     else:
-        raise ValueError('%r cannot be used to seed a numpy.random.Generator'
-                         ' instance' % seed)
+        raise ValueError(f"{seed:r} cannot be used to seed a"
+                         " numpy.random.Generator instance")
