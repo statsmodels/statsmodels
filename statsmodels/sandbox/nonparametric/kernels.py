f62635d7f8d5df152a998f2416b1fc17039fa050
Simardeep27 <42802893+Simardeep27@users.noreply.github.com>
2021-08-23 03:23:51 +0530

    Add files via upload

--- statsmodels/sandbox/nonparametric/kernels.py
+++ statsmodels/sandbox/nonparametric/kernels.py
@@ -563,3 +563,16 @@ class Cosine2(CustomKernel):
         self._L2Norm = 1.5
         self._kernel_var = 0.03267274151216444  # = 1/12. - 0.5 / np.pi**2
         self._order = 2
+
+class Tricube(CustomKernel):
+    """
+    Tricube Kernel
+
+    K(u) = 0.864197530864 * (1 - abs(x)**3)**3 between -1.0 and 1.0
+    """
+    def __init__(self,h=1.0):
+        CustomKernel.__init__(self,shape=lambda x: 0.864197530864 * (1 - abs(x)**3)**3,
+                              h=h, domain=[-1.0, 1.0], norm = 1.0)
+        self._L2Norm = 175.0/247.0
+        self._kernel_var = 35.0/243.0
+        self._order = 2
