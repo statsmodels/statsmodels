3b45b29b015b8ec41033ada6460d17e812054f41
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2021-09-09 09:08:03 +0100

    MAINT: Update versions file
    Update versions file to use new-format version information

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -189,7 +189,7 @@ html_theme_options = {
                'examples/index': 'examples and tutorials to get started with '
                                  'statsmodels'},
     "version_dropdown": True,
-    "version_json": "../versions.json",
+    "version_json": "../versions-v2.json",
 }
 
 language = 'en'
