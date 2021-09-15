246a0c0adf02d6b4e2059bb754a0ec074d94be65
Josef Perktold <josef.pktd@gmail.com>
2021-09-15 10:03:02 -0400

    REF: elliptical base class, ValueError for invalid args

--- statsmodels/distributions/copula/elliptical.py
+++ statsmodels/distributions/copula/elliptical.py
@@ -7,6 +7,7 @@ Author: Pamphile Roy
 License: BSD-3
 
 """
+import warnings
 import numpy as np
 from scipy import stats
 # scipy compat:
@@ -15,7 +16,86 @@ from statsmodels.compat.scipy import multivariate_t
 from statsmodels.distributions.copula.copulas import Copula
 
 
-class GaussianCopula(Copula):
+class EllipticalCopula(Copula):
+    """Base class for elliptical copula
+
+    This class requires subclassing and currently does not have generic
+    methods based on an elliptical generator.
+
+    Notes
+    -----
+    Elliptical copulas require that copula parameters are set when the
+    instance is created. Those parameters currently cannot be provided in the
+    call to methods. (This will most likely change in future versions.)
+    If non-empty ``args`` are provided in methods, then a ValueError is raised.
+    The ``args`` keyword is provided for a consistent interface across
+    copulas.
+
+    """
+    def _handle_args(self, args):
+        if args != () and args is not None:
+            msg = ("Methods in elliptical copulas use copula parameters in"
+                   " attributes. `arg` in the method is ignored")
+            raise ValueError(msg)
+        else:
+            return args
+
+    def rvs(self, nobs=1, args=(), random_state=None):
+        self._handle_args(args)
+        x = self.distr_mv.rvs(size=nobs, random_state=random_state)
+        return self.distr_uv.cdf(x)
+
+    def pdf(self, u, args=()):
+        self._handle_args(args)
+        ppf = self.distr_uv.ppf(u)
+        mv_pdf_ppf = self.distr_mv.pdf(ppf)
+
+        return mv_pdf_ppf / np.prod(self.distr_uv.pdf(ppf), axis=-1)
+
+    def cdf(self, u, args=()):
+        self._handle_args(args)
+        ppf = self.distr_uv.ppf(u)
+        return self.distr_mv.cdf(ppf)
+
+    def tau(self, corr=None):
+        """Bivariate kendall's tau based on correlation coefficient.
+
+        Parameters
+        ----------
+        corr : None or float
+            Pearson correlation. If corr is None, then the correlation will be
+            taken from the copula attribute.
+
+        Returns
+        -------
+        Kendall's tau that corresponds to pearson correlation in the
+        elliptical copula.
+        """
+        if corr is None:
+            corr = self.corr
+        if corr.shape == (2, 2):
+            corr = corr[0, 1]
+        rho = 2 * np.arcsin(corr) / np.pi
+        return rho
+
+    def corr_from_tau(self, tau):
+        """Pearson correlation from kendall's tau.
+
+        Parameters
+        ----------
+        tau : array_like
+            Kendall's tau correlation coefficient.
+
+        Returns
+        -------
+        Pearson correlation coefficient for given tau in elliptical
+        copula. This can be used as parameter for an elliptical copula.
+        """
+        corr = np.sin(tau * np.pi / 2)
+        return corr
+
+
+class GaussianCopula(EllipticalCopula):
     r"""Gaussian copula.
 
     It is constructed from a multivariate normal distribution over
@@ -46,6 +126,20 @@ class GaussianCopula(Copula):
     k_dim : int
         Dimension, number of components in the multivariate random variable.
 
+    Notes
+    -----
+    Elliptical copulas require that copula parameters are set when the
+    instance is created. Those parameters currently cannot be provided in the
+    call to methods. (This will most likely change in future versions.)
+    If non-empty ``args`` are provided in methods, then a ValueError is raised.
+    The ``args`` keyword is provided for a consistent interface across
+    copulas.
+
+    References
+    ----------
+    .. [1] Joe, Harry, 2014, Dependence modeling with copulas. CRC press.
+        p. 163
+
     """
 
     def __init__(self, corr=None, k_dim=2):
@@ -58,32 +152,6 @@ class GaussianCopula(Copula):
         self.corr = np.asarray(corr)
         self.distr_uv = stats.norm
         self.distr_mv = stats.multivariate_normal(cov=corr)
-
-    def rvs(self, nobs=1, args=(), random_state=None):
-        x = self.distr_mv.rvs(size=nobs, random_state=random_state)
-        return self.distr_uv.cdf(x)
-
-    def pdf(self, u, args=()):
-        ppf = self.distr_uv.ppf(u)
-        mv_pdf_ppf = self.distr_mv.pdf(ppf)
-
-        return mv_pdf_ppf / np.prod(self.distr_uv.pdf(ppf), axis=1)
-
-    def cdf(self, u, args=()):
-        ppf = self.distr_uv.ppf(u)
-        return self.distr_mv.cdf(ppf)
-
-    def tau(self, pearson_corr=None):
-
-        if pearson_corr is None:
-            if self.corr.shape == (2, 2):
-                corr = self.corr[0, 1]
-            else:
-                corr = self.corr
-        else:
-            corr = pearson_corr
-        tau = 2 * np.arcsin(corr) / np.pi
-        return tau
 
     def dependence_tail(self, corr=None):
         """
@@ -105,30 +173,12 @@ class GaussianCopula(Copula):
 
         return 0, 0
 
-    def corr_from_tau(self, tau):
-        """pearson correlation from kendall's tau
-
-        Joe (2014) p. 164
-
-        Parameter
-        ---------
-        tau : array_like
-            Kendall's tau correlation coefficient.
-
-        Returns
-        -------
-        Pearson correlation coefficient for given tau in elliptical
-        copula. This can be used as parameter for an elliptical copula.
-        """
-        corr = np.sin(tau * np.pi / 2)
-        return corr
-
     def _arg_from_tau(self, tau):
         # for generic compat
         return self.corr_from_tau(tau)
 
 
-class StudentTCopula(Copula):
+class StudentTCopula(EllipticalCopula):
     """Student t copula.
 
     Parameters
@@ -143,12 +193,25 @@ class StudentTCopula(Copula):
     k_dim : int
         Dimension, number of components in the multivariate random variable.
 
+    Notes
+    -----
+    Elliptical copulas require that copula parameters are set when the
+    instance is created. Those parameters currently cannot be provided in the
+    call to methods. (This will most likely change in future versions.)
+    If non-empty ``args`` are provided in methods, then a ValueError is raised.
+    The ``args`` keyword is provided for a consistent interface across
+    copulas.
+
+    References
+    ----------
+    .. [1] Joe, Harry, 2014, Dependence modeling with copulas. CRC press.
+        p. 181
     """
 
     def __init__(self, corr=None, df=None, k_dim=2):
         super().__init__(k_dim=k_dim)
         if corr is None:
-            corr = [[1., 0.], [0., 1.]]
+            corr = np.eye(k_dim)
         elif k_dim == 2 and np.size(corr) == 1:
             corr = np.array([[1., corr], [corr, 1.]])
 
@@ -158,40 +221,15 @@ class StudentTCopula(Copula):
         self.distr_uv = stats.t(df=df)
         self.distr_mv = multivariate_t(shape=corr, df=df)
 
-    def rvs(self, nobs=1, args=(), random_state=None):
-        x = self.distr_mv.rvs(size=nobs, random_state=random_state)
-        return self.distr_uv.cdf(x)
-
-    def pdf(self, u, args=()):
-        ppf = self.distr_uv.ppf(u)
-        mv_pdf_ppf = self.distr_mv.pdf(ppf)
-
-        return mv_pdf_ppf / np.prod(self.distr_uv.pdf(ppf), axis=1)
-
     def cdf(self, u, args=()):
         raise NotImplementedError("CDF not available in closed form.")
         # ppf = self.distr_uv.ppf(u)
         # mvt = MVT([0, 0], self.corr, self.df)
         # return mvt.cdf(ppf)
 
-    def tau(self, pearson_corr=None):
-        """bivariate kendall's tau
-
-        Joe (2014) p. 182
-        """
-        if pearson_corr is None:
-            if self.corr.shape == (2, 2):
-                corr = self.corr[0, 1]
-            else:
-                corr = self.corr
-        else:
-            corr = pearson_corr
-        rho = 2 * np.arcsin(corr) / np.pi
-        return rho
-
     def spearmans_rho(self, corr=None):
         """
-        Bivariate Spearman's rho.
+        Bivariate Spearman's rho based on correlation coefficient.
 
         Joe (2014) p. 182
 
@@ -242,24 +280,7 @@ class StudentTCopula(Copula):
         lam = 2 * stats.t.cdf(t, df + 1)
         return lam, lam
 
-    def corr_from_tau(self, tau):
-        """Pearson correlation from kendall's tau.
-
-        Joe (2014) p. 164
-
-        Parameter
-        ---------
-        tau : array_like
-            Kendall's tau correlation coefficient.
-
-        Returns
-        -------
-        Pearson correlation coefficient for given tau in elliptical
-        copula. This can be used as parameter for an elliptical copula.
-        """
-        corr = np.sin(tau * np.pi / 2)
-        return corr
-
     def _arg_from_tau(self, tau):
         # for generic compat
+        # this does not provide an estimate of df
         return self.corr_from_tau(tau)
--- statsmodels/distributions/copula/other_copulas.py
+++ statsmodels/distributions/copula/other_copulas.py
@@ -22,11 +22,31 @@ class IndependenceCopula(Copula):
 
         C_\theta(u,v) = uv
 
+    Parameters
+    ----------
+    k_dim : int
+        Dimension, number of components in the multivariate random variable.
+
+    Notes
+    -----
+    IndependenceCopula does not have copula parameters.
+    If non-empty ``args`` are provided in methods, then a ValueError is raised.
+    The ``args`` keyword is provided for a consistent interface across
+    copulas.
+
     """
     def __init__(self, k_dim=2):
         super().__init__(k_dim=k_dim)
 
-    def rvs(self, nobs=1, random_state=None):
+    def _handle_args(self, args):
+        if args != () and args is not None:
+            msg = ("Independence copula does not use copula parameters.")
+            raise ValueError(msg)
+        else:
+            return args
+
+    def rvs(self, nobs=1, args=(), random_state=None):
+        self._handle_args(args)
         rng = check_random_state(random_state)
         x = rng.random((nobs, self.k_dim))
         return x
