6f18ef06158b97eabd15966ff87332c6e84e18f3
Josef Perktold <josef.pktd@gmail.com>
2021-09-21 13:28:56 -0400

    MAINT: more additions to api.py and api.rst

--- statsmodels/distributions/copula/api.py
+++ statsmodels/distributions/copula/api.py
@@ -10,12 +10,22 @@ from statsmodels.distributions.copula.copulas import (
 
 from statsmodels.distributions.copula.archimedean import (
     ArchimedeanCopula, FrankCopula, ClaytonCopula, GumbelCopula)
+import statsmodels.distributions.copula.transforms as transforms
 
 from statsmodels.distributions.copula.elliptical import (
     GaussianCopula, StudentTCopula)
 
 from statsmodels.distributions.copula.extreme_value import (
     ExtremeValueCopula)
+import statsmodels.distributions.copula.depfunc_ev as depfunc_ev
 
 from statsmodels.distributions.copula.other_copulas import (
-    IndependenceCopula)
+    IndependenceCopula, rvs_kernel)
+
+
+__all__ = [
+    "ArchimedeanCopula", "ClaytonCopula", "CopulaDistribution",
+    "ExtremeValueCopula", "FrankCopula", "GaussianCopula", "GumbelCopula",
+    "IndependenceCopula", "StudentTCopula", "depfunc_ev", "rvs_kernel",
+    "transforms",
+    ]
