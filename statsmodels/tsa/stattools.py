02055c9d1fd01c614680318c9e4e81703996421d
Kevin Sheppard <bashtage@users.noreply.github.com>
2021-10-21 14:11:50 +0100

    Merge pull request #7812 from joaomacalos/zivot-andrews-docs
    MAINT: fix ZivotAndrewsUnitRoot.run() docstring

--- statsmodels/tsa/stattools.py
+++ statsmodels/tsa/stattools.py
@@ -2483,11 +2483,11 @@ class ZivotAndrewsUnitRoot(object):
         cvdict : dict
             The critical values for the test statistic at the 1%, 5%, and 10%
             levels.
+        baselag : int
+            The number of lags used for period regressions.
         bpidx : int
             The index of x corresponding to endogenously calculated break period
             with values in the range [0..nobs-1].
-        baselag : int
-            The number of lags used for period regressions.
 
         Notes
         -----
