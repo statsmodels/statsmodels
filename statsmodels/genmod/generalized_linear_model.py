ee52a3aee403118debc09568d32e44d1122d79e9
Josef Perktold <josef.pktd@gmail.com>
2021-11-04 19:56:15 -0400

    Backport 7843

--- statsmodels/genmod/generalized_linear_model.py
+++ statsmodels/genmod/generalized_linear_model.py
@@ -1914,6 +1914,35 @@ class GLMResults(base.LikelihoodModelResults):
 
         return res
 
+    @Appender(pinfer.score_test.__doc__)
+    def score_test(self, exog_extra=None, params_constrained=None,
+                   hypothesis='joint', cov_type=None, cov_kwds=None,
+                   k_constraints=None, observed=True):
+
+        if self.model._has_freq_weights is True:
+            warnings.warn("score test has not been verified with freq_weights",
+                          UserWarning)
+        if self.model._has_var_weights is True:
+            warnings.warn("score test has not been verified with var_weights",
+                          UserWarning)
+
+        # We need to temporarily change model.df_resid for scale computation
+        # TODO: find a nicer way. gh #7840
+        mod_df_resid = self.model.df_resid
+        self.model.df_resid = self.df_resid
+        if k_constraints is not None:
+            self.model.df_resid += k_constraints
+        res = pinfer.score_test(self, exog_extra=exog_extra,
+                                params_constrained=params_constrained,
+                                hypothesis=hypothesis,
+                                cov_type=cov_type, cov_kwds=cov_kwds,
+                                k_constraints=k_constraints,
+                                scale=None,
+                                observed=observed)
+
+        self.model.df_resid = mod_df_resid
+        return res
+
     def get_hat_matrix_diag(self, observed=True):
         """
         Compute the diagonal of the hat matrix
--- statsmodels/genmod/tests/test_score_test.py
+++ statsmodels/genmod/tests/test_score_test.py
@@ -16,44 +16,7 @@ import statsmodels.stats._diagnostic_other as diao
 from statsmodels.base._parameter_inference import score_test
 
 
-class TestScoreTest(object):
-    # compares score to wald, and regression test for pvalues
-    rtol_ws = 5e-3
-    atol_ws = 0
-    rtol_wooldridge = 0.004
-    dispersed = False  # Poisson correctly specified
-    # regression numbers
-    res_pvalue = [0.31786373532550893, 0.32654081685271297]
-    skip_wooldridge = False
-    res_disptest = np.array([[ 0.13927919,  0.44461477],
-                             [ 0.13927919,  0.44461477],
-                             [ 0.21295545,  0.41568086],
-                             [ 0.14935018,  0.88127732],
-                             [ 0.14935018,  0.88127732],
-                             [ 0.14841761,  0.8820132 ],
-                             [ 0.22836064,  0.81936588]])
-    res_disptest_g = [0.052247629593715761, 0.81919738867722225]
-
-    @classmethod
-    def setup_class(cls):
-        nobs, k_vars = 500, 5
-
-        np.random.seed(786452)
-        x = np.random.randn(nobs, k_vars)
-        x[:, 0] = 1
-        x2 = np.random.randn(nobs, 2)
-        xx = np.column_stack((x, x2))
-
-        if cls.dispersed:
-            het = np.random.randn(nobs)
-            y = np.random.poisson(np.exp(x.sum(1) * 0.5 + het))
-            #y_mc = np.random.negative_binomial(np.exp(x.sum(1) * 0.5), 2)
-        else:
-            y = np.random.poisson(np.exp(x.sum(1) * 0.5))
-
-        cls.exog_extra = x2
-        cls.model_full = GLM(y, xx, family=families.Poisson())
-        cls.model_drop = GLM(y, x, family=families.Poisson())
+class CheckScoreTest():
 
     def test_wald_score(self):
         mod_full = self.model_full
@@ -64,13 +27,18 @@ class TestScoreTest(object):
         res_drop = mod_drop.fit()
 
         wald = res_full.wald_test(restriction)
-        lm_constr = np.hstack(score_test(res_constr))
+        # note: need to use method for res_constr for correct df_resid
+        lm_constr = np.hstack(res_constr.score_test())
         lm_extra = np.hstack(score_test(res_drop, exog_extra=self.exog_extra))
+        lm_full = np.hstack(res_full.score_test(
+            params_constrained=res_constr.params,
+            k_constraints=res_constr.k_constr))
 
         res_wald = np.hstack([wald.statistic.squeeze(), wald.pvalue, [wald.df_denom]])
         assert_allclose(lm_constr, res_wald, rtol=self.rtol_ws, atol=self.atol_ws)
         assert_allclose(lm_extra, res_wald, rtol=self.rtol_ws, atol=self.atol_ws)
         assert_allclose(lm_constr, lm_extra, rtol=1e-12, atol=1e-14)
+        assert_allclose(lm_full, lm_extra, rtol=1e-12, atol=1e-14)
         # regression number
         assert_allclose(lm_constr[1], self.res_pvalue[0], rtol=1e-12, atol=1e-14)
 
@@ -100,6 +68,49 @@ class TestScoreTest(object):
             # smoke test
             lm_wooldridge.summary()
 
+
+class TestScoreTest(CheckScoreTest):
+    # compares score to wald, and regression test for pvalues
+    rtol_ws = 5e-3
+    atol_ws = 0
+    rtol_wooldridge = 0.004
+    dispersed = False  # Poisson correctly specified
+    # regression numbers
+    res_pvalue = [0.31786373532550893, 0.32654081685271297]
+    skip_wooldridge = False
+    res_disptest = np.array([
+        [0.1392791916012637, 0.8892295323009857],
+        [0.1392791916012645, 0.8892295323009850],
+        [0.2129554490802097, 0.8313617120611572],
+        [0.1493501809372359, 0.8812773205886350],
+        [0.1493501809372359, 0.8812773205886350],
+        [0.1454862255574059, 0.8843269904545624],
+        [0.2281321688124869, 0.8195434922982738]
+        ])
+    res_disptest_g = [0.052247629593715761, 0.81919738867722225]
+
+    @classmethod
+    def setup_class(cls):
+        nobs, k_vars = 500, 5
+
+        np.random.seed(786452)
+        x = np.random.randn(nobs, k_vars)
+        x[:, 0] = 1
+        x2 = np.random.randn(nobs, 2)
+        xx = np.column_stack((x, x2))
+
+        if cls.dispersed:
+            het = np.random.randn(nobs)
+            y = np.random.poisson(np.exp(x.sum(1) * 0.5 + het))
+            #y_mc = np.random.negative_binomial(np.exp(x.sum(1) * 0.5), 2)
+        else:
+            y = np.random.poisson(np.exp(x.sum(1) * 0.5))
+
+        cls.exog_extra = x2
+        cls.model_full = GLM(y, xx, family=families.Poisson())
+        cls.model_drop = GLM(y, x, family=families.Poisson())
+
+
     def test_dispersion(self):
         res_drop = self.model_drop.fit()
         res_test = diao.dispersion_poisson(res_drop)
@@ -121,17 +132,20 @@ class TestScoreTestDispersed(TestScoreTest):
     rtol_wooldridge = 0.03
     dispersed = True  # Poisson is mis-specified
     res_pvalue = [5.412978775609189e-14, 0.05027602575743518]
-    res_disptest = np.array([[  1.26473634e+02,   0.00000000e+00],
-                             [  1.26473634e+02,   0.00000000e+00],
-                             [  1.19393621e+02,   0.00000000e+00],
-                             [  4.53940519e+00,   5.64131397e-06],
-                             [  4.53940519e+00,   5.64131397e-06],
-                             [  2.98154154e+00,   2.86801135e-03],
-                             [  4.27569194e+00,   1.90544551e-05]])
+    res_disptest = np.array([
+        [1.2647363371056005e+02, 0.0000000000000000e+00],
+        [1.2647363371056124e+02, 0.0000000000000000e+00],
+        [1.1939362149777617e+02, 0.0000000000000000e+00],
+        [4.5394051864300318e+00, 5.6413139746586543e-06],
+        [4.5394051864300318e+00, 5.6413139746586543e-06],
+        [2.9164548934767525e+00, 3.5403391013549782e-03],
+        [4.2714141112771529e+00, 1.9423733575592056e-05]
+        ])
     res_disptest_g = [17.670784788586968, 2.6262956791721383e-05]
 
 
 class TestScoreTestPoisson(TestScoreTest):
+    # same as GLM above but for discrete Poisson
     # compares score to wald, and regression test for pvalues
     rtol_ws = 5e-3
     atol_ws = 0
@@ -140,13 +154,15 @@ class TestScoreTestPoisson(TestScoreTest):
     # regression numbers
     res_pvalue = [0.31786373532550893, 0.32654081685271297]
     skip_wooldridge = False
-    res_disptest = np.array([[ 0.13927919,  0.44461477],
-                             [ 0.13927919,  0.44461477],
-                             [ 0.21295545,  0.41568086],
-                             [ 0.14935018,  0.88127732],
-                             [ 0.14935018,  0.88127732],
-                             [ 0.14841761,  0.8820132 ],
-                             [ 0.22836064,  0.81936588]])
+    res_disptest = np.array([
+        [0.1392791916012637, 0.8892295323009857],
+        [0.1392791916012645, 0.8892295323009850],
+        [0.2129554490802097, 0.8313617120611572],
+        [0.1493501809372359, 0.8812773205886350],
+        [0.1493501809372359, 0.8812773205886350],
+        [0.1454862255574059, 0.8843269904545624],
+        [0.2281321688124869, 0.8195434922982738]
+        ])
     res_disptest_g = [0.052247629593715761, 0.81919738867722225]
 
     @classmethod
@@ -176,16 +192,52 @@ class TestScoreTestPoisson(TestScoreTest):
 
 
 class TestScoreTestPoissonDispersed(TestScoreTestPoisson):
+    # same as GLM above but for discrete Poisson
     rtol_ws = 0.11
     atol_ws = 0.015
     rtol_wooldridge = 0.03
     dispersed = True  # Poisson is mis-specified
     res_pvalue = [5.412978775609189e-14, 0.05027602575743518]
-    res_disptest = np.array([[  1.26473634e+02,   0.00000000e+00],
-                             [  1.26473634e+02,   0.00000000e+00],
-                             [  1.19393621e+02,   0.00000000e+00],
-                             [  4.53940519e+00,   5.64131397e-06],
-                             [  4.53940519e+00,   5.64131397e-06],
-                             [  2.98154154e+00,   2.86801135e-03],
-                             [  4.27569194e+00,   1.90544551e-05]])
+    res_disptest = np.array([
+        [1.2647363371056005e+02, 0.0000000000000000e+00],
+        [1.2647363371056124e+02, 0.0000000000000000e+00],
+        [1.1939362149777617e+02, 0.0000000000000000e+00],
+        [4.5394051864300318e+00, 5.6413139746586543e-06],
+        [4.5394051864300318e+00, 5.6413139746586543e-06],
+        [2.9164548934767525e+00, 3.5403391013549782e-03],
+        [4.2714141112771529e+00, 1.9423733575592056e-05]
+        ])
     res_disptest_g = [17.670784788586968, 2.6262956791721383e-05]
+
+
+class TestScoreTestGaussian(CheckScoreTest):
+    # compares score to wald, and regression test for pvalues
+    rtol_ws = 0.01
+    atol_ws = 0
+    rtol_wooldridge = 0.06
+    dispersed = False
+    # regression numbers for nonrobust and HC0
+    res_pvalue = [0.44423875090566467, 0.4370837418475849]
+    # TODO: check why wooldrige is not very close, which pval1, pval2, pval3 ?
+    skip_wooldridge = True
+
+    @classmethod
+    def setup_class(cls):
+        nobs, k_vars = 500, 5
+
+        np.random.seed(786452)
+        x = np.random.randn(nobs, k_vars)
+        x[:, 0] = 1
+        x2 = np.random.randn(nobs, 2)
+        xx = np.column_stack((x, x2))
+
+        if cls.dispersed:
+            het = np.random.randn(nobs)
+            y = np.random.randn(nobs) + x.sum(1) * 0.5 + het
+            #y_mc = np.random.negative_binomial(np.exp(x.sum(1) * 0.5), 2)
+        else:
+            y = np.random.randn(nobs) + x.sum(1) * 0.5
+
+        cls.exog_extra = x2
+        cls.model_full = GLM(y, xx, family=families.Gaussian())
+        cls.model_drop = GLM(y, x, family=families.Gaussian())
