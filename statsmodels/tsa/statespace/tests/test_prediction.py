4bf098941523ce9e202da45498f82d49fb517c04
Chad Fulton <chad@chadfulton.com>
2022-01-07 22:09:31 -0500

    TST: Add tests for dynamic prediction

--- statsmodels/tsa/statespace/tests/test_prediction.py
+++ statsmodels/tsa/statespace/tests/test_prediction.py
@@ -495,3 +495,121 @@ def test_predicted_filtered_smoothed_TVSS(reset_randomstate):
     assert_allclose(s_signal.var_pred_mean[50:], fcast_signal_cov)
     assert_allclose(f_signal.var_pred_mean[50:], fcast_signal_cov)
     assert_allclose(p_signal.var_pred_mean[50:], fcast_signal_cov)
+
+
+@pytest.mark.parametrize('use_exog', [False, True])
+@pytest.mark.parametrize('trend', ['n', 'c', 't'])
+def test_predicted_filtered_dynamic_varmax(use_exog, trend):
+    endog = np.log(dta[['realgdp', 'cpi']])
+    if trend in ['n', 'c']:
+        endog = endog.diff().iloc[1:] * 100
+    if trend == 'n':
+        endog -= endog.mean()
+    exog = np.ones(100) if use_exog else None
+    if use_exog:
+        exog[20:40] = 2.
+
+    trend_params = [0.1, 0.2]
+    var_params = [0.5, -0.1, 0.0, 0.2]
+    exog_params = [1., 2.]
+    cov_params = [1., 0., 1.]
+
+    params = []
+    if trend in ['c', 't']:
+        params += trend_params
+    params += var_params
+    if use_exog:
+        params += exog_params
+    params += cov_params
+
+    # Compute basic model with 50 observations
+    x_fit1 = exog[:50] if use_exog else None
+    x_fcast1 = exog[50:61] if use_exog else None
+    mod1 = varmax.VARMAX(endog[:50], order=(1, 0), trend=trend, exog=x_fit1)
+
+    res1 = mod1.filter(params)
+
+    # Compute basic model with only 20 observations
+    x_fit2 = exog[:20] if use_exog else None
+    x_fcast2 = exog[20:61] if use_exog else None
+    mod2 = varmax.VARMAX(endog[:20], order=(1, 0), trend=trend, exog=x_fit2)
+    res2 = mod2.filter(params)
+
+    # Test predictions
+    p1 = res1.get_prediction(start=0, dynamic=20, end=60, exog=x_fcast1)
+    p2 = res2.get_prediction(start=0, end=60, exog=x_fcast2)
+    assert_allclose(p1.predicted_mean, p2.predicted_mean)
+    assert_allclose(p1.var_pred_mean, p2.var_pred_mean)
+
+    p1 = res1.get_prediction(start=2, dynamic=18, end=60, exog=x_fcast1)
+    p2 = res2.get_prediction(start=2, end=60, exog=x_fcast2)
+    assert_allclose(p1.predicted_mean, p2.predicted_mean)
+    assert_allclose(p1.var_pred_mean, p2.var_pred_mean)
+
+    p1 = res1.get_prediction(start=20, dynamic=True, end=60, exog=x_fcast1)
+    p2 = res2.get_prediction(start=20, end=60, exog=x_fcast2)
+    assert_allclose(p1.predicted_mean, p2.predicted_mean)
+    assert_allclose(p1.var_pred_mean, p2.var_pred_mean)
+
+    # Test predictions, filtered
+    p1 = res1.get_prediction(start=0, dynamic=20, end=60, exog=x_fcast1,
+                             information_set='filtered')
+    p2 = res2.get_prediction(start=0, end=60, exog=x_fcast2,
+                             information_set='filtered')
+    assert_allclose(p1.predicted_mean, p2.predicted_mean)
+    assert_allclose(p1.var_pred_mean, p2.var_pred_mean)
+
+    p1 = res1.get_prediction(start=2, dynamic=18, end=60, exog=x_fcast1,
+                             information_set='filtered')
+    p2 = res2.get_prediction(start=2, end=60, exog=x_fcast2,
+                             information_set='filtered')
+    assert_allclose(p1.predicted_mean, p2.predicted_mean)
+    assert_allclose(p1.var_pred_mean, p2.var_pred_mean)
+
+    p1 = res1.get_prediction(start=20, dynamic=True, end=60, exog=x_fcast1,
+                             information_set='filtered')
+    p2 = res2.get_prediction(start=20, end=60, exog=x_fcast2,
+                             information_set='filtered')
+    assert_allclose(p1.predicted_mean, p2.predicted_mean)
+    assert_allclose(p1.var_pred_mean, p2.var_pred_mean)
+
+    # Test signals
+    p1 = res1.get_prediction(start=0, dynamic=20, end=60, exog=x_fcast1,
+                             signal_only=True)
+    p2 = res2.get_prediction(start=0, end=60, exog=x_fcast2, signal_only=True)
+    assert_allclose(p1.predicted_mean, p2.predicted_mean)
+    assert_allclose(p1.var_pred_mean, p2.var_pred_mean)
+
+    p1 = res1.get_prediction(start=2, dynamic=18, end=60, exog=x_fcast1,
+                             signal_only=True)
+    p2 = res2.get_prediction(start=2, end=60, exog=x_fcast2, signal_only=True)
+    assert_allclose(p1.predicted_mean, p2.predicted_mean)
+    assert_allclose(p1.var_pred_mean, p2.var_pred_mean)
+
+    p1 = res1.get_prediction(start=20, dynamic=True, end=60, exog=x_fcast1,
+                             signal_only=True)
+    p2 = res2.get_prediction(start=20, end=60, exog=x_fcast2, signal_only=True)
+    assert_allclose(p1.predicted_mean, p2.predicted_mean)
+    assert_allclose(p1.var_pred_mean, p2.var_pred_mean)
+
+    # Test signal, filtered
+    p1 = res1.get_prediction(start=0, dynamic=20, end=60, exog=x_fcast1,
+                             signal_only=True, information_set='filtered')
+    p2 = res2.get_prediction(start=0, end=60, exog=x_fcast2, signal_only=True,
+                             information_set='filtered')
+    assert_allclose(p1.predicted_mean, p2.predicted_mean)
+    assert_allclose(p1.var_pred_mean, p2.var_pred_mean)
+
+    p1 = res1.get_prediction(start=2, dynamic=18, end=60, exog=x_fcast1,
+                             signal_only=True, information_set='filtered')
+    p2 = res2.get_prediction(start=2, end=60, exog=x_fcast2, signal_only=True,
+                             information_set='filtered')
+    assert_allclose(p1.predicted_mean, p2.predicted_mean)
+    assert_allclose(p1.var_pred_mean, p2.var_pred_mean)
+
+    p1 = res1.get_prediction(start=20, dynamic=True, end=60, exog=x_fcast1,
+                             signal_only=True, information_set='filtered')
+    p2 = res2.get_prediction(start=20, end=60, exog=x_fcast2, signal_only=True,
+                             information_set='filtered')
+    assert_allclose(p1.predicted_mean, p2.predicted_mean)
+    assert_allclose(p1.var_pred_mean, p2.var_pred_mean)
