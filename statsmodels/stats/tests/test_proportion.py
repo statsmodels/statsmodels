c72bdbdae8d88c612e6d1c4fb9ab2c9e0f82b5ed
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2022-02-25 19:11:32 +0000

    BUG: Allows arrays in porportions
    Allow arrays
    Enforece symmetry

--- statsmodels/stats/tests/test_proportion.py
+++ statsmodels/stats/tests/test_proportion.py
@@ -29,6 +29,7 @@ from statsmodels.stats.proportion import (
 )
 from statsmodels.tools.sm_exceptions import HypothesisTestWarning
 from statsmodels.tools.testing import Holder
+from statsmodels.stats.tests.results.results_proportion import res_binom, res_binom_methods
 
 probci_methods = {'agresti_coull': 'agresti-coull',
                   'normal': 'asymptotic',
@@ -37,31 +38,27 @@ probci_methods = {'agresti_coull': 'agresti-coull',
                   'jeffreys': 'bayes'
                   }
 
-
-def test_confint_proportion():
-    from .results.results_proportion import res_binom, res_binom_methods
-
-
-    for case in res_binom:
-        count, nobs = case
-        for method in probci_methods:
-            idx = res_binom_methods.index(probci_methods[method])
-            res_low = res_binom[case].ci_low[idx]
-            res_upp = res_binom[case].ci_upp[idx]
-            if np.isnan(res_low) or np.isnan(res_upp):
-                continue
-            if (count == 0 or count == nobs) and method == 'jeffreys':
-                # maybe a bug or different corner case definition
-                continue
-            if method == 'jeffreys' and nobs == 30:
-                # something is strange in extreme case e.g 0/30 or 1/30
-                continue
-            ci = proportion_confint(count, nobs, alpha=0.05, method=method)
-            # we impose that confint is in [0, 1]
-            res_low = max(res_low, 0)
-            res_upp = min(res_upp, 1)
-            assert_almost_equal(ci, [res_low, res_upp], decimal=6,
-                                err_msg=repr(case) + method)
+@pytest.mark.parametrize("case",res_binom)
+@pytest.mark.parametrize("method",probci_methods)
+def test_confint_proportion(method, case):
+    count, nobs = case
+    idx = res_binom_methods.index(probci_methods[method])
+    res_low = res_binom[case].ci_low[idx]
+    res_upp = res_binom[case].ci_upp[idx]
+    if np.isnan(res_low) or np.isnan(res_upp):
+        pytest.skip("Skipping due to NaN value")
+    if (count == 0 or count == nobs) and method == 'jeffreys':
+        # maybe a bug or different corner case definition
+        pytest.skip("Skipping nobs 0 or count and jeffreys")
+    if method == 'jeffreys' and nobs == 30:
+        # something is strange in extreme case e.g 0/30 or 1/30
+        pytest.skip("Skipping nobs is 30 and jeffreys due to extreme case problem")
+    ci = proportion_confint(count, nobs, alpha=0.05, method=method)
+    # we impose that confint is in [0, 1]
+    res_low = max(res_low, 0)
+    res_upp = min(res_upp, 1)
+    assert_almost_equal(ci, [res_low, res_upp], decimal=6,
+                        err_msg=repr(case) + method)
 
 
 @pytest.mark.parametrize('method', probci_methods)
@@ -927,8 +924,12 @@ def test_power_2indep():
 
 
 @pytest.mark.parametrize("count", np.arange(10, 90, 5))
-@pytest.mark.parametrize("method", list(probci_methods.keys()) + ["binom_test"])
-def test_ci_symmetry(count, method):
+@pytest.mark.parametrize(
+    "method", list(probci_methods.keys()) + ["binom_test"]
+)
+@pytest.mark.parametrize("array_like", [False, True])
+def test_ci_symmetry(count, method, array_like):
+    _count = [count] * 3 if array_like else count
     n = 100
     a = proportion_confint(count, n, method=method)
     b = proportion_confint(n - count, n, method=method)
@@ -937,9 +938,12 @@ def test_ci_symmetry(count, method):
 
 @pytest.mark.parametrize("nobs", [47, 50])
 @pytest.mark.parametrize("count", np.arange(48))
-def test_ci_symmetry_binom_test(nobs, count):
-    a = proportion_confint(count, nobs, method="binom_test")
-    b = proportion_confint(nobs - count, nobs, method="binom_test")
+@pytest.mark.parametrize("array_like", [False, True])
+def test_ci_symmetry_binom_test(nobs, count, array_like):
+    _count = [count] * 3 if array_like else count
+    nobs_m_count = [nobs - count] * 3 if array_like else nobs - count
+    a = proportion_confint(_count, nobs, method="binom_test")
+    b = proportion_confint(nobs_m_count, nobs, method="binom_test")
     assert_allclose(np.array(a), 1.0 - np.array(b[::-1]))
 
 
@@ -955,3 +959,14 @@ def test_int_check():
     c = proportion_confint(21, 47.0, method="binom_test")
     assert_allclose(a, b)
     assert_allclose(a, c)
+
+
+@pytest.mark.parametrize("count", np.arange(10, 90, 5))
+@pytest.mark.parametrize(
+    "method", list(probci_methods.keys()) + ["binom_test"]
+)
+def test_ci_symmetry_array(count, method):
+    n = 100
+    a = proportion_confint([count, count], n, method=method)
+    b = proportion_confint([n - count, n - count], n, method=method)
+    assert_allclose(np.array(a), 1.0 - np.array(b[::-1]))
