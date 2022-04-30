d8508da11fcc9d612c2c8a1513ec8d9fd57f05c0
Kevin Sheppard <kevin.sheppard@gmail.com>
2022-04-30 23:56:18 +0100

    MAINT: Uperbound on Cython for CI
    Ensure Cython < 3

--- tools/ci/appveyor_pip.bat
+++ tools/ci/appveyor_pip.bat
@@ -2,7 +2,7 @@ REM Install packages using pip
 set PATH=%PYTHON%;%PYTHON%\Scripts;%PATH%
 python -m pip install -U pip
 IF Defined SCIPY (
-    python -m pip install numpy scipy==%SCIPY% cython pandas nose patsy
+    python -m pip install numpy scipy==%SCIPY% "cython>=0.29.28,<3.0.0" pandas nose patsy
 ) else (
-    python -m pip install numpy scipy cython pandas nose patsy
+    python -m pip install numpy scipy "cython>=0.29.28,<3.0.0" pandas nose patsy
 )
