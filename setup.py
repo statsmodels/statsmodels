c09ec315d80b802fd44a67bd18ec8e9a784359a0
Kevin Sheppard <bashtage@users.noreply.github.com>
2022-10-25 08:07:07 +0100

    Merge pull request #8462 from gw-moore/fix-develop-installs
    MAINT: fix develop installs

--- setup.py
+++ setup.py
@@ -53,12 +53,12 @@ with open("requirements.txt", encoding="utf-8") as req:
     for line in req.readlines():
         INSTALL_REQUIRES.append(line.split("#")[0].strip())
 
+CYTHON_MIN_VER = "0.29.32"  # released 2020
+
 DEVELOP_REQUIRES = []
 with open("requirements-dev.txt", encoding="utf-8") as req:
     for line in req.readlines():
         DEVELOP_REQUIRES.append(line.split("#")[0].strip())
-
-CYTHON_MIN_VER = "0.29.32"  # released 2020
 
 EXTRAS_REQUIRE = {
     "build": ["cython>=" + CYTHON_MIN_VER],
