4efb873f81502318804e55bfe3c256b7e3187528
Josef Perktold <josef.pktd@gmail.com>
2023-04-20 12:39:24 -0400

    ENH: add GMMIVCF control function for endogenous explanatory variable

---
+++ statsmodels/causal/ivcontrol_function.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 20 09:06:01 2023
+
+Author: Josef Perktold
+License: BSD-3
+"""
+
+import numpy as np
+
+from statsmodels.base.model import LikelihoodModel
+from statsmodels.sandbox.regression.gmm import GMM
+from statsmodels.genmod.generalized_linear_model import GLM
+
+
+def momcond(params, modelcf=None):
+    mod_first = modelcf.model_first
+    y2 = modelcf.endog
+    x2 = modelcf.explan
+
+    params1 = params[:2]
+    params2 = params[2:]
+    mom1 = mod_first.score_obs(params1, scale=1)
+    resid = mod_first.endog - mod_first.predict(params1)
+    mod2 = GLM(y2, np.column_stack((x2, resid)), family=modelcf.family)
+    mom2 = mod2.score_obs(params2)
+
+    return np.column_stack([mom1, mom2])
+
+
+class GMMIVCF(GMM):
+    def momcond(self, params):
+        return momcond(params, modelcf=self.model)
+
+
+class GLMIVCF(LikelihoodModel):
+
+    def __init__(self, endog, explan, endog_explan, instruments, family=None
+                 ):
+        super().__init__(
+            endog,
+            explan=explan,
+            endog_explan=endog_explan,
+            instruments=instruments,
+            )
+
+        self.family = family
+
+        y1 = self.endog_explan
+        x1 = self.instruments
+        self.model_first = GLM(y1, x1)
+
+    def fit(self):
+        y2 = self.endog
+        x2 = self.explan
+
+        res_first = self.model_first.fit()
+        res_outcome = GLM(y2,
+                          np.column_stack((x2, res_first.resid_response)),
+                          family=self.family
+                          ).fit()
+
+        params = np.concatenate([res_first.params, res_outcome.params])
+        mod_gmm = GMMIVCF(y2, x2, None, k_moms=len(params),
+                          param_names=[f"s{i}" for i in range(len(params))])
+        mod_gmm.model = self
+        res_gmm = mod_gmm.fit(start_params=params, maxiter=0)
+        return res_gmm
