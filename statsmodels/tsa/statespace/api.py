b27a655cd59b4dd3337237d633eac14b93ff3bd4
leoyzen <leoyzen@gmail.com>
2024-01-19 22:39:42 +0800

    Add Tbats Model and tools to statespace api file

--- statsmodels/tsa/statespace/api.py
+++ statsmodels/tsa/statespace/api.py
@@ -4,4 +4,5 @@ from .sarimax import SARIMAX
 from .exponential_smoothing import ExponentialSmoothing
 from .mlemodel import MLEModel, MLEResults
 from .initialization import Initialization
+from .tbats import TBATSModel, TBATSResults, seasonal_fourier_k_select_ic, tbats_k_order_select_ic
 from . import tools
