68948b5ae2d76182fda4cf4b183428b0ace702b6
Kamil <32775019+quant12345@users.noreply.github.com>
2024-05-14 18:50:41 +0500

    Update conditional_models.py

--- statsmodels/discrete/conditional_models.py
+++ statsmodels/discrete/conditional_models.py
@@ -594,7 +594,6 @@ class ConditionalMNLogit(_ConditionalModel):
             x = lpr[ii, :]
             jj = np.arange(x.shape[0], dtype=int)
             y = self.endog[ii]
-              
             denom = np.sum(x[jj, list(itertools.permutations(y))], axis=1)
             ll += x[(jj, y)].sum() - logsumexp(denom)
 
