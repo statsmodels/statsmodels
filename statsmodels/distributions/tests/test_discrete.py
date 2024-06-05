c4da5d31479e0111a91eba4fbb041b87671c8247
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2024-06-06 00:30:57 +0530

    Convert to int32 before array element occurrence count

--- statsmodels/distributions/tests/test_discrete.py
+++ statsmodels/distributions/tests/test_discrete.py
@@ -4,7 +4,7 @@ from numpy.testing import assert_allclose, assert_equal
 from scipy import stats
 from scipy.stats import poisson, nbinom
 
-from statsmodels.tools.tools import Bunch
+from statsmodels.tools.tools import Bunch, is_wasm
 
 from statsmodels.distributions.discrete import (
     genpoisson_p,
@@ -330,6 +330,10 @@ class CheckDiscretized():
         dfr = mod.get_distr(res.params)
         nobs_rvs = 500
         rvs = dfr.rvs(size=nobs_rvs)
+        # TypeError: Cannot cast array data from dtype('int64') to dtype('int32') according to the rule 'safe',
+        # to fix this, change the dtype of rvs to int32 so that it can be passed to np.bincount
+        if is_wasm():
+            rvs = rvs.astype(np.int32)
         freq = np.bincount(rvs)
         p = mod.predict(res.params, which="probs", k_max=nobs_rvs)
         k = len(freq)
