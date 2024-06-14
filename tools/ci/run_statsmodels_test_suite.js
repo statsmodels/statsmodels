7820ccca43ede08c8246f8ed1bedae3939751c54
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2024-06-15 02:41:17 +0530

    Try using `runPython` instead of `runPythonAsync`

--- tools/ci/run_statsmodels_test_suite.js
+++ tools/ci/run_statsmodels_test_suite.js
@@ -38,7 +38,7 @@ async function main() {
         await pyodide.runPythonAsync("import micropip; micropip.install('pytest')");
         await pyodide.runPythonAsync("import micropip; micropip.install('pytest-cov')");
         await pyodide.runPythonAsync("import micropip; micropip.install('matplotlib')");
-        await pyodide.runPythonAsync(`
+        pyodide.runPython(`
         import sys
         import statsmodels
         result = statsmodels.test(['-ra', '--skip-examples', '--skip-slow'], exit=True)
