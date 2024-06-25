d02c6d601eb7509d7c062452493fc01310605491
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2024-06-25 10:04:21 +0100

    REF: Remove numpy testing import from test runner
    Refactor to avoid importing functions from numpy testing
    when importing statsmodels

--- statsmodels/stats/libqsturng/__init__.py
+++ statsmodels/stats/libqsturng/__init__.py
@@ -1,6 +1,6 @@
 from .qsturng_ import psturng, qsturng, p_keys, v_keys
 
-from statsmodels.tools._testing import PytestTester
+from statsmodels.tools._test_runner import PytestTester
 
 __all__ = ['p_keys', 'psturng', 'qsturng', 'v_keys', 'test']
 
