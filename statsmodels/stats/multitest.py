de4090a73b7feb06a6f51ac139c745fc7d491912
Jake Soloff <jakesoloff@gmail.com>
2024-08-26 15:29:04 -0500

    TST: test lfdrcorrection

--- statsmodels/stats/multitest.py
+++ statsmodels/stats/multitest.py
@@ -8,9 +8,9 @@ License: BSD-3
 """
 
 import numpy as np
+from sklearn.isotonic import isotonic_regression
 
 from statsmodels.stats._knockoff import RegressionFDR
-from sklearn.isotonic import isotonic_regression
 
 __all__ = [
     "NullDistribution",
@@ -96,7 +96,6 @@ _alias_list = [['b', 'bonf', 'bonferroni'],
                ['fdr_gbs'],
                ['lfdr', 'lfdr_sl'],
                ]
-
 
 
 multitest_alias = {}
@@ -419,6 +418,7 @@ def fdrcorrection(pvals, alpha=0.05, method="indep", is_sorted=False):
     else:
         return reject, pvals_corrected
 
+
 def lfdrcorrection(pvals, pi_0_est=1., alpha=0.2, is_sorted=False):
     '''
     p-value correction for the local false discovery rate (lfdr).
@@ -443,7 +443,7 @@ def lfdrcorrection(pvals, pi_0_est=1., alpha=0.2, is_sorted=False):
     rejected : ndarray, bool
         True if a hypothesis is rejected, False if not
     pvalue-corrected : ndarray
-        estimated lfdr values, i.e. adjusted p-values 
+        estimated lfdr values, i.e. adjusted p-values
 
     Examples
     --------
@@ -453,22 +453,21 @@ def lfdrcorrection(pvals, pi_0_est=1., alpha=0.2, is_sorted=False):
     >>> pvals = np.random.rand(30)
     >>> rejected, lvals = lfdrcorrection(pvals)
 
-
     References
     ----------
 
-    U Grenander (1956). On the theory of mortality measurement: part II. 
+    U Grenander (1956). On the theory of mortality measurement: part II.
     Scandinavian Actuarial Journal, 39, 125-153.
 
-    B Efron, R Tibshirani, J D Storey, and V Tusher (2001). Empirical Bayes 
-    analysis of a microarray experiment. Journal of the American Statistical 
+    B Efron, R Tibshirani, J D Storey, and V Tusher (2001). Empirical Bayes
+    analysis of a microarray experiment. Journal of the American Statistical
     Association, 96:456, 1151-1160.
 
-    K Strimmer (2008). A unified approach to false discovery rate estimation. 
+    K Strimmer (2008). A unified approach to false discovery rate estimation.
     BMC Bioinformatics, 9, 1-14.
 
-    J A Soloff, D Xiang, and W Fithian (2024). The edge of discovery: 
-    Controlling the local false discovery rate at the margin. The Annals of 
+    J A Soloff, D Xiang, and W Fithian (2024). The edge of discovery:
+    Controlling the local false discovery rate at the margin. The Annals of
     Statistics, 52:2, 580-601.
 
     Notes
@@ -482,7 +481,7 @@ def lfdrcorrection(pvals, pi_0_est=1., alpha=0.2, is_sorted=False):
     assert pvals.ndim == 1, "pvals must be 1-dimensional, that is of shape (n,)"
 
     nobs = len(pvals)
-    
+
     if not is_sorted:
         pvals_sortind = np.argsort(pvals)
         pvals_sorted = np.take(pvals, pvals_sortind)
@@ -494,9 +493,9 @@ def lfdrcorrection(pvals, pi_0_est=1., alpha=0.2, is_sorted=False):
 
     # compute left-hand slopes of least concave majorant of empirical cdf
     w = W[1:] - W[:-1]
-    slopes = isotonic_regression(np.ones(nobs)/(nobs*w), 
-                                sample_weight=w.copy(), 
-                                increasing=False)
+    slopes = isotonic_regression(
+        np.ones(nobs)/(nobs*w), sample_weight=w.copy(), increasing=False
+    )
 
     # return fitted values in original order
     if not is_sorted:
@@ -504,6 +503,7 @@ def lfdrcorrection(pvals, pi_0_est=1., alpha=0.2, is_sorted=False):
     ell_values = np.minimum(1, pi_0_est/slopes)
 
     return (ell_values <= alpha), ell_values
+
 
 def fdrcorrection_twostage(
     pvals, alpha=0.05, method="bky", maxiter=1, iter=None, is_sorted=False
