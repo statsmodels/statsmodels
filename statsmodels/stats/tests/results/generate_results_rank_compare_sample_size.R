44e1d4960434ea975d462fcb63ca7ef5c5127bb7
YangWu1227 <yangwu2020@gmail.com>
2024-10-17 21:07:34 -0400

    test: cover infeasible sample size exceptions

--- statsmodels/stats/tests/results/generate_results_rank_compare_sample_size.R
+++ statsmodels/stats/tests/results/generate_results_rank_compare_sample_size.R
@@ -44,16 +44,22 @@ generate_result_matrices <- function() {
     ),
     input_alpha = c(0.05, 0.05, 0.05),
     power = c(0.8, 0.8, 0.8),
-    t = c(0.5, 0.55, 0.45)
+    t = c(0.5, 0.55, 0.45),
+    # Seizure example -> relative effect < 0.5
+    # Nasal mucosa example -> relative effect > 0.5
+    # Kidney weight example -> relative effect > 0.5
+    onetail_alternative = c("smaller", "larger", "larger")
   )
 
   data <- pmap(
     .l = inputs_grid,
-    .f = function(x1, x2, input_alpha, power, t) {
+    .f = function(x1, x2, input_alpha, power, t, onetail_alternative) {
       one_two_sided_data <- map(
         .x = c("two-sided", "one-sided"),
         .f = function(alternative) {
-          # Since WMWSSP only supports the two-sided case, multiply input alpha by 2 for the one-sided test
+          # Match the 'alternative' options in the python function (i.e., 'two-sided', 'smaller', or 'larger')
+          alternative <- ifelse(alternative == "two-sided", alternative, onetail_alternative)
+          # Since WMWSSP only supports the two-sided case, multiply input alpha by 2 for the one-sided case
           alpha <- ifelse(alternative == "two-sided", input_alpha, input_alpha * 2)
           result_matrix <- WMWSSP(x1 = x1, x2 = x2, alpha = alpha, power = power, t = t)
           # Reset alpha back to the input alpha level, so it can be used for testing
