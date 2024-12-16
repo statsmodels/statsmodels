a87ba8140790fc73f23acdf268350dacb65b6a96
Kevin Sheppard <bashtage@users.noreply.github.com>
2024-12-16 10:59:51 +0000

    TST: Restore skip when no x13 available

--- statsmodels/tsa/tests/test_x13.py
+++ statsmodels/tsa/tests/test_x13.py
@@ -13,6 +13,8 @@ from statsmodels.tsa.x13 import (
 
 x13path = _find_x12()
 
+pytestmark = pytest.mark.skipif(x13path is False, reason="X13/X12 not available")
+
 dta = macrodata.load_pandas().data
 index = pd.period_range(start="1959Q1", end="2009Q3", freq="Q")
 dta.index = index
