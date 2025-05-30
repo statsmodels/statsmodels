387393ef08c9cb17e09943d4a94c17eb072ede41
Victor Vargas <vvargas@uco.es>
2025-05-30 09:06:59 +0200

    TST: Added tukey homogeneous subsets unbalanced data tests and example

--- statsmodels/sandbox/stats/tests/test_multicomp.py
+++ statsmodels/sandbox/stats/tests/test_multicomp.py
@@ -12,6 +12,15 @@ import pandas as pd
 from statsmodels.sandbox.stats.multicomp import tukey_pvalues
 from .results import result_hsubsets_dataframe
 
+@pytest.fixture
+def sample_data_balanced():
+    df = pd.read_csv('./tests/results/sample_data_balanced.csv')
+    return df
+
+@pytest.fixture
+def sample_data_unbalanced():
+    df = pd.read_csv('./tests/results/sample_data_unbalanced.csv')
+    return df
 
 @pytest.mark.skipif(not SP_LT_116, reason="mvndst removed in SciPy 1.16")
 def test_tukey_pvalues():
@@ -30,15 +39,16 @@ def test_tukey_pvalues():
         (0.1, result_hsubsets_dataframe.expected_df_alpha01),
     ],
 )
-def test_create_homogeneous_subsets_dataframe(
+def test_create_homogeneous_subsets_dataframe_balanced(
     alpha,
     expected_df,
+    sample_data_balanced
 ):
     from statsmodels.stats.multicomp import pairwise_tukeyhsd
     from statsmodels.sandbox.stats.multicomp import TukeyHSDResults
 
-    data = result_hsubsets_dataframe.data
-    groups = result_hsubsets_dataframe.groups
+    data = sample_data_balanced["data"]
+    groups = sample_data_balanced["groups"]
 
     tukey = pairwise_tukeyhsd(
         endog=data,
@@ -48,5 +58,34 @@ def test_create_homogeneous_subsets_dataframe(
     assert isinstance(tukey, TukeyHSDResults)
     hs_df = tukey.create_homogeneous_subsets_dataframe()
     assert isinstance(hs_df, pd.DataFrame)
-    assert hs_df.equals(expected_df)
+    assert pd.testing.assert_frame_equal(hs_df, expected_df) is None
+    assert hs_df.index.name == "Group"
+
+
+@pytest.mark.parametrize(
+    "alpha, expected_df",
+    [
+        (0.05, result_hsubsets_dataframe.expected_df_unbalanced_alpha005),
+    ],
+)
+def test_create_homogeneous_subsets_dataframe_unbalanced(
+    alpha,
+    expected_df,
+    sample_data_unbalanced
+):
+    from statsmodels.stats.multicomp import pairwise_tukeyhsd
+    from statsmodels.sandbox.stats.multicomp import TukeyHSDResults
+
+    data = sample_data_unbalanced["data"]
+    groups = sample_data_unbalanced["groups"]
+
+    tukey = pairwise_tukeyhsd(
+        endog=data,
+        groups=groups,
+        alpha=alpha,
+    )
+    assert isinstance(tukey, TukeyHSDResults)
+    hs_df = tukey.create_homogeneous_subsets_dataframe()
+    assert isinstance(hs_df, pd.DataFrame)
+    assert pd.testing.assert_frame_equal(hs_df, expected_df) is None
     assert hs_df.index.name == "Group"
