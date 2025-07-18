640a4887d98cddb09a1c8da779569d6c0c975d57
Kevin Sheppard <bashtage@users.noreply.github.com>
2025-07-18 14:49:13 +0100

    CI: Accept all conda TOS

--- tools/ci/azure/install-posix.sh
+++ tools/ci/azure/install-posix.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 if [[ ${USE_CONDA} == "true" ]]; then
+  conda tos accept
   conda config --set always_yes true
   conda update --all --quiet
   conda create -n statsmodels-test python=${PYTHON_VERSION} -y
-  conda tos accept
   conda init
   echo ${PATH}
   source activate statsmodels-test
