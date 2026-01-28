dddd074ea121118729d01c5f8677c8bae43bdba2
Deshan <deshang74@gmail.com>
2026-01-28 22:13:07 +0545

    docs: update dead Marden reference to stable Orthogonal Projection handout

--- statsmodels/stats/anova.py
+++ statsmodels/stats/anova.py
@@ -681,6 +681,6 @@ if __name__ == "__main__":
 
     # using Monette-Fox slides and Marden class notes for linear algebra /
     # orthogonal complement
-    # https://netfiles.uiuc.edu/jimarden/www/Classes/STAT324/
+    # https://people.math.aau.dk/~rw/Undervisning/MM_BI/Handouts/anova_orthog.pdf
 
     table = anova_lm(moore_lm, typ=2)
