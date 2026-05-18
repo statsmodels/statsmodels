ee738acc1f02af1ee26dfaa1bbf2fec9376a7f75
pierinicoelho <pierinicoelho@gmail.com>
2026-05-18 17:58:26 -0300

    TST: Relax numerical tolerance in test_smoothed_state_autocovariances_forwards_oos
    The test used default rtol=1e-07 while the actual relative difference
    on some platforms is 1.67e-07 (1/828 elements). This is consistent
    with the tolerance used in the backwards variant of the same test
    (rtol=1e-6).

--- statsmodels/tsa/statespace/tests/test_smoothing.py
+++ statsmodels/tsa/statespace/tests/test_smoothing.py
@@ -1265,7 +1265,7 @@ def test_smoothed_state_autocovariances_forwards_oos(missing,
     acov1 = res.smoothed_state_autocovariance(
         -1, end=mod_oos.nobs, extend_kwargs=extend_kwargs).transpose(2, 0, 1)
     assert_equal(acov1.shape, (mod_oos.nobs, mod.k_states, mod.k_states))
-    assert_allclose(acov1[:, :2, :2], desired_acov1[1:])
+    assert_allclose(acov1[:, :2, :2], desired_acov1[1:], rtol=1e-6)
 
     # Note: now we can compute up to Cov(mod_oos.nobs - 1, mod_oos.nobs + 1)
     # using a model that has state space matrices defined up to mod_oos.nobs.
@@ -1279,7 +1279,7 @@ def test_smoothed_state_autocovariances_forwards_oos(missing,
         -2, end=mod_oos.nobs - 1,
         extend_kwargs=extend_kwargs).transpose(2, 0, 1)
     assert_equal(acov2.shape, (mod_oos.nobs - 1, mod.k_states, mod.k_states))
-    assert_allclose(acov2[:, :2, :2], desired_acov2[2:])
+    assert_allclose(acov2[:, :2, :2], desired_acov2[2:], rtol=1e-6)
 
     # Note: now we can compute up to Cov(mod_oos.nobs - 2, mod_oos.nobs + 1)
     # using a model that has state space matrices defined up to mod_oos.nobs.
@@ -1288,7 +1288,7 @@ def test_smoothed_state_autocovariances_forwards_oos(missing,
         -3, end=mod_oos.nobs - 2,
         extend_kwargs=extend_kwargs).transpose(2, 0, 1)
     assert_equal(acov3.shape, (mod_oos.nobs - 2, mod.k_states, mod.k_states))
-    assert_allclose(acov3[:, :2, :2], desired_acov3[3:])
+    assert_allclose(acov3[:, :2, :2], desired_acov3[3:], rtol=1e-6)
 
 
 @pytest.mark.parametrize("missing", ["all", "partial", "mixed", None])
