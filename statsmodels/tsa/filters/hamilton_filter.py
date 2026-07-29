aa69f8e49e458f7cd145ab1ad321131fbb758d31
Kevin Sheppard <bashtage@users.noreply.github.com>
2026-07-29 12:57:44 +0100

    DOC: Add plot for hamilton_filter

--- statsmodels/tsa/filters/hamilton_filter.py
+++ statsmodels/tsa/filters/hamilton_filter.py
@@ -97,7 +97,15 @@ def hamilton_filter(x, h=8, p=4):
     >>> dta = sm.datasets.macrodata.load_pandas().data
     >>> index = pd.period_range('1959Q1', '2009Q3', freq='Q')
     >>> dta.set_index(index, inplace=True)
-    >>> cycle, trend = sm.tsa.filters.hamilton_filter(dta['realgdp'], h=8, p=4)
+    >>> cycle, trend = sm.tsa.filters.hamilton_filter(dta[["infl", "unemp"]], h=8, p=4)
+
+    >>> import matplotlib.pyplot as plt
+    >>> fig, ax = plt.subplots()
+    >>> cycle.plot(ax=ax, style=['r--', 'b-'])
+    >>> ax.set_title("Cycle extracted using Hamilton Filter (h=8, p=4)")
+    >>> plt.show()
+
+    .. plot:: plots/hamilton_filter_plot.py
     """
     pw = PandasWrapper(x)
     x = array_like(x, "x", maxdim=2)
