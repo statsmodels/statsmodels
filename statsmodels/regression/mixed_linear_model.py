a22274567f37a2c963e05c25462b1b27450b12ea
Aniket <148300120+Aniketsy@users.noreply.github.com>
2026-08-25 12:34:00 +0530

    Fix: cov_type in MixedLM.fit (#9695)
    Co-authored-by: Kevin Sheppard <bashtage@users.noreply.github.com>

--- statsmodels/regression/mixed_linear_model.py
+++ statsmodels/regression/mixed_linear_model.py
@@ -2306,7 +2306,9 @@ class MixedLM(base.LikelihoodModel):
         MixedLMResults
             The model instance containing the fitted results.
 
-        """
+        if "cov_type" in fit_kwargs:
+            raise NotImplementedError("cov_type is not supported in MixedLM.fit")
+
         _allowed_kwargs = [
             "gtol",
             "maxiter",
@@ -2316,6 +2318,7 @@ class MixedLM(base.LikelihoodModel):
             "tol",
             "disp",
             "maxls",
+            "cov_type",
         ]
         disallowed_kwargs = sorted(set(fit_kwargs).difference(_allowed_kwargs))
         if disallowed_kwargs:
